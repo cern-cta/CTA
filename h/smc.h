28547fed5daba4d3f7d928d5ca751b29dc651678
Jean-Damien Durand <jdurand@cern.ch>
1999-07-20 20:53:12 +0000

    Needed for the minimal first version of Ctape to compile

---
+++ h/smc.h
@@ -0,0 +1,48 @@
+/*
+ * $Id: smc.h,v 1.1 1999/07/20 20:53:12 jdurand Exp $
+ *
+ * $Log: smc.h,v $
+ * Revision 1.1  1999/07/20 20:53:12  jdurand
+ * Needed for the minimal first version of Ctape to compile
+ *
+ */
+
+/*
+ * Copyright (C) 1998 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+/*
+ * @(#)smc.h	1.3 05/26/98 CERN IT-PDP/DM   Jean-Philippe Baud
+ */
+
+struct robot_info {
+	char inquiry[32];
+	int transport_start;
+	int transport_count;
+	int slot_start;
+	int slot_count;
+	int port_start;
+	int port_count;
+	int device_start;
+	int device_count;
+};
+
+struct smc_element_info {
+	int element_address;
+	int element_type;
+	int state;
+	unsigned char asc;
+	unsigned char ascq;
+	int source_address;
+	char name[7];
+};
+
+struct smc_status {
+	unsigned char asc;
+	unsigned char ascq;
+	int save_errno;
+	int rc;		/* return code from send_scsi_cmd */
+	unsigned char sensekey;
+	int skvalid;	/* sense key is valid */
+};
