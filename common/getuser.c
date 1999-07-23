4344c768d386137b68eafbb8ca4bcfb5212cce26
Olof Barring <obarring@cern.ch>
1999-07-23 12:13:23 +0000

    Make MT safe

--- common/getuser.c
+++ common/getuser.c
@@ -1,30 +1,44 @@
 /*
- * Copyright (C) 1994-1998 by CERN CN-PDP/CS
+ * $Id: getuser.c,v 1.2 1999/07/23 12:13:23 obarring Exp $
+ * $Log: getuser.c,v $
+ * Revision 1.2  1999/07/23 12:13:23  obarring
+ * Make MT safe
+ *
+ */
+
+/*
+ * Copyright (C) 1994-1999 by CERN IT-PDP/DM
  * All rights reserved
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)getuser.c	1.4 03/26/99 CERN CN-SW/DC Felix Hassine";
+static char cvsId[] = "$Id: getuser.c,v 1.2 1999/07/23 12:13:23 obarring Exp $";
 #endif /* not lint */
 
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 #include <pwd.h>
+#include <Castor_limits.h>
+#include <serrno.h>
 #include <log.h>
 
 #if !defined(linux)
 extern char     *sys_errlist[] ;        /* System error list  */
 #endif
-extern int      errno ;                 /* Current system error index */
 
 #ifndef MAPPING_FILE
 #if defined(_WIN32)
 #define MAPPING_FILE "%SystemRoot%\\system32\\drivers\\etc\\users.ext"
+static char _infile[CA_MAXPATHLEN+1];
 #else
 #define MAPPING_FILE "/etc/ext.users"
-#endif	/* WIN32 */
-#endif 	
+#endif  /* WIN32 */
+#endif  
+
+#if defined(_REENTRANT) || defined(_THREAD_SAFE)
+#define strtok(X,Y) strtok_r(X,Y,&last)
+#endif /* _REENTRANT || _THREAD_SAFE */
 
 static char *infile = MAPPING_FILE;
 
@@ -45,89 +59,92 @@ char *to_user;
 int *to_uid ;
 int *to_gid ;
 {
-   char *p ;
-   struct passwd *pw ;
-   FILE *fsin ;
-   int uid,gid ;
-   char buf[256] ;
-   char maphostnam[50];
-   char mapuser[10] ;
-   char mapuid[6] ;
-   char mapgid[6] ;
-   int counter ;
+    char *p ;
+    struct passwd *pw ;
+    FILE *fsin ;
+    int uid,gid ;
+    char buf[BUFSIZ+1] ;
+    char maphostnam[CA_MAXHOSTNAMELEN+1];
+    char mapuser[CA_MAXUSRNAMELEN+1] ;
+    char mapuid[6] ;
+    char mapgid[6] ;
+    int counter ;
+    char *infile;
 #if defined(_WIN32)
-   char path[256];
-#endif   
+    char path[CA_MAXPATHLEN+1];
+#endif
+#if defined(_REENTRANT) || defined(_THREAD_SAFE)
+    char *last = NULL;
+#endif /* _REENTRANT || _THREAD_SAFE */
 
 #if defined(_WIN32)
-   strcpy(path, infile);
-   if( (strncmp(path, "%SystemRoot%\\", 13) == 0) && ((p = getenv ("SystemRoot")) != NULL) ) {
-      sprintf(infile, "%s\\%s", p, strchr (path, '\\'));
-   }
+    strcpy(path, infile);
+    if( (strncmp(path, "%SystemRoot%\\", 13) == 0) && ((p = getenv ("SystemRoot")) != NULL) ) {
+        sprintf(_infile, "%s\\%s", p, strchr (path, '\\'));
+        infile = _infile;
+    }
 #endif
    
-   if  ( (fsin=fopen(infile,"r"))==NULL ) {
-      log(LOG_ERR, "Could not open file %s, errno %d", infile, errno);
-      /* fprintf(stderr,"Could not open file %s\n",infile); */
-      return -ENOENT ;
-   }
-   while( fgets(buf,256,fsin)!=NULL ) {
-      if ( buf[0] == '#')
-	 continue ;
-      if ( ( p = strrchr( buf,'\n') ) != NULL )
-	 *p='\0' ;
-      log(LOG_DEBUG,"Entry >%s< in %s\n",buf,infile) ;
+    if  ( (fsin=fopen(infile,"r"))==NULL ) {
+       log(LOG_ERR, "Could not open file %s, errno %d", infile, errno);
+       serrno = ENOENT;
+       return -ENOENT ;
+    }
 
-      p = strtok( buf, " \t");
-      if ( p != NULL ) 	
-	 strcpy (maphostnam, p) ;
-      else
-	 continue ;
-      p = strtok( NULL, " \t");
-      if ( p != NULL ) 
-	 strcpy (mapuser, p) ;
-      else
-	 continue ;
-      p = strtok( NULL, " \t");
-      if ( p != NULL ) 
-	 strcpy (mapuid, p);
-      else
-	 continue ;
-      p = strtok( NULL, " \t");
-      if ( p != NULL ) 
-	 strcpy (mapgid, p);
-      else
-	 continue ;
-      p = strtok( NULL, " \t");
-      if ( p != NULL ) 
-	 strcpy (to_user,p);
-      else
-	 continue ;
+    while( fgets(buf,BUFSIZ,fsin)!=NULL ) {
+        if ( buf[0] == '#') continue ;
+        if ( ( p = strrchr( buf,'\n') ) != NULL ) *p='\0' ;
 
-      /* Checking maphostnam */
-      counter = 0 ;
-      if ( strcmp( maphostnam, from_node) && 
-	   !( (p=strchr(maphostnam,'*'))!= NULL && strstr(from_node,p+1) != NULL ) )
-	 continue ;
-      if ( strcmp( mapuser , from_user ) && strcmp( mapuser ,"*" ) )
-	 continue ;
-      if ( strcmp( mapuid, "*" ) && ( (uid=atoi(mapuid)) <= 0 || uid != from_uid ) )
-	 continue ;
-      if ( strcmp( mapgid, "*" ) && ( (gid=atoi(mapgid)) <= 0 || gid != from_gid ) )
-	 continue ;
-      log( LOG_DEBUG,"Found a possible entry: %s\n",to_user);
-      if ( (pw = getpwnam(to_user))==NULL) {
-	 log(LOG_INFO,"getpwnam(): %s\n",sys_errlist[errno]);
-	 continue ;
-      }
-      else {
-	 *to_uid = pw->pw_uid;
-	 *to_gid = pw->pw_gid;
-      }
-      fclose( fsin ) ;
-      return 0 ;
+        log(LOG_DEBUG,"Entry >%s< in %s\n",buf,infile) ;
 
-   }
-   fclose( fsin ) ;
-   return -1 ;
+        p = strtok( buf, " \t");
+        if ( p != NULL ) strcpy (maphostnam, p) ;
+        else continue ;
+
+        p = strtok( NULL, " \t");
+        if ( p != NULL ) strcpy (mapuser, p) ;
+        else continue ;
+
+        p = strtok( NULL, " \t");
+        if ( p != NULL ) strcpy (mapuid, p);
+        else continue ;
+
+        p = strtok( NULL, " \t");
+        if ( p != NULL ) strcpy (mapgid, p);
+        else continue ;
+
+        p = strtok( NULL, " \t");
+        if ( p != NULL ) strcpy (to_user,p);
+        else continue ;
+
+        /* Checking maphostnam */
+        counter = 0 ;
+        if ( strcmp( maphostnam, from_node) && 
+            !( (p=strchr(maphostnam,'*'))!= NULL && strstr(from_node,p+1) != NULL ) )
+            continue ;
+
+        if ( strcmp( mapuser , from_user ) && strcmp( mapuser ,"*" ) )
+            continue ;
+
+        if ( strcmp( mapuid, "*" ) && ( (uid=atoi(mapuid)) <= 0 || uid != from_uid ) ) 
+            continue ;
+
+        if ( strcmp( mapgid, "*" ) && ( (gid=atoi(mapgid)) <= 0 || gid != from_gid ) )
+            continue ;
+
+        log( LOG_DEBUG,"Found a possible entry: %s\n",to_user);
+
+        if ( (pw = getpwnam(to_user))==NULL) {
+            log(LOG_INFO,"getpwnam(): %s\n",sys_errlist[errno]);
+            continue ;
+        } else {
+            *to_uid = pw->pw_uid;
+            *to_gid = pw->pw_gid;
+        }
+        fclose( fsin ) ;
+        return 0 ;
+    }
+    fclose( fsin ) ;
+    serrno = ENOENT;
+    return -1 ;
 }
