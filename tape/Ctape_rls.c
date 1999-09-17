30feb9bcb61037a03f6131fe6bd92ea99f0ee8d3
Jean-Philippe Baud <baud@cern.ch>
1999-09-17 06:46:00 +0000

    change from marshall_WORD to marshall_LONG for jid change from ETPRM to EINVAL

--- tape/Ctape_rls.c
+++ tape/Ctape_rls.c
@@ -4,7 +4,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "%W% %G% CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: Ctape_rls.c,v $ $Revision: 1.2 $ $Date: 1999/09/17 06:46:00 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	Ctape_rls - unload tape and release reservations */
@@ -76,7 +76,7 @@ int flags;
 	}
 
 	if (errflg) {
-		serrno = ETPRM;
+		serrno = EINVAL;
 		return (-1);
 	}
  
@@ -93,7 +93,7 @@ int flags;
 
 	marshall_WORD (sbp, uid);
 	marshall_WORD (sbp, gid);
-	marshall_WORD (sbp, jid);
+	marshall_LONG (sbp, jid);
 	marshall_WORD (sbp, flags);
 	if (path) {
 		marshall_STRING (sbp, fullpath);
