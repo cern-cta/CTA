b75c2e452da684ecdb1b945bbd1bdaceef166bd0
Jean-Philippe Baud <baud@cern.ch>
1999-09-20 07:22:37 +0000

    use new identification string: @(#)$RCSfile$ $Revision$ $Date$

--- tape/unldtape.c
+++ tape/unldtape.c
@@ -4,7 +4,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "%W% %G% CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: unldtape.c,v $ $Revision: 1.2 $ $Date: 1999/09/20 07:22:37 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 #include <errno.h>
