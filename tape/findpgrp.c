32e98c1b9fd929d0572264094e6be20594633c18
Jean-Philippe Baud <baud@cern.ch>
1999-09-20 11:31:31 +0000

    use new ientification string: @(#)$RCSfile$ $Revision$ $Date$

--- tape/findpgrp.c
+++ tape/findpgrp.c
@@ -4,7 +4,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "%W% %G% CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: findpgrp.c,v $ $Revision: 1.2 $ $Date: 1999/09/20 11:31:31 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	findpgrp - get process group (replaces getpgrp broken by C-shell) */
