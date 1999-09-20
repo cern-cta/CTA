426d4bf902a116f861483dc9aa6d9a1f067661bc
Jean-Philippe Baud <baud@cern.ch>
1999-09-20 09:50:55 +0000

    *** empty log message ***

---
+++ tape/checkjobdied.c
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 1990-1999 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: checkjobdied.c,v $ $Revision: 1.1 $ $Date: 1999/09/20 09:50:55 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+/*	checkjobdied - returns the list of jobs that have died */
+
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#ifndef hpux
+#if defined(SOLARIS) || defined(sgi) || (defined(__osf__) && defined(__alpha)) || defined(linux)
+#include <errno.h>
+#include <sys/stat.h>
+#else
+#if _AIX && _IBMR2
+#include <nlist.h>
+#include <sys/proc.h>
+#include <sys/sysconfig.h>
+#include <sys/var.h>
+#endif
+#endif
+#endif
+#include "Ctape.h"
+static char func[16];
+extern char *sys_errlist[];
+checkjobdied(jobs)
+int jobs[];
+{
+	int i, j, k;
+#ifndef hpux
+	int fdkmem;
+#if defined(SOLARIS) || defined(sgi) || (defined(__osf__) && defined(__alpha)) || defined(linux)
+	char name[12];
+	struct stat st;
+#else
+#if _AIX && _IBMR2
+	long lastprocaddr;
+	static struct nlist nl[3];
+	int nproc;
+	struct proc *p;
+	struct proc *proctab;
+	long proctabaddr;
+	int proctabsiz;
+	struct var var;
+#endif
+#endif
+#endif
+	ENTRY (checkjobdied);
+#if hpux
+	for (i = 0, k = 0; jobs[i]; i++) {
+		if (kill(jobs[i], 0) != 0)
+			jobs[k++] = jobs[i];		/* job has died */
+	}
+#else
+#if defined(linux)
+	for (i = 0, k = 0; jobs[i]; i++) {
+		sprintf (name, "/proc/%d", jobs[i]);
+		j = stat (name, &st);
+		if (j < 0 && errno == ENOENT) jobs[k++] = jobs[i];	/* job has died */
+	}
+#else
+#if defined(SOLARIS) || defined(sgi) || (defined(__osf__) && defined(__alpha))
+	for (i = 0, k = 0; jobs[i]; i++) {
+		sprintf (name, "/proc/%.5d", jobs[i]);
+		j = stat (name, &st);
+		if (j < 0 && errno == ENOENT) jobs[k++] = jobs[i];	/* job has died */
+	}
+#else
+#if _AIX && _IBMR2
+	nl[0].n_name = "proc";
+	knlist (nl, 1, sizeof(struct nlist));
+	proctabaddr = nl[0].n_value;
+	if (sysconfig (SYS_GETPARMS, &var, sizeof(var)) < 0) {
+		tplogit (func, TP002, "sysconfig", sys_errlist[errno]);
+		RETURN (-1);
+	}
+	lastprocaddr = (long) var.ve_proc;
+	fdkmem = open ("/dev/kmem", 0);
+	if (fdkmem < 0) {
+		tplogit (func, "TP002 - /dev/kmem : open error : %s\n", sys_errlist[errno]);
+		RETURN (-1);
+	}
+	proctabsiz = lastprocaddr - proctabaddr;
+	proctab = (struct proc *) malloc (proctabsiz);
+	if (proctab == 0) {
+		tplogit (func, TP005);
+		close (fdkmem);
+		RETURN (-1);
+	}
+	if (seek_and_read (fdkmem, proctabaddr, proctab, proctabsiz) < 0) {
+		close (fdkmem);
+		free (proctab);
+		RETURN (-1);
+	}
+	close (fdkmem);
+	nproc = proctabsiz / sizeof(struct proc);
+
+	for (i = 0, k = 0; jobs[i]; i++) {
+		for (j = 0, p = proctab; j < nproc; j++, p++)
+			if (p->p_pid == jobs[i] &&
+			    p->p_stat != 0) break;		/* job still alive */
+		if (j == nproc) jobs[k++] = jobs[i];		/* job has died */
+	}
+	free (proctab);
+#endif
+#endif
+#endif
+#endif
+	RETURN (k);
+}
+
+#if _AIX && _IBMR2
+#define TWO_GIG (0x80000000)
+static int
+seek_and_read(fd, offset, data, data_len)
+int fd;
+long offset;
+void *data;
+int data_len;
+{
+	int rc;
+
+	if (offset >= TWO_GIG) {
+		if (lseek (fd, offset - TWO_GIG, 0) < 0) {
+			tplogit (func, "TP002 - /dev/kmem : lseek error : %s\n",
+			    sys_errlist[errno]);
+			return (-1);
+		}
+		if (readx (fd, data, data_len ,1) < 0) {
+			tplogit (func, "TP002 - /dev/kmem : readx error : %s\n",
+			    sys_errlist[errno]);
+			return (-1);
+		}
+	} else {
+		if (lseek (fd, offset, 0) < 0) {
+			tplogit (func, "TP002 - /dev/kmem : lseek error : %s\n",
+			    sys_errlist[errno]);
+			return (-1);
+		}
+		if (readx (fd, data, data_len ,0) < 0) {
+			tplogit (func, "TP002 - /dev/kmem : readx error : %s\n",
+			    sys_errlist[errno]);
+			return (-1);
+		}
+	}
+	return (0);
+}
+#endif
---
+++ tape/cvtden.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 1993-1999 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: cvtden.c,v $ $Revision: 1.1 $ $Date: 1999/09/20 09:47:22 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+/*	cvtden - check and convert alphanumeric densities to integer */
+#include <sys/types.h>
+#include "Ctape.h"
+static char adens[18][6] = {"0", "800", "1600", "6250", "38000",
+	"8200", "8500", "38KD", "2G", "6G", "10G", "FMT", "RAW", "DDS",
+	"20G", "25G", "35G", "50G"};
+static char adensc[18][6] = {"", "", "", "", "38KC",
+	"8200C", "8500C", "38KDC", "", "", "10GC", "", "", "DDSC",
+	"20GC", "25GC", "35GC", "50GC"};
+cvtden(aden)
+char	*aden;
+{
+	int i;
+
+	for (i = 0; i < 18; i++)
+		if (strcmp (aden, adens[i]) == 0) return (i);
+	for (i = 0; i < 18; i++)
+		if (strcmp (aden, adensc[i]) == 0) return (i | IDRC);
+	if (!strcmp (aden, "38K")) return (D38000);
+	if (!strcmp (aden, "43200")) return (D8200);
+	if (!strcmp (aden, "86400")) return (D8500);
+	return (-1);
+}
+
+/*	den2aden - convert integer densities to alphanumeric */
+char *
+den2aden(den)
+int	den;
+{
+	if (den & IDRC)
+		return (adensc[den & ~IDRC]);
+	else
+		return (adens[den]);
+}
