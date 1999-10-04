01a5f12bb9a5c65ae7226c8400a29e3126924aa7
Jean-Philippe Baud <baud@cern.ch>
1999-10-04 08:56:23 +0000

    add HPSS_AWARE

--- h/patchlevel.h
+++ h/patchlevel.h
@@ -1,9 +1,9 @@
 /*
- * @(#)patchlevel.h	1.5 06/21/94 CERN CN-SW/DC F. Hemmer
+ * @(#)$RCSfile: patchlevel.h,v $ $Revision: 1.2 $ $Date: 1999/10/04 08:56:23 $ CERN IT-PDP/CS F. Hemmer
  */
 
 /*
- * Copyright (C) 1990-1994 by CERN CN-PDP/CS
+ * Copyright (C) 1990-1999 by CERN IT-PDP/CS
  * All rights reserved
  */
 
@@ -12,8 +12,9 @@
 #ifndef _PATCHLEVEL_H_INCLUDED_
 #define _PATCHLEVEL_H_INCLUDED_
 
-#define BASEVERSION     "1.10.3"
-#define PATCHLEVEL      2
-#define TIMESTAMP       "990407081810"
+#define BASEVERSION     __BASEVERSION__
+#define PATCHLEVEL      __PATCHLEVEL__
+#define TIMESTAMP       __TIMESTAMP__
+#define HPSS_AWARE      __HPSS_AWARE__
 
 #endif /* _PATCHLEVEL_H_INCLUDED_ */
