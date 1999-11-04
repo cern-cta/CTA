75a45fe893afc16abd6d3e2d9c29193ff079b4d9
Jean-Philippe Baud <baud@cern.ch>
1999-11-04 16:58:11 +0000

    Initial release

---
+++ tape/posovl.c
@@ -0,0 +1,345 @@
+/*
+ * Copyright (C) 1990-1999 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: posovl.c,v $ $Revision: 1.1 $ $Date: 1999/11/04 16:58:11 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+#include <errno.h>
+#include <stdio.h>
+#include <pwd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include "Ctape.h"
+#include "marshall.h"
+#if SACCT
+#include "sacct.h"
+#endif
+#include "serrno.h"
+#if !defined(linux)
+extern char *sys_errlist[];
+#endif
+char *dvrname;
+char func[16];
+char hostname[CA_MAXHOSTNAMELEN+1];
+int jid;
+char *path;
+int rpfd;
+int tapefd;
+main(argc, argv)
+int	argc;
+char	**argv;
+{
+	char actual_hdr1[81];
+	int blksize;
+	unsigned int blockid;
+	int c;
+	int cfseq;
+	int den;
+	char *devtype;
+	char *dgn;
+	char *drive;
+	char fid[CA_MAXFIDLEN+1];
+	int filstat;
+	int flags;
+	int fsec;
+	int fseq;
+	gid_t gid;
+	char hdr1[LBLBUFSZ];
+	char hdr2[LBLBUFSZ];
+	int i;
+	int lblcode;
+	int lrecl;
+	int method;
+	int mode;
+	int msglen;
+	char name[CA_MAXUSRNAMELEN+1];
+	char *p;
+	struct passwd *pwd;
+	char *q;
+	int Qfirst;
+	int Qlast;
+	char recfm[CA_MAXRECFMLEN+1];
+	char repbuf[REPBUFSZ];
+	int retentd;
+	char *sbp;
+	int scsi;
+	char sendbuf[REQBUFSZ];
+	int sonyraw;
+	char tpfid[CA_MAXFIDLEN+1];
+	uid_t uid;
+	int ux;
+	char *vid;
+	char vol1[LBLBUFSZ];
+	char *vsn;
+
+	void cleanup();
+	void positkilled();
+
+	ENTRY (posovl);
+
+	drive = argv[1];
+	vid = argv[2];
+	rpfd = atoi(argv[3]);
+	uid = atoi (argv[4]);
+	gid = atoi (argv[5]);
+	jid = atoi (argv[6]);
+	ux = atoi(argv[7]);
+	dgn = argv[8];
+	devtype = argv[9];
+	dvrname = argv[10];
+	mode = atoi (argv[11]);
+	lblcode = atoi (argv[12]);
+	vsn = argv[13];
+
+	blockid = atoi (argv[14]);
+	cfseq = atoi (argv[15]);
+	strcpy (fid, argv[16]);
+	filstat = atoi (argv[17]);
+	fsec = atoi (argv[18]);
+	fseq = atoi (argv[19]);
+	method = atoi (argv[20]);
+	path = argv[21];
+	Qfirst = atoi (argv[22]);
+	Qlast = atoi (argv[23]);
+	retentd = atoi (argv[24]);
+
+	strcpy (recfm, argv[25]);
+	blksize = atoi (argv[26]);
+	lrecl = atoi (argv[27]);
+	den = atoi (argv[28]);
+	flags = atoi (argv[29]);
+ 
+#if _AIX
+	scsi = strncmp (dvrname, "mtdd", 4);
+#else
+	scsi = 1;
+#endif
+#if SONYRAW
+	if (strcmp (devtype, "DIR1") == 0 && den == SRAW)
+		sonyraw = 1;
+	else
+		sonyraw = 0;
+#endif
+
+	c = 0;
+	gethostname (hostname, CA_MAXHOSTNAMELEN+1);
+
+        signal (SIGINT, positkilled);
+
+        pwd = getpwuid (uid);
+        strcpy (name, pwd->pw_name);
+
+	/* open device and check drive ready */
+
+#if SONYRAW
+	if (sonyraw) {
+		tapefd = open (path, O_RDWR|O_NDELAY);
+		if (tapefd < 0) {
+			c = errno;
+			if (errno == ENXIO) {	/* drive not operational */
+				usrmsg (func, TP033, drive, hostname);
+				configdown (drive);
+			} else
+				usrmsg (func, TP042, path, "open",
+					sys_errlist[errno]);
+			goto reply;
+		}
+		if (chkdriveready_sony (tapefd) <= 0) {
+			usrmsg (func, TP054);
+			c = ETNRDY;
+			goto reply;
+		}
+		if (blockid < 600) blockid = 7000;
+		if (c = locate_sony (tapefd, path, blockid)) goto reply;
+	} else {
+#endif
+#ifndef SOLARIS
+		if (!scsi)
+#endif
+			tapefd = open (path, O_RDONLY);
+#ifndef SOLARIS
+		else
+			tapefd = open (path, O_RDONLY|O_NDELAY);
+#endif
+		if (tapefd < 0) {
+#if defined(_AIX) || defined(SOLARIS)
+#if _AIX
+			if (!scsi && (errno == EIO || errno == ENOTREADY)) {
+#else
+			if (errno == EIO) {
+#endif
+				usrmsg (func, TP054);
+				c = ETNRDY;
+				goto reply;
+			}
+#endif
+			c = errno;
+			if (errno == ENXIO) {	/* drive not operational */
+				usrmsg (func, TP033, drive, hostname);
+				configdown (drive);
+			} else
+				usrmsg (func, TP042, path, "open",
+					sys_errlist[errno]);
+			goto reply;
+		}
+		if (chkdriveready (tapefd) <= 0) {
+			usrmsg (func, TP054);
+			c = ETNRDY;
+			goto reply;
+		}
+		if ((c = posittape (tapefd, path, devtype, lblcode, mode,
+		    &cfseq, fid, filstat, fsec, fseq, den, flags, Qfirst, Qlast,
+		    vol1, hdr1, hdr2)))
+			goto reply;
+#if SONYRAW
+	}
+#endif
+
+	/* tape is positionned */
+
+#if SACCT
+	tapeacct (TPPOSIT, uid, gid, jid, dgn, drive, vid, cfseq, 0);
+#endif
+	if (lblcode == AL || lblcode == SL) {
+		if (filstat != NEW_FILE) {
+			if (fid[0] == '\0') {
+				strncpy (tpfid, hdr1 + 4, 17);
+				tpfid [17] = '\0';
+				if ((p = strchr (tpfid, ' ')) != NULL) *p = '\0';
+				strcpy (fid, tpfid);
+			}
+			if (hdr2[0]) {
+				if (recfm[0] == '\0') {
+					memset (recfm, 0, 4);
+					recfm[0] = hdr2[4];
+					if (lblcode == SL && hdr2[38] != ' ') {
+						if (hdr2[38] == 'R')
+							memcpy (recfm + 1, "BS", 2);
+						else
+							recfm[1] = hdr2[38];
+					}
+				}
+				if (blksize == 0) sscanf (hdr2 + 5, "%5d", &blksize);
+				if (lrecl == 0) sscanf (hdr2 + 10, "%5d", &lrecl);
+			}
+		} else {
+			if (fid[0] == '\0') {
+				p = strrchr (path, '/') + 1;
+				if ((i = strlen (p) - 17) > 0) p += i;
+				UPPER (p);
+				strcpy (fid, p);
+			}
+			if (recfm[0] == '\0')
+				strcpy (recfm, "U");
+			if (blksize == 0)
+				if (strcmp (devtype, "SD3")) {
+					if (lrecl == 0)
+						blksize = 32760;
+					else
+						blksize = lrecl;
+				} else {
+					if (lrecl == 0)
+						blksize = 262144;
+					else if (strcmp (recfm, "F") == 0) {
+						blksize = (262144 / lrecl) * lrecl;
+						strcpy (recfm, "FB");
+					} else
+						blksize = lrecl;
+				}
+			if (lrecl == 0 && strcmp (recfm, "U")) lrecl = blksize;
+		}
+	}
+
+        /* Build UPDFIL request header */
+ 
+        sbp = sendbuf;
+        marshall_LONG (sbp, TPMAGIC);
+        marshall_LONG (sbp, UPDFIL);
+        q = sbp;        /* save pointer. The next field will be updated */
+        msglen = 3 * LONGSIZE;
+        marshall_LONG (sbp, msglen);
+ 
+        /* Build UPDFIL request body */
+
+        marshall_WORD (sbp, uid);
+        marshall_WORD (sbp, gid);
+        marshall_LONG (sbp, jid);
+        marshall_WORD (sbp, ux);
+        marshall_LONG (sbp, blksize);
+        marshall_LONG (sbp, blockid);
+        marshall_LONG (sbp, cfseq);
+        marshall_STRING (sbp, fid);
+        marshall_LONG (sbp, lrecl);
+        marshall_STRING (sbp, recfm);
+
+        msglen = sbp - sendbuf;
+        marshall_LONG (q, msglen);      /* update length field */
+ 
+        if ((c = send2tpd (NULL, sendbuf, msglen, NULL, 0)) == 0) {
+		sbp = repbuf;
+                marshall_LONG (sbp, cfseq);
+		if (lblcode == AL || lblcode == SL) {
+			buildvollbl (vol1, vsn, lblcode, name);
+			if (mode == WRITE_DISABLE || filstat == APPEND)
+				for (i = 0; i < 80; i++)
+					actual_hdr1[i] = hdr1[i] ? hdr1[i] : ' ';
+			buildhdrlbl (hdr1, hdr2,
+				fid, fsec, cfseq, retentd,
+				recfm, blksize, lrecl, den, lblcode);
+			if (mode == WRITE_ENABLE && filstat != APPEND)
+				memcpy (actual_hdr1, hdr1, 80);
+			vol1[80] = '\0';
+			actual_hdr1[80] = '\0';
+			hdr2[80] = '\0';
+		} else {
+			vol1[0] = '\0';
+			actual_hdr1[0] = '\0';
+			hdr2[0] = '\0';
+		}
+                marshall_STRING (sbp, vol1);
+                marshall_STRING (sbp, actual_hdr1);
+                marshall_STRING (sbp, hdr2);
+		sendrep (rpfd, MSG_DATA, sbp - repbuf, repbuf);
+	}
+reply:
+	if (c < 0) c = -c;
+	if (c) {
+		cleanup();
+	} else {
+		close (tapefd);
+	}
+	sendrep (rpfd, TAPERC, c);
+	exit (0);
+}
+
+void cleanup()
+{
+	tplogit (func, "cleanup started");
+	if (tapefd >= 0)
+		close (tapefd);
+
+	/* must unload and deassign */
+
+	(void) Ctape_rls (path, TPRLS_KEEP_RSV|TPRLS_UNLOAD|TPRLS_NOWAIT);
+}
+
+configdown(drive)
+char *drive;
+{
+	char msg[OPRMSGSZ];
+
+	sprintf (msg, TP033, drive, hostname); /* ops msg */
+	omsgr ("configdown", msg, 0);
+	(void) Ctape_config (drive, CONF_DOWN, TPCD_SYS);
+}
+
+void positkilled()
+{
+	cleanup();
+	exit (2);
+}
