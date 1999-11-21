f8f65f952b9829b8d0bf013b7d23c661b873cae8
Olof Barring <obarring@cern.ch>
1999-11-21 14:22:39 +0000

    Include osdep.h to get EXTERN_C/DLL_DECL/_PROTO macros

--- h/Cnetdb.h
+++ h/Cnetdb.h
@@ -1,6 +1,9 @@
 /*
- * $Id: Cnetdb.h,v 1.3 1999/07/30 15:59:39 obarring Exp $
+ * $Id: Cnetdb.h,v 1.4 1999/11/21 14:22:39 obarring Exp $
  * $Log: Cnetdb.h,v $
+ * Revision 1.4  1999/11/21 14:22:39  obarring
+ * Include osdep.h to get EXTERN_C/DLL_DECL/_PROTO macros
+ *
  * Revision 1.3  1999/07/30 15:59:39  obarring
  * Add copyright notice
  *
@@ -21,45 +24,17 @@
 #ifndef _CNETDB_H
 #define _CNETDB_H
 
-#ifndef EXTERN_C
-#if defined(__cplusplus)
-#define EXTERN_C extern "C"
-#else /* __cplusplus */
-#define EXTERN_C extern
-#endif /* __cplusplus */
-#endif /* EXTERN_C */
-
-#if !(defined(_WIN32) && defined(_DLL))
-#define DLL_DECL
-#else /* _WIN32 && _DLL */
-#ifdef DLL_DECL
-#undef DLL_DECL
-#endif /* DLL_DECL */
-#ifdef _EXPORTING
-#define DLL_DECL __declspec(dllexport) __cdecl
-#else  /* _EXPORTING */
-#define DLL_DECL __declspec(dllimport) __cdecl
-#endif /* _EXPORTING */
-#endif /* _WIN32 && _DLL */
+#include <osdep.h>
 
 #if defined(_REENTRANT) || defined(_THREAD_SAFE)
 #if defined(hpux) || defined(HPUX10) || defined(sgi) || defined(SOLARIS)
-#if defined(__STDC__)
-EXTERN_C int DLL_DECL *__h_errno(void);
-#else /* __STDC__ */
-EXTERN_C int DLL_DECL *__h_errno();
-#endif /* __STDC__ */
+EXTERN_C int DLL_DECL *__h_errno _PROTO((void));
 #define h_errno (*__h_errno())
 #endif /* hpux || HPUX10 || sgi || SOLARIS */
 #endif /* _REENTRANT || _THREAD_SAFE */
 
 
-#if defined(__STDC__)
-EXTERN_C struct hostent DLL_DECL *Cgethostbyname(const char *);
-EXTERN_C struct hostent DLL_DECL *Cgethostbyaddr(const void *, size_t, int);
-#else  /* __STDC__ */
-EXTERN_C struct hostent DLL_DECL *Cgethostbyname();
-EXTERN_C struct hostent DLL_DECL *Cgethostbyaddr();
-#endif /* __STDC__ */
+EXTERN_C struct hostent DLL_DECL *Cgethostbyname _PROTO((const char *));
+EXTERN_C struct hostent DLL_DECL *Cgethostbyaddr _PROTO((const void *, size_t, int));
 
 #endif /* _CNETDB_H */
