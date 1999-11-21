ad31b90e3c20101043c5a7833ffe45dd5630a2fb
Olof Barring <obarring@cern.ch>
1999-11-21 14:19:51 +0000

    Include osdep.h to get DLL_DECL/EXPORT_C/_PROTO macros

--- mediachanger/castorrmc/h/Cglobals.h
+++ mediachanger/castorrmc/h/Cglobals.h
@@ -1,6 +1,9 @@
 /*
- * $Id: Cglobals.h,v 1.3 1999/07/30 16:00:12 obarring Exp $
+ * $Id: Cglobals.h,v 1.4 1999/11/21 14:19:51 obarring Exp $
  * $Log: Cglobals.h,v $
+ * Revision 1.4  1999/11/21 14:19:51  obarring
+ * Include osdep.h to get DLL_DECL/EXPORT_C/_PROTO macros
+ *
  * Revision 1.3  1999/07/30 16:00:12  obarring
  * Correct the copyright notice ....
  *
@@ -17,37 +20,12 @@
 #ifndef _CASTOR_GLOBALS_H
 #define _CASTOR_GLOBALS_H    
 
-#ifndef EXTERN_C
-#if defined(__cplusplus)
-#define EXTERN_C extern "C"
-#else /* __cplusplus */
-#define EXTERN_C extern
-#endif /* __cplusplus */
-#endif /* EXTERN_C */
+#include <osdep.h>
 
-#if !(defined(_WIN32) && defined(_DLL))
-#define DLL_DECL
-#else /* _WIN32 && _DLL */
-#ifdef DLL_DECL
-#undef DLL_DECL
-#endif /* DLL_DECL */
-#ifdef _EXPORTING
-#define DLL_DECL __declspec(dllexport) __cdecl
-#else  /* _EXPORTING */
-#define DLL_DECL __declspec(dllimport) __cdecl
-#endif /* _EXPORTING */
-#endif /* _WIN32 && _DLL */
-
-#if defined(__STDC__)
-EXTERN_C void DLL_DECL Cglobals_init(int (*)(int *, void **),
-                                     int (*)(int *, void *),
-                                     int (*)(void));
-EXTERN_C void DLL_DECL Cglobals_get(int *, void **, size_t size);
-EXTERN_C void DLL_DECL Cglobals_getTid(int *);
-#else /* __STDC__ */
-EXTERN_C void DLL_DECL Cglobals_init();
-EXTERN_C void DLL_DECL Cglobals_get();
-EXTERN_C void DLL_DECL Cglobals_getTid();
-#endif /* __STDC__ */
+EXTERN_C void DLL_DECL Cglobals_init _PROTO((int (*) _PROTO((int *, void **)),
+                                             int (*) _PROTO((int *, void *)),
+                                             int (*) _PROTO((void)) ));
+EXTERN_C void DLL_DECL Cglobals_get _PROTO((int *, void **, size_t size));
+EXTERN_C void DLL_DECL Cglobals_getTid _PROTO((int *));
 
 #endif /* _CASTOR_GLOBALS_H */
