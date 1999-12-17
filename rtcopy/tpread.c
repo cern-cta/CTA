391a58756dbbd31d57b4727b9544adc8fa329ec0
Olof Barring <obarring@cern.ch>
1999-12-17 12:58:53 +0000

    Update to new rtcp_InitLog() prototype.

--- rtcopy/tpread.c
+++ rtcopy/tpread.c
@@ -4,14 +4,16 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: tpread.c,v $ $Revision: 1.1 $ $Date: 1999/11/29 11:22:09 $ CERN IT-PDP/DM Olof Barring";
+static char sccsid[] = "@(#)$RCSfile: tpread.c,v $ $Revision: 1.2 $ $Date: 1999/12/17 12:58:53 $ CERN IT-PDP/DM Olof Barring";
 #endif /* not lint */
 
 /*
  * tpread.c - tpread/tpwrite command line commands
  */
 
-
+#if defined(_WIN32)
+#include <winsock2.h>
+#endif /* _WIN32 */
 #include <stdlib.h>
 #include <errno.h>
 #include <stdarg.h>
@@ -30,7 +32,21 @@ static char sccsid[] = "@(#)$RCSfile: tpread.c,v $ $Revision: 1.1 $ $Date: 1999/
 #include <rtcp_api.h>
 #include <serrno.h>
 
-extern int rtcp_InitLog(char *, FILE *, FILE *);
+extern int rtcp_InitLog(char *, FILE *, FILE *, SOCKET *);
+
+static int CheckRetry(tape_list_t *tape) {
+    tape_list_t *tl;
+    file_list_t *fl;
+
+    if ( tape == NULL ) return(FALSE);
+    CLIST_ITERATE_BEGIN(tape,tl) {
+        if ( (tl->tapereq.err.severity & RTCP_RESELECT_SERV) != 0 ) return(TRUE);
+        CLIST_ITERATE_BEGIN(tl->file,fl) {
+            if ( (fl->filereq.err.severity & RTCP_RESELECT_SERV) != 0 ) return(TRUE);
+        } CLIST_ITERATE_END(tl->file,fl);
+    } CLIST_ITERATE_END(tape,tl);
+    return(FALSE);
+}
 
 int main(int argc, char *argv[]) {
     tape_list_t *tape, *tl;
@@ -40,7 +56,7 @@ int main(int argc, char *argv[]) {
     char errtxt[1024];
     int rc;
 
-    rtcp_InitLog(errtxt,stdout,stderr);
+    rtcp_InitLog(errtxt,stdout,stderr,NULL);
     initlog(argv[0],LOG_INFO,"");
     tape = NULL;
     rc = rtcpc_BuildReq(&tape,argc,argv);
@@ -50,10 +66,16 @@ int main(int argc, char *argv[]) {
         return(1);
     }
 
-    rc = rtcpc(tape);
-    if ( rc == -1 ) {
-        rtcp_log(LOG_ERR,"%s rtcpc(): %s\n",argv[0],sstrerror(serrno));
-        return(1);
+    /*
+     * Retry loop to break out of
+     */
+    for (;;) {
+        rc = rtcpc(tape);
+        if ( rc == -1 ) {
+            rtcp_log(LOG_ERR,"%s rtcpc(): %s\n",argv[0],sstrerror(serrno));
+            if ( CheckRetry(tape) == TRUE ) continue;
+            else return(1); 
+        } else break;
     }
 
     CLIST_ITERATE_BEGIN(tape,tl) {
