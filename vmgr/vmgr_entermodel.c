28e7fdf4ef92498456633519f0416c54062fe061
Jean-Philippe Baud <baud@cern.ch>
2000-01-03 07:56:39 +0000

    Initial release

---
+++ vmgr/vmgr_entermodel.c
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 1999 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+ 
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: vmgr_entermodel.c,v $ $Revision: 1.1 $ $Date: 2000/01/03 07:56:37 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+ 
+/*      vmgr_entermodel - enter a new model of cartridge */
+
+#include <errno.h>
+#include <sys/types.h>
+#if defined(_WIN32)
+#include <winsock2.h>
+#else
+#include <unistd.h>
+#include <netinet/in.h>
+#endif
+#include "marshall.h"
+#include "vmgr_api.h"
+#include "vmgr.h"
+#include "serrno.h"
+
+vmgr_entermodel(const char *model, char *media_letter, int native_capacity, int media_cost)
+{
+	int c;
+	char func[16];
+	gid_t gid;
+	int msglen;
+	char *q;
+	char *sbp;
+	char sendbuf[REQBUFSZ];
+	struct vmgr_api_thread_info *thip;
+	uid_t uid;
+
+        strcpy (func, "vmgr_entermodel");
+        if (vmgr_apiinit (&thip))
+                return (-1);
+        uid = geteuid();
+        gid = getegid();
+#if defined(_WIN32)
+        if (uid < 0 || gid < 0) {
+                vmgr_errmsg (func, VMG53);
+                serrno = SENOMAPFND;
+                return (-1);
+        }
+#endif
+
+	if (! model) {
+		serrno = EFAULT;
+		return (-1);
+	}
+ 
+	/* Build request header */
+
+	sbp = sendbuf;
+	marshall_LONG (sbp, VMGR_MAGIC);
+	marshall_LONG (sbp, VMGR_ENTMODEL);
+	q = sbp;        /* save pointer. The next field will be updated */
+	msglen = 3 * LONGSIZE;
+	marshall_LONG (sbp, msglen);
+
+	/* Build request body */
+ 
+	marshall_LONG (sbp, uid);
+	marshall_LONG (sbp, gid);
+	marshall_STRING (sbp, model);
+	if (media_letter) {
+		marshall_STRING (sbp, media_letter);
+	} else {
+		marshall_STRING (sbp, "");
+	}
+	marshall_LONG (sbp, native_capacity);
+	marshall_LONG (sbp, media_cost);
+ 
+	msglen = sbp - sendbuf;
+	marshall_LONG (q, msglen);	/* update length field */
+
+	c = send2vmgr (NULL, sendbuf, msglen, NULL, 0);
+	return (c);
+}
---
+++ vmgr/vmgr_querymodel.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 1999 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+ 
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: vmgr_querymodel.c,v $ $Revision: 1.1 $ $Date: 2000/01/03 07:56:39 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+ 
+/*      vmgr_querymodel - query about a model of cartridge */
+
+#include <errno.h>
+#include <sys/types.h>
+#if defined(_WIN32)
+#include <winsock2.h>
+#else
+#include <unistd.h>
+#include <netinet/in.h>
+#endif
+#include "marshall.h"
+#include "vmgr_api.h"
+#include "vmgr.h"
+#include "serrno.h"
+
+vmgr_querymodel(const char *model, char *media_letter, int *native_capacity, int *media_cost)
+{
+	int c;
+	char func[16];
+	gid_t gid;
+	int msglen;
+	int n;
+	char *q;
+	char *rbp;
+	char repbuf[10];
+	char *sbp;
+	char sendbuf[REQBUFSZ];
+	struct vmgr_api_thread_info *thip;
+	char tmpbuf[7];
+	uid_t uid;
+
+        strcpy (func, "vmgr_querymodel");
+        if (vmgr_apiinit (&thip))
+                return (-1);
+        uid = geteuid();
+        gid = getegid();
+#if defined(_WIN32)
+        if (uid < 0 || gid < 0) {
+                vmgr_errmsg (func, VMG53);
+                serrno = SENOMAPFND;
+                return (-1);
+        }
+#endif
+
+	if (! model) {
+		serrno = EFAULT;
+		return (-1);
+	}
+ 
+	/* Build request header */
+
+	sbp = sendbuf;
+	marshall_LONG (sbp, VMGR_MAGIC);
+	marshall_LONG (sbp, VMGR_QRYMODEL);
+	q = sbp;        /* save pointer. The next field will be updated */
+	msglen = 3 * LONGSIZE;
+	marshall_LONG (sbp, msglen);
+
+	/* Build request body */
+ 
+	marshall_LONG (sbp, uid);
+	marshall_LONG (sbp, gid);
+	marshall_STRING (sbp, model);
+	if (media_letter) {
+		marshall_STRING (sbp, media_letter);
+	} else {
+		marshall_STRING (sbp, "");
+	}
+ 
+	msglen = sbp - sendbuf;
+	marshall_LONG (q, msglen);	/* update length field */
+
+	c = send2vmgr (NULL, sendbuf, repbuf, sizeof(repbuf));
+	if (c == 0) {
+		rbp = repbuf;
+		unmarshall_STRING (rbp, tmpbuf);
+		if (media_letter)
+			strcpy (media_letter, tmpbuf);
+		unmarshall_LONG (rbp, n);
+		if (native_capacity)
+			*native_capacity = n;
+		unmarshall_LONG (rbp, n);
+		if (media_cost)
+			*media_cost = n;
+	}
+	return (c);
+}
