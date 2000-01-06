68cb4094d488c7bdab7c2d6cd5d1e3e7803ce737
Jean-Damien Durand <jdurand@cern.ch>
2000-01-06 08:35:03 +0000

    include of osdep.h for EXTERN_C, DLL_DECL and _PROTO macros

--- h/rfio_errno.h
+++ h/rfio_errno.h
@@ -1,5 +1,5 @@
 /*
- * $Id: rfio_errno.h,v 1.2 1999/12/09 13:46:19 jdurand Exp $
+ * $Id: rfio_errno.h,v 1.3 2000/01/06 08:35:03 jdurand Exp $
  */
 
 /*
@@ -8,7 +8,7 @@
  */
 
 /*
- * $RCSfile: rfio_errno.h,v $ $Revision: 1.2 $ $Date: 1999/12/09 13:46:19 $
+ * $RCSfile: rfio_errno.h,v $ $Revision: 1.3 $ $Date: 2000/01/06 08:35:03 $
  */
 
 /* rfio_errno.h   Thread safe rfio_errno  */
@@ -16,38 +16,15 @@
 #ifndef _RFIO_ERRNO_H_INCLUDED_
 #define _RFIO_ERRNO_H_INCLUDED_
 
-
-#if !defined(EXTERN_C)
-#if defined(__cplusplus)
-#define EXTERN_C extern "C"
-#else /* __cplusplus */
-#define EXTERN_C extern
-#endif /* __cplusplus */
-#endif /* EXTERN_C */
-
-#if defined(DLL_DECL)
-#undef DLL_DECL
-#endif /* DLL_DECL */
-#if defined(_WIN32) && defined(_DLL)
-#if defined(_EXPORTING)
-#define DLL_DECL __declspec(dllexport)
-#else /* _EXPORTING */
-#define DLL_DECL __declspec(dllimport)
-#endif /* _EXPORTING */
-#else /* _WIN32 && _DLL */
-#define DLL_DECL
-#endif /* _WIN32 && _DLL */
+#include <osdep.h>                  /* EXTERN_C, DLL_DECL and _PROTO */
+#include <sys/types.h>              /* For size_t                    */
 
 #if defined(_REENTRANT) || defined(_THREAD_SAFE) || \
    (defined(_WIN32) && (defined(_MT) || defined(_DLL)))
 /*
  * Multi-thread (MT) environment
  */
-#if defined(__STDC__)
-EXTERN_C int DLL_DECL *__rfio_errno(void);
-#else /* __STDC__ */
-EXTERN_C int DLL_DECL *__rfio_errno();
-#endif /* __STDC__ */
+EXTERN_C int DLL_DECL *__rfio_errno _PROTO((void));
 
 /*
  * Thread safe rfio_errno. Note, __rfio_errno is defined in Cglobals.c rather
@@ -62,19 +39,10 @@ EXTERN_C int DLL_DECL *__rfio_errno();
 extern  int     rfio_errno;                 /* Global error number          */
 #endif /* _REENTRANT || _TREAD_SAFE */
 
-#if defined(__STDC__)
-#include <sys/types.h>
-EXTERN_C char DLL_DECL *rfio_errmsg_r(int, int, char*, size_t); 
-EXTERN_C char DLL_DECL *rfio_errmsg(int, int);
-EXTERN_C char DLL_DECL *rfio_serror_r(char*, size_t);
-EXTERN_C char DLL_DECL *rfio_serror(void);  
-EXTERN_C void DLL_DECL rfio_perror(char *);
-#else /* __STDC__ */
-EXTERN_C char DLL_DECL *rfio_errmsg_r(); 
-EXTERN_C char DLL_DECL *rfio_errmsg();
-EXTERN_C char DLL_DECL *rfio_serror_r();
-EXTERN_C char DLL_DECL *rfio_serror();  
-EXTERN_C void DLL_DECL rfio_perror();
-#endif /* __STDC__ */
+EXTERN_C char DLL_DECL *rfio_errmsg_r _PROTO((int, int, char*, size_t));
+EXTERN_C char DLL_DECL *rfio_errmsg _PROTO((int, int));
+EXTERN_C char DLL_DECL *rfio_serror_r _PROTO((char*, size_t));
+EXTERN_C char DLL_DECL *rfio_serror _PROTO((void));
+EXTERN_C void DLL_DECL rfio_perror _PROTO((char *));
 
 #endif /* _RFIO_ERRNO_H_INCLUDED_ */
