e6eab0cd2272ceeaba9df68ec7ac3f58c7daae22
Jean-Philippe Baud <baud@cern.ch>
2000-01-09 07:32:26 +0000

    split usrlbl.c into 2 files: initlabel.c (no I/O) and usrlbl.c

--- tape/usrlbl.c
+++ tape/usrlbl.c
@@ -4,7 +4,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: usrlbl.c,v $ $Revision: 1.3 $ $Date: 1999/11/25 06:32:38 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: usrlbl.c,v $ $Revision: 1.4 $ $Date: 2000/01/09 07:32:26 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	usrlbl - user callable routines to read/write header and trailer labels */
@@ -23,110 +23,6 @@ static char sccsid[] = "@(#)$RCSfile: usrlbl.c,v $ $Revision: 1.3 $ $Date: 1999/
 #include "Ctape_api.h"
 #include "serrno.h"
 
-/*	initlabelroutines - allocate the structures to store the information
-	needed by the label processing routines */
-
-static int nb_rsvd_resources;
-static struct devlblinfo *devlblinfop;
-initlabelroutines (nrr)
-int nrr;
-{
-	nb_rsvd_resources = nrr;
-	devlblinfop = calloc (nrr, sizeof(struct devlblinfo));
-	if (devlblinfop == NULL) {
-		Ctape_errmsg ("initlabelroutines", TP005);
-		serrno = ENOMEM;
-		return (-1);
-	}
-	return (0);
-}
-
-/*	rmlabelinfo - remove label information from the internal table */
-
-rmlabelinfo (path, flags)
-char *path;
-int flags;
-{
-	struct devlblinfo  *dlip;
-	int i;
-
-	if (devlblinfop == NULL)
-		return (0);
-	if (path) {
-		for (i = 0, dlip = devlblinfop; i < nb_rsvd_resources; i++, dlip++)
-			if (strcmp (dlip->path, path) == 0) break;
-		if (flags == TPRLS_KEEP_RSV) {
-			memset (dlip, '\0', sizeof(struct devlblinfo));
-		} else {
-			nb_rsvd_resources--;
-			if (i != nb_rsvd_resources) {	/* not last entry */
-				memcpy (dlip, dlip + 1, nb_rsvd_resources - i);
-			}
-		}
-	}
-	if (flags == TPRLS_ALL || nb_rsvd_resources == 0)
-		free (devlblinfop);
-}
-
-/*	setdevinfo - set flags for optimization of label processing routines
- *	according to device type and density
- */
-setdevinfo (path, devtype, den, lblcode)
-char *path;
-char *devtype;
-int den;
-int lblcode;
-{
-	struct devlblinfo  *dlip;
-	int i;
-
-	for (i = 0, dlip = devlblinfop; i < nb_rsvd_resources; i++, dlip++)
-		if (dlip->path[0] == '\0') break;
-
-	strcpy (dlip->path, path);
-
-	if (strncmp (devtype, "DLT", 3) == 0 || strcmp (devtype, "3590") == 0 ||
-	    strcmp (devtype, "SD3") == 0 || strcmp (devtype, "9840") == 0)
-		dlip->dev1tm = 1;	/* one TM at EOI is enough */
-	else
-		dlip->dev1tm = 0;
-
-	if (strcmp (devtype, "8200") == 0 || den == D8200 || den == D8200C)
-		dlip->rewritetm = 1;	/* An Exabyte 8200 must be positionned
-					   on the BOT side of a long filemark
-					   before starting to write */
-	else
-		dlip->rewritetm = 0;
-
-	dlip->lblcode = lblcode;
-	return (0);
-}
-
-/*	setlabelinfo - set label information for label processing routines */
-
-setlabelinfo (path, flags, fseq, vol1, hdr1, hdr2)
-char *path;
-int flags;
-int fseq;
-char *vol1;
-char *hdr1;
-char *hdr2;
-{
-	struct devlblinfo  *dlip;
-	int i;
-
-	for (i = 0, dlip = devlblinfop; i < nb_rsvd_resources; i++, dlip++)
-		if (strcmp (dlip->path, path) == 0) break;
-
-	dlip->flags = flags;
-	dlip->fseq = fseq;
-	if (dlip->lblcode == AL || dlip->lblcode == SL) {
-		strcpy (dlip->vol1, vol1);
-		strcpy (dlip->hdr1, hdr1);
-		strcpy (dlip->hdr2, hdr2);
-	}
-}
-
 /*	checkeofeov - check for EOF or EOV */
 /*	return	-errno	in case of error
  *		-ETLBL	for bad label structure
@@ -140,11 +36,9 @@ char	*path;
 	int c;
 	struct devlblinfo  *dlip;
 	char hdr1[LBLBUFSZ];
-	int i;
 
-	for (i = 0, dlip = devlblinfop; i < nb_rsvd_resources; i++, dlip++)
-		if (strcmp (dlip->path, path) == 0) break;
-
+	if (getlabelinfo (path, &dlip) < 0)
+		return (-1);
 	if ((c = readlbl (tapefd, path, hdr1)) < 0)
 		if (dlip->flags & NOTRLCHK)
 			return (0);
@@ -182,12 +76,10 @@ char	*path;
 	struct devlblinfo  *dlip;
 	int fsec;
 	char hdr1[80], hdr2[80];
-	int i;
 	char vol1[80];
 
-	for (i = 0, dlip = devlblinfop; i < nb_rsvd_resources; i++, dlip++)
-		if (strcmp (dlip->path, path) == 0) break;
-
+	if (getlabelinfo (path, &dlip) < 0)
+		return (-1);
 	if (dlip->lblcode != NL)	/* tape is labelled */
 		sscanf (dlip->hdr1 + 27, "%4d", &fsec);
 	else
@@ -221,7 +113,6 @@ int	nblocks;
 	struct devlblinfo  *dlip;
 	int fseq;
 	char hdr1[80], hdr2[80];
-	int i;
 #if defined(hpux) || defined(linux)
 	struct mtget mt_info;
 #endif
@@ -229,9 +120,8 @@ int	nblocks;
 	struct mtop mtop;
 #endif
 
-	for (i = 0, dlip = devlblinfop; i < nb_rsvd_resources; i++, dlip++)
-		if (strcmp (dlip->path, path) == 0) break;
-
+	if (getlabelinfo (path, &dlip) < 0)
+		return (-1);
 	if (dlip->lblcode == NL) return (wrteotmrk (tapefd, path));	/* tape is unlabelled */
 	if ((c = wrttpmrk (tapefd, path, 1)) < 0) return (c);
 
@@ -290,11 +180,9 @@ char	*path;
 	int c;
 	struct devlblinfo  *dlip;
 	char hdr1[81], hdr2[81];
-	int i;
 
-	for (i = 0, dlip = devlblinfop; i < nb_rsvd_resources; i++, dlip++)
-		if (strcmp (dlip->path, path) == 0) break;
-
+	if (getlabelinfo (path, &dlip) < 0)
+		return (-1);
 	if (dlip->fseq == 1) {
 		if ((c = rwndtape (tapefd, path)) < 0) return (c);
 		if (dlip->lblcode != NL) {
@@ -322,11 +210,9 @@ char *path;
 {
 	int c;
 	struct devlblinfo  *dlip;
-	int i;
 
-	for (i = 0, dlip = devlblinfop; i < nb_rsvd_resources; i++, dlip++)
-		if (strcmp (dlip->path, path) == 0) break;
-
+	if (getlabelinfo (path, &dlip) < 0)
+		return (-1);
 	if (! dlip->dev1tm) {
 		/* write 2 tape marks */
 		if ((c = wrttpmrk (tapefd, path, 1)) < 0) return (c);
