9efbc73f7872e86f5e939f379ae050420361fe4e
Jean-Philippe Baud <baud@cern.ch>
2000-01-29 10:11:48 +0000

    add prototype for vmgr_deletepool, vmgr_enterpool, vmgr_modifypool

--- h/vmgr_api.h
+++ h/vmgr_api.h
@@ -1,5 +1,5 @@
 /*
- * $Id: vmgr_api.h,v 1.7 2000/01/03 13:51:33 baud Exp $
+ * $Id: vmgr_api.h,v 1.8 2000/01/29 10:11:48 baud Exp $
  */
 
 /*
@@ -8,7 +8,7 @@
  */
 
 /*
- * @(#)$RCSfile: vmgr_api.h,v $ $Revision: 1.7 $ $Date: 2000/01/03 13:51:33 $ CERN IT-PDP/DM Jean-Philippe Baud
+ * @(#)$RCSfile: vmgr_api.h,v $ $Revision: 1.8 $ $Date: 2000/01/29 10:11:48 $ CERN IT-PDP/DM Jean-Philippe Baud
  */
 
 #ifndef _VMGR_API_H
@@ -30,22 +30,28 @@ struct vmgr_api_thread_info {
 
 #if defined(__STDC__)
 extern int vmgr_deletemodel(const char *, char *);
+extern int vmgr_deletepool(const char *);
 extern int vmgr_deletetape(const char *);
 extern int vmgr_entermodel(const char *, char *, int, int);
+extern int vmgr_enterpool(const char *, uid_t, gid_t);
 extern int vmgr_entertape(const char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
 extern int vmgr_gettape(const char *, int, const char *, char *, char *, char *, char *, char *, int *, unsigned int *);
 extern int vmgr_modifymodel(const char *, char *, int, int);
+extern int vmgr_modifypool(const char *, uid_t, gid_t);
 extern int vmgr_modifytape(const char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
 extern int vmgr_querymodel(const char *, char *, int *, int *);
 extern int vmgr_querytape(const char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, int *, int *, int *);
 extern int vmgr_updatetape(const char *, u_signed64, int, int, int);
 #else
 extern int vmgr_deletemodel();
+extern int vmgr_deletepool();
 extern int vmgr_deletetape();
 extern int vmgr_entermodel();
+extern int vmgr_enterpool();
 extern int vmgr_entertape();
 extern int vmgr_gettape();
 extern int vmgr_modifymodel();
+extern int vmgr_modifypool();
 extern int vmgr_modifytape();
 extern int vmgr_querymodel();
 extern int vmgr_querytape();
