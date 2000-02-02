3045ae3dba9e6a4ad22d1ac47948e2544d23587d
Jean-Damien Durand <jdurand@cern.ch>
2000-02-02 09:37:56 +0000

    Better presentation of assign ofeach member in any DB <-> stage structure direction

--- stage/Cstage_ifce.c
+++ stage/Cstage_ifce.c
@@ -1,5 +1,5 @@
 /*
- * $Id: Cstage_ifce.c,v 1.5 2000/01/03 17:40:33 jdurand Exp $
+ * $Id: Cstage_ifce.c,v 1.6 2000/02/02 09:37:56 jdurand Exp $
  */
 
 /*
@@ -19,7 +19,7 @@
 #include "stage.h"
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: Cstage_ifce.c,v $ $Revision: 1.5 $ $Date: 2000/01/03 17:40:33 $ CERN IT-PDP/DM Jean-Damien Durand";
+static char sccsid[] = "@(#)$RCSfile: Cstage_ifce.c,v $ $Revision: 1.6 $ $Date: 2000/02/02 09:37:56 $ CERN IT-PDP/DM Jean-Damien Durand";
 #endif /* not lint */
 
 
@@ -30,25 +30,25 @@ int DLL_DECL stcp2Cdb(stcp,tape,disk,hsm,alloc)
      struct stgcat_hsm *hsm;
      struct stgcat_alloc *alloc;
 {
+  int i;
+
   if (stcp == NULL ||
       tape == NULL || disk  == NULL ||
       hsm == NULL  || alloc == NULL) {
     return(-1);
   }
 
-  memset(tape,0,sizeof(struct stgcat_tape));
-  memset(disk,0,sizeof(struct stgcat_disk));
-  memset(hsm,0,sizeof(struct stgcat_hsm));
-  memset(alloc,0,sizeof(struct stgcat_alloc));
-
-  /* We fill the "common" part */
   switch (stcp->t_or_d) {
   case 't':
+    memset(tape,0,sizeof(struct stgcat_tape));
     tape->reqid       =   stcp->reqid;
     tape->nread       =   stcp->nread;
     tape->size        =   stcp->size;
     tape->nbaccesses  =   stcp->nbaccesses;
     tape->status      =   stcp->status;
+    tape->blksize     =   stcp->blksize;
+    tape->retentd     =   stcp->u1.t.retentd;
+    tape->lrecl       =   stcp->lrecl;
     tape->uid         =   stcp->uid;
     tape->gid         =   stcp->gid;
     tape->mask        =   stcp->mask;
@@ -60,29 +60,47 @@ int DLL_DECL stcp2Cdb(stcp,tape,disk,hsm,alloc)
     strcpy(tape->group,   stcp->group);
     strcpy(tape->user,    stcp->user);
     tape->keep        =   stcp->keep;
+    strcpy(tape->lbl,     stcp->u1.t.lbl);
+    strcpy(tape->recfm,   stcp->recfm);
+    strcpy(tape->den,     stcp->u1.t.den);
+    strcpy(tape->dgn,     stcp->u1.t.dgn);
+    strcpy(tape->fid,     stcp->u1.t.fid);
+    strcpy(tape->fseq,    stcp->u1.t.fseq);
+    strcpy(tape->tapesrvr,stcp->u1.t.tapesrvr);
+    for (i = 0; i < MAXVSN; i++) {
+      strcpy(tape->vid[i],stcp->u1.t.vid[i]);
+      strcpy(tape->vsn[i],stcp->u1.t.vsn[i]);
+    }
+    tape->filstat    =    stcp->u1.t.filstat;
+    tape->charconv   =    stcp->charconv;
+    tape->E_Tflags   =    stcp->u1.t.E_Tflags;
     break;
   case 'd':
+    memset(disk,0,sizeof(struct stgcat_disk));
     disk->reqid       =   stcp->reqid;
     disk->nread       =   stcp->nread;
     disk->size        =   stcp->size;
     disk->nbaccesses  =   stcp->nbaccesses;
     disk->status      =   stcp->status;
+    disk->blksize     =   stcp->blksize;
+    disk->lrecl       =   stcp->lrecl;
     disk->uid         =   stcp->uid;
     disk->gid         =   stcp->gid;
     disk->mask        =   stcp->mask;
     disk->actual_size =   stcp->actual_size;
     disk->c_time      =   stcp->c_time;
     disk->a_time      =   stcp->a_time;
-    disk->lrecl       =   stcp->lrecl;
-    disk->blksize     =   stcp->blksize;
     strcpy(disk->recfm,   stcp->recfm);
     strcpy(disk->poolname,stcp->poolname);
     strcpy(disk->ipath,   stcp->ipath);
     strcpy(disk->group,   stcp->group);
     strcpy(disk->user,    stcp->user);
     disk->keep        =   stcp->keep;
+    strcpy(disk->xfile,    stcp->u1.d.xfile);
+    strcpy(disk->Xparm,    stcp->u1.d.Xparm);
     break;
   case 'm':
+    memset(hsm,0,sizeof(struct stgcat_hsm));
     hsm->reqid       =   stcp->reqid;
     hsm->nread       =   stcp->nread;
     hsm->size        =   stcp->size;
@@ -99,8 +117,10 @@ int DLL_DECL stcp2Cdb(stcp,tape,disk,hsm,alloc)
     strcpy(hsm->group,   stcp->group);
     strcpy(hsm->user,    stcp->user);
     hsm->keep        =   stcp->keep;
+    strcpy(hsm->xfile,     stcp->u1.m.xfile);
     break;
   case 'a':
+    memset(alloc,0,sizeof(struct stgcat_alloc));
     alloc->reqid       =   stcp->reqid;
     alloc->nread       =   stcp->nread;
     alloc->size        =   stcp->size;
@@ -117,47 +137,11 @@ int DLL_DECL stcp2Cdb(stcp,tape,disk,hsm,alloc)
     strcpy(alloc->group,   stcp->group);
     strcpy(alloc->user,    stcp->user);
     alloc->keep        =   stcp->keep;
-    break;
-  default:
-    return(-1);
-  }
-
-  /* We fill the non-common part of the tables */
-  switch (stcp->t_or_d) {
-    case 't':
-      {
-        int i;
-        
-        tape->blksize     =   stcp->blksize;
-        tape->retentd     =   stcp->u1.t.retentd;
-        tape->lrecl       =   stcp->lrecl;
-        strcpy(tape->lbl,     stcp->u1.t.lbl);
-        strcpy(tape->recfm,   stcp->recfm);
-        strcpy(tape->den,     stcp->u1.t.den);
-        strcpy(tape->dgn,     stcp->u1.t.dgn);
-        strcpy(tape->fid,     stcp->u1.t.fid);
-        strcpy(tape->fseq,    stcp->u1.t.fseq);
-        strcpy(tape->tapesrvr,stcp->u1.t.tapesrvr);
-        for (i = 0; i < MAXVSN; i++) {
-          strcpy(tape->vid[i],stcp->u1.t.vid[i]);
-          strcpy(tape->vsn[i],stcp->u1.t.vsn[i]);
-        }
-        tape->filstat    =    stcp->u1.t.filstat;
-        tape->charconv   =    stcp->charconv;
-        tape->E_Tflags   =    stcp->u1.t.E_Tflags;
-      }
-      break;
-  case 'd':
-    strcpy(disk->xfile,    stcp->u1.d.xfile);
-    strcpy(disk->Xparm,    stcp->u1.d.Xparm);
-    break;
-  case 'a':
     strcpy(alloc->xfile,    stcp->u1.d.xfile);
     strcpy(alloc->Xparm,    stcp->u1.d.Xparm);
     break;
-  case 'm':
-    strcpy(hsm->xfile,     stcp->u1.m.xfile);
-    break;
+  default:
+    return(-1);
   }
 
   /* OKAY */
@@ -188,6 +172,8 @@ int DLL_DECL Cdb2stcp(stcp,tape,disk,hsm,alloc)
      struct stgcat_hsm *hsm;
      struct stgcat_alloc *alloc;
 {
+  int i;
+
   if (stcp == NULL || (tape == NULL && disk == NULL && hsm == NULL && alloc == NULL)) {
     return(-1);
   }
@@ -201,126 +187,117 @@ int DLL_DECL Cdb2stcp(stcp,tape,disk,hsm,alloc)
 
   memset(stcp,0,sizeof(struct stgcat_entry));
 
-  /* We fill the common path of stcp */
   if (tape != NULL) {
-    stcp->t_or_d = 't';
-  } else if (disk != NULL) {
-    stcp->t_or_d = 'd';
-  } else if (hsm != NULL) {
-    stcp->t_or_d = 'm';
-  } else {
-    stcp->t_or_d = 'a';
-  }
-  if (tape != NULL) {
-    stcp->reqid       =   tape->reqid;
-    stcp->nread       =   tape->nread;
-    stcp->size        =   tape->size;
-    stcp->nbaccesses  =   tape->nbaccesses;
-    stcp->status      =   tape->status;
-    stcp->uid         =   tape->uid;
-    stcp->gid         =   tape->gid;
-    stcp->mask        =   tape->mask;
-    stcp->actual_size =   tape->actual_size;
-    stcp->c_time      =   tape->c_time;
-    stcp->a_time      =   tape->a_time;
-    strcpy(stcp->poolname,tape->poolname);
-    strcpy(stcp->ipath,   tape->ipath);
-    strcpy(stcp->group,   tape->group);
-    strcpy(stcp->user,    tape->user);
-    stcp->keep        =   tape->keep;
-  } else if (disk != NULL) {
-    stcp->reqid       =   disk->reqid;
-    stcp->nread       =   disk->nread;
-    stcp->size        =   disk->size;
-    stcp->nbaccesses  =   disk->nbaccesses;
-    stcp->status      =   disk->status;
-    stcp->uid         =   disk->uid;
-    stcp->gid         =   disk->gid;
-    stcp->mask        =   disk->mask;
-    stcp->actual_size =   disk->actual_size;
-    stcp->c_time      =   disk->c_time;
-    stcp->a_time      =   disk->a_time;
-    stcp->blksize     =   disk->blksize;
-    stcp->lrecl       =   disk->lrecl;
-    strcpy(stcp->recfm,   disk->recfm);
-    strcpy(stcp->poolname,disk->poolname);
-    strcpy(stcp->ipath,   disk->ipath);
-    strcpy(stcp->group,   disk->group);
-    strcpy(stcp->user,    disk->user);
-    stcp->keep        =   disk->keep;
-  } else if (hsm != NULL) {
-    stcp->reqid       =   hsm->reqid;
-    stcp->nread       =   hsm->nread;
-    stcp->size        =   hsm->size;
-    stcp->nbaccesses  =   hsm->nbaccesses;
-    stcp->status      =   hsm->status;
-    stcp->uid         =   hsm->uid;
-    stcp->gid         =   hsm->gid;
-    stcp->mask        =   hsm->mask;
-    stcp->actual_size =   hsm->actual_size;
-    stcp->c_time      =   hsm->c_time;
-    stcp->a_time      =   hsm->a_time;
-    strcpy(stcp->poolname,hsm->poolname);
-    strcpy(stcp->ipath,   hsm->ipath);
-    strcpy(stcp->group,   hsm->group);
-    strcpy(stcp->user,    hsm->user);
-    stcp->keep        =   hsm->keep;
-  } else {
-    stcp->reqid       =   alloc->reqid;
-    stcp->nread       =   alloc->nread;
-    stcp->size        =   alloc->size;
-    stcp->nbaccesses  =   alloc->nbaccesses;
-    stcp->status      =   alloc->status;
-    stcp->uid         =   alloc->uid;
-    stcp->gid         =   alloc->gid;
-    stcp->mask        =   alloc->mask;
-    stcp->actual_size =   alloc->actual_size;
-    stcp->c_time      =   alloc->c_time;
-    stcp->a_time      =   alloc->a_time;
-    strcpy(stcp->poolname,alloc->poolname);
-    strcpy(stcp->ipath,   alloc->ipath);
-    strcpy(stcp->group,   alloc->group);
-    strcpy(stcp->user,    alloc->user);
-    stcp->keep        =   alloc->keep;
-  }
-
-  /* We fill the non-common part of stcp */
-  switch (stcp->t_or_d) {
-  case 't':
-    {
-      int i;
-        
-      stcp->blksize      =   tape->blksize;
-      stcp->u1.t.retentd =   tape->retentd;
-      stcp->lrecl        =   tape->lrecl;
-      strcpy(stcp->u1.t.lbl, tape->lbl);
-      strcpy(stcp->recfm,    tape->recfm);
-      strcpy(stcp->u1.t.den, tape->den);
-      strcpy(stcp->u1.t.dgn, tape->dgn);
-      strcpy(stcp->u1.t.fid, tape->fid);
-      strcpy(stcp->u1.t.fseq,tape->fseq);
-      strcpy(stcp->u1.t.tapesrvr,tape->tapesrvr);
-      for (i = 0; i < MAXVSN; i++) {
-        strcpy(stcp->u1.t.vid[i],tape->vid[i]);
-        strcpy(stcp->u1.t.vsn[i],tape->vsn[i]);
-      }
-      stcp->u1.t.filstat  = tape->filstat;
-      stcp->charconv      = tape->charconv;
-      stcp->u1.t.E_Tflags = tape->E_Tflags;
+    stcp->blksize     =         tape->blksize;
+    /* No filler member in tape database */
+    stcp->charconv    =         tape->charconv;
+    stcp->keep        =         tape->keep;
+    stcp->lrecl       =         tape->lrecl;
+    stcp->nread       =         tape->nread;
+    strcpy(stcp->poolname,      tape->poolname);
+    strcpy(stcp->recfm,         tape->recfm);
+    stcp->size        =         tape->size;
+    strcpy(stcp->ipath,         tape->ipath);
+    stcp->t_or_d      =         't';
+    strcpy(stcp->group,         tape->group);
+    strcpy(stcp->user,          tape->user);
+    stcp->uid         =         tape->uid;
+    stcp->gid         =         tape->gid;
+    stcp->mask        =         tape->mask;
+    stcp->reqid       =         tape->reqid;
+    stcp->status      =         tape->status;
+    stcp->actual_size =         tape->actual_size;
+    stcp->c_time      =         tape->c_time;
+    stcp->a_time      =         tape->a_time;
+    stcp->nbaccesses  =         tape->nbaccesses;
+    strcpy(stcp->u1.t.den,      tape->den);
+    strcpy(stcp->u1.t.dgn,      tape->dgn);
+    strcpy(stcp->u1.t.fid,      tape->fid);
+    stcp->u1.t.filstat  =       tape->filstat;
+    strcpy(stcp->u1.t.fseq,     tape->fseq);
+    strcpy(stcp->u1.t.lbl,      tape->lbl);
+    stcp->u1.t.retentd =        tape->retentd;
+    strcpy(stcp->u1.t.tapesrvr, tape->tapesrvr);
+    stcp->u1.t.E_Tflags =       tape->E_Tflags;
+    for (i = 0; i < MAXVSN; i++) {
+      strcpy(stcp->u1.t.vid[i], tape->vid[i]);
+      strcpy(stcp->u1.t.vsn[i], tape->vsn[i]);
     }
-    break;
-  case 'd':
-    strcpy(stcp->u1.d.xfile,disk->xfile);
-    strcpy(stcp->u1.d.Xparm,disk->Xparm);
-    break;
-  case 'm':
-    strcpy(stcp->u1.m.xfile,hsm->xfile);
-    break;
-  case 'a':
-    strcpy(stcp->u1.d.xfile,alloc->xfile);
-    strcpy(stcp->u1.d.Xparm,alloc->Xparm);
-    break;
-  default:
+  } else if (disk != NULL) {
+    stcp->blksize     =      disk->blksize;
+    /* No filler member in disk database */
+    /* No charconv member in disk database */
+    stcp->keep        =      disk->keep;
+    stcp->lrecl       =      disk->lrecl;
+    stcp->nread       =      disk->nread;
+    strcpy(stcp->poolname,   disk->poolname);
+    strcpy(stcp->recfm,      disk->recfm);
+    stcp->size        =      disk->size;
+    strcpy(stcp->ipath,      disk->ipath);
+    stcp->t_or_d      =      'd';
+    strcpy(stcp->group,      disk->group);
+    strcpy(stcp->user,       disk->user);
+    stcp->uid         =      disk->uid;
+    stcp->gid         =      disk->gid;
+    stcp->mask        =      disk->mask;
+    stcp->reqid       =      disk->reqid;
+    stcp->status      =      disk->status;
+    stcp->actual_size =      disk->actual_size;
+    stcp->c_time      =      disk->c_time;
+    stcp->a_time      =      disk->a_time;
+    stcp->nbaccesses  =      disk->nbaccesses;
+    strcpy(stcp->u1.d.xfile, disk->xfile);
+    strcpy(stcp->u1.d.Xparm, disk->Xparm);
+  } else if (hsm != NULL) {
+    /* No blksize member in hsm database */
+    /* No filler member in hsm database */
+    /* No charconv member in hsm database */
+    stcp->keep        =      hsm->keep;
+    /* No lrecl member in hsm database */
+    stcp->nread       =      hsm->nread;
+    strcpy(stcp->poolname,   hsm->poolname);
+    /* No recfm member in hsm database */
+    stcp->size        =      hsm->size;
+    strcpy(stcp->ipath,      hsm->ipath);
+    stcp->t_or_d      =      'm';
+    strcpy(stcp->group,      hsm->group);
+    strcpy(stcp->user,       hsm->user);
+    stcp->uid         =      hsm->uid;
+    stcp->gid         =      hsm->gid;
+    stcp->mask        =      hsm->mask;
+    stcp->reqid       =      hsm->reqid;
+    stcp->status      =      hsm->status;
+    stcp->actual_size =      hsm->actual_size;
+    stcp->c_time      =      hsm->c_time;
+    stcp->a_time      =      hsm->a_time;
+    stcp->nbaccesses  =      hsm->nbaccesses;
+    strcpy(stcp->u1.m.xfile, hsm->xfile);
+  } else if (alloc != NULL) {
+    /* No blksize member in alloc database */
+    /* No filler member in alloc database */
+    /* No charconv member in alloc database */
+    stcp->keep        =      alloc->keep;
+    /* No lrecl member in alloc database */
+    stcp->nread       =      alloc->nread;
+    strcpy(stcp->poolname,   alloc->poolname);
+    /* No recfm member in alloc database */
+    stcp->size        =      alloc->size;
+    strcpy(stcp->ipath,      alloc->ipath);
+    stcp->t_or_d      =      'a';
+    strcpy(stcp->group,      alloc->group);
+    strcpy(stcp->user,       alloc->user);
+    stcp->uid         =      alloc->uid;
+    stcp->gid         =      alloc->gid;
+    stcp->mask        =      alloc->mask;
+    stcp->reqid       =      alloc->reqid;
+    stcp->status      =      alloc->status;
+    stcp->actual_size =      alloc->actual_size;
+    stcp->c_time      =      alloc->c_time;
+    stcp->a_time      =      alloc->a_time;
+    stcp->nbaccesses  =      alloc->nbaccesses;
+    strcpy(stcp->u1.d.xfile, alloc->xfile);
+    strcpy(stcp->u1.d.Xparm, alloc->Xparm);
+  } else {
     return(-1);
   }
 
