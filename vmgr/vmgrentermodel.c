12b0834c581886ba734cdc93a492fefca754bc78
Jean-Damien Durand <jdurand@cern.ch>
2000-02-07 12:41:37 +0000

    New names

---
+++ vmgr/vmgrentermodel.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "serrno.h"
+#include "vmgr_api.h"
+
+int main(argc, argv)
+     int argc;
+     char **argv;
+{
+  char buf[80];
+  char *dp;
+  int errflg = 0;
+  FILE *fopen(), *fs;
+  int media_cost;
+  char *media_letter;
+  char *model;
+  int native_capacity;
+  char *p;
+
+  if (argc != 5) {
+    fprintf (stderr,
+             "usage: vmgr_entermodel model media_letter native_capacity media_cost\n"
+             "\n"
+             "  where model            Model type         ex: SD3\n"
+             "        media_letter     Media Iden         ex: B\n"
+             "        native_capacity  Capacity in KBytes ex: 25000\n"
+             "        media_cost       Media Cost per GB  ex: 100\n"
+             "\n"
+             "Comments to the CASTOR Developpment Team <castor-dev@listbox.cern.ch>\n"
+             "\n");
+    return(EXIT_FAILURE);
+  }
+
+  model           = argv[1];
+  media_letter    = argv[2];
+  native_capacity = atoi(argv[3]);
+  media_cost      = atoi(argv[4]);
+
+  if (vmgr_entermodel (model, media_letter, native_capacity, media_cost)) {
+    fprintf (stderr, "%s: %s\n", model, sstrerror(serrno));
+    return(EXIT_FAILURE);
+  }
+
+  printf("--> OK\n");
+
+  return(EXIT_SUCCESS);
+}
---
+++ vmgr/vmgrentertape.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "serrno.h"
+#include "vmgr_api.h"
+
+main(argc, argv)
+     int argc;
+     char **argv;
+{
+  char buf[80];
+  char *density;
+  char *dgn;
+  char *dp;
+  int errflg = 0;
+  char *lbltype;
+  char *manufacturer;
+  char *media_letter;
+  char *model;
+  char *p;
+  char *poolname;
+  char *sn;
+  char *vid;
+  char *vsn;
+
+  if (argc != 11) {
+    fprintf (stderr,
+             "usage: vmgr_entertape vid vsn dgn density lbltype model media_letter manufacturer sn poolname\n"
+             "\n"
+             "  where vid              Volume ID               ex: Y31100\n"
+             "        vsn              Volume SN               ex: Y31100\n"
+             "        dgn              Device Group Name       ex: SD3R3\n"
+             "        density          Volume Density          ex: 50GC\n"
+             "        lbltype          Volume Label Type       ex: al\n"
+             "        model            Volume Model            ex: SD3\n"
+             "        media_letter     Associated Media Letter ex: C (can be \"\")\n"
+             "        manufacturer     Manufacturer            ex: STK (can be \"\")\n"
+             "        sn               Volume Serial Number    ex: 1234567890 (can be \"\")\n"
+             "        poolname         Associated Poolname     ex: Public (can be \"\")\n"
+             "\n"
+             "Comments to the CASTOR Developpment Team <castor-dev@listbox.cern.ch>\n"
+             "\n");
+    return(EXIT_FAILURE);
+  }
+
+  vid = argv[1];
+  vsn = argv[2];
+  dgn = argv[3];
+  density = argv[4];
+  lbltype = argv[5];
+  model = argv[6];
+  media_letter = argv[7];
+  manufacturer = argv[8];
+  sn = argv[9];
+  poolname = argv[10];
+
+  if (vmgr_entertape (vid, NULL, dgn, density, lbltype, model,
+                      media_letter, manufacturer, sn, poolname)) {
+    fprintf (stderr, "%s: %s\n", vid, sstrerror(serrno));
+    return(EXIT_FAILURE);
+  }
+
+  printf("--> OK\n");
+
+  return(EXIT_SUCCESS);
+}
