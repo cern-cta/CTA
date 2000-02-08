b3953ad662b36d65a751ba69a48023b43c396303
Olof Barring <obarring@cern.ch>
2000-02-08 16:08:13 +0000

    Add prototype for rtcpc_FreeReqLists()

--- h/rtcp_api.h
+++ h/rtcp_api.h
@@ -5,7 +5,7 @@
 
 
 /*
- * $RCSfile: rtcp_api.h,v $ $Revision: 1.5 $ $Date: 2000/01/19 15:18:33 $ CERN IT-PDP/DM Olof Barring
+ * $RCSfile: rtcp_api.h,v $ $Revision: 1.6 $ $Date: 2000/02/08 16:08:13 $ CERN IT-PDP/DM Olof Barring
  */
 
 /*
@@ -25,6 +25,7 @@ EXTERN_C int DLL_DECL rtcpcCallTMS _PROTO((tape_list_t *));
 EXTERN_C void DLL_DECL rtcp_SetErrTxt _PROTO((int, char *, ...));
 EXTERN_C int DLL_DECL rtcpc_BuildReq _PROTO((tape_list_t **, int, char **));
 EXTERN_C int DLL_DECL rtcp_RetvalSHIFT _PROTO((tape_list_t *, file_list_t *, int *));
+EXTERN_C void DLL_DECL rtcpc_FreeReqLists _PROTO((tape_list_t **));
 EXTERN_C int DLL_DECL dumpTapeReq _PROTO((tape_list_t *));
 EXTERN_C int DLL_DECL dumpFileReq _PROTO((file_list_t *));
 
