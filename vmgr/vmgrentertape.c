4837955e03b3a3d7c3638bf18f5244021b6a0cb2
Jean-Philippe Baud <baud@cern.ch>
2000-03-02 14:47:41 +0000

    use option letters

--- vmgr/vmgrentertape.c
+++ vmgr/vmgrentertape.c
@@ -1,69 +1,110 @@
+/*
+ * Copyright (C) 2000 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+ 
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: vmgrentertape.c,v $ $Revision: 1.4 $ $Date: 2000/03/02 14:47:41 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+/*	vmgrentertape - enter a new tape volume */
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include "Cgetopt.h"
 #include "serrno.h"
 #include "vmgr_api.h"
-
 main(argc, argv)
-     int argc;
-     char **argv;
+int argc;
+char **argv;
 {
-  char buf[80];
-  char *density;
-  char *dgn;
-  char *dp;
-  int errflg = 0;
-  char *lbltype;
-  char *manufacturer;
-  char *media_letter;
-  char *model;
-  char *p;
-  char *poolname;
-  char *sn;
-  char *vid;
-  char *vsn;
-  int status;
+	int c;
+	char *density = NULL;
+	char *dgn = NULL;
+	char *dp;
+	int errflg = 0;
+	char *lbltype = NULL;
+	static struct Coptions longopts[] = {
+		{"manufacturer", REQUIRED_ARGUMENT, 0, OPT_MANUFACTURER},
+		{"media_letter", REQUIRED_ARGUMENT, 0, OPT_MEDIA_LETTER},
+		{"ml", REQUIRED_ARGUMENT, 0, OPT_MEDIA_LETTER},
+		{"model", REQUIRED_ARGUMENT, 0, OPT_MODEL},
+		{"pool", REQUIRED_ARGUMENT, 0, 'P'},
+		{"sn", REQUIRED_ARGUMENT, 0, OPT_SN},
+		{"status", REQUIRED_ARGUMENT, 0, OPT_STATUS},
+		{0, 0, 0, 0}
+	};
+	char *manufacturer = NULL;
+	char *media_letter = NULL;
+	char *model = NULL;
+	char *pool_name = NULL;
+	char *sn = NULL;
+	int status = 0;
+	char *vid = NULL;
+	char *vsn = NULL;
 
-  if (argc != 12) {
-    fprintf (stderr,
-             "usage: vmgrentertape vid vsn dgn density lbltype model media_letter manufacturer sn poolname\n"
-             "\n"
-             "  where vid              Volume ID               ex: Y31100\n"
-             "        vsn              Volume SN               ex: Y31100\n"
-             "        dgn              Device Group Name       ex: SD3R3\n"
-             "        density          Volume Density          ex: 50GC\n"
-             "        lbltype          Volume Label Type       ex: al\n"
-             "        model            Volume Model            ex: SD3\n"
-             "        media_letter     Associated Media Letter ex: C (can be \"\")\n"
-             "        manufacturer     Manufacturer            ex: STK (can be \"\")\n"
-             "        sn               Volume Serial Number    ex: 1234567890 (can be \"\")\n"
-             "        poolname         Associated Poolname     ex: Public (can be \"\")\n"
-             "        status           Associated status       ex: 0\n"
-             "\n"
-             "Comments to the CASTOR Developpment Team <castor-dev@listbox.cern.ch>\n"
-             "\n");
-    return(EXIT_FAILURE);
-  }
-
-  vid = argv[1];
-  vsn = argv[2];
-  dgn = argv[3];
-  density = argv[4];
-  lbltype = argv[5];
-  model = argv[6];
-  media_letter = argv[7];
-  manufacturer = argv[8];
-  sn = argv[9];
-  poolname = argv[10];
-  status = atoi(argv[11]);
-
-  if (vmgr_entertape (vid, NULL, dgn, density, lbltype, model,
-                      media_letter, manufacturer, sn, poolname,status)) {
-    fprintf (stderr, "%s: %s\n", vid, sstrerror(serrno));
-    return(EXIT_FAILURE);
-  }
-
-  printf("--> OK\n");
-
-  return(EXIT_SUCCESS);
+	Coptind = 1;
+        while ((c = Cgetopt_long (argc, argv, "d:g:l:P:V:v:", longopts, NULL)) != EOF) {
+                switch (c) {
+		case OPT_MANUFACTURER:
+			manufacturer = Coptarg;
+                        break;
+                case OPT_MEDIA_LETTER:
+			media_letter = Coptarg;
+                        break;
+                case OPT_MODEL:
+			model = Coptarg;
+                        break;
+                case OPT_SN:
+			sn = Coptarg;
+                        break;
+		case OPT_STATUS:
+			if ((status = strtol (Coptarg, &dp, 10)) < 0 ||
+			    *dp != '\0') {
+				fprintf (stderr,
+				    "invalid status %s\n", Coptarg);
+				errflg++;
+			}
+			break;
+		case 'd':
+			density = Coptarg;
+			break;
+		case 'g':
+			dgn = Coptarg;
+			break;
+		case 'l':
+			lbltype = Coptarg;
+			break;
+		case 'P':
+			pool_name = Coptarg;
+			break;
+		case 'V':
+			vid = Coptarg;
+			break;
+		case 'v':
+			vsn = Coptarg;
+			break;
+                case '?':
+                        errflg++;
+                        break;
+                default:
+                        break;
+                }
+        }
+        if (Coptind < argc || vid == NULL || dgn == NULL) {
+                errflg++;
+        }
+        if (errflg) {
+                fprintf (stderr, "usage: %s %s%s%s", argv[0],
+		    "[-d density] -g dgn [-l lbltype] [-P pool_name] -V vid\n",
+		    "[-v vsn] [--ma manufacturer] [--ml media_letter] [--mo model]\n",
+		    "[--pool pool_name] [--sn serail_number] [--st status]\n");
+                exit (USERR);
+        }
+ 
+	if (vmgr_entertape (vid, vsn, dgn, density, lbltype, model,
+	    media_letter, manufacturer, sn, pool_name, status) < 0) {
+		fprintf (stderr, "vmgrentertape %s: %s\n", vid, sstrerror(serrno));
+		exit (USERR);
+	}
+	exit (0);
 }
