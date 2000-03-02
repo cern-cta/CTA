87056afb67117b006a824f9ee1c09416b6bb3432
Jean-Philippe Baud <baud@cern.ch>
2000-03-02 08:31:40 +0000

    use option letters

--- vmgr/vmgrdeletetape.c
+++ vmgr/vmgrdeletetape.c
@@ -1,35 +1,50 @@
+/*
+ * Copyright (C) 2000 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+ 
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: vmgrdeletetape.c,v $ $Revision: 1.2 $ $Date: 2000/03/02 08:31:40 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+/*	vmgrdeletetape - delete a tape volume */
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include "serrno.h"
-#include "vmgr_api.h"
-
+#include "vmgr.h"
+extern	char	*optarg;
+extern	int	optind;
 main(argc, argv)
-     int argc;
-     char **argv;
+int argc;
+char **argv;
 {
-  int errflg = 0;
-  char *vid;
+	int c;
+	int errflg = 0;
+	char *vid = NULL;
 
-  if (argc != 2) {
-    fprintf (stderr,
-             "usage: vmgrdeletetape vid\n"
-             "\n"
-             "  where vid              Volume ID               ex: Y16318\n"
-             "\n"
-             "Comments to the CASTOR Developpment Team <castor-dev@listbox.cern.ch>\n"
-             "\n");
-    return(EXIT_FAILURE);
-  }
-
-  vid = argv[1];
-
-  if (vmgr_deletetape (vid)) {
-    fprintf (stderr, "%s: %s\n", vid, sstrerror(serrno));
-    return(EXIT_FAILURE);
-  }
-
-  printf("--> OK\n");
-
-  return(EXIT_SUCCESS);
+        while ((c = getopt (argc, argv, "V")) != EOF) {
+                switch (c) {
+                case 'V':
+			vid = optarg;
+                        break;
+                case '?':
+                        errflg++;
+                        break;
+                default:
+                        break;
+                }
+        }
+        if (optind < argc) {
+                errflg++;
+        }
+        if (errflg) {
+                fprintf (stderr, "usage: %s -V vid\n", argv[0]);
+                exit (USERR);
+        }
+ 
+	if (vmgr_deletetape (vid) < 0) {
+		fprintf (stderr, "vmgrdeletetapei %s: %s\n", vid, sstrerror(serrno));
+		exit (USERR);
+	}
+	exit (0);
 }
