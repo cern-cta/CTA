1868ace2c683f2be96ff44f3edc5be90b5b1bc2a
Jean-Philippe Baud <baud@cern.ch>
2000-03-06 13:48:49 +0000

    Initial release

---
+++ vmgr/vmgr_deletedenmap.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2000 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+ 
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: vmgr_deletedenmap.c,v $ $Revision: 1.1 $ $Date: 2000/03/06 13:48:49 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+ 
+/*      vmgr_deletedenmap - delete a triplet model/media_letter/density */
+
+#include <errno.h>
+#include <sys/types.h>
+#if defined(_WIN32)
+#include <winsock2.h>
+#else
+#include <unistd.h>
+#include <netinet/in.h>
+#endif
+#include "marshall.h"
+#include "vmgr_api.h"
+#include "vmgr.h"
+#include "serrno.h"
+
+vmgr_deletedenmap(const char *model, char *media_letter, char *density)
+{
+	int c;
+	char func[18];
+	gid_t gid;
+	int msglen;
+	char *q;
+	char *sbp;
+	char sendbuf[REQBUFSZ];
+	struct vmgr_api_thread_info *thip;
+	uid_t uid;
+
+        strcpy (func, "vmgr_deletedenmap");
+        if (vmgr_apiinit (&thip))
+                return (-1);
+        uid = geteuid();
+        gid = getegid();
+#if defined(_WIN32)
+        if (uid < 0 || gid < 0) {
+                vmgr_errmsg (func, VMG53);
+                serrno = SENOMAPFND;
+                return (-1);
+        }
+#endif
+
+	if (! model || ! density) {
+		serrno = EFAULT;
+		return (-1);
+	}
+
+	if (strlen (model) > CA_MAXMODELLEN ||
+	    (media_letter && strlen (media_letter) > 1) ||
+	    strlen (density) > CA_MAXDENLEN) {
+		serrno = EINVAL;
+		return (-1);
+	}
+ 
+	/* Build request header */
+
+	sbp = sendbuf;
+	marshall_LONG (sbp, VMGR_MAGIC);
+	marshall_LONG (sbp, VMGR_DELDENMAP);
+	q = sbp;        /* save pointer. The next field will be updated */
+	msglen = 3 * LONGSIZE;
+	marshall_LONG (sbp, msglen);
+
+	/* Build request body */
+ 
+	marshall_LONG (sbp, uid);
+	marshall_LONG (sbp, gid);
+	marshall_STRING (sbp, model);
+	if (media_letter) {
+		marshall_STRING (sbp, media_letter);
+	} else {
+		marshall_STRING (sbp, " ");
+	}
+	marshall_STRING (sbp, density);
+ 
+	msglen = sbp - sendbuf;
+	marshall_LONG (q, msglen);	/* update length field */
+
+	c = send2vmgr (NULL, sendbuf, msglen, NULL, 0);
+	return (c);
+}
