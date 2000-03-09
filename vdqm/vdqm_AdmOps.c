bfbedc3a2f944011a0fde94fae09afa2ae72bc56
Olof Barring <obarring@cern.ch>
2000-03-09 12:24:14 +0000

    More drive dedication fields

--- vdqm/vdqm_AdmOps.c
+++ vdqm/vdqm_AdmOps.c
@@ -17,12 +17,14 @@ static char sccsid[] = "@(#)vdqm_AdmOps.c,v 1.1 2000/02/29 07:51:10  CERN IT-PDP
 #include <errno.h>
 #include <regex.h>
 #include <stdlib.h>
+#include <pwd.h>
 #include <time.h>
 #include <osdep.h>
 #include <net.h>
 #include <log.h>
 #include <serrno.h>
 #include <Castor_limits.h>
+#include <Cpwd.h>
 #include <vdqm_constants.h>
 #include <vdqm.h>
 
@@ -67,7 +69,15 @@ int vdqm_ResetDedicate(vdqm_drvrec_t *drv) {
 
 int vdqm_DrvMatch(vdqm_volrec_t *vol, vdqm_drvrec_t *drv) {
     vdqmVolReq_t *VolReq;
-    char match_item[256];
+    char keywords[][20] = VDQM_DEDICATE_PREFIX;
+    char formats[][20] = VDQM_DEDICATE_FORMAT;
+    char match_item[256], format[256];
+    struct tm *current_tm, tmstruc;
+    time_t clock;
+    struct passwd *pw;
+    int uid,gid,mode,age;
+    char datestr[11], timestr[9], *name, *group, *vid, *host; 
+    char empty[1] = "";
 
     if ( vol == NULL || drv == NULL ) {
         serrno = EINVAL;
@@ -78,11 +88,27 @@ int vdqm_DrvMatch(vdqm_volrec_t *vol, vdqm_drvrec_t *drv) {
      * If not dedicated, we match all
      */
     if ( *drv->drv.dedicate == '\0' ) return(1);
+    (void)time(&clock);
+#if defined(_REENTRANT) || defined(_THREAD_SAFE)
+    (void)localtime_r(&clock,&tmstruc);
+    current_tm = &tmstruc;
+#else
+    current_tm = localtime(&clock);
+#endif /* _REENTRANT || _THREAD_SAFE */
     VolReq = &vol->vol;
-    sprintf(match_item,
-           "clientUID=%d,clientGID=%d,mode=%d,client_host=%s,volid=%s",
-           VolReq->clientUID,VolReq->clientGID,VolReq->mode,
-           VolReq->client_host,VolReq->volid);
+    (void) strftime(datestr,sizeof(datestr),VDQM_DEDICATE_DATEFORM,current_tm);
+    (void) strftime(timestr,sizeof(timestr),VDQM_DEDICATE_TIMEFORM,current_tm);
+    age = (int)(clock - (time_t)VolReq->recvtime);
+    pw = Cgetpwuid(VolReq->clientUID);
+    uid = VolReq->clientUID;
+    gid = VolReq->clientGID;
+    if ( pw == NULL ) name = empty;
+    else name = pw->pw_name;
+    host = VolReq->client_host;
+    vid = VolReq->volid;
+    mode = VolReq->mode;
+
+    FILL_MATCHSTR(match_item,format,formats);
 
     log(LOG_DEBUG,"vdqm_DrvMatch(): match %s with %s\n",match_item,drv->drv.dedicate);
     if ((regexec(&drv->expbuf, match_item, (size_t)0, NULL, 0)) == 0) {
