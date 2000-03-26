27dadc7d83c76f3e4349b5161ce40c26d276c0e6
Jean-Philippe Baud <baud@cern.ch>
2000-03-26 06:19:21 +0000

    fix hpss_aware + format + compilation if gensrc has not been run

--- client/src/stager/castor.c
+++ client/src/stager/castor.c
@@ -1,5 +1,5 @@
 /*
- * $Id: castor.c,v 1.2 1999/12/09 13:39:06 jdurand Exp $
+ * castor.c,v 1.2 1999/12/09 13:39:06 jdurand Exp
  */
 
 /*
@@ -8,24 +8,31 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: castor.c,v $ $Revision: 1.2 $ $Date: 1999/12/09 13:39:06 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)castor.c,v 1.2 1999/12/09 13:39:06 CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	castor - display current CASTOR version */
 #include <stdio.h>
 #include "patchlevel.h"
+#define __BASEVERSION__ "?"
+#define __PATCHLEVEL__ 0
 
 main(argc, argv)
 int argc;
 char **argv;
 {
 	int c;
+#if defined(HPSSCLIENT)
+	char hpss_aware = 'H';
+#else
+	char hpss_aware = ' ';
+#endif
 
 	while ((c = getopt (argc, argv, "hv")) != EOF) {
 		switch (c) {
 		case 'v':
-			printf ("%s%d%c\n", BASEVERSION, PATCHLEVEL,
-			    HPSS_AWARE ? 'H' : ' ');
+			printf ("%s.%d%c\n", BASEVERSION, PATCHLEVEL,
+			    hpss_aware);
 			break;
 		case 'h':
 		default:
