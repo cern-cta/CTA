b354932a074462934b3bc2dab382e3ecf8f5a3e5
Jean-Philippe Baud <baud@cern.ch>
2000-04-19 12:41:06 +0000

    rename vmgr as vmgrdaemon

--- vmgr/oralink.mk
+++ vmgr/oralink.mk
@@ -1,13 +1,13 @@
 #
-#  Copyright (C) 1999 by CERN/IT/PDP/DM
+#  Copyright (C) 1999-2000 by CERN/IT/PDP/DM
 #  All rights reserved
 #
-#       @(#)$RCSfile: oralink.mk,v $ $Revision: 1.1 $ $Date: 2000/01/23 13:34:51 $ CERN IT-PDP/DM Jean-Philippe Baud
+#       @(#)$RCSfile: oralink.mk,v $ $Revision: 1.2 $ $Date: 2000/04/19 12:41:06 $ CERN IT-PDP/DM Jean-Philippe Baud
  
 #    Link vmgr with Oracle libraries.
 
 include $(ORACLE_HOME)/precomp/lib/env_precomp.mk
 PROLDLIBS=$(LLIBCLNTSH) $(LLIBCLIENT) $(LLIBSQL) $(STATICTTLIBS)
 
-vmgr: $(VMGRDAEMON_OBJS)
-	$(CC) -o vmgr $(CLDFLAGS) $(VMGRDAEMON_OBJS) $(LIBS) -L$(LIBHOME) $(PROLDLIBS)
+vmgrdaemon: $(VMGRDAEMON_OBJS)
+	$(CC) -o vmgrdaemon $(CLDFLAGS) $(VMGRDAEMON_OBJS) $(LIBS) -L$(LIBHOME) $(PROLDLIBS)
