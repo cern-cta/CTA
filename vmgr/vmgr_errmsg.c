de999100539cf840534fc119437aa83ffbd4101e
Jean-Philippe Baud <baud@cern.ch>
2000-05-03 05:37:28 +0000

    include <string.h>

--- vmgr/vmgr_errmsg.c
+++ vmgr/vmgr_errmsg.c
@@ -1,14 +1,15 @@
 /*
- * Copyright (C) 1999 by CERN/IT/PDP/DM
+ * Copyright (C) 1999-2000 by CERN/IT/PDP/DM
  * All rights reserved
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: vmgr_errmsg.c,v $ $Revision: 1.1 $ $Date: 1999/12/15 10:03:16 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: vmgr_errmsg.c,v $ $Revision: 1.2 $ $Date: 2000/05/03 05:37:28 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
 #include <varargs.h>
 #include <sys/types.h>
 #include "vmgr.h"
