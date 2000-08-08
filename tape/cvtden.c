a3cf3921a127b3c833a7789749c399459bf91153
Jean-Philippe Baud <baud@cern.ch>
2000-08-08 12:05:13 +0000

    add densities 40G and 40GC

--- tape/cvtden.c
+++ tape/cvtden.c
@@ -1,29 +1,29 @@
 /*
- * Copyright (C) 1993-1999 by CERN/IT/PDP/DM
+ * Copyright (C) 1993-2000 by CERN/IT/PDP/DM
  * All rights reserved
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: cvtden.c,v $ $Revision: 1.1 $ $Date: 1999/09/20 09:47:22 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: cvtden.c,v $ $Revision: 1.2 $ $Date: 2000/08/08 12:05:13 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	cvtden - check and convert alphanumeric densities to integer */
 #include <sys/types.h>
 #include "Ctape.h"
-static char adens[18][6] = {"0", "800", "1600", "6250", "38000",
+static char adens[19][6] = {"0", "800", "1600", "6250", "38000",
 	"8200", "8500", "38KD", "2G", "6G", "10G", "FMT", "RAW", "DDS",
-	"20G", "25G", "35G", "50G"};
-static char adensc[18][6] = {"", "", "", "", "38KC",
+	"20G", "25G", "35G", "50G", "40G"};
+static char adensc[19][6] = {"", "", "", "", "38KC",
 	"8200C", "8500C", "38KDC", "", "", "10GC", "", "", "DDSC",
-	"20GC", "25GC", "35GC", "50GC"};
+	"20GC", "25GC", "35GC", "50GC", "40GC"};
 cvtden(aden)
 char	*aden;
 {
 	int i;
 
-	for (i = 0; i < 18; i++)
+	for (i = 0; i < 19; i++)
 		if (strcmp (aden, adens[i]) == 0) return (i);
-	for (i = 0; i < 18; i++)
+	for (i = 0; i < 19; i++)
 		if (strcmp (aden, adensc[i]) == 0) return (i | IDRC);
 	if (!strcmp (aden, "38K")) return (D38000);
 	if (!strcmp (aden, "43200")) return (D8200);
