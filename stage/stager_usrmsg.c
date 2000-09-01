103c76aee6209964aee15e0406378478a072533f
Jean-Damien Durand <jdurand@cern.ch>
2000-09-01 13:21:43 +0000

    Special callback for migrator that runs in the background

--- stage/stager_usrmsg.c
+++ stage/stager_usrmsg.c
@@ -1,5 +1,5 @@
 /*
- * $Id: stager_usrmsg.c,v 1.8 2000/07/04 14:32:13 jdurand Exp $
+ * $Id: stager_usrmsg.c,v 1.9 2000/09/01 13:21:43 jdurand Exp $
  */
 
 /*
@@ -8,7 +8,7 @@
  */
 
 #ifndef lint
-static char cvsId[] = "@(#)$RCSfile: stager_usrmsg.c,v $ $Revision: 1.8 $ $Date: 2000/07/04 14:32:13 $ CERN/IT/PDP/DM Jean-Damien Durand";
+static char cvsId[] = "@(#)$RCSfile: stager_usrmsg.c,v $ $Revision: 1.9 $ $Date: 2000/09/01 13:21:43 $ CERN/IT/PDP/DM Jean-Damien Durand";
 #endif /* not lint */
 
 /* stager_usrmsg.c - callback rtcp routine */
@@ -28,11 +28,23 @@ static char cvsId[] = "@(#)$RCSfile: stager_usrmsg.c,v $ $Revision: 1.8 $ $Date:
 #endif /* IRIX5 || __Lynx__ */
 #include <log.h>                /* logging options and definitions      */
 #include <stage.h>              /* FOR MSG_ERR MSG_OUT constants        */
+#include "stage_api.h"          /* For seteuid/setegid macro on hpux */
+#include "osdep.h"
 
 extern int rpfd;
-extern int sendrep();
+EXTERN_C int sendrep _PROTO(());
+EXTERN_C int stglogit _PROTO(());
+
+#define SETEID(thiseuid,thisegid) {              \
+	setegid(0);                                  \
+	seteuid(0);                                  \
+	setegid(thisegid);                           \
+	seteuid(thiseuid);                           \
+}
 
 /*
+ * Version for explicit migration - Calls sendrep
+ *
  * stager_usrmsg should be called with the following syntax
  * stager_usrmsg(LOG_LEVEL,format[,value,...]) ;
  */
@@ -64,6 +76,49 @@ void stager_usrmsg(int level, ...)
 #else
 	if (level != LOG_DEBUG) sendrep(rpfd,RTCOPY_OUT,line,strlen(line)) ;
 #endif
+	va_end(args);
+}
+
+/*
+ * Version for automatic migration - Calls stglogit
+ *
+ * stager_migmsg should be called with the following syntax
+ * stager_migmsg(LOG_LEVEL,format[,value,...]) ;
+ */
+
+#if !defined(IRIX5) && !defined(__Lynx__) && !defined(_WIN32)
+void stager_migmsg(va_alist)     va_dcl
+#else
+void stager_migmsg(int level, ...)
+#endif
+{
+	va_list args ;          /* Variable argument list               */
+	char    *format;        /* Format of the log message            */
+	char    line[BUFSIZ] ;  /* Formatted log message                */
+	int save_euid, save_egid;
+
+#if !defined(IRIX5) && !defined(__Lynx__) && !defined(_WIN32)
+	int     level;          /* Level of the message                 */
+
+	va_start(args);         /* initialize to beginning of list      */
+	level = va_arg(args, int);
+#else
+
+	va_start(args, level);
+#endif /* IRIX5 || __Lynx__ */
+
+	format = va_arg(args, char *);
+	vsprintf(line,format,args);
+	save_euid = geteuid();
+	save_egid = getegid();
+	SETEID(0,0);
+#ifdef STAGER_DEBUG
+    /* In debug mode - we always want to have all messages in stager log-file */
+	stglogit("migrator","%s",line);
+#else
+	if (level != LOG_DEBUG) stglogit("migrator","%s",line);
+#endif
+	SETEID(save_euid,save_egid);
 	va_end(args);
 }
 
