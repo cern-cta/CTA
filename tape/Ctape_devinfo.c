beaffa2a406b9498f99da99ef7300d9ec7142a02
Jean-Philippe Baud <baud@cern.ch>
2000-10-26 15:25:09 +0000

    add support for STK9940 and LTO

--- tape/Ctape_devinfo.c
+++ tape/Ctape_devinfo.c
@@ -4,7 +4,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: Ctape_devinfo.c,v $ $Revision: 1.5 $ $Date: 2000/06/19 07:54:24 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: Ctape_devinfo.c,v $ $Revision: 1.6 $ $Date: 2000/10/26 15:25:09 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	Ctape_devinfo - table of device characteristics */
@@ -23,8 +23,10 @@ static struct devinfo devinfo[] = {
 	"8500", 1, 2, 0,-1, 1,   245760,  32760, 0x00, D8200, 0x14, D8500, 0x15, 0, 0, 0, 0, 0, 0, 0, 0,
 	"8505", 1, 2, 0,-1, 1,   245760,  32760, 0x00, D8200, 0x14, D8500, 0x15, D8200C, 0x90, D8500C, 0x8C, 0, 0, 0, 0,
 	"9840", 1, 1, 1, 0, 1,   262144,  32760, 0x10, D20G, 0, D20GC, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	"9940", 1, 1, 1, 0, 1,   262144,  32760, 0x10, D60G, 0, D60GC, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	"DAT",  1, 2, 0,-1, 1, 16777215,  32760, 0x0F, DDS, 0x24, DDSC, 0x24, 0, 0, 0, 0, 0, 0, 0, 0,
 	"DLT",  1, 1, 1,-1, 1, 16777215,  32760, 0x10, D10G, 0x19, D20G, 0x1A, D35G, 0x1B, D10GC, 0x19, D20GC, 0x1A, D35GC, 0x1B,
+	"LTO",  1, 1, 1,-1, 1, 16777215,  32760, 0x10, D100G, 0x40, D100GC, 0x40, 0, 0, 0, 0, 0, 0, 0, 0,
 	"QIC",  0, 2, 0,-1, 512,    512,    512, 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	"SD1",  0, 0, 1,-1, 144432, 144432, 144432, 0x00, SRAW, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	"SD3",  1, 1, 1, 0, 1,   262144, 262144, 0x10, D10G, 0, D25G, 0, D50G, 0, D10GC, 0, D25GC, 0, D50GC, 0,
