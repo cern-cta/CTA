4b601763f58aace958bc34841496a7b656f68c10
Olof Barring <obarring@cern.ch>
2000-11-06 08:56:11 +0000

    Reset to zero the status sent in stage_updc_tppos() if it is not ETFSQ.

--- rtcopy/rtcpd_stageupdc.c
+++ rtcopy/rtcpd_stageupdc.c
@@ -4,7 +4,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: rtcpd_stageupdc.c,v $ $Revision: 1.47 $ $Date: 2000/11/03 17:23:14 $ CERN IT-PDP/DM Olof Barring";
+static char sccsid[] = "@(#)$RCSfile: rtcpd_stageupdc.c,v $ $Revision: 1.48 $ $Date: 2000/11/06 08:56:11 $ CERN IT-PDP/DM Olof Barring";
 #endif /* not lint */
 
 /*
@@ -136,15 +136,11 @@ int rtcpd_stageupdc(tape_list_t *tape,
     for ( retry=0; retry<RTCP_STGUPDC_RETRIES; retry++ ) {
         status = filereq->err.errorcode;
         /*
-         * Local status is used for tppos only (filcp uses the local
-         * variable "retval"). We have to reset the status in case
-         * the tape file has already been entirely copied into memory and
-         * there was a warning (limited by size, blocks skipped or both).
-         * If we don't reset it the tppos will be skipped while the filcp
-         * still reports success, which will completely confuse the stager.
-         */ 
-        if ( status == ERTLIMBYSZ || status == ERTBLKSKPD ||
-             status == ERTMNYPARY ) status = 0;
+         * Local status variable is used for tppos only (filcp uses the local
+         * variable "retval"). It cannot take any other values than 0 or
+         * ETFSQ (invalid tape file sequence number).
+         */
+        if ( status != ETFSQ ) status = 0;
         if ( filereq->proc_status == RTCP_POSITIONED && status == 0 ||
              (filereq->proc_status == RTCP_FINISHED && 
               (filereq->concat & NOCONCAT_TO_EOD) != 0 && status == ETFSQ) ) { 
