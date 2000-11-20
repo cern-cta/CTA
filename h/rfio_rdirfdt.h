d3406917480d0569cc57b79af972a2abcd674560
Jean-Damien Durand <jdurand@cern.ch>
2000-11-20 14:58:03 +0000

    Added EXTERN_C int DLL_DECL rfio_r[dir|file]fdt_findptr _PROTO(([RDIR|RFILE] *, int)); to solve potential bug while searching if the ptr is local or not : we do not have to use its hypothetical member "s" (.e.g dirp->s, for example) because we do not know in advance if dirp refers to a local or a remote directory (it is an opaque object). Neverthless, if it is local, dirp cannot be in the tables !

--- h/rfio_rdirfdt.h
+++ h/rfio_rdirfdt.h
@@ -1,5 +1,5 @@
 /*
- * $Id: rfio_rdirfdt.h,v 1.1 2000/10/02 08:02:22 jdurand Exp $
+ * $Id: rfio_rdirfdt.h,v 1.2 2000/11/20 14:58:03 jdurand Exp $
  */
 
 #ifndef __rfio_rdirfdt_h
@@ -9,6 +9,7 @@ EXTERN_C int DLL_DECL rfio_rdirfdt_allocentry _PROTO((int));
 #define FINDRDIR_WITH_SCAN     1
 #define FINDRDIR_WITHOUT_SCAN  0
 EXTERN_C int DLL_DECL rfio_rdirfdt_findentry _PROTO((int, int));
+EXTERN_C int DLL_DECL rfio_rdirfdt_findptr _PROTO((RDIR *, int));
 EXTERN_C int DLL_DECL rfio_rdirfdt_freeentry _PROTO((int));
 
 #endif /* __rfio_rdirfdt_h */
--- h/rfio_rfilefdt.h
+++ h/rfio_rfilefdt.h
@@ -1,5 +1,5 @@
 /*
- * $Id: rfio_rfilefdt.h,v 1.1 2000/10/02 08:02:22 jdurand Exp $
+ * $Id: rfio_rfilefdt.h,v 1.2 2000/11/20 14:58:03 jdurand Exp $
  */
 
 #ifndef __rfio_rfilefdt_h
@@ -9,6 +9,7 @@ EXTERN_C int DLL_DECL rfio_rfilefdt_allocentry _PROTO((int));
 #define FINDRFILE_WITH_SCAN     1
 #define FINDRFILE_WITHOUT_SCAN  0
 EXTERN_C int DLL_DECL rfio_rfilefdt_findentry _PROTO((int, int));
+EXTERN_C int DLL_DECL rfio_rfilefdt_findptr _PROTO((RFILE *, int));
 EXTERN_C int DLL_DECL rfio_rfilefdt_freeentry _PROTO((int));
 
 #endif /* __rfio_rfilefdt_h */
