c4a0a9d3ebad9630c66c584ac5b296b9426d9a24
Jean-Philippe Baud <baud@cern.ch>
2001-01-29 12:53:48 +0000

    add prototype for vmgr_querypool()

--- h/vmgr_api.h
+++ h/vmgr_api.h
@@ -1,5 +1,5 @@
 /*
- * $Id: vmgr_api.h,v 1.22 2001/01/03 08:06:59 baud Exp $
+ * $Id: vmgr_api.h,v 1.23 2001/01/29 12:53:48 baud Exp $
  */
 
 /*
@@ -8,7 +8,7 @@
  */
 
 /*
- * @(#)$RCSfile: vmgr_api.h,v $ $Revision: 1.22 $ $Date: 2001/01/03 08:06:59 $ CERN IT-PDP/DM Jean-Philippe Baud
+ * @(#)$RCSfile: vmgr_api.h,v $ $Revision: 1.23 $ $Date: 2001/01/29 12:53:48 $ CERN IT-PDP/DM Jean-Philippe Baud
  */
 
 #ifndef _VMGR_API_H
@@ -58,6 +58,7 @@ EXTERN_C int DLL_DECL vmgr_modifymodel _PROTO((const char *, char *, int, int));
 EXTERN_C int DLL_DECL vmgr_modifypool _PROTO((const char *, uid_t, gid_t));
 EXTERN_C int DLL_DECL vmgr_modifytape _PROTO((const char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, int));
 EXTERN_C int DLL_DECL vmgr_querymodel _PROTO((const char *, char *, int *, int *));
+EXTERN_C int DLL_DECL vmgr_querypool _PROTO((const char *, uid_t *, gid_t *, u_signed64 *, u_signed64 *));
 EXTERN_C int DLL_DECL vmgr_querytape _PROTO((const char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, int *, int *, int *, int *, time_t *, time_t *, int *));
 EXTERN_C int DLL_DECL vmgr_reclaim _PROTO((const char *));
 EXTERN_C int DLL_DECL vmgr_seterrbuf _PROTO((char *, int));
