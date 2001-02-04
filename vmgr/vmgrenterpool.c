5a888095f915d4a171828ff3cd4ef374c6e15ad6
Jean-Philippe Baud <baud@cern.ch>
2001-02-04 08:38:00 +0000

    add options --group and --user

--- vmgr/vmgrenterpool.c
+++ vmgr/vmgrenterpool.c
@@ -4,10 +4,12 @@
  */
  
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: vmgrenterpool.c,v $ $Revision: 1.3 $ $Date: 2000/03/02 14:55:42 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: vmgrenterpool.c,v $ $Revision: 1.4 $ $Date: 2001/02/04 08:37:59 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	vmgrenterpool - define a new tape pool */
+#include <grp.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include "Cgetopt.h"
@@ -20,15 +22,19 @@ char **argv;
 	int c;
 	char *dp;
 	int errflg = 0;
+	struct group *gr;
 	static struct Coptions longopts[] = {
 		{"name", REQUIRED_ARGUMENT, 0, 'P'},
 		{"gid", REQUIRED_ARGUMENT, 0, OPT_POOL_GID},
+		{"group", REQUIRED_ARGUMENT, 0, OPT_POOL_GROUP},
 		{"uid", REQUIRED_ARGUMENT, 0, OPT_POOL_UID},
+		{"user", REQUIRED_ARGUMENT, 0, OPT_POOL_USER},
 		{0, 0, 0, 0}
 	};
 	gid_t pool_gid = 0;
 	char *pool_name = NULL;
 	uid_t pool_uid = 0;
+	struct passwd *pwd;
 
 	Coptind = 1;
         while ((c = Cgetopt_long (argc, argv, "", longopts, NULL)) != EOF) {
@@ -44,6 +50,14 @@ char **argv;
 				errflg++;
 			}
 			break;
+		case OPT_POOL_GROUP:
+			if ((gr = getgrnam (Coptarg)) == NULL) {
+				fprintf (stderr,
+				    "invalid pool_group: %s\n", Coptarg);
+				errflg++;
+			} else
+				pool_gid = gr->gr_gid;
+			break;
 		case OPT_POOL_UID:
 			if ((pool_uid = strtol (Coptarg, &dp, 10)) < 0 ||
 			    *dp != '\0') {
@@ -51,6 +65,14 @@ char **argv;
 				    "invalid pool_uid %s\n", Coptarg);
 				errflg++;
 			}
+			break;
+		case OPT_POOL_USER:
+			if ((pwd = getpwnam (Coptarg)) == NULL) {
+				fprintf (stderr,
+				    "invalid pool_user: %s\n", Coptarg);
+				errflg++;
+			} else
+				pool_uid = pwd->pw_uid;
 			break;
                 case '?':
                         errflg++;
@@ -63,8 +85,9 @@ char **argv;
                 errflg++;
         }
         if (errflg) {
-                fprintf (stderr, "usage: %s %s", argv[0],
-		    "--name pool_name [--gid pool_gid] [--uid pool_uid]\n");
+		fprintf (stderr, "usage: %s %s %s", argv[0],
+		    "--name pool_name [--gid pool_gid] [--group pool_group]\n",
+		    "[--uid pool_uid] [--user pool_user]\n");
                 exit (USERR);
         }
  
--- vmgr/vmgrmodifypool.c
+++ vmgr/vmgrmodifypool.c
@@ -4,10 +4,12 @@
  */
  
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: vmgrmodifypool.c,v $ $Revision: 1.1 $ $Date: 2000/12/15 15:45:43 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: vmgrmodifypool.c,v $ $Revision: 1.2 $ $Date: 2001/02/04 08:38:00 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	vmgrmodifypool - modify an existing tape pool definition */
+#include <grp.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include "Cgetopt.h"
@@ -20,15 +22,19 @@ char **argv;
 	int c;
 	char *dp;
 	int errflg = 0;
+	struct group *gr;
 	static struct Coptions longopts[] = {
 		{"gid", REQUIRED_ARGUMENT, 0, OPT_POOL_GID},
+		{"group", REQUIRED_ARGUMENT, 0, OPT_POOL_GROUP},
 		{"name", REQUIRED_ARGUMENT, 0, 'P'},
 		{"uid", REQUIRED_ARGUMENT, 0, OPT_POOL_UID},
+		{"user", REQUIRED_ARGUMENT, 0, OPT_POOL_USER},
 		{0, 0, 0, 0}
 	};
 	gid_t pool_gid = -1;
 	char *pool_name = NULL;
 	uid_t pool_uid = -1;
+	struct passwd *pwd;
 
 	Coptind = 1;
         while ((c = Cgetopt_long (argc, argv, "P:", longopts, NULL)) != EOF) {
@@ -41,6 +47,14 @@ char **argv;
 				errflg++;
 			}
 			break;
+		case OPT_POOL_GROUP:
+			if ((gr = getgrnam (Coptarg)) == NULL) {
+				fprintf (stderr,
+				    "invalid pool_group: %s\n", Coptarg);
+				errflg++;
+			} else
+				pool_gid = gr->gr_gid;
+			break;
 		case OPT_POOL_UID:
 			if ((pool_uid = strtol (Coptarg, &dp, 10)) < 0 ||
 			    *dp != '\0') {
@@ -48,6 +62,14 @@ char **argv;
 				    "invalid pool_uid %s\n", Coptarg);
 				errflg++;
 			}
+			break;
+		case OPT_POOL_USER:
+			if ((pwd = getpwnam (Coptarg)) == NULL) {
+				fprintf (stderr,
+				    "invalid pool_user: %s\n", Coptarg);
+				errflg++;
+			} else
+				pool_uid = pwd->pw_uid;
 			break;
 		case 'P':
 			pool_name = Coptarg;
@@ -63,8 +85,9 @@ char **argv;
                 errflg++;
         }
         if (errflg) {
-                fprintf (stderr, "usage: %s %s", argv[0],
-		    "--name pool_name [--gid pool_gid] [--uid pool_uid]\n");
+		fprintf (stderr, "usage: %s %s %s", argv[0],
+		    "--name pool_name [--gid pool_gid] [--group pool_group]\n",
+		    "[--uid pool_uid] [--user pool_user]\n");
                 exit (USERR);
         }
  
