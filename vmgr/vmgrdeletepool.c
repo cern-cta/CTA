8a8b78b5860d9d8f7e81590c459cb522ea08eaf6
Jean-Philippe Baud <baud@cern.ch>
2001-02-23 15:09:53 +0000

    Initial release

---
+++ vmgr/vmgrdeletepool.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2001 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+ 
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: vmgrdeletepool.c,v $ $Revision: 1.1 $ $Date: 2001/02/23 15:09:53 $ CERN IT-PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+/*	vmgrdeletepool - delete a tape pool definition */
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include "Cgetopt.h"
+#include "serrno.h"
+#include "vmgr_api.h"
+main(argc, argv)
+int argc;
+char **argv;
+{
+	int c;
+	int errflg = 0;
+	static struct Coptions longopts[] = {
+		{"name", REQUIRED_ARGUMENT, 0, 'P'},
+		{0, 0, 0, 0}
+	};
+	char *pool_name = NULL;
+
+	Copterr = 1;
+	Coptind = 1;
+        while ((c = Cgetopt_long (argc, argv, "", longopts, NULL)) != EOF) {
+                switch (c) {
+                case 'P':
+			pool_name = Coptarg;
+                        break;
+                case '?':
+                        errflg++;
+                        break;
+                default:
+                        break;
+                }
+        }
+        if (Coptind < argc) {
+                errflg++;
+        }
+        if (errflg) {
+                fprintf (stderr, "usage: %s --name pool_name\n", argv[0]);
+                exit (USERR);
+        }
+ 
+	if (vmgr_deletepool (pool_name) < 0) {
+		fprintf (stderr, "vmgrdeletepool %s: %s\n", pool_name,
+		    (serrno == ENOENT) ? "No such pool" : sstrerror(serrno));
+		exit (USERR);
+	}
+	exit (0);
+}
