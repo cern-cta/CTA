e704fe399cd55543bc62f2742dc97b27b1ce9dcf
Jean-Damien Durand <jdurand@cern.ch>
2001-03-02 18:08:19 +0000

    New function stglogtppool() to log API flags

--- stage/stglogit.c
+++ stage/stglogit.c
@@ -1,5 +1,5 @@
 /*
- * $Id: stglogit.c,v 1.15 2001/02/12 12:29:58 jdurand Exp $
+ * $Id: stglogit.c,v 1.16 2001/03/02 18:08:19 jdurand Exp $
  */
 
 /*
@@ -8,7 +8,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: stglogit.c,v $ $Revision: 1.15 $ $Date: 2001/02/12 12:29:58 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: stglogit.c,v $ $Revision: 1.16 $ $Date: 2001/03/02 18:08:19 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 #include <stdio.h>
@@ -57,6 +57,41 @@ int stglogit(va_alist) va_dcl
 	return(0);
 }
 
+int stglogtppool(va_alist) va_dcl
+{
+	va_list args;
+	char *func;
+	char prtbuf[PRTBUFSZ];
+	struct tm *tm;
+	time_t current_time;
+	int fd_log;
+	char *tppool;
+
+	va_start (args);
+	func = va_arg (args, char *);
+	tppool = va_arg (args, char *);
+	va_end (args);
+
+	if (tppool == NULL) {
+		return(-1);
+	}
+	if (tppool[0] == '\0') {
+		return(-1);
+	}
+
+	/* Buffersize all the flags */
+	time (&current_time);		/* Get current time */
+	tm = localtime (&current_time);
+	sprintf (prtbuf, "%02d/%02d %02d:%02d:%02d %5d %s tppool: %s\n", tm->tm_mon+1,
+		tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec, reqid, func, tppool);
+	if ((fd_log = open (LOGFILE, O_WRONLY | O_CREAT | O_APPEND, 0664)) >= 0) {
+		write (fd_log, prtbuf, strlen(prtbuf));
+		close (fd_log);
+	}
+	return(0);
+}
+
+
 int stglogflags(va_alist) va_dcl
 {
 	va_list args;
@@ -71,26 +106,30 @@ int stglogflags(va_alist) va_dcl
 	int something_to_print;
 
 	struct flag2name flag2names[] = {
-		{ STAGE_DEFERRED  , "STAGE_DEFERRED" } ,
-		{ STAGE_GRPUSER   , "STAGE_GRPUSER" } ,
-		{ STAGE_COFF      , "STAGE_COFF" },
-		{ STAGE_UFUN      , "STAGE_UFUN" },
-		{ STAGE_INFO      , "STAGE_INFO" },
-		{ STAGE_ALL       , "STAGE_ALL" },
-		{ STAGE_LINKNAME  , "STAGE_LINKNAME" },
-		{ STAGE_LONG      , "STAGE_LONG" },
-		{ STAGE_PATHNAME  , "STAGE_PATHNAME" },
-		{ STAGE_SORTED    , "STAGE_SORTED" },
-		{ STAGE_STATPOOL  , "STAGE_STATPOOL" },
-		{ STAGE_TAPEINFO  , "STAGE_TAPEINFO" },
-		{ STAGE_USER      , "STAGE_USER" },
-		{ STAGE_EXTENDED  , "STAGE_EXTENDED" },
-		{ STAGE_ALLOCED   , "STAGE_ALLOCED" },
-		{ STAGE_FILENAME  , "STAGE_FILENAME" },
-		{ STAGE_EXTERNAL  , "STAGE_EXTERNAL" },
+		{ STAGE_DEFERRED  , "STAGE_DEFERRED"  },
+		{ STAGE_GRPUSER   , "STAGE_GRPUSER"   },
+		{ STAGE_COFF      , "STAGE_COFF"      },
+		{ STAGE_UFUN      , "STAGE_UFUN"      },
+		{ STAGE_INFO      , "STAGE_INFO"      },
+		{ STAGE_ALL       , "STAGE_ALL"       },
+		{ STAGE_LINKNAME  , "STAGE_LINKNAME"  },
+		{ STAGE_LONG      , "STAGE_LONG"      },
+		{ STAGE_PATHNAME  , "STAGE_PATHNAME"  },
+		{ STAGE_SORTED    , "STAGE_SORTED"    },
+		{ STAGE_STATPOOL  , "STAGE_STATPOOL"  },
+		{ STAGE_TAPEINFO  , "STAGE_TAPEINFO"  },
+		{ STAGE_USER      , "STAGE_USER"      },
+		{ STAGE_EXTENDED  , "STAGE_EXTENDED"  },
+		{ STAGE_ALLOCED   , "STAGE_ALLOCED"   },
+		{ STAGE_FILENAME  , "STAGE_FILENAME"  },
+		{ STAGE_EXTERNAL  , "STAGE_EXTERNAL"  },
 		{ STAGE_MULTIFSEQ , "STAGE_MULTIFSEQ" },
-		{ STAGE_MIGRULES  , "STAGE_MIGRULES" },
-        { 0               , NULL },
+		{ STAGE_MIGRULES  , "STAGE_MIGRULES"  },
+		{ STAGE_SILENT    , "STAGE_SILENT"    },
+		{ STAGE_NOWAIT    , "STAGE_NOWAIT"    },
+		{ STAGE_CLASS     , "STAGE_CLASS"     },
+		{ STAGE_QUEUE     , "STAGE_QUEUE"     },
+        { 0               , NULL              }
 	};
       
 	va_start (args);
