3a4dbec73fb7ba057307f63a7609008a8d969384
Jean-Damien Durand <jdurand@cern.ch>
2001-03-28 14:00:25 +0000

    Added a 0 before bits values - useless from fonctionnality point of view

--- h/stage_constants.h
+++ h/stage_constants.h
@@ -1,5 +1,5 @@
 /*
- * $Id: stage_constants.h,v 1.3 2001/03/02 18:19:07 jdurand Exp $
+ * $Id: stage_constants.h,v 1.4 2001/03/28 14:00:25 jdurand Exp $
  */
 
 #ifndef __stage_constants_h
@@ -14,22 +14,22 @@
 #endif
 #define	MAXVSN 3	/* maximum number of vsns/vids on a stage command */
 
-#define	NOTSTAGED	  0x00000   /* not staged */
-#define	WAITING_SPC	  0x00010	/* waiting space */
-#define	WAITING_REQ	  0x00020	/* waiting on an other request */
-#define	STAGED		  0x00030	/* stage complete */
-#define	KILLED		  0x00040	/* stage killed */
-#define	STG_FAILED	  0x00050	/* stage failed */
-#define	PUT_FAILED	  0x00060	/* stageput failed */
-#define	STAGED_LSZ	  0x00100	/* stage limited by size */
-#define	STAGED_TPE	  0x00200	/* blocks with parity error have been skipped */
-#define	CAN_BE_MIGR	  0x00400	/* file can be migrated */
-#define	LAST_TPFILE	  0x01000   /* last file on this tape */
-#define	BEING_MIGR	  0x02000   /* file being migrated */
-#define ISSTAGEDBUSY  0x04000   /* Internal error while scanning catalog : matched twice in catalog and the one to delete is busy */
-#define ISSTAGEDSYERR 0x10000   /* Internal error while scanning catalog : matched twice in catalog and cannot delete one of them */
-#define	WAITING_MIGR  0x20000   /* file being migrated and waiting confirmation (e.g. the real migration request) */
-#define	WAITING_NS    0x40000   /* file waiting for an entry in the HSM name server */
+#define	NOTSTAGED	  0x000000   /* not staged */
+#define	WAITING_SPC	  0x000010	/* waiting space */
+#define	WAITING_REQ	  0x000020	/* waiting on an other request */
+#define	STAGED		  0x000030	/* stage complete */
+#define	KILLED		  0x000040	/* stage killed */
+#define	STG_FAILED	  0x000050	/* stage failed */
+#define	PUT_FAILED	  0x000060	/* stageput failed */
+#define	STAGED_LSZ	  0x000100	/* stage limited by size */
+#define	STAGED_TPE	  0x000200	/* blocks with parity error have been skipped */
+#define	CAN_BE_MIGR	  0x000400	/* file can be migrated */
+#define	LAST_TPFILE	  0x001000   /* last file on this tape */
+#define	BEING_MIGR	  0x002000   /* file being migrated */
+#define ISSTAGEDBUSY  0x004000   /* Internal error while scanning catalog : matched twice in catalog and the one to delete is busy */
+#define ISSTAGEDSYERR 0x010000   /* Internal error while scanning catalog : matched twice in catalog and cannot delete one of them */
+#define	WAITING_MIGR  0x020000   /* file being migrated and waiting confirmation (e.g. the real migration request) */
+#define	WAITING_NS    0x040000   /* file waiting for an entry in the HSM name server */
 
 #define	MSG_OUT		0
 #define	MSG_ERR		1
