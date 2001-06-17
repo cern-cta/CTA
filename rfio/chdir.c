8fb33d73f5850c3eecd9bef2ba6d34acf37d9fcf
Jean-Philippe Baud <baud@cern.ch>
2001-06-17 13:38:04 +0000

    Initial release

---
+++ rfio/chdir.c
@@ -0,0 +1,56 @@
+/*
+ * $Id: chdir.c,v 1.1 2001/06/17 13:38:04 baud Exp $
+ */
+
+/*
+ * Copyright (C) 2001 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: chdir.c,v $ $Revision: 1.1 $ $Date: 2001/06/17 13:38:04 $ CERN/IT/PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+/* chdir.c       Remote File I/O - change working directory             */
+
+#define RFIO_KERNEL     1       /* KERNEL part of the routines          */
+
+#include "rfio.h"               /* Remote File I/O general definitions  */
+
+int DLL_DECL rfio_chdir(dirpath)
+char		*dirpath;       /* directory path               */
+{
+   char    	*filename;
+   char    	*host;
+   int    	rc;
+
+   INIT_TRACE("RFIO_TRACE");
+   TRACE(1, "rfio", "rfio_chdir(%s)", dirpath);
+
+   if (!rfio_parseln(dirpath, &host, &filename, NORDLINKS)) {
+      /* if not a remote file, must be local or HSM  */
+      if ( host != NULL ) {
+          /*
+           * HSM file
+           */
+          TRACE(1, "rfio", "rfio_chdir: %s is an HSM path", filename);
+          END_TRACE();
+          rfio_errno = 0;
+          if ( (rc = rfio_HsmIf_chdir(filename)) == 0 )
+              rfio_HsmIf_SetCwdServer(host);
+          return(rc);
+      }
+      TRACE(1, "rfio", "rfio_chdir: using local chdir(%s)", filename);
+
+      END_TRACE();
+      rfio_errno = 0;
+      if ( (rc = chdir(filename)) == 0 )
+          rfio_HsmIf_SetCwdType(0);
+      return(rc);
+   }
+
+   END_TRACE();
+   rfio_errno = 0;
+   serrno = SEOPNOTSUP;
+   return (-1);
+}
---
+++ rfio/getcwd.c
@@ -0,0 +1,39 @@
+/*
+ * $Id: getcwd.c,v 1.1 2001/06/17 13:38:04 baud Exp $
+ */
+
+/*
+ * Copyright (C) 2001 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: getcwd.c,v $ $Revision: 1.1 $ $Date: 2001/06/17 13:38:04 $ CERN/IT/PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+/* getcwd.c      Remote File I/O - get current working directory        */
+
+#define RFIO_KERNEL     1       /* KERNEL part of the routines          */
+
+#include "rfio.h"               /* Remote File I/O general definitions  */
+
+char DLL_DECL *rfio_getcwd(char *buf, int size)
+{
+   INIT_TRACE("RFIO_TRACE");
+   TRACE(1, "rfio", "rfio_getcwd()");
+
+   if ( rfio_HsmIf_GetCwdType() > 0 ) {
+          /*
+           * HSM file
+           */
+          TRACE(1, "rfio", "rfio_getcwd: current working directory is an HSM path");
+          END_TRACE();
+          rfio_errno = 0;
+          return(rfio_HsmIf_getcwd(buf, size));
+   }
+   TRACE(1, "rfio", "rfio_getcwd: using local getcwd()");
+
+   END_TRACE();
+   rfio_errno = 0;
+   return(getcwd(buf, size));
+}
