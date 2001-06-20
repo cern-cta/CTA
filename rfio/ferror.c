0a00653c8f0e874de37235f5e510228dc9ff68dc
Jean-Philippe Baud <baud@cern.ch>
2001-06-20 09:40:02 +0000

    correctly handle remote files

--- rfio/ferror.c
+++ rfio/ferror.c
@@ -1,14 +1,14 @@
 /*
- * $Id: ferror.c,v 1.3 1999/12/09 13:46:42 jdurand Exp $
+ * $Id: ferror.c,v 1.4 2001/06/20 09:40:02 baud Exp $
  */
 
 /*
- * Copyright (C) 1990-1999 by CERN/IT/PDP/DM
+ * Copyright (C) 1990-2001 by CERN/IT/PDP/DM
  * All rights reserved
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: ferror.c,v $ $Revision: 1.3 $ $Date: 1999/12/09 13:46:42 $ CERN/IT/PDP/DM Antoine Trannoy";
+static char sccsid[] = "@(#)$RCSfile: ferror.c,v $ $Revision: 1.4 $ $Date: 2001/06/20 09:40:02 $ CERN/IT/PDP/DM Antoine Trannoy";
 #endif /* not lint */
 
 /* ferror.c      Remote File I/O - tell if an error happened            */
@@ -16,10 +16,61 @@ static char sccsid[] = "@(#)$RCSfile: ferror.c,v $ $Revision: 1.3 $ $Date: 1999/
 #define RFIO_KERNEL     1       /* KERNEL part of the routines          */
 
 #include "rfio.h"               /* Remote File I/O general definitions  */
+#include "rfio_rfilefdt.h"
 
 int rfio_ferror(fp)
-	FILE * fp ; 
+	RFILE * fp ; 
 {
-	return (ferror(fp)) ;
+	int     rc 	;
+
+	INIT_TRACE("RFIO_TRACE");
+	TRACE(1, "rfio", "rfio_ferror(%x)", fp);
+
+	if ( fp == NULL ) {
+		errno = EBADF;
+		END_TRACE();
+		return -1 ;
+	}
+
+	/*
+	 * The file is local
+	 */
+	if (rfio_rfilefdt_findptr(fp,FINDRFILE_WITH_SCAN) == -1 ) {
+		rc= ferror((FILE *)fp) ; 
+		END_TRACE() ; 
+		rfio_errno = 0;
+		return rc ;
+	}
+
+	/*
+	 * Checking magic number
+	 */
+	if ( fp->magic != RFIO_MAGIC) {
+		int fps = fp->s;
+		serrno = SEBADVERSION ; 
+		free((char *)fp);
+		(void) close(fps) ;
+		END_TRACE() ;
+		return -1 ;
+	}
+	
+	/*
+	 * The file is remote, using then the eof flag updated
+ 	 * by rfio_fread.
+	 */
+#ifdef linux
+	if ( ((RFILE *)fp)->eof & _IO_ERR_SEEN ) 
+#else
+#ifdef __Lynx__
+	if ( ((RFILE *)fp)->eof & _ERR ) 
+#else
+	if ( ((RFILE *)fp)->eof & _IOERR ) 
+#endif
+#endif
+		rc = 1 ;
+	else 
+		rc = 0 ;
+	END_TRACE() ;
+	return rc ; 
 }
 
