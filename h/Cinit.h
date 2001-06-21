5a1c661eed034fa8e0a9b6a02c30e2359ff0de86
Jean-Damien Durand <jdurand@cern.ch>
2001-06-21 11:41:19 +0000

    Externalized Cinitdaemon

--- h/Cinit.h
+++ h/Cinit.h
@@ -1,5 +1,5 @@
 /*
- * $Id: Cinit.h,v 1.2 2000/08/25 12:53:40 baud Exp $
+ * $Id: Cinit.h,v 1.3 2001/06/21 11:41:19 jdurand Exp $
  */
 
 /*
@@ -8,11 +8,13 @@
  */
 
 /*
- * @(#)$RCSfile: Cinit.h,v $ $Revision: 1.2 $ $Date: 2000/08/25 12:53:40 $ CERN IT-PDP/DM Jean-Philippe Baud
+ * @(#)$RCSfile: Cinit.h,v $ $Revision: 1.3 $ $Date: 2001/06/21 11:41:19 $ CERN IT-PDP/DM Jean-Philippe Baud
  */
 
 #ifndef _CINIT_H
 #define _CINIT_H
+
+#include "osdep.h"
 
 	/* structure to be used with Cinitdaemon()/Cinitservice() */
 
@@ -20,4 +22,7 @@ struct main_args {
 	int	argc;
 	char	**argv;
 };
+
+EXTERN_C int Cinitdaemon _PROTO((char *, void (*) _PROTO((int))));
+
 #endif
