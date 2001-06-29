7cc3368ce2766fc52d31d77170929c4fc0dd1f29
Jean-Philippe Baud <baud@cern.ch>
2001-06-29 05:04:23 +0000

    include <stddef.h>                 /* For size_t */

--- h/Cglobals.h
+++ h/Cglobals.h
@@ -1,9 +1,9 @@
 /*
- * $Id: Cglobals.h,v 1.8 2001/06/14 10:38:54 jdurand Exp $
+ * $Id: Cglobals.h,v 1.9 2001/06/29 05:04:23 baud Exp $
  */
 
 /*
- * Copyright (C) 1999 by CERN/IT/PDP/DM
+ * Copyright (C) 1999-2001 by CERN/IT/PDP/DM
  * All rights reserved
  */
 
@@ -11,6 +11,7 @@
 #define _CASTOR_GLOBALS_H    
 
 #include <osdep.h>
+#include <stddef.h>                 /* For size_t                    */
 
 EXTERN_C void DLL_DECL Cglobals_init _PROTO((int (*) _PROTO((int *, void **)),
                                              int (*) _PROTO((int *, void *)),
