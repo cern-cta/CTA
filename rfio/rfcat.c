b8672df19219569905af185961949b55f26be826
Jean-Philippe Baud <baud@cern.ch>
2001-10-04 13:04:03 +0000

    Initial release

---
+++ rfio/rfcat.c
@@ -0,0 +1,73 @@
+/*
+ * $Id: rfcat.c,v 1.1 2001/10/04 13:04:03 baud Exp $
+ */
+
+/*
+ * Copyright (C) 2001 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: rfcat.c,v $ $Revision: 1.1 $ $Date: 2001/10/04 13:04:03 $ CERN/IT/PDP/DM Jean-Philippe Baud";
+#endif /* not lint */
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#if defined(_WIN32)
+#include <winsock2.h>
+#endif
+#include "rfio_api.h"
+main(argc, argv)
+int argc;
+char **argv;
+{
+	int errflg = 0;
+	int i;
+#if defined(_WIN32)
+	WSADATA wsadata;
+
+	if (WSAStartup (MAKEWORD (2, 0), &wsadata)) {
+		fprintf (stderr, "WSAStartup unsuccessful\n");
+		exit (2);
+	}
+#endif
+
+	if (argc == 1)
+		errflg = catfile ("-");
+	else
+		for (i = 1; i < argc; i++)
+			errflg += catfile (argv[i]);
+#if defined(_WIN32)
+	WSACleanup();
+#endif
+	exit (errflg ? 1 : 0);
+}
+
+catfile(inpfile)
+char *inpfile;
+{
+	char buf [32768];
+	int c;
+	int rc;
+	FILE *s;
+
+	if (strcmp (inpfile, "-") == 0)
+		s  = stdin;
+	else if ((s = rfio_fopen (inpfile, "r")) == NULL) {
+		rfio_perror (inpfile);
+		return (1);
+	}
+	while ((c = rfio_fread (buf, 1, sizeof(buf), s)) > 0) {
+		if ((rc = fwrite (buf, 1, c, stdout)) < c) {
+			fprintf (stderr, "rfcat %s: %s\n", inpfile,
+			    strerror ((rc < 0) ? errno : ENOSPC));
+			if (strcmp (inpfile, "-"))
+				rfio_fclose (s);
+			return (1);
+		}
+	}
+	if (strcmp (inpfile, "-"))
+		rfio_fclose (s);
+	return (0);
+}
