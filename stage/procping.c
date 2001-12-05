bf0b67c0c71eb61115ccedb23ae0db0b1637a985
Jean-Damien Durand <jdurand@cern.ch>
2001-12-05 10:14:54 +0000

    New PING functionnality

---
+++ stage/procping.c
@@ -0,0 +1,137 @@
+/*
+ * $Id: procping.c,v 1.1 2001/12/05 10:14:54 jdurand Exp $
+ */
+
+/*
+ * Copyright (C) 2001 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: procping.c,v $ $Revision: 1.1 $ $Date: 2001/12/05 10:14:54 $ CERN IT-PDP/DM Jean-Damien Durand";
+#endif /* not lint */
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include "osdep.h"
+#include "stage_constants.h"
+#include "stage_messages.h"
+#include "Cgrp.h"
+#include "Castor_limits.h"
+#include "stage_api.h"
+#include "Cgetopt.h"
+#include "marshall.h"
+#define local_unmarshall_STRING(ptr,str)  { str = ptr ; INC_PTR(ptr,strlen(str)+1) ; }
+#if SACCT
+#include "../h/sacct.h"
+#endif
+#include "patchlevel.h"
+#define __BASEVERSION__ "?"
+#define __PATCHLEVEL__ 0
+
+void procpingreq _PROTO((int, int, char *, char *));
+
+extern char func[16];
+extern int rpfd;
+extern int reqid;
+
+void procpingreq(req_type, magic, req_data, clienthost)
+		 int req_type;
+		 int magic;
+		 char *req_data;
+		 char *clienthost;
+{
+	int c;
+	int rc = 0;
+	char *rbp;
+	char *user;
+	gid_t gid;
+	struct group *gr;
+	extern char localhost[CA_MAXHOSTNAMELEN+1];
+	extern time_t started_time;
+	char timestr[64] ;   /* Time in its ASCII format             */
+	static struct Coptions longopts[] =
+	{
+		{"host",               REQUIRED_ARGUMENT,  NULL,      'h'},
+		{"verbose",            NO_ARGUMENT,        NULL,      'v'},
+		{NULL,                 0,                  NULL,        0}
+	};
+	u_signed64 flags;
+	int nargs;
+	int errflg = 0;
+	char **argv = NULL;
+#if defined(HPSSCLIENT)
+	char hpss_aware = 'H';
+#else
+	char hpss_aware = ' ';
+#endif
+	int verbose_flag = 0;
+
+	rbp = req_data;
+	local_unmarshall_STRING (rbp, user);	/* login name */
+	if (req_type > STAGE_00) {
+		unmarshall_LONG (rbp, gid);
+		unmarshall_HYPER (rbp, flags);
+		stglogit (func, "STG92 - %s request by %s (,%d) from %s\n", "stage_ping", user, gid, clienthost);
+    } else {
+		unmarshall_WORD (rbp, gid);
+		stglogit (func, "STG92 - %s request by %s (,%d) from %s\n", "stageping", user, gid, clienthost);
+		nargs = req2argv (rbp, &argv);
+	}
+#if SACCT
+	stageacct (STGCMDR, -1, gid, clienthost,
+						 reqid, req_type, 0, 0, NULL, "");
+#endif
+	
+	if ((gr = Cgetgrgid (gid)) == NULL) {
+		sendrep (rpfd, MSG_ERR, STG36, gid);
+		rc = SYERR;
+		goto reply;
+	}
+
+	if (req_type > STAGE_00) {
+		if ((flags & STAGE_VERBOSE) == STAGE_VERBOSE) {
+			verbose_flag++;
+		}
+		/* Print the flags */
+		stglogflags("stage_ping",flags);
+	} else {
+		Coptind = 1;
+		Copterr = 0;
+		while ((c = Cgetopt_long (nargs, argv, "h:v", longopts, NULL)) != -1) {
+			switch (c) {
+			case 'h':
+				break;
+			case 'v':
+				verbose_flag++;
+				break;
+			case 0:
+				/* These are the long options */
+				break;
+			default:
+				errflg++;
+				break;
+			}
+			if (errflg != 0) break;
+		}
+	}
+
+	if (errflg != 0) {
+		rc = USERR;
+		goto reply;
+	}
+
+	/* We send util information */
+	/* The list of object linked to produce stgdaemon is: */
+	/* stgdaemon.o packfseq.o poolmgr.o procalloc.o procclr.o  stgdb_Cdb_ifce.o Cstage_db.o Cstage_ifce.o  procio.o procping.o procqry.o procupd.o procfilchg.o sendrep.o stglogit.o stageacct.o */
+
+	if (verbose_flag) {
+		stage_util_time(started_time,timestr);
+		sendrep (rpfd, MSG_OUT, "Stager daemon - CASTOR %s.%d%c\n", BASEVERSION, PATCHLEVEL, hpss_aware);
+		sendrep (rpfd, MSG_OUT, "Generated %s around %s\n", __DATE__, __TIME__);
+		sendrep (rpfd, MSG_OUT, "Running since %s\n", timestr);
+	}
+	reply:
+	sendrep (rpfd, STAGERC, STAGEPING, rc);
+	return;
+}
---
+++ stage/stageping.c
@@ -0,0 +1,176 @@
+/*
+ * $Id: stageping.c,v 1.1 2001/12/05 10:14:54 jdurand Exp $
+ */
+
+/*
+ * Copyright (C) 2001 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: stageping.c,v $ $Revision: 1.1 $ $Date: 2001/12/05 10:14:54 $ CERN IT-PDP/DM Jean-Damien Durand";
+#endif /* not lint */
+
+#include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include "Cpwd.h"
+#include "Cgrp.h"
+#include <string.h>
+#if defined(_WIN32)
+#include <winsock2.h>
+#else
+#include <unistd.h>
+#include <netinet/in.h>
+#endif
+#include "marshall.h"
+#include "stage_api.h"
+#include "Cgetopt.h"
+#include "serrno.h"
+
+EXTERN_C int  DLL_DECL  send2stgd_cmd _PROTO((char *, char *, int, int, char *, int));  /* Command-line version */
+void usage _PROTO((char *));
+void cleanup _PROTO((int));
+
+int main(argc, argv)
+		 int	argc;
+		 char	**argv;
+{
+	int c, i;
+	int errflg = 0;
+	gid_t gid;
+	struct group *gr;
+	int msglen;
+	int ntries = 0;
+	int nstg161 = 0;
+	char *p;
+	struct passwd *pw;
+	char *q;
+	char *sbp;
+	char sendbuf[REQBUFSZ];
+	char *stghost = NULL;
+	uid_t uid;
+#if defined(_WIN32)
+	WSADATA wsadata;
+#endif
+	static struct Coptions longopts[] =
+	{
+		{"host",               REQUIRED_ARGUMENT,  NULL,      'h'},
+		{"verbose",            NO_ARGUMENT,        NULL,      'v'},
+		{NULL,                 0,                  NULL,        0}
+	};
+
+	uid = getuid();
+	gid = getgid();
+#if defined(_WIN32)
+	if (uid < 0 || gid < 0) {
+		fprintf (stderr, STG52);
+		exit (USERR);
+	}
+#endif
+	Coptind = 1;
+	Copterr = 1;
+	while ((c = Cgetopt_long (argc, argv, "h:v", longopts, NULL)) != -1) {
+		switch (c) {
+		case 'h':
+			stghost = Coptarg;
+			break;
+		case 'v':
+			break;
+		case 0:
+			/* Here are the long options */
+			break;
+		case '?':
+			errflg++;
+			break;
+		default:
+			errflg++;
+			break;
+		}
+        if (errflg != 0) break;
+	}
+	if (argc > Coptind) {
+		fprintf (stderr, STG16);
+		errflg++;
+	}
+
+	if (errflg != 0) {
+		usage (argv[0]);
+		exit (USERR);
+	}
+
+	/* Build request header */
+
+	sbp = sendbuf;
+	marshall_LONG (sbp, STGMAGIC);
+	marshall_LONG (sbp, STAGEPING);
+	q = sbp;	/* save pointer. The next field will be updated */
+	msglen = 3 * LONGSIZE;
+	marshall_LONG (sbp, msglen);
+
+	/* Build request body */
+
+	if ((pw = Cgetpwuid (uid)) == NULL) {
+		char uidstr[8];
+		sprintf (uidstr, "%d", uid);
+		p = uidstr;
+		fprintf (stderr, STG11, p);
+		exit (SYERR);
+	}
+	marshall_STRING (sbp, pw->pw_name);	/* login name */
+	marshall_WORD (sbp, gid);
+	marshall_WORD (sbp, argc);
+	for (i = 0; i < argc; i++)
+		marshall_STRING (sbp, argv[i]);
+	
+	msglen = sbp - sendbuf;
+	marshall_LONG (q, msglen);	/* update length field */
+	
+#if defined(_WIN32)
+	if (WSAStartup (MAKEWORD (2, 0), &wsadata)) {
+		fprintf (stderr, STG51);
+		exit (SYERR);
+	}
+#endif
+	
+#if !defined(_WIN32)
+	signal (SIGHUP, cleanup);
+#endif
+	signal (SIGINT, cleanup);
+#if !defined(_WIN32)
+	signal (SIGQUIT, cleanup);
+#endif
+	signal (SIGTERM, cleanup);
+	
+	while (1) {
+		c = send2stgd_cmd (stghost, sendbuf, msglen, 1, NULL, 0);
+		if (c == 0 || serrno == EINVAL) break;
+		if (serrno == ESTNACT && nstg161++ == 0) fprintf(stderr, STG161);
+		if (serrno != ESTNACT && ntries++ > MAXRETRY) break;
+		sleep (RETRYI);
+	}
+#if defined(_WIN32)
+	WSACleanup();
+#endif
+	exit (c == 0 ? 0 : rc_castor2shift(serrno));
+}
+
+void cleanup(sig)
+		 int sig;
+{
+	signal (sig, SIG_IGN);
+	
+#if defined(_WIN32)
+	WSACleanup();
+#endif
+	exit (USERR);
+}
+
+void usage(cmd)
+		 char *cmd;
+{
+	fprintf (stderr, "usage: %s ", cmd);
+	fprintf (stderr, "%s", "[-h stage_host] [-v]\n");
+}
