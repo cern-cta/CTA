5796e2b95748bacea9a749edf36aaf74fef200c1
Jean-Philippe Baud <baud@cern.ch>
2002-02-07 06:12:58 +0000

    add support for new fields: library, nbsides, etime, side, rhost, whost, rjid and wjid do not call vmgr_querytape

--- vmgr/vmgrlisttape.c
+++ vmgr/vmgrlisttape.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2000-2001 by CERN/IT/PDP/DM
+ * Copyright (C) 2000-2002 by CERN/IT/PDP/DM
  * All rights reserved
  */
  
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: vmgrlisttape.c,v $ $Revision: 1.15 $ $Date: 2001/07/24 07:57:13 $ CERN IT-PDP/DM Jean-Philippe Baud";
+static char sccsid[] = "@(#)$RCSfile: vmgrlisttape.c,v $ $Revision: 1.16 $ $Date: 2002/02/07 06:12:58 $ CERN IT-PDP/DM Jean-Philippe Baud";
 #endif /* not lint */
 
 /*	vmgrlisttape - query a given volume or list all existing tapes */
@@ -27,27 +27,14 @@ int argc;
 char **argv;
 {
 	int c;
-	char density[CA_MAXDENLEN+1];
 	char dgn[CA_MAXDGNLEN+1];
 	int errflg = 0;
 	int flags;
-	int free_space;
-	char lbltype[3];
 	vmgr_list list;
 	struct vmgr_tape_info *lp;
-	char manufacturer[CA_MAXMANUFLEN+1];
-	char media_letter[CA_MAXMLLEN+1];
-	char model[CA_MAXMODELLEN+1];
-	int nbfiles;
 	char pool_name[CA_MAXPOOLNAMELEN+1];
-	int rcount;
-	time_t rtime;
-	char sn[CA_MAXSNLEN+1];
-	int status;
+	struct vmgr_tape_info *tape_info;
 	char vid[CA_MAXVIDLEN+1];
-	char vsn[CA_MAXVSNLEN+1];
-	int wcount;
-	time_t wtime;
 #if defined(_WIN32)
 	WSADATA wsadata;
 #endif
@@ -98,57 +85,25 @@ char **argv;
 		exit (SYERR);
 	}
 #endif
-	if (*vid) {
-		if (vmgr_querytape (vid, vsn, dgn, density, lbltype, model,
-		    media_letter, manufacturer, sn, pool_name, &free_space,
-		    &nbfiles, &rcount, &wcount, &rtime, &wtime, &status) < 0) {
-			fprintf (stderr, "vmgrlisttape %s: %s\n", vid,
-			    (serrno == ENOENT) ? "No such tape" : sstrerror(serrno));
-#if defined(_WIN32)
-			WSACleanup();
-#endif
-			exit (USERR);
-		}
-		listentry (vid, vsn, dgn, density, lbltype, model,
-		    media_letter, manufacturer, sn, pool_name, free_space,
-		    nbfiles, rcount, wcount, rtime, wtime, status, xflag);
-	} else {
-		flags = VMGR_LIST_BEGIN;
-		while ((lp = vmgr_listtape (pool_name, flags, &list)) != NULL) {
-			listentry (lp->vid, lp->vsn, lp->dgn, lp->density,
-			    lp->lbltype, lp->model, lp->media_letter,
-			    lp->manufacturer, lp->sn, lp->poolname,
-			    lp->estimated_free_space, lp->nbfiles, lp->rcount,
-			    lp->wcount, lp->rtime, lp->wtime, lp->status, xflag);
-			flags = VMGR_LIST_CONTINUE;
-		}
-		(void) vmgr_listtape (pool_name, VMGR_LIST_END, &list);
+	flags = VMGR_LIST_BEGIN;
+	serrno = 0;
+	while ((lp = vmgr_listtape (vid, pool_name, flags, &list)) != NULL) {
+		listentry (lp, xflag);
+		flags = VMGR_LIST_CONTINUE;
 	}
+	if (serrno)
+		fprintf (stderr, "vmgrlisttape: %s\n",
+		    (serrno == ENOENT) ? "No such tape" : sstrerror(serrno));
+	else
+		(void) vmgr_listtape (vid, pool_name, VMGR_LIST_END, &list);
 #if defined(_WIN32)
 	WSACleanup();
 #endif
 	exit (0);
 }
 
-listentry(vid, vsn, dgn, density, lbltype, model, media_letter, manufacturer,
-sn, pool_name, free_space, nbfiles, rcount, wcount, rtime, wtime, status, xflag)
-char *vid;
-char *vsn;
-char *dgn;
-char *density;
-char *lbltype;
-char *model;
-char *media_letter;
-char *manufacturer;
-char *sn;
-char *pool_name;
-int free_space;
-int nbfiles;
-int rcount;
-int wcount;
-time_t rtime;
-time_t wtime;
-int status;
+listentry(lp, xflag)
+struct vmgr_tape_info *lp;
 int xflag;
 {
 	time_t ltime;
@@ -157,12 +112,16 @@ int xflag;
 	char tmpbuf[9];
 	u_signed64 u64;
 
-	u64 = ((u_signed64) free_space) * 1024;
-	printf ("%-6s %-6s %-6s %-8s %-2s ",
-	    vid, vsn, dgn, density, lbltype);
+	u64 = ((u_signed64) lp->estimated_free_space) * 1024;
+	if (lp->nbsides > 1)
+		printf ("%-6s/%d ", lp->vid, lp->side);
+	else
+		printf ("%-6s   ", lp->vid);
+	printf ("%-6s %-8s %-8s %-3s ",
+	    lp->vsn, lp->library, lp->density, lp->lbltype);
 	if (! xflag) {
-		printf ("%-15s %-8sB ", pool_name, u64tostru (u64, tmpbuf, 8));
-		ltime = (wtime < rtime) ? rtime : wtime;
+		printf ("%-15s %-8sB ", lp->poolname, u64tostru (u64, tmpbuf, 8));
+		ltime = (lp->wtime < lp->rtime) ? lp->rtime : lp->wtime;
 		if (ltime) {
 			tm = localtime (&ltime);
 			printf ("%04d%02d%02d ",
@@ -170,43 +129,52 @@ int xflag;
 		} else
 			printf ("00000000 ");
 	} else {
-		printf ("%-6s %-2s %-12s %-25s %-15s %-8sB %6d %5d %5d ",
-		    model, media_letter, manufacturer, sn, pool_name,
-		    u64tostru (u64, tmpbuf, 8), nbfiles, rcount, wcount);
-		if (rtime) {
-			tm = localtime (&rtime);
+		printf ("%-6s %-2s %-12s %-25s %-15s ",
+		    lp->model, lp->media_letter, lp->manufacturer, lp->sn,
+		    lp->poolname);
+		if (lp->etime) {
+			tm = localtime (&lp->etime);
 			printf ("%04d%02d%02d ",
 			    tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday);
 		} else
 			printf ("00000000 ");
-		if (wtime) {
-			tm = localtime (&wtime);
+		printf ("%-8sB %6d %5d %5d %-10s %-10s %10d %10d ",
+		    u64tostru (u64, tmpbuf, 8), lp->nbfiles, lp->rcount,
+		    lp->wcount, lp->rhost, lp->whost, lp->rjid, lp->wjid);
+		if (lp->rtime) {
+			tm = localtime (&lp->rtime);
+			printf ("%04d%02d%02d ",
+			    tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday);
+		} else
+			printf ("00000000 ");
+		if (lp->wtime) {
+			tm = localtime (&lp->wtime);
 			printf ("%04d%02d%02d ",
 			    tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday);
 		} else
 			printf ("00000000 ");
 	}
-	if (status & TAPE_FULL) {
+	if (lp->status & TAPE_FULL) {
 		printf ("FULL");
 		p_stat = '|';
 	}
-	if (status & TAPE_BUSY) {
+	if (lp->status & TAPE_BUSY) {
 		printf (p_stat ? "|BUSY" : "BUSY");
 		p_stat = '|';
 	}
-	if (status & TAPE_RDONLY) {
+	if (lp->status & TAPE_RDONLY) {
 		printf (p_stat ? "|RDONLY" : "RDONLY");
 		p_stat = '|';
 	}
-	if (status & EXPORTED) {
+	if (lp->status & EXPORTED) {
 		printf (p_stat ? "|EXPORTED" : "EXPORTED");
 		p_stat = '|';
 	}
-	if (status & DISABLED) {
+	if (lp->status & DISABLED) {
 		printf (p_stat ? "|DISABLED" : "DISABLED");
 		p_stat = '|';
 	}
-	if (status & ARCHIVED) {
+	if (lp->status & ARCHIVED) {
 		printf (p_stat ? "|ARCHIVED" : "ARCHIVED");
 		p_stat = '|';
 	}
