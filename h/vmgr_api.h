d82bb5ca3bb0038906796621f3ab80fe075e2bdd
Jean-Philippe Baud <baud@cern.ch>
2002-02-07 05:58:08 +0000

    modify function prototypes for library, side and tape history support

--- h/vmgr_api.h
+++ h/vmgr_api.h
@@ -1,14 +1,14 @@
 /*
- * $Id: vmgr_api.h,v 1.26 2001/03/08 10:22:30 baud Exp $
+ * $Id: vmgr_api.h,v 1.27 2002/02/07 05:58:08 baud Exp $
  */
 
 /*
- * Copyright (C) 1999-2001 by CERN/IT/PDP/DM
+ * Copyright (C) 1999-2002 by CERN/IT/PDP/DM
  * All rights reserved
  */
 
 /*
- * @(#)$RCSfile: vmgr_api.h,v $ $Revision: 1.26 $ $Date: 2001/03/08 10:22:30 $ CERN IT-PDP/DM Jean-Philippe Baud
+ * @(#)$RCSfile: vmgr_api.h,v $ $Revision: 1.27 $ $Date: 2002/02/07 05:58:08 $ CERN IT-PDP/DM Jean-Philippe Baud
  */
 
 #ifndef _VMGR_API_H
@@ -42,7 +42,7 @@ typedef struct {
 			/* function prototypes */
 
 EXTERN_C int DLL_DECL vmgr_deletedenmap _PROTO((const char *, char *, char *));
-EXTERN_C int DLL_DECL vmgr_deletedgnmap _PROTO((const char *));
+EXTERN_C int DLL_DECL vmgr_deletedgnmap _PROTO((const char *, char *));
 EXTERN_C int DLL_DECL vmgr_deletelibrary _PROTO((const char *));
 EXTERN_C int DLL_DECL vmgr_deletemodel _PROTO((const char *, char *));
 EXTERN_C int DLL_DECL vmgr_deletepool _PROTO((const char *));
@@ -52,14 +52,14 @@ EXTERN_C int DLL_DECL vmgr_enterdgnmap _PROTO((const char *, char *, char *));
 EXTERN_C int DLL_DECL vmgr_enterlibrary _PROTO((const char *, int, int));
 EXTERN_C int DLL_DECL vmgr_entermodel _PROTO((const char *, char *, int, int));
 EXTERN_C int DLL_DECL vmgr_enterpool _PROTO((const char *, uid_t, gid_t));
-EXTERN_C int DLL_DECL vmgr_entertape _PROTO((const char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, int));
-EXTERN_C int DLL_DECL vmgr_gettape _PROTO((const char *, u_signed64, const char *, char *, char *, char *, char *, char *, char *, int *, u_signed64 *));
+EXTERN_C int DLL_DECL vmgr_entertape _PROTO((const char *, char *, char *, char *, char *, char *, char *, char *, char *, int, char *, int));
+EXTERN_C int DLL_DECL vmgr_gettape _PROTO((const char *, u_signed64, const char *, char *, char *, char *, char *, char *, char *, int *, int *, u_signed64 *));
 EXTERN_C struct vmgr_tape_denmap DLL_DECL *vmgr_listdenmap _PROTO((int, vmgr_list *));
 EXTERN_C struct vmgr_tape_dgnmap DLL_DECL *vmgr_listdgnmap _PROTO((int, vmgr_list *));
 EXTERN_C struct vmgr_tape_library DLL_DECL *vmgr_listlibrary _PROTO((int, vmgr_list *));
 EXTERN_C struct vmgr_tape_media DLL_DECL *vmgr_listmodel _PROTO((int, vmgr_list *));
 EXTERN_C struct vmgr_tape_pool DLL_DECL *vmgr_listpool _PROTO((int, vmgr_list *));
-EXTERN_C struct vmgr_tape_info DLL_DECL *vmgr_listtape _PROTO((char *, int, vmgr_list *));
+EXTERN_C struct vmgr_tape_info DLL_DECL *vmgr_listtape _PROTO((char *, char *, int, vmgr_list *));
 EXTERN_C int DLL_DECL vmgr_modifylibrary _PROTO((const char *, int, int));
 EXTERN_C int DLL_DECL vmgr_modifymodel _PROTO((const char *, char *, int, int));
 EXTERN_C int DLL_DECL vmgr_modifypool _PROTO((const char *, uid_t, gid_t));
@@ -67,9 +67,9 @@ EXTERN_C int DLL_DECL vmgr_modifytape _PROTO((const char *, char *, char *, char
 EXTERN_C int DLL_DECL vmgr_querylibrary _PROTO((const char *, int *, int *, int *));
 EXTERN_C int DLL_DECL vmgr_querymodel _PROTO((const char *, char *, int *, int *));
 EXTERN_C int DLL_DECL vmgr_querypool _PROTO((const char *, uid_t *, gid_t *, u_signed64 *, u_signed64 *));
-EXTERN_C int DLL_DECL vmgr_querytape _PROTO((const char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, int *, int *, int *, int *, time_t *, time_t *, int *));
+EXTERN_C int DLL_DECL vmgr_querytape _PROTO((const char *, int, struct vmgr_tape_info *, char *));
 EXTERN_C int DLL_DECL vmgr_reclaim _PROTO((const char *));
 EXTERN_C int DLL_DECL vmgr_seterrbuf _PROTO((char *, int));
-EXTERN_C int DLL_DECL vmgr_tpmounted _PROTO((const char *, int));
-EXTERN_C int DLL_DECL vmgr_updatetape _PROTO((const char *, u_signed64, int, int, int));
+EXTERN_C int DLL_DECL vmgr_tpmounted _PROTO((const char *, int, int));
+EXTERN_C int DLL_DECL vmgr_updatetape _PROTO((const char *, int, u_signed64, int, int, int));
 #endif
