d89bd92a900b352d02e0d86e8b732c48a3f14e8e
Jean-Philippe Baud <baud@cern.ch>
2002-08-15 10:17:52 +0000

    fix problem of stageupdc being sent before the physical close (and flush)

--- rfio/fclose.c
+++ rfio/fclose.c
@@ -1,14 +1,14 @@
 /*
- * $Id: fclose.c,v 1.10 2001/07/04 11:37:40 baud Exp $
+ * $Id: fclose.c,v 1.11 2002/08/15 10:17:52 baud Exp $
  */
 
 /*
- * Copyright (C) 1990-2001 by CERN/IT/PDP/DM
+ * Copyright (C) 1990-2002 by CERN/IT/PDP/DM
  * All rights reserved
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: fclose.c,v $ $Revision: 1.10 $ $Date: 2001/07/04 11:37:40 $ CERN/IT/PDP/DM F. Hemmer, A. Trannoy";
+static char sccsid[] = "@(#)$RCSfile: fclose.c,v $ $Revision: 1.11 $ $Date: 2002/08/15 10:17:52 $ CERN/IT/PDP/DM F. Hemmer, A. Trannoy";
 #endif /* not lint */
 
 /* fclose.c     Remote File I/O - close a binary file                   */
@@ -17,6 +17,7 @@ static char sccsid[] = "@(#)$RCSfile: fclose.c,v $ $Revision: 1.10 $ $Date: 2001
  * System remote file I/O definitions
  */
 #define RFIO_KERNEL     1     
+#include "Castor_limits.h"    
 #include "rfio.h"    
 #include "rfio_rfilefdt.h"
 
@@ -28,6 +29,7 @@ RFILE *fp;                      /* Remote file pointer                  */
    char    *p = buf;
    int     HsmType;
    int     status;
+   int     status1;
 
 
    INIT_TRACE("RFIO_TRACE");
@@ -47,26 +49,33 @@ RFILE *fp;                      /* Remote file pointer                  */
     * The file is local : this is the only way to detect it !
     */
    if (rfio_rfilefdt_findptr(fp,FINDRFILE_WITH_SCAN) == -1) {
+      char upath[CA_MAXHOSTNAMELEN+CA_MAXPATHLEN+2];
       int fd = fileno((FILE *)fp);
       HsmType = rfio_HsmIf_GetHsmType(fd,NULL);
       if ( HsmType > 0 ) {
-          status = rfio_HsmIf_close(fd);
-          if ( HsmType != RFIO_HSM_CNS ) return(status);
+          if ( HsmType != RFIO_HSM_CNS ) {
+              status = rfio_HsmIf_close(fd);
+              END_TRACE() ; 
+              return(status);
+          }
+          status1 = rfio_HsmIf_getipath(fd,upath);
       }
       status= fclose((FILE *)fp) ;
+      if ( HsmType == RFIO_HSM_CNS ) {
+          if ( status1 == 1 ) {
+              status1 = rfio_HsmIf_reqtoput(upath);
+          }
+      } else {
+          status1 = 0;
+      }
       END_TRACE() ; 
       rfio_errno = 0;
-      return status ; 
+      return (status ? status : status1) ; 
    }
 
    /*
     * The file is remote
     */
-   HsmType = rfio_HsmIf_GetHsmType(fp->s,NULL);
-   if ( HsmType > 0 ) {
-       status = rfio_HsmIf_close(fp->s);
-       if ( HsmType != RFIO_HSM_CNS ) return(status);
-   }
    if ( fp->magic != RFIO_MAGIC ) {
       int fps = fp->s;
 
