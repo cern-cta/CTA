a8da4c9531048a560a8eae91dcf875faf69d366c
Jean-Damien Durand <jdurand@cern.ch>
2002-10-09 07:37:19 +0000

    Conform to new entry points stage_<function>_<more>

--- h/stage_api.h
+++ h/stage_api.h
@@ -1,5 +1,5 @@
 /*
- * $Id: stage_api.h,v 1.62 2002/10/08 08:48:32 jdurand Exp $
+ * $Id: stage_api.h,v 1.63 2002/10/09 07:37:19 jdurand Exp $
  */
 
 #ifndef __stage_api_h
@@ -57,37 +57,37 @@ EXTERN_C int DLL_DECL stage_iowc _PROTO((int,                       /* req_type 
                                          int,                       /* nstpp_input */
                                          struct stgpath_entry *));  /* stpp_input */
 #endif /* hpux */
-#define stagein_tape(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_in_tape(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_IN, 't',flags,openflags,(mode_t) 0,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagein_disk(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_in_disk(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_IN, 'd',flags,openflags,(mode_t) 0,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagein_hsm(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_in_hsm(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_IN, 'm',flags,openflags,(mode_t) 0,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagein_castor(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_in_castor(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_IN, 'h',flags,openflags,(mode_t) 0,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stageout_tape(flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_out_tape(flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_OUT,'t',flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stageout_disk(flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_out_disk(flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_OUT,'d',flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stageout_hsm(flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_out_hsm(flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_OUT,'m',flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stageout_castor(flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_out_castor(flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_OUT,'h',flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagewrt_tape(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_wrt_tape(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_WRT,'t',flags,openflags,(mode_t) 0,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagewrt_disk(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_wrt_disk(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_WRT,'d',flags,openflags,(mode_t) 0,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagewrt_hsm(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_wrt_hsm(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_WRT,'m',flags,openflags,(mode_t) 0,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagewrt_castor(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_wrt_castor(flags,openflags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_WRT,'h',flags,openflags,(mode_t) 0,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagecat_tape(flags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_cat_tape(flags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_CAT,'t',flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagecat_disk(flags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_cat_disk(flags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_CAT,'d',flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagecat_hsm(flags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_cat_hsm(flags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_CAT,'m',flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
-#define stagecat_castor(flags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
+#define stage_cat_castor(flags,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input) \
   stage_iowc(STAGE_CAT,'h',flags,openflags,openmode,hostname,pooluser,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_input,stpp_input)
 
 /* --------------------------------- */
@@ -153,42 +153,42 @@ EXTERN_C int DLL_DECL stage_qry _PROTO((char,                      /* t_or_d */
                                         int *,                     /* nstpp_output */
                                         struct stgpath_entry **)); /* stpp_output */
 #endif /* hpux */
-#define stageqry_tape(flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output) \
+#define stage_qry_tape(flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output) \
   stage_qry('t',flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output)
-#define stageqry_disk(flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output) \
+#define stage_qry_disk(flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output) \
   stage_qry('d',flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output)
-#define stageqry_hsm(flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output) \
+#define stage_qry_hsm(flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output) \
   stage_qry('m',flags,hostname,nstcp_input,stcp_input,nstcp_output,stcp_output,nstpp_output,stpp_output)
 
 /* ----------------------------- */
 /* Shorthand STAGE_QRY interface */
 /* ----------------------------- */
 
-EXTERN_C int DLL_DECL stageqry_Tape _PROTO((u_signed64,                /* flags */
-                                            char *,                    /* hostname */
-                                            char *,                    /* poolname */
-                                            char *,                    /* tape */
-                                            char *,                    /* fseq */
-                                            int *,                     /* nstcp_output */
-                                            struct stgcat_entry **,    /* stcp_output */
-                                            int *,                     /* nstpp_output */
-                                            struct stgpath_entry **)); /* stpp_output */
-EXTERN_C int DLL_DECL stageqry_Disk _PROTO((u_signed64,                /* flags */
-                                            char *,                    /* hostname */
-                                            char *,                    /* poolname */
-                                            char *,                    /* filename */
-                                            int *,                     /* nstcp_output */
-                                            struct stgcat_entry **,    /* stcp_output */
-                                            int *,                     /* nstpp_output */
-                                            struct stgpath_entry **)); /* stpp_output */
-EXTERN_C int DLL_DECL stageqry_Hsm _PROTO((u_signed64,                /* flags */
-                                           char *,                    /* hostname */
-                                           char *,                    /* poolname */
-                                           char *,                    /* hsmname */
-                                           int *,                     /* nstcp_output */
-                                           struct stgcat_entry **,    /* stcp_output */
-                                           int *,                     /* nstpp_output */
-                                           struct stgpath_entry **)); /* stpp_output */
+EXTERN_C int DLL_DECL stage_qry_Tape _PROTO((u_signed64,                /* flags */
+											 char *,                    /* hostname */
+											 char *,                    /* poolname */
+											 char *,                    /* tape */
+											 char *,                    /* fseq */
+											 int *,                     /* nstcp_output */
+											 struct stgcat_entry **,    /* stcp_output */
+											 int *,                     /* nstpp_output */
+											 struct stgpath_entry **)); /* stpp_output */
+EXTERN_C int DLL_DECL stage_qry_Disk _PROTO((u_signed64,                /* flags */
+											 char *,                    /* hostname */
+											 char *,                    /* poolname */
+											 char *,                    /* filename */
+											 int *,                     /* nstcp_output */
+											 struct stgcat_entry **,    /* stcp_output */
+											 int *,                     /* nstpp_output */
+											 struct stgpath_entry **)); /* stpp_output */
+EXTERN_C int DLL_DECL stage_qry_Hsm _PROTO((u_signed64,                /* flags */
+											char *,                    /* hostname */
+											char *,                    /* poolname */
+											char *,                    /* hsmname */
+											int *,                     /* nstcp_output */
+											struct stgcat_entry **,    /* stcp_output */
+											int *,                     /* nstpp_output */
+											struct stgpath_entry **)); /* stpp_output */
 
 /* ---------------------------- */
 /* Generic STAGE_CLR interface  */
@@ -207,37 +207,37 @@ EXTERN_C int DLL_DECL stage_clr _PROTO((char,                      /* t_or_d */
                                         int,                       /* nstpp_input */
                                         struct stgpath_entry *));  /* stpp_input */
 #endif /* hpux */
-#define stageclr_tape(flags,hostname,nstcp_input,stcp_input) \
+#define stage_clr_tape(flags,hostname,nstcp_input,stcp_input) \
   stage_clr('t',flags,hostname,nstcp_input,stcp_input,0,(struct stgpath_entry *) NULL)
-#define stageclr_disk(flags,hostname,nstcp_input,stcp_input) \
+#define stage_clr_disk(flags,hostname,nstcp_input,stcp_input) \
   stage_clr('d',flags,hostname,nstcp_input,stcp_input,0,(struct stgpath_entry *) NULL)
-#define stageclr_hsm(flags,hostname,nstcp_input,stcp_input) \
+#define stage_clr_hsm(flags,hostname,nstcp_input,stcp_input) \
   stage_clr('m',flags,hostname,nstcp_input,stcp_input,0,(struct stgpath_entry *) NULL)
-#define stageclr_path(flags,hostname,nstpp_input,stpp_input) \
+#define stage_clr_path(flags,hostname,nstpp_input,stpp_input) \
   stage_clr('p',flags,hostname,0,(struct stgcat_entry *) NULL,nstpp_input,stpp_input)
-#define stageclr_link(flags,hostname,nstpp_input,stpp_input) \
+#define stage_clr_link(flags,hostname,nstpp_input,stpp_input) \
   stage_clr('l',flags,hostname,0,(struct stgcat_entry *) NULL,nstpp_input,stpp_input)
 
 /* ----------------------------- */
 /* Shorthand STAGE_CLR interface */
 /* ----------------------------- */
 
-EXTERN_C int DLL_DECL stageclr_Tape _PROTO((u_signed64,                /* flags */
-                                            char *,                    /* hostname */
-                                            char *,                    /* tape */
-                                            char *));                  /* fseq */
-EXTERN_C int DLL_DECL stageclr_Disk _PROTO((u_signed64,                /* flags */
-                                            char *,                    /* hostname */
-                                            char *));                  /* filename */
-EXTERN_C int DLL_DECL stageclr_Hsm _PROTO((u_signed64,                 /* flags */
-                                           char *,                     /* hostname */
-                                           char *));                   /* hsmname */
-EXTERN_C int DLL_DECL stageclr_Path _PROTO((u_signed64,                /* flags */
-                                           char *,                     /* hostname */
-                                           char *));                   /* pathname */
-EXTERN_C int DLL_DECL stageclr_Link _PROTO((u_signed64,                /* flags */
-                                           char *,                     /* hostname */
-                                           char *));                   /* pathname */
+EXTERN_C int DLL_DECL stage_clr_Tape _PROTO((u_signed64,                /* flags */
+											 char *,                    /* hostname */
+											 char *,                    /* tape */
+											 char *));                  /* fseq */
+EXTERN_C int DLL_DECL stage_clr_Disk _PROTO((u_signed64,                /* flags */
+											 char *,                    /* hostname */
+											 char *));                  /* filename */
+EXTERN_C int DLL_DECL stage_clr_Hsm _PROTO((u_signed64,                 /* flags */
+											char *,                     /* hostname */
+											char *));                   /* hsmname */
+EXTERN_C int DLL_DECL stage_clr_Path _PROTO((u_signed64,                /* flags */
+											 char *,                     /* hostname */
+											 char *));                   /* pathname */
+EXTERN_C int DLL_DECL stage_clr_Link _PROTO((u_signed64,                /* flags */
+											 char *,                     /* hostname */
+											 char *));                   /* pathname */
 
 /* ---------------------------- */
 /* Generic STAGE_UPDC interface */
@@ -245,16 +245,16 @@ EXTERN_C int DLL_DECL stageclr_Link _PROTO((u_signed64,                /* flags 
 #ifdef hpux
 /* Why does hpux's cc complain on the following prototype when */
 /* compiled with -Ae ? */
-EXTERN_C int DLL_DECL stageupdc _PROTO(());
+EXTERN_C int DLL_DECL stage_updc _PROTO(());
 #else /* hpux */
-EXTERN_C int DLL_DECL stageupdc _PROTO((u_signed64,               /* flags */
-                                        char *,                    /* hostname */
-                                        char *,                    /* pooluser */
-                                        int,                       /* status */
-                                        int *,                     /* nstcp_output */
-                                        struct stgcat_entry **,    /* stcp_output */
-                                        int,                       /* nstpp_input */
-                                        struct stgpath_entry *));  /* stpp_input */
+EXTERN_C int DLL_DECL stage_updc _PROTO((u_signed64,               /* flags */
+										 char *,                    /* hostname */
+										 char *,                    /* pooluser */
+										 int,                       /* status */
+										 int *,                     /* nstcp_output */
+										 struct stgcat_entry **,    /* stcp_output */
+										 int,                       /* nstpp_input */
+										 struct stgpath_entry *));  /* stpp_input */
 #endif /* hpux */
 
 /* --------------------------- */
