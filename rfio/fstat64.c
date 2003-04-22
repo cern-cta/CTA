a3b8bbc835cfe152761e9a673ede48224363d896
Jean-Philippe Baud <baud@cern.ch>
2003-04-22 11:09:48 +0000

    handle correctly the case of read ahead followed by fstat64

--- rfio/fstat64.c
+++ rfio/fstat64.c
@@ -1,5 +1,5 @@
 /*
- * $Id: fstat64.c,v 1.3 2003/01/22 10:18:18 baud Exp $
+ * $Id: fstat64.c,v 1.4 2003/04/22 11:09:48 baud Exp $
  */
 
 /*
@@ -8,7 +8,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: fstat64.c,v $ $Revision: 1.3 $ $Date: 2003/01/22 10:18:18 $ CERN/IT/PDP/DM F. Hemmer, A. Trannoy, P. Gaillardon";
+static char sccsid[] = "@(#)$RCSfile: fstat64.c,v $ $Revision: 1.4 $ $Date: 2003/04/22 11:09:48 $ CERN/IT/PDP/DM F. Hemmer, A. Trannoy, P. Gaillardon";
 #endif /* not lint */
 
 /* fstat64.c      Remote File I/O - get file status                       */
@@ -35,10 +35,6 @@ struct stat64  *statbuf;
    int temp=0 ;
    char     rfio_buf[BUFSIZ];
    int s_index = -1;
-   WORD    req ; 
-   LONG  rcode ;
-   LONG msgsiz ;
-   int       n ;
 
    INIT_TRACE("RFIO_TRACE");
    TRACE(1, "rfio", "rfio_fstat64(%d, %x)", s, statbuf);
@@ -78,87 +74,94 @@ struct stat64  *statbuf;
    /*
     * Getting data from the network.
     */
+   for(;;) {
+      WORD    req ; 
+      LONG  rcode ;
+      LONG msgsiz ;
+      int       n ;
 
-   TRACE(2, "rfio", "rfio_fstat64: reading %d bytes",rfilefdt[s_index]->_iobuf.hsize) ;
-   n = netread_timeout(s,rfio_buf,rfilefdt[s_index]->_iobuf.hsize,RFIO_DATA_TIMEOUT); 
-   if (n != rfilefdt[s_index]->_iobuf.hsize) {
-      TRACE(2, "rfio", "rfio_fstat64: read(): ERROR occured (errno=%d)", errno);
-      if ( temp ) (void) free(trp) ;
-      END_TRACE() ;
-      return -1 ; 
-   }
-   p = rfio_buf ;
-   unmarshall_WORD(p,req) ;
-   unmarshall_LONG(p,status) ; 
-   unmarshall_LONG(p, rcode) ; 
-   unmarshall_LONG(p,msgsiz) ;
-   switch(req) {
-      case RQST_FSTAT64:
-         TRACE(2, "rfio", "rfio_fstat64: reading %d bytes",msgsiz);
-         if (netread_timeout(s,rfio_buf,msgsiz,RFIO_DATA_TIMEOUT) != msgsiz ) {
-            TRACE(2,"rfio","rfio_fstat64: read(): ERROR occured (errno=%d)",errno) ;
+      TRACE(2, "rfio", "rfio_fstat64: reading %d bytes",rfilefdt[s_index]->_iobuf.hsize) ;
+      n = netread_timeout(s,rfio_buf,rfilefdt[s_index]->_iobuf.hsize,RFIO_DATA_TIMEOUT); 
+      if (n != rfilefdt[s_index]->_iobuf.hsize) {
+         TRACE(2, "rfio", "rfio_fstat64: read(): ERROR occured (errno=%d)", errno);
+         if ( temp ) (void) free(trp) ;
+         END_TRACE() ;
+         return -1 ; 
+      }
+      p = rfio_buf ;
+      unmarshall_WORD(p,req) ;
+      unmarshall_LONG(p,status) ; 
+      unmarshall_LONG(p, rcode) ; 
+      unmarshall_LONG(p,msgsiz) ;
+      switch(req) {
+         case RQST_FSTAT64:
+            TRACE(2, "rfio", "rfio_fstat64: reading %d bytes",msgsiz);
+            if (netread_timeout(s,rfio_buf,msgsiz,RFIO_DATA_TIMEOUT) != msgsiz ) {
+               TRACE(2,"rfio","rfio_fstat64: read(): ERROR occured (errno=%d)",errno) ;
+               if ( temp ) (void) free(trp) ;
+               END_TRACE() ;
+               return -1 ; 
+            }
+            p = rfio_buf ;
+            unmarshall_WORD(p,statbuf->st_dev);
+            unmarshall_HYPER(p,statbuf->st_ino);
+            unmarshall_WORD(p,statbuf->st_mode);
+            unmarshall_WORD(p,statbuf->st_nlink);
+            unmarshall_WORD(p,statbuf->st_uid);
+            unmarshall_WORD(p,statbuf->st_gid);
+            unmarshall_HYPER(p,statbuf->st_size);
+            unmarshall_LONG(p,statbuf->st_atime);
+            unmarshall_LONG(p,statbuf->st_mtime);
+            unmarshall_LONG(p,statbuf->st_ctime);
+#if !defined(_WIN32)
+            if ( msgsiz > (5*WORDSIZE+3*LONGSIZE+2*HYPERSIZE) ) {
+               unmarshall_LONG(p, statbuf->st_blksize);
+               unmarshall_HYPER(p, statbuf->st_blocks);
+            } else {
+               statbuf->st_blksize = 0;
+               statbuf->st_blocks  = 0;
+            }
+#endif
+            rfio_errno= rcode ;
+            if ( temp ) (void) free(trp) ;
+            TRACE(1,"rfio","rfio_fstat64: return status %d, rcode %d", status, rcode) ;
+            END_TRACE() ;
+            return status ;
+         case RQST_READAHEAD:
+         case RQST_READAHD64:
+         case RQST_LASTSEEK:
+         case RQST_PRESEEK:
+         case RQST_PRESEEK64:
+            /* 
+             * At this point a temporary buffer may need to be created
+             * to receive data which is going to be thrown away.
+             */
+            if ( temp == 0 ) {
+               if ( rfilefdt[s_index]->_iobuf.base==NULL || rfilefdt[s_index]->_iobuf.dsize<msgsiz ) {
+                  temp= 1 ; 
+                  TRACE(3,"rfio","rfio_fstat: allocating momentary buffer of size %d",msgsiz) ; 
+                  if ( (trp= ( char *) malloc(msgsiz)) == NULL ) {
+                     TRACE(3,"rfio","rfio_fstat: malloc(): ERROR occured (errno=%d)",errno) ; 
+                     END_TRACE() ; 
+                     return -1 ;
+                  }
+               }
+               else
+                  trp= iodata(rfilefdt[s_index]) ;
+            }
+            if ( netread_timeout(s,trp,msgsiz,RFIO_DATA_TIMEOUT) != msgsiz ) {
+               TRACE(2,"rfio","rfio_fstat: read(): ERROR occured (errno=%d)",errno) ;
+               if ( temp ) (void) free(trp) ; 
+               END_TRACE() ; 
+               return -1 ;
+            }
+            break ; 
+         default:
+            TRACE(1,"rfio","rfio_fstat64(): Bad control word received\n") ; 
+            serrno= SEINTERNAL ;
             if ( temp ) (void) free(trp) ;
             END_TRACE() ;
-            return -1 ; 
-         }
-         p = rfio_buf ;
-         unmarshall_WORD(p,statbuf->st_dev);
-         unmarshall_HYPER(p,statbuf->st_ino);
-         unmarshall_WORD(p,statbuf->st_mode);
-         unmarshall_WORD(p,statbuf->st_nlink);
-         unmarshall_WORD(p,statbuf->st_uid);
-         unmarshall_WORD(p,statbuf->st_gid);
-         unmarshall_HYPER(p,statbuf->st_size);
-         unmarshall_LONG(p,statbuf->st_atime);
-         unmarshall_LONG(p,statbuf->st_mtime);
-         unmarshall_LONG(p,statbuf->st_ctime);
-#if !defined(_WIN32)
-         if ( msgsiz > (5*WORDSIZE+3*LONGSIZE+2*HYPERSIZE) ) {
-            unmarshall_LONG(p, statbuf->st_blksize);
-            unmarshall_HYPER(p, statbuf->st_blocks);
-         } else {
-            statbuf->st_blksize = 0;
-            statbuf->st_blocks  = 0;
-         }
-#endif
-         rfio_errno= rcode ;
-         if ( temp ) (void) free(trp) ;
-         TRACE(1,"rfio","rfio_fstat64: return status %d, rcode %d", status, rcode) ;
-         END_TRACE() ;
-         return status ;
-      case RQST_READAHD64:
-      case RQST_LASTSEEK:
-      case RQST_PRESEEK64:
-	  /* 
-	   * At this point a temporary buffer may need to be created
-	   * to receive data which is going to be thrown away.
-	   */
-	  if ( temp == 0 ) {
-	     if ( rfilefdt[s_index]->_iobuf.base==NULL || rfilefdt[s_index]->_iobuf.dsize<msgsiz ) {
-		temp= 1 ; 
-		TRACE(3,"rfio","rfio_fstat: allocating momentary buffer of size %d",msgsiz) ; 
-		if ( (trp= ( char *) malloc(msgsiz)) == NULL ) {
-		   TRACE(3,"rfio","rfio_fstat: malloc(): ERROR occured (errno=%d)",errno) ; 
-		   END_TRACE() ; 
-		   return -1 ;
-		}
-	     }
-	     else
-		trp= iodata(rfilefdt[s_index]) ;
-	  }
-	  if ( netread_timeout(s,trp,msgsiz,RFIO_DATA_TIMEOUT) != msgsiz ) {
-	     TRACE(2,"rfio","rfio_fstat: read(): ERROR occured (errno=%d)",errno) ;
-	     if ( temp ) (void) free(trp) ; 
-	     END_TRACE() ; 
-	     return -1 ;
-	  }
-	  break ; 
-      default:
-         TRACE(1,"rfio","rfio_fstat64(): Bad control word received\n") ; 
-         serrno= SEINTERNAL ;
-         if ( temp ) (void) free(trp) ;
-         END_TRACE() ;
-         return -1 ;
+            return -1 ;
+      }
    }
-   
 }
