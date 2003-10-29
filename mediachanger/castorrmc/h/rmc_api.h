4cfe3af13b0f2ecfec304c687689106f40990b52
Jean-Philippe Baud <baud@cern.ch>
2003-10-29 12:37:27 +0000

    add prototypes for functions send2xxx, xxx_apiinit and xxx_errmsg

--- mediachanger/castorrmc/h/rmc_api.h
+++ mediachanger/castorrmc/h/rmc_api.h
@@ -1,14 +1,14 @@
 /*
- * $Id: rmc_api.h,v 1.1 2002/12/01 07:31:56 baud Exp $
+ * $Id: rmc_api.h,v 1.2 2003/10/29 12:37:27 baud Exp $
  */
 
 /*
- * Copyright (C) 2002 by CERN/IT/DS/HSM
+ * Copyright (C) 2002-2003 by CERN/IT/DS/HSM
  * All rights reserved
  */
 
 /*
- * @(#)$RCSfile: rmc_api.h,v $ $Revision: 1.1 $ $Date: 2002/12/01 07:31:56 $ CERN IT-DS/HSM Jean-Philippe Baud
+ * @(#)$RCSfile: rmc_api.h,v $ $Revision: 1.2 $ $Date: 2003/10/29 12:37:27 $ CERN IT-DS/HSM Jean-Philippe Baud
  */
 
 #ifndef _RMC_API_H
@@ -18,6 +18,7 @@
                         /*  function prototypes */
 
 EXTERN_C int DLL_DECL rmc_dismount _PROTO((char *, char *, char *, int, int));
+EXTERN_C int DLL_DECL rmc_errmsg _PROTO((char *, char *, ...));
 EXTERN_C int DLL_DECL rmc_export _PROTO((char *, char *, char *));
 EXTERN_C int DLL_DECL rmc_find_cartridge _PROTO((char *, char *, char *, int, int, int, struct smc_element_info *));
 EXTERN_C int DLL_DECL rmc_get_geometry _PROTO((char *, char *, struct robot_info *));
@@ -25,4 +26,5 @@ EXTERN_C int DLL_DECL rmc_import _PROTO((char *, char *, char *));
 EXTERN_C int DLL_DECL rmc_mount _PROTO((char *, char *, char *, int, int));
 EXTERN_C int DLL_DECL rmc_read_elem_status _PROTO((char *, char *, int, int, int, struct smc_element_info *));
 EXTERN_C int DLL_DECL rmc_seterrbuf _PROTO((char *, int));
+EXTERN_C int DLL_DECL send2rmc _PROTO((char *, char *, int, char *, int));
 #endif
