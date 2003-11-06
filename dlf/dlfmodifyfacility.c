3816f8af141109f12c82af76dcb069d67fde15d8
Vitali Motyakov <motiakov@cern.ch>
2003-11-06 07:28:09 +0000

    First try to port DLF to windows

--- dlf/dlfmodifyfacility.c
+++ dlf/dlfmodifyfacility.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: dlfmodifyfacility.c,v $ $Revision: 1.1 $ $Date: 2003/08/20 13:06:36 $ CERN IT-ADC/CA Vitaly Motyakov";
+static char sccsid[] = "@(#)$RCSfile: dlfmodifyfacility.c,v $ $Revision: 1.2 $ $Date: 2003/11/06 07:28:09 $ CERN IT-ADC/CA Vitaly Motyakov";
 #endif /* not lint */
 
 #include <errno.h>
@@ -38,6 +38,9 @@ char **argv;
 	char fac_name[DLF_MAXFACNAMELEN + 1];
 	int fac_name_set = 0;
 	int fac_no_set = 0;
+#if defined (_WIN32)
+	WSADATA wsadata;
+#endif
 
 	errflg = 0;
         while ((c = getopt (argc, argv, "F:n:?")) != EOF) {
