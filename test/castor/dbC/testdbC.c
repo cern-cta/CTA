e27b687586f708e5bf6a46c662ea713a9c997337
Sebastien Ponce <sponcec3@cern.ch>
2004-05-13 12:58:29 +0000

    Added tests for the CPP part and the C to CPP interface

---
+++ test/castor/dbC/testdbC.c
@@ -0,0 +1,154 @@
+/******************************************************************************
+ *                      testdb.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile: testdbC.c,v $ $Revision: 1.1 $ $Release$ $Date: 2004/05/13 12:58:29 $ $Author: sponcec3 $
+ *
+ * 
+ *
+ * @author Sebastien Ponce
+ *****************************************************************************/
+
+#include "castor/rh/Request.h"
+#include "castor/rh/FileRequest.h"
+#include "castor/rh/StageInRequest.h"
+#include "castor/rh/File.h"
+#include "castor/rh/Client.h"
+#include "castor/rh/Copy.h"
+#include "castor/Services.h"
+#include "castor/BaseAddress.h"
+#include "castor/db/DbAddress.h"
+#include "castor/IObject.h"
+#include "castor/IClient.h"
+#include "castor/Constants.h"
+#include <stdio.h>
+
+int main (int argc, char** argv) {
+  // Prepare a request
+  struct Crh_StageInRequest_t* fr;
+  struct Crh_FileRequest_t* frfr;
+  struct Crh_Request_t* frr;
+  struct C_IObject_t* frio;
+  struct Crh_Client_t *cl;
+  struct C_IClient_t *clic;
+  struct Crh_File_t* f1;
+  struct Crh_Copy_t* c1;
+  struct Crh_File_t* f2;
+  struct Crh_Copy_t* c2;
+  struct Crh_Copy_t* c3;
+  struct Crh_Copy_t* c4;
+  struct C_Services_t* svcs;
+  struct C_BaseAddress_t *ad;
+  struct C_IAddress_t *adia;
+  struct Cdb_DbAddress_t *ad2;
+  struct C_BaseAddress_t *ad2ba;
+  struct C_IAddress_t *ad2ia;
+  struct C_IObject_t* fr2Obj;
+  struct Crh_Request_t* fr2r;
+  struct Crh_FileRequest_t* fr2;
+  unsigned long id;
+
+  Crh_StageInRequest_create(&fr);
+  frfr = Crh_StageInRequest_getFileRequest(fr);
+  frr  = Crh_FileRequest_getRequest(frfr);
+  frio = Crh_Request_getIObject(frr);
+  Crh_FileRequest_setDescription(frfr, "This is a test FileRequest");
+  Crh_FileRequest_setSuspended(frfr, 0);
+    
+  Crh_Client_create(&cl);
+  clic = Crh_Client_getIClient(cl);
+  Crh_Client_setIpAddress(cl, 0606);
+  Crh_Client_setPort(cl, 0707);
+  C_IClient_setRequest(clic, frr);
+  Crh_Request_setClient(frr, clic);
+
+  Crh_File_create(&f1);
+  Crh_File_setName(f1, "First test File");
+  Crh_FileRequest_addFiles(frfr, f1);
+  Crh_File_setRequest(f1, frfr);
+
+  Crh_Copy_create(&c1);
+  Crh_Copy_setNum(c1, 45);
+  Crh_Copy_setParentFile(c1, f1);
+  Crh_File_addCopy(f1, c1);
+
+  Crh_File_create(&f2);
+  Crh_File_setName(f2, "2nd test File");
+  Crh_FileRequest_addFiles(frfr, f2);
+  Crh_File_setRequest(f2, frfr);
+
+  Crh_Copy_create(&c2);
+  Crh_Copy_setNum(c2, 46);
+  Crh_Copy_setParentFile(c2, f2);
+  Crh_File_addCopy(f2, c2);
+
+  Crh_Copy_create(&c3);
+  Crh_Copy_setNum(c3, 47);
+  Crh_Copy_setParentFile(c3, f2);
+  Crh_File_addCopy(f2, c3);
+
+  Crh_Copy_create(&c4);
+  Crh_Copy_setNum(c4, 48);
+  Crh_Copy_setParentFile(c4, f2);
+  Crh_File_addCopy(f2, c4);
+
+  // Get a Services instance
+  C_Services_create(&svcs);
+
+  // Stores the request
+  C_BaseAddress_create("OraCnvSvc", SVC_ORACNV, &ad);
+  adia = C_BaseAddress_getIAddress(ad);
+  if (C_Services_createRep(svcs, adia, frio, 1) != 0) {
+    printf("Error caught in createRep.\n");
+    // release the memory
+    C_BaseAddress_delete(ad);
+    C_Services_delete(svcs);
+    Crh_StageInRequest_delete(fr);
+    return 1;
+  }
+  C_BaseAddress_delete(ad);
+
+  // Retrieves it in a separate object
+  Crh_StageInRequest_id(fr, &id);
+  Cdb_DbAddress_create(id, "OraCnvSvc", SVC_ORACNV, &ad2);
+  ad2ba = Cdb_DbAddress_getBaseAddress(ad2);
+  ad2ia = C_BaseAddress_getIAddress(ad2ba);
+  if (C_Services_createObj(svcs,
+                           ad2ia,
+                           &fr2Obj) != 0) {
+    printf("Error caught in createObj\n.");
+    Cdb_DbAddress_delete(ad2);
+    C_Services_delete(svcs);
+    C_IObject_delete(fr2Obj);
+    return 1;
+  }
+  Cdb_DbAddress_delete(ad2);
+  fr2r = Crh_Request_fromIObject(fr2Obj);
+  fr2 = Crh_FileRequest_fromRequest(fr2r);
+  
+  // Display both objects for comparison
+  printf("Originally :\n");
+  Crh_StageInRequest_print(fr);
+  printf("Finally :\n");
+  Crh_FileRequest_print(fr2);
+
+  C_Services_delete(svcs);
+  Crh_StageInRequest_delete(fr);
+  Crh_FileRequest_delete(fr2);
+  return 0;
+}
---
+++ test/castor/io/testio.cpp
@@ -0,0 +1,130 @@
+/******************************************************************************
+ *                      testio.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile: testio.cpp,v $ $Revision: 1.1 $ $Release$ $Date: 2004/05/13 12:58:29 $ $Author: sponcec3 $
+ *
+ * 
+ *
+ * @author Sebastien Ponce
+ *****************************************************************************/
+
+#include "castor/rh/StageInRequest.hpp"
+#include "castor/rh/File.hpp"
+#include "castor/rh/Client.hpp"
+#include "castor/rh/Copy.hpp"
+#include "castor/Services.hpp"
+#include "castor/Constants.hpp"
+#include "castor/ObjectSet.hpp"
+#include "castor/BaseAddress.hpp"
+#include "castor/io/StreamAddress.hpp"
+#include "castor/IObject.hpp"
+#include "castor/IClient.hpp"
+#include <castor/io/biniostream.h>
+#include <iostream>
+
+int main (int argc, char** argv) {
+  // Prepare a request
+  castor::rh::StageInRequest* fr = new castor::rh::StageInRequest();
+  fr->setDescription("This is a test FileRequest");
+  fr->setSuspended(true);
+  
+  castor::rh::Client *cl = new castor::rh::Client();
+  cl->setIpAddress(0606);
+  cl->setPort(0707);
+  cl->setRequest(fr);
+  fr->setClient(cl);
+
+  castor::rh::File* f1 = new castor::rh::File();
+  f1->setName("First test File");
+  fr->addFiles(f1);
+  f1->setRequest(fr);
+
+  castor::rh::Copy* c1 = new castor::rh::Copy();
+  c1->setNum(45);
+  c1->setParentFile(f1);
+  f1->addCopy(c1);
+
+  castor::rh::File* f2 = new castor::rh::File();
+  f2->setName("2nd test File");
+  fr->addFiles(f2);
+  f2->setRequest(fr);
+
+  castor::rh::Copy* c2 = new castor::rh::Copy();
+  c2->setNum(46);
+  c2->setParentFile(f2);
+  f2->addCopy(c2);
+
+  castor::rh::Copy* c3 = new castor::rh::Copy();
+  c3->setNum(47);
+  c3->setParentFile(f2);
+  f2->addCopy(c3);
+
+  castor::rh::Copy* c4 = new castor::rh::Copy();
+  c4->setNum(48);
+  c4->setParentFile(f2);
+  f2->addCopy(c4);
+
+  // Get a Services instance
+  castor::Services* svcs = new castor::Services();
+
+  // Stores the request
+  castor::io::biniostream buffer;
+  castor::io::StreamAddress ad(buffer, "StreamCnvSvc", castor::SVC_STREAMCNV);
+  try {
+    svcs->createRep(&ad, fr, true);
+  } catch (castor::Exception e) {
+    std::cout << "Error caught in createRep :" << std::endl
+              << e.getMessage().str() << std::endl;
+    // release the memory
+    delete svcs;
+    delete fr;
+    return 1;
+  }
+
+  // Displays the content of the string
+  std::cout << "\"" << buffer.str() << "\"" << std::endl;
+
+  // Retrieves it in a separate object
+  castor::io::StreamAddress ad2 (buffer, "StreamCnvSvc", castor::SVC_STREAMCNV);
+  castor::rh::FileRequest* fr2;
+  try{
+    castor::IObject* fr2Obj = svcs->createObj(&ad2);
+    fr2 = dynamic_cast<castor::rh::FileRequest*>(fr2Obj);
+  } catch (castor::Exception e) {
+    std::cout << "Error caught in createObj :" << std::endl
+              << e.getMessage().str() << std::endl;
+    delete svcs;
+    delete fr;
+    return 1;
+  }
+  
+  // Display both objects for comparison
+  std::cout << "Originally :" << std::endl;
+  castor::ObjectSet alreadyPrinted;
+  fr->print(std::cout, "", alreadyPrinted);
+  castor::ObjectSet alreadyPrinted2;
+  std::cout << "Finally :" << std::endl;
+  fr2->print(std::cout, "", alreadyPrinted2);
+
+  delete svcs;
+  delete fr;
+  delete fr2;
+  return 0;
+}
+
