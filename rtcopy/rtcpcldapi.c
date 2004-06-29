0e3e6a81df02590c410ba9588a834e6ee8929678
Jean-Damien Durand <jdurand@cern.ch>
2004-06-29 20:33:56 +0000

    SIGSEGV fix;
    <                      fl->filereq.blockid,
    ---
    >                      (int)fl->filereq.blockid[0],
    >                      (int)fl->filereq.blockid[1],
    >                      (int)fl->filereq.blockid[2],
    >                      (int)fl->filereq.blockid[3],

--- rtcopy/rtcpcldapi.c
+++ rtcopy/rtcpcldapi.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: rtcpcldapi.c,v $ $Revision: 1.17 $ $Release$ $Date: 2004/06/29 17:34:49 $ $Author: obarring $
+ * @(#)$RCSfile: rtcpcldapi.c,v $ $Revision: 1.18 $ $Release$ $Date: 2004/06/29 20:33:56 $ $Author: jdurand $
  *
  * 
  *
@@ -25,7 +25,7 @@
  *****************************************************************************/
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: rtcpcldapi.c,v $ $Revision: 1.17 $ $Date: 2004/06/29 17:34:49 $ CERN-IT/ADC Olof Barring";
+static char sccsid[] = "@(#)$RCSfile: rtcpcldapi.c,v $ $Revision: 1.18 $ $Date: 2004/06/29 20:33:56 $ CERN-IT/ADC Olof Barring";
 #endif /* not lint */
 
 #include <errno.h>
@@ -811,7 +811,10 @@ static int doFullUpdate(
                      tl->tapereq.side,
                      (tl->tapereq.mode == WRITE_DISABLE ? "r" : "w"),
                      fl->filereq.tape_fseq,
-                     fl->filereq.blockid,
+                     (int)fl->filereq.blockid[0],
+                     (int)fl->filereq.blockid[1],
+                     (int)fl->filereq.blockid[2],
+                     (int)fl->filereq.blockid[3],
                      fl->filereq.file_path
                      );
             rc = addSegment(
