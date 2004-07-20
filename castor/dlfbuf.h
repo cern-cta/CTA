9a3e8f814cae67ea440ea9d1d49ed803f7596186
Sebastien Ponce <sponcec3@cern.ch>
2004-07-20 15:40:29 +0000

    Oups, typo in the fix...

--- castor/dlfbuf.h
+++ castor/dlfbuf.h
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: dlfbuf.h,v $ $Revision: 1.2 $ $Release$ $Date: 2004/07/20 15:35:22 $ $Author: sponcec3 $
+ * @(#)$RCSfile: dlfbuf.h,v $ $Revision: 1.3 $ $Release$ $Date: 2004/07/20 15:40:29 $ $Author: sponcec3 $
  *
  * A string buffer for logging into dlf
  *
@@ -64,7 +64,7 @@ namespace castor {
                 << "The log destination used in dlf_init ("
                 << name
                 << ") cannot be found in the configuration file.\n"
-                << "Please check your configuration."
+                << "Please check your configuration.";
             } else {
               e.getMessage() << "Unable to initialize DLF :"
                              << std::endl
