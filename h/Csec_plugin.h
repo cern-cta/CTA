34c98d0c6331267ade2790b562f8777e1b808177
Vitali Motyakov <motiakov@cern.ch>
2004-08-27 15:13:25 +0000

    Done modifications associated with function names changes

--- h/Csec_plugin.h
+++ h/Csec_plugin.h
@@ -3,31 +3,39 @@
 
 #include <Csec_common.h>
 
-int DLL_DECL  Csec_init_context_impl _PROTO ((Csec_context_t *ctx));
-int DLL_DECL  Csec_reinit_context_impl _PROTO ((Csec_context_t *ctx));
-int DLL_DECL  Csec_delete_connection_context_impl _PROTO ((Csec_context_t *));
-int DLL_DECL  Csec_delete_creds_impl _PROTO ((Csec_context_t *));
-int DLL_DECL  Csec_acquire_creds_impl _PROTO ((Csec_context_t *ctx,
-					       char *service_name,
-					       int is_client));
-int DLL_DECL  Csec_server_establish_context_impl _PROTO ((Csec_context_t *ctx,
-							  int s));
-int DLL_DECL  Csec_server_establish_context_ext_impl _PROTO ((Csec_context_t *ctx,
-							      int s,
-							      char *buf,
-							      int len));
-int DLL_DECL  Csec_client_establish_context_impl _PROTO ((Csec_context_t *ctx,
-							  int s));
-int DLL_DECL Csec_map2name_impl _PROTO((Csec_context_t *ctx, 
-					char *principal, 
-					char *name, 
-					int maxnamelen));
+#define CSEC_METHOD_NAME_X(METH, MECH) METH ## MECH
+#define CSEC_METHOD_NAME(METH, MECH) CSEC_METHOD_NAME_X(METH, MECH)
 
-
-int DLL_DECL Csec_get_service_name_impl _PROTO ((Csec_context_t *ctx, 
-						 int service_type, 
-						 char *host, 
-						 char *domain,
-						 char *service_name, 
+#define CSEC_DECLARE_FUNCTIONS(MECH) \
+int DLL_DECL  Csec_init_context_ ## MECH _PROTO ((Csec_context_t *ctx)); \
+int DLL_DECL  Csec_reinit_context_ ## MECH _PROTO ((Csec_context_t *ctx)); \
+int DLL_DECL  Csec_delete_connection_context_ ## MECH _PROTO ((Csec_context_t *)); \
+int DLL_DECL  Csec_delete_creds_ ## MECH _PROTO ((Csec_context_t *)); \
+int DLL_DECL  Csec_acquire_creds_ ## MECH _PROTO ((Csec_context_t *ctx, \
+					       char *service_name, \
+					       int is_client)); \
+int DLL_DECL  Csec_server_establish_context_ ## MECH _PROTO ((Csec_context_t *ctx, \
+							  int s)); \
+int DLL_DECL  Csec_server_establish_context_ext_ ## MECH _PROTO ((Csec_context_t *ctx, \
+							      int s, \
+							      char *buf, \
+							      int len)); \
+int DLL_DECL  Csec_client_establish_context_ ## MECH _PROTO ((Csec_context_t *ctx, \
+							  int s)); \
+int DLL_DECL Csec_map2name_ ## MECH _PROTO((Csec_context_t *ctx, \
+					char *principal, \
+					char *name, \
+					int maxnamelen)); \
+int DLL_DECL Csec_get_service_name_ ## MECH _PROTO ((Csec_context_t *ctx, \
+						 int service_type, \
+						 char *host, \
+						 char *domain, \
+						 char *service_name, \
 						 int service_namelen));
+
+CSEC_DECLARE_FUNCTIONS(KRB4)
+CSEC_DECLARE_FUNCTIONS(KRB5)
+CSEC_DECLARE_FUNCTIONS(GSI)
+CSEC_DECLARE_FUNCTIONS(ID)
+
 #endif /* Csec_plugin.h */
