c4fb4b22bcb3f98fd4b03f32f8c72187d4d1aa51
Olof Barring <obarring@cern.ch>
2004-10-12 08:37:34 +0000

    Add license header

--- rtcopy/rtcpcldcommon.c
+++ rtcopy/rtcpcldcommon.c
@@ -1,17 +1,31 @@
-/*
+/******************************************************************************
+ *                      rtcpcldcommon.c
  *
- * Copyright (C) 2004 by CERN/IT/ADC/CA
- * All rights reserved
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
  *
- * @(#)$RCSfile: rtcpcldcommon.c,v $ $Revision: 1.13 $ $Release$ $Date: 2004/10/12 06:46:49 $ $Author: obarring $
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
+ * @(#)$RCSfile: rtcpcldcommon.c,v $ $Revision: 1.14 $ $Release$ $Date: 2004/10/12 08:37:34 $ $Author: obarring $
  *
+ * 
  *
  * @author Olof Barring
- */
+ *****************************************************************************/
 
 #ifndef lint
-static char sccsid[] = "@(#)$RCSfile: rtcpcldcommon.c,v $ $Revision: 1.13 $ $Release$ $Date: 2004/10/12 06:46:49 $ Olof Barring";
+static char sccsid[] = "@(#)$RCSfile: rtcpcldcommon.c,v $ $Revision: 1.14 $ $Release$ $Date: 2004/10/12 08:37:34 $ Olof Barring";
 #endif /* not lint */
 
 #include <stdlib.h>
