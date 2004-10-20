3780b2100a711c856e4ceba58f4e336de650f4b1
Jean-Damien Durand <jdurand@cern.ch>
2004-10-20 16:23:29 +0000

    Moved current config api definitions in stager_admin_api.h

---
+++ h/stager_admin_api.h
@@ -0,0 +1,20 @@
+/*
+ * $Id: stager_admin_api.h,v 1.1 2004/10/20 16:23:29 jdurand Exp $
+ */
+
+#ifndef stager_admin_api_h
+#define stager_admin_api_h
+
+#include <sys/types.h>
+#include "osdep.h"
+
+EXTERN_C int DLL_DECL stager_configTimeout           _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configPort              _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configDebug             _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configTrace             _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configDbNthread         _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configUserNthread       _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configFacility          _PROTO((size_t, char *));
+EXTERN_C int DLL_DECL stager_configIgnoreCommandLine _PROTO((int *));
+
+#endif /* stager_admin_api_h */
--- h/stager_api.h
+++ h/stager_api.h
@@ -1,19 +1,10 @@
 /*
- * $Id: stager_api.h,v 1.2 2004/10/20 09:25:38 jdurand Exp $
+ * $Id: stager_api.h,v 1.3 2004/10/20 16:23:29 jdurand Exp $
  */
 
 #ifndef stager_api_h
 #define stager_api_h
 
-#include <sys/types.h>
-#include "osdep.h"
-
-EXTERN_C int DLL_DECL stager_configTimeout     _PROTO((int *));
-EXTERN_C int DLL_DECL stager_configPort        _PROTO((int *));
-EXTERN_C int DLL_DECL stager_configDebug       _PROTO((int *));
-EXTERN_C int DLL_DECL stager_configTrace       _PROTO((int *));
-EXTERN_C int DLL_DECL stager_configDbNthread   _PROTO((int *));
-EXTERN_C int DLL_DECL stager_configUserNthread _PROTO((int *));
-EXTERN_C int DLL_DECL stager_configFacility     _PROTO((size_t, char *));
+#include "stager_admin_api.h"
 
 #endif /* stager_api_h */
