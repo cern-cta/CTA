50cecc1018d71b0b794ba3239a8e10a492b60ca6
Jean-Damien Durand <jdurand@cern.ch>
2004-10-22 09:43:56 +0000

    ignoreCommandLine option

--- h/stagerConstants.h
+++ h/stagerConstants.h
@@ -1,5 +1,5 @@
 /*
- * $Id: stagerConstants.h,v 1.1 2004/10/19 18:16:43 jdurand Exp $
+ * $Id: stagerConstants.h,v 1.2 2004/10/22 09:43:56 jdurand Exp $
  */
 
 #ifndef __stagerConstants_h
@@ -12,6 +12,7 @@
 #define STAGER_DEFAULT_DB_NBTHREAD   5                   /* Default number of db threads */
 #define STAGER_DEFAULT_USER_NBTHREAD 5                   /* Default number of user threads */
 #define STAGER_DEFAULT_FACILITY   "stager"               /* Default Facility name */
+#define STAGER_DEFAULT_IGNORECOMMANDLINE   0             /* Default ignore-comamnd-line mode (!= 0 means yes) */
 
 #define STAGER_CLASS              "STAGER"               /* Label in config file (1st column) */
 
@@ -22,6 +23,7 @@
 #define STAGER_CLASS_DB_NBTHREAD  "DB_NBTHREAD"          /* Sub-label in config file for db_nbthread */
 #define STAGER_CLASS_USER_NBTHREAD "USER_NBTHREAD"       /* Sub-label in config file for user_nbthread */
 #define STAGER_CLASS_FACILITY     "FACILITY"             /* Sub-label in config file for facility */
+#define STAGER_CLASS_IGNORECOMMANDLINE  "IGNORECOMMANDLINE" /* Sub-label in config file for ignore-command-line */
 
 #define STAGER_ENV_TIMEOUT        "STAGER_TIMEOUT"       /* Environment variable for timeout */
 #define STAGER_ENV_PORT           "STAGER_PORT"          /* Environment variable for port */
@@ -30,6 +32,7 @@
 #define STAGER_ENV_DB_NBTHREAD    "STAGER_DB_NBTHREAD"   /* Environment variable for db_nbthread */
 #define STAGER_ENV_USER_NBTHREAD  "STAGER_USER_NBTHREAD" /* Environment variable for user_nbthread */
 #define STAGER_ENV_FACILITY       "STAGER_FACILITY"      /* Environment variable for facility */
+#define STAGER_ENV_IGNORECOMMANDLINE "STAGER_IGNORECOMMANDLINE" /* Environment variable for ignore-command-line */
 
 #define STAGER_MUTEX_TIMEOUT      10                     /* Timeout on getting a mutex */
 #define STAGER_COND_TIMEOUT       1                      /* Timeout on waiting on a condition variable */
