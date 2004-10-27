76154d5d42195f37a73e3d88119cd69d5da4187c
Jean-Damien Durand <jdurand@cern.ch>
2004-10-27 13:23:39 +0000

    < EXTERN_C int DLL_DECL stager_configUserNthread       _PROTO((int *)); --- > EXTERN_C int DLL_DECL stager_configQueryNthread      _PROTO((int *)); > EXTERN_C int DLL_DECL stager_configUpdateNthread     _PROTO((int *)); > EXTERN_C int DLL_DECL stager_configGetnextNthread    _PROTO((int *));

--- h/stager_admin_api.h
+++ h/stager_admin_api.h
@@ -1,5 +1,5 @@
 /*
- * $Id: stager_admin_api.h,v 1.5 2004/10/25 13:15:35 jdurand Exp $
+ * $Id: stager_admin_api.h,v 1.6 2004/10/27 13:23:39 jdurand Exp $
  */
 
 #ifndef stager_admin_api_h
@@ -15,7 +15,9 @@ EXTERN_C int DLL_DECL stager_configSecure            _PROTO((int *));
 EXTERN_C int DLL_DECL stager_configDebug             _PROTO((int *));
 EXTERN_C int DLL_DECL stager_configTrace             _PROTO((int *));
 EXTERN_C int DLL_DECL stager_configDbNthread         _PROTO((int *));
-EXTERN_C int DLL_DECL stager_configUserNthread       _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configQueryNthread      _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configUpdateNthread     _PROTO((int *));
+EXTERN_C int DLL_DECL stager_configGetnextNthread    _PROTO((int *));
 EXTERN_C int DLL_DECL stager_configFacility          _PROTO((size_t, char *));
 EXTERN_C int DLL_DECL stager_configIgnoreCommandLine _PROTO((int *));
 EXTERN_C int DLL_DECL stager_sendToStager            _PROTO((u_signed64, char *));
