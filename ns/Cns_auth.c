12d3ae29a7386700704427326f2db58176301f63
Benjamin Couturier <bcouturi@cern.ch>
2004-11-03 10:33:42 +0000

    Use ids from context in Cns_access

--- ns/Cns_auth.c
+++ ns/Cns_auth.c
@@ -20,6 +20,9 @@ static char sccsid[] = "$id$";
 #include <unistd.h>
 #endif
 
+#define MODE_EFFECTIVE_ID 0
+#define MODE_REAL_ID      1
+
 /** Cns_setid
  *
  * Sets the authorization id in the per thread structure.
@@ -69,6 +72,58 @@ Cns_unsetid()
 }
 
 
+/** Cns_getid_ext
+ *
+ * Gets the authorization id from the per thread structure.
+ * Either Cns_setid has been done, in which case that id is used,
+ * or getuid/getgid are used to get the values.
+ *
+ * @param uid Pointer to where to store the uid
+ * @param gid Pointer to where to store the gid
+ * @param mode Indicates wheteher real of effective uid 
+ *             should be looked up by default
+ * @returns 0 in case of successs, -1 otherwise.
+ */
+static int DLL_DECL
+Cns_getid_ext(uid, gid, mode)
+ uid_t *uid;
+ gid_t *gid;
+ int mode;
+{
+  char func[16];
+  struct Cns_api_thread_info *thip;
+  
+  strcpy (func, "Cns_getid");
+  if (Cns_apiinit (&thip))
+    return (-1);
+  
+  if (uid != NULL) {
+    if(thip->use_authorization_id) {
+      *uid = thip->uid;
+    } else {
+      if (mode == MODE_REAL_ID) {
+	*uid = getuid();
+      } else {
+	*uid = geteuid();
+      }
+    }
+  }
+
+  if (gid != NULL) {
+    if(thip->use_authorization_id) {
+      *gid = thip->gid;
+    } else {
+      if (mode == MODE_REAL_ID) {
+	*gid = getgid();
+      } else {
+	*gid = getegid();
+      }
+    }
+  }
+  return(0);
+}
+
+
 /** Cns_getid
  *
  * Gets the authorization id from the per thread structure.
@@ -84,27 +139,23 @@ Cns_getid(uid, gid)
  uid_t *uid;
  gid_t *gid;
 {
-  char func[16];
-  struct Cns_api_thread_info *thip;
-  
-  strcpy (func, "Cns_getid");
-  if (Cns_apiinit (&thip))
-    return (-1);
-  
-  if (uid != NULL) {
-    if(thip->use_authorization_id) {
-      *uid = thip->uid;
-    } else {
-      *uid = geteuid();
-    }
-  }
+  return Cns_getid_ext(uid, gid, MODE_EFFECTIVE_ID);
+}
 
-  if (gid != NULL) {
-    if(thip->use_authorization_id) {
-      *gid = thip->gid;
-    } else {
-      *gid = getegid();
-    }
-  }
-  return(0);
+/** Cns_getrealid
+ *
+ * Gets the authorization id from the per thread structure.
+ * Either Cns_setid has been done, in which case that id is used,
+ * or geteuid/getegid are used to get the values.
+ *
+ * @param uid Pointer to where to store the uid
+ * @param gid Pointer to where to store the gid
+ * @returns 0 in case of successs, -1 otherwise.
+ */
+int DLL_DECL
+Cns_getrealid(uid, gid)
+ uid_t *uid;
+ gid_t *gid;
+{
+  return Cns_getid_ext(uid, gid, MODE_REAL_ID);
 }
