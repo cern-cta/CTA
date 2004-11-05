b5d7c177ad0822de738a38b37d233bd3fa807edd
Benjamin Couturier <bcouturi@cern.ch>
2004-11-05 14:22:42 +0000

    Added missing headers

--- castor/io/StreamCnvSvc.hpp
+++ castor/io/StreamCnvSvc.hpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: StreamCnvSvc.hpp,v $ $Revision: 1.7 $ $Release$ $Date: 2004/11/04 08:54:27 $ $Author: sponcec3 $
+ * @(#)$RCSfile: StreamCnvSvc.hpp,v $ $Revision: 1.8 $ $Release$ $Date: 2004/11/05 14:22:42 $ $Author: bcouturi $
  *
  *
  *
@@ -30,6 +30,8 @@
 #include "castor/Constants.hpp"
 #include "castor/BaseCnvSvc.hpp"
 #include "castor/exception/Exception.hpp"
+#include "castor/io/StreamAddress.hpp"
+#include "castor/ObjectCatalog.hpp"
 
 namespace castor {
 
