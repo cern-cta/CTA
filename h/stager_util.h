938e11fc2b92da25758e04b8c8fe8f18866a927b
Jean-Damien Durand <jdurand@cern.ch>
2004-11-09 09:28:55 +0000

    Utilities

---
+++ h/stager_util.h
@@ -0,0 +1,14 @@
+/*
+ * $Id: stager_util.h,v 1.1 2004/11/09 09:28:55 jdurand Exp $
+ */
+
+#ifndef stager_util_h
+#define stager_util_h
+
+#include "osdep.h"
+#include <sys/types.h>
+#include <time.h>
+
+EXTERN_C void DLL_DECL stager_util_time _PROTO((time_t, char *));
+
+#endif /* stager_api_h */
