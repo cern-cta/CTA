f7c39a3a1534a9ce61c45ecbb0f9d980fa7c612b
Jean-Damien Durand <jdurand@cern.ch>
2004-11-25 13:27:12 +0000

    The names says all

---
+++ h/stager_service2name.h
@@ -0,0 +1,13 @@
+/* $Id: stager_service2name.h,v 1.1 2004/11/25 13:27:12 jdurand Exp $ */
+
+#ifndef __stager_service2name_h
+#define __stager_service2name_h
+
+char *service2name[STAGER_SERVICE_MAX] = { "STAGER_SERVICE_DB",
+					   "STAGER_SERVICE_QUERY",
+					   "STAGER_SERVICE_UPDATE",
+					   "STAGER_SERVICE_GETNEXT",
+					   "STAGER_SERVICE_JOB"
+};
+
+#endif /* __stager_service2name_h */
