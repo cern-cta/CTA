bfb54f8ac8131e12e8f9bf806224020c51d07d50
Sebastien Ponce <sponcec3@cern.ch>
2004-11-30 11:31:43 +0000

    Forgot to add these files

---
+++ castor/io/StreamBaseAddressCnv.cpp
@@ -0,0 +1,156 @@
+/******************************************************************************
+ *                      castor/io/StreamBaseAddressCnv.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile$ $Revision$ $Release$ $Date$ $Author$
+ *
+ * 
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+// Include Files
+#include "StreamBaseAddressCnv.hpp"
+#include "castor/BaseAddress.hpp"
+#include "castor/CnvFactory.hpp"
+#include "castor/Constants.hpp"
+#include "castor/IAddress.hpp"
+#include "castor/ICnvFactory.hpp"
+#include "castor/ICnvSvc.hpp"
+#include "castor/IObject.hpp"
+#include "castor/ObjectCatalog.hpp"
+#include "castor/ObjectSet.hpp"
+#include "castor/exception/Exception.hpp"
+#include "castor/io/StreamAddress.hpp"
+#include "castor/io/StreamCnvSvc.hpp"
+#include "osdep.h"
+#include <string>
+
+//------------------------------------------------------------------------------
+// Instantiation of a static factory class
+//------------------------------------------------------------------------------
+static castor::CnvFactory<castor::io::StreamBaseAddressCnv> s_factoryStreamBaseAddressCnv;
+const castor::ICnvFactory& StreamBaseAddressCnvFactory = 
+  s_factoryStreamBaseAddressCnv;
+
+//------------------------------------------------------------------------------
+// Constructor
+//------------------------------------------------------------------------------
+castor::io::StreamBaseAddressCnv::StreamBaseAddressCnv(castor::ICnvSvc* cnvSvc) :
+  StreamBaseCnv(cnvSvc) {}
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+castor::io::StreamBaseAddressCnv::~StreamBaseAddressCnv() throw() {
+}
+
+//------------------------------------------------------------------------------
+// ObjType
+//------------------------------------------------------------------------------
+const unsigned int castor::io::StreamBaseAddressCnv::ObjType() {
+  return castor::BaseAddress::TYPE();
+}
+
+//------------------------------------------------------------------------------
+// objType
+//------------------------------------------------------------------------------
+const unsigned int castor::io::StreamBaseAddressCnv::objType() const {
+  return ObjType();
+}
+
+//------------------------------------------------------------------------------
+// createRep
+//------------------------------------------------------------------------------
+void castor::io::StreamBaseAddressCnv::createRep(castor::IAddress* address,
+                                                 castor::IObject* object,
+                                                 bool autocommit,
+                                                 unsigned int type)
+  throw (castor::exception::Exception) {
+  castor::BaseAddress* obj = 
+    dynamic_cast<castor::BaseAddress*>(object);
+  StreamAddress* ad = 
+    dynamic_cast<StreamAddress*>(address);
+  ad->stream() << obj->type();
+  ad->stream() << obj->objType();
+  ad->stream() << obj->cnvSvcName();
+  ad->stream() << obj->cnvSvcType();
+  ad->stream() << obj->id();
+}
+
+//------------------------------------------------------------------------------
+// createObj
+//------------------------------------------------------------------------------
+castor::IObject* castor::io::StreamBaseAddressCnv::createObj(castor::IAddress* address)
+  throw (castor::exception::Exception) {
+  StreamAddress* ad = 
+    dynamic_cast<StreamAddress*>(address);
+  // create the new Object
+  castor::BaseAddress* object = new castor::BaseAddress();
+  // Now retrieve and set members
+  unsigned int objType;
+  ad->stream() >> objType;
+  object->setObjType(objType);
+  std::string cnvSvcName;
+  ad->stream() >> cnvSvcName;
+  object->setCnvSvcName(cnvSvcName);
+  unsigned int cnvSvcType;
+  ad->stream() >> cnvSvcType;
+  object->setCnvSvcType(cnvSvcType);
+  u_signed64 id;
+  ad->stream() >> id;
+  object->setId(id);
+  return object;
+}
+
+//------------------------------------------------------------------------------
+// marshalObject
+//------------------------------------------------------------------------------
+void castor::io::StreamBaseAddressCnv::marshalObject(castor::IObject* object,
+                                                     castor::io::StreamAddress* address,
+                                                     castor::ObjectSet& alreadyDone)
+  throw (castor::exception::Exception) {
+  castor::BaseAddress* obj = 
+    dynamic_cast<castor::BaseAddress*>(object);
+  if (0 == obj) {
+    // Case of a null pointer
+    address->stream() << castor::OBJ_Ptr << ((unsigned int)0);
+  } else if (alreadyDone.find(obj) == alreadyDone.end()) {
+    // Case of a pointer to a non streamed object
+    createRep(address, obj, true);
+    // Mark object as done
+    alreadyDone.insert(obj);
+  } else {
+    // case of a pointer to an already streamed object
+    address->stream() << castor::OBJ_Ptr << alreadyDone[obj];
+  }
+}
+
+//------------------------------------------------------------------------------
+// unmarshalObject
+//------------------------------------------------------------------------------
+castor::IObject* castor::io::StreamBaseAddressCnv::unmarshalObject(castor::io::biniostream& stream,
+                                                                   castor::ObjectCatalog& newlyCreated)
+  throw (castor::exception::Exception) {
+  castor::io::StreamAddress ad(stream, "StreamCnvSvc", SVC_STREAMCNV);
+  castor::IObject* object = createObj(&ad);
+  // Mark object as created
+  newlyCreated.insert(object);
+  return object;
+}
+
---
+++ castor/io/StreamBasicResponseCnv.cpp
@@ -0,0 +1,152 @@
+/******************************************************************************
+ *                      castor/io/StreamBasicResponseCnv.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile$ $Revision$ $Release$ $Date$ $Author$
+ *
+ * 
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+// Include Files
+#include "StreamBasicResponseCnv.hpp"
+#include "castor/CnvFactory.hpp"
+#include "castor/Constants.hpp"
+#include "castor/IAddress.hpp"
+#include "castor/ICnvFactory.hpp"
+#include "castor/ICnvSvc.hpp"
+#include "castor/IObject.hpp"
+#include "castor/ObjectCatalog.hpp"
+#include "castor/ObjectSet.hpp"
+#include "castor/exception/Exception.hpp"
+#include "castor/io/StreamAddress.hpp"
+#include "castor/io/StreamCnvSvc.hpp"
+#include "castor/rh/BasicResponse.hpp"
+#include "osdep.h"
+#include <string>
+
+//------------------------------------------------------------------------------
+// Instantiation of a static factory class
+//------------------------------------------------------------------------------
+static castor::CnvFactory<castor::io::StreamBasicResponseCnv> s_factoryStreamBasicResponseCnv;
+const castor::ICnvFactory& StreamBasicResponseCnvFactory = 
+  s_factoryStreamBasicResponseCnv;
+
+//------------------------------------------------------------------------------
+// Constructor
+//------------------------------------------------------------------------------
+castor::io::StreamBasicResponseCnv::StreamBasicResponseCnv(castor::ICnvSvc* cnvSvc) :
+  StreamBaseCnv(cnvSvc) {}
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+castor::io::StreamBasicResponseCnv::~StreamBasicResponseCnv() throw() {
+}
+
+//------------------------------------------------------------------------------
+// ObjType
+//------------------------------------------------------------------------------
+const unsigned int castor::io::StreamBasicResponseCnv::ObjType() {
+  return castor::rh::BasicResponse::TYPE();
+}
+
+//------------------------------------------------------------------------------
+// objType
+//------------------------------------------------------------------------------
+const unsigned int castor::io::StreamBasicResponseCnv::objType() const {
+  return ObjType();
+}
+
+//------------------------------------------------------------------------------
+// createRep
+//------------------------------------------------------------------------------
+void castor::io::StreamBasicResponseCnv::createRep(castor::IAddress* address,
+                                                   castor::IObject* object,
+                                                   bool autocommit,
+                                                   unsigned int type)
+  throw (castor::exception::Exception) {
+  castor::rh::BasicResponse* obj = 
+    dynamic_cast<castor::rh::BasicResponse*>(object);
+  StreamAddress* ad = 
+    dynamic_cast<StreamAddress*>(address);
+  ad->stream() << obj->type();
+  ad->stream() << obj->errorCode();
+  ad->stream() << obj->errorMessage();
+  ad->stream() << obj->id();
+}
+
+//------------------------------------------------------------------------------
+// createObj
+//------------------------------------------------------------------------------
+castor::IObject* castor::io::StreamBasicResponseCnv::createObj(castor::IAddress* address)
+  throw (castor::exception::Exception) {
+  StreamAddress* ad = 
+    dynamic_cast<StreamAddress*>(address);
+  // create the new Object
+  castor::rh::BasicResponse* object = new castor::rh::BasicResponse();
+  // Now retrieve and set members
+  unsigned int errorCode;
+  ad->stream() >> errorCode;
+  object->setErrorCode(errorCode);
+  std::string errorMessage;
+  ad->stream() >> errorMessage;
+  object->setErrorMessage(errorMessage);
+  u_signed64 id;
+  ad->stream() >> id;
+  object->setId(id);
+  return object;
+}
+
+//------------------------------------------------------------------------------
+// marshalObject
+//------------------------------------------------------------------------------
+void castor::io::StreamBasicResponseCnv::marshalObject(castor::IObject* object,
+                                                       castor::io::StreamAddress* address,
+                                                       castor::ObjectSet& alreadyDone)
+  throw (castor::exception::Exception) {
+  castor::rh::BasicResponse* obj = 
+    dynamic_cast<castor::rh::BasicResponse*>(object);
+  if (0 == obj) {
+    // Case of a null pointer
+    address->stream() << castor::OBJ_Ptr << ((unsigned int)0);
+  } else if (alreadyDone.find(obj) == alreadyDone.end()) {
+    // Case of a pointer to a non streamed object
+    createRep(address, obj, true);
+    // Mark object as done
+    alreadyDone.insert(obj);
+  } else {
+    // case of a pointer to an already streamed object
+    address->stream() << castor::OBJ_Ptr << alreadyDone[obj];
+  }
+}
+
+//------------------------------------------------------------------------------
+// unmarshalObject
+//------------------------------------------------------------------------------
+castor::IObject* castor::io::StreamBasicResponseCnv::unmarshalObject(castor::io::biniostream& stream,
+                                                                     castor::ObjectCatalog& newlyCreated)
+  throw (castor::exception::Exception) {
+  castor::io::StreamAddress ad(stream, "StreamCnvSvc", SVC_STREAMCNV);
+  castor::IObject* object = createObj(&ad);
+  // Mark object as created
+  newlyCreated.insert(object);
+  return object;
+}
+
