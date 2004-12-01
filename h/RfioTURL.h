cc58e387e705032633fa835c060381a21e51fab8
Benjamin Couturier <bcouturi@cern.ch>
2004-12-01 18:15:24 +0000

    Initial release

---
+++ h/RfioTURL.h
@@ -0,0 +1,131 @@
+/*
+ *
+ * Copyright (C) 2003 by CERN/IT/ADC/CA
+ * All rights reserved
+ *
+ * @(#)$RCSfile: RfioTURL.h,v $ $Revision: 1.1 $ $Release$ $Date: 2004/12/01 18:15:24 $ $Author: bcouturi $
+ *
+ *
+ *
+ * @author Olof Barring
+ */
+
+/** RfioTURL.c - RFIO TURL handling
+ *
+ * Auxiliary routines for handling the RFIO TURL convention:
+ * <BR><CODE>
+ * rfio://[hostname][:port]/path
+ * </CODE><P>
+ * Examples:
+ * - Physical (remote) disk file following the SHIFT/CASTOR "NFS" convention:
+ *   - TURL: rfio://pub001d//shift/pub001d/data01/c3/stage/abc.123
+ *   - RFIO path: /shift/pub001d/data01/c3/stage/abc.123
+ * - Physical (remote) disk file, standard format:
+ *   - TURL: rfio://wacdr002d//tmp/abc.123
+ *   - RFIO path: wacdr002d:/tmp/abc.123
+ * - CASTOR file:
+ *   - TURL: rfio:////castor/cern.ch/user/n/nobody/abc.123
+ *   - RFIO path: /castor/cern.ch/user/n/nobody/abc.123
+ * - Remote file on a windows file server (shows the importance of the '/' delimiter)
+ *   - TURL: rfio://pcwin32/c:\temp\abc.123
+ *   - RFIO path: pcwin32:c:\temp\abc.123
+ *
+ */
+
+#ifndef _RFIOTURL_H 
+#define _RFIOTURL_H 1
+
+#define DEFAULT_RFIO_TURL_PREFIX "rfio://"
+#define RFIO_PROTOCOL_NAME "rfio"
+
+/** RFIO TURL structure */
+typedef struct RfioTURL
+{
+  char rfioProtocolName[10]; /**< Should always be "rfio" */
+  unsigned long rfioPort;    /**< The RFIO port number */
+  char rfioHostName[CA_MAXHOSTNAMELEN+1]; /**< The rfio server host */
+  char rfioPath[CA_MAXPATHLEN+1]; /**< The remote path */
+} RfioTURL_t;
+
+/** Initialize a new RFIO TURL prefix
+ *
+ * @param prefix - the new prefix
+ *
+ * Sets the (thread-specific) RFIO prefix. The default RFIO
+ * prefix is defined by the macro DEFAULT_RFIO_TURL_PREFIX to "rfio://".
+ * This routine should only be called if a different RFIO TURL
+ * prefix is required.
+ *
+ * @return 0 == OK, -1 initialisation of (thread specific) memory failed
+ */
+EXTERN_C int DLL_DECL initRfioTURLPrefix _PROTO((
+                                                 char *prefix
+                                                 ));
+
+/** Get the current RFIO TURL prefix
+ *
+ * Returns a character pointer to the current RFIO TURL in use.
+ *
+ * @see initRfioTURLPrefix()
+ */
+EXTERN_C char DLL_DECL *getRfioTURLPrefix _PROTO((
+                                         void
+                                         ));
+/** Build a RFIO TURL structure from a string representation
+ *
+ * @param tURLString - the string to be parsed
+ * @param rfioTURL - output RFIO TURL structure
+ *
+ * Parses the passed tURLString parameter and sets the corresponding
+ * fields in the output rfioTURL structure.
+ *
+ * @return 0 == OK, -1 parsing failed. On failure the serrno global is set
+ * to one of following values:
+ * - EINVAL either the TURL PREFIX did not match the RFIO 
+ *   TURL prefix or the tURLString format was wrong
+ * - E2BIG The protocol name, host name or remote path was too long
+ */
+EXTERN_C int DLL_DECL rfioTURLFromString _PROTO((
+                                                 char *tURLString,
+                                                 RfioTURL_t *rfioTURL
+                                                 ));
+/** Build a RFIO TURL string representation
+ *
+ * @param rfioTURL - RFIO TURL structure
+ * @param tURLString - output RFIO TURL string allocated by caller
+ * @param len - lenght of the passed string array
+ * 
+ * This routine is dual to rfioTURLFromString(). It builds
+ * a string representation of the RFIO TURL.
+ *
+ * @return 0 == OK, -1 if a string representation could not be built. In
+ * case of failure the serrno global is set to one of following values:
+ * - EINVAL if rfioTURL or tURLString are NULL or len is negative
+ * - E2BIG the string representation exceeds the passed len parameter
+ */
+EXTERN_C int DLL_DECL rfioTURLToString _PROTO((
+                                               RfioTURL_t *rfioTURL,
+                                               char *tURLString,
+                                               int len
+                                               ));
+/** Builds a RFIO TURL from the input RFIO path
+ *  
+ * @param rfioPath - input RFIO path
+ * @param rfioTURL - output RFIO TURL structure
+ *
+ * This routine builds a RFIO TURL structure given the input RFIO
+ * path.
+ *
+ * @return 0 == OK, -1 if a string representation could not be built. In
+ * case of failure the serrno global is set to one of following values:
+ * - EINVAL if rfioPath or rfioTURL are NULL 
+ * - E2BIG The protocol name, host name or remote path was too long
+ *
+ */
+EXTERN_C int DLL_DECL rfioPathToTURL _PROTO((
+                                             char *rfioPath,
+                                             RfioTURL_t *rfioTURL
+                                             ));
+
+
+#endif // _RFIOTURL_H
---
+++ rfio/RfioTURL.c
@@ -0,0 +1,291 @@
+/*
+ *
+ * Copyright (C) 2003 by CERN/IT/ADC/CA
+ * All rights reserved
+ *
+ * @(#)$RCSfile: RfioTURL.c,v $ $Revision: 1.1 $ $Release$ $Date: 2004/12/01 18:14:56 $ $Author: bcouturi $
+ *
+ *
+ *
+ * @author Olof Barring
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: RfioTURL.c,v $ $Revision: 1.1 $ $Release$ $Date: 2004/12/01 18:14:56 $ Olof Barring";
+#endif /* not lint */
+
+/** RfioTURL.c - RFIO TURL handling
+ *
+ * Auxiliary routines for handling the RFIO TURL convention:
+ * <BR><CODE>
+ * rfio://[hostname][:port]/path
+ * </CODE><P>
+ * Examples:
+ * - Physical (remote) disk file following the SHIFT/CASTOR "NFS" convention:
+ *   - TURL: rfio://pub001d//shift/pub001d/data01/c3/stage/abc.123
+ *   - RFIO path: /shift/pub001d/data01/c3/stage/abc.123
+ * - Physical (remote) disk file, standard format:
+ *   - TURL: rfio://wacdr002d//tmp/abc.123
+ *   - RFIO path: wacdr002d:/tmp/abc.123
+ * - CASTOR file:
+ *   - TURL: rfio:////castor/cern.ch/user/n/nobody/abc.123
+ *   - RFIO path: /castor/cern.ch/user/n/nobody/abc.123
+ * - Remote file on a windows file server (shows the importance of the '/' delimiter)
+ *   - TURL: rfio://pcwin32/c:\temp\abc.123
+ *   - RFIO path: pcwin32:c:\temp\abc.123
+ *
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include <Castor_limits.h>
+#include <osdep.h>
+#include <serrno.h>
+#include <Cglobals.h>
+#include <rfio_api.h>
+#include <RfioTURL.h>
+EXTERN_C int DLL_DECL rfio_parse _PROTO((char *, char **, char **));
+
+static int tURLPrefixKey = -1;
+static int tURLPrefixLenKey = -1;
+
+static int checkAlphaNum(char *p) 
+{
+  int i;
+  if ( p == NULL ) return(-1);
+  for ( i=0; p[i] != '\0'; i++ ) {
+    if ( !isalnum(p[i]) ) return(0);
+  }
+  return(1);
+}
+
+static int checkNum(char *p) 
+{
+  int i;
+  if ( p == NULL ) return(-1);
+  for ( i=0; p[i] != '\0'; i++ ) {
+    if ( !isdigit(p[i]) ) return(0);
+  }
+  return(1);
+}
+
+  
+
+int initRfioTURLPrefix(char *prefix) 
+{
+  char *str = NULL;
+  int rc, len, *tURLLen = NULL;
+  if ( prefix == NULL ) {
+    serrno = EINVAL;
+    return(-1);
+  }
+  
+  len = strlen(prefix)+1;
+  if ( len < CA_MAXLINELEN + 1 ) len = CA_MAXLINELEN+1;
+  rc = Cglobals_get(&tURLPrefixKey,(void *)&str,len);
+  if ( rc == -1 || str == NULL ) return(-1);
+  strcpy(str,prefix);
+
+  rc = Cglobals_get(&tURLPrefixLenKey,(void *)&tURLLen,sizeof(int));
+  if ( rc == -1 || tURLLen == NULL ) return(-1);
+  *tURLLen = len;
+  
+  return(0);
+}
+
+char *getRfioTURLPrefix() 
+{
+  char *str;
+  int rc, *tURLLen = NULL;
+  
+  rc = Cglobals_get(&tURLPrefixLenKey,(void *)&tURLLen,sizeof(int));
+  if ( rc == -1 || tURLLen == NULL ) return(NULL);
+
+  if ( *tURLLen == 0 ) return(DEFAULT_RFIO_TURL_PREFIX);
+  
+  rc = Cglobals_get(&tURLPrefixKey,(void *)&str,*tURLLen);
+  if ( rc == -1 || str == NULL ) return(NULL);
+  return(str);
+}
+
+int rfioTURLFromString(
+                       char *tURLStr,
+                       RfioTURL_t *tURL
+                       ) 
+{
+  char *p, *q, *q1, *orig, *prefix;
+  RfioTURL_t _tURL;
+  
+  if ( tURLStr == NULL || tURL == NULL ) {
+    serrno = EINVAL;
+    return(-1);
+  }
+
+  prefix = getRfioTURLPrefix();
+  if ( prefix == NULL ) return(-1);
+
+  if ( strstr(tURLStr,prefix) != tURLStr ) {
+    serrno = EINVAL;
+    return(-1);
+  }  
+
+  /*
+   * Extract the protocol name
+   */
+  orig = p = strdup(tURLStr);
+  if ( p == NULL ) return(-1);
+  q = strstr(p,":");
+  if ( q == NULL ) {
+    serrno = EINVAL;
+    free(orig);
+    return(-1);
+  }
+
+  *q = '\0';
+  if ( strlen(p) >= sizeof(tURL->rfioProtocolName) ) {
+    serrno = E2BIG;
+    free(orig);
+    return(-1);
+  }
+
+  if ( checkAlphaNum(p) != 1 ) {
+    serrno = EINVAL;
+    free(orig);
+    return(-1);
+  }
+  
+  strcpy(_tURL.rfioProtocolName,p);
+  *q = ':';
+
+  /*
+   * Split off the prefix since we don't need it anymore.
+   */
+  p += strlen(prefix);
+
+  /*
+   * Expect the [host][:port] name at this point. The hostname is delimited from the path by a '/'
+   * which is mandatory (thus, even if there is no hostname, like in a castor path)
+   */
+  q = strstr(p,"/");
+  if ( q == NULL ) {
+    serrno = EINVAL;
+    free(orig);
+    return(-1);
+  }
+  *q = '\0';
+  q1 = strstr(p,":");
+  if ( q1 != NULL ) {
+    *q1 = '\0';
+    q1++;
+    if ( checkNum(q1) == 0 ) {
+      serrno = EINVAL;
+      free(orig);
+      return(-1);
+    }
+    _tURL.rfioPort = atoi(q1);
+  } else _tURL.rfioPort = 0;
+  
+  if ( strlen(p) >= sizeof(tURL->rfioHostName) ) {
+    serrno = E2BIG;
+    free(orig);
+    return(-1);
+  }
+  strcpy(_tURL.rfioHostName,p);
+
+  /*
+   * Finally we only have the path left
+   */
+  p = q+1;
+  if ( strlen(p) >= sizeof(tURL->rfioPath) ) {
+    serrno = E2BIG;
+    free(orig);
+    return(-1);
+  }
+  strcpy(_tURL.rfioPath,p);
+  free(orig);
+  /*
+   * Everything OK. Copy the temporary structure to the output
+   */
+  *tURL = _tURL;
+  return(0);
+}
+
+int rfioTURLToString(RfioTURL_t *tURL, char *str, int len) 
+{
+  char *prefix, portStr[12];
+  int _len = 0;
+
+  if ( tURL == NULL || str == NULL || len <= 0 ) {
+    serrno = EINVAL;
+    return(-1);
+  }
+
+  prefix = getRfioTURLPrefix();
+  if ( prefix != NULL ) _len = strlen(prefix);
+
+  _len += strlen(tURL->rfioHostName);
+  *portStr = '\0';
+  if ( tURL->rfioPort > 0 ) {
+    _len++; /* for the ':' delimiter */
+    sprintf(portStr,"%lu",tURL->rfioPort);
+    _len += strlen(portStr);
+  }
+  _len++; /* '/' between [host][:port] and path */  
+  _len += strlen(tURL->rfioPath);
+  _len++; /* terminating '\0' */
+  if ( len < _len ) {
+    serrno = E2BIG;
+    return(-1);
+  }
+  sprintf(str,"%s%s%s%s/%s",prefix,tURL->rfioHostName,(tURL->rfioPort > 0 ? ":" : ""),
+          (tURL->rfioPort > 0 ? portStr : ""),tURL->rfioPath);
+  return(0);
+}
+
+int rfioPathToTURL(char *rfioPath, RfioTURL_t *tURL) 
+{
+  char *host = NULL, *path = NULL;
+  char lhost[CA_MAXHOSTNAMELEN+1];
+  RfioTURL_t _tURL;
+  int rc;
+
+  if ( rfioPath == NULL || tURL == NULL ) {
+    serrno = EINVAL;
+    return(-1);
+  }
+
+  if ( (rc = rfio_parse(rfioPath,&host,&path)) == -1 ) return(-1);
+
+  if ( path == NULL ) path = rfioPath;
+  if ( strlen(path) >= sizeof(tURL->rfioPath) ) {
+    serrno = E2BIG;
+    return(-1);
+  }
+  strcpy(_tURL.rfioProtocolName,RFIO_PROTOCOL_NAME);
+  _tURL.rfioPort = 0; /* use whatever local default is set to */
+  if ( host == NULL ) {
+    /* Local file */
+    if ( gethostname(lhost,sizeof(lhost)) == -1 ) return(-1);
+    host = lhost;
+    if ( strlen(host) >= sizeof(tURL->rfioHostName) ) {
+      serrno = E2BIG;
+      return(-1);
+    }
+    strcpy(_tURL.rfioHostName,host);
+  } else if ( rc == 0 ) {
+    /* HSM (Castor) file */
+    *_tURL.rfioHostName = '\0';
+  } else {
+    /* Normal remote file */
+    strcpy(_tURL.rfioHostName,host);
+  }
+  
+  strcpy(_tURL.rfioPath,path);
+
+  *tURL = _tURL;  
+  return(0);
+}
