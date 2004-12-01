d259cd733397b364c22888503d22cfd468737802
Sebastien Ponce <sponcec3@cern.ch>
2004-12-01 17:40:25 +0000

    Fix createTapeCopySegmentsForRecall : status of the Tape was not updated

--- castor/db/ora/OraStagerSvc.cpp
+++ castor/db/ora/OraStagerSvc.cpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: OraStagerSvc.cpp,v $ $Revision: 1.65 $ $Release$ $Date: 2004/12/01 16:33:54 $ $Author: sponcec3 $
+ * @(#)$RCSfile: OraStagerSvc.cpp,v $ $Revision: 1.66 $ $Release$ $Date: 2004/12/01 17:40:25 $ $Author: sponcec3 $
  *
  *
  *
@@ -1495,6 +1495,7 @@ void castor::db::ora::OraStagerSvc::createTapeCopySegmentsForRecall
     case castor::stager::TAPE_UNKNOWN:
       tape->setStatus(castor::stager::TAPE_PENDING);
     }
+    cnvSvc()->updateRep(&ad, tape false);
     // Link Tape with Segment
     segment->setTape(tape);
     tape->addSegments(segment);
