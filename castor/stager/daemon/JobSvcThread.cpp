4ee3d58aabb78a2a7b62c14c57408fe352494e92
Sebastien Ponce <sponcec3@cern.ch>
2004-12-02 17:57:34 +0000

    changed implementation of stager_job_service from C to C++

---
+++ castor/stager/daemon/JobSvcThread.cpp
@@ -0,0 +1,331 @@
+/*
+ * $Id: JobSvcThread.cpp,v 1.1 2004/12/02 17:57:34 sponcec3 Exp $
+ */
+
+/*
+ * Copyright (C) 2004 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef lint
+static char *sccsid = "@(#)$RCSfile: JobSvcThread.cpp,v $ $Revision: 1.1 $ $Date: 2004/12/02 17:57:34 $ CERN IT-ADC/CA Ben Couturier";
+#endif
+
+/* ================================================================= */
+/* Local headers for threads : to be included before ANYTHING else   */
+/* ================================================================= */
+#include "Cthread_api.h"
+#include "Cmutex.h"
+
+/* ============== */
+/* System headers */
+/* ============== */
+#include <list>
+#include <vector>
+
+/* ============= */
+/* Local headers */
+/* ============= */
+#include "castor/Services.hpp"
+#include "castor/Constants.hpp"
+#include "castor/BaseAddress.hpp"
+#include "castor/IObject.hpp"
+#include "castor/IClient.hpp"
+#include "castor/IService.hpp"
+#include "castor/stager/IStagerSvc.hpp"
+#include "castor/exception/Exception.hpp"
+#include "castor/exception/Internal.hpp"
+#include "castor/BaseObject.hpp"
+#include "castor/stager/Request.hpp"
+#include "castor/stager/SubRequest.hpp"
+#include "castor/stager/DiskCopy.hpp"
+#include "castor/stager/DiskCopyForRecall.hpp"
+#include "castor/stager/FileSystem.hpp"
+#include "castor/stager/GetUpdateStartRequest.hpp"
+#include "castor/rh/GetUpdateStartResponse.hpp"
+#include "castor/replier/RequestReplier.hpp"
+#undef logfunc
+
+#include "stager_job_service.h"
+#include "stager_macros.h"
+#include "serrno.h"
+
+#undef NULL
+#define NULL 0
+
+/* ------------------------------------- */
+/* stager_job_select()                   */
+/*                                       */
+/* Purpose: Job 'select' part of service */
+/*                                       */
+/* Input:  n/a                           */
+/*                                       */
+/* Output:  (void **) output   Selected  */
+/*                                       */
+/* Return: 0 [OK] or -1 [ERROR, serrno]  */
+/* ------------------------------------- */
+
+
+
+EXTERN_C int DLL_DECL stager_job_select(void **output) {
+  char *func =  "stager_job_select";
+  int rc;
+
+  STAGER_LOG_ENTER();
+
+  if (output == NULL) {
+    serrno = EFAULT;
+    return -1;
+  }
+
+  castor::stager::Request* req = 0;
+  castor::Services *svcs;
+  castor::stager::IStagerSvc *stgSvc;
+
+  try {
+
+    /* Loading services */
+    /* ---------------- */
+    STAGER_LOG_DEBUG(NULL,"Loading services");
+    svcs = castor::BaseObject::services();
+    castor::IService* svc =
+      svcs->service("OraStagerSvc", castor::SVC_ORASTAGERSVC);
+    stgSvc = dynamic_cast<castor::stager::IStagerSvc*>(svc);
+
+
+    /* Get any new request to do    */
+    /* ---------------------------- */
+    STAGER_LOG_DEBUG(NULL,"Getting any request to do");
+    std::vector<castor::ObjectsIds> types;
+    types.push_back(castor::OBJ_GetUpdateStartRequest);
+    castor::stager::Request* req = stgSvc->requestToDo(types);
+
+    if (0 == req) {
+      /* Nothing to do */
+      STAGER_LOG_DEBUG(NULL,"Nothing to do");
+      serrno = ENOENT;
+      rc = -1;
+    } else {
+      STAGER_LOG_DEBUG(NULL,"req FOUND");
+      *output = req;
+      rc = 0;
+    }
+
+  } catch (castor::exception::Exception e) {
+    serrno = e.code();
+    STAGER_LOG_DB_ERROR(NULL,"stager_job_select",
+                        e.getMessage().str().c_str());
+    rc = -1;
+    if (req) delete req;
+  }
+
+  // Cleanup
+  stgSvc->release();
+
+  // Return
+  STAGER_LOG_RETURN(rc);
+}
+
+
+/* -------------------------------------- */
+/* stager_job_process()                   */
+/*                                        */
+/* Purpose: Job 'process' part of service */
+/*                                        */
+/* Input:  (void *) output    Selection   */
+/*                                        */
+/* Output: n/a                            */
+/*                                        */
+/* Return: 0 [OK] or -1 [ERROR, serrno]   */
+/* -------------------------------------- */
+EXTERN_C int DLL_DECL stager_job_process(void *output) {
+
+  char *func =  "stager_job_process";
+  STAGER_LOG_ENTER();
+
+  serrno = 0;
+  if (output == NULL) {
+    serrno = EFAULT;
+    return -1;
+  }
+
+  /* ===========================================================
+   *
+   * 1) PREPARATION PHASE
+   * Retrieving request from the database
+   */
+
+  castor::stager::GetUpdateStartRequest* req = 0;
+  castor::Services *svcs = 0;
+  castor::stager::IStagerSvc *stgSvc = 0;
+  castor::IClient *client = 0;
+
+  /* Setting the address to access Oracle */
+  /* ------------------------------------ */
+  castor::BaseAddress ad;
+  ad.setCnvSvcName("OraCnvSvc");
+  ad.setCnvSvcType(castor::SVC_ORACNV);
+
+  try {
+
+    /* Loading services */
+    /* ---------------- */
+    STAGER_LOG_DEBUG(NULL,"Loading services");
+    svcs = castor::BaseObject::services();
+    castor::IService* svc =
+      svcs->service("OraStagerSvc", castor::SVC_ORASTAGERSVC);
+    stgSvc = dynamic_cast<castor::stager::IStagerSvc*>(svc);
+
+    /* Casting the request */
+    /* ------------------- */
+    STAGER_LOG_DEBUG(NULL, "Casting Request");
+    req = (castor::stager::GetUpdateStartRequest*)(output);
+    if (0 == req) {
+      castor::exception::Internal e;
+      e.getMessage() << "Request cast error";
+      throw e;
+    }
+
+    /* Getting the client  */
+    /* ------------------- */
+    svcs->fillObj(&ad, req, castor::OBJ_IClient);
+    client = req->client();
+    if (0 == client) {
+      castor::exception::Internal e;
+      e.getMessage() << "No client associated with request ! Cannot answer !";
+      throw e;
+    }
+
+  } catch (castor::exception::Exception e) {
+    // If we fail here, we do NOT have enough information to
+    // reply to the client !
+    serrno = e.code();
+    STAGER_LOG_DB_ERROR(NULL,"stager_job_select",
+                        e.getMessage().str().c_str());
+    if (req) delete req;
+    if (client) delete client;
+    if (stgSvc) stgSvc->release();
+    return -1;
+  }
+
+  /* ===========================================================
+   *
+   * 2) CALLING PHASE
+   * At this point we can send a reply to the client
+   * We get prepared to call the method
+   */
+
+  castor::stager::FileSystem *fs = 0;
+  castor::stager::SubRequest *subreq = 0;
+  castor::stager::DiskCopy *dc = 0;
+  castor::IClient *cl = 0;
+  std::string error;
+  std::list<castor::stager::DiskCopyForRecall*> sources;
+
+  try {
+
+    /* Loading the subrequest from Oracle */
+    /* ---------------------------------- */
+    STAGER_LOG_DEBUG(NULL, "Loading the subrequest from Oracle");
+    ad.setId(req->subreqId());
+    castor::IObject *obj = svcs->createObj(&ad);
+    if (0 == obj) {
+      castor::exception::Internal e;
+      e.getMessage() << "Found no object for ID:" << req->subreqId();
+      throw e;
+    }
+    subreq = dynamic_cast< castor::stager::SubRequest *>(obj);
+    if (0 == subreq) {
+      castor::exception::Internal e;
+      e.getMessage() << "Expected SubRequest found type "
+                     << castor::ObjectsIdStrings[obj->type()]
+                     << " for ID:" << req->subreqId();
+      delete obj;
+      throw e;
+    }
+
+    /* Getting the FileSystem Object      */
+    /* ---------------------------------- */
+    STAGER_LOG_DEBUG(NULL, "Getting the FileSystem Object");
+    fs = stgSvc->selectFileSystem(req->fileSystem(),
+                                  req->diskServer());
+    if (0==fs) {
+      castor::exception::Internal e;
+      e.getMessage() << "Could not find suitable filesystem for"
+                     << req->fileSystem() << "/"
+                     << req->diskServer();
+      throw e;
+    }
+
+    /* Invoking the method                */
+    /* ---------------------------------- */
+    STAGER_LOG_DEBUG(NULL, "Invoking getUpdateStart");
+    cl = stgSvc->getUpdateStart(subreq, fs, &dc, sources);
+
+    /* Fill DiskCopy with SubRequest           */
+    /* --------------------------------------- */
+    STAGER_LOG_DEBUG(NULL,"Filling DiskCopy with SubRequest");
+    svcs->fillObj(&ad, dc, castor::OBJ_SubRequest);
+
+  } catch (castor::exception::Exception e) {
+    serrno = e.code();
+    error = e.getMessage().str();
+    STAGER_LOG_DB_ERROR(NULL,"stager_job_process",
+                        e.getMessage().str().c_str());
+  }
+
+  /* ===========================================================
+   *
+   * 3) REPLY PHASE
+   * Actually sending a reply to the client
+   */
+
+  try {
+    /* Build the response             */
+    /* ------------------------------ */
+    STAGER_LOG_DEBUG(NULL, "Building Response");
+    castor::rh::GetUpdateStartResponse res;
+    if (0 != serrno) {
+      res.setErrorCode(serrno);
+      res.setErrorMessage(error);
+    } else {
+      res.setDiskCopy(dc);
+      for (std::list<castor::stager::DiskCopyForRecall*>::iterator it =
+             sources.begin();
+           it != sources.end();
+           it++) {
+        res.addSources(*it);
+      }
+      res.setClient(cl);  
+    }
+    
+    /* Send Response to client        */
+    /* ------------------------------ */
+    STAGER_LOG_DEBUG(NULL, "Sending Response");
+    castor::replier::RequestReplier *rr =
+      castor::replier::RequestReplier::getInstance();
+    rr->sendResponse(client, &res);
+    rr->sendEndResponse(client);
+
+  } catch (castor::exception::Exception e) {
+    serrno = e.code();
+    STAGER_LOG_DB_ERROR(NULL,"stager_job_process",
+                        e.getMessage().str().c_str());
+  }
+
+  if (req) delete req;
+  if (client) delete client;
+  if (cl) delete cl;
+  if (subreq) delete subreq;
+  if (dc) delete dc;
+  if (fs) delete fs;
+  for (std::list<castor::stager::DiskCopyForRecall*>::iterator it =
+         sources.begin();
+       it != sources.end();
+       it++) {
+    delete *it;
+  }
+  stgSvc->release();
+  STAGER_LOG_RETURN(serrno == 0 ? 0 : -1);
+}
+
