d28ab1c0e68d2a85dbcfaeaf7574d291005a4c1a
Sebastien Ponce <sponcec3@cern.ch>
2004-12-03 16:17:11 +0000

    Forgot the cvs add for this file

---
+++ castor/IClient.cpp
@@ -0,0 +1,78 @@
+/******************************************************************************
+ *                      castor/IClient.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile$ $Revision$ $Release$ $Date$ $Author$
+ *
+ * 
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+// Include Files
+#include "castor/IClient.hpp"
+#include "castor/ObjectSet.hpp"
+#include "castor/stager/Request.hpp"
+#include <iostream>
+#include <string>
+
+//------------------------------------------------------------------------------
+// Constructor
+//------------------------------------------------------------------------------
+castor::IClient::IClient() throw() :
+  m_request(0) {
+};
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+castor::IClient::~IClient() throw() {
+  if (0 != m_request) {
+    m_request->setClient(0);
+  }
+};
+
+//------------------------------------------------------------------------------
+// print
+//------------------------------------------------------------------------------
+void castor::IClient::print(std::ostream& stream,
+                            std::string indent,
+                            castor::ObjectSet& alreadyPrinted) const {
+  stream << indent << "[# IClient #]" << std::endl;
+  if (alreadyPrinted.find(this) != alreadyPrinted.end()) {
+    // Circular dependency, this object was already printed
+    stream << indent << "Back pointer, see above" << std::endl;
+    return;
+  }
+  alreadyPrinted.insert(this);
+  stream << indent << "Request : " << std::endl;
+  if (0 != m_request) {
+    m_request->print(stream, indent + "  ", alreadyPrinted);
+  } else {
+    stream << indent << "  null" << std::endl;
+  }
+}
+
+//------------------------------------------------------------------------------
+// print
+//------------------------------------------------------------------------------
+void castor::IClient::print() const {
+  ObjectSet alreadyPrinted;
+  print(std::cout, "", alreadyPrinted);
+}
+
