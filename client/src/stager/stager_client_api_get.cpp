d93358be622d80a941a6bdbbabed7ec6535ffef3
Benjamin Couturier <bcouturi@cern.ch>
2004-12-15 21:40:33 +0000

    Implemented prepareToGet client

--- client/src/stager/stager_client_api_get.cpp
+++ client/src/stager/stager_client_api_get.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: stager_client_api_get.cpp,v 1.11 2004/12/03 17:20:04 bcouturi Exp $
+ * $Id: stager_client_api_get.cpp,v 1.12 2004/12/15 21:40:33 bcouturi Exp $
  */
 
 /*
@@ -8,12 +8,14 @@
  */
 
 #ifndef lint
-static char *sccsid = "@(#)$RCSfile: stager_client_api_get.cpp,v $ $Revision: 1.11 $ $Date: 2004/12/03 17:20:04 $ CERN IT-ADC/CA Benjamin Couturier";
+static char *sccsid = "@(#)$RCSfile: stager_client_api_get.cpp,v $ $Revision: 1.12 $ $Date: 2004/12/15 21:40:33 $ CERN IT-ADC/CA Benjamin Couturier";
 #endif
 
 /* ============== */
 /* System headers */
 /* ============== */
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /* ============= */
 /* Local headers */
@@ -57,9 +59,110 @@ EXTERN_C int DLL_DECL stage_prepareToGet(const char *userTag,
                                   struct stage_options* opts) {
 
  
+  char *func = "stage_prepareToGet";
+ 
+  if (requests == NULL
+      || nbreqs <= 0
+      || responses == NULL
+      || nbresps == NULL) {
+    serrno = EINVAL;
+    stage_errmsg(func, "Invalid input parameter");
+    return -1;
+  }
+
+
+  try {
+    castor::BaseObject::initLog("", castor::SVC_NOMSG);
+    
+	
+    // Uses a BaseClient to handle the request
+    castor::client::BaseClient client;
+    castor::stager::StagePrepareToGetRequest req;
+
+    if (0 != userTag) {
+      req.setUserTag(std::string(userTag));
+    }
+    mode_t mask;
+    umask (mask = umask(0));
+    req.setMask(mask);
+
+    // Preparing the requests
+    for(int i=0; i<nbreqs; i++) {
+      castor::stager::SubRequest *subreq = new castor::stager::SubRequest();
+      
+      if (!(requests[i].filename)) {
+        serrno = EINVAL;
+        stage_errmsg(func, "filename in request %d is NULL", i);
+        return -1;
+      }
+
+      req.addSubRequests(subreq);
+      std::string sfilename(requests[i].filename);
+      subreq->setFileName(sfilename);
+      subreq->setRequest(&req);
+
+    }
+
+    // Using the VectorResponseHandler which stores everything in
+    // A vector. BEWARE, the responses must be de-allocated afterwards
+    std::vector<castor::rh::Response *>respvec;    
+    castor::client::VectorResponseHandler rh(&respvec);
+    client.sendRequest(&req, &rh);
+
+    // Parsing the responses which have been stored in the vector
+    int nbResponses =  respvec.size();
+
+    if (nbResponses <= 0) {
+      // We got not replies, this is not normal !
+      serrno = SEINTERNAL;
+      stage_errmsg(func, "No responses received");
+      return -1;
+    }
+    
+
+    // Creating the array of file responses
+    // Same size as requests as we only do files for the moment
+    *responses = (struct stage_prepareToGet_fileresp *) 
+      malloc(sizeof(struct stage_prepareToGet_fileresp) * nbResponses);
+
+    if (*responses == NULL) {
+      serrno = ENOMEM;
+      stage_errmsg(func, "Could not allocate memory for responses");
+      return -1;
+    }
+    *nbresps = nbResponses;
+    
+
+    for (int i=0; i<(int)respvec.size(); i++) {
+
+      // Casting the response into a FileResponse !
+      castor::rh::FileResponse* fr = 
+        dynamic_cast<castor::rh::FileResponse*>(respvec[i]);
+      if (0 == fr) {
+        castor::exception::Exception e(SEINTERNAL);
+        e.getMessage() << "Error in dynamic cast, response was NOT a file response";
+        throw e;
+      }
+
+      (*responses)[i].filename = strdup(fr->castorFileName().c_str());
+      (*responses)[i].status = fr->status();
+      (*responses)[i].errorCode = fr->errorCode();
+      if (fr->errorMessage().length() > 0) {
+        (*responses)[i].errorMessage= strdup(fr->errorMessage().c_str());
+      } else {
+        (*responses)[i].errorMessage=0;
+      }
+      // The responses should be deallocated by the API !
+      delete respvec[i];
+    } // for
+    
+  } catch (castor::exception::Exception e) {
+    serrno = e.code();
+    stage_errmsg(func, (char *)(e.getMessage().str().c_str()));
+    return -1;
+  }
+  
   return 0;
-
-
 
 }
 
@@ -100,7 +203,12 @@ EXTERN_C int DLL_DECL stage_get(const char *userTag,
       std::string suserTag(userTag);
       req.setUserTag(suserTag);
     }
-    
+
+    // Setting next
+    mode_t mask;
+    umask (mask = umask(0));
+    req.setMask(mask);
+
     if (protocol) {
       std::string sprotocol(protocol);
       subreq->setProtocol(sprotocol);
