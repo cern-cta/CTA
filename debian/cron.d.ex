2a94b5715958a23936e66223677e82af69bdca9f
Jean-Damien Durand <jdurand@cern.ch>
2005-01-23 14:42:29 +0000

    Removed template files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the castor package
-#
-0 4	* * *	root	castor_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(castor):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="castor" command="/usr/bin/castor"
