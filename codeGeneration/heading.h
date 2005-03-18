377ecf59198e92bb33629bde8bba056f5ea35890
Sebastien Ponce <sponcec3@cern.ch>
2005-03-18 14:04:37 +0000

    Put Castor Dev team as author for all files

--- codeGeneration/heading.h
+++ codeGeneration/heading.h
@@ -17,9 +17,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: heading.h,v $ $Revision: 1.1 $ $Release$ $Date: 2004/09/28 15:08:00 $ $Author: sponcec3 $
+ * @(#)$RCSfile: heading.h,v $ $Revision: 1.2 $ $Release$ $Date: 2005/03/18 14:04:37 $ $Author: sponcec3 $
  *
  * 
  *
- * @author %author%
+ * @author Castor Dev team, castor-dev@cern.ch
  *****************************************************************************/
