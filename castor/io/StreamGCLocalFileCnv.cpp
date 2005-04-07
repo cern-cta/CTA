98037bef0ab71d87a8927afc00eb0933b344f3de
Sebastien Ponce <sponcec3@cern.ch>
2005-04-07 12:41:21 +0000

    Added serialization for GCLocalFile (was preventing the GC to work)

---
+++ castor/io/StreamGCLocalFileCnv.cpp
@@ -0,0 +1,152 @@
+/******************************************************************************
+ *                      castor/io/StreamGCLocalFileCnv.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile$ $Revision$ $Release$ $Date$ $Author$
+ *
+ * 
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+// Include Files
+#include "StreamGCLocalFileCnv.hpp"
+#include "castor/CnvFactory.hpp"
+#include "castor/Constants.hpp"
+#include "castor/IAddress.hpp"
+#include "castor/ICnvFactory.hpp"
+#include "castor/ICnvSvc.hpp"
+#include "castor/IObject.hpp"
+#include "castor/ObjectCatalog.hpp"
+#include "castor/ObjectSet.hpp"
+#include "castor/exception/Exception.hpp"
+#include "castor/io/StreamAddress.hpp"
+#include "castor/io/StreamCnvSvc.hpp"
+#include "castor/stager/GCLocalFile.hpp"
+#include "osdep.h"
+#include <string>
+
+//------------------------------------------------------------------------------
+// Instantiation of a static factory class
+//------------------------------------------------------------------------------
+static castor::CnvFactory<castor::io::StreamGCLocalFileCnv> s_factoryStreamGCLocalFileCnv;
+const castor::ICnvFactory& StreamGCLocalFileCnvFactory = 
+  s_factoryStreamGCLocalFileCnv;
+
+//------------------------------------------------------------------------------
+// Constructor
+//------------------------------------------------------------------------------
+castor::io::StreamGCLocalFileCnv::StreamGCLocalFileCnv(castor::ICnvSvc* cnvSvc) :
+  StreamBaseCnv(cnvSvc) {}
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+castor::io::StreamGCLocalFileCnv::~StreamGCLocalFileCnv() throw() {
+}
+
+//------------------------------------------------------------------------------
+// ObjType
+//------------------------------------------------------------------------------
+const unsigned int castor::io::StreamGCLocalFileCnv::ObjType() {
+  return castor::stager::GCLocalFile::TYPE();
+}
+
+//------------------------------------------------------------------------------
+// objType
+//------------------------------------------------------------------------------
+const unsigned int castor::io::StreamGCLocalFileCnv::objType() const {
+  return ObjType();
+}
+
+//------------------------------------------------------------------------------
+// createRep
+//------------------------------------------------------------------------------
+void castor::io::StreamGCLocalFileCnv::createRep(castor::IAddress* address,
+                                                 castor::IObject* object,
+                                                 bool autocommit,
+                                                 unsigned int type)
+  throw (castor::exception::Exception) {
+  castor::stager::GCLocalFile* obj = 
+    dynamic_cast<castor::stager::GCLocalFile*>(object);
+  StreamAddress* ad = 
+    dynamic_cast<StreamAddress*>(address);
+  ad->stream() << obj->type();
+  ad->stream() << obj->fileName();
+  ad->stream() << obj->diskCopyId();
+  ad->stream() << obj->id();
+}
+
+//------------------------------------------------------------------------------
+// createObj
+//------------------------------------------------------------------------------
+castor::IObject* castor::io::StreamGCLocalFileCnv::createObj(castor::IAddress* address)
+  throw (castor::exception::Exception) {
+  StreamAddress* ad = 
+    dynamic_cast<StreamAddress*>(address);
+  // create the new Object
+  castor::stager::GCLocalFile* object = new castor::stager::GCLocalFile();
+  // Now retrieve and set members
+  std::string fileName;
+  ad->stream() >> fileName;
+  object->setFileName(fileName);
+  u_signed64 diskCopyId;
+  ad->stream() >> diskCopyId;
+  object->setDiskCopyId(diskCopyId);
+  u_signed64 id;
+  ad->stream() >> id;
+  object->setId(id);
+  return object;
+}
+
+//------------------------------------------------------------------------------
+// marshalObject
+//------------------------------------------------------------------------------
+void castor::io::StreamGCLocalFileCnv::marshalObject(castor::IObject* object,
+                                                     castor::io::StreamAddress* address,
+                                                     castor::ObjectSet& alreadyDone)
+  throw (castor::exception::Exception) {
+  castor::stager::GCLocalFile* obj = 
+    dynamic_cast<castor::stager::GCLocalFile*>(object);
+  if (0 == obj) {
+    // Case of a null pointer
+    address->stream() << castor::OBJ_Ptr << ((unsigned int)0);
+  } else if (alreadyDone.find(obj) == alreadyDone.end()) {
+    // Case of a pointer to a non streamed object
+    createRep(address, obj, true);
+    // Mark object as done
+    alreadyDone.insert(obj);
+  } else {
+    // case of a pointer to an already streamed object
+    address->stream() << castor::OBJ_Ptr << alreadyDone[obj];
+  }
+}
+
+//------------------------------------------------------------------------------
+// unmarshalObject
+//------------------------------------------------------------------------------
+castor::IObject* castor::io::StreamGCLocalFileCnv::unmarshalObject(castor::io::biniostream& stream,
+                                                                   castor::ObjectCatalog& newlyCreated)
+  throw (castor::exception::Exception) {
+  castor::io::StreamAddress ad(stream, "StreamCnvSvc", SVC_STREAMCNV);
+  castor::IObject* object = createObj(&ad);
+  // Mark object as created
+  newlyCreated.insert(object);
+  return object;
+}
+
