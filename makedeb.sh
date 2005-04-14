4a30f5187ee049559efefd255ef1d1487b3abc2e
Jean-Damien Durand <jdurand@cern.ch>
2005-04-14 09:08:55 +0000

    Explicit list of flags to force to YES, idem for NO

--- makedeb.sh
+++ makedeb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Id: makedeb.sh,v 1.9 2005/03/11 08:44:22 jdurand Exp $
+# $Id: makedeb.sh,v 1.10 2005/04/14 09:08:55 jdurand Exp $
 
 if [ "x${MAJOR_CASTOR_VERSION}" = "x" ]; then
   echo "No MAJOR_CASTOR_VERSION environment variable"
@@ -53,10 +53,18 @@ cd castor-${a}.${b}.${c}
 for this in BuildCommands BuildCommon BuildCupvClient BuildCupvDaemon BuildCupvLibrary BuildGcDaemon BuildHsmTools BuildMonitorClient BuildMonitorServer BuildMonitorLibrary BuildMsgClient BuildMsgServer BuildMsgLibrary BuildNameServerClient BuildNameServerDaemon BuildNameServerLibrary BuildRfioClient BuildRfioServer BuildRfioLibrary BuildRmcServer BuildRmcLibrary BuildJob BuildRtcopyClient BuildRtcopyServer BuildRtcopyLibrary BuildRtcpclientd BuildRtstat BuildStageClient BuildStageDaemon BuildStageLibrary BuildRmMaster BuildRmNode BuildRmClient BuildRmLibrary BuildTapeClient BuildTapeDaemon BuildTapeLibrary BuildTpusage BuildVdqmClient BuildVdqmServer BuildVdqmLibrary BuildVolumeMgrClient BuildVolumeMgrDaemon BuildVolumeMgrLibrary BuildDlfClient BuildDlfDaemon BuildDlfLibrary BuildExpertClient BuildExpertDaemon BuildExpertLibrary BuildRHCpp BuildOraCpp BuildCastorClientCPPLibrary Accounting HasNroff HasCDK UseCupv UseOracle UseVmgr UseScheduler UseExpert UseMaui; do
     perl -pi -e "s/$this.*(YES|NO)/$this\tYES/g" config/site.def
 done
+
+#
+## Force build rules to NO for the others
+#
+for this in Accounting BuildCastorClientCPPLibrary BuildCommands BuildCommon BuildCupvClient BuildCupvDaemon BuildCupvLibrary BuildDlfClient BuildDlfDaemon BuildDlfLibrary BuildExpertClient BuildExpertDaemon BuildExpertLibrary BuildGCCpp BuildHsmTools BuildJob BuildMonitorClient BuildMonitorLibrary BuildMonitorServer BuildMsgClient BuildMsgLibrary BuildMsgServer BuildNameServerClient BuildNameServerDaemon BuildNameServerLibrary BuildOraCpp BuildRHCpp BuildRfioClient BuildRfioLibrary BuildRfioServer BuildRmClient BuildRmLibrary BuildRmMaster BuildRmNode BuildRmcLibrary BuildRmcServer BuildRtcopyClient BuildRtcopyLibrary BuildRtcopyServer BuildRtcpclientd BuildRtstat BuildSchedPlugin BuildStageClient BuildStageDaemon BuildStageLibrary BuildTapeClient BuildTapeDaemon BuildTapeLibrary BuildTpusage BuildVdqmClient BuildVdqmLibrary BuildVdqmServer BuildVolumeMgrClient BuildVolumeMgrDaemon BuildVolumeMgrLibrary HasCDK HasNroff UseCastorOnGlobalFileSystem UseCupv UseExpert UseLsf UseMaui UseOracle UseScheduler UseStorageTank UseVmgr; do
+    perl -pi -e "s/$this.*(YES|NO)/$this\tNO/g" config/site.def
+done
+
 #
 ## Force build rules to NO for a lot of things
 #
-for this in BuildExamples BuildSchedPlugin BuildCppTest ClientLogging HasNis RFIODaemonRealTime UseAdns UseDMSCapi UseHpss UseMtx UseMySQL UseLsf FollowRtLinks BuildSecurity; do
+for this in BuildCppTest BuildExamples BuildGcDaemon BuildRmNode BuildSecurity BuildTest ClientLogging FollowRtLinks HasNis RFIODaemonRealTime UseAdns UseDMSCapi UseHpss UseMtx UseMySQL; do
     perl -pi -e "s/$this.*(YES|NO)/$this\tNO/g" config/site.def
 done
 #
