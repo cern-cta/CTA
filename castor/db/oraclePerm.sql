17690cfc88bfd24db8d41372816ba138db060ea4
Sebastien Ponce <sponcec3@cern.ch>
2005-06-09 09:18:14 +0000

    fixed getUpdateFailed procedure. Still this STUPID PL/SQL feature that you cannot use a variable that is also a column name

--- castor/db/oraclePerm.sql
+++ castor/db/oraclePerm.sql
@@ -1306,10 +1306,10 @@ END;
 
 /* PL/SQL method implementing getUpdateFailed */
 CREATE OR REPLACE PROCEDURE getUpdateFailedProc
-(subReqId IN NUMBER) AS
+(srId IN NUMBER) AS
 BEGIN
   UPDATE SubRequest SET status = 7 -- FAILED
-   WHERE id = subReqId;
+   WHERE id = srId;
 END;
 
 /* PL/SQL method implementing putFailedProc */
