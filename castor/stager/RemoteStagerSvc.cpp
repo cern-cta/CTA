e85052ee2ed0dca490ffed808bcfe413e162f731
Jean-Damien Durand <jdurand@cern.ch>
2005-07-11 09:55:22 +0000

    Moved #include <errno.h> at the beginning, some scope declarations needs it

--- castor/stager/RemoteStagerSvc.cpp
+++ castor/stager/RemoteStagerSvc.cpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: RemoteStagerSvc.cpp,v $ $Revision: 1.47 $ $Release$ $Date: 2005/07/07 12:58:44 $ $Author: sponcec3 $
+ * @(#)$RCSfile: RemoteStagerSvc.cpp,v $ $Revision: 1.48 $ $Release$ $Date: 2005/07/11 09:55:22 $ $Author: jdurand $
  *
  *
  *
@@ -25,6 +25,7 @@
  *****************************************************************************/
 
 // Include Files
+#include <errno.h>
 #include "castor/IAddress.hpp"
 #include "castor/IObject.hpp"
 #include "castor/IService.hpp"
@@ -58,7 +59,6 @@
 #include "castor/rh/StartResponse.hpp"
 #include "castor/exception/NotSupported.hpp"
 #include "castor/exception/Internal.hpp"
-#include <errno.h>
 #include <list>
 
 EXTERN_C char DLL_DECL *getconfent _PROTO((char *, char *, int));
