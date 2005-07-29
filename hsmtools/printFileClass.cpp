774047ef74754967b61dcba07f7f3d4126e25309
Sebastien Ponce <sponcec3@cern.ch>
2005-07-29 15:44:25 +0000

    New version of printFileClass (working)

---
+++ hsmtools/printFileClass.cpp
@@ -0,0 +1,126 @@
+/******************************************************************************
+ *                      printFileClass.c
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile: printFileClass.cpp,v $ $Revision: 1.1 $ $Release$ $Date: 2005/07/29 15:41:13 $ $Author: sponcec3 $
+ *
+ * 
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+#include <iostream>
+#include <castor/BaseObject.hpp>
+#include <castor/stager/FileClass.hpp>
+#include <castor/stager/ICommonSvc.hpp>
+#include <castor/Services.hpp>
+#include <castor/BaseAddress.hpp>
+#include <castor/Constants.h>
+#include <Cgetopt.h>
+
+int help_flag =0;
+
+static struct Coptions longopts[] = {
+  {"help",NO_ARGUMENT,&help_flag,'h'},
+  {NULL, 0, NULL, 0}
+};
+
+void usage(char *cmd) {
+  std::cout << "Usage : " << cmd
+            << " [-h|--help] FileClassName"
+            << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+
+  try {
+    
+    char* progName = argv[0];
+    
+    // Deal with options
+    Coptind = 1;
+    Copterr = 1;
+    int ch;
+    while ((ch = Cgetopt_long(argc,argv,"h",longopts,NULL)) != EOF) {
+      switch (ch) {
+      case 'h':
+        help_flag = 1;
+        break;
+      case '?':
+        usage(progName);
+        exit(1);
+      default:
+        break;
+      }
+    }
+
+    // Display help if required
+    if (help_flag != 0) {
+      usage(progName);
+      return 0;
+    }
+
+    // Check the arguments
+    argc -= Coptind;
+    argv += Coptind;
+    if (argc != 1) {
+      std::cerr << "Error : wrong number of arguments." << std::endl;
+      usage(progName);
+      return(1);
+    }
+    char* fileClassName = argv[0];    
+
+    // Initializing the log
+    castor::BaseObject::initLog("NewStagerLog", castor::SVC_NOMSG);
+    // retrieve a Services object
+    castor::Services* svcs = castor::BaseObject::services();
+    // retrieve the DB service
+    castor::IService* isvc = svcs->service("DB",castor::SVC_DBSTAGERSVC);
+    if (0 == isvc) {
+      std::cerr << "Unable to retrieve Stager Service." << std::endl
+                << "Please check your configuration." << std::endl;
+      exit(1);
+    }
+    castor::stager::ICommonSvc* commonSvc =
+      dynamic_cast<castor::stager::ICommonSvc*>(isvc);
+
+    // get the FileClass
+    castor::stager::FileClass* fileClass = commonSvc->selectFileClass(fileClassName);
+    if (0 == fileClass) {
+      std::cerr << "FileClass " << fileClassName << " does not exist"
+                << std::endl;
+      if (0 != commonSvc) commonSvc->release();
+      return(0);
+    }
+    
+    // print out everything
+    fileClass->print();
+  
+    // cleanup
+    delete fileClass;
+    if (0 != commonSvc) commonSvc->release();
+
+  } catch (castor::exception::Exception e) {
+    std::cerr << "Caught exception :\n"
+              << e.getMessage().str() << std::endl;    
+  }
+  
+}
+
+
+
