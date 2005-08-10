e7c9c0b818c51153fbbb9aeb149d620a94ded0b7
Jean-Damien Durand <jdurand@cern.ch>
2005-08-10 09:07:58 +0000

    strerror() wrapper that guarantees errno is intouched

---
+++ common/Cstrerror.c
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2005 by CERN/IT/ADC/CA 
+ * All rights reserved
+ */
+
+/*
+ * $Id: Cstrerror.c,v 1.1 2005/08/10 09:07:58 jdurand Exp $
+ */
+
+#include <string.h>
+#include <errno.h>
+#include "osdep.h"
+#include "Cstrerror.h"
+#include "Cglobals.h"
+
+#ifndef lint
+static char sccsid[] =  "@(#)$RCSfile: Cstrerror.c,v $ $Revision: 1.1 $ $Date: 2005/08/10 09:07:58 $ CERN IT-ADC/CA Jean-Damien Durand";
+#endif
+
+#define INTERNAL_BUFLEN 1023
+
+char *static_message = "Cstrerror internal error";
+
+static int Cstrerror_bufp_key = 0;
+static int Cstrerror_buflen_key = 0;
+static int Cstrerror_getbuf _PROTO((char **, int *));
+static int Cstrerror_setbuf _PROTO((char *, int));
+
+static int Cstrerror_getbuf(buffer, buflen)
+     char **buffer;
+     int *buflen;
+{
+  char **bufp;
+  int *buflenp;
+
+  Cglobals_get (&Cstrerror_bufp_key, (void **)&bufp, sizeof(char *));
+  Cglobals_get (&Cstrerror_buflen_key, (void **)&buflenp, sizeof(int));
+  if (bufp == NULL || buflenp == NULL) {
+    return(-1);
+  }
+  *buffer = *bufp;
+  *buflen = *buflenp;
+  return(0);
+}
+
+static int Cstrerror_setbuf(buffer, buflen)
+     char *buffer;
+     int buflen;
+{
+  char **bufp;
+  int *buflenp;
+
+  Cglobals_get (&Cstrerror_bufp_key, (void **)&bufp, sizeof(char *));
+  Cglobals_get (&Cstrerror_buflen_key, (void **)&buflenp, sizeof(int));
+  if (bufp == NULL || buflenp == NULL) {
+    return(-1);
+  }
+  *bufp = buffer;
+  *buflenp = buflen;
+  return(0);
+}
+
+char DLL_DECL *Cstrerror(code)
+     int code;
+{
+  char *bufp;
+  int buflen = INTERNAL_BUFLEN+1;
+  char *message;
+
+  /* Get thread-specific buffers */
+  if (Cstrerror_getbuf(&bufp,&buflen) != 0) {
+    errno = code;
+    return(static_message);
+  }
+  if (bufp == NULL) {
+    /* Assign a thread-specific buffer - a priori 1023+1 characters will always be enough */
+    if ((bufp = (char *) malloc(buflen)) == NULL) {
+      errno = code;
+      return(static_message);
+    }
+    if (Cstrerror_setbuf(bufp,buflen) != 0) {
+      errno = code;
+      return(static_message);
+    }
+  }
+
+#if linux
+  /* Retreive error message in bufp */
+  if (strerror_r(code,bufp,(size_t) (buflen-1)) != 0) {
+    errno = code;
+    return(static_message);
+  }
+  /* Who knows */
+  bufp[buflen] = '\0';
+#else
+  /* TODO */
+  if ((message = strerror(code)) == NULL) {
+    /* Should never happen */
+    errno = code;
+    return(static_message);
+  }
+  /* Keep a copy anyway */
+  strncpy(bufp, message, buflen-1);
+  bufp[buflen] = '\0';
+#endif
+
+  errno = code;
+  return(bufp);
+}
---
+++ h/Cstrerror.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2005 by CERN/IT/ADC/CA 
+ * All rights reserved
+ */
+
+/*
+ * $Id: Cstrerror.h,v 1.1 2005/08/10 09:07:58 jdurand Exp $
+ */
+
+#ifndef __Cstrerror_h
+#define __Cstrerror_h
+
+#include "osdep.h"
+
+EXTERN_C char DLL_DECL *Cstrerror _PROTO((int));
+
+#endif /* __Cstrerror_h */
