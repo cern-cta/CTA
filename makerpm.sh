f82b82066b98586479cce5b8146998a4347c86db
Jean-Damien Durand <jdurand@cern.ch>
2005-09-20 12:05:04 +0000

    Do not assume existence of fakeroot when building rpms

--- makerpm.sh
+++ makerpm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Id: makerpm.sh,v 1.1 2005/05/02 08:08:27 jdurand Exp $
+# $Id: makerpm.sh,v 1.2 2005/09/20 12:05:04 jdurand Exp $
 
 #
 ## Do tarball
@@ -22,24 +22,7 @@ d=`echo ${MINOR_CASTOR_VERSION} | sed 's/.*\.//g'`
 
 echo "### INFO ### Creating rpms"
 
-#
-## Great - the install is doing chown - which is allowed
-## only as root
-## Is there any fakeroot by change in here?
-#
-fakeroot=`which fakeroot 2>/dev/null`
-if [ `id -u` -ne 0 ]; then
-    if [ -z "${fakeroot}" ]; then
-	echo "### WARNING ### The build is likely to fail if you are not root"
-	sleep 1
-	rpmbuild -ta castor-${a}.${b}.${c}.tar.gz
-    else
-	echo "### INFO ### Using fakeroot rpmbuild ..."
-	fakeroot rpmbuild -ta castor-${a}.${b}.${c}.tar.gz
-    fi
-else
-  rpmbuild -ta castor-${a}.${b}.${c}.tar.gz
-fi
+rpmbuild -ta castor-${a}.${b}.${c}.tar.gz
 thisstatus=$?
 
 exit $thisstatus
