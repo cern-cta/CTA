6da6e7ce3c5d6de821340fc3280ea50c2dd6eaae
Giuseppe Lo Presti <itglp@cern.ch>
2005-11-11 10:36:30 +0000

    A couple of Oracle statement for DBAs.

---
+++ castor/db/analyze/admin.sql
@@ -0,0 +1,16 @@
+as oracle user:
+$ sqlplus "/ as sysdba"
+
+-- to create a new user:
+select * from dba_tablespaces;
+
+create user username identified by passwd default tablespace tsname quota 300M on tsname;
+
+grant designer to username;
+
+
+-- to take a snapshot of the db for monitoring:
+begin dbms_workload_repository.create_snapshot; end;
+
+
+
