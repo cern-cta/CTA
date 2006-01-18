a184c8ac96477af51b738e1a3f16f7aec39305af
Felix Ehm <felixehm@cern.ch>
2006-01-18 14:21:25 +0000

    added new files for repack project

---
+++ castor/repack/DatabaseHelper.cpp
@@ -0,0 +1,78 @@
+/******************************************************************************
+ *                      castor/repack/DatabaseHelper.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile$ $Revision$ $Release$ $Date$ $Author$
+ *
+ * 
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+// Include Files
+#include "castor/repack/DatabaseHelper.hpp"
+#include "castor/io/StreamRepackRequestCnv.hpp"
+
+
+
+
+//------------------------------------------------------------------------------
+// Constructor
+//------------------------------------------------------------------------------
+castor::repack::DatabaseHelper::DatabaseHelper() throw() {
+};
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+castor::repack::DatabaseHelper::~DatabaseHelper() throw() {
+};
+
+//------------------------------------------------------------------------------
+// store_Request 
+//------------------------------------------------------------------------------
+int castor::repack::DatabaseHelper::store_Request(castor::repack::RepackRequest* rreq)
+{
+	stage_trace(2,"Storing Request in DB" );
+
+	castor::BaseAddress ad;
+	ad.setCnvSvcName("DbCnvSvc");
+	ad.setCnvSvcType(castor::SVC_DBCNV);
+
+	
+	try {
+	  
+	    // Store files for RepackRequest
+		if ( rreq != NULL ) {
+		  svcs()->createRep(&ad, rreq, false);
+		  svcs()->fillRep(&ad, rreq, OBJ_RepackSegment, false);
+		}
+	
+		svcs()->commit(&ad);
+		// "Request stored in DB" message
+		castor::dlf::Param params[] =
+		  {castor::dlf::Param("ID", rreq->id())};
+	    castor::dlf::dlf_writep(nullCuuid, DLF_LVL_SYSTEM, 12, 1, params);
+	
+	  } catch (castor::exception::Exception e) {
+	    svcs()->rollback(&ad);
+	    throw e;
+	  }
+
+	return 0;
+}
---
+++ castor/repack/RepackCommonHeader.hpp
@@ -0,0 +1,11 @@
+#ifndef REPACKCOMMONHEADER
+#define REPACKCOMMONHEADER
+
+#include <iostream>
+#include <string>
+#include "castor/exception/Exception.hpp"
+#include "castor/exception/Internal.hpp"
+#include "h/stager_client_api_common.h"   // for stage_trace("..")
+#include "castor/dlf/Dlf.hpp"
+#include "castor/Constants.hpp"
+#endif //REPACKCOMMONHEADER
