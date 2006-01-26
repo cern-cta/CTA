3859ff8b19ecb6328e4464e58cee6e56a5b8fe6f
Benjamin Couturier <bcouturi@cern.ch>
2006-01-26 15:28:45 +0000

    Added new limit for name server with ACLs

--- mediachanger/castorrmc/h/Castor_limits.h
+++ mediachanger/castorrmc/h/Castor_limits.h
@@ -17,6 +17,7 @@
 	/* all maximum lengths defined below do not include the trailing null */
 #define CA_MAXDENNUM      27    /*  ...  */
 
+#define CA_MAXACLENTRIES 300	/* maximum number of ACL entries for a file/dir */
 #define CA_MAXCLASNAMELEN 15	/* maximum length for a fileclass name */
 #define	CA_MAXCOMMENTLEN 255	/* maximum length for user comments in metadata */
 #define	CA_MAXDENFIELDS    9	/* maximum number of density values in devinfo */
@@ -27,6 +28,7 @@
 #define	CA_MAXFIDLEN      17	/* maximum length for a fid (DSN) */
 #define	CA_MAXFSEQLEN     14	/* maximum length for a fseq string */
 #define	CA_MAXGRPNAMELEN   2	/* maximum length for a group name */
+#define	CA_MAXGUIDLEN     36	/* maximum length for a guid */
 #define CA_MAXHOSTNAMELEN 63	/* maximum length for a hostname */
 #define	CA_MAXLBLTYPLEN    3	/* maximum length for a label type */
 #define CA_MAXLINELEN   1023	/* maximum length for a line in a log */
@@ -39,15 +41,20 @@
 #define	CA_MAXNBDRIVES    32	/* maximum number of tape drives per server */
 #define	CA_MAXPATHLEN   1023	/* maximum length for a pathname */
 #define CA_MAXPOOLNAMELEN 15	/* maximum length for a pool name */
+#define CA_MAXPROTOLEN     7	/* maximum length for a protocol name */
 #define	CA_MAXRBTNAMELEN  17	/* maximum length for a robot name */
 #define	CA_MAXRECFMLEN     3	/* maximum length for a record format */
 #define CA_MAXREGEXPLEN   63    /* Maximum length for a regular expression */
+#define CA_MAXCSECNAMELEN 512	/* Maximum length for a Csec authorization id */
+#define CA_MAXCSECPROTOLEN 20	/* Maximum length for a Csec mechanism */
+#define	CA_MAXSFNLEN    1103	/* maximum length for a replica */
 #define CA_MAXSHORTHOSTLEN 10	/* maximum length for a hostname without domain */
 #define	CA_MAXSNLEN       24	/* maximum length for a cartridge serial nb */
 #define	CA_MAXSTGRIDLEN   77	/* maximum length for a stager full request id */
 				/* must be >= nb digits in CA_MAXSTGREQID +
 				   CA_MAXHOSTNAMELEN + 8 */
 #define	CA_MAXSTGREQID 999999	/* maximum value for a stager request id */
+#define	CA_MAXSYMLINKS     5	/* maximum number of symbolic links */
 #define	CA_MAXTAGLEN     255	/* maximum length for a volume tag */
 #define	CA_MAXTAPELIBLEN   8	/* maximum length for a tape library name */
 #define	CA_MAXUNMLEN       8	/* maximum length for a drive name */
@@ -57,8 +64,6 @@
 #define CA_MAXCKSUMNAMELEN 15    /* maximum length for a checksum algorithm name */
 #define CA_MAXDMPROTNAMELEN 15    /* maximum length for Disk Mover protocol name */
 #define CA_MAXJOBIDLEN     36   /* Maximum length for the representation of the Cuuid */
-#define CA_MAXCSECNAMELEN    512   /* Maximum length for a client or service name */
-#define CA_MAXCSECPROTOLEN 20   /* Maximum length for a Csec authentication protocol name */
 #define CA_MAXUSERTAGLEN    63   /* Maximum length for a user tag (stage request) */
 
 
