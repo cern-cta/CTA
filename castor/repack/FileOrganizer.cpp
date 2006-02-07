a0526a2e41d7d26b560778a1484bc1e3af2316f0
Felix Ehm <felixehm@cern.ch>
2006-02-07 20:02:53 +0000

    Added new Messages for DLF If a request job arrives, now the segments are added and the subrequest updated in DB removed mem lack

--- castor/repack/FileOrganizer.cpp
+++ castor/repack/FileOrganizer.cpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: FileOrganizer.cpp,v $ $Revision: 1.4 $ $Release$ $Date: 2006/02/03 15:49:31 $ $Author: felixehm $
+ * @(#)$RCSfile: FileOrganizer.cpp,v $ $Revision: 1.5 $ $Release$ $Date: 2006/02/07 20:02:53 $ $Author: felixehm $
  *
  *
  *
@@ -59,6 +59,8 @@ FileOrganizer::~FileOrganizer() throw() {
 //------------------------------------------------------------------------------
 void FileOrganizer::run(void *param) throw() {
 	
+	/* this is just to see, if there is a enviroment variable for polling time
+	 */
 	char* ptime;
 	unsigned long polling_time;
 	if ( (ptime=getenv(REPACK_POLL))!=0 ){
@@ -75,23 +77,26 @@ void FileOrganizer::run(void *param) throw() {
 	}else
 		polling_time = 10;
 	
+	/* now, here we go */
 	while ( m_run )
 	{
-		/* Lets see, if good old pal DB has a Job for us :*/
+		/* Lets see, if good old pal DB has a Job for us */
 		RepackSubRequest* sreq = m_dbhelper->requestToDo();
+		
 		if ( sreq != NULL ){
+			stage_trace(3,"New Job! %s",sreq->vid().c_str());
 			castor::dlf::Param params[] =
 			{castor::dlf::Param("VID", sreq->vid()),
 			 castor::dlf::Param("ID", sreq->id()),
 			 castor::dlf::Param("STATUS", sreq->status())};
-			castor::dlf::dlf_writep(nullCuuid, DLF_LVL_SYSTEM, 18, 3, params);
+			castor::dlf::dlf_writep(nullCuuid, DLF_LVL_SYSTEM, 22, 3, params);
 			/* stage the files */
 			stage_files(sreq);
 			/* we are responsible for this object, so we have to delete it */
 			delete sreq;
 		}
 		else
-			std::cerr << "Running, but no Request!" << std::endl;
+			std::cerr << "Running, but no Job!" << std::endl;
 		
 		sleep(polling_time);
 	}
@@ -136,47 +141,64 @@ void FileOrganizer::stage_files(RepackSubRequest* sreq) throw() {
 	struct stage_prepareToGet_fileresp* response;
 	/*-------------------------*/
 
-	flh.getFileListSegs(sreq);				/* TODO: retrieve the segments from NS to be up-to-date
-											this is still neccessary, because right now we don't save the segments
-											in the DB - so not really todo!*/
-	filenamelist = flh.getFileList(sreq);	/* and now all parent_fileids */
+	stage_trace(3,"Updating SubRequest with its segs");
+	
+	flh.getFileListSegs(sreq);				/* get the Segs for this tape !*/
 
+	/* 
+	 * TODO:We have to check for space on the DiscCache !
+	u_signed64 free = stager_free();
+	if ( sreq->xsize() > free ){
+		stage_trace(3,"FileOrganizer::stage_files(..): Not enough space left for this job! Skipping..");
+		castor::dlf::Param params[] =
+		{castor::dlf::Param("Requested space", sreq->xsize()),
+		 castor::dlf::Param("Available space", free)}
+		castor::dlf::dlf_writep(nullCuuid, DLF_LVL_ERROR, 21, 2, params);
+		return;
+	}
+	*/
+
+	filenamelist = flh.getFileList(sreq);	/* and now all parent_fileids */
 	struct stage_prepareToGet_filereq requests [filenamelist->size()] ;
 
-
+	stage_trace(3,"Now, get the paths.");
 
 	/* now call the stager */
 	for (i=0; i < filenamelist->size();i++) {
 		Cns_getpath("castorns.cern.ch",filenamelist->at(i),path);	//TODO: remove hardcoded ns name
-		stage_trace(3,"%s", path);
-        requests[i].filename = path;
-        requests[i].protocol = "repack";		// for the stager to recognize
-        										// that this is a special call
-      }
+		//stage_trace(3,"%s", path);
+		requests[i].filename = path;
+		requests[i].protocol = "repack";		// for the stager to recognize
+											// that this is a special call
+	}
       
-        // Before calling the stager, set the status of all the subrequests to in progress
-        stage_trace(3,"Updating Request to STAGING");
-		sreq->setStatus(SUBREQUEST_STAGING);
-		m_dbhelper->updateRep(sreq);
-		stage_trace(3,"Now staging");
-      //rc = stage_prepareToGet(NULL, requests, filenamelist->size(), &response, &nbresps, &reqId, NULL);
+        
+	// Before calling the stager, set the status of all the subrequests to in progress
+	stage_trace(3,"Updating Request to STAGING and add its segs");
+	castor::dlf::dlf_writep(nullCuuid, DLF_LVL_SYSTEM, 25, 0, NULL);
+	sreq->setStatus(SUBREQUEST_STAGING);
+	m_dbhelper->updateSubRequest(sreq);
+	
+	stage_trace(3,"Now staging");
+	castor::dlf::dlf_writep(nullCuuid, DLF_LVL_SYSTEM, 26, 0, NULL);
+	//rc = stage_prepareToGet(NULL, requests, filenamelist->size(), &response, &nbresps, &reqId, NULL);
 
       
-      if ( rc != 0 ) {
-      	castor::exception::Internal ex;
+	if ( rc != 0 ) {
+		castor::exception::Internal ex;
 		ex.getMessage() << "FileOrganizer::stage_files(..):" 
 						<< sstrerror(serrno) << std::endl;
 		castor::dlf::Param params[] =
 		{castor::dlf::Param("Standard Message", sstrerror(ex.code())),
-    	 castor::dlf::Param("Precise Message", ex.getMessage().str())};
-        castor::dlf::dlf_writep(nullCuuid, DLF_LVL_ERROR, 15, 2, params);
-	    throw ex;
-      }
+		 castor::dlf::Param("Precise Message", ex.getMessage().str())};
+		castor::dlf::dlf_writep(nullCuuid, DLF_LVL_ERROR, 21, 2, params);
+		throw ex;
+	}
+    filenamelist->clear();
+	delete filenamelist;
+    //TODO: if staging this has to be uncommented !    delete response;
       
-      delete filenamelist;
-      //TODO: if stageing this has to be uncommented !    delete response;
-      
-	  // the Request has now status SUBREQUEST_STAGING
+	// the Request has now status SUBREQUEST_STAGING
 }
 		
 		
