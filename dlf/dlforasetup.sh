b3a3b2a521e60c3875871a2173632a8b6591830c
Giuseppe Lo Presti <itglp@cern.ch>
2006-02-10 11:13:47 +0000

    Added Repack in the list of facilities.

--- dlf/dlforasetup.sh
+++ dlf/dlforasetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-## $Id: dlforasetup.sh,v 1.5 2005/12/09 10:08:15 sponcec3 Exp $
+## $Id: dlforasetup.sh,v 1.6 2006/02/10 11:13:47 itglp Exp $
 #
 
 killall -9 dlfserver
@@ -37,5 +37,6 @@ dlfenterfacility -F Vdqm -n 11
 dlfenterfacility -F rfio -n 12
 dlfenterfacility -F SRMServer -n 13
 dlfenterfacility -F SRMDaemon -n 14
+dlfenterfacility -F Repack -n 15
 
 dlflistfacility
