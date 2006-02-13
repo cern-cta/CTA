f568d14c642245054b244bb2de21f7a32f2546d0
Giuseppe Lo Presti <itglp@cern.ch>
2006-02-13 14:50:56 +0000

    Further minor fix.

--- shlib/makeshlib.sh
+++ shlib/makeshlib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# $Id: makeshlib.sh,v 1.2 2006/02/13 12:57:46 itglp Exp $
+# $Id: makeshlib.sh,v 1.3 2006/02/13 14:50:56 itglp Exp $
 
 if (( $# < 3 )); then
 	echo This script is internally executed by make to build the shared libraries.
@@ -14,12 +14,13 @@ cmd=$1
 target=$2
 echo  building $target.$MAJOR_CASTOR_VERSION
 
-# this nasty trick allows getting all the argv[] from the 3rd one on
+# this nasty trick allows getting all the args from the 3rd one on
 shift
 shift
 cd tmp ; \
-`echo $cmd -Wl,-soname,$target.$MAJOR_CASTOR_VERSION -o ../$target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION *.o $@` ; \
+$cmd -Wl,-soname,$target.$MAJOR_CASTOR_VERSION -o ../$target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION *.o $@ ; \
 cd ..
+
 rm -rf tmp
 rm -f $target.$MAJOR_CASTOR_VERSION
 ln -s $target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION $target.$MAJOR_CASTOR_VERSION
