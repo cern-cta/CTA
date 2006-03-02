7eb3f278747427553fa08b5fd642160dfec19d31
Giuseppe Lo Presti <itglp@cern.ch>
2006-03-02 09:10:18 +0000

    Fixed rule to make install shared libraries with correct versioning.

--- shlib/makeshlib.sh
+++ shlib/makeshlib.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-# $Id: makeshlib.sh,v 1.3 2006/02/13 14:50:56 itglp Exp $
+# $Id: makeshlib.sh,v 1.4 2006/03/02 09:10:17 itglp Exp $
 
 if (( $# < 3 )); then
-	echo This script is internally executed by make to build the shared libraries.
+	echo This script is internally executed by make to build or install the shared libraries.
 	exit 1
 fi
 
 if [ "x$MAJOR_CASTOR_VERSION" = "x" ]; then MAJOR_CASTOR_VERSION=__MAJOR_CASTOR_VERSION__ ; fi
 if [ "x$MINOR_CASTOR_VERSION" = "x" ]; then MINOR_CASTOR_VERSION=__MINOR_CASTOR_VERSION__ ; fi
+
+if [ "$1" = "install" ]; then
+
+target=$3/lib$2.so
+echo  installing lib$2.so.$MAJOR_CASTOR_VERSION
+
+install lib$2.so.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION $target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION
+rm -f $target.$MAJOR_CASTOR_VERSION
+ln -s $target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION $target.$MAJOR_CASTOR_VERSION
+rm -f $target
+ln -s $target.$MAJOR_CASTOR_VERSION $target
+
+else
 
 cmd=$1
 target=$2
@@ -26,3 +39,6 @@ rm -f $target.$MAJOR_CASTOR_VERSION
 ln -s $target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION $target.$MAJOR_CASTOR_VERSION
 rm -f $target
 ln -s $target.$MAJOR_CASTOR_VERSION $target
+
+fi
+
