c46ec0e7f18c480095a164d1ae983769991dcabe
Giuseppe Lo Presti <itglp@cern.ch>
2006-03-10 13:50:39 +0000

    One more fix in the rule to make install the shared libraries.

--- shlib/makeshlib.sh
+++ shlib/makeshlib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# $Id: makeshlib.sh,v 1.4 2006/03/02 09:10:17 itglp Exp $
+# $Id: makeshlib.sh,v 1.5 2006/03/10 13:50:38 itglp Exp $
 
 if (( $# < 3 )); then
 	echo This script is internally executed by make to build or install the shared libraries.
@@ -10,21 +10,21 @@ fi
 if [ "x$MAJOR_CASTOR_VERSION" = "x" ]; then MAJOR_CASTOR_VERSION=__MAJOR_CASTOR_VERSION__ ; fi
 if [ "x$MINOR_CASTOR_VERSION" = "x" ]; then MINOR_CASTOR_VERSION=__MINOR_CASTOR_VERSION__ ; fi
 
+target=$2
+
 if [ "$1" = "install" ]; then
 
-target=$3/lib$2.so
-echo  installing lib$2.so.$MAJOR_CASTOR_VERSION
+echo  installing $target.$MAJOR_CASTOR_VERSION
 
-install lib$2.so.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION $target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION
-rm -f $target.$MAJOR_CASTOR_VERSION
-ln -s $target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION $target.$MAJOR_CASTOR_VERSION
-rm -f $target
-ln -s $target.$MAJOR_CASTOR_VERSION $target
+install $target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION $3.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION
+rm -f $3.$MAJOR_CASTOR_VERSION
+ln -s $target.$MAJOR_CASTOR_VERSION.$MINOR_CASTOR_VERSION $3.$MAJOR_CASTOR_VERSION
+rm -f $3
+ln -s $target.$MAJOR_CASTOR_VERSION $3
 
 else
 
 cmd=$1
-target=$2
 echo  building $target.$MAJOR_CASTOR_VERSION
 
 # this nasty trick allows getting all the args from the 3rd one on
