3cda8715944e005476adf8caf75c3d9060d11326
Giuseppe Lo Presti <itglp@cern.ch>
2006-04-07 16:46:22 +0000

    Added a query to see what has been done by the script.

--- castor/db/analyze/cleanDiskCopyReplicas.sql
+++ castor/db/analyze/cleanDiskCopyReplicas.sql
@@ -10,18 +10,28 @@ begin
            and Diskcopy.status = 0    -- STAGED
          group by castorfile.id)
         where c > 1) loop
-    update diskcopy set status = 8 where id = (   -- GCCANDIDATE
+    update diskcopy set status = 88 where id = (
     select id from (
         select dc.id
           from diskcopy dc, filesystem fs
          where dc.status = 0
            and dc.castorfile = cf.id
            and dc.filesystem = fs.id
-         order by fs.free asc
+         order by (fs.totalsize - fs.free) desc
          )
      where ROWNUM < 2);
   end loop;
 end;
+
+-- To look at where were the removed replicas:
+select ds.name, count(*)
+  from diskcopy dc, filesystem fs, diskserver ds
+ where dc.status = 88
+   and dc.filesystem = fs.id 
+   and fs.diskserver = ds.id
+ group by ds.name order by ds.name;
+
+update diskcopy set status = 8 where status = 88;   -- GCCANDIDATE
 
 -- To look at the current distribution of replicas in diskcopies:
 select count(*) as n, c from (
