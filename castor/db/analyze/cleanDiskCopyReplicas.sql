0205d74417f97b5ad352bb8495cb2fb1a2dec2f3
Giuseppe Lo Presti <itglp@cern.ch>
2006-04-10 16:39:01 +0000

    Improved script taking into account the number of diskcopies per filesystem as a metric to decide wich diskcopy to drop. The resulting diskcopy distribution can still be improved.

--- castor/db/analyze/cleanDiskCopyReplicas.sql
+++ castor/db/analyze/cleanDiskCopyReplicas.sql
@@ -1,43 +1,56 @@
 -- This script garbage collects diskcopies that are replicas:
--- only the diskcopy in the emptiest filesystem is kept.
+-- only the diskcopy in the filesystem with least copies is kept.
 -- Note that for files with more than 2 replicas you have to run it more times
 begin
   for cf in (
         select id from
-        (select count(*) as c, castorfile.id
-          from Diskcopy, Castorfile
-         where Diskcopy.castorfile = castorfile.id
-           and Diskcopy.status = 0    -- STAGED
-         group by castorfile.id)
+		(select count(*) as c, castorfile.id
+		  from Diskcopy, Castorfile
+		 where Diskcopy.castorfile = castorfile.id
+		   and Diskcopy.status = 0    -- STAGED
+		 group by castorfile.id)
         where c > 1) loop
+
     update diskcopy set status = 88 where id = (
-    select id from (
-        select dc.id
-          from diskcopy dc, filesystem fs
-         where dc.status = 0
-           and dc.castorfile = cf.id
-           and dc.filesystem = fs.id
-         order by (fs.totalsize - fs.free) desc
-         )
-     where ROWNUM < 2);
+        select dc1.id
+          from diskcopy dc1
+         where dc1.castorfile = cf.id
+	   and dc1.status = 0      -- STAGED
+           and dc1.filesystem = (
+		   select fsid from (
+		        -- here we select the filesystems and their diskcopy count
+			select dccount.filesystem as fsid
+			  from diskcopy dc, filesystem fs, diskcopy dccount
+			 where dc.castorfile = cf.id
+			   and dc.status = 0     -- STAGED
+			   and dc.filesystem = fs.id
+			   and dccount.filesystem = fs.id
+			 group by dccount.filesystem
+			 order by count(dccount.id) desc
+			)
+		   where rownum < 2
+		   )
+	   and rownum < 2
+	);
   end loop;
 end;
 
--- To look at where were the removed replicas:
-select ds.name, count(*)
-  from diskcopy dc, filesystem fs, diskserver ds
- where dc.status = 88
-   and dc.filesystem = fs.id 
-   and fs.diskserver = ds.id
- group by ds.name order by ds.name;
+update diskcopy set status = 8 where status = 88;
+select count(*) from diskcopy where status = 88;
 
-update diskcopy set status = 8 where status = 88;   -- GCCANDIDATE
-
--- To look at the current distribution of replicas in diskcopies:
+-- To look at the current distribution of diskcopies' replicas:
 select count(*) as n, c from (
     select count(*) as c
       from Diskcopy, Castorfile
-     where Diskcopy.castorfile = castorfile.id
-       and Diskcopy.status = 0    -- STAGED
+     where Diskcopy.status = 0
+       and Diskcopy.castorfile = castorfile.id
      group by castorfile.id)
   group by c order by c;
+
+-- To look at the current distribution of diskcopies in filesystems
+select count(*) as c, dc.filesystem, diskserver.name
+  from diskcopy dc, filesystem, diskserver
+ where (dc.status = 0)
+   and dc.filesystem = filesystem.id
+   and filesystem.diskserver = diskserver.id
+ group by dc.filesystem, diskserver.name order by c desc;
