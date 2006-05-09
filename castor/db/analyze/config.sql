dbbf62dbe40c57ac2f21793ffcba8c8128278a49
Giuseppe Lo Presti <itglp@cern.ch>
2006-05-09 10:08:18 +0000

    Enhanced query on the file systems

--- castor/db/analyze/config.sql
+++ castor/db/analyze/config.sql
@@ -4,22 +4,25 @@
 -----------------------------------------------------------------------------
 
 -- Status of fileSystems and correspondent diskServers/diskPools:
-SELECT fs.id, fs.totalsize/1048576000 as totsize, fs.free/1048576000 as free, fs.minfreespace,
-       fs.maxfreespace, fs.mountpoint, fs.status, diskserver.name as diskserver,
+SELECT fs.id, fs.totalsize/1048576000 as totsize, fs.free/1048576000 as free, (fs.totalsize-fs.free)*100.0/fs.totalsize as occupancy,
+       fs.minfreespace, fs.maxfreespace, fs.status as fsStatus, diskserver.name as diskserver, fs.mountpoint,
        diskserver.status as dsStatus, diskpool.name as diskpool
   FROM filesystem fs, diskserver, diskpool
- WHERE fs.diskserver = diskserver.id AND fs.diskpool = diskpool.id;
-
+ WHERE fs.diskserver = diskserver.id AND fs.diskpool = diskpool.id
+ ORDER BY diskpool;
 
 SELECT * FROM diskserver;
 SELECT * FROM diskpool;
+SELECT * FROM fileclass;
 
 -- configuration of SvcClasses and associated DiskPools&TapePools:
 SELECT svcclass.*, diskpool.name as diskpool FROM svcclass, diskpool2svcclass, diskpool
  WHERE diskpool2svcclass.child = svcclass.id and diskpool2svcclass.parent = diskpool.id;
 
-SELECT dp.name as diskpool, count(*) as nbFS FROM diskpool dp, filesystem fs
- WHERE fs.diskpool(+) = dp.id group by dp.name;
-
 SELECT svcclass.*, tapepool.name as tapepool FROM svcclass, svcclass2tapepool, tapepool
  WHERE svcclass2tapepool.parent = svcclass.id and svcclass2tapepool.child = tapepool.id;
+
+-- number of fileSystems per diskPool
+SELECT dp.name as diskpool, count(*) as nbFS 
+  FROM diskpool dp, filesystem fs
+ WHERE fs.diskpool = dp.id GROUP BY dp.name;
