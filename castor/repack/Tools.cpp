3756f41d0894a43e3ffff3284f6ab9b76565964e
Felix Ehm <felixehm@cern.ch>
2006-05-22 09:15:37 +0000

    Added new function which frees a stager response

--- castor/repack/Tools.cpp
+++ castor/repack/Tools.cpp
@@ -1,22 +1,39 @@
+
+
+
 #include "Tools.hpp"
 
 namespace castor {
 	namespace repack {
 //------------------------------------------------------------------------------
-// Helper for shit Cuuid structs
+// Helper for shit Cuuid C structs
 //------------------------------------------------------------------------------
-std::string Cuuidtostring(_Cuuid_t *cuuid){
-		char buf[CUUID_STRING_LEN+1];
-	    Cuuid2string(buf, CUUID_STRING_LEN+1, cuuid);
-	    std::string tmp (buf,CUUID_STRING_LEN);
-	    return tmp;
-}
-_Cuuid_t stringtoCuuid(std::string strcuuid){
-	_Cuuid_t cuuid;
-	string2Cuuid(&cuuid, (char*)strcuuid.c_str());
-	return cuuid;
-}
-//-----------------------------------------------------------------------------
+  std::string Cuuidtostring(_Cuuid_t *cuuid){
+		  char buf[CUUID_STRING_LEN+1];
+	     Cuuid2string(buf, CUUID_STRING_LEN+1, cuuid);
+	     std::string tmp (buf,CUUID_STRING_LEN);
+	     return tmp;
+  }
+  _Cuuid_t stringtoCuuid(std::string strcuuid){
+	 _Cuuid_t cuuid;
+	 string2Cuuid(&cuuid, (char*)strcuuid.c_str());
+	 return cuuid;
+  }
+  //-----------------------------------------------------------------------------
+  
+
+//------------------------------------------------------------------------------
+// Helper for cleaning the stager file responses
+//------------------------------------------------------------------------------  
+  void free_stager_response(struct  stage_filequery_resp* resp){
+    free( resp->filename);
+    free( resp->castorfilename);
+    free( resp->diskserver);
+    free( resp->poolname);
+    if ( resp->errorCode) free( resp->errorMessage);
+    //free (resp);
+  }
+
 
 	}
 }
--- castor/repack/Tools.hpp
+++ castor/repack/Tools.hpp
@@ -3,11 +3,12 @@
 
 #include <string>
 #include "Cuuid.h"
-
+#include "stager_client_api.h"
 namespace castor {
 	namespace repack {
 		std::string Cuuidtostring(_Cuuid_t *cuuid);
 		_Cuuid_t stringtoCuuid(std::string strcuuid);
+    void free_stager_response(struct  stage_filequery_resp* resp);
 	}
 }
 #endif
