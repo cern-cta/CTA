6f7e0bea9df790d3781928046c835b6c659e81c1
Sebastien Ponce <sponcec3@cern.ch>
2006-06-09 13:15:39 +0000

    Fix (again !) fsupdateFileClosed

--- castor/db/oracleJob.sql
+++ castor/db/oracleJob.sql
@@ -1,6 +1,6 @@
 /*******************************************************************
  *
- * @(#)$RCSfile: oracleJob.sql,v $ $Revision: 1.271 $ $Release$ $Date: 2006/06/08 14:45:00 $ $Author: sponcec3 $
+ * @(#)$RCSfile: oracleJob.sql,v $ $Revision: 1.272 $ $Release$ $Date: 2006/06/09 13:15:39 $ $Author: sponcec3 $
  *
  * This file contains SQL code that is not generated automatically
  * and is inserted at the end of the generated code
@@ -497,7 +497,7 @@ CREATE OR REPLACE PROCEDURE updateFsFileClosed
 (fs IN INTEGER, reservation IN INTEGER, fileSize IN INTEGER) AS
   deviation NUMBER;
   ds INTEGER;
-  unused INTEGER;
+  unused "numList";
 BEGIN
  /* We have to do this first in order to take locks on all the filesystems
     of the DiskServer in an atomical way. Otherwise, the fact that
@@ -505,7 +505,7 @@ BEGIN
     leads to a dead lock if 2 threads are running this in parallel :
     they will both lock one filesystem to start with and try to get the
     others locks afterwards. */
- SELECT id into unused FROM FileSystem WHERE diskServer = ds FOR UPDATE;
+ SELECT id BULK COLLECT INTO unused FROM FileSystem WHERE diskServer = ds FOR UPDATE;
  /* now we can safely go */
  UPDATE FileSystem SET deltaWeight = deltaWeight + deviation
   WHERE diskServer = ds;
