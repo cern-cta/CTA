023a2aa83d37492b0ae3b94dfe6a0924428c6737
Dennis Waldron <waldron@cern.ch>
2006-06-13 14:09:33 +0000

    new DLF interface definitions

--- h/dlf_api.h
+++ h/dlf_api.h
@@ -1,106 +1,245 @@
-/*
- *
- * Copyright (C) 2003 by CERN/IT/ADC/CA
- * All rights reserved
- *
- * @(#)$RCSfile: dlf_api.h,v $ $Revision: 1.13 $ $Date: 2006/03/23 14:31:28 $ CERN IT-ADC Vitaly Motyakov
+/******************************************************************************************************
+ *                                                                                                    *
+ * dlf_api.h - Castor Distribution Logging Facility                                                   *
+ * Copyright (C) 2005 CERN IT/FIO (castor-dev@cern.ch)                                                *
+ *                                                                                                    *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU *
+ * General Public License as published by the Free Software Foundation; either version 2 of the       *
+ * License, or (at your option) any later version.                                                    *
+ *                                                                                                    *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without  *
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ * General Public License for more details.                                                           *
+ *                                                                                                    *
+ * You should have received a copy of the GNU General Public License along with this program; if not, *
+ * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,  *
+ * USA.                                                                                               *
+ *                                                                                                    *
+ ******************************************************************************************************/
+
+/**
+ * @file  dlf_api.h
+ * @brief exposes the DLF client API (Application Programming Interface)
  */
 
-#ifndef _DLFAPI_H_
-#define _DLFAPI_H_
+#ifndef _DLF_H
+#define _DLF_H
 
-#include "dlf_struct.h"
+/* headers */
+#include "Cuuid.h"
+#include "Cns_api.h"
+#include "osdep.h"
 
-#define DLF_PRTBUF_LEN 4094
+/* severity levels */
+#define DLF_LVL_EMERGENCY     10            /**< system is unusable                       */
+#define DLF_LVL_ALERT         9             /**< action must be taken immediately         */
+#define DLF_LVL_ERROR         8             /**< error conditions                         */
+#define DLF_LVL_WARNING       7             /**< warning conditions                       */
+#define DLF_LVL_AUTH          6             /**< auth error                               */
+#define DLF_LVL_SECURITY      5             /**< Csec error                               */
+#define DLF_LVL_USAGE         4             /**< trace of routines                        */
+#define DLF_LVL_SYSTEM        3             /**< system error condition                   */
+#define DLF_LVL_IMPORTANT     2             /**< informative                              */
+#define DLF_LVL_DEBUG         1             /**< debug-level messages                     */
 
-#define DLF_DST_FILE 1
-#define DLF_DST_TCPHOST 2
-#define DLF_DST_UDPHOST 3
+/* parameter types */
+#define DLF_MSG_PARAM_DOUBLE  1             /**< double precision floating point value    */
+#define DLF_MSG_PARAM_INT64   2             /**< 64 bit integers                          */
+#define DLF_MSG_PARAM_STR     3             /**< general purpose string                   */
+#define DLF_MSG_PARAM_TPVID   4             /**< tape visual identifier string            */
+#define DLF_MSG_PARAM_UUID    5             /**< subrequest identifier                    */
+#define DLF_MSG_PARAM_FLOAT   6             /**< single precision floating point value    */
+#define DLF_MSG_PARAM_INT     7             /**< integer parameter                        */
 
-#define DLF_MSG_PARAM_STR     1
-#define DLF_MSG_PARAM_NUM     2
-#define DLF_MSG_PARAM_UUID    3
-#define DLF_MSG_PARAM_INT     4
-#define DLF_MSG_PARAM_INT64   5
-#define DLF_MSG_PARAM_FLOAT   6
-#define DLF_MSG_PARAM_DOUBLE  7
-#define DLF_MSG_PARAM_TPVID   8
+/* protocol
+ *   - changes these could have significant effects on the database interface layer!!
+ */
+#define DLF_MAGIC             0x68767001    /**< protocol version                         */
 
-#define DLF_LVL_EMERGENCY  1
-#define DLF_LVL_ALERT      2
-#define DLF_LVL_ERROR      3
-#define DLF_LVL_WARNING    4
-#define DLF_LVL_AUTH       5
-#define DLF_LVL_SECURITY   6
-#define DLF_LVL_USAGE      7
-#define DLF_LVL_SYSTEM     8
-#define DLF_LVL_IMPORTANT  9
-#define DLF_LVL_DEBUG     10
+#define DLF_LEN_HOSTNAME      63            /**< same as CA_MAXHOSTNAMELEN                */
+#define DLF_LEN_FACNAME       20            /**< max length of a facility name            */
+#define DLF_LEN_PARAMNAME     20            /**< max length of a msg parameter name       */
+#define DLF_LEN_TIMESTAMP     14            /**< length of a time string                  */
+#define DLF_LEN_STRINGVALUE   1024          /**< max length of a string value             */
+#define DLF_LEN_NUMBERVALUE   38            /**< max length of a number value             */
+#define DLF_LEN_MSGTEXT       512           /**< max length of a message text             */
+#define DLF_LEN_TAPEID        20            /**< max length of a tape id                  */
 
-#define DLF_LVL_ALL         -1
-#define DLF_LVL_SERVICE_ONLY 0
+/* limits */
+#define DLF_MAX_MSGTEXTS      1024          /**< max number of message texts per facility */
 
-#define CLOSE_RETURN(x) \
-        { \
-        fclose (in_file); \
-        return (x); \
-        }
+/* errors code */
+#define DLF_ERR_PROTO         1             /**< invalid dlf magic, protocol violation    */
+#define DLF_ERR_INVALIDREQ    2             /**< invalid request type                     */
+#define DLF_ERR_MARSHALL      3             /**< marhsall'ing failure                     */
+#define DLF_ERR_DB            4             /**< database error                           */
+#define DLF_ERR_SYSERR        5             /**< internal system error                    */
+#define DLF_ERR_UNKNOWNFAC    6             /**< unknown facility name                    */
+#define DLF_ERR_UNKNOWNTYPE   7             /**< unknown message parameter type           */
+#define DLF_ERR_MAX           8             /**< maximum number of error messages         */
 
-/* Function prototypes */
+/* error messages */
+#define DLF_ERR_01            "Invalid protocol version"
+#define DLF_ERR_02            "Invalid request type"
+#define DLF_ERR_03            "Marshalling error"
+#define DLF_ERR_04            "Database error"
+#define DLF_ERR_05            "Internal server error"
+#define DLF_ERR_06            "Invalid facility name"
+#define DLF_ERR_07            "Unknown message parameter type"
+#define DLF_ERR_08            "Unknown error"
 
-EXTERN_C int DLL_DECL dlf_init _PROTO((const char*));
-EXTERN_C void DLL_DECL dlf_shutdown _PROTO((void));
-EXTERN_C int DLL_DECL dlf_reinit _PROTO((const char*));
-EXTERN_C char DLL_DECL * dlf_get_severity_name _PROTO((int));
-EXTERN_C char DLL_DECL * dlf_get_token _PROTO((const char*, const char*, char*, char*, int));
-EXTERN_C char DLL_DECL * dlf_parse_dst _PROTO((const char*, int*, int*, char*));
-EXTERN_C int DLL_DECL dlf_store_log_destinations _PROTO((const char*, int, dlf_log_dst_slist_t*));
-EXTERN_C dlf_log_dst_t DLL_DECL * dlf_find_dst _PROTO((int, int, const char*, dlf_log_dst_slist_t*));
-EXTERN_C int DLL_DECL dlf_add_to_log_dst _PROTO((int, int, const char*, int, dlf_log_dst_slist_t*));
-EXTERN_C dlf_log_dst_t DLL_DECL * dlf_find_server _PROTO((int));
-EXTERN_C char DLL_DECL * dlf_get_msg_text _PROTO((int));
-EXTERN_C int DLL_DECL dlf_gettexts _PROTO((const char*, unsigned int*, dlf_msg_text_slist_t*));
-EXTERN_C int DLL_DECL dlf_gettexts_from_file _PROTO((const char*, unsigned int*, dlf_msg_text_slist_t*));
-EXTERN_C int DLL_DECL dlf_add_to_text_list _PROTO((int, const char*, dlf_msg_text_slist_t*));
-EXTERN_C char DLL_DECL * dlf_format_str _PROTO((char*, int, const char*, ...));
-EXTERN_C int DLL_DECL dlf_write _PROTO((Cuuid_t, int, int, struct Cns_fileid*, int, ...));
-EXTERN_C int DLL_DECL dlf_writep _PROTO((Cuuid_t, int, int, struct Cns_fileid *, int, struct dlf_write_param []));
-EXTERN_C char DLL_DECL * dlf_uuid2hex _PROTO((const Cuuid_t, char*, int));
-EXTERN_C int DLL_DECL dlf_write_to_file _PROTO((dlf_log_dst_t*, dlf_log_message_t*));
-EXTERN_C int DLL_DECL dlf_calc_msg_size _PROTO((dlf_log_message_t*));
-EXTERN_C int DLL_DECL dlf_send_to_host _PROTO((dlf_log_dst_t*, dlf_log_message_t*, int, int));
-EXTERN_C int DLL_DECL islittleendian _PROTO((void));
-EXTERN_C dlf_log_message_t DLL_DECL * dlf_new_log_message _PROTO((void));
-EXTERN_C void DLL_DECL dlf_free_log_message _PROTO((dlf_log_message_t*));
-EXTERN_C void DLL_DECL dlf_delete_param_list _PROTO((dlf_msg_param_list_t*));
-EXTERN_C int DLL_DECL dlf_log_message_init _PROTO((dlf_log_message_t*));
-EXTERN_C dlf_msg_param_t DLL_DECL * new_dlf_param _PROTO((void));
-EXTERN_C int DLL_DECL dlf_add_str_parameter _PROTO((dlf_log_message_t*, const char*, const char*));
-EXTERN_C int DLL_DECL dlf_add_int_parameter _PROTO((dlf_log_message_t*, const char*, HYPER));
-EXTERN_C int DLL_DECL dlf_add_double_parameter _PROTO((dlf_log_message_t*, const char*, double));
-EXTERN_C int DLL_DECL dlf_add_to_param_list _PROTO((dlf_msg_param_list_t*, dlf_msg_param_t*));
-EXTERN_C int DLL_DECL dlf_add_subreq_id _PROTO((dlf_log_message_t*, Cuuid_t));
-EXTERN_C int DLL_DECL dlf_add_tpvid_parameter _PROTO((dlf_log_message_t*, const char*));
-EXTERN_C int DLL_DECL getrep _PROTO((int, char**, int*, int*));
-EXTERN_C int DLL_DECL send2dlf _PROTO((int*, dlf_log_dst_t*, char*, int));
-EXTERN_C int DLL_DECL dlf_isinteger _PROTO((const char*));
-EXTERN_C int DLL_DECL dlf_isfloat _PROTO((const char*));
-EXTERN_C int DLL_DECL dlf_hex2uuid _PROTO((const char*, Cuuid_t));
-EXTERN_C int DLL_DECL dlf_hexto4bits _PROTO((const int));
-EXTERN_C int DLL_DECL dlf_hex2u64 _PROTO((const char*, u_signed64*));
+/* reply types */
+#define DLF_REP_ERR           1             /**< error response                           */
+#define DLF_REP_RC            2             /**< continue and disconnect                  */
+#define DLF_REP_IRC           3             /**< continue, ack, next                      */
 
-EXTERN_C int DLL_DECL dlf_enterfacility _PROTO((const char*, int));
-EXTERN_C int DLL_DECL dlf_entertext _PROTO((const char*, int, const char*));
-EXTERN_C int DLL_DECL dlf_modifyfacility _PROTO((const char*, int));
-EXTERN_C int DLL_DECL dlf_modifytext _PROTO((const char*, int, const char*));
-EXTERN_C int DLL_DECL dlf_deletefacility _PROTO((const char*));
-EXTERN_C int DLL_DECL dlf_deletetext _PROTO((const char*, int));
+/* request types */
+#define DLF_INIT              1             /**< initialisation request                   */
+#define DLF_LOG               2             /**< log message request                      */
 
-EXTERN_C int DLL_DECL dlf_errmsg _PROTO((const char *, ...));
-EXTERN_C int DLL_DECL dlf_seterrbuf _PROTO((char *, int));
 
-#if defined(_WIN32)
-EXTERN_C int DLL_DECL getopt _PROTO((int, const char **, const char *));
+/**
+ * opaque structures
+ */
+
+typedef struct dlf_write_param_t dlf_write_param_t;
+
+
+/* structures
+ *   - provide for c++ user convenience in castor/dlf/Param.hpp
+ */
+
+struct dlf_write_param_t {
+	char               *name;          /**< name of the parameter                    */
+	int                type;           /**< parameter type, one of DLF_MSG_PAARM_*   */
+
+	/* types */
+	union {
+		char       *par_string;    /** param of type DLF_PARAM_STRING            */
+		int        par_int;        /** param of type DLF_PARAM_INT               */
+		U_HYPER    par_u64;        /** param of type DLF_PARAM_INT64             */
+		double     par_double;     /** param of type DLF_PARAM_DOUBLE            */
+		Cuuid_t    par_uuid;       /** param of type DLF_PARAM_UUID              */
+	} par;
+};
+
+
+/**
+ * Initialise the DLF interface. Read the castor2 common configuration file "/etc/castor/castor.conf" or
+ * the environment variables for DLF and create the necessary data structures for writing to files and
+ * remote servers. This function is also responsible for creating the threads for communication between
+ * client and server.
+ *
+ * Once the interface is initialised it cannot be re-initialised without a prior call to dlf_shutdown()
+ *
+ * @param facility : the name of the facility to initialise
+ * @param errptr   : the error text returned from the function on -1, terminated by a '\n'. The error
+ *                   buffer must have a minimum size of CA_MAXLINELEN bytes
+ *
+ * @returns        : 0 on success
+ * @returns        : -1 on failure and *errptr will contain the associated error message
+ *
+ * @note the interface is restricted to API_MAX_TARGETS and API_MAX_THREADS.
+ */
+
+EXTERN_C int DLL_DECL dlf_init _PROTO((const char *facility, char *errptr));
+
+
+/**
+ * Shutdown the DLF interface, terminate all threads and reclaim all allocated resources. If the interface
+ * is already shutdown then no action will be taken. The api will attempt to shutdown its associated
+ * threads for anything up to 10 seconds, this gives threads enough time to gracefully shutdown. After
+ * this time any threads left running will have all their data destroyed.
+ *
+ * @returns        : 0 on success
+ */
+
+EXTERN_C int DLL_DECL dlf_shutdown _PROTO((void));
+
+
+/**
+ * Register a message text for the given facility with the interface. All the message texts must be
+ * initialised prior to the dlf_init() call. Message registration after this point is not permitted!
+ * No more then DLF_MAX_MSGTEXTS may be registered at any one time.
+ *
+ * @param msg_no   : the message number
+ * @param msg_text : the text associated with the msg_no
+ *
+ * @returns        : 0 on success
+ * @returns        : -1 on failure
+ */
+
+EXTERN_C int DLL_DECL dlf_regtext _PROTO((unsigned short msg_no, const char *msg_text));
+
+
+/**
+ * This function writes a log message to the logging targets/destinations specified in the castor2 common
+ * configuration file "/etc/castor/castor.conf" or through environment variables.
+ *
+ * @param reqid      : the unique request identifier associated with the log messages
+ * @param severity   : severity level of the message
+ * @param msg_no     : the message assigned number
+ * @param Cns_fileid : pointer to the structure containing the Castor name server invariant associated
+ *                     with the message. This consists of the hostname and file id. If no invariant is
+ *                     associated with the message the value should be NULL. The nshostname and nsfileid
+ *                     will read "N/A" and 0 respectively
+ *
+ * @param numparams  : is the number of log message parameters passed
+ * @param params     : array of dlf_write_param_t parameters
+ *
+ *
+ * @returns          : 0 on success
+ * @returns          : -1 on failure
+ *
+ * @note when an error occurs it could be because of multiple problems. At the moment the api returns
+ * no serrno and errno style return codes. The reason for this is that there is very little the client
+ * can do without recompilation to correct the problem. Also as the api is asynchronous with respect to
+ * sending messages to a remote host its very difficult to throw an error with an association to a
+ * specific message as messages are not uniquely identifiable.
+ *
+ * The most common errors are using an invalid severity level or attempting to log a message for a
+ * message number that has either not be pre registered with the api or is NULL.
+ *
+ * If the api encounters problems sending messages to the server because of network/transport issues. An
+ * error message will be added to the LOGERROR log under the facility DLF. DLF errors written here are
+ * not reported to the server. Users should pay particular attention to this category especially when
+ * parsing the file with scripts as not all messages in the file maybe associated to one particular
+ * facility!
+ *
+ * @warning although the function may return successfully, this does not mean that the message has been
+ * successfully transmitted to the server. The api is asynchronous and depending on the load on the
+ * server and the network conditions it could take several minutes for a message to make it to the server
+ * and several minutes after that before insertion into the database
+ *
+ * @see doc/example.c
+ */
+
+
+EXTERN_C int DLL_DECL dlf_writep _PROTO((Cuuid_t reqid,
+					 int sevetity,
+					 int msg_no,
+					 struct Cns_fileid *ns,
+					 int numparams,
+					 dlf_write_param_t params[]));
+
+
+/**
+ * This function writes a log message to the logging targets/destinations specified in the castor2 common
+ * configuration file "/etc/castor/castor.conf" or through environment variables. dlf_write() is a wrapper
+ * function around dlf_writep(). It basically takes a variable argument list '...' and converts it to an
+ * array of dlf_write_param_t structures which is passed to dlf_writep() along with the other parameters. All
+ * the other arguments and returns values are identical to that of dlf_write()
+ *
+ * @see dlf_write()
+ */
+
+EXTERN_C int DLL_DECL dlf_write _PROTO((Cuuid_t reqid,
+					int severity,
+					int msg_no,
+					struct Cns_fileid *ns,
+					int numparams, ...));
+
+
 #endif
 
-#endif /* _DLFAPI_H_ */
+/** End-of-File **/
