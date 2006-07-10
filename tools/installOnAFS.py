02c4aec3256b7fa8a1a8f1f222b8f5f8bb568b1a
Giuseppe Lo Presti <itglp@cern.ch>
2006-07-10 09:55:58 +0000

    Improved script to install CASTOR to AFS including lib-compat

--- tools/installOnAFS.py
+++ tools/installOnAFS.py
@@ -1,8 +1,9 @@
-#!python
+#!/usr/bin/python
 
-castordir = '/afs/cern.ch/project/cndoc/wwwds/HSM/CASTOR/DIST/CERN/savannah/CASTOR.pkg/'
+castordir = '/afs/cern.ch/project/cndoc/wwwds/HSM/CASTOR/DIST/CERN/savannah/'
 afsdir = '/afs/cern.ch/sw/lcg/external/castor/'
 version = '2.0.4-1'
+compatversion = '2.0.2-1'
 packages = ['ns-client', 'doc', 'rtcopy-client', 'upv-client', 'commands', 'stager-client', 'vdqm-client', 'rfio-server', 'lib', 'devel', 'stager-clientold', 'vmgr-client', 'rtcopy-messages', 'tape-client', 'rfio-client', 'scriptlets']
 
 platformConversion = {'SLC3' : 'slc3', 'SLC4' : 'slc4'}
@@ -12,7 +13,8 @@ gccVersion = {'SLC3' : 'gcc323', 'SLC4' : 'gcc345'}
 for plat in ('SLC3', 'SLC4'):
     for arch in ('i386', 'ia64', 'x86_64'):
         archdir = afsdir + version + '/' + platformConversion[plat] + '_' + archConversion[arch] + '_' + gccVersion[plat]
-        rpmdir = castordir + version + '/' + plat + '/' + arch + '/'
+        rpmdir = castordir + 'CASTOR.pkg/' + version + '/' + plat + '/' + arch + '/'
         print 'mkdir -p ' + archdir + ';cd ' + archdir
         for p in packages:
           print 'rpm2cpio ' + rpmdir + 'castor-' + p + '-' + version + '.' + arch + '.rpm | cpio -idmv'
+	print 'rpm2cpio ' + castordir + 'CASTOR-compat.pkg/castor-lib-compat-' + compatversion + '.' + platformConversion[plat] + '.' + arch + '.rpm | cpio -idmv'
