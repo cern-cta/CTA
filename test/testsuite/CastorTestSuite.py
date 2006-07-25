f34de0df85fcac400b44b46e9b832b289527e610
Giulia Taurelli <gtaur@cern.ch>
2006-07-25 15:28:25 +0000

    common part to handle different test scripts

---
+++ test/testsuite/CastorTestSuite.py
@@ -0,0 +1,98 @@
+import os
+import sys
+import unittest
+import ClientTest
+import string
+
+
+########################### for each module its own test suite ##########################
+
+class CommonSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)
+
+class ServerSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)
+
+class ClientSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)
+
+class RfioSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)
+
+class DlfSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)
+	
+class CsvcsSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)	
+	
+class DisksvrSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)	
+	
+class TapeSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)	
+	
+class AdminSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)
+	
+
+#####  allCastorSuites is a dictonary with a test suite for each module
+	
+allCastorSuites={'COMMON':CommonSuite(),'SERVER':ServerSuite(),'CLIENT':ClientSuite(),'RFIO':RfioSuite(),'DLF':DlfSuite(),'CSVC':CsvcsSuite(),'DISKSVR':DisksvrSuite(),'TAPE':TapeSuite(),'ADMIN':AdminSuite()}
+
+
+################ for each module all the possible test suites included #######################
+
+commonTest={'TEST':0}
+serverTest={'TEST':0}
+clientTest={'PREREQ': ClientTest.StagerPreClientSuite(),'PUT':ClientTest.StagerPutSuite(),'PUTDONE':ClientTest.StagerPutDoneSuite(),'GET':ClientTest.StagerGetSuite(),'RM':ClientTest.StagerRmSuite(),'EXTRAQRY':ClientTest.StagerQuerySpecialSuite()}
+rfioTest={'TEST':0}
+dlfTest={'TEST':0}
+csvcsTest={'TEST':0}
+disksvrTest={'TEST':0}
+tapeTest={'TEST':0}
+adminTest={'TEST':0}
+
+listOfTest={'COMMON':commonTest,'SERVER':serverTest,'CLIENT':clientTest,'RFIO':rfioTest,'DLF':dlfTest,'CSVCS':csvcsTest,'DISKSVR':disksvrTest,'TAPE':tapeTest,'ADMIN':adminTest}
+
+
+#################### opening the file with the list of tests wanted  ################################
+
+f=open("/home/gtaur/CASTORTESTCONFIG","r")
+configFileInfo=f.read()
+f.close
+
+newSuites=configFileInfo.split("\n\n")
+
+for mySuite in newSuites:
+    newCases=mySuite.splitlines()
+    for myCase in newCases:
+       	if myCase.find("YES"):
+            elem=myCase.split()
+            testToAdd=(listOfTest[elem[0]])[elem[1]]
+            if testToAdd != 0:
+                (allCastorSuites[elem[0]]).addTest(testToAdd)
+
+
+############### this is the global test suite with all the tests required in the CASTORTESTCONFIG ####################
+
+class CastorGlobalSuite(unittest.TestSuite):
+    def __init__(self):    
+	unittest.TestSuite.__init__(self)
+
+
+myGlobalSuite=CastorGlobalSuite()
+for differentSuite in allCastorSuites:
+	myGlobalSuite.addTest(allCastorSuites[differentSuite])
+	
+	
+runner=unittest.TextTestRunner()
+runner.run(myGlobalSuite)
---
+++ test/testsuite/UtilityForCastorTest.py
@@ -0,0 +1,80 @@
+import threading
+import os
+import sys
+import time
+
+def getTicket():
+    newTime=time.gmtime()
+    return str(((newTime[7]*24+newTime[3])*60 + newTime[4])*60 +newTime[5]) 
+
+# function called if one of the request takes too much time
+
+def timeOut(myCmd):
+    print "TIME OUT !!!"
+    print "Castor is not working :("
+    listProc=os.popen("ps").read().split("\n")
+
+    # I retrieve and  kill the process that created problems
+    procName=(myCmd.split(" "))[0]
+    for item in listProc:
+        if item.find(procName) != -1:
+            os.system("kill -9 "+item[0:5])
+            print procName+" was blocked"
+    os._exit(0)  
+
+
+def saveOnFile(namefile,cmdS):
+    t=threading.Timer(120.0,timeOut,[cmdS])
+    t.start()
+    fin=open(namefile,"wb")
+    fin.write(os.popen(cmdS).read())
+    fin.close()
+    t.cancel()
+      
+def runOnShell(cmdS):
+    t=threading.Timer(120.0,timeOut,[cmdS])
+    t.start()
+    os.system(cmdS)
+    t.cancel()
+    
+    
+def logUser():
+    
+#function to find the right directory on castor 
+    myUid= os.geteuid()
+   
+    strUid=0
+   
+    fin=open("/etc/passwd",'r')
+    for line in fin:
+        elem=line.split(":")
+        if elem[2] == str(myUid):
+            strUid=elem[0]
+    fin.close()
+    return strUid
+
+
+# prepare string for castor file
+
+def prepareCastorString():
+    userId=logUser()
+    if userId==-1:
+	     return -1
+    return "/castor/cern.ch/user/"+userId[0]+"/"+userId+"/"
+	    
+# check if the user of the program is a castor user
+
+def checkUser():
+    userId=logUser()
+    myCmd=prepareString("","")
+    myCmd="nsls "+myCmd
+    ret=runSafe(myCmd)
+    if ret.find("No such file or directory") != -1:
+        print "You are "+userId+" and you are not a Castor user. Go away!"
+        return -1
+    return userId
+
+
+
+
+
