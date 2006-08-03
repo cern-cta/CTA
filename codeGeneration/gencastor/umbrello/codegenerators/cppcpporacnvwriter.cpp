77ed60860895f324e7fe7e3a712979795308fa12
Giulia Taurelli <gtaur@cern.ch>
2006-08-03 08:52:13 +0000

    handle exception new fix

--- codeGeneration/gencastor/umbrello/codegenerators/cppcpporacnvwriter.cpp
+++ codeGeneration/gencastor/umbrello/codegenerators/cppcpporacnvwriter.cpp
@@ -2746,7 +2746,7 @@ void CppCppOraCnvWriter::printSQLError(QString name,
                                        AssocList& assocs) {
   fixTypeName("OraCnvSvc", "castor::db::ora", m_classInfo->packageName);
    m_indent++;
-  *m_stream << getIndent() <<"castor::db::ora::OraBaseObj::handleException(e);" << endl;
+  *m_stream << getIndent() <<"cnvSvc()->handleException(e);" << endl;
   *m_stream<< getIndent()
             << fixTypeName("InvalidArgument",
                            "castor.exception",
