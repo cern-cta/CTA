788a5aeb5fab6cfecc07258c1bd35fed19e38bf6
Sebastien Ponce <sponcec3@cern.ch>
2006-09-25 08:26:44 +0000

    New exception OutOfMemory

---
+++ castor/exception/OutOfMemory.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+ *                      OutOfMemory.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile: OutOfMemory.cpp,v $ $Revision: 1.1 $ $Release$ $Date: 2006/09/25 08:26:44 $ $Author: sponcec3 $
+ *
+ * 
+ *
+ * @author Sebastien Ponce
+ *****************************************************************************/
+
+// Include Files
+#include <errno.h>
+#include "serrno.h"
+#include "castor/exception/OutOfMemory.hpp"
+
+// -----------------------------------------------------------------------
+// Constructor
+// -----------------------------------------------------------------------
+castor::exception::OutOfMemory::OutOfMemory() :
+  castor::exception::Exception(ENOMEM) {}
