7fbd11765a8af8ea365f21adeb9cdaac65a3a09c
Felix Ehm <felixehm@cern.ch>
2006-09-27 12:46:09 +0000

    Added create and drop sql files for the repack schema

---
+++ castor/repack/repack_oracle_create.sql
@@ -0,0 +1,14 @@
+/* SQL statements for type RepackSubRequest */
+CREATE TABLE RepackSubRequest (vid VARCHAR2(2048), xsize INTEGER, status NUMBER, cuuid VARCHAR2(2048), filesMigrating NUMBER, filesStaging NUMBER, files NUMBER, filesFailed NUMBER, id INTEGER PRIMARY KEY, requestID INTEGER) INITRANS 50 PCTFREE 50;
+
+/* SQL statements for type RepackSegment */
+CREATE TABLE RepackSegment (fileid INTEGER, compression NUMBER, segsize INTEGER, filesec INTEGER, copyno NUMBER, blockid INTEGER, fileseq INTEGER, id INTEGER PRIMARY KEY, vid INTEGER) INITRANS 50 PCTFREE 50;
+
+/* SQL statements for type RepackRequest */
+CREATE TABLE RepackRequest (machine VARCHAR2(2048), userName VARCHAR2(2048), creationTime NUMBER, serviceclass VARCHAR2(2048), pid INTEGER, command NUMBER, pool VARCHAR2(2048), id INTEGER PRIMARY KEY) INITRANS 50 PCTFREE 50;
+
+/* SQL statements for Castor_Version */
+CREATE TABLE castor_version (version VARCHAR2(2048));
+
+/* SQL statements for ID2Type*/
+CREATE TABLE ID2TYPE (ID INTEGER, TYPE NUMBER);
---
+++ castor/repack/repack_oracle_drop.sql
@@ -0,0 +1,14 @@
+/* SQL statements for type RepackSubRequest */
+DROP TABLE RepackSubRequest;
+
+/* SQL statements for type RepackSegment */
+DROP TABLE RepackSegment;
+
+/* SQL statements for type RepackRequest */
+DROP TABLE RepackRequest;
+
+/* SQL statements for type ID2TYPE */
+DROP TABLE ID2TYPE;
+
+/* SQL statements for type CASTORVERSION */
+DROP TABLE CASTORVERSION;
