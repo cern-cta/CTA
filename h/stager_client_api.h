1a0908388f6942d20a986bc3898df602d53674ca
Victor Kotlyar <kotlyar@cern.ch>
2006-10-09 14:34:50 +0000

    added  known mover protocols    #define MOVER_PROTOCOL_XROOT "xroot"    #define MOVER_PROTOCOL_GSIFTP "gsiftp"

--- h/stager_client_api.h
+++ h/stager_client_api.h
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: stager_client_api.h,v $ $Revision: 1.33 $ $Release$ $Date: 2006/09/26 15:19:03 $ $Author: riojac3 $
+ * @(#)$RCSfile: stager_client_api.h,v $ $Revision: 1.34 $ $Release$ $Date: 2006/10/09 14:34:50 $ $Author: kotlyar $
  *
  * the client API to the castor stager
  *
@@ -25,11 +25,11 @@
  *****************************************************************************/
 
 /** @file $RCSfile: stager_client_api.h,v $
- * @version $Revision: 1.33 $
- * @date $Date: 2006/09/26 15:19:03 $
+ * @version $Revision: 1.34 $
+ * @date $Date: 2006/10/09 14:34:50 $
  */
 /** @mainpage CASTOR New Stager API Proposal
- * $RCSfile: stager_client_api.h,v $ $Revision: 1.33 $
+ * $RCSfile: stager_client_api.h,v $ $Revision: 1.34 $
  *
  * @section intro Introduction
  * The new API for the CASTOR stager has been based on the requirements for the 
@@ -1541,7 +1541,8 @@ FREE_STRUCT_LIST_DECL(findrequest_resp)
 
 #define MOVER_PROTOCOL_RFIO "rfio"
 #define MOVER_PROTOCOL_ROOT "root"
-
+#define MOVER_PROTOCOL_XROOT "xroot"
+#define MOVER_PROTOCOL_GSIFTP "gsiftp"
 
 
 
