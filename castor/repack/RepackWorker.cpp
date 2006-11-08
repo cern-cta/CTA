4f5fe01e87cea3257fa959b6aa2e76ce63cf97ff
Felix Ehm <felixehm@cern.ch>
2006-11-08 14:04:45 +0000

    logic missing : A Request should be able to be restarted, if in FINISHED, when files are left in staging/migrating (e.g. in case the request was aborted with -R )

--- castor/repack/RepackWorker.cpp
+++ castor/repack/RepackWorker.cpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: RepackWorker.cpp,v $ $Revision: 1.27 $ $Release$ $Date: 2006/10/30 17:50:45 $ $Author: felixehm $
+ * @(#)$RCSfile: RepackWorker.cpp,v $ $Revision: 1.28 $ $Release$ $Date: 2006/11/08 14:04:45 $ $Author: felixehm $
  *
  *
  *
@@ -305,7 +305,7 @@ void RepackWorker::restart(RepackRequest* rreq) throw (castor::exception::Intern
           delete tmp;
           throw ex;
         }
-        if ( tmp->filesMigrating() || tmp->filesStaging() ){
+        if ( tmp->status() != SUBREQUEST_DONE && (tmp->filesMigrating() || tmp->filesStaging()) ){
           castor::exception::Internal ex;
           ex.getMessage() << "There are still files staging/migrating, Restart abort." <<std::endl;
           delete tmp;
