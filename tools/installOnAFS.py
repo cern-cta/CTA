f9bb64000a822ed878133b07b05eb5005b61a666
Rosa Maria Garcia Rioja <riojac3@cern.ch>
2006-11-17 13:03:17 +0000

    Remove ia64 architecture

--- tools/installOnAFS.py
+++ tools/installOnAFS.py
@@ -12,11 +12,11 @@ compatversion = '2.0.2-11'
 packages = ['ns-client', 'doc', 'rtcopy-client', 'upv-client', 'commands', 'stager-client', 'vdqm-client', 'rfio-server', 'lib', 'devel', 'stager-clientold', 'vmgr-client', 'rtcopy-messages', 'tape-client', 'rfio-client', 'scriptlets']
 
 platformConversion = {'SLC3' : 'slc3', 'SLC4' : 'slc4'}
-archConversion = {'i386' : 'ia32', 'ia64': 'ia64', 'x86_64' : 'amd64'}
+archConversion = {'i386' : 'ia32', 'x86_64' : 'amd64'}
 gccVersion = {'SLC3' : 'gcc323', 'SLC4' : 'gcc34'}
 
 for plat in ('SLC3', 'SLC4'):
-    for arch in ('i386', 'ia64', 'x86_64'):
+    for arch in ('i386', 'x86_64'):
         archdir = afsdir + version + '/' + platformConversion[plat] + '_' + archConversion[arch] + '_' + gccVersion[plat]
         rpmdir = castordir + 'CASTOR.pkg/' + version + '/' + plat + '/' + arch + '/'
         print 'mkdir -p ' + archdir + ';cd ' + archdir
