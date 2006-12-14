ccc4f3c58c4c4510b5be4814002649ad4ac14b40
Giulia Taurelli <gtaur@cern.ch>
2006-12-14 15:18:23 +0000

    to get information from the stagemapper we check first the user and then the group mapping

--- client/src/stager/stager_client_commandline.cpp
+++ client/src/stager/stager_client_commandline.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: stager_client_commandline.cpp,v 1.2 2006/12/14 14:50:00 itglp Exp $
+ * $Id: stager_client_commandline.cpp,v 1.3 2006/12/14 15:18:23 gtaur Exp $
  *
  * Copyright (C) 2004-2006 by CERN/IT/FIO/FD
  * All rights reserved
@@ -34,102 +34,110 @@
 EXTERN_C char DLL_DECL *getconfent _PROTO((char *, char *, int));
 
 
-/* This function was in RfioTURL, now it's imported here after the DPM merge.
-   Check the header file for its explanation.
-*/
+/********************************************************************************************************************
+ * This Function:                                                                                                   *
+ * if *host or *svc are null or empty strings retrive the values, the same if *port and *version are <= 0.          * 
+ * To retrive the values it looks if:                                                                               *
+ * enviroment variables are set or stager_mapper as values defined or castor.conf or (if it doesn't have valid)     * 
+ * it uses default hard coded values.                                                                               *
+ *                                                                                                                  *
+ *******************************************************************************************************************/
 
-int DLL_DECL getDefaultForGlobal (
-                      char** host,
-                      int* port,
-                      char** svcClass,
-                      int* version)
+int DLL_DECL getDefaultForGlobal(
+			char** host,
+			int* port,
+			char** svc,
+			int* version)
+
+
 { 
 	char *hostMap, *hostDefault, *svcMap, *svcDefault;
 	int versionMap,versionDefault, portDefault, ret;
 	char* aux=NULL; 
-  struct group* grp=NULL; 
+        struct group* grp=NULL; 
 	gid_t gid;
-  
-  hostMap=hostDefault=svcMap=svcDefault=NULL;
-  versionMap=versionDefault=portDefault=ret=0;
-  
-	if(host == NULL || port == NULL || svcClass == NULL || version == NULL)
-    { return (-1); }
-	hostDefault=*host;
-	svcDefault=*svcClass;
+
+        hostMap=hostDefault=svcMap=svcDefault=NULL;
+        versionMap=versionDefault=portDefault=ret=0;
+
+	if(host == NULL || port == NULL || svc == NULL || version == NULL ){ return (-1);}
+	if (*host) hostDefault=strdup(*host);
+	else hostDefault=NULL;
+	
+        if (*svc) svcDefault=strdup(*svc);
+	else svcDefault=NULL;
+
 	portDefault=*port;
 	versionDefault=*version;
-  
+
+	ret=just_stage_mapper(getenv("USER"),NULL,&hostMap,&svcMap,&versionMap);
 	gid=getgid();
-  
-	if((grp = getgrgid(gid)) <= 0 ){
-		ret=just_stage_mapper(getenv("USER"),NULL,&hostMap,&svcMap,&versionMap); 
-	}
-	else{
-		ret=just_stage_mapper(NULL,grp->gr_name,&hostMap,&svcMap,&versionMap);
-	}
-  
+
+	if (ret<0 && (grp = getgrgid(gid)) == NULL )
+	  ret=just_stage_mapper(NULL,grp->gr_name,&hostMap,&svcMap,&versionMap);
+	
+
 	if(hostDefault==NULL || strcmp(hostDefault,"")==0){
-		if(hostDefault){free(hostDefault);}
+		if(hostDefault != NULL){free(hostDefault);hostDefault=NULL;}
 		aux=getenv("STAGE_HOST");
 		hostDefault=aux==NULL?NULL:strdup(aux);
 		if (hostDefault==NULL || strcmp(hostDefault,"")==0 ){
-			if(hostDefault){free(hostDefault);}
+			if(hostDefault !=NULL){free(hostDefault);}
 			if (hostMap==NULL || strcmp(hostMap,"")==0 ){
 				aux=(char*)getconfent("STAGER","HOST", 0);
 				hostDefault=aux==NULL?NULL:strdup(aux);
 				if (hostDefault==NULL || strcmp(hostDefault,"")==0 ){
-					if(hostDefault){free(hostDefault);}
+					if(hostDefault != NULL){free(hostDefault);hostDefault=NULL;}
 					hostDefault=strdup(DEFAULT_HOST);
 				}
 			}
 			else{
-				if(hostDefault){free(hostDefault);}
+				if(hostDefault!=NULL){free(hostDefault);hostDefault=NULL;}
 				hostDefault=strdup(hostMap);
 			}
 		}
 	}
 	
-  
+
 	if (svcDefault==NULL || strcmp(svcDefault,"")==0){
-		if(svcDefault){free(svcDefault);}
+	  if(svcDefault != NULL){free(svcDefault);svcDefault=NULL;}
 		aux=getenv("STAGE_SVCCLASS");
 		svcDefault= aux==NULL? NULL: strdup(aux);
 		if (svcDefault==NULL || strcmp(svcDefault,"")==0 ){
-			if(svcDefault){free(svcDefault);}
+		  if(svcDefault!= NULL &&strcmp(svcDefault,"")){free(svcDefault);svcDefault=NULL;}
 			if (svcMap==NULL || strcmp(svcMap,"")==0 ){
 				aux=(char*)getconfent("STAGER","SVC_CLASS", 0);
 				svcDefault=aux==NULL?NULL:strdup(aux);
 				if (svcDefault==NULL || strcmp(svcDefault,"")==0 ){
-					if(svcDefault){free(svcDefault);}
-					svcDefault=strdup("");
+				        if(svcDefault!=NULL){free(svcDefault);svcDefault=NULL;}
+					svcDefault=strdup(DEFAULT_SVCCLASS);
 				}
 			}
 			else{
-				if(svcDefault){free(hostDefault);}
-				svcDefault=strdup(DEFAULT_SVCCLASS);
+			  if(svcDefault!=NULL && strcmp(svcDefault,"") ){free(svcDefault);svcDefault=NULL;}
+				svcDefault=strdup(svcMap);
 			}
 		}
-    
+
 	}
 	if (versionDefault<=0){
 		aux=getenv("RFIO_USE_CASTOR_V2");
 		if(aux){
-      
+		        
 			versionDefault=strcasecmp(aux,"YES")==0?2:1;
 		}else{versionDefault=0;}
-    if (versionDefault<=0){
+    		if (versionDefault<=0){
 			versionDefault=versionMap+1;
 			if (versionDefault<=0){
-        aux=(char*)getconfent("STAGER","VERSION",0);
-        versionDefault=aux==NULL?0:atoi(aux);
+			aux=(char*)getconfent("STAGER","VERSION",0);
+			versionDefault=aux==NULL?0:atoi(aux);
 				if (versionDefault<=0){
-          versionDefault= DEFAULT_VERSION;
+					   versionDefault= DEFAULT_VERSION;
 			 	}
 			}
 		}
 	}
-  
+
 	if (portDefault<=0){
 		aux=getenv("STAGE_PORT");
 		portDefault=aux==NULL?0:atoi(aux);
@@ -137,16 +145,21 @@ int DLL_DECL getDefaultForGlobal (
 			aux=(char*)getconfent("STAGER", "PORT", 0);
 			portDefault=aux==NULL?0:atoi(aux);
 			if (portDefault<=0){
-        portDefault= versionDefault==2?DEFAULT_PORT2:DEFAULT_PORT1;
+			   portDefault= versionDefault==2?DEFAULT_PORT2:DEFAULT_PORT1;
 			}
 		}
 		
 	}
-  
-	if (*host==NULL || strcmp(*host,"")){*host=hostDefault;}	
+
+	if (*host==NULL || strcmp(*host,"")){*host=strdup(hostDefault);}	
 	if (port==NULL || *port<=0) {*port=portDefault;}
-	if (*svcClass==NULL || strcmp(*svcClass,"")){*svcClass=svcDefault;}
+	if (*svc==NULL || strcmp(*svc,"")){*svc=strdup(svcDefault);}
 	if (version==NULL || *version<=0){*version=versionDefault;}
-  
+
 	return (1);
 }
+
+
+
+
+
