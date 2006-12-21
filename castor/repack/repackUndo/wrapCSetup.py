cad55037ce287be7b3e6a2a350bd4ebcb332e91e
Giulia Taurelli <gtaur@cern.ch>
2006-12-21 15:16:42 +0000

    Now create the .so in the correct way and there is no need of extra make files. It uses the .o and the library straight.

--- castor/repack/repackUndo/wrapCSetup.py
+++ castor/repack/repackUndo/wrapCSetup.py
@@ -1,8 +1,15 @@
 from distutils.core import setup, Extension
 
+CASTOR_HOME='/usr/local/src/CASTOR2/CASTOR2'
+CNV_DIR=CASTOR_HOME+'/castor/db/cnv'
+REPACK_DIR=CASTOR_HOME+'/castor/repack'
+
+
 module1 = Extension('wrapcns',
-                    include_dirs=['.','/usr/local/src/CASTOR2/CASTOR2/h'],
-                    sources = ['WrapCns.cpp'])
+                    include_dirs=['.',CASTOR_HOME+'/h'],
+                    sources = [REPACK_DIR+'/repackUndo/WrapCns.cpp'], library_dirs=['/usr/lib'],
+                    extra_objects=[CNV_DIR+'/DbRepackRequestCnv.o',CNV_DIR+'/DbRepackSegmentCnv.o',CNV_DIR+'/DbRepackSubRequestCnv.o', REPACK_DIR+'/RepackSubRequest.o',REPACK_DIR+'/RepackRequest.o',REPACK_DIR+'/RepackSegment.o',REPACK_DIR+'/DatabaseHelper.o',REPACK_DIR+'/Tools.o'],
+                    libraries=['shift', 'castorCnvs'])
 
 setup (name = 'wrapcns',
        version = '1.0',
