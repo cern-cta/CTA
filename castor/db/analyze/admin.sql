0951f4bc5b008e78ab1ea6cfd9e55361d81bc323
Giuseppe Lo Presti <itglp@cern.ch>
2007-04-20 09:41:43 +0000

    Added query for getting the physical space used by a given table

--- castor/db/analyze/admin.sql
+++ castor/db/analyze/admin.sql
@@ -8,7 +8,6 @@ column columnname format a20  -- alphanumeric 20 chars max
 
 -- to create a new user:
 select * from dba_tablespaces;
-
 create user username identified by passwd default tablespace tsname quota 300M on tsname;
 
 grant designer to username;
@@ -17,6 +16,10 @@ grant execute on dbms_lock to username;
 
 -- to take a snapshot of the db for monitoring:
 begin dbms_workload_repository.create_snapshot; end;
+
+-- to get how much space tables use on disk:
+select sum(bytes)/(1024*1024) "MB" from user_extents
+where segment_name='TABLENAME' and segment_type='TABLE';
 
 -- too see deadlocks and sessions
 https://oraweb.cern.ch/pls/castor_dev/webinstance.sessions.show_sessions
