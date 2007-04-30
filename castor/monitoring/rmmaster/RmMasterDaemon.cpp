fff780acfb19793b03876d6d17c39daa188a391d
Dennis Waldron <waldron@cern.ch>
2007-04-30 08:56:50 +0000

    switch to 3 listener threads

--- castor/monitoring/rmmaster/RmMasterDaemon.cpp
+++ castor/monitoring/rmmaster/RmMasterDaemon.cpp
@@ -126,7 +126,7 @@ int main(int argc, char* argv[]) {
         new castor::monitoring::rmmaster::CollectorThread
         (daemon.clusterStatus()),
         port));
-    daemon.getThreadPool('C')->setNbThreads(1);
+    daemon.getThreadPool('C')->setNbThreads(3);
 
     // Heartbeat threadpool
     daemon.addThreadPool
