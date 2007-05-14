2382a721801da9dc2c43a003a9645cd7535764c7
Maria Isabel Martin <mmartins@cern.ch>
2007-05-14 15:13:30 +0000

    Handler for the Update request. It inherits from the StagerJobRequestHandler and it doesn't need to reply to the client. "Heavy handler" when it follows the schedule flow (if the file exists and so on) or "Heavy/intermediate handler" when it follows the same flow that the Put request (stagerService->recreateCastorFile, call to the rm)

---
+++ castor/stager/daemon/UpdateHandler.hpp
@@ -0,0 +1,61 @@
+/****************************************************************************************************************************/
+/* handler for the Update subrequest, since it is jobOriented, it uses the mostly part of the StagerJobRequestHandler class*/
+/* depending if the file exist, it can follow the huge flow (jobOriented, as Get) or a small one                          */
+/*************************************************************************************************************************/
+
+
+#ifndef STAGER_UPDATE_HANDLER_HPP
+#define STAGER_UPDATE_HANDLER_HPP 1
+
+
+#include "castor/stager/dbService/StagerRequestHelper.hpp"
+#include "castor/stager/dbService/StagerCnsHelper.hpp"
+#include "castor/stager/dbService/StagerReplyHelper.hpp"
+
+#include "castor/stager/dbService/StagerRequestHandler.hpp"
+#include "castor/stager/dbService/StagerJobRequestHandler.hpp"
+
+
+#include "stager_uuid.h"
+#include "stager_constants.h"
+#include "serno.h"
+#include "Cns_api.h"
+#include "expert_api.h"
+#include "rm_api.h"
+#include "Cpwd.h"
+#include "Cgrp.h"
+#include "castor/IClientFactory.h"
+#include "castor/stager/SubRequestStatusCodes.hpp"
+
+#include <iostream>
+#include <string>
+
+
+namespace castor{
+  namespace stager{
+    namespace dbService{
+
+      class StagerRequestHelper;
+      class StagerCnsHelper;
+
+      class StagerUpdateRequest : public StagerJobRequestHandler{
+	
+      public:
+	/* constructor */
+	StagerUpdateHandler::StagerUpdateHandler(StagerRequestHelper* stgRequestHelper, StagerCnsHelper* stgCnsHelper, std::string message, bool toRecreateCastorFile) throw();
+	/* destructor */
+	StagerUpdateHandler::~StagerUpdateHandler() throw();
+
+	/* handler for the Update request  */
+	void StagerUpdateHandler::handle(void *param) throw();
+      
+      }//end StagerUpdateHandler class
+
+
+    }//end namespace dbService
+  }//end namespace stager
+}//end namespace castor
+
+
+
+#endif
