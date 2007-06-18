931e44311071ae08d3ba659ac87773df0274aed8
Giuseppe Lo Presti <itglp@cern.ch>
2007-06-18 12:52:50 +0000

    A boiler-plate trailer with only Id2Type and versioning for Repack

---
+++ castor/repack/oracleTrailer.sql
@@ -0,0 +1,29 @@
+/*******************************************************************
+ *
+ * @(#)$RCSfile: oracleTrailer.sql,v $ $Revision: 1.1 $ $Release$ $Date: 2007/06/18 12:52:50 $ $Author: itglp $
+ *
+ * This file contains SQL code that is not generated automatically
+ * and is inserted at the end of the generated code
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *******************************************************************/
+
+/* A small table used to cross check code and DB versions */
+CREATE TABLE CastorVersion (schemaVer VARCHAR2(20), release VARCHAR2(20));
+INSERT INTO CastorVersion VALUES ('2_1_3_16', '2_1_3_16');
+
+/* Sequence for indices */
+CREATE SEQUENCE ids_seq CACHE 300;
+
+/* SQL statements for object types */
+CREATE TABLE Id2Type (id INTEGER PRIMARY KEY, type NUMBER);
+CREATE INDEX I_Id2Type_typeId on Id2Type (type, id);
+
+/* get current time as a time_t. Not that easy in ORACLE */
+CREATE OR REPLACE FUNCTION getTime RETURN NUMBER IS
+  ret NUMBER;
+BEGIN
+  SELECT (SYSDATE - to_date('01-jan-1970 01:00:00','dd-mon-yyyy HH:MI:SS')) * (24*60*60) INTO ret FROM DUAL;
+  RETURN ret;
+END;
+
