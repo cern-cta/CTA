9322994475f0d19632d96716fd145a9200696331
Giuseppe Lo Presti <itglp@cern.ch>
2007-06-27 16:20:03 +0000

    First complete version, to be tested

--- tools/buildRPMs.py
+++ tools/buildRPMs.py
@@ -2,7 +2,7 @@
 import sys, os, shutil, re
 
 def usage():
-    print "Usage: " + sys.argv[0] + " os arch version tarball"
+    print "Usage:", sys.argv[0], "os arch version tarball"
     print "This script is for internal use only. Please use makeRelease.py"
     sys.exit(1)
 
@@ -18,23 +18,25 @@ tarball = sys.argv[4]
 workDir = os.tempname('/build', 'CastorRelease')
 os.mkdir(workDir)
 os.chdir(workDir)
-os.popen('mkdir BUILD RPMS SOURCES SPECS SRPMS RPMS/i386 RPMS/x86_64')
+os.mkdir(['BUILD', 'RPMS', 'SOURCES', 'SPECS', 'SRPMS', 'RPMS/i386', 'RPMS/x86_64'])
 
 # build RPMs
 print 'Building RPMs for ' + targetOs + '/' + targetArch + ' ...'
-rpmOutput = os.popen('RPM_BUILD_ROOT=`pwd` ORACLE_HOME=/afs/cern.ch/project/oracle/@sys/10203 LD_LIBRARY_PATH=$ORACLE_HOME/lib PATH=$ORACLE_HOME/bin:/usr/X11R6/bin:$PATH rpmbuild -ta ' + tarball)
+cmd = 'RPM_BUILD_ROOT=`pwd` ORACLE_HOME=/afs/cern.ch/project/oracle/@sys/10203 LD_LIBRARY_PATH=$ORACLE_HOME/lib PATH=$ORACLE_HOME/bin:/usr/X11R6/bin:$PATH rpmbuild -ta ' + tarball
+rpmOutput = os.popen4(cmd)[1].read()
 m = false
 if rpmOutput.find('Wrote'):
     resultRegExp = re.compile('Wrote: ([\w/]+)castor-(\w+)-' + fullVersion + '.' + targetArch + '.rpm')
     m = resultRegExp.match(rpmOutput)
-if not m:
-    # copy output for debugging
-    
+
+rpmLog = open(workDir + '/castorBuildOutput', 'w')
+rpmLog.write(rpmOutput)
+rpmLog.close()
+if not m || rpmOutput.close() != None:
     print 'The RPM build failed, output in ' + workDir + '/castorBuildOutput. Exiting'
     sys.exit(2)
 
 rpmDir = m.group(1)
-
 # copy RPMs to internal releases space
 intReleaseDir = '/afs/cern.ch/project/cndoc/wwwds/HSM/CASTOR/DIST/intReleases/' + fullVersion
 rpmList = os.listdir(rpmDir)
@@ -45,9 +47,10 @@ for p in rpmList:
 if targetOs = 'SLC3' && targetArch = 'i386':
     # here we also copy the source RPM
     shutil.copyfile(rpmDir + '/../SRPMS/castor-' + fullVersion + '.src.rpm', intReleaseDir)
-    # this is temporary needed until we get rid of GridFTPv1, which is only built for 32 bit  
-    shutil.copyfile(rpmDir + '/castor-lib-' + fullVersion + '.rpm', intReleaseDir + '/SLC4/x86_64')
+    # this is temporarily needed until we get rid of GridFTPv1, which is only built for 32 bit  
+    shutil.copyfile(rpmDir + os.sep + 'castor-lib-' + fullVersion + '.i386.rpm', intReleaseDir + os.sep + 'SLC4/x86_64')
 
 # cleanup
 shutil.rmtree(workDir)
 shutil.rmtree(rpmDir)
+#os.remove(rpmLog)
