7611bf797d662a9357b0f939dc507b0d6d1bfa1e
Sebastien Ponce <sponcec3@cern.ch>
2007-07-11 13:13:40 +0000

    Also export the 32bits SLC3 castor lib to SLC4 64 bits

--- tools/export2swrep.py
+++ tools/export2swrep.py
@@ -29,6 +29,16 @@ minrelease = m.group(4)
 version = str(majversion) + '.' + str(minversion) + '.' + str(majrelease) + '-' + str(minrelease)
 print "Exporting release " + version + " of CASTOR to swrep"
 
+# exports a package
+def export2swrep(dir, arch, rpm):
+    for rpm in os.listdir(dir):
+        cmd = "swrep-soap-client put " + arch + " /cern/cc " + rpm
+        f = os.popen(cmd)
+        out = f.read()
+        if (f.close() != None):
+            print cmd
+            print out
+    
 # send all packages to swrep, with one thread per platform to be faster
 class SWREP (threading.Thread):
     def __init__ (self, archDesc):
@@ -36,13 +46,11 @@ class SWREP (threading.Thread):
         self.arch = archDesc[1]
         self.swrepArch = archDesc[2]
     def run ( self ):
-        dir = intReleaseDir + os.sep + OS + os.sep + arch
-        for rpm in os.listdir(dir):
-            cmd = "swrep-soap-client put " + self.swrepArch + " /cern/cc " + rpm
-            f = os.popen(cmd)
-            out = f.read()
-            if (f.close() != None):
-                print cmd
-                print out
+        export2swrep(intReleaseDir + os.sep + OS + os.sep + arch, self.swrepArch, rpm)
+        if OS == 'SLC3' and arch == 'i386' and rpm.startswith('castor-lib-2'):
+           castorlibslc3i386 = rpm 
+castorlibslc3i386 = ''
 for p in platforms:
     SWREP(p).start()
+# export the 32bit castor-lib in 64 bits
+export2swrep(intReleaseDir + os.sep + 'SLC3' + os.sep + 'i386', 'x86_64_slc4', castorlibslc3i386)
