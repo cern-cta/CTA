9c1767127921270c403f8ead1c768bd6708187e0
Giuseppe Lo Presti <itglp@cern.ch>
2007-07-12 08:38:48 +0000

    deb-packaged gencastor version 7.0.0

--- codeGeneration/gencastor/genCastor.cpp
+++ codeGeneration/gencastor/genCastor.cpp
@@ -30,9 +30,9 @@ static KCmdLineOptions options[] = {
 int main(int argc, char *argv[]) {
 
   KAboutData aboutData("umbrello", I18N_NOOP("Umbrello UML Modeller - Castor edition"),
-	                     "6.0.1", "Umbrello UML Modeller - Castor edition", KAboutData::License_GPL,
-	                     I18N_NOOP("2004-2005 Sebastien Ponce & Giuseppe Lo Presti"), 0,
-	                     "http://cern.ch/castor");
+                       "7.0.0", "Umbrello UML Modeller - Castor edition", KAboutData::License_GPL,
+                       I18N_NOOP("2004-2007 Sebastien Ponce & Giuseppe Lo Presti"), 0,
+                       "http://cern.ch/castor");
   KCmdLineArgs::init( argc, argv, &aboutData );
   KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
 
