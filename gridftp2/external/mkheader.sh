91bb5767d6041496f6499fcfd860af67ece77fb8
Victor Kotlyar <kotlyar@cern.ch>
2007-08-16 14:55:54 +0000

    added favor for x86_64

--- gridftp2/external/mkheader.sh
+++ gridftp2/external/mkheader.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
+FLAVOR=gcc32dbg
+ARCH=`uname -m`
+if [ "${ARCH}" = "x86_64" ]
+then
+   FLAVOR=gcc64dbg
+fi
 
-COMM="$GLOBUS_LOCATION/bin/globus-makefile-header -flavor=gcc32dbg globus_gridftp_server > makefile_header"
+COMM="$GLOBUS_LOCATION/bin/globus-makefile-header -flavor=${FLAVOR} globus_gridftp_server > makefile_header"
 echo ${COMM}
 echo ${COMM} | sh
