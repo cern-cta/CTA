b6af42964c533e7030a171895a7c50d3e8bad697
Sebastien Ponce <sponcec3@cern.ch>
2007-08-28 12:11:37 +0000

    Fixed exportation to swrep

--- tools/export2swrep.py
+++ tools/export2swrep.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 import sys, re, os, threading
 
+def usage():
+    print sys.argv[0] + " <castor version>"
+
 # list of platforms as a tuple (OS, arch)
 platforms = (('SLC4', 'x86_64', 'x86_64_slc4'),
              ('SLC3', 'i386', 'i386_slc3'),
              ('SLC4', 'i386', 'i386_slc4'))
-# internal release repository where to find the packages
-intReleaseDir = '/afs/cern.ch/project/cndoc/wwwds/HSM/CASTOR/DIST/intReleases' + os.sep + fullVersion
 
 # First check the arguments
 if len(sys.argv) != 2:
@@ -29,15 +30,18 @@ minrelease = m.group(4)
 version = str(majversion) + '.' + str(minversion) + '.' + str(majrelease) + '-' + str(minrelease)
 print "Exporting release " + version + " of CASTOR to swrep"
 
+# internal release repository where to find the packages
+intReleaseDir = '/afs/cern.ch/project/cndoc/wwwds/HSM/CASTOR/DIST/intReleases' + os.sep + version
+
 # exports a package
-def export2swrep(dir, arch, rpm):
-    for rpm in os.listdir(dir):
-        cmd = "swrep-soap-client put " + arch + " /cern/cc " + rpm
-        f = os.popen(cmd)
-        out = f.read()
-        if (f.close() != None):
-            print cmd
-            print out
+def export2swrep(arch, rpm):
+    cmd = "swrep-soap-client put " + arch + " /cern/cc " + rpm
+    print cmd
+    f = os.popen(cmd)
+    out = f.read()
+    if (f.close() != None):
+        print cmd
+        print out
     
 # send all packages to swrep, with one thread per platform to be faster
 class SWREP (threading.Thread):
@@ -45,12 +49,15 @@ class SWREP (threading.Thread):
         self.OS = archDesc[0]
         self.arch = archDesc[1]
         self.swrepArch = archDesc[2]
+        threading.Thread.__init__(self)
     def run ( self ):
-        export2swrep(intReleaseDir + os.sep + OS + os.sep + arch, self.swrepArch, rpm)
-        if OS == 'SLC3' and arch == 'i386' and rpm.startswith('castor-lib-2'):
-           castorlibslc3i386 = rpm 
+        dir = intReleaseDir + os.sep + self.OS + os.sep + self.arch
+        for rpm in os.listdir(dir):
+            export2swrep(self.swrepArch, dir + os.sep + rpm)
+            # export the 32bit castor-lib in 64 bits
+            if self.OS == 'SLC3' and self.arch == 'i386' and rpm.startswith('castor-lib-2'):
+                export2swrep('x86_64_slc4', dir + os.sep + rpm)
+
 castorlibslc3i386 = ''
 for p in platforms:
     SWREP(p).start()
-# export the 32bit castor-lib in 64 bits
-export2swrep(intReleaseDir + os.sep + 'SLC3' + os.sep + 'i386', 'x86_64_slc4', castorlibslc3i386)
