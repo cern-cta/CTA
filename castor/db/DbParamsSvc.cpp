2d8c29e5354358c2eafdf04cb0e8d3c5ef4e5e5d
Giuseppe Lo Presti <itglp@cern.ch>
2007-10-17 18:18:12 +0000

    Changed schema version to 2_1_5_0

--- castor/db/DbParamsSvc.cpp
+++ castor/db/DbParamsSvc.cpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: DbParamsSvc.cpp,v $ $Revision: 1.3 $ $Release$ $Date: 2007/10/02 09:46:37 $ $Author: gtaur $
+ * @(#)$RCSfile: DbParamsSvc.cpp,v $ $Revision: 1.4 $ $Release$ $Date: 2007/10/17 18:18:12 $ $Author: itglp $
  *  
  * A service to provide parameters to access the db layer of a Castor application
  *
@@ -33,7 +33,7 @@
 // and provide their versioning.
 namespace castor {
   namespace db {
-    const std::string STAGERSCHEMAVERSION = "2_1_5";
+    const std::string STAGERSCHEMAVERSION = "2_1_5_0";
   }
 }
 
