150531dc830a2e22b534a5ffc7cb106a423688c8
Arne Wiebalck <wiebalck@cern.ch>
2007-11-20 15:41:29 +0000

    Make the timeout on locate configurable. Minimum threshold (and default) is 180 seconds.

--- tape/locate.c
+++ tape/locate.c
@@ -4,7 +4,7 @@
  */
  
 #ifndef lint
-/* static char sccsid[] = "@(#)$RCSfile: locate.c,v $ $Revision: 1.6 $ $Date: 2007/10/25 09:16:31 $ CERN IT-PDP/DM Jean-Philippe Baud"; */
+/* static char sccsid[] = "@(#)$RCSfile: locate.c,v $ $Revision: 1.7 $ $Date: 2007/11/20 15:41:28 $ CERN IT-PDP/DM Jean-Philippe Baud"; */
 #endif /* not lint */
 
 #include <errno.h>
@@ -15,6 +15,7 @@
 #include "Ctape_api.h"
 #include "scsictl.h"
 #include "serrno.h"
+#include <stdlib.h>
 
 extern int send_scsi_cmd( int, char *, int, char *, int, char *, int, char *, int, int, int, int *, char **);
 
@@ -28,13 +29,33 @@ unsigned char *blockid;
 	char *msgaddr;
 	int nb_sense_ret;
 	char sense[MAXSENSE];
+        int locate_timeout = LOCATE_MIN_TIMEOUT; 
+        char* getconfent(); 
+        char * p = NULL;
 
 	ENTRY (locate);
 	memset (cdb, 0, sizeof(cdb));
 	cdb[0] = 0x2B;		/* locate */
 	memcpy (&cdb[3], blockid, 4);
+        
+        p = getconfent ("TAPE", "LOCATE_TIMEOUT", 0);
+        if (NULL != p) {                
+                locate_timeout = (int)atoi(p);
+                /* 
+                   atoi()'s behaviour is undefined if
+                   the value is not convertible. For 
+                   this reason (and in order to prevent
+                   to give up too early), reset the 
+                   timeout to the lower threshold if 
+                   it is too small.
+                */   
+                if (locate_timeout < LOCATE_MIN_TIMEOUT) {
+                        locate_timeout = LOCATE_MIN_TIMEOUT;
+                }
+        }
+                
 	if (send_scsi_cmd (tapefd, path, 0, cdb, 10, NULL, 0,
-	    sense, 38, 900000, SCSI_NONE, &nb_sense_ret, &msgaddr) < 0) {
+	    sense, 38, locate_timeout, SCSI_NONE, &nb_sense_ret, &msgaddr) < 0) {
 		usrmsg (func, "%s", msgaddr);
 		RETURN (-1);
 	}
