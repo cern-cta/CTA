eb64ed9921cb2db187fe4c0986c9127e27ae7aea
Giuseppe Lo Presti <itglp@cern.ch>
2007-11-20 18:14:14 +0000

    Catch exception on subRequestToDo

--- castor/stager/daemon/RequestSvc.cpp
+++ castor/stager/daemon/RequestSvc.cpp
@@ -71,14 +71,22 @@ namespace castor{
         castor::stager::IStagerSvc* stgService =
           dynamic_cast<castor::stager::IStagerSvc*>(svc);
         // we have already initialized it in the main, so we know the pointer is valid
-        IObject* s = stgService->subRequestToDo(m_name);
-        if(s != 0) {
-          castor::dlf::Param params[]={ castor::dlf::Param("SubRequestId", s->id()),
-              castor::dlf::Param("Thread pool", m_name)
-          };
-          castor::dlf::dlf_writep(nullCuuid, DLF_LVL_DEBUG, STAGER_SUBREQ_SELECTED, 2, params);	    
+        try {
+          IObject* s = stgService->subRequestToDo(m_name);
+          if(s != 0) {
+            castor::dlf::Param params[]={ castor::dlf::Param("SubRequestId", s->id()),
+                castor::dlf::Param("Thread pool", m_name)
+            };
+            castor::dlf::dlf_writep(nullCuuid, DLF_LVL_DEBUG, STAGER_SUBREQ_SELECTED, 2, params);	    
+          }
+          return s;
+        } catch (castor::exception::Exception e) {
+          castor::dlf::Param params[] =
+            {castor::dlf::Param("Code", sstrerror(e.code())),
+             castor::dlf::Param("Message", e.getMessage().str())};
+          castor::dlf::dlf_writep(nullCuuid, DLF_LVL_ERROR, castor::stager::dbService::STAGER_DAEMON_EXCEPTION, 2, params);
+          return 0;
         }
-        return s;
       }
      
      
