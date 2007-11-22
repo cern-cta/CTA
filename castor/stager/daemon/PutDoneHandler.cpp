d562423f70bc90094d780ef4acb9a6a20a9ed3fc
Giuseppe Lo Presti <itglp@cern.ch>
2007-11-22 18:07:51 +0000

    Set status to archived when putDone was ok

--- castor/stager/daemon/PutDoneHandler.cpp
+++ castor/stager/daemon/PutDoneHandler.cpp
@@ -57,65 +57,66 @@ namespace castor{
       
       StagerPutDoneHandler::StagerPutDoneHandler(StagerRequestHelper* stgRequestHelper) throw(castor::exception::Exception)
       {     	
-	this->stgRequestHelper = stgRequestHelper;
-	this->typeRequest = OBJ_StagePutDoneRequest;
-
-	
+        this->stgRequestHelper = stgRequestHelper;
+        this->typeRequest = OBJ_StagePutDoneRequest;
+        
+        
       }
-
-
+      
+      
       void StagerPutDoneHandler::handle() throw(castor::exception::Exception)
       {
-	StagerReplyHelper* stgReplyHelper= NULL;
-	try{
-
-	  /**************************************************************************/
-	  /* common part for all the handlers: get objects, link, check/create file*/
-	  
-	  stgRequestHelper->logToDlf(DLF_LVL_DEBUG, STAGER_PUTDONE, &(stgCnsHelper->cnsFileid));
-
-	  jobOriented();/* until it will be explored */
-	 
-	  
-	  switch(stgRequestHelper->stagerService->processPutDoneRequest(stgRequestHelper->subrequest)) {
-      
-      case -1:  // request put in wait
-        stgRequestHelper->logToDlf(DLF_LVL_SYSTEM, STAGER_WAITSUBREQ, &(stgCnsHelper->cnsFileid));
-        break;
-      
-      case 0:   // error
-        stgRequestHelper->logToDlf(DLF_LVL_USER_ERROR, STAGER_UNABLETOPERFORM, &(stgCnsHelper->cnsFileid));
-        break;
-      
-      case 1:   // ok
-        /* we archive the subrequest */
-        stgRequestHelper->stagerService->archiveSubReq(stgRequestHelper->subrequest->id());
-        
-        /* replyToClient Part: */
-        stgReplyHelper = new StagerReplyHelper();
-        stgReplyHelper->setAndSendIoResponse(stgRequestHelper,&(stgCnsHelper->cnsFileid), 0,  "No error");
-        stgReplyHelper->endReplyToClient(stgRequestHelper);
-        delete stgReplyHelper;
-        break;
-    }
-      
-	}
-  catch(castor::exception::Exception e){
-	  if(stgReplyHelper != NULL) delete stgReplyHelper;
-	  
-	  castor::dlf::Param params[]={castor::dlf::Param("Error Code",sstrerror(e.code())),
-				       castor::dlf::Param("Error Message",e.getMessage().str())
-	  };
-	  
-	  castor::dlf::dlf_writep(stgRequestHelper->requestUuid, DLF_LVL_ERROR, STAGER_PUTDONE, 2 ,params, &(stgCnsHelper->cnsFileid));
-	  throw(e);
-	}
+        StagerReplyHelper* stgReplyHelper= NULL;
+        try{
+          
+          /**************************************************************************/
+          /* common part for all the handlers: get objects, link, check/create file*/
+          
+          stgRequestHelper->logToDlf(DLF_LVL_DEBUG, STAGER_PUTDONE, &(stgCnsHelper->cnsFileid));
+          
+          jobOriented();/* until it will be explored */
+          
+          
+          switch(stgRequestHelper->stagerService->processPutDoneRequest(stgRequestHelper->subrequest)) {
+            
+            case -1:  // request put in wait
+            stgRequestHelper->logToDlf(DLF_LVL_SYSTEM, STAGER_WAITSUBREQ, &(stgCnsHelper->cnsFileid));
+            break;
+            
+            case 0:   // error
+            stgRequestHelper->logToDlf(DLF_LVL_USER_ERROR, STAGER_UNABLETOPERFORM, &(stgCnsHelper->cnsFileid));
+            break;
+            
+            case 1:   // ok
+            /* we archive the subrequest */
+            stgRequestHelper->subrequest->setStatus(SUBREQUEST_ARCHIVED);
+            stgRequestHelper->stagerService->archiveSubReq(stgRequestHelper->subrequest->id());
+            
+            /* replyToClient Part: */
+            stgReplyHelper = new StagerReplyHelper();
+            stgReplyHelper->setAndSendIoResponse(stgRequestHelper,&(stgCnsHelper->cnsFileid), 0,  "No error");
+            stgReplyHelper->endReplyToClient(stgRequestHelper);
+            delete stgReplyHelper;
+            break;
+          }
+          
+        }
+        catch(castor::exception::Exception e){
+          if(stgReplyHelper != NULL) delete stgReplyHelper;
+          
+          castor::dlf::Param params[]={castor::dlf::Param("Error Code",sstrerror(e.code())),
+            castor::dlf::Param("Error Message",e.getMessage().str())
+          };
+          
+          castor::dlf::dlf_writep(stgRequestHelper->requestUuid, DLF_LVL_ERROR, STAGER_PUTDONE, 2 ,params, &(stgCnsHelper->cnsFileid));
+          throw(e);
+        }
       }
-
-
-
-
-
+      
+      
+      
+      
+      
     }//end namespace dbService
   }//end namespace stager
 }//end namespace castor
