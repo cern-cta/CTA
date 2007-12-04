9b29707405cef943b02fb9a44ce19dccbb20e385
Giulia Taurelli <gtaur@cern.ch>
2007-12-04 14:16:24 +0000

    used standard name convention and provided example files for the policies

---
+++ debian/rechandler.py
@@ -0,0 +1,15 @@
+##########################################################################
+#                                                                        #
+#                           CASTOR POLICY SERVICE                        #
+#                        Example For Recall Policy File                  #
+#                                                                        #
+##########################################################################
+
+# Modules
+import sys
+import os
+
+def globalRecallPolicy(vid, numFiles, dataVolume, oldestTime):
+    return 1
+
+# End-of-File
