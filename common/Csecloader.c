8ae88ebb6c439db71a2dff427220e1eb0fb7acd5
Rosa Maria Garcia Rioja <riojac3@cern.ch>
2007-12-05 13:57:18 +0000

    Provisional security dymanice loader

---
+++ common/Csecloader.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include <Csec_api.h>
+
+
+   static void *handle;
+   static int (*Cclient_initContext)(Csec_context_t *, int,Csec_protocol *);
+   static int (*Cclient_establishContext)(Csec_context_t *,int);
+   static int (*Cserver_initContext)(Csec_context_t *,int,Csec_protocol *);
+   static int (*CclearContext)(Csec_context_t *);
+   static int (*Cserver_establishContext)(Csec_context_t *,int);
+   static int (*CgetClientId)(Csec_context_t *, char **, char **);
+   static int (*CmapUser)(const char *, const char *, char *, size_t, uid_t *, gid_t *);
+   static int loaded=0;
+   //double (*cosine)(double);
+   char *error;
+
+  /* void executer(Csec_context_t *);
+   int loader();
+   void close();
+ */
+/*int main(int argc, char **argv) {
+   Csec_context_t m_security_context;
+
+   printf("voy a cargar la libreria\n");
+   loader();
+   printf("voy a ejecurtar\n");
+   executer(&m_security_context);
+   printf("voy a cerrar\n");
+   close();
+}
+*/
+  
+int loader(){
+   if (loaded==1)
+     return 1; // is already loaded
+
+   handle = dlopen ("libsecurity.so", RTLD_LAZY);
+   if (!handle) {
+      fprintf (stderr, "%s\n", dlerror());
+      return -1;
+   }
+   dlerror();    /* Clear any existing error */
+   //*(void **) (&cosine) = dlsym(handle, "cos");
+   *(void **) (&Cclient_initContext) = dlsym(handle, "Csec_client_initContext");
+   *(void **) (&Cserver_initContext) = dlsym(handle, "Csec_server_initContext");
+   *(void **) (&Cclient_establishContext) = dlsym(handle, "Csec_client_establishContext");
+   *(void **) (&Cserver_establishContext) = dlsym(handle, "Csec_server_establishContext");
+   *(void **) (&CclearContext) = dlsym(handle, "Csec_clearContext");
+   *(void **) (&CgetClientId) = dlsym(handle, "Csec_server_getClientId");
+   *(void **) (&CmapUser) = dlsym(handle, "Csec_mapToLocalUser");
+    
+    
+   if ((error = dlerror()) != NULL)  {
+     fprintf (stderr, "%s\n", error);
+     return -1;
+   }
+  
+  loaded=1;
+  return 0;
+}
+
+int getServer_initContext(Csec_context_t * security_context, int service_type, Csec_protocol * protocol){
+  return (*Cserver_initContext)(security_context, service_type, protocol);
+  
+}
+
+int getClient_initContext(Csec_context_t *security_context, int service_type,Csec_protocol *protocol){
+  return (*Cclient_initContext)(security_context, service_type , protocol);
+}
+
+int getClient_establishContext(Csec_context_t *security_context ,int sock){
+  return(*Cclient_establishContext)(security_context,sock);
+}
+
+int getServer_establishContext(Csec_context_t *security_context ,int sock){
+ return (*Cserver_establishContext)(security_context,sock);
+}
+
+int getClientId(Csec_context_t *security_context , char **mech, char **name){
+ return (*CgetClientId)(security_context, mech, name);
+}
+
+int getMapUser(const char *mech, const char *principal, char *username, size_t usernamesize, uid_t *euid, gid_t *egid){ 
+ return (*CmapUser)(mech,principal, NULL,0, euid,egid);
+}
+
+int getClearContext(Csec_context_t *security_context){
+ return  (*CclearContext)(security_context);
+}
+/*
+void executer(Csec_context_t *  security_context){   
+
+   printf("%d \n",(*Cclient_initContext)(security_context,0,NULL));
+   int sol=getClient_initContext(security_context,0,NULL);
+   printf("%d \n",sol);
+   //printf ("%f\n", (*cosine)(2.0));
+ //  dlclose(handle);
+   //return 0;
+}
+
+void close(){
+  dlclose(handle);
+}
+
+
+
+
+int main(int argc, char **argv) {
+   Csec_context_t m_security_context;
+
+   printf("voy a cargar la libreria\n");
+   loader();
+   printf("voy a ejecurtar\n");
+   executer(&m_security_context);
+   printf("voy a cerrar\n");
+   close();
+}
+*/
+
+
