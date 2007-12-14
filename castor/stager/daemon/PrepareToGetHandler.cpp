8b0a0d773fd6b97bdc47976ee156d802ef18444d
Giuseppe Lo Presti <itglp@cern.ch>
2007-12-14 17:24:54 +0000

    Answer to client when request is put in wait

--- castor/stager/daemon/PrepareToGetHandler.cpp
+++ castor/stager/daemon/PrepareToGetHandler.cpp
@@ -65,6 +65,7 @@ namespace castor{
         switch(stgRequestHelper->stagerService->processPrepareRequest(stgRequestHelper->subrequest)) {
           case -2:
             stgRequestHelper->logToDlf(DLF_LVL_SYSTEM, STAGER_WAITSUBREQ, &(stgCnsHelper->cnsFileid));
+            result = true;
             break;
           
           case -1:
