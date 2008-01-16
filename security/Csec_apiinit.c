6a40652e8bce90b61de90bc551d71e02a3b04039
Rosa Maria Garcia Rioja <riojac3@cern.ch>
2008-01-16 15:37:29 +0000

    Sources taken from DPM CVS

---
+++ security/Csec_apiinit.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2003 by CERN/IT/ADC/CA
+ * All rights reserved
+ */
+ 
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: Csec_apiinit.c,v $ $Revision: 1.6 $ $Date: 2008/01/16 15:37:28 $ CERN IT-ADC-CA Benjamin Couturier";
+#endif /* not lint */
+
+/*	Csec_apiinit - allocate thread specific or global structures */
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include "Cglobals.h"
+#include "serrno.h"
+
+#include "Csec.h"
+
+static int Csec_api_key = -1;
+
+int DLL_DECL
+Csec_apiinit(thip)
+     struct Csec_api_thread_info **thip;
+{
+  Cglobals_get (&Csec_api_key,
+		(void **) thip, sizeof(struct Csec_api_thread_info));
+
+  if (*thip == NULL) {
+    fprintf(stderr, "ENOMEM\n");
+    serrno = ENOMEM;
+    return (-1);
+  }
+
+  if ((*thip)->init_done == 0) {
+    (*thip)->init_done = 1;
+    /* BEWARE the init_done must be done BEFORE the call to
+       setup_trace(), because setup_trace calls apiinit itself ! */
+    Csec_setup_trace();
+  }
+    
+  return (0);
+}
+
---
+++ security/Csec_errmsg.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2003 by CERN/IT/ADC/CA
+ * All rights reserved
+ */
+
+#ifndef lint
+static char sccsid[] = "@(#)$RCSfile: Csec_errmsg.c,v $ $Revision: 1.6 $ $Date: 2008/01/16 15:37:28 $ CERN IT/ADC/CA Benjamin Couturier";
+#endif /* not lint */
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/types.h>
+
+#include "Csec.h"
+#include "serrno.h"
+
+/* Csec_errmsg - send error message to error buffer in per-thread structure */
+/* should preserve errno, serrno */
+int DLL_DECL
+Csec_errmsg(char *func, char *msg, ...) {
+  
+  va_list args;
+  int save_errno, save_serrno;
+  struct Csec_api_thread_info *thip;
+  int funlen=0;
+  
+  save_errno = errno;
+  save_serrno = serrno;
+
+  if (Csec_apiinit (&thip))
+    return (-1);
+  va_start (args, msg);
+  
+  if (func) {
+    snprintf (thip->errbuf, ERRBUFSIZE, "%s: ", func);
+  }
+   
+  funlen = strlen(thip->errbuf);
+  
+  vsnprintf (thip->errbuf + funlen, ERRBUFSIZE - funlen -1, msg, args);
+  thip->errbuf[ERRBUFSIZE] = '\0';
+    
+  Csec_trace("ERROR", "%s\n", thip->errbuf);
+  
+  errno = save_errno;
+  serrno = save_serrno;
+  return (0);
+}
+
+
+
+/* Csec_errErrorMessage - send error message to user defined client buffer or to stderr */
+char *Csec_getErrorMessage() {
+
+  struct Csec_api_thread_info *thip;
+  if (Csec_apiinit (&thip))
+    return NULL;
+
+  return thip->errbuf;
+
+}
