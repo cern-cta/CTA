e9cc9ce6763a74ad68454a49bf7fddf9e6cdf99e
Dennis Waldron <waldron@cern.ch>
2008-01-21 15:25:16 +0000

    Added parseCommandLine back, it is not overriden in the MainThread. The command line is parsed twice

--- castor/job/diskcopy/DiskCopyTransfer.cpp
+++ castor/job/diskcopy/DiskCopyTransfer.cpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: DiskCopyTransfer.cpp,v $ $Revision: 1.3 $ $Release$ $Date: 2008/01/21 15:16:45 $ $Author: itglp $
+ * @(#)$RCSfile: DiskCopyTransfer.cpp,v $ $Revision: 1.4 $ $Release$ $Date: 2008/01/21 15:25:16 $ $Author: waldron $
  *
  * @author Dennis Waldron
  *****************************************************************************/
@@ -57,6 +57,7 @@ int main(int argc, char *argv[]) {
 
     // Start daemon
     daemon.setForeground(true);
+    daemon.parseCommandLine(argc, argv);
     daemon.start();
     return 0;
     
