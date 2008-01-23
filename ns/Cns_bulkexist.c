046fbe00a84c2a02c3b58c9c5ef842c7bd334e99
Dennis Waldron <waldron@cern.ch>
2008-01-23 10:26:50 +0000

    Fixed indentation

--- ns/Cns_bulkexist.c
+++ ns/Cns_bulkexist.c
@@ -21,54 +21,54 @@
 int DLL_DECL
 Cns_bulkexist(const char* server, u_signed64 *fileIds, int *nbFileIds)
 {
-  char func[16];
-  int msglen;
-  char *rbp;
-  char *repbuf;
-  char *sbp;
-  char* sendbuf;
-  int i, c;
-  gid_t gid;
-  uid_t uid;
-  struct Cns_api_thread_info *thip;
-
-  strcpy (func, "Cns_bulkexist");
-  if (Cns_apiinit (&thip))
-    return (-1);
-  Cns_getid(&uid, &gid);
+	char func[16];
+	int msglen;
+	char *rbp;
+	char *repbuf;
+	char *sbp;
+	char* sendbuf;
+	int i, c;
+	gid_t gid;
+	uid_t uid;
+	struct Cns_api_thread_info *thip;
 	
-  /* Build request header */
-  msglen = 6 * LONGSIZE + (*nbFileIds) * HYPERSIZE;
-  sendbuf = (char*) malloc(msglen);
-  sbp = sendbuf;
-  marshall_LONG (sbp, CNS_MAGIC2);
-  marshall_LONG (sbp, CNS_BULKEXIST);
-  marshall_LONG (sbp, msglen);
-  marshall_LONG (sbp, uid);
-  marshall_LONG (sbp, gid);
-  marshall_LONG (sbp, *nbFileIds);
- 
-  /* Build request body */
-  for (i = 0; i < *nbFileIds; i++) {
-    marshall_HYPER (sbp, fileIds[i]);
-  }
-  
-  repbuf = (char*) malloc(msglen);
-  while ((c = send2nsd (NULL, (char*)server, sendbuf, msglen, repbuf, msglen)) &&
-         serrno == ENSNACT)
-    sleep (RETRYI);
-
-  if (c == 0) {
-    rbp = repbuf;
-    unmarshall_LONG (rbp, *nbFileIds);
-    for (i = 0; i < *nbFileIds; i++) {
-      unmarshall_HYPER (rbp, fileIds[i]);
-    }
-  } else {
-    *nbFileIds = 0;
-  }
-  free(sendbuf);
-  free(repbuf);
-  return (c);
+	strcpy (func, "Cns_bulkexist");
+	if (Cns_apiinit (&thip))
+		return (-1);
+	Cns_getid(&uid, &gid);
+	
+	/* Build request header */
+	msglen = 6 * LONGSIZE + (*nbFileIds) * HYPERSIZE;
+	sendbuf = (char*) malloc(msglen);
+	sbp = sendbuf;
+	marshall_LONG (sbp, CNS_MAGIC2);
+	marshall_LONG (sbp, CNS_BULKEXIST);
+	marshall_LONG (sbp, msglen);
+	marshall_LONG (sbp, uid);
+	marshall_LONG (sbp, gid);
+	marshall_LONG (sbp, *nbFileIds);
+	
+	/* Build request body */
+	for (i = 0; i < *nbFileIds; i++) {
+		marshall_HYPER (sbp, fileIds[i]);
+	}
+	
+	repbuf = (char*) malloc(msglen);
+	while ((c = send2nsd (NULL, (char*)server, sendbuf, msglen, repbuf, msglen)) &&
+	       serrno == ENSNACT)
+		sleep (RETRYI);
+	
+	if (c == 0) {
+		rbp = repbuf;
+		unmarshall_LONG (rbp, *nbFileIds);
+		for (i = 0; i < *nbFileIds; i++) {
+			unmarshall_HYPER (rbp, fileIds[i]);
+		}
+	} else {
+		*nbFileIds = 0;
+	}
+	free(sendbuf);
+	free(repbuf);
+	return (c);
 }
 
