974df4136c40fa08664876faab5b074c1a5b4684
Rosa Maria Garcia Rioja <riojac3@cern.ch>
2008-02-28 11:11:02 +0000

    Moved attributes to the parent

--- castor/stager/GetUpdateStartRequest.cpp
+++ castor/stager/GetUpdateStartRequest.cpp
@@ -41,8 +41,6 @@
 //------------------------------------------------------------------------------
 castor::stager::GetUpdateStartRequest::GetUpdateStartRequest() throw() :
   StartRequest(),
-  m_fileId(0),
-  m_nsHost(""),
   m_id(0) {
 }
 
@@ -67,8 +65,6 @@ void castor::stager::GetUpdateStartRequest::print(std::ostream& stream,
   // Call print on the parent class(es)
   this->StartRequest::print(stream, indent, alreadyPrinted);
   // Output of all members
-  stream << indent << "fileId : " << m_fileId << std::endl;
-  stream << indent << "nsHost : " << m_nsHost << std::endl;
   stream << indent << "id : " << m_id << std::endl;
   alreadyPrinted.insert(this);
 }
