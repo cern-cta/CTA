5fada0d6b6b807110a2d2c6f7caaa6d1fbf91b01
Steven Murray <murrayc3@cern.ch>
2008-02-29 13:17:59 +0000

    dropAllObjects now generates the sql commands to be executed to drop all the database objects as opposed to actually dropping them

--- castor/vdqm/dropAllObjects.sql
+++ castor/vdqm/dropAllObjects.sql
@@ -1,38 +1,24 @@
-/*
+set feedback OFF
+set pagesize 0
+set linesize 1000
+prompt -- Drop functions
 select 'drop ' || object_type || ' ' || object_name || ';' as command
 from user_procedures where object_type='FUNCTION' order by object_name;
+prompt
+prompt -- Drop procedures
 select 'drop ' || object_type || ' ' || object_name || ';' from user_procedures where object_type='PROCEDURE' order by object_name;
+prompt
+prompt -- Drop packages
 select 'drop ' || object_type || ' ' || object_name || ';' from user_procedures where object_type='PACKAGE' order by object_name;
+prompt
+prompt -- Drop views
 select 'drop view ' || view_name || ';' from user_views order by view_name;
+prompt
+prompt -- Drop sequences
 select 'drop sequence ' || SEQUENCE_NAME || ';' from user_sequences order by sequence_name;
+prompt
+prompt -- Drop foreign key constraints
+select 'alter table ' || table_name || ' drop constraint ' || constraint_name || ';' from user_constraints where constraint_type='R';
+prompt
+prompt -- Drop tables
 select 'drop table ' || table_name || ';' from user_tables order by table_name;
-*/
-
-drop FUNCTION GETTIME;
-
-drop PROCEDURE ALLOCATEDRIVE;
-drop PROCEDURE REQUESTTOSUBMIT;
-drop PROCEDURE REUSETAPEALLOCATION;
-
-drop PACKAGE CASTORVDQM;
-
-drop view CANDIDATEDRIVEALLOCATIONS_VIEW;
-drop view TAPEDRIVESHOWQUEUES_VIEW;
-drop view TAPEREQUESTSSHOWQUEUES_VIEW;
-
-drop sequence IDS_SEQ;
-
-drop table CLIENTIDENTIFICATION;
-drop table DEVICEGROUPNAME;
-drop table ERRORHISTORY;
-drop table ID2TYPE;
-drop table TAPEACCESSSPECIFICATION;
-drop table TAPEDRIVE2TAPEDRIVECOMP;
-drop table TAPEDRIVECOMPATIBILITY;
-drop table TAPEDRIVEDEDICATION;
-drop table TAPEREQUEST;
-drop table TAPESERVER;
-drop table VDQMTAPE;
--- Drop TAPEDRIVE last because it is referenced by foreign keys
-drop table TAPEDRIVE;
-
