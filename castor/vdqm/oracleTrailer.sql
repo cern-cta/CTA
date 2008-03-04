0797e3f4d2bdbb6b9346983565282115f6799c71
Steven Murray <murrayc3@cern.ch>
2008-03-04 11:00:15 +0000

    Added object id foreign keys

--- castor/vdqm/oracleTrailer.sql
+++ castor/vdqm/oracleTrailer.sql
@@ -1,12 +1,19 @@
 /*******************************************************************
  *
- * @(#)$RCSfile: oracleTrailer.sql,v $ $Revision: 1.25 $ $Release$ $Date: 2008/02/29 14:07:24 $ $Author: murrayc3 $
+ * @(#)$RCSfile: oracleTrailer.sql,v $ $Revision: 1.26 $ $Release$ $Date: 2008/03/04 11:00:15 $ $Author: murrayc3 $
  *
  * This file contains SQL code that is not generated automatically
  * and is inserted at the end of the generated code
  *
  * @author Castor Dev team, castor-dev@cern.ch
  *******************************************************************/
+
+/* SQL statements for object types */
+CREATE TABLE Id2Type (
+  id   INTEGER,
+  type NUMBER,
+  CONSTRAINT PK_Id2Type PRIMARY KEY (id));
+CREATE INDEX I_Id2Type_typeId on Id2Type (type, id);
 
 /* Enumerations */
 CREATE TABLE TapeServerStatusCodes (
@@ -57,17 +64,33 @@ INSERT INTO TapeRequestStatusCodes VALUES (4, 'REQUEST_FAILED');
 COMMIT;
 
 /* Foreign key constraints */
-ALTER TABLE TapeServer
-  ADD CONSTRAINT FK_TapeServer_actingMode
-    FOREIGN KEY (actingMode)
-    REFERENCES TapeServerStatusCodes (id);
+ALTER TABLE ClientIdentification
+  ADD CONSTRAINT FK_ClientIdentification_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type(id);
 
-ALTER TABLE TapeDriveCompatibility
-  ADD CONSTRAINT FK_TapeDriveComp_accessSpec
-    FOREIGN KEY (tapeAccessSpecification)
-    REFERENCES TapeAccessSpecification (id);
+ALTER TABLE DeviceGroupName
+  ADD CONSTRAINT FK_DeviceGroupName_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type(id);
+
+ALTER TABLE ErrorHistory
+  ADD CONSTRAINT FK_ErrorHistory_tapeDrive
+    FOREIGN KEY (tapeDrive)
+    REFERENCES TapeDrive (id)
+  ADD CONSTRAINT FK_ErrorHistory_tape
+    FOREIGN KEY (tape)
+    REFERENCES VdqmTape (id);
+
+ALTER TABLE TapeAccessSpecification
+  ADD CONSTRAINT FK_TapeAccessSpecification_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type (id);
 
 ALTER TABLE TapeDrive
+  ADD CONSTRAINT FK_TapeDrive_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type (id)
   ADD CONSTRAINT FK_TapeDrive_tape
     FOREIGN KEY (tape)
     REFERENCES VdqmTape (id)
@@ -84,7 +107,26 @@ ALTER TABLE TapeDrive
     FOREIGN KEY (tapeServer)
     REFERENCES TapeServer (id);
 
+ALTER TABLE TapeDriveCompatibility
+  ADD CONSTRAINT FK_TapeDriveCompatibility_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type (id)
+  ADD CONSTRAINT FK_TapeDriveComp_accessSpec
+    FOREIGN KEY (tapeAccessSpecification)
+    REFERENCES TapeAccessSpecification (id);
+
+ALTER TABLE TapeDriveDedication
+  ADD CONSTRAINT FK_TapeDriveDedication_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type (id)
+  ADD CONSTRAINT FK_TapeDriveDedic_tapeDrive
+    FOREIGN KEY (tapeDrive)
+    REFERENCES TapeDrive (id);
+
 ALTER TABLE TapeRequest
+  ADD CONSTRAINT FK_TapeRequest_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type (id)
   ADD CONSTRAINT FK_TapeRequest_tape
     FOREIGN KEY (tape)
     REFERENCES VdqmTape (id)
@@ -107,20 +149,18 @@ ALTER TABLE TapeRequest
     FOREIGN KEY (client)
     REFERENCES ClientIdentification (id);
 
-ALTER TABLE ErrorHistory
-  ADD CONSTRAINT FK_ErrorHistory_tapeDrive
-    FOREIGN KEY (tapeDrive)
-    REFERENCES TapeDrive (id)
-  ADD CONSTRAINT FK_ErrorHistory_tape
-    FOREIGN KEY (tape)
-    REFERENCES VdqmTape (id);
-
-ALTER TABLE TapeDriveDedication
-  ADD CONSTRAINT FK_TapeDriveDedic_tapeDrive
-    FOREIGN KEY (tapeDrive)
-    REFERENCES TapeDrive (id);
+ALTER TABLE TapeServer
+  ADD CONSTRAINT FK_TapeServer_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type (id)
+  ADD CONSTRAINT FK_TapeServer_actingMode
+    FOREIGN KEY (actingMode)
+    REFERENCES TapeServerStatusCodes (id);
 
 ALTER TABLE VdqmTape
+  ADD CONSTRAINT FK_VdqmTape_id
+    FOREIGN KEY (id)
+    REFERENCES Id2Type (id)
   ADD CONSTRAINT FK_VdqmTape_status
     FOREIGN KEY (status)
     REFERENCES TapeStatusCodes (id);
@@ -145,13 +185,6 @@ UPDATE CastorVersion SET schemaVersion = '2_1_6_0';
 
 /* Sequence for indices */
 CREATE SEQUENCE ids_seq CACHE 200;
-
-/* SQL statements for object types */
-CREATE TABLE Id2Type (
-  id   INTEGER,
-  type NUMBER,
-  CONSTRAINT PK_Id2Tape PRIMARY KEY (id));
-CREATE INDEX I_Id2Type_typeId on Id2Type (type, id);
 
 /* get current time as a time_t. Not that easy in ORACLE */
 CREATE OR REPLACE FUNCTION getTime RETURN NUMBER IS
