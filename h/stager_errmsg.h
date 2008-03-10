571e170cc1003569d5cfba43d5d156130aeb335c
Giuseppe Lo Presti <itglp@cern.ch>
2008-03-10 17:25:17 +0000

    Fixed headers and made them compilable with C++

--- h/stager_errmsg.h
+++ h/stager_errmsg.h
@@ -7,13 +7,21 @@
 
 #include "osdep.h"
 
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
 int DLL_DECL stager_seterrbuf _PROTO((char *, int));
 int DLL_DECL stager_setoutbuf _PROTO((char *, int));
 int DLL_DECL stager_geterrbuf _PROTO((char **, int *));
 int DLL_DECL stager_getoutbuf _PROTO((char **, int *));
 int DLL_DECL stager_setlog _PROTO((void (*) _PROTO((int, char *))));
 int DLL_DECL stager_getlog _PROTO((void (**) _PROTO((int, char *))));
-int DLL_DECL stager_errmsg _PROTO((char *, char *, ...));
-int DLL_DECL stager_outmsg _PROTO((char *, char *, ...));
+int DLL_DECL stager_errmsg _PROTO((const char *, const char *, ...));
+int DLL_DECL stager_outmsg _PROTO((const char *, const char *, ...));
+
+#ifdef __cplusplus
+ }
+#endif
 
 #endif /* __stager_errmsg_h */
