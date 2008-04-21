57430fbdbb2204593df54cd34c3ac296a3374e66
Rosa Maria Garcia Rioja <riojac3@cern.ch>
2008-04-21 14:54:16 +0000

    Pass the Euid and Egid to the diskserver for security porposes

--- castor/jobmanager/SubmissionProcess.cpp
+++ castor/jobmanager/SubmissionProcess.cpp
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: SubmissionProcess.cpp,v $ $Revision: 1.20 $ $Release$ $Date: 2008/03/27 18:23:56 $ $Author: waldron $
+ * @(#)$RCSfile: SubmissionProcess.cpp,v $ $Revision: 1.21 $ $Release$ $Date: 2008/04/21 14:54:16 $ $Author: riojac3 $
  *
  * The Submission Process is used to submit new jobs into the scheduler. It is
  * run inside a separate process allowing for setuid and setgid calls to take
@@ -422,6 +422,8 @@ void castor::jobmanager::SubmissionProcess::submitJob
 	<< ((request->ipAddress() & 0x000000FF))
 	<< ":" << request->port() << "\""
 	<< " " << request->clientSecure()
+        << " " << request->euid()
+	<< " " << request->egid()
 	<< " " << request->requestCreationTime();
   }
   strncpy(command, cmd.str().c_str(), MAX_CMD_DESC_LEN);
