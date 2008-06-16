7e1dd7221738e05da72b13ee06bbfdfe46dc3b23
Dennis Waldron <waldron@cern.ch>
2008-06-16 07:47:58 +0000

    Updated

--- castor/db/castor_oracle_grant.sql
+++ castor/db/castor_oracle_grant.sql
@@ -1,3 +1,29 @@
+/******************************************************************************
+ *              castor_oracle_grant.sql
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)$RCSfile$ $Release: 1.2 $ $Release$ $Date$ $Author$
+ *
+ * This script grant access to all tables through the castor_read account
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
 DECLARE
   unused NUMBER;
 BEGIN
@@ -15,3 +41,6 @@ BEGIN
 EXCEPTION WHEN NO_DATA_FOUND THEN
   NULL;
 END;
+
+
+/* End-of-File */
