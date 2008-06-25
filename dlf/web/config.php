6c3b8b8b75adddde7fdb45e03585e432c9d9a96c
Dennis Waldron <waldron@cern.ch>
2008-06-25 12:44:01 +0000

    Removed the migrselectpolicy column from the svcclass table of the stager database viewer

--- dlf/web/config.php
+++ dlf/web/config.php
@@ -242,7 +242,7 @@ $stager_sql_tables = array(
     "table"	=> "svcclass",
     "lookup"	=> "id",
     "fields"	=> array(
-      "nbdrives", "name", "defaultfilesize:size", "maxperlicanb", "replicationpolicy", "gcpolicy", "migratorpolicy", "recallerpolicy", "hasdiskonlybehavior", "forcedfileclass", "streampolicy", "gcenabled", "migrselectpolicy"
+      "nbdrives", "name", "defaultfilesize:size", "maxreplicanb", "replicationpolicy", "gcpolicy", "migratorpolicy", "recallerpolicy", "hasdiskonlybehavior", "forcedfileclass", "streampolicy", "gcenabled"
     ),
   ),
 
