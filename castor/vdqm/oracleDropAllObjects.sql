c43b428b7cf797eada36f824037158bb9e13da3a
Steven Murray <murrayc3@cern.ch>
2008-07-20 18:42:02 +0000

    oracleDropAllObjects.sqlplus now puts object names in double quotes so that they can contain strange characters, e.g. objects in the recycle bin

--- castor/vdqm/oracleDropAllObjects.sql
+++ castor/vdqm/oracleDropAllObjects.sql
@@ -7,41 +7,41 @@ set trimspool on
 spool oracleDropAllObjects.tmp
 prompt -- Drop triggers
 select unique
-  'drop ' || object_type || ' ' || object_name || ';' as cmd
+  'drop ' || object_type || ' "' || object_name || '";' as cmd
   from user_objects
   where object_type='TRIGGER';
 prompt
 prompt -- Drop packages
 select unique
-  'drop ' || object_type || ' ' || object_name || ';' as cmd
+  'drop ' || object_type || ' "' || object_name || '";' as cmd
   from user_objects
   where object_type='PACKAGE';
 prompt
 prompt -- Drop views
 select unique
-  'drop view ' || view_name || ';' as cmd
+  'drop view "' || view_name || '";' as cmd
   from user_views;
 prompt
 prompt -- Drop sequences
 select unique
-  'drop sequence ' || SEQUENCE_NAME || ';' as cmd
+  'drop sequence "' || SEQUENCE_NAME || '";' as cmd
   from user_sequences;
 prompt
 prompt -- Drop foreign key constraints
 select unique
-  'alter table ' || table_name || ' drop constraint ' || constraint_name || ';'
+  'alter table "' || table_name || '" drop constraint "' || constraint_name || '";'
     as cmd
   from user_constraints
   where constraint_type='R';
 prompt
 prompt -- Drop tables
 select unique
-  'drop table ' || table_name || ';' as cmd
+  'drop table "' || table_name || '";' as cmd
   from user_tables;
 prompt
 prompt -- Drop synonyms
 select unique
-  'drop synonym ' || synonym_name || ';' as cmd
+  'drop synonym "' || synonym_name || '";' as cmd
   from user_synonyms;
 spool off
 
