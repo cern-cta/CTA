1687941ea0e4a27a2af7ca1674291f77cb49f8a8
Giuseppe Lo Presti <itglp@cern.ch>
2008-08-08 09:29:56 +0000

    Updated to reflect changes in SQL creation scripts

--- makesql.sh
+++ makesql.sh
@@ -6,10 +6,9 @@ tag=`echo $ver | sed 's/\./_/g' | sed 's/-/_/g'`
 
 # generate creation scripts for the db-based CASTOR components
 # if $1 != NULL, it is interpreted as destination directory where to install the generated script
-tools/makeSqlScripts.sh castor castor/db $tag $1
-tools/makeSqlScripts.sh repack castor/repack $tag $1
-tools/makeSqlScripts.sh vdqm castor/vdqm $tag $1
-cd dlf/scripts
-./makeDlfScripts.sh $tag oracle/oracleCreate.sql oracle/dlf_oracle_create.sql $1
-cd ../..
+tools/makeSqlScripts.sh castor $tag castor/db $1
+tools/makeSqlScripts.sh repack $tag castor/repack $1
+tools/makeSqlScripts.sh vdqm $tag castor/vdqm $1
+tools/makeProCSqlScripts.sh dlf $tag dlf/scripts/oracle $1
+tools/makeProCSqlScripts.sh cns $tag ns $1
 
