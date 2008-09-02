86486f863eeb4905f3a94077b45825873c6bd7c7
Sebastien Ponce <sponcec3@cern.ch>
2008-09-02 12:15:23 +0000

    Fixes : dropped compat package, removed commands package and added trailing comma to tuples with a single value

--- tools/installOnAFS.py
+++ tools/installOnAFS.py
@@ -8,18 +8,16 @@ if len(sys.argv) != 2:
 castordir = '/afs/cern.ch/project/cndoc/wwwds/HSM/CASTOR/DIST/CERN/savannah/'
 afsdir = '/afs/cern.ch/sw/lcg/external/castor/'
 version = sys.argv[1]
-compatversion = '2.0.2-11'
-packages = ['ns-client', 'doc', 'rtcopy-client', 'upv-client', 'commands', 'stager-client', 'vdqm-client', 'rfio-server', 'lib', 'devel', 'stager-clientold', 'vmgr-client', 'rtcopy-messages', 'tape-client', 'rfio-client']
+packages = ['ns-client', 'doc', 'rtcopy-client', 'upv-client', 'stager-client', 'vdqm-client', 'rfio-server', 'lib', 'devel', 'stager-clientold', 'vmgr-client', 'rtcopy-messages', 'tape-client', 'rfio-client']
 
 platformConversion = {'SLC4' : 'slc4'}
 archConversion = {'i386' : 'ia32', 'x86_64' : 'amd64'}
 gccVersion = {'SLC4' : 'gcc34'}
 
-for plat in ('SLC4'):
+for plat in ('SLC4',):
     for arch in ('i386', 'x86_64'):
         archdir = afsdir + version + '/' + platformConversion[plat] + '_' + archConversion[arch] + '_' + gccVersion[plat]
         rpmdir = castordir + 'CASTOR.pkg/' + version + '/' + plat + '/' + arch + '/'
         print 'mkdir -p ' + archdir + ';cd ' + archdir
         for p in packages:
           print 'rpm2cpio ' + rpmdir + 'castor-' + p + '-' + version + '.' + arch + '.rpm | cpio -idmv'
-	print 'rpm2cpio ' + castordir + 'CASTOR-compat.pkg/' + version[0:version.index('-')] + '-\*/castor-lib-compat-' + compatversion + '.' + platformConversion[plat] + '.' + arch + '.rpm | cpio -idmv'
