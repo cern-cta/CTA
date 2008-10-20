07eff17ec1a29df24bcb59184f539657e9115a9d
Steven Murray <murrayc3@cern.ch>
2008-10-20 17:27:47 +0000

    Updated the convenience script oracleRecreateDb.sqlplus.  This script is not used by end users, however it does show by example how to re-create the VDQM2 database.

--- castor/vdqm/oracleRecreateDb.sql
+++ castor/vdqm/oracleRecreateDb.sql
@@ -14,23 +14,6 @@ CONNECT &vdqm_conn
 -- Create the new schema
 @vdqm_oracle_create.sqlplus
 
--- Grant writer account priviledges
--- Note that oracleWriterGrants.sqlplus assumes the writer account name is:
---    vdqm_writer
--- Replace this account name if the writer account is different
-@oracleWriterGrants.sqlplus
-
-PROMPT Login as the VDQM writer
-ACCEPT vdqm_writer_conn char PROMPT 'Enter VDQM writer connection string (user@tns): '
-CONNECT &vdqm_writer_conn
-
--- Drop all the exisiting database objects
-@oracleDropAllObjects.sqlplus
-
--- Create the writer synonyms
-@oracleWriterSynonyms.sqlplus
-
 -- Note that vdqmDBInit uses the database connection details given in:
 --     /etc/castor/ORAVDQMCONFIG
--- These connection details should be those of the writer account.
-!$CASTOR_CVS/hsmtools/vdqmDBInit
+!vdqmDBInit
