07fe38ccd2c3c97bd53c25104d1bab64b6f62a4a
Steven Murray <murrayc3@cern.ch>
2008-11-03 10:06:58 +0000

    The tape aggregator will have a default port of 5070

---
+++ h/tape_aggregator_constants.h
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *                      tape_aggregator_constants.h
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+#if !defined(TAPE_AGGREGTOR_CONSTANTS_H)
+#define RTCP_CONSTANTS_H
+
+#define TAPE_AGGREGATOR_PORT (5070)
+
+#endif /* TAPE_AGGREGTOR_CONSTANTS_H */
