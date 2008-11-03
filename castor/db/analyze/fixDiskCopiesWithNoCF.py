581e1dc9ada1d67ca35768ae7e90cf95f6e2dd21
Giuseppe Lo Presti <itglp@cern.ch>
2008-11-03 15:29:14 +0000

    Integrated modifications from Ignacio + some extra logging

--- castor/db/analyze/fixDiskCopiesWithNoCF.py
+++ castor/db/analyze/fixDiskCopiesWithNoCF.py
@@ -1,45 +1,37 @@
 #!/usr/bin/python
 import os
 import sys
-try:
-  import cx_Oracle
-except:
-  print '''Fatal: could not load module cx_Oracle.
-  Make sure it is installed and $PYTHONPATH includes the directory where cx_Oracle.so resides.\n'''
-  sys.exit(2)
+import getopt
+import castor_tools
 
 nsHost = "castorns"          # write your nameserver host alias here
 
-nsFileCl = {'0' : 'null'}
-
-
-# create the nsFileCl dictionary from nslistclass
-def parseNsListClass():
-  print "Parsing nslistclass output..."
-  #nsout = open('nslistclass_output.txt', 'r')
-  nsout = os.popen("nslistclass | grep -C1 CLASS_ID | awk '{ print $2 }'", 'r')
-  while(1):
-    currentLine = nsout.readline()
-    if currentLine == '':
-      nsout.close()
-      return
-    nsFileCl[currentLine.strip('\n')] = nsout.readline().strip('\n')
-    nsout.readline()
-    nsout.readline()
-
+# usage function
+def usage(exitCode):
+  print 'Usage : ' + sys.argv[0] + ' [-h]'
+  sys.exit(exitCode)
 
 ## main()
+# first parse the options
+try:
+    options, args = getopt.getopt(sys.argv[1:], 'h', ['help'])
+except Exception, e:
+    print e
+    usage(1)
+for f, v in options:
+    if f == '-h' or f == '--help':
+        usage(0)
+    else:
+        print "unknown option : " + f
+        usage(1)
 
 # connect to DB
-stgDb = raw_input("Please insert the full connection string to a CASTOR2 stager db (user/passwd@stgdb): ")
-print "Connecting to the stager db..."
-conn = cx_Oracle.connect(stgDb)
-dbcursor = conn.cursor()
-
-parseNsListClass()
-
-# create temp procedure
-sql = '''
+try:
+    stconn = castor_tools.connectToStager()
+    castor_tools.parseNsListClass()
+    dbcursor = stconn.cursor()
+    # create temp procedure
+    sql = '''
 CREATE OR REPLACE PROCEDURE reinsertCastorFile(
         dcId IN NUMBER,
         fid IN NUMBER,
@@ -61,33 +53,37 @@ BEGIN
     FROM FileClass
    WHERE name = fcname;
   selectCastorFile(fid, nh, scId, fcId, fileSize, fname, cfId, unused);
-  UPDATE DiskCopy set castorFile = cfId WHERE id = dcId;
+  UPDATE DiskCopy set castorFile = cfId, status = 7 WHERE id = dcId;
   COMMIT;
-END;
-'''
-
-dbcursor.execute(sql)
-
-# Get list of files to fix and do it
-sqlList = '''
+END;'''
+    dbcursor.execute(sql)
+    # Get list of files to fix and do it
+    sqlList = '''
 SELECT diskcopy.id, substr(path, instr(path, '/',1,1)+1, instr(path,'@',1,1)-instr(path, '/',1,1)-1)
-  FROM DiskCopy WHERE castorFile = 0 or castorFile is null
-'''
-dbcursor.execute(sqlList)
-files = dbcursor.fetchall()
-print 'Found %d diskcopies to be updated, starting...' % len(files)
-for f in files:
-    namefd = os.popen('nsgetpath castorns ' + str(f[1]))
-    name = namefd.read().strip('\n')
-    rc = namefd.close()
-    if rc == None:
-      lsfd = os.popen('nsls -l --class ' + name)
-      ls = lsfd.read().strip('\n').split()
-      dbcursor.callproc('reinsertCastorFile', ([f[0], f[1], nsHost, nsFileCl[ls[0]], ls[5], ls[9]]));
-#   else the file does not exist: we can probably drop everything then
-
-# cleanup
-sql = 'DROP PROCEDURE reinsertCastorFile'
-dbcursor.execute(sql)
-
-print 'Update completed'
+  FROM DiskCopy
+ WHERE (castorFile = 0 or castorFile is null)'''
+    dbcursor.execute(sqlList)
+    files = dbcursor.fetchall()
+    if len(files) == 0:
+        print 'Found no diskcopy to fix, exiting'
+    else:
+        print 'Found %d diskcopies to be updated, starting...' % len(files)
+    for f in files:
+        namefd = os.popen('nsgetpath castorns ' + str(f[1]))
+        name = namefd.read().strip('\n')
+        rc = namefd.close()
+        if rc == None:
+            print 'fileid %d' % f[1]
+            lsfd = os.popen('nsls -l --class ' + name)
+            ls = lsfd.read().strip('\n').split()
+            #print [f[0], f[1], nsHost, nsFileCl[ls[0]], ls[5], ls[9]]
+            dbcursor.callproc('reinsertCastorFile', ([f[0], f[1], nsHost, castor_tools.nsFileCl[ls[0]], ls[5], ls[9]]));
+    #   else the file does not exist: we can probably drop everything then
+    
+    # cleanup
+    sql = 'DROP PROCEDURE reinsertCastorFile'
+    dbcursor.execute(sql)
+    print 'Update completed'
+except Exception, e:
+    print e
+    sys.exit(-1)
