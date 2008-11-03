8f35d25f646c0c39509b8abb91c859112e875e55
Dennis Waldron <waldron@cern.ch>
2008-11-03 10:12:52 +0000

    Fixed: #43254: nsrename with no arguments seg faults

--- ns/nsrename.c
+++ ns/nsrename.c
@@ -16,9 +16,25 @@
 #endif
 #include "Cns.h"
 #include "Cns_api.h"
+#include "Cgetopt.h"
 #include "serrno.h"
 
-extern char *getenv();
+void usage(int status, char *name) {
+  if (status != 0) {
+    fprintf (stderr, "Try `%s --help` for more information.\n", name);
+  } else {
+    printf ("Usage: %s [OPTION]... SOURCE DEST\n", name);
+    printf ("Rename a file or directory in the name server.\n\n");
+    printf ("  -f, --force    do not prompt before overwriting\n");
+    printf ("  -v, --verbose  explain what is being done\n");
+    printf ("      --help     display this help and exit\n\n");
+    printf ("Report bugs to <castor.support@cern.ch>.\n");
+  }
+#if defined(_WIN32)
+  WSACleanup();
+#endif
+  exit (status);
+}
 
 int isyes()
 {
@@ -40,16 +56,31 @@ int main(argc, argv)
   char oldpath[CA_MAXPATHLEN+1];
   int errflg = 0;
   int fflag = 0;
+  int hflg = 0;
+  int vflg = 0;
   char *p;
   char *path;
   struct Cns_filestat statbuf;
 #if defined(_WIN32)
   WSADATA wsadata;
 #endif
-  while ((c = getopt (argc, argv, "f")) != EOF) {
+
+  Coptions_t longopts[] = {
+    { "force",   NO_ARGUMENT, NULL, 'f' },
+    { "verbose", NO_ARGUMENT, NULL, 'v' },
+    { "help",    NO_ARGUMENT, &hflg, 1  },
+    { NULL,      0,           NULL,  0  }
+  };
+
+  Coptind = 1;
+  Copterr = 1;
+  while ((c = Cgetopt_long (argc, argv, "fv", longopts, NULL)) != EOF) {
     switch (c) {
     case 'f':
       fflag++;
+      break;
+    case 'v':
+      vflg++;
       break;
     case '?':
       errflg++;
@@ -58,12 +89,14 @@ int main(argc, argv)
       break;
     }
   }
-  if (errflg || (argc - optind) >= 3) {
-    fprintf (stderr, "usage: %s [-f] oldname newname\n", argv[0]);
-    exit (USERR);
+  if (hflg) {
+    usage (0, argv[0]);
+  }
+  if (errflg || Coptind > (argc - 2)) {
+    usage (USERR, argv[0]);
   }
 
-  path = argv[optind];
+  path = argv[Coptind];
   if (*path != '/' && strstr (path, ":/") == NULL) {
     if ((p = getenv (CNS_HOME_ENV)) == NULL ||
         strlen (p) + strlen (path) + 1 > CA_MAXPATHLEN) {
@@ -79,7 +112,7 @@ int main(argc, argv)
     } else
       strcpy (oldpath, path);
   }
-  path = argv[optind + 1];
+  path = argv[Coptind + 1];
   if (*path != '/' && strstr (path, ":/") == NULL) {
     if ((p = getenv (CNS_HOME_ENV)) == NULL ||
         strlen (p) + strlen (path) + 1 > CA_MAXPATHLEN) {
@@ -103,11 +136,17 @@ int main(argc, argv)
     exit (SYERR);
   }
 #endif
+  if (vflg)
+    printf ("%s: renaming %s to %s\n", argv[0], oldpath, newpath);
   if (! fflag && Cns_lstat(newpath, &statbuf) == 0) {
     if (isatty(fileno(stdin))) {
       printf("%s: overwrite %s? ", argv[0], newpath);
-      if (! isyes())
-        exit(0);
+      if (! isyes()) {
+#if defined(_WIN32)
+	WSACleanup();
+#endif
+        exit (0);
+      }
     }
   }
 
