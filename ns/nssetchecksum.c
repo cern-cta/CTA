7efaa95e5094c43a18922dbe9bc5b4cc1649a7ed
Dennis Waldron <waldron@cern.ch>
2008-11-03 10:22:31 +0000

    Updated to only work against files. The previous segment logic has been moved to the nssetsegment command

--- ns/nssetchecksum.c
+++ ns/nssetchecksum.c
@@ -1,208 +1,172 @@
 /*
- * Copyright (C) 2004 by CERN/IT/ADC/CA
+ * Copyright (C) 2004 by CERN/IT/DM
  * All rights reserved
  */
 
-/*
- * Utility to set/reset the tape segment checksum in the name server
- */
-
+/* nssetchecksum - utility to set the checksum of a file in the name server */
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <Cns_api.h>
-#include <Cgetopt.h>
-#include <serrno.h>
+#include <sys/types.h>
+#if defined(_WIN32)
+#include <winsock2.h>
+#endif
+#include "Cns.h"
+#include "Cns_api.h"
+#include "Cgetopt.h"
+#include "serrno.h"
 
-#define OPT_COPYNO 10
-#define OPT_SEGMENTNO 11
-#define OPT_CHECKSUM_NAME 12
-#define OPT_CHECKSUM 13
-#define OPT_UPDATE 14
-#define OPT_CLR 15
-
-void usage(char *name) {
-  printf("Usage: %s --copyno copy_nb --segmentno segment_nb\n", name);
-  printf("          [--update] --checksum_name name --checksum value file_name\n");
-  printf("Or:    %s --copyno copy_nb --segmentno segment_nb --clr\n", name);
-  printf("Or:    %s --checksum_name name --checksum value file_name\n", name);
+void usage(int status, char *name) {
+  if (status != 0) {
+    fprintf (stderr, "Try `%s --help` for more information.\n", name);
+  } else {
+    printf ("Usage: %s [OPTION]... PATH\n", name);
+    printf ("Set or reset a checksum for a file.\n\n");
+    printf ("  -n, --name=CKSUMNAME  the name of the checksum to be stored. E.g. adler32\n");
+    printf ("  -k, --checksum=CKSUM  the value of the checksum to be stored\n");
+    printf ("      --clear           reset the checksum value to 0\n");
+    printf ("      --help            display this help and exit\n\n");
+    printf ("Report bugs to <castor.support@cern.ch>.\n");
+  }
+#if defined(_WIN32)
+  WSACleanup();
+#endif
+  exit (status);
 }
 
-int main(int argc, char *argv[]) {
-  struct Cns_filestat stat;
-  struct Cns_segattrs rsat, *outsat;
-  int outnb, i, rc;
-  unsigned int targetcopyno, targetfsec, targetchecksum, targetfound=0;
-  int hastgcopyno = 0, hastgfsec=0, hastgchecksum=0, hastgchecksum_name =0;
-  int clrflag = 0, updateflag = 0, fileflag = 0;
-  char targetchecksum_name[CA_MAXCKSUMNAMELEN+1];
-  char targetchecksum_value[CA_MAXCKSUMLEN+1];
+int main(argc, argv)
+     int argc;
+     char **argv;
+{
+  int c;
   int errflg = 0;
-  char *dp, *filename, c;
+  int hflg   = 0;
+  int clear  = 0;
+  char *path = NULL;
+  char *dp   = NULL;
+  char *p;
+  char filepath[CA_MAXPATHLEN+1];
 
+  unsigned int checksum = 0;
+  char chksumvalue[CA_MAXCKSUMLEN+1];
+  char chksumname[CA_MAXCKSUMNAMELEN+1];
 
-  static struct Coptions longopts[] = {
-    {"copyno", REQUIRED_ARGUMENT, 0, OPT_COPYNO},
-    {"segmentno", REQUIRED_ARGUMENT, 0, OPT_SEGMENTNO},
-    {"checksum_name", REQUIRED_ARGUMENT, 0, OPT_CHECKSUM_NAME},
-    {"checksum", REQUIRED_ARGUMENT, 0, OPT_CHECKSUM},
-    {"update", NO_ARGUMENT, 0, OPT_UPDATE},
-    {"clr", NO_ARGUMENT, 0, OPT_CLR},
-    {0, 0, 0, 0}
+  Coptions_t longopts[] = {
+    { "checksum",  REQUIRED_ARGUMENT, NULL,   'k' },
+    { "clear",     NO_ARGUMENT,       &clear,  1  },
+    { "name",      REQUIRED_ARGUMENT, NULL,   'n' },
+    { "help",      NO_ARGUMENT,       &hflg,   1  },
+    { NULL,        0,                 NULL,    0  }
   };
 
-  Copterr = 1;
   Coptind = 1;
-  while ((c = Cgetopt_long (argc, argv, "", longopts, NULL)) != EOF) {
+  Copterr = 1;
+  chksumvalue[0] = chksumname[0] = '\0';
+  while ((c = Cgetopt_long (argc, argv, "k:n:", longopts, NULL)) != EOF && !errflg) {
     switch (c) {
-    case OPT_COPYNO:
-      if ((targetcopyno = strtol (Coptarg, &dp, 10)) <= 0 ||
-          *dp != '\0') {
-        fprintf (stderr, "invalid copy number %s\n", Coptarg);
-        errflg++;
+    case 'k':
+      if (((checksum = strtoul (Coptarg, &dp, 16)) < 0) || (*dp != '\0')) {
+	fprintf (stderr, "%s: invalid checksum: %s\n", argv[0], Coptarg);
+	errflg++;
       }
-      hastgcopyno=1;
-      break;
-    case OPT_SEGMENTNO:
-      if ((targetfsec = strtol (Coptarg, &dp, 10)) <= 0 ||
-          *dp != '\0') {
-        fprintf (stderr, "invalid segment number %s\n", Coptarg);
-        errflg++;
+      if (strlen(Coptarg) > CA_MAXCKSUMLEN) {
+	fprintf (stderr, "%s: checksum value: %s exceeds %d characters in length\n",
+		 argv[0], Coptarg, CA_MAXCKSUMLEN);
+	errflg++;
       }
-      hastgfsec=1;
+      strncpy(chksumvalue, Coptarg, CA_MAXCKSUMLEN);
+      chksumvalue[CA_MAXCKSUMLEN] = '\0';
       break;
-    case OPT_CHECKSUM_NAME:
-      strncpy(targetchecksum_name, Coptarg, CA_MAXCKSUMNAMELEN);
-      targetchecksum_name[CA_MAXCKSUMNAMELEN]='\0';
-      hastgchecksum_name=1;
-      break;
-    case OPT_CHECKSUM:
-      if ((targetchecksum = strtoul (Coptarg, &dp, 16)) < 0 ||
-          *dp != '\0') {
-        fprintf (stderr, "invalid checksum %s\n", Coptarg);
-        errflg++;
+    case 'n':
+      if (strlen(Coptarg) > CA_MAXCKSUMNAMELEN) {
+	fprintf (stderr, "%s: checksum name: %s exceeds %d characters in length\n",
+		 argv[0], Coptarg, CA_MAXCKSUMNAMELEN);
+	errflg++;
       }
-      strncpy(targetchecksum_value, Coptarg, CA_MAXCKSUMLEN);
-      targetchecksum_value[CA_MAXCKSUMLEN]='\0';
-      hastgchecksum=1;
+      strncpy(chksumname, Coptarg, CA_MAXCKSUMNAMELEN);
+      chksumname[CA_MAXCKSUMLEN] = '\0';
+      if (strcmp(chksumname, "CS") == 0 ||
+	  strcmp(chksumname, "MD") == 0 ||
+	  strcmp(chksumname, "MD") == 0 ||
+	  strcmp(chksumname, "adler32") == 0 ||
+	  strcmp(chksumname, "crc32")   == 0 ||
+	  strcmp(chksumname, "md5")     == 0) {
+	if (strcmp(chksumname, "adler32") == 0)
+	  strcpy(chksumname, "PA");
+	else if (strcmp(chksumname, "crc32") == 0)
+	  strcpy(chksumname, "PC");
+	else if (strcmp(chksumname, "md5") == 0)
+	  strcpy(chksumname, "PM");
+      } else {
+	fprintf (stderr, "%s: invalid checksum name: %s\n", argv[0], Coptarg);
+	errflg++;
+      }
       break;
-    case OPT_UPDATE:
-      updateflag = 1;
-      break;
-    case OPT_CLR:
-      clrflag = 1;
+    case '?':
+    case ':':
+      errflg++;
       break;
     default:
       break;
     }
   }
-  if ((hastgchecksum && hastgchecksum_name) && !(hastgcopyno && hastgfsec)) fileflag=1;
-  if (Coptind >= argc
-      || !((hastgcopyno && hastgfsec) || fileflag)
-      || (!clrflag && !(hastgchecksum && hastgchecksum_name))) {
+  if (hflg) {
+    usage (0, argv[0]);
+  }
+  if (errflg || Coptind >= argc) {
+    errflg++;
+  } else if (clear && !errflg) {
+    if (chksumname[0] != '\0') {
+      fprintf (stderr, "%s: option -n, --name cannot be specified with --clear\n", argv[0]);
+      errflg++;
+    } else if (chksumvalue[0] != '\0') {
+      fprintf (stderr, "%s: option -k, --checksum cannot be specified with --clear\n", argv[0]);
+      errflg++;
+    }
+  } else if (!clear && ((chksumname[0] == '\0') || (chksumvalue[0] == '\0'))) {
+    fprintf (stderr, "%s: mandatory option -n, --name and/or -k, --checksum are missing\n", argv[0]);
     errflg++;
   }
-
-  if ((clrflag && updateflag)
-      || (clrflag && hastgchecksum)
-      || (clrflag && hastgchecksum_name)) {
-    fprintf(stderr, "Cannot specify --clr and value for checksum!\n");
-    usage(argv[0]);
-    exit(USERR);
-  }
-
   if (errflg) {
-    usage(argv[0]);
-    exit(USERR);
+    usage (USERR, argv[0]);
   }
-
-  filename = argv[Coptind];
-
-  rc = Cns_stat(filename, &stat);
-  if (rc != 0) {
-    fprintf(stderr, "Cns_stat error %d : %s\n",
-            serrno,
-            sstrerror(serrno));
-    return EXIT_FAILURE;
+#if defined(_WIN32)
+  if (WSAStartup (MAKEWORD (2, 0), &wsadata)) {
+    fprintf (stderr, NS052);
+    exit (SYERR);
   }
-
-  if(fileflag && hastgchecksum && hastgchecksum_name) { /* we will update the file checksum in Cns_file_metadata */
-    /* in CNS we know only AD, CS, MD, PA, PC, PM so for a user we will convert checksum name to the predefined checksum name
-       admims may use AD, CS, MD if they want to change checksums for a file.
-       User can use empty checksum name to clear checksum */
-    if (*targetchecksum_name =='\0' || strcmp(targetchecksum_name,"CS") == 0 || strcmp(targetchecksum_name,"MD") == 0 || strcmp(targetchecksum_name,"AD") == 0
-        || strcmp(targetchecksum_name,"adler32") == 0 || strcmp(targetchecksum_name,"crc32") == 0 || strcmp(targetchecksum_name,"md5") == 0) {
-      if (strcmp(targetchecksum_name,"adler32") == 0) strcpy(targetchecksum_name,"PA");
-      else if (strcmp(targetchecksum_name,"crc32") == 0) strcpy(targetchecksum_name,"PC");
-      else if (strcmp(targetchecksum_name,"md5") == 0) strcpy(targetchecksum_name,"PM");
-    } else {
-      fprintf(stderr, "Unknown checksum name %s\n",targetchecksum_name);
-      return EXIT_FAILURE;
-    }
-    rc = Cns_updatefile_checksum(filename,targetchecksum_name,targetchecksum_value);
-    if (rc != 0) {
-      fprintf(stderr, "Cns_updatefile_checksum error %d : %s\n",
-              serrno,
-              sstrerror(serrno));
-      return EXIT_FAILURE;
-    }
+#endif
+  path = argv[Coptind];
+  if (*path != '/' && strstr (path, ":/") == NULL) {
+    if ((p = getenv (CNS_HOME_ENV)) == NULL ||
+	strlen (p) + strlen (path) + 1 > CA_MAXPATHLEN) {
+      fprintf (stderr, "%s: %s: invalid path\n", argv[0], path);
+      errflg++;
+    } else
+      sprintf (filepath, "%s/%s", p, path);
+  } else {
+    if (strlen (path) > CA_MAXPATHLEN) {
+      fprintf (stderr, "%s: %s: %s\n", argv[0], path,
+	       sstrerror(SENAMETOOLONG));
+      errflg++;
+    } else
+      strcpy (filepath, path);
   }
-  else { /* we are working with tape segments */
-    rc = Cns_getsegattrs(filename,
-                         NULL,
-                         &outnb,
-                         &outsat);
-
-    if (rc != 0) {
-      fprintf(stderr, "Cns_getsegattrs error %d : %s\n",
-              serrno,
-              sstrerror(serrno));
-      return EXIT_FAILURE;
+  if (!errflg) {
+    if (clear) {
+      chksumname[0] = '\0';
+      chksumvalue[0] = '\0';
     }
-
-    for (i=0; i<outnb; i++) {
-      if (outsat[i].copyno == targetcopyno
-          && outsat[i].fsec == targetfsec) {
-        targetfound = 1;
-
-        rsat = outsat[i];
-        if (clrflag) {
-          rsat.checksum_name[0] = '\0';
-          rsat.checksum = 0;
-        } else {
-          strcpy(rsat.checksum_name, targetchecksum_name);
-          rsat.checksum = targetchecksum;
-        }
-
-        if (updateflag) {
-          rc = Cns_updateseg_checksum(NULL, stat.fileid,
-                                      &(outsat[i]), &rsat);
-          if (rc != 0) {
-            fprintf(stderr, "Cns_updateseg_checksum error %d : %s\n",
-                    serrno,
-                    sstrerror(serrno));
-          }
-        } else {
-          rc = Cns_replaceseg(NULL, stat.fileid,
-                              &(outsat[i]), &rsat);
-          if (rc != 0) {
-            fprintf(stderr, "Cns_replaceseg_checksum error %d : %s\n",
-                    serrno,
-                    sstrerror(serrno));
-          }
-        }
-        return EXIT_FAILURE;
-      }
-    }
-
-    if (outnb > 0) {
-      free(outsat);
-    }
-
-    if (!targetfound) {
-      fprintf(stderr, "Could not find segment !\n");
-      return EXIT_FAILURE;
+    if (Cns_updatefile_checksum(filepath, chksumname, chksumvalue)) {
+      fprintf (stderr, "%s: %s: %s\n", argv[0], filepath, sstrerror(serrno));
+      errflg++;
     }
   }
-  return EXIT_SUCCESS;
+#if defined(_WIN32)
+  WSACleanup();
+#endif
+  if (errflg)
+    exit (USERR);
+  exit (0);
 }
