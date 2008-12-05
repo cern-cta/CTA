bc28524261246dd16e07c8b5e74a6f053cb48d3b
Steven Murray <murrayc3@cern.ch>
2008-12-05 14:30:57 +0000

    I broke the build (not intentially).  Whilst working on the tape aggregator, I changed the names of the constants the following constants file:
      CASTOR2/h/tape_aggregator_constants.h
    
    I forgot that this file is shared with the VDQM and I therefore broke the
    build of the VDQM.

--- castor/vdqm/RTCPJobSubmitterThread.cpp
+++ castor/vdqm/RTCPJobSubmitterThread.cpp
@@ -406,7 +406,7 @@ void castor::vdqm::RTCPJobSubmitterThread::submitJob(const Cuuid_t &cuuid,
   if(remoteCopyType == "RTCPD") {
     port = RTCOPY_PORT;
   } else if(remoteCopyType == "AGGREGATOR") {
-    port = AGGREGATOR_PORT;
+    port = AGGREGATOR_VDQMPORT;
   } else {
     castor::exception::Internal ie;
 
