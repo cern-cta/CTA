b52533cb8263bf4e5b0c82ce806ee1eecc4c99d2
Sebastien Ponce <sponcec3@cern.ch>
2009-01-05 15:21:03 +0000

    Added missing declaration, leading to compilation warnings

--- h/Csec_protocol_policy.h
+++ h/Csec_protocol_policy.h
@@ -9,5 +9,7 @@ int Csec_server_lookup_protocols(long client_address,
 			      int *nbprotocols);
 int Csec_server_set_protocols(Csec_context_t *ctx,
 			      int socket);
+int Csec_initialize_protocols_from_list(Csec_context_t *ctx,
+                                        Csec_protocol *protocol);
     
 #endif /* _CSEC_PROTOCOL_POLICY_H */
