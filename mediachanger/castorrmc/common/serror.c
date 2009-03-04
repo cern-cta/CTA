b156d8acacf219e9a54e0053b552499820aadb4b
Dennis Waldron <waldron@cern.ch>
2009-03-04 10:28:07 +0000

    Fixed 'excess elements in array initializer' compilation warning

--- mediachanger/castorrmc/common/serror.c
+++ mediachanger/castorrmc/common/serror.c
@@ -471,7 +471,6 @@ char *sys_secerrlist[ESECMAXERR-ESECBASEOFF+2] =
    "Could not lookup security protocol",
    "Csec incompatibility",
    "Unexpected response from peer",
-   "Security exception",
    "BAD ERROR NUMBER"
   };
 
