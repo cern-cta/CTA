7cb60496d3c71cdba7aa48f05093e5e3a0411344
Theodoros Rekatsinas <trekas@cern.ch>
2009-04-30 07:54:03 +0000

    notice disabled

--- monitoring/castor-mon-web/html/lib/_oci_no_cache.php
+++ monitoring/castor-mon-web/html/lib/_oci_no_cache.php
@@ -1,6 +1,7 @@
 <?php 
 
 //checks if included dynamic library needed for the graphs
+error_reporting(E_ALL ^ E_NOTICE);
 if (strtoupper(substr(PHP_OS, 0, 3) == 'WIN')) {
     if (!function_exists('imagepng')) {
         dl('php_gd2.dll');
