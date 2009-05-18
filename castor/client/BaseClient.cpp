c52c994b6bff8970ca7f98d0a56627c78f37464b
Dennis Waldron <waldron@cern.ch>
2009-05-18 13:36:20 +0000

    Fixed indentation

--- castor/client/BaseClient.cpp
+++ castor/client/BaseClient.cpp
@@ -1,29 +1,29 @@
 /******************************************************************************
-*                      BaseClient.cpp
-*
-* This file is part of the Castor project.
-* See http://castor.web.cern.ch/castor
-*
-* Copyright (C) 2003  CERN
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*
-* @(#)BaseClient.cpp,v 1.37 $Release$ 2006/02/16 15:56:58 sponcec3
-*
-* A base class for all castor clients. Implements many
-* useful and common parts for all clients.
-*
-* @author Sebastien Ponce
-*****************************************************************************/
+ *                      BaseClient.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @(#)BaseClient.cpp,v 1.37 $Release$ 2006/02/16 15:56:58 sponcec3
+ *
+ * A base class for all castor clients. Implements many
+ * useful and common parts for all clients.
+ *
+ * @author Sebastien Ponce
+ *****************************************************************************/
 
 // Include Files
 #include <errno.h>
@@ -67,8 +67,8 @@
 
 
 extern "C" {
-  #include <Cgetopt.h>
-  #include <common.h>
+#include <Cgetopt.h>
+#include <common.h>
 }
 
 // Local includes
@@ -119,17 +119,17 @@ static char strftime_format[] = "%b %e %H:%M:%S";
 
 void DLL_DECL BaseClient_util_time(time_t then, char *timestr) {
   time_t this_time = time(NULL);
-  #if defined(_REENTRANT) || defined(_THREAD_SAFE)
+#if defined(_REENTRANT) || defined(_THREAD_SAFE)
   struct tm tmstruc;
-  #endif /* _REENTRANT || _THREAD_SAFE */
+#endif /* _REENTRANT || _THREAD_SAFE */
   struct tm *tp;
   
-  #if ((defined(_REENTRANT) || defined(_THREAD_SAFE)) && !defined(_WIN32))
+#if ((defined(_REENTRANT) || defined(_THREAD_SAFE)) && !defined(_WIN32))
   localtime_r(&(then),&tmstruc);
   tp = &tmstruc;
-  #else
+#else
   tp = localtime(&(then));
-  #endif /* _REENTRANT || _THREAD_SAFE */
+#endif /* _REENTRANT || _THREAD_SAFE */
   if ((this_time >= then) && ((this_time - then) > SIXMONTHS)) {
     // Too much in past
     strftime(timestr,64,strftime_format_sixmonthsold,tp);
@@ -146,16 +146,16 @@ void DLL_DECL BaseClient_util_time(time_t then, char *timestr) {
 //------------------------------------------------------------------------------
 castor::client::BaseClient::BaseClient
 (int acceptTimeout, int transferTimeout) throw() :
-BaseObject(),
-m_rhPort(-1),
-m_callbackSocket(0),
-m_requestId(""),
-m_acceptTimeout(acceptTimeout),
-m_transferTimeout(transferTimeout),
-m_hasAuthorizationId(false),
-m_authUid(0),
-m_authGid(0),
-m_hasSecAuthorization(false) {
+  BaseObject(),
+  m_rhPort(-1),
+  m_callbackSocket(0),
+  m_requestId(""),
+  m_acceptTimeout(acceptTimeout),
+  m_transferTimeout(transferTimeout),
+  m_hasAuthorizationId(false),
+  m_authUid(0),
+  m_authGid(0),
+  m_hasSecAuthorization(false) {
   setAuthorization();
 }
 
@@ -171,8 +171,8 @@ castor::client::BaseClient::~BaseClient() throw() {
 //------------------------------------------------------------------------------
 std::string castor::client::BaseClient::sendRequest
 (castor::stager::Request* req,
-  castor::client::IResponseHandler* rh)
-throw(castor::exception::Exception) {
+ castor::client::IResponseHandler* rh)
+  throw(castor::exception::Exception) {
   
   // Build and send the Request with the Client information
   createClientAndSend(req);
@@ -187,7 +187,7 @@ throw(castor::exception::Exception) {
 // createClient
 //------------------------------------------------------------------------------
 castor::IClient* castor::client::BaseClient::createClient()
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   // if no callbackSocket
   if (0 == m_callbackSocket) {
     castor::exception::Internal ex;
@@ -210,7 +210,7 @@ throw (castor::exception::Exception) {
 //------------------------------------------------------------------------------
 std::string castor::client::BaseClient::internalSendRequest
 (castor::stager::Request& request)
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   std::string requestId;
   
   // The service class has been previously resolved, attach it to the request
@@ -225,12 +225,12 @@ throw (castor::exception::Exception) {
   
   // preparing the timing information
   clock_t startTime;
-  #if !defined(_WIN32)
+#if !defined(_WIN32)
   struct tms buf;
   startTime = times(&buf);
-  #else
+#else
   startTime = clock();
-  #endif
+#endif
   
   // Creates a socket
   if (m_hasSecAuthorization) {
@@ -254,7 +254,7 @@ throw (castor::exception::Exception) {
     // Wait for acknowledgment
     IObject* obj = s->readObject();
     castor::MessageAck* ack =
-    dynamic_cast<castor::MessageAck*>(obj);
+      dynamic_cast<castor::MessageAck*>(obj);
     if (0 == ack) {
       castor::exception::InvalidArgument e; // XXX To be changed
       e.getMessage() << "No Acknowledgement from the Server";
@@ -269,26 +269,26 @@ throw (castor::exception::Exception) {
       throw e;
     }
     delete ack;
-    #if !defined(_WIN32)
-      clock_t endTime = times(&buf);
-    #else
-      clock_t endTime = clock();
-    #endif
+#if !defined(_WIN32)
+    clock_t endTime = times(&buf);
+#else
+    clock_t endTime = clock();
+#endif
     stage_trace(3, "%s SND %.2f s to send the request", 
-      requestId.c_str(),
-    #if !defined(_WIN32)
-      ((float)(endTime - startTime)) / ((float)sysconf(_SC_CLK_TCK)));
-    #else
-      ((float)(endTime - startTime)) / CLOCKS_PER_SEC);
-    #endif
-    delete s;
-    return requestId;
-  }
-  catch (castor::exception::Exception e) {
-    // forward any exception after closing the socket
-    delete s;
-    throw e;
-  }    
+                requestId.c_str(),
+#if !defined(_WIN32)
+                ((float)(endTime - startTime)) / ((float)sysconf(_SC_CLK_TCK)));
+#else
+    ((float)(endTime - startTime)) / CLOCKS_PER_SEC);
+#endif
+  delete s;
+  return requestId;
+}
+catch (castor::exception::Exception e) {
+  // forward any exception after closing the socket
+  delete s;
+  throw e;
+ }    
 }
 
 //------------------------------------------------------------------------------
@@ -296,27 +296,27 @@ throw (castor::exception::Exception) {
 //------------------------------------------------------------------------------
 // This function blocks until one call back has been made
 castor::io::ServerSocket* castor::client::BaseClient::waitForCallBack()
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   
   stage_trace(3, "Waiting for callback from castor");
   
   clock_t startTime;
-  #if !defined(_WIN32)
+#if !defined(_WIN32)
   struct tms buf;
   startTime = times(&buf);
-  #else
+#else
   startTime = clock();
-  #endif
+#endif
   
   // Set the socket to non blocking
   int rc; 
-  #if !defined(_WIN32)
+#if !defined(_WIN32)
   int nonblocking = 1;
   rc = ioctl(m_callbackSocket->socket(), FIONBIO, &nonblocking);
-  #else
+#else
   u_long nonblocking = 1;
   rc = ioctlsocket(m_callbackSocket->socket(), FIONBIO, &nonblocking);
-  #endif
+#endif
   if (rc == SOCKET_ERROR) {
     castor::exception::InvalidArgument e;
     e.getMessage() << "Could not set socket asynchonous";
@@ -358,20 +358,20 @@ throw (castor::exception::Exception) {
   
   std::ostringstream ipToStr;
   ipToStr << ((ip & 0xFF000000) >> 24) << "." << ((ip & 0x00FF0000) >> 16) << "."
-  << ((ip & 0x0000FF00) >> 8) << "." << ((ip & 0x000000FF));
-  #if !defined(_WIN32)
+          << ((ip & 0x0000FF00) >> 8) << "." << ((ip & 0x000000FF));
+#if !defined(_WIN32)
   clock_t endTime = times(&buf);
-  #else
+#else
   clock_t endTime = clock();
-  #endif
+#endif
   stage_trace(3, "%s CBK %.2f s before callback from %s was received", 
-    requestId().c_str(),
-    #if !defined(_WIN32)
-    ((float)(endTime - startTime)) / ((float)sysconf(_SC_CLK_TCK)),
-    #else
-    ((float)(endTime - startTime)) / CLOCKS_PER_SEC,
-    #endif
-    ipToStr.str().c_str());
+              requestId().c_str(),
+#if !defined(_WIN32)
+              ((float)(endTime - startTime)) / ((float)sysconf(_SC_CLK_TCK)),
+#else
+              ((float)(endTime - startTime)) / CLOCKS_PER_SEC,
+#endif
+              ipToStr.str().c_str());
   
   return socket;
 }
@@ -380,17 +380,17 @@ throw (castor::exception::Exception) {
 // setRhPort
 //------------------------------------------------------------------------------
 void castor::client::BaseClient::setRhPort()
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   setRhPort(0);
 }
 
 void castor::client::BaseClient::setRhPort(int optPort)
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   
   if (optPort > 65535) {
     castor::exception::Exception e(errno);
     e.getMessage() << "Invalid port value : " << optPort
-    << ". Must be < 65535." << std::endl;
+                   << ". Must be < 65535." << std::endl;
     throw e;
   }
   if (optPort > 0) {
@@ -426,12 +426,12 @@ throw (castor::exception::Exception) {
 // setRhHost
 //------------------------------------------------------------------------------
 void castor::client::BaseClient::setRhHost()
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   setRhHost("");
 }
 
 void castor::client::BaseClient::setRhHost(std::string optHost)
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   // RH server host. Can be passed given through the
   // STAGER_HOST environment variable or in the castor.conf
   // file as a STAGER/HOST entry
@@ -458,12 +458,12 @@ throw (castor::exception::Exception) {
 // setRhSvcClass
 //------------------------------------------------------------------------------
 void castor::client::BaseClient::setRhSvcClass()
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   setRhSvcClass("");
 }
 
 void castor::client::BaseClient::setRhSvcClass(std::string optSvcClass)
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   // RH server host. Can be passed given through the
   // RH_HOST environment variable or in the castor.conf
   // file as a RH/HOST entry
@@ -488,7 +488,7 @@ throw (castor::exception::Exception) {
 // setOptions
 //------------------------------------------------------------------------------
 void castor::client::BaseClient::setOptions(struct stage_options* opts)
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   if (0 != opts) {
     setRhHost(opts->stage_host);
     setRhPort(opts->stage_port);
@@ -518,7 +518,7 @@ std::string castor::client::BaseClient::requestId() {
 // setAutorizationId
 //----------------------------------------------------------------------------
 void castor::client::BaseClient::setAuthorizationId() 
-throw(castor::exception::Exception) {
+  throw(castor::exception::Exception) {
   if (stage_getid(&m_authUid, &m_authGid) < 0) {
     castor::exception::Exception e(serrno);
     e.getMessage() << "Error in stage_getid" << std::endl;
@@ -545,22 +545,22 @@ void castor::client::BaseClient::setAuthorization() throw(castor::exception::Exc
   char *mech;
   // Check if security env option is set. 
   if (((security = getenv (castor::client::SECURITY_ENV)) != 0 ||
-    (security = getconfent((char *)castor::client::CLIENT_CONF,
-      (char *)castor::client::SECURITY_CONF,0)) != 0 ) && 
-    strcasecmp(security, "YES") == 0) {
-  if (( mech = getenv (castor::client::SEC_MECH_ENV)) != 0) {
-    if (strlen(mech) > CA_MAXCSECPROTOLEN) {
-      serrno = EINVAL;
-      castor::exception::Exception e(serrno);
-      e.getMessage() << "Supplied security protocol is too long" << std::endl;
-      throw e;
-    } else {
-      m_Sec_mech = mech;
-      m_hasSecAuthorization = true;
-      stage_trace(3, "Setting security mechanism: %s", mech);
+       (security = getconfent((char *)castor::client::CLIENT_CONF,
+                              (char *)castor::client::SECURITY_CONF,0)) != 0 ) && 
+      strcasecmp(security, "YES") == 0) {
+    if (( mech = getenv (castor::client::SEC_MECH_ENV)) != 0) {
+      if (strlen(mech) > CA_MAXCSECPROTOLEN) {
+        serrno = EINVAL;
+        castor::exception::Exception e(serrno);
+        e.getMessage() << "Supplied security protocol is too long" << std::endl;
+        throw e;
+      } else {
+        m_Sec_mech = mech;
+        m_hasSecAuthorization = true;
+        stage_trace(3, "Setting security mechanism: %s", mech);
+      }
     }
   }
-    }
 }
 
 //------------------------------------------------------------------------------
@@ -597,7 +597,7 @@ void castor::client::BaseClient::setAuthorization(char *mech, char *id) throw(ca
 //------------------------------------------------------------------------------
 std::string castor::client::BaseClient::createClientAndSend
 (castor::stager::Request *req)
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   // builds the Client (uuid,guid,hostname,etc)
   buildClient(req);
   
@@ -610,7 +610,7 @@ throw (castor::exception::Exception) {
 // buildClient
 //------------------------------------------------------------------------------
 void castor::client::BaseClient::buildClient(castor::stager::Request* req)
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   
   // Uid
   uid_t euid;
@@ -677,7 +677,7 @@ throw (castor::exception::Exception) {
   if (m_rhHost == "") {
     castor::exception::Exception e(errno);
     e.getMessage() << "Could not get RH host name"
-    <<  strerror(errno);
+                   <<  strerror(errno);
     throw e;
   }
   
@@ -690,25 +690,25 @@ throw (castor::exception::Exception) {
   int highPort = HIGH_CLIENT_PORT_RANGE;
   char* sport;
   if ((sport = getconfent((char *)castor::client::CLIENT_CONF,
-    (char *)castor::client::LOWPORT_CONF,0)) != 0) {
+                          (char *)castor::client::LOWPORT_CONF,0)) != 0) {
     lowPort = castor::System::porttoi(sport);
-    }
-    if ((sport = getconfent((char *)castor::client::CLIENT_CONF,
-      (char *)castor::client::HIGHPORT_CONF,0)) != 0) {
-      highPort = castor::System::porttoi(sport);
-      }
+  }
+  if ((sport = getconfent((char *)castor::client::CLIENT_CONF,
+                          (char *)castor::client::HIGHPORT_CONF,0)) != 0) {
+    highPort = castor::System::porttoi(sport);
+  }
       
-      // Bind the socket
-      m_callbackSocket->bind(lowPort, highPort);
-      m_callbackSocket->listen();
-      unsigned short port;
-      unsigned long ip;
-      m_callbackSocket->getPortIp(port, ip);
-      stage_trace(3, "Creating socket for castor callback - Using port %d", port);
+  // Bind the socket
+  m_callbackSocket->bind(lowPort, highPort);
+  m_callbackSocket->listen();
+  unsigned short port;
+  unsigned long ip;
+  m_callbackSocket->getPortIp(port, ip);
+  stage_trace(3, "Creating socket for castor callback - Using port %d", port);
       
-      // Set the Client
-      castor::IClient *cl = createClient();
-      req->setClient(cl);
+  // Set the Client
+  castor::IClient *cl = createClient();
+  req->setClient(cl);
 }
 
 //------------------------------------------------------------------------------
@@ -716,7 +716,7 @@ throw (castor::exception::Exception) {
 //------------------------------------------------------------------------------
 void castor::client::BaseClient::pollAnswersFromStager
 (castor::stager::Request* req, castor::client::IResponseHandler* rh)
-throw (castor::exception::Exception) {
+  throw (castor::exception::Exception) {
   
   // Check parameters
   if ((req == NULL) || (req->client() == NULL)) {
@@ -733,7 +733,7 @@ throw (castor::exception::Exception) {
   // stager with all the responses.
   unsigned int nbReplies = 1;
   castor::stager::FileRequest *fr =
-  dynamic_cast<castor::stager::FileRequest *>(req);
+    dynamic_cast<castor::stager::FileRequest *>(req);
   if (fr != 0) {
     nbReplies = fr->subRequests().size();
   }
@@ -774,7 +774,7 @@ throw (castor::exception::Exception) {
         
         // Cast response into Response*
         castor::rh::Response *res =
-        dynamic_cast<castor::rh::Response *>(obj);
+          dynamic_cast<castor::rh::Response *>(obj);
         if (res == 0) {
           castor::exception::Communication e(requestId(), SEINTERNAL);
           e.getMessage() << "Receivd bad response type :" << obj->type();
@@ -783,7 +783,7 @@ throw (castor::exception::Exception) {
         
         // Check that the request id sent is what we expected
         if ((res->reqAssociated().length() != 0) &&
-          (res->reqAssociated() != requestId())) {
+            (res->reqAssociated() != requestId())) {
           castor::exception::Communication e(requestId(), SEINTERNAL);
           e.getMessage() << "Receieved data for another request: "
                          << res->reqAssociated();
