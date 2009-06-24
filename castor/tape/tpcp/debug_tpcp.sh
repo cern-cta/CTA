7acf6659426bb04557c9a9ef0d24468dd7a73cf7
Steven Murray <murrayc3@cern.ch>
2009-06-24 11:59:48 +0000

    debug_tpcp.sh now works with non-root users

--- castor/tape/tpcp/debug_tpcp.sh
+++ castor/tape/tpcp/debug_tpcp.sh
@@ -3,5 +3,10 @@
 LD_LIBRARY_PATH=`find ../../.. -name '*.so*' | sed 's|/[^/]*$||' | sort | uniq`
 LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | sed 's/ /:/g'`
 PARAMS=$@
+CMD="export LD_LIBRARY_PATH=$LD_LIBRARY_PATH; `pwd`/tpcp $PARAMS"
 
-sudo -u stage sh -c "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH; `pwd`/tpcp $PARAMS"
+if test $USER = root; then
+  sudo -u stage sh -c "$CMD"
+else
+  sh -c "$CMD"
+fi
