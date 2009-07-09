4692821288d2022fa8d7621065c4a747b7575a56
Steven Murray <murrayc3@cern.ch>
2009-07-09 08:15:10 +0000

    testtpcp.sh is now independent of the present wporking directory (pwd)

--- test/castor/tape/tpcp/testtpcp.sh
+++ test/castor/tape/tpcp/testtpcp.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=`find ../../../.. -name '*.so*' | sed 's|/[^/]*$||' | sort | uniq`
+if test "x$CASTOR_CVS" = x; then
+  echo "Error: The environment variable CASTOR_CVS is not set"
+  echo
+  echo "CASTOR_CVS should be the full path to the CASTOR source code up to and"
+  echo "including CASTOR2, e.g."
+  echo
+  echo "    export CASTOR_CVS=/usr/local/src/CASTOR2"
+  echo
+  exit -1
+fi
+
+LD_LIBRARY_PATH=`find $CASTOR_CVS -name '*.so*' | sed 's|/[^/]*$||' | sort | uniq`
 LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | sed 's/ /:/g'`
 PARAMS=$@
-CMD="export LD_LIBRARY_PATH=$LD_LIBRARY_PATH; `pwd`/testtpcp $PARAMS"
+CMD="export LD_LIBRARY_PATH=$LD_LIBRARY_PATH; $CASTOR_CVS/test/castor/tape/tpcp/testtpcp $PARAMS"
 
 if test $USER = root; then
   sudo -u stage sh -c "$CMD"
