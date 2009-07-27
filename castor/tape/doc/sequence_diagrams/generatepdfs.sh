36608f341b1d1a1c8d5402c320294b61da20ef15
Steven Murray <murrayc3@cern.ch>
2009-07-27 08:30:22 +0000

    Added the convenience script 'generatepdfs.sh'.  This script generates a pdf for each of the sequence diagrams in this directory

---
+++ castor/tape/doc/sequence_diagrams/generatepdfs.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+if test "x$CASTOR_CVS" = x; then
+  echo "Error: The environment variable CASTOR_CVS is not set"
+  echo
+  echo "CASTOR_CVS should be the full path to the CASTOR source code up to and"
+  echo "including CASTOR2, e.g."
+  echo
+  echo "    export CASTOR_CVS=/usr/local/src/CASTOR2"
+  echo
+  exit -1
+fi
+
+if test ! -d $CASTOR_CVS; then
+  echo "Error: The directory specified by CASTOR_CVS does not exist"
+  echo
+  echo "CASTOR_CVS=\"$CASTOR_CVS\""
+  echo
+  exit -1
+fi
+
+SEQUENCE_DIR="$CASTOR_CVS/castor/tape/doc/sequence_diagrams"
+
+if test ! -d $SEQUENCE_DIR; then
+  echo "Error: The following directory does not exist"
+  echo
+  echo "\"$SEQUENCE_DIR\""
+  echo
+  exit -1
+fi
+
+SEQUENCE_PIC="$SEQUENCE_DIR/sequence.pic"
+if test ! -e $SEQUENCE_PIC; then
+  echo "Error: The file $SEQUENCE_PIC does not exist"
+  echo
+  exit -1
+fi
+
+which pic2plot > /dev/null
+if test $? -ne 0; then
+  echo "Error: The pic2plot command is not on the path"
+  echo
+  exit -1
+fi
+
+which ps2pdf > /dev/null
+if test $? -ne 0; then
+  echo "Error: The ps2pdf command is not on the path"
+  echo
+  exit -1
+fi
+
+PICS=`ls $SEQUENCE_DIR/*.pic | egrep -v '\/sequence.pic$'`
+
+for PIC in $PICS; do
+  PDF=`echo $PIC | sed 's/\.pic$/.pdf/'`
+
+  echo "Generating $PDF"
+
+  if test ! -f $PIC; then
+    echo "Error: The following pic file does not exist:"
+    echo
+    echo "\"$PIC\""
+    echo
+    exit -1
+  fi
+
+  pic2plot -T ps $PIC | ps2pdf - $PDF
+done
+
+exit
