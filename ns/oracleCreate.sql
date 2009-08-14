d99201e4d55c2bbf482c371f7941583008146331
Giuseppe Lo Presti <itglp@cern.ch>
2009-08-14 16:47:39 +0000

    Moved prototyped code for the stat functionality in PL/SQL in a different file. This code is not meant (yet) for a production release of the Castor nameserver.

--- ns/oracleCreate.sql
+++ ns/oracleCreate.sql
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * @(#)$RCSfile: oracleCreate.sql,v $ $Release: 1.2 $ $Release$ $Date: 2009/06/30 12:54:05 $ $Author: waldron $
+ * @(#)$RCSfile: oracleCreate.sql,v $ $Release: 1.2 $ $Release$ $Date: 2009/08/14 16:47:39 $ $Author: itglp $
  *
  * This script creates a new Castor Name Server schema
  *
@@ -144,35 +144,11 @@ INSERT INTO Cns_version VALUES ('2_1_9_0', 'releaseTag');
 -- Create indexes on Cns_file_metadata
 CREATE INDEX PARENT_FILEID_IDX on Cns_file_metadata(parent_fileid);
 CREATE INDEX I_file_metadata_fileclass on Cns_file_metadata(fileclass);
-CREATE INDEX I_file_metadata_par_name_id on Cns_file_metadata(parent_fileid, name, fileid, acl);
 
 -- Temporary table to support Cns_bulkexist calls
 CREATE GLOBAL TEMPORARY TABLE Cns_files_exist_tmp
   (tmpFileId NUMBER) ON COMMIT DELETE ROWS;
 
--- Useful type for the tokenizer function
-CREATE OR REPLACE TYPE strList AS TABLE OF VARCHAR2(2048);
-
--- PL/SQL declaration for the castorNS package
-CREATE OR REPLACE PACKAGE castorns AS
-  TYPE Cns_Stat IS RECORD (
-    fileid NUMBER,
-    parent_fileid NUMBER,
-    filemode NUMBER(6),
-    nlink NUMBER(6),
-    euid NUMBER(6),
-    egid NUMBER(6),
-    filesize NUMBER,
-    atime NUMBER(10),
-    mtime NUMBER(10),
-    ctime NUMBER(10),
-    fileclass NUMBER(10),
-    status CHAR(1),
-    name VARCHAR2(1024)
-  );
-  TYPE Cns_Stat_Cur IS REF CURSOR RETURN Cns_Stat;
-END castorns;
-/
 
 -- A function to extract the full path of a file in one go
 CREATE OR REPLACE FUNCTION getPathForFileid(fid IN NUMBER) RETURN VARCHAR2 IS
@@ -194,75 +170,5 @@ BEGIN
      raise_application_error(-20001, '');
    END IF;
    RETURN p;
-END;
-/
-
--- A string tokenizer in PL/SQL
-CREATE OR REPLACE FUNCTION strTokenizer (p_list VARCHAR2, p_del VARCHAR2 := ',')
-  RETURN strList pipelined IS
-  l_idx INTEGER;
-  l_list VARCHAR2(32767) := p_list;
-  l_value VARCHAR2(32767);
-BEGIN
-  LOOP
-    l_idx := instr(l_list, p_del);
-    IF l_idx > 0 THEN
-      PIPE ROW(ltrim(rtrim(substr(l_list, 1, l_idx-1))));
-      l_list := substr(l_list, l_idx + length(p_del));
-    ELSE
-      PIPE ROW(ltrim(rtrim(l_list)));
-      EXIT;
-    END IF;
-  END LOOP;
-  RETURN;
-END;
-/
-
--- PL/SQL function to canonicalize a filepath, i.e. to drop multiple '/'s and resolve any '..'
-CREATE OR REPLACE FUNCTION canonicalizePath(path IN VARCHAR2) RETURN VARCHAR2 IS
-  buf VARCHAR2(2048);
-  ret VARCHAR2(2048);
-BEGIN
-  ret := REGEXP_REPLACE(path, '(/){2,}', '/');
-  LOOP
-    buf := ret;
-    -- a path component is by definition anything between two slashes
-    ret := REGEXP_REPLACE(buf, '/[^/]+/../', '/');
-    EXIT WHEN ret = buf;
-  END LOOP;
-  RETURN ret;
-END;
-/
-
--- PL/SQL function implementing the stat functionality, as in Cns_srv_statx()
-CREATE OR REPLACE PROCEDURE cnsStat(filename IN VARCHAR2, stat OUT castorns.Cns_Stat_Cur) AS
--- This is for the moment without permission checking. The final version will have to
--- include the uid,gid of the requestor + the CWD - see Cns_procreq.c:6088
-  fid NUMBER;
-  parentFid NUMBER;
-  acl NUMBER;
-BEGIN
-  fid := 2;
-  FOR component IN (SELECT * FROM TABLE(strTokenizer(canonicalizePath(filename), '/'))
-                     WHERE column_value IS NOT NULL) LOOP
-    --dbms_output.put_line(component.column_value || ' ' || fid);
-    SELECT /*+ INDEX(Cns_file_metadata I_file_metadata_par_name_id) */ 
-           fileId, parent_fileId, acl
-      INTO fid, parentFid, acl
-      FROM Cns_file_metadata
-     WHERE parent_fileId = fid
-       AND name = component.column_value;
-    -- ACL verification goes here
-    -- ...
-  END LOOP;
-  OPEN stat FOR
-    SELECT
-       FILEID, PARENT_FILEID,
-       FILEMODE, NLINK, OWNER_UID, GID, FILESIZE,
-       ATIME, MTIME, CTIME,
-       FILECLASS, STATUS, NAME
-     FROM Cns_file_metadata
-    WHERE fileid = fid;
-  --dbms_output.put_line(lastComponent || ' ' || fid);
 END;
 /
