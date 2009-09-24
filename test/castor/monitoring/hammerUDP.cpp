b28728926d1b36c0541866a619d5c93a71b8beed
Dennis Waldron <waldron@cern.ch>
2009-09-24 13:35:44 +0000

    Updated

--- test/castor/monitoring/hammerUDP.cpp
+++ test/castor/monitoring/hammerUDP.cpp
@@ -19,9 +19,8 @@
  *
  * @(#)$RCSfile: hammerUDP.cpp,v $ $Revision: 1.4 $ $Release$ $Date: 2009/07/23 12:22:06 $ $Author: waldron $
  *
- * test program hammering the monitoring with UDP messages of
- * psuedo migrators and/or recallers in order to test
- * rmmaster daemon stability
+ * Test program hammering the monitoring with UDP messages of psuedo migrators
+ * and/or recallers in order to test rmmaster daemon stability
  *
  * @author Sebastien Ponce
  *****************************************************************************/
@@ -31,65 +30,34 @@
 #include "castor/monitoring/StreamDirection.hpp"
 #include "castor/io/UDPSocket.hpp"
 #include "castor/monitoring/StreamReport.hpp"
+#include "castor/System.hpp"
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
-#include "getconfent.h"
 
 int main(int argc, char** argv) {
-
-  try {
-    
-    char* rmMasterHost = getconfent("RM","HOST", 0);
-    if (0 == rmMasterHost) {
-      // Raise an exception
-      castor::exception::NoEntry e;
-      e.getMessage() << "Found null entry RM/HOST in config file";
-      throw e;
-    }
-    int rmMasterPort = 15003;
-    char* rmMasterPortStr = getconfent("RM","PORT", 0);
-    if (rmMasterPortStr){
-      rmMasterPort = std::strtol(rmMasterPortStr,0,10);
-      if (0 == rmMasterPort) {
-	// Go back to default
-	rmMasterPort = 15003;
-	// "Bad rmmaster port value in configuration file"
-	castor::dlf::Param initParams[] =
-	  {castor::dlf::Param("Message", "Bad rmmaster port value in configuration file, default used"),
-	   castor::dlf::Param("Given value", rmMasterPortStr)};
-	castor::dlf::dlf_writep(nullCuuid, DLF_LVL_WARNING, 0, 2, initParams);
-      }
-    }
-
-    // get host name
-    char* hostname = (char*) calloc(200, 1);
-    if (gethostname(hostname, 200) < 0) {
-      std::cerr << "Couldn't get hostname" << std::endl;
-      exit(-1);
-    }
-    
-    // endless loop flooding the server with UDP messages
+  try {    
+    u_signed64 count = 1;
+    // Endless loop flooding the server with UDP messages
     while(1) {
       try {
-	castor::io::UDPSocket s(rmMasterPort, rmMasterHost);
-	castor::monitoring::StreamReport sr;
-	sr.setDiskServerName(hostname);
-	sr.setMountPoint("/srv/castor/01/");
-	sr.setDirection(castor::monitoring::STREAMDIRECTION_READ);
-	sr.setCreated(true);
-	s.sendObject(sr);
+        castor::io::UDPSocket s(15003, "localhost");
+        castor::monitoring::StreamReport sr;
+        sr.setDiskServerName(castor::System::getHostName());
+        sr.setMountPoint("/srv/castor/01/");
+        sr.setDirection(castor::monitoring::STREAMDIRECTION_READ);
+        sr.setCreated(true);
+        s.sendObject(sr);
+        std::cout << count << std::endl;
+        count++;
       } catch (castor::exception::Exception ex) {
-	castor::dlf::Param initParams[] =
-	  {castor::dlf::Param("Message", "Failed to send StreamReport to rmmaster daemon via UDP. Exception Caught."),
-	   castor::dlf::Param("Original Error", ex.getMessage().str())};
-	castor::dlf::dlf_writep(nullCuuid, DLF_LVL_ERROR, 0, 2, initParams);
+        std::cerr << "Caught exception: "
+                  << ex.getMessage().str() << std::endl;
       }
     }
 
   } catch (castor::exception::Exception e) {
     std::cerr << "Caught exception\n" << e.getMessage().str()
-	      << "\nExiting" << std::endl;
+              << "\nExiting" << std::endl;
   }
-
 }
