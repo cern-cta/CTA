60f7f2b6eb3bff95a2eae64ef73c1073bc0b3a74
Sebastien Ponce <sponcec3@cern.ch>
2009-09-24 15:38:12 +0000

    added gridftp test suite

---
+++ test/newtestsuite/castortests/resources/gridftp/tags.py
@@ -0,0 +1,15 @@
+if os.name == 'posix':
+    pathVar = 'LD_LIBRARY_PATH'
+elif os.name == 'mac':
+    pathVar = 'DYLD_LIBRARY_PATH'
+else:
+    raise OSError('Unsupported OS : ' + os.name)
+os.environ[pathVar] = os.environ['GLOBUSSYS'] + os.sep + 'lib' + os.pathsep + os.environ[pathVar]
+
+def globus_url_copy(self):
+    return os.environ['GLOBUSSYS'] + os.sep + 'bin' + os.sep + 'globus-url-copy'
+Setup.getTag_globus_url_copy = globus_url_copy
+
+def grid_proxy_info(self):
+    return os.environ['GLOBUSSYS'] + os.sep + 'bin' + os.sep + 'grid-proxy-info'
+Setup.getTag_grid_proxy_info = grid_proxy_info
