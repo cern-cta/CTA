2fb1ee8736656eac03c44b71dec41591bd47d5e4
Dennis Waldron <waldron@cern.ch>
2009-10-15 13:30:03 +0000

    Fixed: #57047: 2.1.9 name server should be backwards compatible with 2.1.8 database schemas

--- ns/oracleSchema.sql
+++ ns/oracleSchema.sql
@@ -24,119 +24,79 @@
  * @author Castor Dev team, castor-dev@cern.ch
  *****************************************************************************/
 
-CREATE TABLE Cns_class_metadata (
-       classid NUMBER(5),
-       name VARCHAR2(15),
-       owner_uid NUMBER(6),
-       gid NUMBER(6),
-       min_filesize NUMBER,
-       max_filesize NUMBER,
-       flags NUMBER(2),
-       maxdrives NUMBER(3),
-       max_segsize NUMBER,
-       migr_time_interval NUMBER,
-       mintime_beforemigr NUMBER,
-       nbcopies NUMBER(1),
-       retenp_on_disk NUMBER);
+CREATE TABLE Cns_class_metadata (classid NUMBER(5), name VARCHAR2(15), owner_uid NUMBER(6), gid NUMBER(6), min_filesize NUMBER, max_filesize NUMBER, flags NUMBER(2), maxdrives NUMBER(3), max_segsize NUMBER, migr_time_interval NUMBER, mintime_beforemigr NUMBER, nbcopies NUMBER(1), retenp_on_disk NUMBER);
 
-CREATE TABLE Cns_tp_pool (
-       classid NUMBER(5),
-       tape_pool VARCHAR2(15));
+CREATE TABLE Cns_tp_pool (classid NUMBER(5), tape_pool VARCHAR2(15));
 
-CREATE TABLE Cns_file_metadata (
-       fileid NUMBER,
-       parent_fileid NUMBER,
-       guid CHAR(36),
-       name VARCHAR2(255),
-       filemode NUMBER(6),
-       nlink NUMBER(6),
-       owner_uid NUMBER(6),
-       gid NUMBER(6),
-       filesize NUMBER,
-       atime NUMBER(10),
-       mtime NUMBER(10),
-       ctime NUMBER(10),
-       fileclass NUMBER(5),
-       status CHAR(1),
-       csumtype VARCHAR2(2),
-       csumvalue VARCHAR2(32),
-       acl VARCHAR2(3900))
-	STORAGE (INITIAL 5M NEXT 5M PCTINCREASE 0);
+CREATE TABLE Cns_file_metadata (fileid NUMBER, parent_fileid NUMBER, guid CHAR(36), name VARCHAR2(255), filemode NUMBER(6), nlink NUMBER(6), owner_uid NUMBER(6), gid NUMBER(6), filesize NUMBER, atime NUMBER(10), mtime NUMBER(10), ctime NUMBER(10), fileclass NUMBER(5), status CHAR(1), csumtype VARCHAR2(2), csumvalue VARCHAR2(32), acl VARCHAR2(3900)) STORAGE (INITIAL 5M NEXT 5M PCTINCREASE 0);
 
-CREATE TABLE Cns_user_metadata (
-       u_fileid NUMBER,
-       comments VARCHAR2(255));
+CREATE TABLE Cns_user_metadata (u_fileid NUMBER, comments VARCHAR2(255));
 
-CREATE TABLE Cns_seg_metadata (
-       s_fileid NUMBER,
-       copyno NUMBER(1),
-       fsec NUMBER(3),
-       segsize NUMBER,
-       compression NUMBER,
-       s_status CHAR(1),
-       vid VARCHAR2(6),
-       side NUMBER (1),
-       fseq NUMBER(10),
-       blockid RAW(4),
-       checksum_name VARCHAR2(16),
-       checksum NUMBER);
+CREATE TABLE Cns_seg_metadata (s_fileid NUMBER, copyno NUMBER(1),fsec NUMBER(3), segsize NUMBER, compression NUMBER, s_status CHAR(1), vid VARCHAR2(6), side NUMBER (1), fseq NUMBER(10), blockid RAW(4), checksum_name VARCHAR2(16), checksum NUMBER);
 
-CREATE TABLE Cns_symlinks (
-       fileid NUMBER,
-       linkname VARCHAR2(1023));
+CREATE TABLE Cns_symlinks (fileid NUMBER, linkname VARCHAR2(1023));
 
-CREATE TABLE Cns_groupinfo (
-       gid NUMBER(10),
-       groupname VARCHAR2(255));
+CREATE TABLE Cns_groupinfo (gid NUMBER(10), groupname VARCHAR2(255));
 
-CREATE TABLE Cns_userinfo (
-       userid NUMBER(10),
-       username VARCHAR2(255));
+CREATE TABLE Cns_userinfo (userid NUMBER(10), username VARCHAR2(255));
 
-CREATE SEQUENCE Cns_gid_seq START WITH 101 INCREMENT BY 1;
-CREATE SEQUENCE Cns_uid_seq START WITH 101 INCREMENT BY 1;
-CREATE SEQUENCE Cns_unique_id START WITH 3 INCREMENT BY 1;
+CREATE SEQUENCE Cns_unique_id START WITH 3 INCREMENT BY 1 CACHE 20;
 
 ALTER TABLE Cns_class_metadata
-       ADD CONSTRAINT pk_classid PRIMARY KEY (classid)
-       ADD CONSTRAINT classname UNIQUE (name);
+  ADD CONSTRAINT pk_classid PRIMARY KEY (classid)
+  ADD CONSTRAINT classname UNIQUE (name);
 
 ALTER TABLE Cns_file_metadata
-       ADD CONSTRAINT pk_fileid PRIMARY KEY (fileid)
-       ADD CONSTRAINT file_full_id UNIQUE (parent_fileid, name)
-       ADD CONSTRAINT file_guid UNIQUE (guid)
-	USING INDEX STORAGE (INITIAL 2M NEXT 2M PCTINCREASE 0);
+  ADD CONSTRAINT pk_fileid PRIMARY KEY (fileid)
+  ADD CONSTRAINT file_full_id UNIQUE (parent_fileid, name)
+  ADD CONSTRAINT file_guid UNIQUE (guid)
+    USING INDEX STORAGE (INITIAL 2M NEXT 2M PCTINCREASE 0);
 
 ALTER TABLE Cns_user_metadata
-       ADD CONSTRAINT pk_u_fileid PRIMARY KEY (u_fileid);
+  ADD CONSTRAINT pk_u_fileid PRIMARY KEY (u_fileid);
 
 ALTER TABLE Cns_seg_metadata
-       ADD CONSTRAINT pk_s_fileid PRIMARY KEY (s_fileid, copyno, fsec);
+  ADD CONSTRAINT pk_s_fileid PRIMARY KEY (s_fileid, copyno, fsec);
 
 ALTER TABLE Cns_symlinks
-       ADD CONSTRAINT pk_l_fileid PRIMARY KEY (fileid);
+  ADD CONSTRAINT pk_l_fileid PRIMARY KEY (fileid);
 
 ALTER TABLE Cns_groupinfo
-       ADD CONSTRAINT map_groupname UNIQUE (groupname);
+  ADD CONSTRAINT map_groupname UNIQUE (groupname);
 
 ALTER TABLE Cns_userinfo
-       ADD CONSTRAINT map_username UNIQUE (username);
+  ADD CONSTRAINT map_username UNIQUE (username);
 
 ALTER TABLE Cns_user_metadata
-       ADD CONSTRAINT fk_u_fileid FOREIGN KEY (u_fileid) REFERENCES Cns_file_metadata (fileid);
+  ADD CONSTRAINT fk_u_fileid FOREIGN KEY (u_fileid)
+  REFERENCES Cns_file_metadata (fileid);
 
 ALTER TABLE Cns_file_metadata
-       ADD CONSTRAINT fk_class FOREIGN KEY (fileclass) REFERENCES Cns_class_metadata (classid);
+  ADD CONSTRAINT fk_class FOREIGN KEY (fileclass)
+  REFERENCES Cns_class_metadata (classid);
 
 ALTER TABLE Cns_seg_metadata
-       ADD CONSTRAINT fk_s_fileid FOREIGN KEY (s_fileid) REFERENCES Cns_file_metadata (fileid)
-       ADD CONSTRAINT tapeseg UNIQUE (vid, side, fseq);
+  ADD CONSTRAINT fk_s_fileid FOREIGN KEY (s_fileid)
+  REFERENCES Cns_file_metadata (fileid)
+  ADD CONSTRAINT tapeseg UNIQUE (vid, side, fseq);
 
 ALTER TABLE Cns_tp_pool
-       ADD CONSTRAINT classpool UNIQUE (classid, tape_pool);
+  ADD CONSTRAINT classpool UNIQUE (classid, tape_pool);
 
 ALTER TABLE Cns_symlinks
-       ADD CONSTRAINT fk_l_fileid FOREIGN KEY (fileid) REFERENCES Cns_file_metadata (fileid);
+  ADD CONSTRAINT fk_l_fileid FOREIGN KEY (fileid)
+  REFERENCES Cns_file_metadata (fileid);
+
+-- Constraint to prevent negative nlinks
+ALTER TABLE Cns_file_metadata 
+  ADD CONSTRAINT CK_FileMetadata_Nlink
+  CHECK (nlink >= 0 AND nlink IS NOT NULL);
+
+-- Constraint to prevent a NULL file class on non symlink entries
+ALTER TABLE Cns_file_metadata
+  ADD CONSTRAINT CK_FileMetadata_FileClass
+  CHECK ((bitand(filemode, 40960) = 40960  AND fileclass IS NULL)
+     OR  (bitand(filemode, 40960) != 40960 AND fileclass IS NOT NULL));
 
 -- Create indexes on Cns_file_metadata
 CREATE INDEX PARENT_FILEID_IDX ON Cns_file_metadata (parent_fileid);
