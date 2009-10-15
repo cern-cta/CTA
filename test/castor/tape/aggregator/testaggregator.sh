962050a8ec3f612e4fa7a3d8773ad091f2eaba25
Steven Murray <murrayc3@cern.ch>
2009-10-15 09:36:00 +0000

    The environment variable CASTOR_CVS used for testing has been changed to CASTOR_SRC in order to be independent of the name of the source code repository software.

--- test/castor/tape/aggregator/testaggregator.sh
+++ test/castor/tape/aggregator/testaggregator.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-if test "x$CASTOR_CVS" = x; then
-  echo "Error: The environment variable CASTOR_CVS is not set"
+if test "x$CASTOR_SRC" = x; then
+  echo "Error: The environment variable CASTOR_SRC is not set"
   echo
-  echo "CASTOR_CVS should be the full path to the CASTOR source code up to and"
-  echo "including CASTOR2, e.g."
+  echo "CASTOR_SRC should be the full path to the CASTOR source code, e.g."
   echo
-  echo "    export CASTOR_CVS=/usr/local/src/CASTOR2"
+  echo "    export CASTOR_SRC=/usr/local/src/CASTOR_SVN_CO/trunk"
   echo
   exit -1
 fi
 
-LD_LIBRARY_PATH=`find $CASTOR_CVS -name '*.so*' | sed 's|/[^/]*$||' | sort | uniq`
+LD_LIBRARY_PATH=`find $CASTOR_SRC -name '*.so*' | sed 's|/[^/]*$||' | sort | uniq`
 LD_LIBRARY_PATH=`echo $LD_LIBRARY_PATH | sed 's/ /:/g'`
 PARAMS=$@
-CMD="export LD_LIBRARY_PATH=$LD_LIBRARY_PATH; $CASTOR_CVS/test/castor/tape/aggregator/testaggregator $PARAMS"
+CMD="export LD_LIBRARY_PATH=$LD_LIBRARY_PATH; $CASTOR_SRC/test/castor/tape/aggregator/testaggregator $PARAMS"
 
 if test $USER = root; then
   sudo -u stage sh -c "$CMD"
