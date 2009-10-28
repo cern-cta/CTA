39fb486dff666b7d69a383e14594cdd174d9833a
Steven Murray <murrayc3@cern.ch>
2009-10-28 16:10:04 +0000

    The possible NO_DATA_FOUND exceptions of all of the "select into" statements are now caught and converted into application exceptions"

--- vmgr/temporaryOracleTrailerWithTPMSProcedures.sql
+++ vmgr/temporaryOracleTrailerWithTPMSProcedures.sql
@@ -26,140 +26,205 @@
 UPDATE UpgradeLog SET schemaVersion = '2_1_9_3';
 
 
+/**
+ * Deletes the tape pool with the specified name from the VMGR database.
+ *
+ * This procedure raises application error number -20501 if the specified
+ * tape pool does not exist in the VMGR database.
+ *
+ * This procedure raises application error number -20502 if the specified
+ * tape pool is not empty.
+ *
+ * @param pool_name_var The name of the tape pool to be deleted.
+ */
 CREATE OR REPLACE PROCEDURE VMGR_DELETE_POOL (
-  pool_name IN VARCHAR2)
+  pool_name_var IN VARCHAR2)
 IS
-  tmp_capacity           NUMBER;
-  tmp_free_space         NUMBER;
-  nonempty_pool_deletion EXCEPTION;
+  capacity_var   NUMBER := NULL;
+  free_space_var NUMBER := NULL;
 BEGIN
 
-  -- Check whether the pool is empty or not - do not allow the removal a pool
-  -- that still contains tapes.
+  BEGIN
+    -- Lock the database row representing the tape pool
+    --
+    -- The lock is required to make sure the following check to see if the pool
+    -- still contains tapes is still valid when and the deletion of the row is
+    -- carried out
+    SELECT tot_free_space, capacity
+      INTO free_space_var, capacity_var
+      FROM vmgr_tape_pool
+      WHERE name = pool_name_var
+      FOR UPDATE;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      RAISE_APPLICATION_ERROR (-20501,
+        'The ' || pool_name_var ||
+        ' tape pool cannot be deleted,' ||
+        ' because it does not exist in the VMGR database.');
+  END;
 
-  SELECT tot_free_space, capacity
-    INTO tmp_free_space, tmp_capacity
-    FROM vmgr_tape_pool
-    WHERE name = pool_name;
-
-  IF ((tmp_capacity > 0) OR (tmp_free_space > 0)) THEN
-    RAISE nonempty_pool_deletion;
+  -- The tape pool must not be deleted if it is not empty
+  IF ((capacity_var > 0) OR (free_space_var > 0)) THEN
+    RAISE_APPLICATION_ERROR (-20502,
+      'The ' || pool_name_var ||
+      ' tape pool cannot be deleted, because it is not empty.' ||
+      ' Both the capacity and the free space of the tape pool must be 0.');
   END IF;
 
-  DELETE FROM vmgr_tape_pool WHERE name = pool_name;
+  -- It is now safe to delete the tape pool
+  DELETE FROM vmgr_tape_pool WHERE name = pool_name_var;
 
-EXCEPTION
-  WHEN nonempty_pool_deletion
-  THEN
-    RAISE_APPLICATION_ERROR (-20501,
-      'Pool: ' || pool_name ||
-      ' can not be deleted because it is not empty.' ||
-      ' Please check: capacity and free space - they must be both 0.');
 END VMGR_DELETE_POOL;
 /
 
 
+/**
+ * Inserts the specified new tape pool into the VMGR database.
+ *
+ * @param pool_name_var The name of the new tape pool.
+ * @param owner_uid_var The user ID of the owner of the new tape pool.
+ * @param owner_gid_var The group ID of the owner of the new tape pool.
+ */
 CREATE OR REPLACE PROCEDURE VMGR_INSERT_POOL (
-  pool_name IN VARCHAR2,
-  owner_uid IN NUMBER,
-  owner_gid IN NUMBER)
+  pool_name_var IN VARCHAR2,
+  owner_uid_var IN NUMBER,
+  owner_gid_var IN NUMBER)
 IS
 BEGIN
   INSERT INTO vmgr_tape_pool(name, owner_uid, gid, tot_free_space, capacity)
-    VALUES (pool_name, owner_uid, owner_gid, 0, 0);
+    VALUES (pool_name_var, owner_uid_var, owner_gid_var, 0, 0);
 END VMGR_INSERT_POOL;
 /
 
 
+/**
+ * Moves the specified tape to the specified destination tape pool.
+ *
+ * This procedure raises application error number -20503 if the specified
+ * tape does not exist in the VMGR database.
+ *
+ * This procedure raises application error number -20504 if the capacity of the
+ * specified tape does not exist in the VMGR database.
+ *
+ * This procedure raises application error number -20505 if the information
+ * about the sides of the specified tape does not exist in the VMGR database.
+ *
+ * This procedure raises application error number -20506 if the specified
+ * destination pool does not exist in the VMGR database.
+ *
+ * @param vid_tape_var         The VID of the tape to be moved.
+ * @param destination_pool_var The name of the destination tape pool.
+ */
 CREATE OR REPLACE PROCEDURE VMGR_MOVE_TAPE2POOL(
-  vid_tape         IN VARCHAR2,
-  destination_pool IN VARCHAR2)
+  vid_tape_var         IN VARCHAR2,
+  destination_pool_var IN VARCHAR2)
 IS
-  destination_pool_check  VARCHAR2(15);
-  estimated_free_tape     NUMBER;
-  source_pool             VARCHAR2(15);
-  tape_capacity           NUMBER;
-  tape_density            VARCHAR2(8);
-  model                   VARCHAR2(6);
-  media_letter            VARCHAR2(1);
-  no_pool                 EXCEPTION;
-  no_tape                 EXCEPTION;
+  dummy_destination_pool_var VARCHAR2(15);
+  estimated_free_space_var   NUMBER;
+  source_pool_var            VARCHAR2(15);
+  tape_capacity_var          NUMBER;
+  tape_density_var           VARCHAR2(8);
+  model_var                  VARCHAR2(6);
+  media_letter_var           VARCHAR2(1);
 BEGIN
-  -- Lock the row with the VID to keep the estimated_free_space and the
-  -- capacity consistent
-
   BEGIN
+    -- Lock the the vmgr_tape_info row
     SELECT density, model, media_letter
-      INTO tape_density, model, media_letter
+      INTO tape_density_var, model_var, media_letter_var
       FROM  vmgr_tape_info
-      WHERE vid = vid_tape FOR UPDATE;
+      WHERE vid = vid_tape_var FOR UPDATE;
   EXCEPTION
     WHEN NO_DATA_FOUND THEN
-      raise_application_error (-20502, 'Cannot find tape ' || vid_tape ||
-        ' in the VMGR_TAPE_INFO table.' ||
-        ' Perhaps it does not exist in the system.');
+      RAISE_APPLICATION_ERROR(-20503, 'The ' || vid_tape_var || ' tape' ||
+        ' cannot be moved, because it does not exist in the VMGR database.'); 
   END;
 
-  SELECT native_capacity
-    INTO tape_capacity
-    FROM vmgr_tape_denmap
-    WHERE
-       md_density      = tape_density AND
-       md_model        = model        AND
-       md_media_letter = media_letter;
-
-  SELECT estimated_free_space, poolname
-    INTO estimated_free_tape, source_pool
-    FROM vmgr_tape_side
-    WHERE vid = vid_tape AND side = 0 FOR UPDATE;
-
-  -- Check whether the destination pool actually exists.
   BEGIN
-    SELECT name
-      INTO destination_pool_check
-      FROM vmgr_tape_pool
-      WHERE name = destination_pool
+    -- Get the capacity of the tape
+    SELECT native_capacity
+      INTO tape_capacity_var
+      FROM vmgr_tape_denmap
+      WHERE
+         md_density      = tape_density_var AND
+         md_model        = model_var        AND
+         md_media_letter = media_letter_var;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      RAISE_APPLICATION_ERROR(-20504, 'The ' || vid_tape_var || ' tape' ||
+        ' cannot be moved, because the capacity of the tape does not exist' ||
+        ' in the VMGR database.');
+  END;
+
+  BEGIN
+    -- Lock the tape side info row
+    SELECT estimated_free_space    , poolname
+      INTO estimated_free_space_var, source_pool_var
+      FROM vmgr_tape_side
+      WHERE vid = vid_tape_var AND side = 0
       FOR UPDATE;
   EXCEPTION
     WHEN NO_DATA_FOUND THEN
-      raise_application_error (-20503, 'Destination pool ' || destination_pool
-        || ' does not exist in the VMGR_TAPE_POOL table. Tape ' || vid_tape ||
-        ' can not be moved.');
+      RAISE_APPLICATION_ERROR(-20505, 'The ' || vid_tape_var || ' tape' ||
+        ' cannot be moved, because the information about the sides of the' ||
+        ' tape is not in the database.');
+  END;
+
+  BEGIN
+    -- Lock the tape pool row
+    SELECT name
+      INTO dummy_destination_pool_var
+      FROM vmgr_tape_pool
+      WHERE name = destination_pool_var
+      FOR UPDATE;
+  EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+      raise_application_error (-20506, 'The ' || vid_tape_var || ' tape' ||
+        ' cannot be moved, because the destination pool ' ||
+        destination_pool_var ||
+        ' does not exist in the VMGR database.');
   END;
 
   -- Substract the tape's free space and capacity from the source pool
   UPDATE vmgr_tape_pool
-    SET tot_free_space = (tot_free_space - estimated_free_tape),
-        capacity = (capacity - tape_capacity)
-    WHERE name = source_pool;
+    SET
+      tot_free_space = tot_free_space - estimated_free_space_var,
+      capacity       = capacity       - tape_capacity_var
+    WHERE name = source_pool_var;
 
   -- Add the tape's free space and capacity from the source pool
   UPDATE vmgr_tape_pool
-    SET tot_free_space = (tot_free_space + estimated_free_tape),
-        capacity = (capacity + tape_capacity)
-    WHERE name = destination_pool;
+    SET
+      tot_free_space = tot_free_space + estimated_free_space_var,
+      capacity       = capacity       + tape_capacity_var
+    WHERE name = destination_pool_var;
 
   -- Move the tape from the source pool to the destination pool
   UPDATE vmgr_tape_side
-    SET poolname = destination_pool
-    WHERE vid = vid_tape AND side=0;
-
---  INSERT INTO tpms_log_table (source, message)
---    VALUES ('VMGR_MOVE_TAPE2POOL', 'moved tape ' || vid_tape || ' from ' || source_pool || ' to ' || destination_pool);
-
-  COMMIT;
+    SET poolname = destination_pool_var
+    WHERE
+      vid  = vid_tape_var AND
+      side = 0;
 END;
 /
 
 
-CREATE OR REPLACE PROCEDURE VMGR_UPDATE_POOL(
-  pool_name     IN VARCHAR2,
-  tmp_owner_uid IN NUMBER,
-  owner_gid     IN NUMBER)
+/**
+ * Updates the owner user ID and group ID of the specified tape pool.
+ *
+ * @param pool_name_var The name of the tape pool to be updated.
+ * @param owner_uid_var The new owner user ID.
+ * @param owner_gid_var The new owner group ID.
+ */
+CREATE OR REPLACE PROCEDURE VMGR_UPDATE_POOL_OWNER(
+  pool_name_var IN VARCHAR2,
+  owner_uid_var IN NUMBER,
+  owner_gid_var IN NUMBER)
 IS
 BEGIN
-  UPDATE vmgr_tape_pool
-    SET owner_uid = tmp_owner_uid, gid = owner_gid
-    WHERE name = pool_name;
-END VMGR_UPDATE_POOL;
+  UPDATE vmgr_tape_pool SET
+      owner_uid = owner_uid_var,
+      gid       = owner_gid_var
+    WHERE name = pool_name_var;
+END VMGR_UPDATE_POOL_OWNER;
 /
+
