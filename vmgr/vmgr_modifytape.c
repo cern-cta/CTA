a6c9e8213db3c03f3b5ddb1bc949f3e22a4e89cb
Nicola Bessone <nbessone@cern.ch>
2010-02-02 16:45:40 +0000

    This commit concern the service request:
    sr #112497: RFE: Modification of the vmgrmodifytape command-line user feedback.
    
    Now the vmgrmodifytape command-line in cased the user try to modify the density of a tape that still contain tape files will report:
    
    vmgrmodifytape XXXXXX: There are physical files on the tape. Please reclaim the tape before modify the density.
    
    instead of the previous general message:" Files exists "

--- vmgr/vmgr_modifytape.c
+++ vmgr/vmgr_modifytape.c
@@ -20,90 +20,90 @@
 
 int vmgr_modifytape(const char *vid, char *vsn, char *library, char *density, char *lbltype, char *manufacturer, char *sn, char *poolname, int status)
 {
-	int c;
-	char func[15];
-	gid_t gid;
-	int msglen;
-	char *q;
-	char *sbp;
-	char sendbuf[REQBUFSZ];
-	struct vmgr_api_thread_info *thip;
-	uid_t uid;
+  int c;
+  char func[15];
+  gid_t gid;
+  int msglen;
+  char *q;
+  char *sbp;
+  char sendbuf[REQBUFSZ];
+  struct vmgr_api_thread_info *thip;
+  uid_t uid;
 
-        strcpy (func, "vmgr_modifytape");
-        if (vmgr_apiinit (&thip))
-                return (-1);
-        uid = geteuid();
-        gid = getegid();
+  strcpy (func, "vmgr_modifytape");
+  if (vmgr_apiinit (&thip))
+          return (-1);
+  uid = geteuid();
+  gid = getegid();
 #if defined(_WIN32)
-        if (uid < 0 || gid < 0) {
-                vmgr_errmsg (func, VMG53);
-                serrno = SENOMAPFND;
-                return (-1);
-        }
+  if (uid < 0 || gid < 0) {
+          vmgr_errmsg (func, VMG53);
+          serrno = SENOMAPFND;
+          return (-1);
+  }
 #endif
 
-	if (! vid) {
-		serrno = EFAULT;
-		return (-1);
-	}
+  if (! vid) {
+    serrno = EFAULT;
+    return (-1);
+  }
  
-	/* Build request header */
+  /* Build request header */
 
-	sbp = sendbuf;
-	marshall_LONG (sbp, VMGR_MAGIC);
-	marshall_LONG (sbp, VMGR_MODTAPE);
-	q = sbp;        /* save pointer. The next field will be updated */
-	msglen = 3 * LONGSIZE;
-	marshall_LONG (sbp, msglen);
+  sbp = sendbuf;
+  marshall_LONG (sbp, VMGR_MAGIC);
+  marshall_LONG (sbp, VMGR_MODTAPE);
+  q = sbp;        /* save pointer. The next field will be updated */
+  msglen = 3 * LONGSIZE;
+  marshall_LONG (sbp, msglen);
 
-	/* Build request body */
+  /* Build request body */
  
-	marshall_LONG (sbp, uid);
-	marshall_LONG (sbp, gid);
-	marshall_STRING (sbp, vid);
-	if (vsn) {
-		marshall_STRING (sbp, vsn);
-	} else {
-		marshall_STRING (sbp, "");
-	}
-	if (library) {
-		marshall_STRING (sbp, library);
-	} else {
-		marshall_STRING (sbp, "");
-	}
-	if (density) {
-		marshall_STRING (sbp, density);
-	} else {
-		marshall_STRING (sbp, "");
-	}
-	if (lbltype) {
-		marshall_STRING (sbp, lbltype);
-	} else {
-		marshall_STRING (sbp, "");
-	}
-	if (manufacturer) {
-		marshall_STRING (sbp, manufacturer);
-	} else {
-		marshall_STRING (sbp, "");
-	}
-	if (sn) {
-		marshall_STRING (sbp, sn);
-	} else {
-		marshall_STRING (sbp, "");
-	}
-	if (poolname) {
-		marshall_STRING (sbp, poolname);
-	} else {
-		marshall_STRING (sbp, "");
-	}
-	marshall_LONG (sbp, status);
+  marshall_LONG (sbp, uid);
+  marshall_LONG (sbp, gid);
+  marshall_STRING (sbp, vid);
+  if (vsn) {
+    marshall_STRING (sbp, vsn);
+  } else {
+    marshall_STRING (sbp, "");
+  }
+  if (library) {
+    marshall_STRING (sbp, library);
+  } else {
+    marshall_STRING (sbp, "");
+  }
+  if (density) {
+    marshall_STRING (sbp, density);
+  } else {
+    marshall_STRING (sbp, "");
+  }
+  if (lbltype) {
+    marshall_STRING (sbp, lbltype);
+  } else {
+    marshall_STRING (sbp, "");
+  }
+  if (manufacturer) {
+    marshall_STRING (sbp, manufacturer);
+  } else {
+    marshall_STRING (sbp, "");
+  }
+  if (sn) {
+    marshall_STRING (sbp, sn);
+  } else {
+    marshall_STRING (sbp, "");
+  }
+  if (poolname) {
+    marshall_STRING (sbp, poolname);
+  } else {
+    marshall_STRING (sbp, "");
+  }
+  marshall_LONG (sbp, status);
  
-	msglen = sbp - sendbuf;
-	marshall_LONG (q, msglen);	/* update length field */
+  msglen = sbp - sendbuf;
+  marshall_LONG (q, msglen);  /* update length field */
 
-	while ((c = send2vmgr (NULL, sendbuf, msglen, NULL, 0)) &&
-	    serrno == EVMGRNACT)
-		sleep (RETRYI);
-	return (c);
+  while ((c = send2vmgr (NULL, sendbuf, msglen, NULL, 0)) &&
+      serrno == EVMGRNACT)
+    sleep (RETRYI);
+  return (c);
 }
--- vmgr/vmgrmodifytape.c
+++ vmgr/vmgrmodifytape.c
@@ -3,7 +3,7 @@
  * All rights reserved
  */
 
-/*	vmgrmodifytape - modify an existing tape volume */
+/*  vmgrmodifytape - modify an existing tape volume */
 #include <ctype.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -19,128 +19,135 @@ int main(argc, argv)
 int argc;
 char **argv;
 {
-	int c;
-	char *density = NULL;
-	char *dp;
-	int errflg = 0;
-	char *lbltype = NULL;
-	char *library = NULL;
-	static struct Coptions longopts[] = {
-		{"library", REQUIRED_ARGUMENT, 0, OPT_LIBRARY_NAME},
-		{"manufacturer", REQUIRED_ARGUMENT, 0, OPT_MANUFACTURER},
-		{"pool", REQUIRED_ARGUMENT, 0, 'P'},
-		{"sn", REQUIRED_ARGUMENT, 0, OPT_SN},
-		{"status", REQUIRED_ARGUMENT, 0, OPT_STATUS},
-		{0, 0, 0, 0}
-	};
-	char *manufacturer = NULL;
-	char *p;
-	char *pool_name = NULL;
-	char *sn = NULL;
-	int status = -1;
-	char statusa[50];
-	char *vid = NULL;
-	char *vsn = NULL;
+  int c;
+  char *density = NULL;
+  char *dp;
+  int errflg = 0;
+  char *lbltype = NULL;
+  char *library = NULL;
+  static struct Coptions longopts[] = {
+    {"library", REQUIRED_ARGUMENT, 0, OPT_LIBRARY_NAME},
+    {"manufacturer", REQUIRED_ARGUMENT, 0, OPT_MANUFACTURER},
+    {"pool", REQUIRED_ARGUMENT, 0, 'P'},
+    {"sn", REQUIRED_ARGUMENT, 0, OPT_SN},
+    {"status", REQUIRED_ARGUMENT, 0, OPT_STATUS},
+    {0, 0, 0, 0}
+  };
+  char *manufacturer = NULL;
+  char *p;
+  char *pool_name = NULL;
+  char *sn = NULL;
+  int status = -1;
+  char statusa[50];
+  char *vid = NULL;
+  char *vsn = NULL;
 
-	Copterr = 1;
-	Coptind = 1;
-        while ((c = Cgetopt_long (argc, argv, "d:l:P:V:v:", longopts, NULL)) != EOF) {
-                switch (c) {
-		case OPT_LIBRARY_NAME:
-			library = Coptarg;
-			break;
-		case OPT_MANUFACTURER:
-			manufacturer = Coptarg;
-                        break;
-                case OPT_SN:
-			sn = Coptarg;
-                        break;
-		case OPT_STATUS:
-			if (isdigit (*Coptarg)) {
-				if ((status = strtol (Coptarg, &dp, 10)) < 0 ||
-				    *dp != '\0') {
-					fprintf (stderr,
-					    "invalid status %s\n", Coptarg);
-					errflg++;
-				}
-			} else {
-				if (strlen (Coptarg) >= sizeof (statusa)) {
-					fprintf (stderr,
-					    "invalid status %s\n", Coptarg);
-					errflg++;
-					break;
-				}
-				status = 0;
-				strcpy (statusa, Coptarg);
-				p = strtok (statusa, "|");
-				while (p) {
-					if (strcmp (p, "DISABLED") == 0)
-						status |= DISABLED;
-					else if (strcmp (p, "EXPORTED") == 0)
-						status |= EXPORTED;
-					else if (strcmp (p, "TAPE_FULL") == 0)
-						status |= TAPE_FULL;
-					else if (strcmp (p, "FULL") == 0)
-						status |= TAPE_FULL;
-					else if (strcmp (p, "TAPE_RDONLY") == 0)
-						status |= TAPE_RDONLY;
-					else if (strcmp (p, "RDONLY") == 0)
-						status |= TAPE_RDONLY;
-					else if (strcmp (p, "ARCHIVED") == 0)
-						status |= ARCHIVED;
-					else {
-						fprintf (stderr,
-						    "invalid status %s\n", Coptarg);
-						errflg++;
-						break;
-					}
-					p = strtok (NULL, "|");
-				}
-			}
-			break;
-		case 'd':
-			density = Coptarg;
-			break;
-		case 'l':
-			lbltype = Coptarg;
-			break;
-		case 'P':
-			pool_name = Coptarg;
-			break;
-		case 'V':
-			vid = Coptarg;
-			break;
-		case 'v':
-			vsn = Coptarg;
-			break;
-                case '?':
-                        errflg++;
-                        break;
-                default:
-                        break;
-                }
+  Copterr = 1;
+  Coptind = 1;
+  while ((c = Cgetopt_long (argc, argv, "d:l:P:V:v:", longopts, NULL)) != EOF) {
+    switch (c) {
+    case OPT_LIBRARY_NAME:
+      library = Coptarg;
+      break;
+    case OPT_MANUFACTURER:
+      manufacturer = Coptarg;
+      break;
+    case OPT_SN:
+      sn = Coptarg;
+      break;
+    case OPT_STATUS:
+      if (isdigit (*Coptarg)) {
+        if ((status = strtol (Coptarg, &dp, 10)) < 0 ||
+            *dp != '\0') {
+          fprintf (stderr,
+              "invalid status %s\n", Coptarg);
+          errflg++;
         }
-        if (Coptind < argc) {
-                errflg++;
+      } else {
+        if (strlen (Coptarg) >= sizeof (statusa)) {
+          fprintf (stderr,
+              "invalid status %s\n", Coptarg);
+          errflg++;
+          break;
         }
-	if (vid == NULL) {
-		fprintf (stderr, "no vid\n");
-		errflg++;
-	}
+        status = 0;
+        strcpy (statusa, Coptarg);
+        p = strtok (statusa, "|");
+        while (p) {
+          if (strcmp (p, "DISABLED") == 0)
+            status |= DISABLED;
+          else if (strcmp (p, "EXPORTED") == 0)
+            status |= EXPORTED;
+          else if (strcmp (p, "TAPE_FULL") == 0)
+            status |= TAPE_FULL;
+          else if (strcmp (p, "FULL") == 0)
+            status |= TAPE_FULL;
+          else if (strcmp (p, "TAPE_RDONLY") == 0)
+            status |= TAPE_RDONLY;
+          else if (strcmp (p, "RDONLY") == 0)
+            status |= TAPE_RDONLY;
+          else if (strcmp (p, "ARCHIVED") == 0)
+            status |= ARCHIVED;
+          else {
+            fprintf (stderr,
+                "invalid status %s\n", Coptarg);
+            errflg++;
+            break;
+          } // else
+          p = strtok (NULL, "|");
+        } // while (p)
+      } // else
+      break;
+    case 'd':
+      density = Coptarg;
+      break;
+    case 'l':
+      lbltype = Coptarg;
+      break;
+    case 'P':
+      pool_name = Coptarg;
+      break;
+    case 'V':
+      vid = Coptarg;
+      break;
+    case 'v':
+      vsn = Coptarg;
+      break;
+    case '?':
+      errflg++;
+      break;
+    default:
+      break;
+    } // switch (c)
+  } // while ((c = Cgetopt_
+  if (Coptind < argc) {
+     errflg++;
+  }
+  if (vid == NULL) {
+    fprintf (stderr, "no vid\n");
+    errflg++;
+  }
 
-	if (errflg) {
-                fprintf (stderr, "usage: %s %s%s%s", argv[0],
-		    "[-d density] [-l lbltype] [-P pool_name] -V vid [-v vsn]\n",
-		    "[--library library] [--ma manufacturer] [--pool pool_name]\n",
-		    "[--sn serial_number] [--st status]\n");
-                exit (USERR);
-        }
+  if (errflg) {
+     fprintf (stderr, "usage: %s %s%s%s", argv[0],
+        "[-d density] [-l lbltype] [-P pool_name] -V vid [-v vsn]\n",
+        "[--library library] [--ma manufacturer] [--pool pool_name]\n",
+        "[--sn serial_number] [--st status]\n");
+     exit (USERR);
+   }
  
-	if (vmgr_modifytape (vid, vsn, library, density, lbltype,
-	    manufacturer, sn, pool_name, status) < 0) {
-		fprintf (stderr, "vmgrmodifytape %s: %s\n", vid,
-			 (serrno == ENOENT) ? "No such tape" : sstrerror(serrno));
-		exit (USERR);
-	}
-	exit (0);
+  if (vmgr_modifytape (vid, vsn, library, density, lbltype,
+      manufacturer, sn, pool_name, status) < 0) {
+    if(serrno == EEXIST){
+      fprintf (stderr, "vmgrmodifytape %s:"
+       " There are physical files on the tape."
+       " Please reclaim the tape before modify the density or the lable.\n", vid);
+    }
+    else {
+      fprintf (stderr, "vmgrmodifytape %s: %s\n", vid,
+       (serrno == ENOENT) ? "No such tape" : sstrerror(serrno));
+    }
+    exit (USERR);
+  }
+  exit (0);
 }
