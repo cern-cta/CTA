677304fb1cb6e255da49388185e49b10ed4f54c9
Giuseppe Lo Presti <itglp@cern.ch>
2010-03-05 09:20:01 +0000

    Added useful commands to trace sessions server side (credits to Nilo)

--- castor/db/analyze/admin.sql
+++ castor/db/analyze/admin.sql
@@ -21,12 +21,29 @@ begin dbms_workload_repository.create_snapshot; end;
 select sum(bytes)/(1024*1024) "MB", segment_name from user_extents
 where segment_type='TABLE' group by segment_name;
 
--- too see deadlocks and sessions
+-- To see deadlocks and sessions
 https://oraweb.cern.ch/pls/castor_dev/webinstance.sessions.show_sessions
 
--- recalculate statistics
+-- Recalculate statistics
 BEGIN
   DBMS_STATS.GATHER_TABLE_STATS (ownname=>'CASTOR_STAGER', tabname=>'...', estimate_percent=>100, 
 	  method_opt=>'FOR ALL COLUMNS SIZE 1',no_invalidate=>FALSE,force=>TRUE);
 END;
 /
+
+-- Show running sessions and their PIDs
+select (select a.spid from v$process a where a.addr=b.paddr) SERVER_PID
+        ,b.username || '*' || b.sid ORACLE_USER
+         ,b.osuser || '@' || b.machine CLIENT_USER
+         ,decode(b.process,NULL,'*',b.process) CLIENT_PID, b.client_Info
+   from v$session b
+  where b.type = 'USER' and
+        b.username is not null and b.machine='<your client machine>' and
+        b.status not in ('KILLED','SNIPED')
+  order by SERVER_PID, ORACLE_USER;
+
+-- To trace a session
+SQL> oradebug setospid PID   -- where PID is the process id at the server level
+
+SQL> oradebug trace name context forever, level 12;
+SQL> oradebug trace name context off;
