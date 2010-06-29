0ae947b10af54f1ce97a34af5de1ef6a3b869b78
Steven Murray <murrayc3@cern.ch>
2010-06-29 16:00:16 +0000

    Added a helper PL/SQL procedure for cleaning up the tape-gateway part of the stager schema.  This procedure may become part of the offical tape-gateway release.

---
+++ test/tapegateway_release_tests/createProcedureOps_resetTapeCopy.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE PROCEDURE ops_resetTapeCopy(
+  tapeCopyIdVar IN NUMBER
+) AS
+
+-- This procedure resets the tape-copy with the specified id to
+-- TAPECOPY_CREATED.  This includes detaching the tape-copy from the streams
+-- and deleting the corresponding tape-gateway sub-request.
+--
+-- This procedure raises an application error with code -20001 if the specified
+-- tape-copy does not exist.
+
+  tapeGatewaySubRequestIdVar NUMBER(38);
+BEGIN
+  -- Try to set the status of the tape-copy to TAPECOPY_CREATED
+  UPDATE TapeCopy
+    SET STATUS = 0 -- TAPECOPY_CREATED
+    WHERE id = tapeCopyIdVar;
+
+  -- Raise an exception if the tape-copy does not exist
+  IF SQL%ROWCOUNT = 0 THEN
+    RAISE_APPLICATION_ERROR(-20001,
+      'Tape-copy does not exist: tapeCopyIdVar=' || tapeCopyIdVar);
+  END IF;
+
+  -- Detach the tape-copy from the streams
+  DELETE FROM Stream2TapeCopy WHERE child = tapeCopyIdVar;
+
+  -- Delete the corresponding tape-gateway sub-request
+  DELETE FROM TapeGatewaySubRequest WHERE tapeCopy = tapeCopyIdVar
+    RETURNING id INTO tapeGatewaySubRequestIdVar;
+  IF SQL%ROWCOUNT != 0 THEN
+    DELETE FROM id2Type WHERE id = tapeGatewaySubRequestIdVar;
+  END IF;
+END;
+/
