4380b9e1cbb87392ad44778f04a0e99e3308bae5
Krzysztof Blaszczyk <kblaszcz@cern.ch>
2010-07-28 14:42:42 +0000

    Changes since 21.07.2010
    code changes:
    -adding a flexible way to associate a "Dimension" of a dataset to a graphical property. 
    -adding some useful template filters like add, sub, mul, div and a function for escaping
    -fixing a bug that resized the text depending on the proportions of the generated image
    -introducing parent methods to define how to find a parent of a given dataset
    
    interface changes:
    -introducing CSS tooltips and removing usage of imagemap
    -introducing a sequence of links below the image to be able to go back more than only one level at time
    -changing image size to 800x600px
    -removing the borders from treemap rectangles by setting inbordersize to 0
    -redesigning arrow.png
    -changing global html colors
    
    other changes:
    -removed some garbage

--- monitoring/treemap/sites/settings.py
+++ monitoring/treemap/sites/settings.py
@@ -76,7 +76,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/kblaszcz/FasticView/templates'
+    '/home/kblaszcz/workspace/FasticView/templates' 
 )
 
 INSTALLED_APPS = (
