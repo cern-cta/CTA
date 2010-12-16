70a4334f57bbdd142cc375d4e7d7d3e0bb556c5c
Giuseppe Lo Presti <itglp@cern.ch>
2010-12-16 13:08:29 +0000

    Modified upgrade script to support 2.1.10-0-1 hotfix release

--- upgrades/cns_2.1.10-0_to_2.1.10-0-1.sql
+++ upgrades/cns_2.1.10-0_to_2.1.10-0-1.sql
@@ -1,5 +1,5 @@
 /******************************************************************************
- *                 cns_2.1.10-0_to_2.1.10-1.sql
+ *                 cns_2.1.10-0_to_2.1.10-0-1.sql
  *
  * This file is part of the Castor project.
  * See http://castor.web.cern.ch/castor
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * This script upgrades a CASTOR v2.1.10-0 CNS database to v2.1.10-1
+ * This hotfix script upgrades a CASTOR v2.1.10-0 CNS database to v2.1.10-0-1
  *
  * @author Castor Dev team, castor-dev@cern.ch
  *****************************************************************************/
@@ -28,7 +28,7 @@ BEGIN
   UPDATE UpgradeLog
      SET failureCount = failureCount + 1
    WHERE schemaVersion = '2_1_9_3'
-     AND release = '2_1_10_1'
+     AND release = '2_1_10_0_1'
      AND state != 'COMPLETE';
   COMMIT;
 END;
@@ -40,7 +40,7 @@ DECLARE
 BEGIN
   SELECT release INTO unused FROM CastorVersion
    WHERE schemaName = 'CNS'
-     AND release LIKE '2_1_10_0%';
+     AND release = '2_1_10_0';
 EXCEPTION WHEN NO_DATA_FOUND THEN
   -- Error, we can't apply this script
   raise_application_error(-20000, 'PL/SQL release mismatch. Please run previous upgrade scripts for the CNS before this one.');
@@ -48,26 +48,15 @@ END;
 /
 
 INSERT INTO UpgradeLog (schemaVersion, release, type)
-VALUES ('2_1_9_3', '2_1_10_1', 'TRANSPARENT');
+VALUES ('2_1_9_3', '2_1_10_0_1', 'TRANSPARENT');
 COMMIT;
 
 /* Schema changes go here */
 /**************************/
 
 -- Create index for bug #71565
--- Because of the 2.1.10-0-1 hotfix script, we test whether to do it or not
-DECLARE
-  unused VARCHAR2(30);
-BEGIN
-  SELECT index_name INTO unused
-    FROM user_indexes
-   WHERE index_name = 'I_SEG_METADATA_TAPESUM';
-EXCEPTION WHEN NO_DATA_FOUND THEN
-  EXECUTE IMMEDIATE 'CREATE INDEX I_seg_metadata_tapesum
-                     ON Cns_seg_metadata (vid, s_fileid, segsize, compression)';
-  EXECUTE IMMEDIATE 'DROP INDEX I_seg_metadata_vid_fid_segsize';
-END;
-/
+CREATE INDEX I_seg_metadata_tapesum ON Cns_seg_metadata (vid, s_fileid, segsize, compression);
+DROP INDEX I_seg_metadata_vid_fid_segsize;
 
 /* Recompile all invalid procedures, triggers and functions */
 /************************************************************/
@@ -91,5 +80,5 @@ END;
 /* Flag the schema upgrade as COMPLETE */
 /***************************************/
 UPDATE UpgradeLog SET endDate = sysdate, state = 'COMPLETE'
- WHERE release = '2_1_10_1';
+ WHERE release = '2_1_10_0_1';
 COMMIT;
