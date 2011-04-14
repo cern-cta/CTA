c7b0edb7fa6fffe278ddd48622c81d824d7e214c
Dennis Waldron <waldron@cern.ch>
2011-04-14 14:39:09 +0000

    Moved to MONITORING subproject

--- monitoring/treemap/app/errors/NoDataAvailableError.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on Jul 2, 2010
-self explanatory...
-@author: kblaszcz
-'''
-
-class NoDataAvailableError( Exception ):
-    def __init__( self, msg ):
-        Exception.__init__( self, msg )
