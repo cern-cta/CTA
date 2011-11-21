8002e86658f0055904265e5524ec290c3e680495
Sebastien Ponce <sponcec3@cern.ch>
2011-11-21 16:08:54 +0000

    Fixed previous commited. I had not saved latest version of the file...

--- castor/db/oracleTapeGateway.sql
+++ castor/db/oracleTapeGateway.sql
@@ -537,7 +537,7 @@ PROCEDURE tg_defaultMigrSelPolicy(inMountId IN INTEGER,
     SELECT /*+ FIRST_ROWS_1 
                INDEX(CastorFile  PK_CastorFile_Id)
                INDEX(DiskCopy I_DiskCopy_CastorFile)
-               INDEX(TapePool I_MigrationJob_TPStatusCFId)
+               INDEX(MigrationJob I_MigrationJob_TPStatusCFId)
                LEADING(MigrationMount MigrationJob CastorFile DiskCopy FileSystem DiskServer) */
            DiskServer.name, FileSystem.mountPoint, DiskCopy.path, DiskCopy.id, CastorFile.lastKnownFilename,
            CastorFile.fileId, CastorFile.nsHost, CastorFile.fileSize, MigrationJob.id, CastorFile.lastUpdateTime
