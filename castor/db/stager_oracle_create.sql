ab562265e1bd124cfa38b2f0c29f4954ebf16f5b
Steven Murray <murrayc3@cern.ch>
2012-02-13 10:06:51 +0000

    Preparation for CASTOR inetrnal release v2.1.12-2

--- castor/db/stager_oracle_create.sql
+++ castor/db/stager_oracle_create.sql
@@ -446,7 +446,7 @@ ALTER TABLE UpgradeLog
   CHECK (type IN ('TRANSPARENT', 'NON TRANSPARENT'));
 
 /* SQL statement to populate the intial release value */
-INSERT INTO UpgradeLog (schemaVersion, release) VALUES ('-', '2_1_12_1');
+INSERT INTO UpgradeLog (schemaVersion, release) VALUES ('-', '2_1_12_2');
 
 /* SQL statement to create the CastorVersion view */
 CREATE OR REPLACE VIEW CastorVersion
