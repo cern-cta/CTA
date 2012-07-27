95c421f2937f804f2561b9133077b29376ec964c
Steven Murray <murrayc3@cern.ch>
2012-07-27 09:49:26 +0000

    bug #96375: reclaim command parses the CNS HOST entry of castor.conf incorrectly
    Merged implementation from development branch into the trunk.

--- hsmtools/reclaim.c
+++ hsmtools/reclaim.c
@@ -5,15 +5,18 @@
 
 /*      reclaim - reset information concerning a volume */
 #include <errno.h>
-#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include "Cns_api.h"
-#include "serrno.h"
-#include "vmgr_api.h"
-extern  char  *getconfent();
+#include <unistd.h>
+
+#include "h/Cns_api.h"
+#include "h/Cns.h"
+#include "h/getconfent.h"
+#include "h/serrno.h"
+#include "h/vmgr_api.h"
+
 extern  char  *optarg;
 extern  int  optind;
 
@@ -21,14 +24,12 @@ int main(int argc,
          char **argv)
 {
   int c = 0;
-  char *Cns_hosts = NULL;
   FILE *df = NULL;
   struct Cns_direntape *dtp = NULL;
   int errflg = 0;
   int flags = 0;
   char *host = NULL;
   Cns_list list;
-  char *p = NULL;
   char p_stat[9];
   char path[CA_MAXPATHLEN+1];
   struct vmgr_tape_info_byte_u64 tape_info;
@@ -58,8 +59,21 @@ int main(int argc,
     exit (USERR);
   }
 
-  /* check if the volume is FULL */
+  /* If the CASTOR name-server host was not specified on the command line */
+  /* then try to get it from castor.conf                                  */
+  if(NULL == host) {
+    host = getconfent (CNS_SCE, "HOST", 0);
+  }
 
+  /* Abort with an appropriate error message if the user did not provide the */
+  /* name-server host name on either the command line or in castor.conf      */
+  if (!host) {
+    fprintf (stderr, "Error: CASTOR name-server host name must either be"
+      " provided on the command line or in castor.conf\n");
+    exit (USERR);
+  }
+
+  /* check if the volume is FULL */
   if (vmgr_querytape_byte_u64 (vid, 0, &tape_info, NULL) < 0) {
     fprintf (stderr, "reclaim %s: %s\n", vid,
         (serrno == ENOENT) ? "No such volume" : sstrerror(serrno));
@@ -77,65 +91,35 @@ int main(int argc,
   }
 
   /* check if the volume still contains active files */
-
   if ((df = tmpfile ()) == NULL) {
     fprintf (stderr, "Cannot create temporary file\n");
     exit (USERR);
   }
-
-  if (! host && (Cns_hosts = getconfent ("CNS", "HOST", 1))) {
-    Cns_hosts = strdup (Cns_hosts);
-    host = Cns_hosts;
-    if ((p = strpbrk (Cns_hosts, " \t\n"))) {
-      *p = '\0';
-      Cns_hosts = p + 1;
-    } else
-      Cns_hosts = NULL;
-  }
-
-  /* Abort with an appropriate error message if the user did not provide the */
-  /* name-server host name on either the command line or in castor.conf      */
-  if (!host && !Cns_hosts) {
-    fprintf (stderr, "Error: Name-server host name must either be provided"
-      " on the command line or in castor.conf\n");
-    exit (USERR);
-  }
-
-  while (1) {
-    flags = CNS_LIST_BEGIN;
-    while ((dtp = Cns_listtape (host, vid, flags, &list, 0)) != NULL) {
-      flags = CNS_LIST_CONTINUE;
-      if (dtp->s_status == 'D') {
-        if (Cns_getpath (host, dtp->parent_fileid, path) < 0) {
-          fprintf (stderr, "%s\n", sstrerror(serrno));
-          exit (USERR);
-        }
-        fprintf (df, "%s\n", path);
-      } else {
-        fprintf (stderr,
-            "Volume %s still contains active files\n", vid);
+  flags = CNS_LIST_BEGIN;
+  while ((dtp = Cns_listtape (host, vid, flags, &list, 0)) != NULL) {
+    flags = CNS_LIST_CONTINUE;
+    if (dtp->s_status == 'D') {
+      if (Cns_getpath (host, dtp->parent_fileid, path) < 0) {
+        fprintf (stderr, "%s\n", sstrerror(serrno));
         exit (USERR);
       }
+      fprintf (df, "%s\n", path);
+    } else {
+      fprintf (stderr,
+          "Volume %s still contains active files\n", vid);
+      exit (USERR);
     }
-    if (serrno > 0 && serrno != ENOENT) {
-      fprintf (stderr, "reclaim %s: %s\n", vid,
-          sstrerror(serrno));
-      exit (SYERR);
-    }
-    if (serrno == 0) {
-      (void) Cns_listtape (host, vid, CNS_LIST_END, &list, 0);
-    }
-    if (! Cns_hosts) break;
-    host = Cns_hosts;
-    if ((p = strpbrk (Cns_hosts, " \t\n"))) {
-      *p = '\0';
-      Cns_hosts = p + 1;
-    } else
-      Cns_hosts = NULL;
-  } // while (1)
+  }
+  if (serrno > 0 && serrno != ENOENT) {
+    fprintf (stderr, "reclaim %s: %s\n", vid,
+        sstrerror(serrno));
+    exit (SYERR);
+  }
+  if (serrno == 0) {
+    (void) Cns_listtape (host, vid, CNS_LIST_END, &list, 0);
+  }
 
   /* remove logically deleted files */
-
   rewind (df);
   while (fgets (path, CA_MAXPATHLEN, df)) {
     *(path + strlen (path)) = '\0';
@@ -148,7 +132,6 @@ int main(int argc,
   fclose (df);
 
   /* reset nbfiles, estimated_free_space and status for this volume */
-
   if (vmgr_reclaim (vid)) {
     fprintf (stderr, "reclaim %s: %s\n", vid,
         (serrno == ENOENT) ? "No such volume" : sstrerror(serrno));
