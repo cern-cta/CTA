1b9ec4d066e42e5dbdffacfd4f2dd008ddf9a540
Steven Murray <murrayc3@cern.ch>
2012-08-14 06:31:23 +0000

    The ALB tape format has been superseded by the use of immediate tape marks.  I am therefore removing this directory from svn.

--- castor/tape/format/moveToDisk15.bash
+++
@@ -1,4 +0,0 @@
-
-scp -r ./*.o ./albwriter ./albreader ./run.bash root@lxc2disk15:/var/ReadWriteFile/castor/tape/format
-
-scp -r ./*.so root@lxc2disk15:/var/ReadWriteFile/castor/tape/format/required_so
--- castor/tape/format/run.bash
+++
@@ -1,6 +0,0 @@
-
-
-valgrind --tool=memcheck --log-file-exactly=valgrind-albwriter.output ./albwriter
-
-
-valgrind --tool=memcheck --log-file-exactly=valgrind-albreader.output ./albreader
