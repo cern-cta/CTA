2524dfcf4e5b7623d78e63c6d3cd55765acacfa3
Benjamin Fiorini <bfiorini@cern.ch>
2012-11-12 10:43:53 +0000

    Support #131825 : publish migrations/recalls backlog statistics in stagerd log file

--- castor/db/oracleMonitoring.sql
+++ castor/db/oracleMonitoring.sql
@@ -7,29 +7,10 @@
 
 /* CastorMon Package Specification */
 CREATE OR REPLACE PACKAGE CastorMon AS
-
-  /**
-   * This procedure generates statistics on the status of all diskcopies. The
-   * results are grouped by DiskServer, MountPoint and DiskCopy status.
-   *
-   * @param interval The frequency at which the data is refreshed.
-   */
-  PROCEDURE diskCopyStats(interval IN NUMBER);
-
-  /**
-   * This procedure generates statistics on files which are waiting to be
-   * migrated to tape.
-   * @param interval The frequency at which the data is refreshed.
-   */
-  PROCEDURE waitTapeMigrationStats(interval IN NUMBER);
-
-  /**
-   * This procedure generates statistics on files which are waiting to be
-   * recalled from tape.
-   * @param interval The frequency at which the data is refreshed.
-   */
-  PROCEDURE waitTapeRecallStats(interval IN NUMBER);
-
+  -- Backlog statistics for migrations, grouped by TapePool
+  PROCEDURE waitTapeMigrationStats;
+  -- Backlog statistics for recalls, grouped by SvcClass
+  PROCEDURE waitTapeRecallStats;
 END CastorMon;
 /
 
@@ -38,199 +19,55 @@ END CastorMon;
 CREATE OR REPLACE PACKAGE BODY CastorMon AS
 
   /**
-   * PL/SQL method implementing diskCopyStats
-   * See the castorMon package specification for documentation.
-   */
-  PROCEDURE diskCopyStats(interval IN NUMBER) AS
-  BEGIN
-    -- Truncate the MonDiskCopyStats table
-    EXECUTE IMMEDIATE 'DELETE FROM MonDiskCopyStats';
-    -- Populate the MonDiskCopyStats table
-    INSERT INTO MonDiskCopyStats
-      (timestamp, interval, diskServer, mountPoint, dsStatus, fsStatus,
-       available, status, totalFileSize, nbFiles)
-      -- Gather data
-      SELECT sysdate timestamp, interval, a.name diskServer, a.mountPoint,
-             a.dsStatus, a.fsStatus, a.available, a.statusName status,
-             nvl(b.totalFileSize, 0) totalFileSize, nvl(b.nbFiles, 0) nbFiles
-        FROM (
-          -- Produce a matrix of all possible diskservers, filesystems and
-          -- diskcopy states.
-          SELECT DiskServer.name, FileSystem.mountPoint, FileSystem.id,
-                 (SELECT statusName FROM ObjStatus
-                   WHERE object = 'DiskServer'
-                     AND field = 'status'
-                     AND statusCode = DiskServer.status) dsStatus,
-                 (SELECT statusName FROM ObjStatus
-                   WHERE object = 'FileSystem'
-                     AND field = 'status'
-                     AND statusCode = FileSystem.status) fsStatus,
-                 decode(DiskServer.status, 2, 'N',
-                   decode(FileSystem.status, 2, 'N', 'Y')) available,
-                 ObjStatus.statusName
-            FROM DiskServer, FileSystem, ObjStatus
-           WHERE FileSystem.diskServer = DiskServer.id
-             AND ObjStatus.object(+) = 'DiskCopy'
-             AND ObjStatus.field = 'status'
-             AND ObjStatus.statusCode IN (0, 4, 5, 6, 7, 9, 10, 11)) a
-        -- Attach the aggregation information for all filesystems to the results
-        -- extracted above.
-        LEFT JOIN (
-          SELECT DiskCopy.fileSystem, ObjStatus.statusName,
-                 sum(DiskCopy.diskCopySize) totalFileSize, count(*) nbFiles
-            FROM DiskCopy, ObjStatus
-           WHERE DiskCopy.status = ObjStatus.statusCode
-             AND ObjStatus.object = 'DiskCopy'
-             AND ObjStatus.field = 'status'
-             AND DiskCopy.status IN (0, 4, 5, 6, 7, 9, 10, 11)
-           GROUP BY DiskCopy.fileSystem, ObjStatus.statusName) b
-          ON (a.id = b.fileSystem AND a.statusName = b.statusName)
-       ORDER BY a.name, a.mountPoint, a.statusName;
-  END diskCopyStats;
-
-
-  /**
    * PL/SQL method implementing waitTapeMigrationStats
    * See the castorMon package specification for documentation.
    */
-  PROCEDURE waitTapeMigrationStats(interval IN NUMBER) AS
+  PROCEDURE waitTapeMigrationStats AS
+    CURSOR rec IS
+      SELECT tapePool tapePool,
+             nvl(sum(fileSize), 0) totalFileSize, 
+             count(fileSize) nbFiles
+      FROM (
+        SELECT
+               TapePool.name tapePool,
+               MigrationJob.filesize fileSize
+        FROM MigrationJob, TapePool
+        WHERE TapePool.id = MigrationJob.tapePool(+) -- Left outer join to have zeroes when there is no migrations
+      )
+      GROUP BY tapePool;
   BEGIN
-    -- Truncate the MonWaitTapeMigrationStats table
-    EXECUTE IMMEDIATE 'DELETE FROM MonWaitTapeMigrationStats';
-    -- Populate the MonWaitTapeMigrationStats table
-    INSERT INTO MonWaitTapeMigrationStats
-      (timestamp, interval, tapePool, status, minWaitTime, maxWaitTime,
-       avgWaitTime, minFileSize, maxFileSize, avgFileSize, bin_LT_1,
-       bin_1_To_6, bin_6_To_12, bin_12_To_24, bin_24_To_48, bin_GT_48,
-       totalFileSize, nbFiles)
-      -- Gather data
-      SELECT sysdate timestamp, interval, b.tapePool, nvl(b.status, '-') status,
-             -- File age statistics
-             round(nvl(min(a.waitTime), 0), 0) minWaitTime,
-             round(nvl(max(a.waitTime), 0), 0) maxWaitTime,
-             round(nvl(avg(a.waitTime), 0), 0) avgWaitTime,
-             -- File size statistics
-             round(nvl(min(a.diskCopySize), 0), 0) minFileSize,
-             round(nvl(max(a.diskCopySize), 0), 0) maxFileSize,
-             round(nvl(avg(a.diskCopySize), 0), 0) avgFileSize,
-             -- Wait time stats
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 1     AND 3600
-                      THEN 1 ELSE 0 END) BIN_LT_1,
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 3600  AND 21600
-                      THEN 1 ELSE 0 END) BIN_1_TO_6,
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 21600 AND 43200
-                      THEN 1 ELSE 0 END) BIN_6_TO_12,
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 43200 AND 86400
-                      THEN 1 ELSE 0 END) BIN_12_TO_24,
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 86400 AND 172800
-                      THEN 1 ELSE 0 END) BIN_24_TO_48,
-             sum(CASE WHEN nvl(a.waitTime, 0) > 172800
-                      THEN 1 ELSE 0 END) BIN_GT_48,
-             -- Summary values
-             nvl(sum(a.diskCopySize), 0) totalFileSize, nvl(sum(a.found), 0) nbFiles
-        FROM (
-          -- Determine the service class of all tapecopies and their associated
-          -- status (in a summarized form)
-          -- In castor 2.1.11:
-          -- Output is 'Pending' for 'PENDING' or 'TOBEMIGRATED',
-          --           'Selected' for 'WAITINSTREAMS' or 'SELECTED'
-          --           'Failed' for 'FAILED'
-          -- Other states are ignored ('TOBERECALLED', 'STAGED', 'WAITPOLICY', 'REC_RETRY', 'MIG_RETRY')
-          -- New version (2.1.12) will be:
-          --          'Pending' for 'PENDING'
-          --          'Selected' for 'SELECTED'
-          --          'MIGRATED' and 'RETRY' will be ignored.
-          SELECT tapePool, status, waitTime, diskCopySize, found FROM (
-            SELECT /*+ USE_NL(MigrationJob DiskCopy CastorFile) */
-                   TapePool.name tapePool,
-                   decode(MigrationJob.status, tconst.MIGRATIONJOB_PENDING, 'PENDING',
-                                               tconst.MIGRATIONJOB_SELECTED, 'SELECTED','UNKNOWN')
-                   status,
-                   (getTime() - DiskCopy.creationTime) waitTime,
-                   DiskCopy.diskCopySize, 1 found, RANK() OVER (PARTITION BY
-                   DiskCopy.castorFile ORDER BY DiskCopy.id ASC) rank
-              FROM DiskCopy, CastorFile, MigrationJob, TapePool
-             WHERE DiskCopy.castorFile = CastorFile.id
-               AND MigrationJob.castorFile = CastorFile.id
-               AND MigrationJob.tapePool = TapePool.id
-               AND decode(DiskCopy.status, 10, DiskCopy.status, NULL) = 10  -- CANBEMIGR
-               AND MigrationJob.status IN (tconst.MIGRATIONJOB_PENDING, 
-                                           tconst.MIGRATIONJOB_SELECTED))
-           WHERE rank = 1
-        ) a
-        -- Attach a list of all service classes and possible states (PENDING,
-        -- SELECTED) to the results above.
-        RIGHT JOIN (
-          SELECT TapePool.name tapePool, a.status
-            FROM TapePool,
-             (SELECT 'PENDING'  status FROM Dual UNION ALL
-              SELECT 'SELECTED' status FROM Dual) a) b
-           ON (a.tapePool = b.tapePool AND a.status = b.status)
-       GROUP BY GROUPING SETS (b.tapePool, b.status), (b.tapePool)
-       ORDER BY b.tapePool, b.status;
+    FOR r in rec LOOP
+      logToDLF(NULL, dlf.LVL_SYSTEM, 'waitTapeMigrationStats', 0, '', 'stagerd', 
+            'TapePool="' || r.tapePool ||
+            '" totalFileSize="' || r.totalFileSize || 
+            '" nbFiles="' || r.nbFiles || '"');
+    END LOOP;
   END waitTapeMigrationStats;
-
 
   /**
    * PL/SQL method implementing waitTapeRecallStats
    * See the castorMon package specification for documentation.
    */
-  PROCEDURE waitTapeRecallStats(interval IN NUMBER) AS
+  PROCEDURE waitTapeRecallStats AS
+    CURSOR rec IS
+      SELECT svcClass svcClass,
+             nvl(sum(fileSize), 0) totalFileSize,
+             count(fileSize) nbFiles
+      FROM (
+        SELECT
+               SvcClass.name svcClass,
+               RecallJob.filesize fileSize
+        FROM RecallJob, SvcClass
+        WHERE SvcClass.id = RecallJob.SvcClass(+) -- Left outer join to have zeroes when there is no recall
+      )
+      GROUP BY svcClass;
   BEGIN
-    -- Truncate the MonWaitTapeRecallStats table
-    EXECUTE IMMEDIATE 'DELETE FROM MonWaitTapeRecallStats';
-    -- Populate the MonWaitTapeRecallStats table
-    INSERT INTO MonWaitTapeRecallStats
-      (timestamp, interval, svcClass, minWaitTime, maxWaitTime, avgWaitTime,
-       minFileSize, maxFileSize, avgFileSize, bin_LT_1, bin_1_To_6, bin_6_To_12,
-       bin_12_To_24, bin_24_To_48, bin_GT_48, totalFileSize, nbFiles)
-      -- Gather data
-      SELECT sysdate timestamp, interval, SvcClass.name svcClass,
-             -- File age statistics
-             round(nvl(min(a.waitTime), 0), 0) minWaitTime,
-             round(nvl(max(a.waitTime), 0), 0) maxWaitTime,
-             round(nvl(avg(a.waitTime), 0), 0) avgWaitTime,
-             -- File size statistics
-             round(nvl(min(a.fileSize), 0), 0) minFileSize,
-             round(nvl(max(a.fileSize), 0), 0) maxFileSize,
-             round(nvl(avg(a.fileSize), 0), 0) avgFileSize,
-             -- Wait time stats
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 1     AND 3600
-                      THEN 1 ELSE 0 END) BIN_LT_1,
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 3600  AND 21600
-                      THEN 1 ELSE 0 END) BIN_1_TO_6,
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 21600 AND 43200
-                      THEN 1 ELSE 0 END) BIN_6_TO_12,
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 43200 AND 86400
-                      THEN 1 ELSE 0 END) BIN_12_TO_24,
-             sum(CASE WHEN nvl(a.waitTime, 0) BETWEEN 86400 AND 172800
-                      THEN 1 ELSE 0 END) BIN_24_TO_48,
-             sum(CASE WHEN nvl(a.waitTime, 0) > 172800
-                      THEN 1 ELSE 0 END) BIN_GT_48,
-             -- Summary values
-             nvl(sum(a.fileSize), 0) totalFileSize, nvl(sum(a.found), 0) nbFiles
-        FROM (
-          -- Determine the list of ongoing a. We need to join with the
-          -- SubRequest and Request tables here to work out the service class
-          -- as the DiskCopy filesystem pointer is 0 until the file is
-          -- successfully recalled.
-          SELECT Request.svcClassName svcClass, CastorFile.fileSize,
-                 (getTime() - Subrequest.creationTime) waitTime, 1 found
-            FROM SubRequest, CastorFile,
-              (SELECT id, svcClassName FROM StageGetRequest UNION ALL
-               SELECT id, svcClassName FROM StagePrepareToGetRequest UNION ALL
-               SELECT id, svcClassName FROM StageUpdateRequest UNION ALL
-               SELECT id, svcClassName FROM StageRepackRequest) Request
-           WHERE SubRequest.castorFile = CastorFile.id
-             AND SubRequest.status = 4  -- SUBREQUEST_WAITTAPERECALL
-             AND SubRequest.parent = 0
-             AND SubRequest.request = Request.id
-        ) a
-        -- Attach a list of all service classes
-        RIGHT JOIN SvcClass
-           ON SvcClass.name = a.svcClass
-        GROUP BY SvcClass.name
-        ORDER BY SvcClass.name;
+    FOR r in rec LOOP
+      logToDLF(NULL, dlf.LVL_SYSTEM, 'waitTapeRecallStats', 0, '', 'stagerd', 
+            'SvcClass="' || r.svcClass ||
+            '" totalFileSize="' || r.totalFileSize || 
+            '" nbFiles="' || r.nbFiles || '"');
+    END LOOP;
   END waitTapeRecallStats;
 
 END CastorMon;
@@ -248,20 +85,14 @@ BEGIN
 
   -- Recreate monitoring jobs
   DBMS_SCHEDULER.CREATE_JOB(
-      JOB_NAME        => 'monitoringJob_60mins',
+      JOB_NAME        => 'monitoringJob_1min',
       JOB_TYPE        => 'PLSQL_BLOCK',
-      JOB_ACTION      => 'DECLARE
-                            interval NUMBER := 3600;
-                          BEGIN
-                            castorMon.diskCopyStats(interval);
-                            castorMon.waitTapeMigrationStats(interval);
-                            castorMon.waitTapeRecallStats(interval);
-                          END;',
+      JOB_ACTION      => 'BEGIN startDbJob(''BEGIN CastorMon.waitTapeMigrationStats(); CastorMon.waitTapeRecallStats(); END;'', ''stagerd''); END;',
       JOB_CLASS       => 'CASTOR_MON_JOB_CLASS',
-      START_DATE      => SYSDATE + 60/1440,
-      REPEAT_INTERVAL => 'FREQ=MINUTELY; INTERVAL=60',
+      START_DATE      => SYSDATE + 1/3600,
+      REPEAT_INTERVAL => 'FREQ=MINUTELY; INTERVAL=1',
       ENABLED         => TRUE,
-      COMMENTS        => 'Generation of monitoring information');
+      COMMENTS        => 'Generation of monitoring information about migrations and recalls backlog');
 END;
 /
 
