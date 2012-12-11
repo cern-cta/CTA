3e25e293cb1cc9dcc90f65e8b5198cff5c68dd66
Giuseppe Lo Presti <itglp@cern.ch>
2012-12-11 13:07:37 +0000

    Dropped StageRepackRequest from XMI and moved its definition to oracleCommon.schema.sql

--- castor/db/oracleSchema.sql
+++ castor/db/oracleSchema.sql
@@ -182,19 +182,6 @@ END;
 /* SQL statements for type SetFileGCWeight */
 CREATE TABLE SetFileGCWeight (flags INTEGER, userName VARCHAR2(2048), euid NUMBER, egid NUMBER, mask NUMBER, pid NUMBER, machine VARCHAR2(2048), svcClassName VARCHAR2(2048), userTag VARCHAR2(2048), reqId VARCHAR2(2048), creationTime INTEGER, lastModificationTime INTEGER, weight NUMBER, id INTEGER CONSTRAINT PK_SetFileGCWeight_Id PRIMARY KEY, svcClass INTEGER, client INTEGER) INITRANS 50 PCTFREE 50 ENABLE ROW MOVEMENT;
 
-/* SQL statements for type StageRepackRequest */
-CREATE TABLE StageRepackRequest (flags INTEGER, userName VARCHAR2(2048), euid NUMBER, egid NUMBER, mask NUMBER, pid NUMBER, machine VARCHAR2(2048), svcClassName VARCHAR2(2048), userTag VARCHAR2(2048), reqId VARCHAR2(2048), creationTime INTEGER, lastModificationTime INTEGER, repackVid VARCHAR2(2048), id INTEGER CONSTRAINT PK_StageRepackRequest_Id PRIMARY KEY, svcClass INTEGER, client INTEGER, status INTEGER) INITRANS 50 PCTFREE 50 ENABLE ROW MOVEMENT;
-
-BEGIN
-  setObjStatusName('StageRepackRequest', 'status', 0, 'REPACK_STARTING');
-  setObjStatusName('StageRepackRequest', 'status', 1, 'REPACK_ONGOING');
-  setObjStatusName('StageRepackRequest', 'status', 2, 'REPACK_FINISHED');
-  setObjStatusName('StageRepackRequest', 'status', 3, 'REPACK_FAILED');
-  setObjStatusName('StageRepackRequest', 'status', 4, 'REPACK_ABORTING');
-  setObjStatusName('StageRepackRequest', 'status', 5, 'REPACK_ABORTED');
-END;
-/
-
 /* SQL statements for type StageDiskCopyReplicaRequest */
 CREATE TABLE StageDiskCopyReplicaRequest (flags INTEGER, userName VARCHAR2(2048), euid NUMBER, egid NUMBER, mask NUMBER, pid NUMBER, machine VARCHAR2(2048), svcClassName VARCHAR2(2048), userTag VARCHAR2(2048), reqId VARCHAR2(2048), creationTime INTEGER, lastModificationTime INTEGER, id INTEGER CONSTRAINT PK_StageDiskCopyReplicaRequ_Id PRIMARY KEY, svcClass INTEGER, client INTEGER, sourceSvcClass INTEGER, destDiskCopy INTEGER, sourceDiskCopy INTEGER) INITRANS 50 PCTFREE 50 ENABLE ROW MOVEMENT;
 
