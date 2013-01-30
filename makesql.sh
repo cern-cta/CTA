a3a26196b62a237b72d2b61d8290307f84c9af39
Giuseppe Lo Presti <itglp@cern.ch>
2013-01-30 17:37:59 +0000

    Dropped generation of DLF and MON schemas

--- makesql.sh
+++ makesql.sh
@@ -7,8 +7,6 @@ tag=`echo $ver | sed 's/\./_/g' | sed 's/-/_/g'`
 # generate creation scripts for the db-based CASTOR components
 # if $1 != NULL, it is interpreted as destination directory where to install the generated script
 tools/makeSqlScripts.sh cns $tag ns $1
-tools/makeSqlScripts.sh dlf $tag dlf $1
-tools/makeSqlScripts.sh mon $tag monitoring $1
 tools/makeSqlScripts.sh stager $tag castor/db $1
 tools/makeSqlScripts.sh cupv $tag upv $1
 tools/makeSqlScripts.sh vdqm $tag castor/vdqm $1
