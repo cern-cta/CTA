927eeb439eda8884d10cd88780b7adf0270dd6d4
Elvin Alin Sindrilaru <esindril@cern.ch>
2013-02-28 10:45:26 +0000

    XROOTD: Do a pull before a checkout to avoid potential git checkout error

--- xrootd/get-xrootd-git-stable.sh
+++ xrootd/get-xrootd-git-stable.sh
@@ -10,10 +10,11 @@ XROOTD_VERSION=`rpm -qa | grep xrootd-server- | awk -F"-" '{ input=$3; if (index
 if [ -d xrootd/xrootd_hdr ]; then
     echo "Clone already present, just have to checkout the correct version."
 else
-    git clone http://xrootd.cern.ch/repos/xrootd.git xrootd/xrootd_hdr;   
+    git clone http://xrootd.cern.ch/repos/xrootd.git xrootd/xrootd_hdr
 fi
 
 cd xrootd/xrootd_hdr
+git pull
 echo "Try to checkout the correct version of header files i.e. v$XROOTD_VERSION"
 git checkout -b local-v$XROOTD_VERSION v$XROOTD_VERSION
 
