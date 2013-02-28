aed79865868e07f8c7d8c10e1e95f465fa03dd31
Elvin Alin Sindrilaru <esindril@cern.ch>
2013-02-28 11:07:31 +0000

    XROOTD: Extract the XROOTD_VERSION using the version of xrdcp

--- xrootd/get-xrootd-git-stable.sh
+++ xrootd/get-xrootd-git-stable.sh
@@ -5,7 +5,7 @@
 # corresponding to that version used for compiling the castor-xrootd-plugin
 #-------------------------------------------------------------------------------
 
-XROOTD_VERSION=`rpm -qa | grep xrootd-server- | awk -F"-" '{ input=$3; if (index(input, ".") != NULL) {print input} }'`
+XROOTD_VERSION=`xrdcp --version`
 
 if [ -d xrootd/xrootd_hdr ]; then
     echo "Clone already present, just have to checkout the correct version."
@@ -16,5 +16,5 @@ fi
 cd xrootd/xrootd_hdr
 git pull
 echo "Try to checkout the correct version of header files i.e. v$XROOTD_VERSION"
-git checkout -b local-v$XROOTD_VERSION v$XROOTD_VERSION
+git checkout -b local-$XROOTD_VERSION $XROOTD_VERSION
 
