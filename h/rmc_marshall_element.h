bb4f49d7d82882c13a0d3fad238426e3960bdd37
Steven Murray <murrayc3@cern.ch>
2013-10-01 07:37:03 +0000

    Moved rmc_marshall_element() into its own object file

---
+++ h/rmc_marshall_element.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2001 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#ifndef _RMC_MARSHALL_ELEMENT_H
+#define _RMC_MARSHALL_ELEMENT_H 1
+
+#include "h/smc_struct.h"
+
+int rmc_marshall_element (char **const sbpp, const struct smc_element_info *const element_info);
+
+#endif
