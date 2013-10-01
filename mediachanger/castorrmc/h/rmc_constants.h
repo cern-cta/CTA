12b3f9ddafb26ce31df932aa0d9ceede4e7111d0
Steven Murray <murrayc3@cern.ch>
2013-10-01 07:36:55 +0000

    Added placeholder functions for the new rmcd protocol

--- mediachanger/castorrmc/h/rmc_constants.h
+++ mediachanger/castorrmc/h/rmc_constants.h
@@ -33,13 +33,15 @@
 
 			/* Request types */
 
-#define	RMC_GETGEOM	1	/* Get robot geometry */
-#define	RMC_FINDCART	2	/* Find cartridge(s) */
-#define RMC_READELEM	3	/* Read element status */
-#define	RMC_MOUNT	4	/* Mount request */
-#define	RMC_UNMOUNT	5	/* Unmount request */
-#define	RMC_EXPORT	6	/* Export tape request */
-#define	RMC_IMPORT	7	/* Import tape request */
+#define	RMC_GETGEOM        1 /* Get robot geometry */
+#define	RMC_FINDCART       2 /* Find cartridge(s) */
+#define RMC_READELEM       3 /* Read element status */
+#define	RMC_MOUNT          4 /* Mount request */
+#define	RMC_UNMOUNT        5 /* Unmount request */
+#define	RMC_EXPORT         6 /* Export tape request */
+#define	RMC_IMPORT         7 /* Import tape request */
+#define	RMC_GENERICMOUNT   8 /* Generic (SCSI or ACS) mount request */
+#define	RMC_GENERICUNMOUNT 9 /* Generic (SCSI or ACS) mount request */
 
 			/* SCSI media changer server reply types */
 
--- mediachanger/castorrmc/h/rmc_server_api.h
+++ mediachanger/castorrmc/h/rmc_server_api.h
@@ -6,12 +6,13 @@
 
                         /*  function prototypes */
 
-EXTERN_C int rmc_srv_export (char*, char*);
-EXTERN_C int rmc_srv_findcart (char*, char*);
-EXTERN_C int rmc_srv_getgeom (char*, char*);
-EXTERN_C int rmc_srv_import (char*, char*);
-EXTERN_C int rmc_srv_mount (char*, char*);
-EXTERN_C int rmc_srv_readelem (char*, char*);
-EXTERN_C int rmc_srv_unmount (char*, char*);
-
+int rmc_srv_export(const int rpfd, char *const req_data, const char *const clienthost);
+int rmc_srv_findcart(const int rpfd, char *const req_data, const char *const clienthost);
+int rmc_srv_getgeom(const int rpfd, char *const req_data, const char *const clienthost);
+int rmc_srv_import(const int rpfd, char *const req_data, const char *const clienthost);
+int rmc_srv_mount( const int rpfd, char *const req_data, const char *const clienthost);
+int rmc_srv_readelem(const int rpfd, char *const req_data, const char *const clienthost);
+int rmc_srv_unmount(const int rpfd, char *const req_data, const char *const clienthost);
+int rmc_srv_genericmount(const int rpfd, char *const req_data, const char *const clienthost);
+int rmc_srv_genericunmount(const int rpfd, char *const req_data, const char *const clienthost);
 #endif
