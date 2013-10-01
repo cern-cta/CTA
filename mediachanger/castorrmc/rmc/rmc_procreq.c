276e243c5fa88af5e110deebdf76843fdd26b42c
Steven Murray <murrayc3@cern.ch>
2013-10-01 07:37:01 +0000

    Moved rmc_marshall_element() into its own object file

--- mediachanger/castorrmc/rmc/rmc_procreq.c
+++ mediachanger/castorrmc/rmc/rmc_procreq.c
@@ -18,6 +18,7 @@
 #include "h/rmc_constants.h"
 #include "h/rmc_logit.h"
 #include "h/rmc_logreq.h"
+#include "h/rmc_marshall_element.h"
 #include "h/rmc_smcsubr.h"
 #include "h/rmc_smcsubr2.h"
 #include "h/rmc_sendrep.h"
@@ -28,24 +29,6 @@
 extern struct extended_robot_info extended_robot_info;
 extern char localhost[CA_MAXHOSTNAMELEN+1];
  
-static int rmc_marshall_element (
-	char **const sbpp,
-	const struct smc_element_info *const element_info)
-{
-	char *sbp = *sbpp;
-
-	marshall_WORD (sbp, element_info->element_address);
-	marshall_BYTE (sbp, element_info->element_type);
-	marshall_BYTE (sbp, element_info->state);
-	marshall_BYTE (sbp, element_info->asc);
-	marshall_BYTE (sbp, element_info->ascq);
-	marshall_BYTE (sbp, element_info->flags);
-	marshall_WORD (sbp, element_info->source_address);
-	marshall_STRING (sbp, element_info->name);
-	*sbpp = sbp;
-	return (0);
-}
-
 /*	rmc_srv_export - export/eject a cartridge from the robot */
 
 int rmc_srv_export(
