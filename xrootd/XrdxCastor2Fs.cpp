32774d7cf2b3a84e8af669f5869d8b115aed7438
Elvin Sindrilaru <esindril@cern.ch>
2013-10-28 15:17:05 +0100

    XROOTD: Fix deadlock in RoleMapping of the XRootD plugin

--- xrootd/XrdxCastor2Fs.cpp
+++ xrootd/XrdxCastor2Fs.cpp
@@ -581,10 +581,12 @@ XrdxCastor2Fs::RoleMap(const XrdSecEntity* client,
     {
       mappedClient.name = STRINGSTORE("root");
     }
-
-    MapMutex.UnLock();  // <--
+    
+    break;
   }
   while (0);
+
+  MapMutex.UnLock();  // <--
 
   XrdSecEntity* newentity = new XrdSecEntity();
 
