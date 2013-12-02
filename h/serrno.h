120c058cba36bb0c3725dbcc95eedc125ebb722e
Steven Murray <steven.murray@cern.ch>
2013-12-02 10:50:01 +0100

    Added the following CASTOR exception:
        castor::exception::InvalidNbArguments

--- h/serrno.h
+++ h/serrno.h
@@ -79,8 +79,9 @@
 #define SEMISSINGOPER   SEBASEOFF+45    /* Missing operand              */
 #define SEMISMATCH      SEBASEOFF+46    /* Mismatch                     */
 #define SEREQUESTFAILED SEBASEOFF+47    /* Request failed               */
+#define SEINVALIDNBARGS SEBASEOFF+48    /* Invalid number of arguments  */
 
-#define SEMAXERR        SEBASEOFF+47    /* Maximum error number         */
+#define SEMAXERR        SEBASEOFF+48    /* Maximum error number         */
 
 #define SERRNO  (serrno - SEBASEOFF)    /* User convenience             */
 /*
