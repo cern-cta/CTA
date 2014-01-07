7e161b276eb3b40686f1aa50758dec22eaa796cb
Sebastien Ponce <Sebastien.Ponce@cern.ch>
2014-01-07 11:31:10 +0100

    More complete fix for bug #103190: Constraint violation errors thrown by the GC

--- castor/db/oracleGC.sql
+++ castor/db/oracleGC.sql
@@ -480,9 +480,9 @@ BEGIN
         -- file from the nameserver. For safety, we thus keep it
         NULL;
       WHEN CONSTRAINT_VIOLATED THEN
-        IF sqlerrm LIKE '%constraint (CASTOR_STAGER.FK_DISK2DISKCOPYJOB_CASTORFILE) violated%' THEN
-          -- Ignore the deletion, probably some draining/rebalancing activity created a Disk2DiskCopyJob entity
-          -- while we were attempting to drop the CastorFile
+        IF sqlerrm LIKE '%constraint (CASTOR_STAGER.FK_%_CASTORFILE) violated%' THEN
+          -- Ignore the deletion, probably some draining/rebalancing/recall activity created
+	  -- a new Disk2DiskCopyJob/RecallJob entity while we were attempting to drop the CastorFile
           NULL;
         ELSE
           -- Any other constraint violation is an error
