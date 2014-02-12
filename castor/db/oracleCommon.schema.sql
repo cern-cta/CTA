8e95448cf95a2fd22b9611f1d5f415e1c5ad7ba0
Giuseppe Lo Presti <Giuseppe.LoPresti@cern.ch>
2014-02-12 10:37:14 +0100

    Fixes after performing DB diff to validate the upgrade script

--- castor/db/oracleCommon.schema.sql
+++ castor/db/oracleCommon.schema.sql
@@ -765,7 +765,7 @@ CREATE TABLE FileMigrationResultsHelper
 CREATE INDEX I_FileMigResultsHelper_ReqId ON FileMigrationResultsHelper(ReqId);
 
 /* SQL statements for type DiskServer */
-CREATE TABLE DiskServer (name VARCHAR2(2048), lastHeartbeatTime NUMBER, id INTEGER CONSTRAINT PK_DiskServer_Id PRIMARY KEY, status INTEGER, hwOnline INTEGER DEFAULT 0) INITRANS 50 PCTFREE 50 ENABLE ROW MOVEMENT;
+CREATE TABLE DiskServer (name VARCHAR2(2048), lastHeartbeatTime NUMBER DEFAULT 0, id INTEGER CONSTRAINT PK_DiskServer_Id PRIMARY KEY, status INTEGER, hwOnline INTEGER DEFAULT 0) INITRANS 50 PCTFREE 50 ENABLE ROW MOVEMENT;
 CREATE UNIQUE INDEX I_DiskServer_name ON DiskServer (name);
 ALTER TABLE DiskServer MODIFY
   (status CONSTRAINT NN_DiskServer_Status NOT NULL,
@@ -1176,7 +1176,6 @@ CREATE TABLE DrainingJob
    egid           INTEGER CONSTRAINT NN_DrainingJob_Egid NOT NULL,
    pid            INTEGER CONSTRAINT NN_DrainingJob_Pid NOT NULL,
    machine        VARCHAR2(2048) CONSTRAINT NN_DrainingJob_Machine NOT NULL,
-   reqId          VARCHAR2(2048) CONSTRAINT NN_DrainingJob_ReqId NOT NULL,
    creationTime   INTEGER CONSTRAINT NN_DrainingJob_CT NOT NULL,
    lastModificationTime INTEGER CONSTRAINT NN_DrainingJob_LMT NOT NULL,
    status         INTEGER CONSTRAINT NN_DrainingJob_Status NOT NULL,
