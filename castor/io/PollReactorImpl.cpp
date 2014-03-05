18820c2b8d5e40bcb6787c9e020f830be5d9cbe4
Steven Murray <steven.murray@cern.ch>
2014-03-05 14:00:34 +0100

    Added a concrete sub-class of castor::io::PollReactor
    This concrete implementation will hopefully be generic enough
    to be shared between the future tape server and remote media
    changer daemons.

---
+++ castor/io/PollReactorImpl.cpp
@@ -0,0 +1,139 @@
+/******************************************************************************
+ *                castor/io/PollReactor.cpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @author Steven.Murray@cern.ch
+ *****************************************************************************/
+
+#include "castor/exception/BadAlloc.hpp"
+#include "castor/io/PollReactorImpl.hpp"
+#include "castor/utils/SmartArrayPtr.hpp"
+
+#include <unistd.h>
+
+//------------------------------------------------------------------------------
+// constructor
+//------------------------------------------------------------------------------
+castor::io::PollReactorImpl::PollReactorImpl(log::Logger &log) throw():
+  m_log(log) {
+}
+
+//------------------------------------------------------------------------------
+// destructor
+//------------------------------------------------------------------------------
+castor::io::PollReactorImpl::~PollReactorImpl() throw() {
+  for(HandlerMap::const_iterator itor = m_handlers.begin();
+    itor !=  m_handlers.end(); itor++) {
+    delete itor->second;
+  }
+}
+
+//------------------------------------------------------------------------------
+// registerHandler
+//------------------------------------------------------------------------------
+void castor::io::PollReactorImpl::registerHandler(
+  PollEventHandler *const handler) throw(castor::exception::Exception) {
+  m_handlers[handler->getFd()] = handler;
+}
+
+//------------------------------------------------------------------------------
+// handleEvents
+//------------------------------------------------------------------------------
+void castor::io::PollReactorImpl::handleEvents(const int timeout)
+  throw(castor::exception::Exception) {
+  nfds_t nfds = 0;
+  castor::utils::SmartArrayPtr<struct pollfd> fds(buildPollFds(nfds));
+
+  const int pollrc = poll(fds.get(), nfds, timeout);
+
+  switch(pollrc) {
+  case 0:
+    // Timeout - do nothing
+    break;
+  case -1:
+    {
+      const int pollErrno = errno;
+
+      log::Param params[] = {
+        log::Param("errno", pollErrno),
+        log::Param("message", sstrerror(pollErrno))};
+      m_log(LOG_ERR, "Failed to handle a pending vdqm request: poll() failed",
+        params);
+    }
+  default:
+    dispatchEventHandlers(fds.get(), nfds);
+  }
+}
+
+//------------------------------------------------------------------------------
+// buildPollFdsArray
+//------------------------------------------------------------------------------
+struct pollfd *castor::io::PollReactorImpl::buildPollFds(nfds_t &nfds)
+  throw(castor::exception::Exception) {
+  nfds = m_handlers.size();
+
+  castor::utils::SmartArrayPtr<struct pollfd> fds;
+  try {
+    fds.reset(new struct pollfd[nfds]);
+  } catch(std::bad_alloc &ba) {
+    castor::exception::BadAlloc ex;
+    ex.getMessage() <<
+      "Failed to allocate memory for the file-descriptors of poll()"
+      ": " << ba.what();
+  }
+
+  int i=0;
+  for(HandlerMap::const_iterator itor = m_handlers.begin();
+    itor != m_handlers.end(); itor++) {
+    itor->second->fillPollFd(fds[i]);
+    i++;
+  }
+
+  return fds.release();
+}
+
+//------------------------------------------------------------------------------
+// dispatchEventHandlers
+//------------------------------------------------------------------------------
+void castor::io::PollReactorImpl::dispatchEventHandlers(
+  const struct pollfd *const fds, const nfds_t nfds)
+  throw(castor::exception::Exception) {
+  // For each poll() file descriptor
+  for(nfds_t i=0; i<nfds; i++) {
+    // Find and dispatch the appropriate handler if there is a pending event
+    if(0 != fds[i].revents) {
+      findHandler(fds[i].fd)->handleEvent(fds[i]);
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+// findHandler
+//------------------------------------------------------------------------------
+castor::io::PollEventHandler *castor::io::PollReactorImpl::findHandler(
+  const int fd) throw(castor::exception::Exception) {
+  HandlerMap::iterator itor = m_handlers.find(fd);
+  if(itor == m_handlers.end()) {
+    castor::exception::Internal ex;
+    ex.getMessage() <<
+      "Failed to find event handler for file descriptor " << fd;
+    throw ex;
+  }
+
+  return itor->second;
+}
---
+++ castor/io/PollReactorImpl.hpp
@@ -0,0 +1,123 @@
+/******************************************************************************
+ *                castor/io/PollReactorImpl.hpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @author Steven.Murray@cern.ch
+ *****************************************************************************/
+
+#ifndef CASTOR_IO_POLLREACTORIMPL_HPP
+#define CASTOR_IO_POLLREACTORIMPL_HPP 1
+
+#include "castor/io/PollReactor.hpp"
+#include "castor/log/Logger.hpp"
+
+#include <map>
+
+namespace castor {
+namespace io {
+
+/**
+ * Concrete implementation of the a reactor that wraps the poll() system call.
+ *
+ * This class is part of an implementation of the Reactor architecture pattern
+ * described in the following book:
+ *
+ *    Pattern-Oriented Software Architecture Volume 2
+ *    Patterns for Concurrent and Networked Objects
+ *    Authors: Schmidt, Stal, Rohnert and Buschmann
+ *    Publication date: 2000
+ *    ISBN 0-471-60695-2
+ */
+class PollReactorImpl: public PollReactor {
+public:
+  /**
+   * Constructor.
+   *
+   * @param log The object representing the API of the CASTOR logging system.
+   */
+  PollReactorImpl(log::Logger &log) throw();
+
+  /**
+   * Destructor.
+   */
+  ~PollReactorImpl() throw();
+
+  /**
+   * Registers the specified handler.
+   *
+   * Please note that the reactor takes ownership of the handler and will
+   * delete it as appropriate.
+   *
+   * @param handler The handler to be registered.
+   */
+  void registerHandler(PollEventHandler *const handler)
+    throw(castor::exception::Exception);
+
+  /**
+   * Handles any pending events.
+   *
+   * @param timeout Timeout in milliseconds.
+   */
+  void handleEvents(const int timeout) throw(castor::exception::Exception);
+
+private:
+
+  /**
+   * Object representing the API of the CASTOR logging system.
+   */
+  log::Logger &m_log;
+
+  /**
+   * Type used to map file descriptor to event handler.
+   */
+  typedef std::map<int, PollEventHandler*> HandlerMap;
+
+  /**
+   * Map of file descriptor to registered event handler.
+   */
+  HandlerMap m_handlers;
+
+  /**
+   * Allocates and builds the array of file descriptors to be passed to poll().
+   *
+   * @param nfds Output parameter: The number of elements in the array.
+   * @return The array of file descriptors.  Please note that is the
+   * responsibility of the caller to delete the array.
+   */
+  struct pollfd *buildPollFds(nfds_t &nfds) throw(castor::exception::Exception);
+
+  /**
+   * Dispatches the appropriate event handlers based on the specified result
+   * from poll().
+   */
+  void dispatchEventHandlers(const struct pollfd *const fds, const nfds_t nfds)
+    throw(castor::exception::Exception);
+
+  /**
+   * Returns the event handler associated with the specified integer
+   * file-descriptor.
+   */
+  PollEventHandler *findHandler(const int fd)
+    throw(castor::exception::Exception);
+
+}; // class PollReactorImpl
+
+} // namespace io
+} // namespace castor
+
+#endif // CASTOR_IO_POLLREACTORIMPL_HPP
