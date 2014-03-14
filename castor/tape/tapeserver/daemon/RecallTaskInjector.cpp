9c6ce1cd86cec937661c99a4f14f87b5d8d3d739
David COME <david.come@cern.ch>
2014-03-14 16:58:29 +0100

    Add comments and logging

--- castor/tape/tapeserver/daemon/RecallTaskInjector.cpp
+++ castor/tape/tapeserver/daemon/RecallTaskInjector.cpp
@@ -1,5 +1,9 @@
 #include "RecallTaskInjector.hpp"
+#include "castor/log/LogContext.hpp"
 #include <stdint.h>
+
+using castor::log::LogContext;
+using castor::log::Param;
 
 namespace castor{
 namespace tape{
@@ -14,10 +18,15 @@ RecallTaskInjector::RecallTaskInjector(MemoryManager & mm,
         m_client(client),m_lc(lc) 
 {}
 
-void RecallTaskInjector::requestInjection(int maxFiles, int maxBlocks, bool lastCall) {
-  printf("RecallJobInjector::requestInjection()\n");
+void RecallTaskInjector::requestInjection(int maxFiles, int byteSizeThreshold, bool lastCall) {
+  
+  LogContext::ScopedParam sp01(m_lc, Param("maxFiles", maxFiles));
+  LogContext::ScopedParam sp02(m_lc, Param("byteSizeThreshold",byteSizeThreshold));
+  LogContext::ScopedParam sp03(m_lc, Param("lastCall", lastCall));
+
+  m_lc.log(LOG_INFO,"Request more jobs");
   castor::tape::threading::MutexLocker ml(&m_producerProtection);
-          m_queue.push(Request(maxFiles, maxBlocks, lastCall));
+          m_queue.push(Request(maxFiles, byteSizeThreshold, lastCall));
 }
 
 void RecallTaskInjector::waitThreads() {
@@ -29,20 +38,36 @@ void RecallTaskInjector::startThreads() {
 }
 
 void RecallTaskInjector::injectBulkRecalls(const std::list<RecallJob>& jobs) {
-  for (std::list<RecallJob>::const_iterator i = jobs.begin(); i != jobs.end(); ++i) {
-    DiskWriteFileTask * dwt = new DiskWriteFileTask(i->fileId, i->blockCount, m_memManager);
-    TapeReadFileTask * trt = new TapeReadFileTask(*dwt, i->fSeq, i->blockCount);
+  for (std::list<RecallJob>::const_iterator it = jobs.begin(); it != jobs.end(); ++it) {
+    
+    LogContext::ScopedParam sp(m_lc, Param("NSFILEID", it->fileId));
+    LogContext::ScopedParam sp2(m_lc, Param("NSFILESEQNUMBER", it->fSeq));
+    m_lc.log(LOG_INFO, "Logged file to recall");
+    
+    DiskWriteFileTask * dwt = new DiskWriteFileTask(it->fileId, it->blockCount, m_memManager);
+    TapeReadFileTask * trt = new TapeReadFileTask(*dwt, it->fSeq, it->blockCount);
+    
     m_diskWriter.push(dwt);
     m_tapeReader.push(trt);
   }
+  LogContext::ScopedParam sp03(m_lc, Param("nbFile", jobs.size()));
 }
 
-bool RecallTaskInjector::synchronousInjection(uint64_t maxFiles, uint64_t maxSize)
+bool RecallTaskInjector::synchronousInjection(uint64_t maxFiles, uint64_t byteSizeThreshold)
 {
-  ClientInterface::RequestReport requestReporter;
-  std::list<RecallJob> jobs ; m_client.getFilesToRecall(maxFiles, maxSize,requestReporter);
-  
+  ClientInterface::RequestReport reqReport;  
+  //@TODO interface TBD   
+  LogContext::ScopedParam sp(m_lc, Param("maxFiles", maxFiles));
+  LogContext::ScopedParam sp0(m_lc, Param("byteSizeThreshold",byteSizeThreshold));
+
+  std::list<RecallJob> jobs ;/*=*/ m_client.getFilesToRecall(maxFiles, byteSizeThreshold,reqReport);
+
+  LogContext::ScopedParam sp01(m_lc, Param("transactionId", reqReport.transactionId));
+  LogContext::ScopedParam sp02(m_lc, Param("connectDuration", reqReport.connectDuration));
+  LogContext::ScopedParam sp03(m_lc, Param("sendRecvDuration", reqReport.sendRecvDuration));
+
   if(jobs.empty()) {
+    m_lc.log(LOG_ERR, "Get called but no files to retrieve");
     return false;
   }
   else {
@@ -50,18 +75,25 @@ bool RecallTaskInjector::synchronousInjection(uint64_t maxFiles, uint64_t maxSiz
     return true;
   }
 }
-  
+
 //--------------------------------------
 void RecallTaskInjector::WorkerThread::run()
 {
+  using castor::log::LogContext;
+  
       while (1) {
         Request req = _this.m_queue.pop();
 	printf("RecallJobInjector:run: about to call client interface\n");
-        ClientInterface::RequestReport requestReporter;
-        std::list<RecallJob> jobs ; _this.m_client.getFilesToRecall(req.maxFiles, req.maxBlocks,requestReporter);
+        ClientInterface::RequestReport reqReport;
+        std::list<RecallJob> jobs ; /*=*/ _this.m_client.getFilesToRecall(req.nbMaxFiles, req.byteSizeThreshold,reqReport);
+        
+        LogContext::ScopedParam sp01(_this.m_lc, Param("transactionId", reqReport.transactionId));
+        LogContext::ScopedParam sp02(_this.m_lc, Param("connectDuration", reqReport.connectDuration));
+        LogContext::ScopedParam sp03(_this.m_lc, Param("sendRecvDuration", reqReport.sendRecvDuration));
+  
         if (jobs.empty()) {
           if (req.lastCall) {
-	    printf("In RecallJobInjector::WorkerThread::run(): last call turned up empty: triggering the end of session.\n");
+	    _this.m_lc.log(LOG_INFO,"No more file to recall: triggering the end of session.\n");
             _this.m_tapeReader.finish();
             _this.m_diskWriter.finish();
             break;
@@ -69,7 +101,6 @@ void RecallTaskInjector::WorkerThread::run()
 	    printf("In RecallJobInjector::WorkerThread::run(): got empty list, but not last call. NoOp.\n");
 	  }
         } else {
-	  printf("In RecallJobInjector::WorkerThread::run(): got %zd files.\n", jobs.size());
           _this.injectBulkRecalls(jobs);
         }
       } // end of while(1)
--- castor/tape/tapeserver/daemon/RecallTaskInjector.hpp
+++ castor/tape/tapeserver/daemon/RecallTaskInjector.hpp
@@ -37,18 +37,15 @@
 #include "ClientInterface.hpp"
 #include "castor/log/LogContext.hpp"
 
-
-using castor::log::LogContext;
-
 namespace castor{
 namespace tape{
 namespace tapeserver{
 namespace daemon {
 
 /**
- * This classis reponsible for creating the tasks in case of a recall job
-  */
-class RecallTaskInjector: public JobInjector {
+ * This classis responsible for creating the tasks in case of a recall job
+ */
+class RecallTaskInjector: public JobInjector {  
 public:
 
   RecallTaskInjector(MemoryManager & mm, 
@@ -56,24 +53,66 @@ public:
         DiskWriteThreadPool & diskWriter,ClientInterface& client,
         castor::log::LogContext& lc);
 
+  
+  /**
+   * Function for a feed-back loop purpose between RecallTaskInjector and 
+   * DiskWriteThreadPool. When DiskWriteThreadPool::popAndRequestMoreJobs detects 
+   * it has not enough jobs to do to, it is class to push a request 
+   * in order to (try) fill up the queue. 
+   * @param maxFiles  files count requested.
+   * @param maxBlocks total bytes count at least requested
+   * @param lastCall true if we want the new request to be a last call. 
+   * See Request::lastCall 
+   */
+  virtual void requestInjection(int maxFiles, int byteSizeThreshold, bool lastCall);
 
-  virtual void requestInjection(int maxFiles, int maxBlocks, bool lastCall);
+    /**
+     * Contact the client to make sure there are really something to do
+     * Something = recall at most  maxFiles or at least maxBytes
+     * 
+     * @param maxFiles files count requested.
+     * @param byteSizeThreshold total bytes count  at least requested
+     * @return true if there are jobs to be done, false otherwise 
+     */
+  bool synchronousInjection(uint64_t maxFiles, uint64_t byteSizeThreshold);
 
-  bool synchronousInjection(uint64_t maxFiles, uint64_t maxBlocks);
-
+  /**
+   * Wait for the inner thread to finish
+   */
   void waitThreads();
+  
+  /**
+   * Start the inner thread 
+   */
   void startThreads();
 private:
+  
+  /**
+   * Create all the tape-read and write-disk tasks for set of files to retrieve
+   * @param jobs
+   */
   void injectBulkRecalls(const std::list<RecallJob>& jobs);
 
   /**
-   * A request of files to recall
+   * A request of files to recall. We request EITHER
+   * - a maximum of nbMaxFiles files
+   * - OR at least byteSizeThreshold bytes. 
+   * That means we stop as soon as we have nbMaxFiles files or the cumulated size 
+   * is equal or above byteSizeThreshold. 
    */
   class Request {
   public:
-    Request(int mf, int mb, bool lc): maxFiles(mf), maxBlocks(mb), lastCall(lc) {}
-    const int maxFiles;
-    const int maxBlocks;
+    Request(int mf, int mb, bool lc):
+    nbMaxFiles(mf), byteSizeThreshold(mb), lastCall(lc) {}
+    
+    const int nbMaxFiles;
+    const int byteSizeThreshold;
+    
+    /** 
+     * True if it is the last call for the set of requests :it means
+     *  we don't need to try to get more files to recall 
+     *  and can send into all the different threads a signal  .
+     */
     const bool lastCall;
   };
   
@@ -87,11 +126,17 @@ private:
   } m_thread;
 
   MemoryManager & m_memManager;
+  
+
   TapeReadSingleThread & m_tapeReader;
   DiskWriteThreadPool & m_diskWriter;
-
   ClientInterface& m_client;
+  
+  /**
+   * utility member to log some pieces of information
+   */
   castor::log::LogContext& m_lc;
+  
   castor::tape::threading::Mutex m_producerProtection;
   castor::tape::threading::BlockingQueue<Request> m_queue;
 };
