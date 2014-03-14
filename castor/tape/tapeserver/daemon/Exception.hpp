80f3409e980581f6ab62d461210fba72d23c3da0
David COME <david.come@cern.ch>
2014-03-14 10:51:01 +0100

    astor/tape/tapeserver/daemon/Exception.hpp has been split into 2 files Indeed, otherwhise I got multiple definitions errors with errnoExeptLauncher

--- castor/tape/tapeserver/daemon/Exception.hpp
+++ castor/tape/tapeserver/daemon/Exception.hpp
@@ -41,28 +41,4 @@ private:
   std::string m_what;
 };
 
-void errnoExeptLauncher(int ret, void * obj) {
-  if(ret) {
-    char s[1000];
-    char t[1000];
-#ifdef _GNU_SOURCE
-    char * errorStr = ::strerror_r(ret, s, sizeof(s));
-    if (!errorStr) {
-      snprintf(t, sizeof(t), "Failed to get error string for ret=%d obj=0x%p", ret, obj);
-      throw MemException(t);
-    } else {
-      snprintf(t, sizeof(t), "%s (ret=%d) obj=0x%p",errorStr, ret, obj);
-      throw MemException(t);
-    }
-#else /* This should work for CLANG on MAC */
-    int r2 = ::strerror_r(ret, s, sizeof(s));
-    if (!r2) {
-      snprintf(t, sizeof(t), "Failed to get error string for ret=%d obj=0x%p", ret, obj);
-      throw MemException(t);
-    } else {
-      snprintf(t, sizeof(t), "%s (ret=%d) obj=0x%p",s, ret, obj);
-      throw MemException(t);
-    }
-#endif
-  }
-}
+void errnoExeptLauncher(int ret, void * obj);
