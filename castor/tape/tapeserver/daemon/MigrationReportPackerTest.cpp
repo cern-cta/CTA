4618fb8e42ffa45047aee43a450a828eef0708a3
David COME <david.come@cern.ch>
2014-03-26 13:37:54 +0100

    (minor) Fixed name in cartouche of MigrationReportPackerTest.cpp

--- castor/tape/tapeserver/daemon/MigrationReportPackerTest.cpp
+++ castor/tape/tapeserver/daemon/MigrationReportPackerTest.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- *                      MigrationReportPackerTest.hpp
+ *                      MigrationReportPackerTest.cpp
  *
  * This file is part of the Castor project.
  * See http://castor.web.cern.ch/castor
