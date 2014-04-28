8a7578a8547873c36b76b7efb2ebd9e3acd00b33
David COME <david.come@cern.ch>
2014-04-28 11:59:47 +0200

    Added an entry in order to suppress a race detected by hellgrind in SLC5

---
+++ castor/tape/tapeserver/daemon/TapeReadTaskInterface.hpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+ *                      TapeReadTask.hpp
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * 
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+#pragma once
+
+#include "castor/tape/tapeserver/daemon/Exception.hpp"
+#include "castor/tape/tapeserver/drive/Drive.hpp"
+#include "castor/tape/tapeserver/file/File.hpp"
+
+namespace castor {
+namespace tape {
+namespace tapeserver {
+namespace daemon {
+class TapeReadTask {
+public:
+  virtual void execute(castor::tape::tapeFile::ReadSession & /*rs*/,
+    castor::log::LogContext & /*lc*/) { 
+    throw MemException("Tring to execute a non-execuatble TapeReadTask"); 
+  };
+  virtual ~TapeReadTask() {}
+};
+}
+}
+}
+}
