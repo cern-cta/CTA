2409456a8584948e74c491dc0336cf2d2d64ce1c
Sebastien Ponce <sebastien.ponce@cern.ch>
2014-06-19 13:33:03 +0200

    complete reindentation/cleanup of the gridFTP code to make it readable

--- gridftp2/internal/dsi_CASTOR2int.h
+++ gridftp2/internal/dsi_CASTOR2int.h
@@ -17,56 +17,53 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
+ * GridFTP plugin for CASTOR Diskservers
  *
- *
- *
- * @author Victor Kotlyar, Victor.Kotlyar@cern.ch
+ * @author castor-dev@cern.ch
  *****************************************************************************/
 
 #pragma once
 
 #include "globus_gridftp_server.h"
 
-typedef struct checksum_block_list_s 
-  {
+typedef struct checksum_block_list_s {
   globus_off_t offset;
   globus_size_t size;
   unsigned int csumvalue; /* only for 32bit checksums as Adler32 or CRC32 */
   struct checksum_block_list_s *next;
-  } checksum_block_list_t;   
+} checksum_block_list_t;
 
 // comparison of 2 checksum_block_list_t* on their offset for the use of qsort
 int offsetComparison(const void *first, const void *second);
-  
-typedef struct globus_l_gfs_CASTOR2int_handle_s
-   {
-   globus_mutex_t		mutex;
-   int				fd;
-   globus_result_t		cached_res;
-   int               		outstanding;
-   int 				optimal_count;
-   globus_bool_t		done;
-   globus_off_t			blk_length;
-   globus_off_t			blk_offset;
-   globus_size_t		block_size;
-   globus_gfs_operation_t	op;
-   char *			uuid;           /* must be pointers to environment variables */
-   char *			fullDestPath;   /* we do not allocate or free memory here    */
-   char *                       access_mode;
-   checksum_block_list_t *      checksum_list; /* we have to save all blocs checksums for on the fly calculation */
-   checksum_block_list_t *      checksum_list_p;
-   unsigned long                number_of_blocks;
-   char                         useCksum;     /* GSIFTP USE_CKSUM value in castor.conf, default yes */
-   } globus_l_gfs_CASTOR2int_handle_t;
-   
-static void globus_l_gfs_file_net_read_cb(
-	        globus_gfs_operation_t,
-		globus_result_t,
-		globus_byte_t *,
-		globus_size_t,
-		globus_off_t,
-		globus_bool_t,
-		void *);
+
+typedef struct globus_l_gfs_CASTOR2int_handle_s {
+  globus_mutex_t mutex;
+  int fd;
+  globus_result_t cached_res;
+  int outstanding;
+  int optimal_count;
+  globus_bool_t done;
+  globus_off_t blk_length;
+  globus_off_t blk_offset;
+  globus_size_t block_size;
+  globus_gfs_operation_t op;
+  char *uuid;           /* must be pointers to environment variables */
+  char *fullDestPath;   /* we do not allocate or free memory here    */
+  char *access_mode;
+  /* we have to save all blocs checksums for on the fly calculation */
+  checksum_block_list_t * checksum_list;
+  checksum_block_list_t *checksum_list_p;
+  unsigned long number_of_blocks;
+  char useCksum;        /* GSIFTP USE_CKSUM value in castor.conf, default yes */
+} globus_l_gfs_CASTOR2int_handle_t;
+
+static void globus_l_gfs_file_net_read_cb(globus_gfs_operation_t,
+                                          globus_result_t,
+                                          globus_byte_t *,
+                                          globus_size_t,
+                                          globus_off_t,
+                                          globus_bool_t,
+                                          void *);
 static void globus_l_gfs_CASTOR2int_read_from_net(globus_l_gfs_CASTOR2int_handle_t * );
 static globus_result_t globus_l_gfs_make_error(const char *);
 
@@ -75,12 +72,8 @@ void free_stat_array(globus_gfs_stat_t * ,int);
 void free_checksum_list(checksum_block_list_t *);
 int CASTOR2int_handle_open(char *, int, int, globus_l_gfs_CASTOR2int_handle_t *);
 static globus_bool_t globus_l_gfs_CASTOR2int_send_next_to_client(globus_l_gfs_CASTOR2int_handle_t *);
-static void globus_l_gfs_net_write_cb(
-		globus_gfs_operation_t,
-		globus_result_t,
-		globus_byte_t *,
-		globus_size_t,
-		void *);
-
-
-
+static void globus_l_gfs_net_write_cb(globus_gfs_operation_t,
+                                      globus_result_t,
+                                      globus_byte_t *,
+                                      globus_size_t,
+                                      void *);
