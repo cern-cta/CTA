fa3a107376b058c481e7a176dcf8782ec316827b
Sebastien Ponce <sebastien.ponce@cern.ch>
2014-08-08 10:52:17 +0200

    Adapted test suite to xrootd4

--- test/testsuite/castortests/resources/xroot/tags.py
+++ test/testsuite/castortests/resources/xroot/tags.py
@@ -60,20 +60,13 @@ def xrdcpURLparam(self, nb=0):
 Setup.getTag_xrdcpURLparam = xrdcpURLparam
 
 def corexrdcp(self):
-    return os.environ['XROOTSYS'] + os.sep + 'bin' + os.sep + 'xrdcp'
+    return os.environ['XROOTSYS'] + os.sep + 'bin' + os.sep + 'xrdcp -np'
 Setup.getTag_corexrdcp = corexrdcp
 
 def xrdcp(self):
-    cmd = os.environ['XROOTSYS'] + os.sep + 'bin' + os.sep + 'xrdcp'
+    cmd = os.environ['XROOTSYS'] + os.sep + 'bin' + os.sep + 'xrdcp -np'
     return [cmd, 'KRB5CCNAME=nonexistinghost ' + cmd]
 Setup.getTag_xrdcp = xrdcp
-
-# This is the old xroot client, but we still use it for the prepare test
-# as the new client (3.3.x) does not provide the equivalent command. It should
-# be dropped once we adopt xroot 4.0.0, which provides a prepare command.
-def xrd(self):
-    return os.environ['XROOTSYS'] + os.sep + 'bin' + os.sep + 'xrd'
-Setup.getTag_xrd = xrd
 
 def xrdfs(self):
     return os.environ['XROOTSYS'] + os.sep + 'bin' + os.sep + 'xrdfs'
