a0fc9a9a0caeacd21d1966f8b2ff9c4be9494994
Sebastien Ponce <sebastien.ponce@cern.ch>
2014-08-19 17:12:13 +0200

    Readded foreign key constraint FK_DrainingErrors_DJ lost by mistake

--- castor/db/oracleCommon.schema.sql
+++ castor/db/oracleCommon.schema.sql
@@ -1221,6 +1221,11 @@ ENABLE ROW MOVEMENT;
 CREATE INDEX I_DrainingErrors_DJ_CF ON DrainingErrors (drainingJob, CastorFile);
 
 ALTER TABLE DrainingErrors
+  ADD CONSTRAINT FK_DrainingErrors_DJ
+  FOREIGN KEY (drainingJob)
+  REFERENCES DrainingJob (id);
+
+ALTER TABLE DrainingErrors
   ADD CONSTRAINT FK_DrainingErrors_CastorFile
   FOREIGN KEY (castorFile)
   REFERENCES CastorFile (id);
