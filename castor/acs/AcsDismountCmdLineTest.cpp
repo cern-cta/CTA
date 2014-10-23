9f74d3049cc54519a19e90061b986e251dc73570
Steven Murray <Steven.Murray@cern.ch>
2014-10-23 12:16:06 +0200

    CASTOR-4816 RFE: 2.1.15-x should provide castor-tape-acs-dismount
    Done.

---
+++ castor/acs/AcsDismountCmdLineTest.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *
+ * This file is part of the Castor project.
+ * See http://castor.web.cern.ch/castor
+ *
+ * Copyright (C) 2003  CERN
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *
+ *
+ * @author Castor Dev team, castor-dev@cern.ch
+ *****************************************************************************/
+
+#include "castor/acs/AcsDismountCmdLine.hpp"
+
+#include <gtest/gtest.h>
+
+namespace unitTests {
+
+class castor_acs_AcsDismountCmdLineTest: public ::testing::Test {
+protected:
+
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() {
+  }
+
+}; // class castor_acs_AcsDismountCmdLineTest
+
+TEST_F(castor_acs_AcsDismountCmdLineTest, constructor) {
+  const castor::acs::AcsDismountCmdLine cmdLine;
+  ASSERT_EQ(false, cmdLine.debug);
+  ASSERT_EQ((BOOLEAN)FALSE, cmdLine.force);
+  ASSERT_EQ(false, cmdLine.help);
+  ASSERT_EQ(0, cmdLine.timeout);
+  ASSERT_EQ(0, cmdLine.queryInterval);
+  ASSERT_EQ(0, (int)cmdLine.driveId.panel_id.lsm_id.acs);
+  ASSERT_EQ(0, (int)cmdLine.driveId.panel_id.lsm_id.lsm);
+  ASSERT_EQ(0, (int)cmdLine.driveId.panel_id.panel);
+  ASSERT_EQ(0, (int)cmdLine.driveId.drive);
+  ASSERT_EQ('\0', cmdLine.volId.external_label[0]);
+}
+
+} // namespace unitTests
