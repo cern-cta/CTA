97c0a5a496ff88e80a1ff737a134f4e35e0628b4
Giuseppe Lo Presti <Giuseppe.LoPresti@cern.ch>
2014-11-24 15:03:20 +0100

    Dropped listing of tape transfers: this is going to be replaced by querying the xroot server for the ongoing transfers

--- castor/scheduler/diskmanager/manager.py
+++ castor/scheduler/diskmanager/manager.py
@@ -54,8 +54,6 @@ class ManagerThread(threading.Thread):
         self.transferQueue.pollD2dDest()
         # check for transfer to be canceled for various reasons (timeout, missing resources, ...)
         self.transferQueue.checkForTransfersCancelation()
-        # recompute list of ongoing tape transfers
-        self.runningTransfers.listTapeTransfers()
       except connectionpool.Timeout, e:
         # 'Caught exception in Management thread' message
         dlf.writenotice(msgs.MGMTEXCEPTION, Type=str(e.__class__), Message=str(e))
