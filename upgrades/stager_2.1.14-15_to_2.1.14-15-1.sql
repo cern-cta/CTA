62a8ae503e1890540c518b5616016abf3c460451
Giuseppe Lo Presti <lopresti@cern.ch>
2015-01-20 17:10:02 +0100

    CASTOR-4863: Closure of transfer protocol not working
    Amended the fix to take into account clients that set an empty
    protocol (yes, stager_put does that...), that was accepted so far.

--- upgrades/stager_2.1.14-15_to_2.1.14-15-1.sql
+++ upgrades/stager_2.1.14-15_to_2.1.14-15-1.sql
@@ -491,8 +491,8 @@ BEGIN
   SELECT svcHandler INTO svcHandler FROM Type2Obj WHERE type=inReqType;
   -- Loop on subrequests
   FOR i IN srFileNames.FIRST .. srFileNames.LAST LOOP
-    -- check protocol validity
-    IF INSTR(protocols, ' ' || srProtocols(i) || ' ') = 0 THEN
+    -- check protocol validity for Get/Update/Put requests only, for other requests the protocol is irrelevant
+    IF inReqType IN (35, 40, 44) AND INSTR(protocols, ' ' || srProtocols(i) || ' ') = 0 THEN
       raise_application_error(-20122, 'Unsupported protocol in insertFileRequest : ' || srProtocols(i));
     END IF;
     -- get unique ids for the subrequest
