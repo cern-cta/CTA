156174cde496d7addb66f4f318c85256fa7e7d7a
Steven Murray <Steven.Murray@cern.ch>
2015-02-02 15:49:51 +0100

    Simplifed DirectoryEntry by replacing multiple fields with the new field named 'mode'

--- libs/client/DirectoryEntry.hpp
+++ libs/client/DirectoryEntry.hpp
@@ -14,29 +14,6 @@ namespace cta {
 struct DirectoryEntry {
 
   /**
-   * Enumeration of the different types of directory entry.
-   */
-  enum EntryType {
-    NONE,
-    FILE_ENTRY,
-    DIRECTORY_ENTRY
-  };
-
-  /**
-   * Thread safe method that returns the string representation of the
-   * specified enumeration (integer) value.
-   *
-   * @param enumValue The enumeration (integer) value.
-   * @return The string value.
-   */
-  static const char *typeToString(const EntryType enumValue) throw();
-
-  /**
-   * The type of the directory entry.
-   */
-  EntryType entryType;
-
-  /**
    * The name of the directory.
    */
   std::string name;
@@ -52,22 +29,11 @@ struct DirectoryEntry {
   uint32_t groupId;
 
   /**
-   * Access permissions for the file's owner "a la" posix. For example a value
-   * of 7 means read, write and execute permissions.
+   * The mode bits of the file. These bits will have the same values as the
+   * st_mode field of the "struct stat" as described in the unix manual page
+   * "man 2 stat".
    */
-  uint8_t ownerPerms;
-
-  /**
-   * Access permissions for the file's group  "a la" posix. For example a
-   * value of 7 means read, write and execute permissions.
-   */
-  uint8_t groupPerms;
-
-  /**
-   * Access permissions for users other than the owner or members of the file's
-   * group.
-   */
-  uint8_t otherPerms;
+  uint16_t mode;
 
   /**
    * The attributes of the directory.
