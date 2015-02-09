1e10fc64eb5ba5a41961ea4972fff868d73aedbd
Eric Cano <Eric.Cano@cern.ch>
2015-02-09 19:55:04 +0100

    Various debugging, plus changed the end criterion for the main process: now counter based.

---
+++ objectstore/Counter.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ObjectOps.hpp"
+#include "objectstore/cta.pb.h"
+
+namespace cta { namespace objectstore {
+
+  
+class Counter: private ObjectOps<serializers::Counter> {
+public:
+  Counter(const std::string & name, Agent & agent):
+  ObjectOps<serializers::Counter>(agent.objectStore(), name)
+  {
+    // check the presence of the entry
+    serializers::Counter cs;
+    updateFromObjectStore(cs, agent.getFreeContext());
+  }
+  
+  void inc(Agent & agent) {
+    serializers::Counter cs;
+    ContextHandle & ctx = agent.getFreeContext();
+    lockExclusiveAndRead(cs, ctx, __func__);
+    cs.set_count(cs.count()+1);
+    unlock(ctx, __func__);
+  }
+  
+  uint64_t get(Agent & agent) {
+    serializers::Counter cs;
+    updateFromObjectStore(cs, agent.getFreeContext());
+    return cs.count();
+  }
+};
+
+}}
