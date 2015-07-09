c12c3b2ff4e97356242180dde59e04c140ba42f7
Steven Murray <Steven.Murray@cern.ch>
2015-07-09 16:04:24 +0200

    Errnum now calls Utils::errnoToString()

--- common/exception/Errnum.cpp
+++ common/exception/Errnum.cpp
@@ -16,34 +16,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/******************************************************************************
- *
- * This file is part of the Castor project.
- * See http://castor.web.cern.ch/castor
- *
- * Copyright (C) 2003  CERN
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- *
- *
- *
- * @author Castor Dev team, castor-dev@cern.ch
- *****************************************************************************/
-
 #include "common/exception/Errnum.hpp"
-#include "common/strerror_r_wrapper.hpp"
-#include <string.h>
+#include "common/Utils.hpp"
+
 #include <errno.h>
+#include <string.h>
 
 using namespace cta::exception;
 
@@ -58,19 +35,7 @@ Errnum::Errnum(int err, std::string what):Exception("") {
 }
 
 void Errnum::ErrnumConstructorBottomHalf(const std::string & what) {
-  char buf[100];
-
-  if(strerror_r_wrapper(m_errnum, buf, sizeof(buf))) {
-    // sstrerror_r() failed
-    const int new_errno = errno;
-    std::stringstream w;
-    w << "Errno=" << m_errnum << ". In addition, failed to read the corresponding error string (sstrerror_r gave errno="
-            << new_errno << ")";
-    m_strerror = w.str();
-  } else {
-    // sstrerror_r() succeeded
-    m_strerror = buf;
-  }
+  m_strerror = Utils::errnoToString(m_errnum);
   std::stringstream w2;
   if (what.size())
     w2 << what << " ";
