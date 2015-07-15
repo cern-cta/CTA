0435a713e1a3086d97260903a53f0085c0b9f6d3
Daniele Kruse <dkruse@cern.ch>
2015-07-15 17:23:57 +0200

    WIP bringing in EOS

---
+++ remotens/EosNS.hpp
@@ -0,0 +1,98 @@
+/*
+ * The CERN Tape Archive (CTA) project
+ * Copyright (C) 2015  CERN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "common/RemoteFileStatus.hpp"
+#include "remotens/RemoteNS.hpp"
+
+#include "XrdCl/XrdClFileSystem.hh"
+
+#include <map>
+
+namespace cta {
+
+/**
+ * A mock proxy class for the namespace of a remote storage system.
+ */
+class EosNS: public RemoteNS {
+public:
+
+  /**
+   * Destructor.
+   */
+  ~EosNS() throw();
+  
+  /**
+   * Constructor.
+   */
+  EosNS(const std::string &XrootServerURL = "localhost:1094");
+
+  /**
+   * Returns the status of the specified file or directory within the remote
+   * storage system or NULL if the fil eor directory does not exist.
+   *
+   * @param path The absolute path of the file or directory.
+   * @return The status of the file or directory or NULL if the file or
+   * directory does not exist.
+   */
+  std::unique_ptr<RemoteFileStatus> statFile(const RemotePath &path);
+
+  /**
+   * Renames the specified remote file to the specified new name.
+   *
+   * @param remoteFile The current path.
+   * @param newRemoteFile The new path.
+   */
+  void rename(const RemotePath &remoteFile, const RemotePath &newRemoteFile);
+
+  /**
+   * Parses the specified remote file and returns the file name.
+   *
+   * @param path The absolute path of the file.
+   * @return The file name.
+   */
+  std::string getFilename(const RemotePath &remoteFile) const;
+  
+  /**
+   * Adds a directory or file to the remote NS with the desired status
+   * 
+   * @param status The desired status of the entry to be created
+   * @param path The absolute path of the file or directory
+   */
+  void createEntry(const RemotePath &path, const RemoteFileStatus &status);
+  
+private:
+  
+  /**
+   * The xroot server of EOS
+   */
+  XrdCl::FileSystem m_fs;
+  
+  /**
+   * Sends the command and waits for the reply
+   *
+   * @param argc The number of command-line arguments.
+   * @param argv The command-line arguments. 
+   * @return the return code
+   */
+  int sendCommand(const std::string &cmd) const;
+
+}; // class EosNS
+
+} // namespace cta
