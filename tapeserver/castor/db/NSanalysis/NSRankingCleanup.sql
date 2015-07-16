b53ab1531aed4adcc8ee6c7e1c19592f61d4c80e
Steven Murray <Steven.Murray@cern.ch>
2015-07-16 16:05:06 +0200

    Removed more CASTOR directories that are not related to tapeserverd

--- tapeserver/castor/db/NSanalysis/NSRankingCleanup.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE BiggestOnTapeTempTable;
-DROP TABLE BiggestOnDiskTempTable;
-DROP FUNCTION sec2date;
-DROP FUNCTION duration2char;
-DROP FUNCTION size2char;
-DROP PROCEDURE biggestOnTape;
-DROP PROCEDURE biggestOnDisk;
