b4ea89834494fbfa392acc099bcdae63b4cbd9b0
Steven Murray <Steven.Murray@cern.ch>
2015-07-29 16:38:55 +0200

    Removed unused CASTOR tests

--- tapeserver/test/security/nw.h
+++
@@ -1,9 +0,0 @@
-
-
-#pragma once
-
-int nw_create_socket(unsigned short port);
-ssize_t nw_readn(int fd, void *vptr, size_t n);
-ssize_t nw_writen(int fd, const void *vptr, size_t n);
-int nw_connect_to_server(char *host, unsigned short port);
-ssize_t nw_readline(int fd, void *vptr, size_t maxlen);
