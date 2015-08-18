9821449d6f6a80dbfd66959f5b53fbecd33fcf4e
Steven Murray <Steven.Murray@cern.ch>
2015-08-18 15:00:14 +0200

    Added castorlogunittests

---
+++ tapeserver/h/log.h
@@ -0,0 +1,29 @@
+/*
+ * The CERN Tape Archive (CTA) project
+ * Copyright (C) 2015  CERN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define LOG_EMERG       0       /* system is unusable                   */
+#define LOG_ALERT       1       /* action must be taken immediately     */
+#define LOG_CRIT        2       /* critical conditions                  */
+#define LOG_ERR         3       /* error conditions                     */
+#define LOG_WARNING     4       /* warning conditions                   */
+#define LOG_NOTICE      5       /* normal but signification condition   */
+#define LOG_INFO        6       /* informational                        */
+#define LOG_DEBUG       7       /* debug-level messages                 */
+
