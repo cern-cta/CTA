6c5995a47e8008318a71fe1fd35d04ffbdfdad8a
Eric Cano <Eric.Cano@cern.ch>
2015-11-20 14:17:35 +0100

    Added missing file for previous commit (protobuf checker/cleaner).

---
+++ objectstore/ProtobufCleaner.cpp
@@ -0,0 +1,38 @@
+/*
+ * The CERN Tape Archive (CTA) project
+ * Copyright (C) 2015  CERN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <google/protobuf/service.h>
+
+namespace cta::objectstore {
+/**
+ * This singleton class will check the compatibility of the runtime library
+ * with the headers it was compiled against on construction (library load)
+ * It will also cleanup 
+ */
+class ProtobufCleaner {
+  ProtobufCleaner() {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+  }
+  
+ virtual ~ProtobufCleaner() {
+   // Make protobuf cleanup memory to not trigger memory leaks detectors.
+   google::protobuf::ShutdownProtobufLibrary();
+ }
+} g_protobufCleaner;
+
+} // end of namespaces
