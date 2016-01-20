2e3a3e0b3e616496ecf199365754f1d3dede669c
Steven Murray <Steven.Murray@cern.ch>
2016-01-20 17:41:13 +0100

    Added the beginnings of CTA catalogue

---
+++ catalogue/Catalogue.hpp
@@ -0,0 +1,61 @@
+/*
+ * The CERN Tape Archive (CTA) project
+ * Copyright (C) 2015  CERN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "common/ArchiveRequest.hpp"
+
+#include <stdint.h>
+#include <string>
+
+namespace cta {
+
+/**
+ * Abstract class defining the interface to the CTA catalogue responsible for
+ * storing crticial information about archive files, tapes and tape files.
+ */
+class Catalogue {
+public:
+
+  /**
+   * Destructor.
+   */
+  virtual ~Catalogue() = 0;
+
+  /**
+   * Returns the next identifier to be used for a new archive file.
+   *
+   * @return The next identifier to be used for a new archive file.
+   */
+  virtual uint64_t getNextArchiveFileId() = 0;
+
+  /**
+   * Notifies the catalogue that a file has been written to tape.
+   *
+   * @param archiveRequest The identifier of the archive file.
+   *
+   */
+  virtual void fileWrittenToTape(
+    const ArchiveRequest &archiveRequest,
+    const std::string vid,
+    const uint64_t fSeq,
+    const uint64_t blockId) = 0;
+
+}; // class Catalogue
+
+} // namespace cta
---
+++ catalogue/MockCatalogue.hpp
@@ -0,0 +1,71 @@
+/*
+ * The CERN Tape Archive (CTA) project
+ * Copyright (C) 2015  CERN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "catalogue/Catalogue.hpp"
+
+#include <string>
+
+namespace cta {
+
+/**
+ * Mock CTA catalogue to facilitate unit testing.
+ */
+class MockCatalogue: public Catalogue {
+public:
+
+  /**
+   * Constructor.
+   */
+  MockCatalogue();
+
+  /**
+   * Destructor.
+   */
+  virtual ~MockCatalogue();
+
+  /**
+   * Returns the next identifier to be used for a new archive file.
+   *
+   * @return The next identifier to be used for a new archive file.
+   */
+  virtual uint64_t getNextArchiveFileId();
+
+  /**
+   * Notifies the catalogue that a file has been written to tape.
+   *
+   * @param archiveRequest The identifier of the archive file.
+   *
+   */
+  virtual void fileWrittenToTape(
+    const ArchiveRequest &archiveRequest,
+    const std::string vid,
+    const uint64_t fSeq,
+    const uint64_t blockId);
+
+private:
+
+  /**
+   * The next identifier to be used for a new archive file.
+   */
+  uint64_t m_nextArchiveFileId;
+
+}; // class MockCatalogue
+
+} // namespace cta
