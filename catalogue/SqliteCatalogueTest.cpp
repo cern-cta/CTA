77658353ea05611b0834131412e9aa1cfb5f59ea
Steven Murray <Steven.Murray@cern.ch>
2016-03-03 19:07:37 +0100

    Added createAdminUser test which intentionally fails

--- catalogue/SqliteCatalogueTest.cpp
+++ catalogue/SqliteCatalogueTest.cpp
@@ -50,25 +50,27 @@ TEST_F(cta_catalogue_SqliteCatalogueTest, createBootstrapAdminAndHostNoAuth) {
   common::dataStructures::UserIdentity admin1User;
   admin1User.setUid(m_admin1.getUid());
   admin1User.setGid(m_admin1.getGid());
-  const std::string comment = "createBootstrapAdminAndHostNoAuth";
+  const std::string bootstrapComment = "createBootstrapAdminAndHostNoAuth";
   ASSERT_NO_THROW(catalogue->createBootstrapAdminAndHostNoAuth(
-    m_cliIdentity, admin1User, m_admin1.getHost(), comment));
+    m_cliIdentity, admin1User, m_admin1.getHost(), bootstrapComment));
 
-  std::list<common::dataStructures::AdminUser> admins;
-  ASSERT_NO_THROW(admins = catalogue->getAdminUsers(m_admin1));
-  ASSERT_EQ(1, admins.size());
+  {
+    std::list<common::dataStructures::AdminUser> admins;
+    ASSERT_NO_THROW(admins = catalogue->getAdminUsers(m_admin1));
+    ASSERT_EQ(1, admins.size());
 
-  const common::dataStructures::AdminUser admin = admins.front();
-  ASSERT_EQ(comment, admin.getComment());
+    const common::dataStructures::AdminUser admin = admins.front();
+    ASSERT_EQ(bootstrapComment, admin.getComment());
 
-  const common::dataStructures::EntryLog creationLog = admin.getCreationLog();
-  ASSERT_EQ(m_cliIdentity.getUid(), creationLog.getUser().getUid());
-  ASSERT_EQ(m_cliIdentity.getGid(), creationLog.getUser().getGid());
-  ASSERT_EQ(m_cliIdentity.getHost(), creationLog.getHost());
+    const common::dataStructures::EntryLog creationLog = admin.getCreationLog();
+    ASSERT_EQ(m_cliIdentity.getUid(), creationLog.getUser().getUid());
+    ASSERT_EQ(m_cliIdentity.getGid(), creationLog.getUser().getGid());
+    ASSERT_EQ(m_cliIdentity.getHost(), creationLog.getHost());
 
-  const common::dataStructures::EntryLog lastModificationLog =
-    admin.getLastModificationLog();
-  ASSERT_EQ(creationLog, lastModificationLog);
+    const common::dataStructures::EntryLog lastModificationLog =
+      admin.getLastModificationLog();
+    ASSERT_EQ(creationLog, lastModificationLog);
+  }
 }
 
 TEST_F(cta_catalogue_SqliteCatalogueTest, createAdminUser) {
@@ -77,45 +79,43 @@ TEST_F(cta_catalogue_SqliteCatalogueTest, createAdminUser) {
   std::unique_ptr<catalogue::SqliteCatalogue> catalogue;
   ASSERT_NO_THROW(catalogue.reset(new catalogue::SqliteCatalogue()));
 
-  common::dataStructures::SecurityIdentity cliIdentity;
-  cliIdentity.setGid(1);
+  common::dataStructures::UserIdentity admin1User;
+  admin1User.setUid(m_admin1.getUid());
+  admin1User.setGid(m_admin1.getGid());
+  const std::string bootstrapComment = "createBootstrapAdminAndHostNoAuth";
+  ASSERT_NO_THROW(catalogue->createBootstrapAdminAndHostNoAuth(
+    m_cliIdentity, admin1User, m_admin1.getHost(), bootstrapComment));
 
-  const gid_t admin1Gid = 1;
-  const uid_t admin1Uid = 2;
-  const std::string adminHost1 = "adminhost1";
-
-  {
-    common::dataStructures::SecurityIdentity admin1;
-    admin1.setGid(admin1Gid);
-    admin1.setUid(admin1Uid);
-    admin1.setHost(adminHost1);
-  }
-
-  
-/*
   {
     std::list<common::dataStructures::AdminUser> admins;
+    ASSERT_NO_THROW(admins = catalogue->getAdminUsers(m_admin1));
+    ASSERT_EQ(1, admins.size());
 
-    ASSERT_NO_THROW(admins = catalogue->getAdminUSers(
-  ASSERT_TRUE
-*/
-  const gid_t creator_gid = 1;
-  const uid_t creator_uid = 2;
-  const std::string creator_host = "creator_host";
-  common::dataStructures::SecurityIdentity requester;
-  requester.setGid(creator_gid);
-  requester.setUid(creator_uid);
-  requester.setHost(creator_host);
+    const common::dataStructures::AdminUser admin = admins.front();
+    ASSERT_EQ(bootstrapComment, admin.getComment());
 
-  const gid_t admin_gid = 3;
-  const uid_t admin_uid = 4;
-  common::dataStructures::UserIdentity user;
-  user.setGid(admin_gid);
-  user.setUid(admin_uid);
+    const common::dataStructures::EntryLog creationLog = admin.getCreationLog();
+    ASSERT_EQ(m_cliIdentity.getUid(), creationLog.getUser().getUid());
+    ASSERT_EQ(m_cliIdentity.getGid(), creationLog.getUser().getGid());
+    ASSERT_EQ(m_cliIdentity.getHost(), creationLog.getHost());
 
-  const std::string comment = "comment";
-  
-  ASSERT_NO_THROW(catalogue->createAdminUser(requester, user, comment));
+    const common::dataStructures::EntryLog lastModificationLog =
+      admin.getLastModificationLog();
+    ASSERT_EQ(creationLog, lastModificationLog);
+  }
+
+  const std::string createAdminUserComment = "createAdminUser";
+  common::dataStructures::UserIdentity admin2User;
+  admin2User.setUid(m_admin2.getUid());
+  admin2User.setGid(m_admin2.getGid());
+  ASSERT_NO_THROW(catalogue->createAdminUser(m_admin1, admin2User,
+    createAdminUserComment));
+
+  {
+    std::list<common::dataStructures::AdminUser> admins;
+    ASSERT_NO_THROW(admins = catalogue->getAdminUsers(m_admin1));
+    ASSERT_EQ(2, admins.size());
+  }
 }
 
 } // namespace unitTests
