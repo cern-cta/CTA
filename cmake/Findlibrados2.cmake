02b3cd0281641a90fe791eb399fabd1576bc63bc
Steven Murray <steven.murray@cern.ch>
2016-03-15 14:52:01 +0100

    Added Findlibrados2

---
+++ cmake/Findlibrados2.cmake
@@ -0,0 +1,34 @@
+# The CERN Tape Archive(CTA) project
+# Copyright(C) 2015  CERN
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This module will set the following variables:
+#     LIBRADOS2_FOUND
+#     LIBRADOS2_INCLUDE_DIRS
+#     LIBRADOS2_LIBRARIES
+
+find_path(LIBRADOS2_INCLUDE_DIRS
+  librados.hpp
+  PATHS /usr/include/rados
+  NO_DEFAULT_PATH)
+
+find_library(LIBRADOS2_LIBRARIES
+  NAME rados
+  PATHS /usr/lib64
+  NO_DEFAULT_PATH)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(librados2 DEFAULT_MSG
+  LIBRADOS2_INCLUDE_DIRS LIBRADOS2_LIBRARIES)
