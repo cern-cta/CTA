ce7aa0a3cad52ea4e82f320d3f1fe057876db6e1
Steven Murray <Steven.Murray@cern.ch>
2016-03-15 13:39:33 +0100

    Fixed sqlite linking problems caused by new Findsqlite.cmake

--- cmake/Findsqlite.cmake
+++ cmake/Findsqlite.cmake
@@ -17,12 +17,18 @@
 # This module will set the following variables:
 #     SQLITE_FOUND
 #     SQLITE_INCLUDE_DIRS
+#     SQLITE_LIBRARIES
 
-find_path (SQLITE_INCLUDE_DIRS
+find_path(SQLITE_INCLUDE_DIRS
   sqlite3.h
   PATHS /usr/include
   NO_DEFAULT_PATH)
 
+find_library(SQLITE_LIBRARIES
+  NAME sqlite3
+  PATHS /usr/lib64
+  NO_DEFAULT_PATH)
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(sqlite DEFAULT_MSG
-  SQLITE_INCLUDE_DIRS)
+  SQLITE_INCLUDE_DIRS SQLITE_LIBRARIES)
