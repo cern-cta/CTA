09278d84c88f4130b09aef17644807d23a3948d1
Steven Murray <steven.murray@cern.ch>
2016-07-26 20:47:40 +0200

    WIP

---
+++ common/make_unique.hpp
@@ -0,0 +1,35 @@
+/*
+ * The CERN Tape Archive (CTA) project
+ * Copyright (C) 2015  CERN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+namespace cta {
+
+/**
+ * An implementation of std::make_unique() function that should be replaced by
+ * the official std library version when CTA moves to a new enough compiler.
+ *
+ * @param params The parameters to be passed to the constructor of object to be
+ * created on the heap and wrapped by an std::unique_ptr.
+ * @return The std::unique_ptr.
+ */
+template<typename T, typename... Ts> std::unique_ptr<T> make_unique(Tss&&... params) {
+  return std::unique_ptr<T>(new T(std::foward<Ts>(params)...));
+}
+
+} // namespace cta
