1c1d4b5439ece774849702be475d3103f269db5d
Steven Murray <steven.murray@cern.ch>
2016-08-03 16:33:02 +0200

    Implemented RdbmsCatalogue::modifyTapeLastWrittenLog()

--- catalogue/RdbmsCatalogue.cpp
+++ catalogue/RdbmsCatalogue.cpp
@@ -1949,7 +1949,35 @@ void RdbmsCatalogue::modifyTapeLabelLog(const common::dataStructures::SecurityId
 //------------------------------------------------------------------------------
 void RdbmsCatalogue::modifyTapeLastWrittenLog(const common::dataStructures::SecurityIdentity &cliIdentity,
   const std::string &vid, const std::string &drive) {
-  throw exception::Exception(std::string(__FUNCTION__) + " not implemented");
+  try {
+    const time_t now = time(nullptr);
+    const char *const sql =
+      "UPDATE TAPE SET "
+        "LAST_WRITE_DRIVE = :LAST_WRITE_DRIVE,"
+        "LAST_WRITE_TIME = :LAST_WRITE_TIME,"
+        "LAST_UPDATE_USER_NAME = :LAST_UPDATE_USER_NAME,"
+        "LAST_UPDATE_HOST_NAME = :LAST_UPDATE_HOST_NAME,"
+        "LAST_UPDATE_TIME = :LAST_UPDATE_TIME "
+      "WHERE "
+        "VID = :VID";
+    auto conn = m_connPool.getConn();
+    auto stmt = conn->createStmt(sql, rdbms::Stmt::AutocommitMode::ON);
+    stmt->bindString(":LAST_WRITE_DRIVE", drive);
+    stmt->bindUint64(":LAST_WRITE_TIME", now);
+    stmt->bindString(":LAST_UPDATE_USER_NAME", cliIdentity.username);
+    stmt->bindString(":LAST_UPDATE_HOST_NAME", cliIdentity.host);
+    stmt->bindUint64(":LAST_UPDATE_TIME", now);
+    stmt->bindString(":VID", vid);
+    stmt->executeNonQuery();
+
+    if(0 == stmt->getNbAffectedRows()) {
+      throw exception::UserError(std::string("Cannot modify tape ") + vid + " because it does not exist");
+    }
+  } catch(exception::UserError &) {
+    throw;
+  } catch (exception::Exception &ex) {
+    throw exception::Exception(std::string(__FUNCTION__) + " failed: " + ex.getMessage().str());
+  }
 }
 
 //------------------------------------------------------------------------------
