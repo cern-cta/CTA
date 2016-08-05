25bc1ebd7c45355a4acb8c22d66f76cde1816b2a
Steven Murray <steven.murray@cern.ch>
2016-08-05 16:17:24 +0200

    Implemented RdbmsCatalogue::modifyMountPolicyRetrieveMinRequestAge()

--- catalogue/RdbmsCatalogue.cpp
+++ catalogue/RdbmsCatalogue.cpp
@@ -3042,8 +3042,35 @@ void RdbmsCatalogue::modifyMountPolicyRetrievePriority(const common::dataStructu
 //------------------------------------------------------------------------------
 // modifyMountPolicyRetrieveMinRequestAge
 //------------------------------------------------------------------------------
-void RdbmsCatalogue::modifyMountPolicyRetrieveMinRequestAge(const common::dataStructures::SecurityIdentity &cliIdentity, const std::string &name, const uint64_t minRetrieveRequestAge) {
-  throw exception::Exception(std::string(__FUNCTION__) + " not implemented");
+void RdbmsCatalogue::modifyMountPolicyRetrieveMinRequestAge(const common::dataStructures::SecurityIdentity &cliIdentity,
+  const std::string &name, const uint64_t minRetrieveRequestAge) {
+  try {
+    const time_t now = time(nullptr);
+    const char *const sql =
+      "UPDATE MOUNT_POLICY SET "
+        "RETRIEVE_MIN_REQUEST_AGE = :RETRIEVE_MIN_REQUEST_AGE,"
+        "LAST_UPDATE_USER_NAME = :LAST_UPDATE_USER_NAME,"
+        "LAST_UPDATE_HOST_NAME = :LAST_UPDATE_HOST_NAME,"
+        "LAST_UPDATE_TIME = :LAST_UPDATE_TIME "
+      "WHERE "
+        "MOUNT_POLICY_NAME = :MOUNT_POLICY_NAME";
+    auto conn = m_connPool.getConn();
+    auto stmt = conn->createStmt(sql, rdbms::Stmt::AutocommitMode::ON);
+    stmt->bindUint64(":RETRIEVE_MIN_REQUEST_AGE", minRetrieveRequestAge);
+    stmt->bindString(":LAST_UPDATE_USER_NAME", cliIdentity.username);
+    stmt->bindString(":LAST_UPDATE_HOST_NAME", cliIdentity.host);
+    stmt->bindUint64(":LAST_UPDATE_TIME", now);
+    stmt->bindString(":MOUNT_POLICY_NAME", name);
+    stmt->executeNonQuery();
+
+    if(0 == stmt->getNbAffectedRows()) {
+      throw exception::UserError(std::string("Cannot modify mount policy ") + name + " because they do not exist");
+    }
+  } catch(exception::UserError &) {
+    throw;
+  } catch(exception::Exception &ex) {
+    throw exception::Exception(std::string(__FUNCTION__) +  " failed: " + ex.getMessage().str());
+  }
 }
 
 //------------------------------------------------------------------------------
