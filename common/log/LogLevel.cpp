5cd91c64da78c2b685d01957bc804ada9fc33471
Steven Murray <steven.murray@cern.ch>
2016-11-09 17:39:08 +0100

    Added cta::log::toLogLevel()

---
+++ common/log/LogLevel.cpp
@@ -0,0 +1,46 @@
+/*
+ * The CERN Tape Archive (CTA) project
+ * Copyright (C) 2015  CERN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common/exception/Exception.hpp"
+#include "common/log/Constants.hpp"
+
+namespace cta {
+namespace log {
+
+//------------------------------------------------------------------------------
+// toLogLevel
+//------------------------------------------------------------------------------
+int toLogLevel(const std::string &s) {
+  if(s == "EMERG")   return EMERG;
+  if(s == "ALERT")   return ALERT;
+  if(s == "CRIT")    return CRIT;
+  if(s == "ERR")     return ERR;
+  if(s == "WARNING") return WARNING;
+  if(s == "NOTICE")  return NOTICE;
+  if(s == "INFO")    return INFO;
+  if(s == "DEBUG")   return DEBUG;
+
+  // It is a convention of CTA to use syslog level of LOG_NOTICE to label
+  // user errors.
+  if(s == "USERERR") return NOTICE;
+
+  throw exception::Exception(s + " is not a valid log level");
+}
+
+} // namespace log
+} // namespace cta
---
+++ common/log/LogLevelTest.cpp
@@ -0,0 +1,91 @@
+/*
+ * The CERN Tape Archive (CTA) project
+ * Copyright (C) 2015  CERN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common/log/LogLevel.hpp"
+
+#include <gtest/gtest.h>
+
+namespace unitTests {
+
+class cta_log_LogLevelTest : public ::testing::Test {
+protected:
+
+  virtual void SetUp() {
+  }
+
+  virtual void TearDown() {
+  }
+}; // class cta_log_LogLevelTest
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_EMERG) {
+  using namespace cta::log;
+
+  ASSERT_EQ(EMERG, toLogLevel("EMERG"));
+}
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_ALERT) {
+  using namespace cta::log;
+
+  ASSERT_EQ(ALERT, toLogLevel("ALERT"));
+}
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_CRIT) {
+  using namespace cta::log;
+
+  ASSERT_EQ(CRIT, toLogLevel("CRIT"));
+}
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_ERR) {
+  using namespace cta::log;
+
+  ASSERT_EQ(ERR, toLogLevel("ERR"));
+}
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_WARNING) {
+  using namespace cta::log;
+
+  ASSERT_EQ(WARNING, toLogLevel("WARNING"));
+}
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_NOTICE) {
+  using namespace cta::log;
+
+  ASSERT_EQ(NOTICE, toLogLevel("NOTICE"));
+}
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_INFO) {
+  using namespace cta::log;
+
+  ASSERT_EQ(INFO, toLogLevel("INFO"));
+}
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_DEBUG) {
+  using namespace cta::log;
+
+  ASSERT_EQ(DEBUG, toLogLevel("DEBUG"));
+}
+
+TEST_F(cta_log_LogLevelTest, toLogLevel_USERERR) {
+  using namespace cta::log;
+
+  // It is a convention of CTA to use syslog level of LOG_NOTICE to label
+  // user errors.
+  ASSERT_EQ(NOTICE, toLogLevel("USERERR"));
+}
+
+} // namespace unitTests
