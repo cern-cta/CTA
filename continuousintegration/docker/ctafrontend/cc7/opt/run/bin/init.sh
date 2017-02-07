132f9516e676c12728d84170dbf4b1488010483b
Julien Leduc <julien.leduc@cern.ch>
2017-02-07 12:13:53 +0100

    Adding run scripts from cta-orchestration

---
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/init.sh
@@ -0,0 +1,70 @@
+#!/bin/sh 
+
+# enable cta repository from previously built artifacts
+yum-config-manager --enable cta-artifacts
+yum-config-manager --enable ceph
+
+# install needed packages
+yum -y install cta-objectstore-tools cta-doc mt-st mtx lsscsi sg3_utils cta-catalogueutils ceph-common
+yum clean packages
+
+echo "Using this configuration for library:"
+/shared/bin/init_library.sh
+cat /tmp/library-rc.sh
+. /tmp/library-rc.sh
+
+echo "Creating objectstore"
+/shared/bin/init_objectstore.sh
+. /tmp/objectstore-rc.sh
+
+if [ "$OBJECTSTORETYPE" == "file" ]; then
+  rm -fr $OBJECTSTOREURL
+  mkdir -p $OBJECTSTOREURL
+  cta-objectstore-initialize $OBJECTSTOREURL
+  chmod -R 777 $OBJECTSTOREURL
+else
+  if [[ $(rados -p $OBJECTSTOREPOOL --id $OBJECTSTOREID --namespace $OBJECTSTORENAMESPACE ls | wc -l) -gt 0 ]]; then
+    echo "Rados objectstore ${OBJECTSTOREURL} is not empty: deleting content"
+    rados -p $OBJECTSTOREPOOL --id $OBJECTSTOREID --namespace $OBJECTSTORENAMESPACE ls | xargs -itoto rados -p $OBJECTSTOREPOOL --id $OBJECTSTOREID --namespace $OBJECTSTORENAMESPACE rm toto
+  fi
+  cta-objectstore-initialize $OBJECTSTOREURL
+  echo "Rados objectstore ${OBJECTSTOREURL} content:"
+  rados -p $OBJECTSTOREPOOL --id $OBJECTSTOREID --namespace $OBJECTSTORENAMESPACE ls
+fi
+
+echo "Creating DB"
+/shared/bin/init_database.sh
+. /tmp/database-rc.sh
+
+if [ "$DATABASETYPE" == "sqlite" ]; then
+  mkdir -p $(dirname $(echo ${DATABASEURL} | cut -d: -f2))
+  echo ${DATABASEURL} >/etc/cta/cta_catalogue_db.conf
+  cta-catalogue-schema-create /etc/cta/cta_catalogue_db.conf
+  chmod -R 777 $(dirname $(echo ${DATABASEURL} | cut -d: -f2)) # needed?
+else
+  echo ${DATABASEURL} >/etc/cta/cta_catalogue_db.conf
+  cta-catalogue-schema-create /etc/cta/cta_catalogue_db.conf
+fi
+
+if [ ! $LIBRARYTYPE == "mhvtl" ]; then
+  echo "Real tapes, not labelling";
+else
+  # library management
+  # BEWARE STORAGE SLOTS START @1 and DRIVE SLOTS START @0!!
+  echo "Labelling tapes using the first drive in ${LIBRARYNAME}: ${DRIVENAMES[${driveslot}]} on /dev/${DRIVEDEVICES[${driveslot}]}:"
+  for ((i=0; i<${#TAPES[@]}; i++)); do
+    vid=${TAPES[${i}]}
+    tapeslot=$((${i}+1)) # tape slot is 1 for tape[0] and so on...
+
+    echo -n "${vid} in slot ${tapeslot} "
+    mtx -f /dev/${LIBRARYDEVICE} load ${tapeslot} ${driveslot}
+    cd /tmp
+      echo "VOL1${vid}                           CASTOR                                    3">label.file
+      dd if=label.file of=/dev/${DRIVEDEVICES[${driveslot}]} bs=80 count=1
+      mt -f /dev/${DRIVEDEVICES[${driveslot}]} rewind
+    mtx -f /dev/${LIBRARYDEVICE} unload ${tapeslot} ${driveslot}
+    echo "OK"
+  done
+fi
+
+echo "### INIT COMPLETED ###"
---
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/taped.sh
@@ -0,0 +1,78 @@
+#!/bin/sh 
+
+yum-config-manager --enable cta-artifacts
+yum-config-manager --enable ceph
+yum-config-manager --enable castor
+
+# Install missing RPMs
+yum -y install mt-st mtx lsscsi sg3_utils cta-taped cta-debuginfo castor-rmc-server ceph-common
+
+/shared/bin/init_pod.sh
+
+echo "Using this configuration for library:"
+/shared/bin/init_library.sh
+cat /tmp/library-rc.sh
+. /tmp/library-rc.sh
+
+ln -s /dev/${LIBRARYDEVICE} /dev/smc
+#/usr/bin/rmcd -f /dev/smc&
+
+mkdir -p /etc/castor
+
+tpconfig="${DRIVENAMES[${driveslot}]} ${LIBRARYNAME} /dev/${DRIVEDEVICES[${driveslot}]} smc${driveslot}"
+
+/shared/bin/init_objectstore.sh
+. /tmp/objectstore-rc.sh
+
+echo "Configuring database"
+/shared/bin/init_database.sh
+. /tmp/database-rc.sh
+
+echo ${DATABASEURL} >/etc/cta/cta_catalogue_db.conf
+
+# cta-tapserverd setup 
+# to be drop later
+  echo "${tpconfig}" > /etc/castor/TPCONFIG
+  echo "TapeServer ObjectStoreBackendPath $OBJECTSTOREURL" >/etc/castor/castor.conf
+  echo "TapeServer BufSize 5242880" >>/etc/castor/castor.conf
+  echo "TapeServer NbBufs 10" >>/etc/castor/castor.conf
+  echo "TapeServer EOSRemoteHostAndPort ${eoshost}" >>/etc/castor/castor.conf
+
+# cta-taped setup
+  echo "taped BufferCount 10" > /etc/cta/cta.conf
+  echo "general ObjectStoreURL $OBJECTSTOREURL" >> /etc/cta/cta.conf
+  echo "${tpconfig}" > /etc/cta/TPCONFIG
+
+
+####
+# configuring taped
+CTATAPEDSSS="cta_tape_server.keytab"
+
+# key generated with 'echo y | xrdsssadmin -k taped+ -u stage -g tape  add /tmp/taped.keytab'
+#echo '0 u:stage g:tape n:taped+ N:6361736405290319874 c:1481207182 e:0 f:0 k:8e2335f24cf8c7d043b65b3b47758860cbad6691f5775ebd211b5807e1a6ec84' >> /etc/cta/${CTATAPEDSSS}
+echo -n '0 u:daemon g:daemon n:ctaeos+ N:6361884315374059521 c:1481241620 e:0 f:0 k:1a08f769e9c8e0c4c5a7e673247c8561cd23a0e7d8eee75e4a543f2d2dd3fd22' > /etc/cta/${CTATAPEDSSS}
+chmod 600 /etc/cta/${CTATAPEDSSS}
+chown stage /etc/cta/${CTATAPEDSSS}
+
+cat <<EOF > /etc/sysconfig/cta-taped
+export CTA_TAPED_OPTIONS="-fl /cta-taped.log"
+
+export XrdSecPROTOCOL=sss
+
+export XrdSecSSSKT=/etc/cta/${CTATAPEDSSS}
+
+EOF
+
+. /etc/sysconfig/cta-taped
+
+
+tail -F /cta-taped.log &
+
+# cta-taped is ran with runuser to avoid a bug with Docker that prevents both
+# the setresgid(-1, 1474, -1) and setresuid(-1, 14029, -1) system calls from
+# working correctly
+runuser -c "/bin/cta-taped ${CTA_TAPED_OPTIONS}"
+
+echo "taped died"
+
+sleep infinity
