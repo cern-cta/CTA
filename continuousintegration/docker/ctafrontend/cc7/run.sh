596cb08b45c0e62350f1c5d07b2a97edeeb1122f
Julien Leduc <julien.leduc@cern.ch>
2017-02-07 09:05:50 +0100

    simplify default run.sh because we simply do not use it. Does nothing but waits for pod termination...

--- continuousintegration/docker/ctafrontend/cc7/run.sh
+++ continuousintegration/docker/ctafrontend/cc7/run.sh
@@ -1,12 +1,5 @@
 #!/bin/sh 
 
-echo "ObjectStore BackendPath $objectstore" > /etc/cta/cta-frontend.conf
-echo "Catalogue NumberOfConnections 1" >>/etc/cta/cta-frontend.conf
-
-echo ${catdb} >/etc/cta/cta_catalogue_db.conf
-
-useradd cta
-# disable kerberos5 check for the admin privileges
-  sed -i -e "s/krb5/unix/" /usr/lib64/libXrdCtaOfs.so
-runuser --shell='/bin/bash' --session-command='cd ~cta; xrootd -n cta -c /etc/xrootd/xrootd-cta.cfg -I v4' cta&
-/bin/bash
+echo "Default run command: sleeping forever"
+# sleep forever but exit immediately when pod is deleted
+exec /bin/bash -c "trap : TERM INT; sleep infinity & wait"
