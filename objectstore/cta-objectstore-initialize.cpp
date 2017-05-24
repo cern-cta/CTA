0b269d9fd0c60e91528c7002b99744387eb47fd0
Eric Cano <Eric.Cano@cern.ch>
2017-05-24 11:25:57 +0200

    Fixed another missing fetch after relock error.

--- objectstore/cta-objectstore-initialize.cpp
+++ objectstore/cta-objectstore-initialize.cpp
@@ -59,6 +59,7 @@ int main(int argc, char ** argv) {
     rel.release();
     ag.insertAndRegisterSelf();
     rel.lock(re);
+    re.fetch();
     re.addOrGetDriveRegisterPointerAndCommit(agr, el);
     re.addOrGetSchedulerGlobalLockAndCommit(agr,el);
     rel.release();
