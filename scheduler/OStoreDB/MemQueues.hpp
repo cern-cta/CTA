165ce0e84f57276a92978faafe1adf0d6ba380e2
Eric Cano <Eric.Cano@cern.ch>
2017-08-16 18:01:18 +0200

    Added vid/tapepool in logs of MemQueue<Request, Queue>::sharedAddToQueue().

--- scheduler/OStoreDB/MemQueues.hpp
+++ scheduler/OStoreDB/MemQueues.hpp
@@ -326,6 +326,13 @@ std::shared_ptr<SharedQueueLock<Queue, Request>> MemQueue<Request, Queue>::share
     }
     {
       log::ScopedParamContainer params(logContext);
+      if (typeid(Queue) == typeid(objectstore::ArchiveQueue)) {
+        params.add("type", "Archive")
+              .add("tapepool", queueIndex);
+      } else if (typeid(Queue) == typeid(objectstore::RetrieveQueue)) {
+        params.add("type", "Retrieve")
+              .add("vid", queueIndex);
+      }
       params.add("objectQueue", queue.getAddressIfSet())
             .add("jobsBefore", qJobsBefore)
             .add("jobsAfter", qJobsAfter)
