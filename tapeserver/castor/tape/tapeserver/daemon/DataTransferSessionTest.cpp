5e8bda12ce57be525d7a5e3137c0c88638ce4024
Steven Murray <Steven.Murray@cern.ch>
2017-11-09 16:44:49 +0100

    Corrected ar.diskFileID within unit-test DataTransferSessionGooddayMigration

--- tapeserver/castor/tape/tapeserver/daemon/DataTransferSessionTest.cpp
+++ tapeserver/castor/tape/tapeserver/daemon/DataTransferSessionTest.cpp
@@ -1260,7 +1260,7 @@ TEST_P(DataTransferSessionTest, DataTransferSessionGooddayMigration) {
       ar.requester.name = requester.username;
       ar.requester.group = "group";
       ar.fileSize = 1000;
-      ar.diskFileID = "x";
+      ar.diskFileID = std::to_string(fseq);
       ar.diskFileInfo.path = "y";
       ar.diskFileInfo.owner = "z";
       ar.diskFileInfo.group = "g";
