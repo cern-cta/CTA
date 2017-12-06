df620e8fa84e1ac85faab8cb4425c3087e46a149
Julien Leduc <julien.leduc@cern.ch>
2017-12-06 06:57:52 +0100

    Switching to krb5 for prepare

--- continuousintegration/orchestration/tests/archive_retrieve.sh
+++ continuousintegration/orchestration/tests/archive_retrieve.sh
@@ -35,6 +35,7 @@ echo "Launching simple_client_ar.sh on client pod"
 echo " Archiving file: xrdcp as user1"
 echo " Retrieving it as poweruser1"
 kubectl -n ${NAMESPACE} cp simple_client_ar.sh client:/root/client_ar.sh
+kubectl -n ${NAMESPACE} cp client_helper.sh client:/root/client_helper.sh
 kubectl -n ${NAMESPACE} exec client -- bash /root/simple_client_ar.sh || exit 1
 
 NB_FILES=1000
@@ -46,7 +47,6 @@ echo " Archiving ${NB_FILES} files of ${FILE_SIZE_KB}kB each"
 echo " Archiving files: xrdcp as user1"
 echo " Retrieving them as poweruser1"
 kubectl -n ${NAMESPACE} cp client_ar.sh client:/root/client_ar.sh
-kubectl -n ${NAMESPACE} cp client_helper.sh client:/root/client_helper.sh
 kubectl -n ${NAMESPACE} exec client -- bash /root/client_ar.sh -n ${NB_FILES} -s ${FILE_SIZE_KB} -p 10 -d /eos/ctaeos/preprod -v -r || exit 1
 
 exit 0
