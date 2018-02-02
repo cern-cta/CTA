b7cee8ade7b35662781542511b85425622e3c8c7
Michael Davis <michael.davis@cern.ch>
2018-02-02 15:07:44 +0100

    [kill-bash] Reinstates cta-cli.conf

--- continuousintegration/docker/ctafrontend/cc7/opt/run/bin/ctacli.sh
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/ctacli.sh
@@ -10,5 +10,11 @@ if [ ! -e /etc/buildtreeRunner ]; then
   yum -y install cta-cli cta-debuginfo
 fi
 
+cat <<EOF > /etc/cta/cta-cli.conf
+# The CTA frontend address in the form <FQDN>:<TCPPort>
+# solved by kubernetes DNS server so KIS...
+ctafrontend:10955
+EOF
+
 # sleep forever but exit immediately when pod is deleted
 exec /bin/bash -c "trap : TERM INT; sleep infinity & wait"
--- continuousintegration/docker/ctafrontend/cc7/opt/run/bin/eos_configure_preprod.sh
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/eos_configure_preprod.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 PREPROD_DIR=/eos/ctaeos/preprod
-CTA_BIN=/usr/bin/eoscta_stub
-
-mkdir -p /var/eos/wfe/bash
-cp /opt/ci/ctaeos/var/eos/wfe/bash/* /var/eos/wfe/bash/
-chmod 755 /var/eos/wfe/bash/*
 
 eos mkdir ${PREPROD_DIR}
 eos chmod 555 ${PREPROD_DIR}
 eos attr set sys.acl=g:eosusers:rwx!d,u:poweruser1:rwx+dp,u:poweruser2:rwx+dp ${PREPROD_DIR}
 
 eos attr set CTA_StorageClass=ctaStorageClass ${PREPROD_DIR}
-
 eos attr set CTA_TapeFsId=65535 ${PREPROD_DIR}
 
 eos attr set sys.workflow.closew.default="proto/cta:ctafrontend:10955 <parent/file>" ${PREPROD_DIR}
