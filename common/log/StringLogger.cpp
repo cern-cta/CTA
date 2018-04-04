fd00036c10de3ce7503c0e7e227ba306480eff1c
Steven Murray <Steven.Murray@cern.ch>
2018-04-04 17:37:34 +0200

    Updated printf comment in StringLogger::writeMsgToUnderlyingLoggingSystem()

--- common/log/StringLogger.cpp
+++ common/log/StringLogger.cpp
@@ -55,7 +55,7 @@ void StringLogger::writeMsgToUnderlyingLoggingSystem(const std::string &header, 
   m_log << headerPlusBody << std::endl;
 
   // Uncomment this to get the logs printed to stdout during unit tests.
-  // printf ("%s\n", headerPlusBody.substr(0, m_maxMsgLen).c_str());
+  // printf ("%s\n", headerPlusBody.c_str());
 }
 
 } // namespace log
