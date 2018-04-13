0e369f157d10e8e7924ca4faabdeb01d129e8aa2
Steven Murray <Steven.Murray@cern.ch>
2018-04-13 23:05:25 +0200

    Replaced workflow actions of eos_configure_preprod.sh with simply "proto".

--- continuousintegration/docker/ctafrontend/cc7/opt/run/bin/eos_configure_preprod.sh
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/eos_configure_preprod.sh
@@ -8,8 +8,8 @@ eos attr set sys.acl=g:eosusers:rwx!d,u:poweruser1:rwx+dp,u:poweruser2:rwx+dp ${
 eos attr set CTA_StorageClass=ctaStorageClass ${PREPROD_DIR}
 eos attr set CTA_TapeFsId=65535 ${PREPROD_DIR}
 
-eos attr set sys.workflow.sync::create.default="proto:ctafrontend:10955 ctafrontend" ${PREPROD_DIR}
-eos attr set sys.workflow.closew.default="proto:ctafrontend:10955 ctafrontend" ${PREPROD_DIR}
-eos attr set sys.workflow.archived.default="proto:ctafrontend:10955 ctafrontend" ${PREPROD_DIR}
-eos attr set sys.workflow.sync::prepare.default="proto:ctafrontend:10955 ctafrontend" ${PREPROD_DIR}
-eos attr set sys.workflow.sync::delete.default="proto:ctafrontend:10955 ctafrontend" ${PREPROD_DIR}
+eos attr set sys.workflow.sync::create.default="proto" ${PREPROD_DIR}
+eos attr set sys.workflow.closew.default="proto" ${PREPROD_DIR}
+eos attr set sys.workflow.archived.default="proto" ${PREPROD_DIR}
+eos attr set sys.workflow.sync::prepare.default="proto" ${PREPROD_DIR}
+eos attr set sys.workflow.sync::delete.default="proto" ${PREPROD_DIR}
