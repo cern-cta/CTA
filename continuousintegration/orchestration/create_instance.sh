ac10c74dede7ea5c72bbd80d9dd6c631ab355f1e
Steven Murray <Steven.Murray@cern.ch>
2018-04-13 23:03:51 +0200

    Replaced workflow actions of create_instance.sh with simply "proto".

--- continuousintegration/orchestration/create_instance.sh
+++ continuousintegration/orchestration/create_instance.sh
@@ -333,8 +333,8 @@ echo "Setting workflows in namespace ${instance} pod ctaeos:"
 CTA_WF_DIR=/eos/${EOSINSTANCE}/proc/cta/workflow
 for WORKFLOW in sync::create.default closew.default archived.default sync::prepare.default sync::delete.default
 do
-  echo "eos attr set sys.workflow.${WORKFLOW}=\"proto:${CTA_ENDPOINT} ctafrontend\" ${CTA_WF_DIR}"
-  kubectl --namespace=${instance} exec ctaeos -- bash -c "eos attr set sys.workflow.${WORKFLOW}=\"proto:${CTA_ENDPOINT} ctafrontend\" ${CTA_WF_DIR}"
+  echo "eos attr set sys.workflow.${WORKFLOW}=\"proto\" ${CTA_WF_DIR}"
+  kubectl --namespace=${instance} exec ctaeos -- bash -c "eos attr set sys.workflow.${WORKFLOW}=\"proto\" ${CTA_WF_DIR}"
 done
 
 
