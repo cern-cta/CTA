7c0e1f8b6920d874f1b9d92673a7a0b7e1aafb61
Steven Murray <Steven.Murray@cern.ch>
2018-04-19 11:40:04 +0200

    TimeBasedCache::getCachedValue() now returns a stale value if another thread is already determing the new fresh value

--- catalogue/TimeBasedCache.hpp
+++ catalogue/TimeBasedCache.hpp
@@ -18,9 +18,11 @@
 
 #pragma once
 
+#include "common/make_unique.hpp"
 #include "common/threading/Mutex.hpp"
 #include "common/threading/MutexLocker.hpp"
 
+#include <atomic>
 #include <map>
 #include <time.h>
 
@@ -39,6 +41,34 @@ public:
   }
 
   /**
+   * When an instance of this class is destroyed, it will clear the
+   * the specified std::atomic_flag.
+   */
+  class AtomicFlagClearer {
+  public:
+    /**
+     * Constructor.
+     *
+     * @param flag The std::atomic_flag to be cleared.
+     */
+    AtomicFlagClearer(std::atomic_flag &flag): m_flag(flag) {
+    }
+
+    /**
+     * Destructor that clears the std::atomic_flag.
+     */
+    ~AtomicFlagClearer() {
+      m_flag.clear();
+    }
+
+  private:
+    /**
+     * The std::atomic_flag to be cleared.
+     */
+    std::atomic_flag &m_flag;
+  };
+
+  /**
    * Get the cached value corresponing to the specified key.
    *
    * This method updates the cache when necessary.
@@ -46,24 +76,41 @@ public:
   template<typename Callable> Value getCachedValue(const Key &key, const Callable &getNonCachedValue) {
     const time_t now = time(nullptr);
 
-    threading::MutexLocker lock(m_mutex);
+    threading::MutexLocker cacheLock(m_mutex);
     const auto cacheItor = m_cache.find(key);
     const bool cacheHit = m_cache.end() != cacheItor;
 
     if(cacheHit) {
-      const auto &cachedValue = cacheItor->second;
+      auto &cachedValue = *(cacheItor->second);
+      threading::MutexLocker cachedValueLock(cachedValue.mutex);
       const time_t ageSecs = now - cachedValue.timestamp;
-      if (m_maxAgeSecs >= ageSecs) {
+
+      if (m_maxAgeSecs >= ageSecs) { // Cached value is fresh
         return cachedValue.value;
-      } else {
-        const auto &newValue = TimestampedValue(now, getNonCachedValue());
-        m_cache[key] = newValue;
-        return newValue.value;
+      } else { // Cached value is stale
+        // If another thread is already updating the value
+        if(cachedValue.valueBeingUpdated.test_and_set()) {
+          // Return the stale value
+          return cachedValue.value;
+        } else {
+          // Release locks so other threads can read
+          cachedValueLock.unlock();
+          cacheLock.unlock();
+
+          // Update and return the new value
+          AtomicFlagClearer clearer(cachedValue.valueBeingUpdated);
+          const auto newValue = getNonCachedValue();
+
+          threading::MutexLocker updateCachedValueLock(cachedValue.mutex);
+          cachedValue.value = newValue;
+          cachedValue.timestamp = ::time(nullptr);
+          return cachedValue.value;
+        }
       }
     } else { // No cache hit
-      const auto &newValue = TimestampedValue(now, getNonCachedValue());
-      m_cache[key] = newValue;
-      return newValue.value;
+      const auto emplaceResult = m_cache.emplace(std::make_pair(key,
+        make_unique<TimestampedValue>(now, getNonCachedValue())));
+      return emplaceResult.first->second->value;
     }
   }
 
@@ -80,18 +127,9 @@ private:
   threading::Mutex m_mutex;
 
   /**
-   * A timestamped tape copy to tape pool map.
+   * A timestamped value.
    */
   struct TimestampedValue {
-    /**
-     * The timestamp.
-     */
-    time_t timestamp;
-
-    /**
-     * The value.
-     */
-    Value value;
 
     /**
      * Constructor.
@@ -105,16 +143,34 @@ private:
      * @param t The timestamp.
      * @param v The value.
      */
-    TimestampedValue(const time_t t, const Value &v):
-      timestamp(t),
-      value(v) {
+    TimestampedValue(const time_t t, const Value &v): timestamp(t), value(v) {
     }
+
+    /**
+     * Mutex used to protect the timestamp and value.
+     */
+    threading::Mutex mutex;
+
+    /**
+     * The timestamp.
+     */
+    time_t timestamp;
+
+    /**
+     * The value.
+     */
+    Value value;
+
+    /**
+     * Flag used to ensure only one thread at a time is updating the value.
+     */
+    std::atomic_flag valueBeingUpdated = ATOMIC_FLAG_INIT;
   }; // struct TimestampedValue
 
   /**
    * The cache.
    */
-  std::map<Key, TimestampedValue> m_cache;
+  std::map<Key, std::unique_ptr<TimestampedValue> > m_cache;
 }; // class TimeBasedCache
 
 } // namespace catalogue
