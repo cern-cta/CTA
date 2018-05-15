11807582afe2e2229825dd382509e3c6832ef503
Eric Cano <Eric.Cano@cern.ch>
2018-05-15 15:35:41 +0200

    Change synchronisation to ensure order of jobs in the queue.
    The test relies on this order.

--- scheduler/OStoreDB/OStoreDBTest.cpp
+++ scheduler/OStoreDB/OStoreDBTest.cpp
@@ -131,8 +131,8 @@ TEST_P(OStoreDBTest, getBatchArchiveJob) {
     afqc.mountPolicy.retrieveMinRequestAge = 1;
     afqc.mountPolicy.maxDrivesAllowed = 1;
     osdbi.queueArchive("testInstance", ar, afqc, lc);
+    osdbi.waitSubthreadsComplete();
   }
-  osdbi.waitSubthreadsComplete();
   // Delete the first job from the queue, change owner of second.
   // They will be automatically skipped when getting jobs
   std::string aqAddr;
