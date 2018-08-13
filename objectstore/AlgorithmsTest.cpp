0e64c913b17237f3c8ef6b2c463f2f991c0acead
Michael Davis <michael.davis@cern.ch>
2018-08-13 15:57:55 +0200

    [os-generic-queues] Removes ASSERT_NO_THROW from sess.execute()

--- objectstore/AlgorithmsTest.cpp
+++ objectstore/AlgorithmsTest.cpp
@@ -17,8 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define STDOUT_LOGGING
-
 #include "RootEntry.hpp"
 #include "AgentReference.hpp"
 #include "Agent.hpp"
