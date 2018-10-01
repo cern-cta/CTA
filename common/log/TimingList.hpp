add199b55d7d566a68b2b4509ad5502e41b09b33
Eric Cano <Eric.Cano@cern.ch>
2018-10-01 16:35:07 +0200

    Added support for increment to timings in TimingsLists.

--- common/log/TimingList.hpp
+++ common/log/TimingList.hpp
@@ -32,10 +32,13 @@ class ScopedParamContainer;
 
 class TimingList: public std::list<std::tuple<std::string, double>> {
 public:
-  void insert(const std::string &, double);
+  void insertOrIncrement(const std::string &, double);
+  void insOrIncAndReset(const std::string&, utils::Timer &);
   void insertAndReset(const std::string&, utils::Timer &);
   TimingList & operator+= (const TimingList &);
   void addToLog(ScopedParamContainer &);
+private:
+  double & at(const std::string&);
 };
 
 }} // namespace cta::log
