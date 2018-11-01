4e8b380154467059a36598b138d6fe8e296e6621
Steven Murray <Steven.Murray@cern.ch>
2018-11-01 17:36:29 +0100

    Moved OracleCatalogue::deleteArchiveFile to RdbmsCatalogue because Oracle, MySQL and PostgreSQL support 'SELECT FOR UPDATE'.  SQLite is the odd one out in that it does not provide support.

--- catalogue/OracleCatalogue.hpp
+++ catalogue/OracleCatalogue.hpp
@@ -62,27 +62,6 @@ public:
   ~OracleCatalogue() override;
 
   /**
-   * Deletes the specified archive file and its associated tape copies from the
-   * catalogue.
-   *
-   * Please note that the name of the disk instance is specified in order to
-   * prevent a disk instance deleting an archive file that belongs to another
-   * disk instance.
-   *
-   * Please note that this method is idempotent.  If the file to be deleted does
-   * not exist in the CTA catalogue then this method returns without error.
-   *
-   * @param instanceName The name of the instance from where the deletion request
-   * originated
-   * @param archiveFileId The unique identifier of the archive file.
-   * @param lc The log context.
-   * @return The metadata of the deleted archive file including the metadata of
-   * the associated and also deleted tape copies.
-   */
-  void deleteArchiveFile(const std::string &diskInstanceName, const uint64_t archiveFileId, log::LogContext &lc)
-    override;
-
-  /**
    * Returns a unique archive ID that can be used by a new archive file within
    * the catalogue.
    *
