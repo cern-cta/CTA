9b802c6def5a8f5f341d799d9e8a3dc81903a793
Julien Leduc <julien.leduc@cern.ch>
2019-01-16 09:55:22 +0100

    Adding -d option to delete_instance.sh to discard pod logs if these are too big or useless

--- continuousintegration/orchestration/delete_instance.sh
+++ continuousintegration/orchestration/delete_instance.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 LOG_DIR=/tmp  # by default writte tar files of logs to /tmp
+collectlogs=1   # by default collect all container logs
 
 usage() { cat <<EOF 1>&2
-Usage: $0 -n <namespace> [-d <log_directory>]
+Usage: $0 -n <namespace> [-d <log_directory>] [-D]
+
+Options:
+  -D    Discard logs (by default logs are collected before deleting the instance)
 EOF
 exit 1
 }
 
-while getopts "n:d:" o; do
+while getopts "n:d:D" o; do
     case "${o}" in
         n)
             instance=${OPTARG}
             ;;
         d)
             LOG_DIR=${OPTARG}
+            ;;
+        D)
+            collectlogs=0
             ;;
         *)
             usage
@@ -52,25 +59,31 @@ for backtracefile in $(kubectl --namespace ${instance} exec ctacli -- bash -c 'f
   kubectl --namespace ${instance} exec ctacli -- cat ${backtracefile}
 done
 
-###
-# Collect the logs
-###
 
-# First in a temporary directory so that we can get the logs on the gitlab runner if something bad happens
-# indeed if the system test fails, artifacts are not collected for the build
-tmpdir=$(mktemp --tmpdir=${LOG_DIR} -d -t ${instance}_delete_XXXX)
-echo "Collecting stdout logs of pods to ${tmpdir}"
-for podcontainer in "init -c ctainit" "client -c client" "ctacli -c ctacli" "ctaeos -c mgm" "ctafrontend -c ctafrontend" "kdc -c kdc" "tpsrv01 -c taped" "tpsrv01 -c rmcd" "tpsrv02 -c taped" "tpsrv02 -c rmcd"; do
-  kubectl --namespace ${instance} logs ${podcontainer} > ${tmpdir}/$(echo ${podcontainer} | sed -e 's/ -c /-/').log
-done
-kubectl --namespace ${instance} exec ctacli -- tar -C /mnt/logs -zcf - . > ${tmpdir}/varlog.tgz
+###
+# Collect the logs?
+###
+if [ $collectlogs == 1 ] ; then
+  # First in a temporary directory so that we can get the logs on the gitlab runner if something bad happens
+  # indeed if the system test fails, artifacts are not collected for the build
+  tmpdir=$(mktemp --tmpdir=${LOG_DIR} -d -t ${instance}_delete_XXXX)
+  echo "Collecting stdout logs of pods to ${tmpdir}"
+  for podcontainer in "init -c ctainit" "client -c client" "ctacli -c ctacli" "ctaeos -c mgm" "ctafrontend -c ctafrontend" "kdc -c kdc" "tpsrv01 -c taped" "tpsrv01 -c rmcd" "tpsrv02 -c taped" "tpsrv02 -c rmcd"; do
+    kubectl --namespace ${instance} logs ${podcontainer} > ${tmpdir}/$(echo ${podcontainer} | sed -e 's/ -c /-/').log
+  done
+  kubectl --namespace ${instance} exec ctacli -- tar -C /mnt/logs -zcf - . > ${tmpdir}/varlog.tgz
 
-if [ ! -z "${CI_PIPELINE_ID}" ]; then
-	# we are in the context of a CI run => save artifacts in the directory structure of the build
-        echo "Saving logs as artifacts"
-	mkdir -p ../../pod_logs/${instance}
-	cp -r ${tmpdir}/* ../../pod_logs/${instance}
+  if [ ! -z "${CI_PIPELINE_ID}" ]; then
+    # we are in the context of a CI run => save artifacts in the directory structure of the build
+    echo "Saving logs as artifacts"
+    mkdir -p ../../pod_logs/${instance}
+    cp -r ${tmpdir}/* ../../pod_logs/${instance}
+  fi
+else
+  # Do not Collect logs
+  echo "Discarding logs for the current run"
 fi
+
 
 echo "Deleting ${instance} instance"
 
