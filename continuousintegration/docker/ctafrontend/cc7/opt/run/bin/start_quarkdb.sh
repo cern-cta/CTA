d69e923b3469b4f166d825d963829e602b54a41c
Julien Leduc <julien.leduc@cern.ch>
2019-02-08 09:33:41 +0100

    Adding an indirection level in the directory structure to allow a dedicated PV for quarkdb

--- continuousintegration/docker/ctafrontend/cc7/opt/run/bin/start_quarkdb.sh
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/start_quarkdb.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# I need 2 level of directories for quarkdb:
+# /var/lib/<possible PV mount point>/<directory owned by xrootd>
+# this way I can either use:
+# - a PV that mounts a dedicated SSD for long tests
+# - a directory in the container for shorter ones
+
 QUARKDB_CONFIG=/etc/config/eos/xrootd-quarkdb.cfg
 
-QUARKDB_DIRECTORY=$(cat ${QUARKDB_CONFIG} | grep redis.database | sed -e 's/.*redis.database\ \+//')
+QUARKDB_DIRECTORY=$(cat ${QUARKDB_CONFIG} | grep redis.database | sed -e 's/.*redis.database\ \+//;s%/\ *$%%')
+
+QUARKDB_SUBDIRECTORY=$(echo ${QUARKDB_DIRECTORY} | sed -e 's%/[^/]\+/*\ *$%%')
+
+# make sure the first level of directory exists if there is no mounted PV
+mkdir -p ${QUARKDB_SUBDIRECTORY}
 
 yum-config-manager --enable eos-quarkdb
 
