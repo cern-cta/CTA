dce657737d552d8c40bdc85cdd6209be4a66853f
Michael Davis <michael.davis@cern.ch>
2019-05-09 18:31:24 +0200

    [checksum] Implements unit tests

--- common/checksum/ChecksumBlobTest.cpp
+++ common/checksum/ChecksumBlobTest.cpp
@@ -30,61 +30,136 @@ protected:
   }
 };
 
-TEST_F(cta_ChecksumBlobTest, default_constructor) {
-  using namespace cta::checksum;
-
-  const ChecksumBlob checksumBlob;
-
-  ASSERT_EQ(checksumBlob.length(), 0);
-}
-
 TEST_F(cta_ChecksumBlobTest, checksum_types) {
   using namespace cta::checksum;
+  using namespace cta::exception;
 
   ChecksumBlob checksumBlob;
-  checksumBlob.insert(NONE, "");
-  checksumBlob.insert(ADLER32, "12\0004");
+  ASSERT_EQ(checksumBlob.empty(), true);
+  ASSERT_EQ(checksumBlob.size(), 0);
+
+  // valid insertions
+  checksumBlob.insert(NONE, "");                      // 0 bits
+  ASSERT_EQ(checksumBlob.size(), 1);
+  ASSERT_EQ(checksumBlob.empty(), false);
+  checksumBlob.insert(ADLER32, "1234");               // 32 bits
+  ASSERT_EQ(checksumBlob.size(), 2);
+  checksumBlob.insert(CRC32, "1234");                 // 32 bits
+  ASSERT_EQ(checksumBlob.size(), 3);
+  checksumBlob.insert(CRC32C, "1234");                // 32 bits
+  ASSERT_EQ(checksumBlob.size(), 4);
+  checksumBlob.insert(MD5, "1234567890123456");       // 128 bits
+  ASSERT_EQ(checksumBlob.size(), 5);
+  checksumBlob.insert(SHA1, "12345678901234567890");  // 160 bits
+  ASSERT_EQ(checksumBlob.size(), 6);
+
+  // check each of the checksums in turn
+  ASSERT_EQ(checksumBlob.contains(NONE, ""), true);
+  ASSERT_EQ(checksumBlob.contains(ADLER32, "1234"), true);
+  ASSERT_EQ(checksumBlob.contains(CRC32, "1234"), true);
+  ASSERT_EQ(checksumBlob.contains(CRC32C, "1234"), true);
+  ASSERT_EQ(checksumBlob.contains(MD5, "1234567890123456"), true);
+  ASSERT_EQ(checksumBlob.contains(SHA1, "12345678901234567890"), true);
+
+  // invalid insertions
+  ASSERT_THROW(checksumBlob.insert(NONE, "0"),                ChecksumValueMismatch);
+  ASSERT_THROW(checksumBlob.insert(ADLER32, "12345"),         ChecksumValueMismatch);
+  ASSERT_THROW(checksumBlob.insert(CRC32, "123"),             ChecksumValueMismatch);
+  ASSERT_THROW(checksumBlob.insert(CRC32C, ""),               ChecksumValueMismatch);
+  ASSERT_THROW(checksumBlob.insert(MD5, "1234"),              ChecksumValueMismatch);
+  ASSERT_THROW(checksumBlob.insert(SHA1, "1234567890123456"), ChecksumValueMismatch);
+  ASSERT_THROW(checksumBlob.insert(MD5, 0x12345678),          ChecksumTypeMismatch);
+  ASSERT_THROW(checksumBlob.insert(SHA1, 0x12345678),         ChecksumTypeMismatch);
+
+  // Blob types are different
+  ChecksumBlob checksumBlob2, checksumBlob3;
+  checksumBlob2.insert(NONE, "");
+  checksumBlob3.insert(ADLER32, "1234");
+  ASSERT_THROW(checksumBlob2.validate(checksumBlob3), ChecksumTypeMismatch);
+  ASSERT_NE(checksumBlob2,checksumBlob3);
+
+  // Blob sizes are different
+  checksumBlob3.insert(NONE, "");
+  ASSERT_THROW(checksumBlob2.validate(checksumBlob3), ChecksumBlobSizeMismatch);
+  ASSERT_NE(checksumBlob2,checksumBlob3);
+
+  // Blob values are different
+  checksumBlob2 = checksumBlob;
+  checksumBlob2.insert(ADLER32, 0x0a0b0c0d);
+  ASSERT_THROW(checksumBlob.validate(checksumBlob2), ChecksumValueMismatch);
+  ASSERT_NE(checksumBlob,checksumBlob2);
+
+  // Clear the blob
+  checksumBlob.clear();
+  ASSERT_EQ(checksumBlob.empty(), true);
+  ASSERT_EQ(checksumBlob.size(), 0);
+}
+
+TEST_F(cta_ChecksumBlobTest, hex_to_byte_array) {
+  using namespace cta::checksum;
+
+  // Check some limiting cases
+  ASSERT_EQ(ChecksumBlob::ByteArrayToHex(ChecksumBlob::HexToByteArray("0")), "00");
+  ASSERT_EQ(ChecksumBlob::ByteArrayToHex(ChecksumBlob::HexToByteArray("0xFFFFFFFF")), "ffffffff");
+  ASSERT_EQ(ChecksumBlob::ByteArrayToHex(ChecksumBlob::HexToByteArray("0X10a0BFC")), "010a0bfc");
+  ASSERT_EQ(ChecksumBlob::ByteArrayToHex(ChecksumBlob::HexToByteArray("000a000b000c000d000e000f000abcdef1234567890")),
+                                                                     "0000a000b000c000d000e000f000abcdef1234567890");
 }
 
 TEST_F(cta_ChecksumBlobTest, adler32) {
   using namespace cta::checksum;
+  using namespace cta::exception;
 
   ChecksumBlob checksumBlob;
+  ASSERT_THROW(checksumBlob.validate(ADLER32, "invalid"), ChecksumTypeMismatch);
+  ASSERT_EQ(checksumBlob.contains(ADLER32, "invalid"), false);
+
   checksumBlob.insert(ADLER32, 0x0A141E28);
-  ASSERT_EQ(checksumBlob.length(), 1);
+  ASSERT_EQ(checksumBlob.size(), 1);
+  ASSERT_THROW(checksumBlob.validate(ADLER32, "invalid"), ChecksumValueMismatch);
+  ASSERT_EQ(checksumBlob.contains(ADLER32, "invalid"), false);
 
+  // Check internal representation
   std::string bytearray = checksumBlob.at(ADLER32);
-
   ASSERT_EQ(4, bytearray.length());
-  ASSERT_EQ(static_cast<uint8_t>(10), bytearray[0]);
-  ASSERT_EQ(static_cast<uint8_t>(20), bytearray[1]);
-  ASSERT_EQ(static_cast<uint8_t>(30), bytearray[2]);
-  ASSERT_EQ(static_cast<uint8_t>(40), bytearray[3]);
-}
+  ASSERT_EQ(static_cast<uint8_t>(0x28), bytearray[0]);
+  ASSERT_EQ(static_cast<uint8_t>(0x1E), bytearray[1]);
+  ASSERT_EQ(static_cast<uint8_t>(0x14), bytearray[2]);
+  ASSERT_EQ(static_cast<uint8_t>(0x0A), bytearray[3]);
 
-TEST_F(cta_ChecksumBlobTest, invalid_checksums) {
-  using namespace cta::checksum;
+  // Check we can convert back to the original hex value
+  ASSERT_EQ(ChecksumBlob::ByteArrayToHex(bytearray), "0a141e28");
 
-  ChecksumBlob checksumBlob;
-  checksumBlob.insert(NONE, "none");
-  checksumBlob.insert(MD5, 0x0A141E28);
-}
+  // Check construction by bytearray yields the same result
+  ChecksumBlob checksumBlob2;
+  checksumBlob2.insert(ADLER32, bytearray);
+  ASSERT_EQ(checksumBlob2.size(), 1);
+  ASSERT_EQ(checksumBlob, checksumBlob2);
 
-TEST_F(cta_ChecksumBlobTest, bytearray_to_hex) {
-  using namespace cta::checksum;
+  // Check construction by hex string yields the same result
+  ChecksumBlob checksumBlob3;
+  checksumBlob3.insert(ADLER32, ChecksumBlob::HexToByteArray("0x0A141E28"));
+  ASSERT_EQ(checksumBlob3.size(), 1);
 
-  ChecksumBlob checksumBlob;
-  checksumBlob.insert(ADLER32, 0x00120abc);
-
-  std::stringstream ss;
-  ss << checksumBlob;
-  ASSERT_EQ(ss.str(), "[ {\"ADLER32\",0x00120abc} ]");
+  // Check alternate hex string yields the same result
+  ChecksumBlob checksumBlob4;
+  checksumBlob4.insert(ADLER32, ChecksumBlob::HexToByteArray("a141e28"));
+  ASSERT_EQ(checksumBlob4.size(), 1);
+  ASSERT_EQ(checksumBlob, checksumBlob4);
 }
 
 TEST_F(cta_ChecksumBlobTest, serialize_deserialize) {
   using namespace cta::checksum;
 
   ChecksumBlob checksumBlob1;
+
+  checksumBlob1.insert(NONE, "");                      // 0 bits
+  checksumBlob1.insert(ADLER32, "1234");               // 32 bits
+  checksumBlob1.insert(CRC32, "1234");                 // 32 bits
+  checksumBlob1.insert(CRC32C, "1234");                // 32 bits
+  checksumBlob1.insert(MD5, "1234567890123456");       // 128 bits
+  checksumBlob1.insert(SHA1, "12345678901234567890");  // 160 bits
+
   auto bytearray = checksumBlob1.serialize();
 
   ChecksumBlob checksumBlob2;
