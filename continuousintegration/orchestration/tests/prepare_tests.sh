23b70cbf7bccb99c41768aa192b6e471b1631bcd
Victor Kotlyar <Victor.Kotlyar@cern.ch>
2019-06-20 11:19:30 +0200

    Make gitlab CI tests stop after catalogue verification errors.

--- continuousintegration/orchestration/tests/prepare_tests.sh
+++ continuousintegration/orchestration/tests/prepare_tests.sh
@@ -67,6 +67,10 @@ NB_TAPEDRIVES_IN_USE=${#TAPEDRIVES_IN_USE[@]}
 echo "Preparing CTA configuration for tests"
   # verify the catalogue DB schema
     kubectl --namespace ${NAMESPACE} exec ctafrontend -- cta-catalogue-schema-verify /etc/cta/cta-catalogue.conf
+    if [ $? -ne 0 ]; then
+      echo "ERROR: failed to verify the catalogue DB schema"
+      exit 1
+    fi
   kubectl --namespace ${NAMESPACE} exec ctafrontend -- cta-catalogue-admin-user-create /etc/cta/cta-catalogue.conf --username ctaadmin1 -m "docker cli"
   for ((i=0; i<${#TAPEDRIVES_IN_USE[@]}; i++)); do
     kubectl --namespace ${NAMESPACE} exec ctacli -- cta-admin logicallibrary add \
