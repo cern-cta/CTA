9de8548ef98ffda42843b771c33ccc8d3d223798
Michael Davis <michael.davis@cern.ch>
2019-06-26 16:17:36 +0200

    [cta-admin] Displays new uid/gid and checksum formats

--- cmdline/EosCtaStub.cpp
+++ cmdline/EosCtaStub.cpp
@@ -24,7 +24,8 @@
 
 #include <XrdSsiPbLog.hpp>
 
-#include "common/dataStructures/FrontendReturnCode.hpp"
+#include <common/dataStructures/FrontendReturnCode.hpp>
+#include <common/checksum/ChecksumBlobSerDeser.hpp>
 #include "CtaFrontendApi.hpp"
 
 
@@ -109,10 +110,15 @@ void base64Decode(cta::eos::Notification &notification, const std::string &argva
          notification.mutable_file()->mutable_mtime()->set_nsec(stoi(val.substr(pt_pos+1)));
       }
       else if(key == "size") notification.mutable_file()->set_size(stoi(val));
-#ifdef CTA_CHECKSUMS_DONE
-      else if(key == "xstype") notification.mutable_file()->mutable_cks()->set_type(val);
-      else if(key == "xs") notification.mutable_file()->mutable_cks()->set_value(val);
-#endif
+      else if(key == "xs")
+      {
+         // In principle it's possible to set the full checksum blob with multiple checksums of different
+         // types, but this is not currently supported in eos_wfe_stub. It's only possible to set one
+         // checksum, which is assumed to be of type ADLER32.
+         auto cs = notification.mutable_file()->mutable_csb()->add_cs();
+         cs->set_type(cta::common::ChecksumBlob::Checksum::ADLER32);
+         cs->set_value(cta::checksum::ChecksumBlob::HexToByteArray(val));
+      }
       else if(key == "mode") notification.mutable_file()->set_mode(stoi(val));
       else if(key == "file") notification.mutable_file()->set_lpath(val);
       else {
@@ -222,10 +228,15 @@ void fillNotification(cta::eos::Notification &notification, int argc, const char
       else if(argstr == "--diskfileowner")       notification.mutable_file()->mutable_owner()->set_uid(std::stoi(argval));
       else if(argstr == "--diskfilegroup")       notification.mutable_file()->mutable_owner()->set_gid(std::stoi(argval));
       else if(argstr == "--size")                notification.mutable_file()->set_size(std::stoi(argval));
-#ifdef CTA_CHECKSUMS_DONE
-      else if(argstr == "--checksumtype")        notification.mutable_file()->mutable_cks()->set_type(argval);
-      else if(argstr == "--checksumvalue")       notification.mutable_file()->mutable_cks()->set_value(argval);
-#endif
+      else if(argstr == "--checksumvalue")
+      {
+         // In principle it's possible to set the full checksum blob with multiple checksums of different
+         // types, but this is not currently supported in eos_wfe_stub. It's only possible to set one
+         // checksum, which is assumed to be of type ADLER32.
+         auto cs = notification.mutable_file()->mutable_csb()->add_cs();
+         cs->set_type(cta::common::ChecksumBlob::Checksum::ADLER32);
+         cs->set_value(cta::checksum::ChecksumBlob::HexToByteArray(argval));
+      }
       else if(argstr == "--diskfilepath")        notification.mutable_file()->set_lpath(argval);
       else if(argstr == "--storageclass")        {
          google::protobuf::MapPair<std::string,std::string> sc("CTA_StorageClass", argval);
