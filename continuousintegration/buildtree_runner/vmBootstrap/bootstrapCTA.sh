503e09c7aea1f88545dc5b712cf7a70aa0ad12ee
Eric Cano <Eric.Cano@cern.ch>
2019-07-04 09:58:44 +0200

    Updated vm bootstraping scripts.

--- continuousintegration/buildtree_runner/vmBootstrap/bootstrapCTA.sh
+++ continuousintegration/buildtree_runner/vmBootstrap/bootstrapCTA.sh
@@ -18,17 +18,16 @@ mkdir -p ~/CTA-build-srpm
 (cd ~/CTA-build-srpm && cmake -DPackageOnly:Bool=true ../CTA; make cta_srpm)
 
 echo Installing repos
-sudo yum-config-manager --add-repo=../../docker/ctafrontend/cc7/etc/yum.repos.d/eos.repo
-sudo yum-config-manager --add-repo=ceph-internal.repo
-sudo yum-config-manager --add-repo=../../docker/ctafrontend/cc7/etc/yum.repos.d/cta-ci.repo
-sudo yum-config-manager --add-repo=../../docker/ctafrontend/cc7/etc/yum.repos.d/castor.repo
+for r in `ls ../../docker/ctafrontend/cc7/etc/yum.repos.d/*.repo` ceph-internal.repo; do
+  sudo yum-config-manager --add-repo=$r
+done
 sudo yum install -y yum-plugin-priorities
 
 echo Adding versionlock for xrootd:
 sudo cp ../../docker/ctafrontend/cc7/etc/yum/pluginconf.d/versionlock.list /etc/yum/pluginconf.d/versionlock.list
 
 echo Installing build dependencies
-sudo yum-builddep -y ~/CTA-build-srpm/RPM/SRPMS/cta-0-0.src.rpm --enablerepo=cernonly --nogpgcheck
+sudo yum-builddep -y ~/CTA-build-srpm/RPM/SRPMS/cta-0-1.src.rpm --enablerepo=cernonly --nogpgcheck
 
 echo Installing mhvtl
 sudo yum install -y mhvtl-utils kmod-mhvtl mhvtl-utils --enablerepo=castor
