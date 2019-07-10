2d583c3e2b07bbac0b6b6c3d1a5c8f4bf5203226
Steven Murray <Steven.Murray@cern.ch>
2019-07-10 11:56:45 +0200

    Added sys.workflow.sync::abort_prepare.default

--- continuousintegration/docker/ctafrontend/cc7/opt/run/bin/eos_configure_preprod.sh
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/eos_configure_preprod.sh
@@ -13,6 +13,7 @@ eos attr set sys.workflow.sync::closew.default="proto" ${PREPROD_DIR}
 eos attr set sys.workflow.sync::archived.default="proto" ${PREPROD_DIR}
 eos attr set sys.workflow.sync::archive_failed.default="proto" ${PREPROD_DIR}
 eos attr set sys.workflow.sync::prepare.default="proto" ${PREPROD_DIR}
+eos attr set sys.workflow.sync::abort_prepare.default="proto" ${PREPROD_DIR}
 eos attr set sys.workflow.sync::closew.retrieve_written="proto" ${PREPROD_DIR}
 eos attr set sys.workflow.sync::retrieve_failed.default="proto" ${PREPROD_DIR}
 eos attr set sys.workflow.sync::delete.default="proto" ${PREPROD_DIR}
