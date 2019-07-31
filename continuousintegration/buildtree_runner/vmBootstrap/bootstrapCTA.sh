8ab81aa065d91ae39334f337ad1ff6d04e161c2a
Eric Cano <Eric.Cano@cern.ch>
2019-07-31 16:58:44 +0200

    Improved/udpated buildtree vm bootstrap.
    Old SRPM is now cleaned up.
    We automatically fetch all existing repos from the docker tree.
    Fixed package version.

--- continuousintegration/buildtree_runner/vmBootstrap/bootstrapCTA.sh
+++ continuousintegration/buildtree_runner/vmBootstrap/bootstrapCTA.sh
@@ -14,21 +14,22 @@ chmod +x ~/CTA/.git/hooks/post-checkout
 cp ~/CTA/.git/hooks/post-checkout ~/CTA/.git/hooks/post-merge
 
 echo Creating source rpm
+rm -rf ~/CTA-build-srpm
 mkdir -p ~/CTA-build-srpm
 (cd ~/CTA-build-srpm && cmake -DPackageOnly:Bool=true ../CTA; make cta_srpm)
 
 echo Installing repos
-sudo yum-config-manager --add-repo=../../docker/ctafrontend/cc7/etc/yum.repos.d/eos.repo
+for r in `ls -1 ../../docker/ctafrontend/cc7/etc/yum.repos.d/*.repo`; do
+  yum-config-manager --add-repo=$r
+done
 sudo yum-config-manager --add-repo=ceph-internal.repo
-sudo yum-config-manager --add-repo=../../docker/ctafrontend/cc7/etc/yum.repos.d/cta-ci.repo
-sudo yum-config-manager --add-repo=../../docker/ctafrontend/cc7/etc/yum.repos.d/castor.repo
 sudo yum install -y yum-plugin-priorities
 
 echo Adding versionlock for xrootd:
 sudo cp ../../docker/ctafrontend/cc7/etc/yum/pluginconf.d/versionlock.list /etc/yum/pluginconf.d/versionlock.list
 
 echo Installing build dependencies
-sudo yum-builddep -y ~/CTA-build-srpm/RPM/SRPMS/cta-0-0.src.rpm --enablerepo=cernonly --nogpgcheck
+sudo yum-builddep -y ~/CTA-build-srpm/RPM/SRPMS/cta-0-1.src.rpm --enablerepo=cernonly --nogpgcheck
 
 echo Installing mhvtl
 sudo yum install -y mhvtl-utils kmod-mhvtl mhvtl-utils --enablerepo=castor
