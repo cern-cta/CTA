ac9841146e0221b133027a09d2b6a40304059b3e
Michael Davis <michael.davis@cern.ch>
2019-08-20 16:00:48 +0200

    [ci] Configure migration tools in client pod

--- continuousintegration/docker/ctafrontend/cc7/opt/run/bin/client.sh
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/client.sh
@@ -7,19 +7,30 @@ if [ ! -e /etc/buildtreeRunner ]; then
   yum-config-manager --enable ceph
 
   # Install missing RPMs
-  yum -y install cta-cli cta-debuginfo xrootd-client eos-client jq
+  yum -y install cta-cli cta-debuginfo xrootd-client eos-client cta-migration-tools jq
 
   ## Keep this temporary fix that may be needed if going to protobuf3-3.5.1 for CTA
   # Install eos-protobuf3 separately as eos is OK with protobuf3 but cannot use it..
   # Andreas is fixing eos-(client|server) rpms to depend on eos-protobuf3 instead
   # yum -y install eos-protobuf3
-
 fi
 
 cat <<EOF > /etc/cta/cta-cli.conf
 # The CTA frontend address in the form <FQDN>:<TCPPort>
 # solved by kubernetes DNS server so KIS...
 cta.endpoint ctafrontend:10955
+EOF
+
+cat <<EOF >/etc/cta/castor-migration.conf
+castor.db_login               oracle:castor/<password>@castor
+castor.json                   true
+castor.max_num_connections    1
+castor.batch_size             100
+castor.prefix                 /castor/cern.ch
+eos.dry_run                   false
+eos.prefix                    /eos/grpc
+eos.endpoint                  eoscta:50051
+eos.token                     migrationtesttoken
 EOF
 
 if [ "-${CI_CONTEXT}-" == '-nosystemd-' ]; then
