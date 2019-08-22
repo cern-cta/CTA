4cd49cca8c2d4f363f94e97c8dd7b96f15fae045
Michael Davis <michael.davis@cern.ch>
2019-08-22 16:38:04 +0200

    [ci] Changes eos vid add gateway

--- continuousintegration/docker/ctafrontend/cc7/opt/run/bin/ctaeos-mgm.sh
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/ctaeos-mgm.sh
@@ -213,7 +213,8 @@ fi
   # Configure gRPC interface:
   #
   # 1. Map requests to EOS virtual identities
-  eos -r 0 0 vid add gateway eoscta grpc
+  #eos -r 0 0 vid add gateway eoscta grpc
+  eos -r 0 0 vid add gateway [:1] grpc
   # 2. Add authorisation key
   #
   # Note: EOS_AUTH_KEY must be the same as the one specified in client.sh
