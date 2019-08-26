163960e867025f12ebcf8133ed4af90496dc9ce3
Michael Davis <michael.davis@cern.ch>
2019-08-26 10:26:49 +0200

    [ci] Reduce injected fileid < uint32_max

--- continuousintegration/orchestration/tests/grpc_dir_inject.sh
+++ continuousintegration/orchestration/tests/grpc_dir_inject.sh
@@ -57,12 +57,13 @@ json-pretty-print.sh ${TMPFILE}
 rm ${TMPFILE}
 ${EOS_CMD} ls -l ${EOS_PREFIX}
 ${EOS_CMD} fileinfo ${EOS_PREFIX}/test_dir1
-#${EOS_CMD} rmdir ${EOS_PREFIX}/test_dir1
+${EOS_CMD} attr ls ${EOS_PREFIX}/test_dir1
+${EOS_CMD} quota ls ${EOS_PREFIX}/test_dir1
 /usr/bin/eos -r 1000 1000 root://${EOSINSTANCE} rmdir ${EOS_PREFIX}/test_dir1
 
 # Create directory with self-assigned file id -- should succeed
 echoc $LT_BLUE "Creating directory with self-assigned file id"
-${EOS_TEST_DIR_INJECT} --fileid 9876543210 --path ${CASTOR_PREFIX}/test_dir2 >${TMPFILE}
+${EOS_TEST_DIR_INJECT} --fileid 987654321 --path ${CASTOR_PREFIX}/test_dir2 >${TMPFILE}
 [ $? -eq 0 ] || error "Creating directory with self-assigned file id failed"
 json-pretty-print.sh ${TMPFILE}
 rm ${TMPFILE}
