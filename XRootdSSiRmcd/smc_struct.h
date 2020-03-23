23c9077b2faa64e1097cff98f9fddbcd752441a6
Steven Murray <Steven.Murray@cern.ch>
2020-03-23 11:28:45 +0100

    Revert "Removed the no longer used XRootdSSiRmcd sub-directory"
    This reverts commit 20c4f60a52c98a8cd4365d5b95b2f1bf2b5a1c42.

---
+++ XRootdSSiRmcd/smc_struct.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 1998-2002 by CERN/IT/PDP/DM
+ * All rights reserved
+ */
+
+#pragma once
+
+#include "Castor_limits.h"
+
+struct robot_info {
+  char inquiry[32];
+  int transport_start;
+  int transport_count;
+  int slot_start;
+  int slot_count;
+  int port_start;
+  int port_count;
+  int device_start;
+  int device_count;
+};
+
+struct extended_robot_info {
+  int     smc_fd;
+  char    smc_ldr[CA_MAXRBTNAMELEN+1];
+  struct robot_info robot_info;
+};
+
+struct smc_element_info {
+  int element_address;
+  int element_type;
+  int state;
+  unsigned char asc;
+  unsigned char ascq;
+  int flags;
+  int source_address;
+  char name[9];
+};
+
+struct smc_status {
+  unsigned char asc;
+  unsigned char ascq;
+  int save_errno;
+  int rc;		/* return code from send_scsi_cmd */
+  unsigned char sensekey;
+  int skvalid;	/* sense key is valid */
+};
+
