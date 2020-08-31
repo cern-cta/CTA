385044c5cea9bb156ae02ac2a043ff9a60eec4e2
Cedric Caffy <cedric.caffy@cern.ch>
2020-08-31 17:45:56 +0200

    [SKIP CI] WIP InterpolationFilePositionEstimatorTest

--- tapeserver/castor/tape/tapeserver/RAO/RAOTest.cpp
+++ tapeserver/castor/tape/tapeserver/RAO/RAOTest.cpp
@@ -17,21 +17,55 @@
  */
 #include <gtest/gtest.h>
 
+#include <vector>
+#include <map>
+
+#include "InterpolationFilePositionEstimator.hpp"
 
 namespace unitTests {
-  class castor_tape_tapeserver_rao_RAOTest: public ::testing::Test {
+  
+  using namespace castor::tape::tapeserver;
+  
+  class RAOTestEnvironment : public ::testing::Environment {
+  public:
+    static std::vector<drive::endOfWrapPosition> getEndOfWrapPositions() {
+      std::vector<drive::endOfWrapPosition> ret;
+      ret.push_back({0,208310,0});
+      ret.push_back({1,416271,0});
+      ret.push_back({2,624562,0});
+      return ret;
+    }
+    
+    static cta::catalogue::MediaType getLTO7MMediaType() {
+      cta::catalogue::MediaType mediaType;
+      mediaType.name = "LTO7M";
+      mediaType.minLPos = 2696;
+      mediaType.maxLPos = 171097;
+      return mediaType;
+    }
+  };
+  
+  class RAOTest: public ::testing::Test {
   protected:
-
+    
     void SetUp() {
+      
     }
 
     void TearDown() {
     }
 
   }; 
+
+  TEST_F(RAOTest,InterpolationFilePositionEstimatorTest){
+    
+    
+    std::vector<drive::endOfWrapPosition> eowPositions = RAOTestEnvironment::getEndOfWrapPositions();
+    cta::catalogue::MediaType mediaType = RAOTestEnvironment::getLTO7MMediaType();
   
-  TEST_F(castor_tape_tapeserver_rao_RAOTest,InterpolationFilePositionEstimatorTest){
-    //TODO
+    rao::InterpolationFilePositionEstimator estimator(eowPositions,mediaType);
+    //TODO : GENERATE vector of cta::RetrieveJob
+    
     ASSERT_TRUE(true);
   }
   
