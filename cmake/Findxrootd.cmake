ea9a2eef963aad9632a4d68f95efc90d4b460d77
Steven Murray <Steve.Murray@cern.ch>
2020-09-02 17:43:18 +0200

    Added XROOTD_XRDSSI_LIB to cmake/Findxrootd.cmake

--- cmake/Findxrootd.cmake
+++ cmake/Findxrootd.cmake
@@ -8,9 +8,9 @@
 #                              words the header files that do not contribute to
 #                              the xrootd ABI.
 # XROOTD_XRDCL_LIB           - location of the XrdCl library
+# XROOTD_XRDPOSIX_LIB        - location of the XrdPosix library
 # XROOTD_XRDSERVER_LIB       - location of the XrdServer library
 # XROOTD_XRDUTILS_LIB        - location of the XrdUtils library
-# XROOTD_XRDPOSIX_LIB        - location of the XrdPosix library
 
 # Be silent if XROOTD_INCLUDE_DIR is already cached
 if (XROOTD_INCLUDE_DIR)
@@ -26,21 +26,25 @@ find_path (XROOTD_PRIVATE_INCLUDE_DIR XrdOss/XrdOssApi.hh
 )
 
 find_library (XROOTD_XRDCL_LIB XrdCl)
-find_library (XROOTD_XRDSERVER_LIB XrdServer)
-find_library (XROOTD_XRDUTILS_LIB XrdUtils)
 find_library (XROOTD_XRDPOSIX_LIB XrdPosixPreload)
+find_library (XROOTD_XRDSERVER_LIB XrdServer)
+find_library (XROOTD_XRDSSI_LIB NAMES XrdSsi-4 XrdSsi-5)
+find_library (XROOTD_XRDUTILS_LIB XrdUtils)
 
 message (STATUS "XROOTD_INCLUDE_DIR         = ${XROOTD_INCLUDE_DIR}")
 message (STATUS "XROOTD_PRIVATE_INCLUDE_DIR = ${XROOTD_PRIVATE_INCLUDE_DIR}")
 message (STATUS "XROOTD_XRDCL_LIB           = ${XROOTD_XRDCL_LIB}")
-message (STATUS "XROOTD_XRDSERVER_LIB       = ${XROOTD_XRDSERVER_LIB}")
 message (STATUS "XROOTD_XRDPOSIX_LIB        = ${XROOTD_XRDPOSIX_LIB}")
+message (STATUS "XROOTD_XRDSERVER_LIB       = ${XROOTD_XRDSERVER_LIB}")
+message (STATUS "XROOTD_XRDSSI_LIB          = ${XROOTD_XRDSSI_LIB}")
+message (STATUS "XROOTD_XRDUTILS_LIB        = ${XROOTD_XRDUTILS_LIB}")
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (xrootd DEFAULT_MSG 
   XROOTD_INCLUDE_DIR
   XROOTD_PRIVATE_INCLUDE_DIR
   XROOTD_XRDCL_LIB
+  XROOTD_XRDPOSIX_LIB
   XROOTD_XRDSERVER_LIB
-  XROOTD_XRDUTILS_LIB
-  XROOTD_XRDPOSIX_LIB)
+  XROOTD_XRDSSI_LIB
+  XROOTD_XRDUTILS_LIB)
