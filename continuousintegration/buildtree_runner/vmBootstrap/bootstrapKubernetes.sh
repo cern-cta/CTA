1b83027a852accc7a2847b8fd4e35a466a8aff86
Volodymyr Yurchenko <volodymyr.yurchenko@cern.ch>
2020-09-04 11:06:37 +0200

    Do not fail k8s setup if there is no firewalld

--- continuousintegration/buildtree_runner/vmBootstrap/bootstrapKubernetes.sh
+++ continuousintegration/buildtree_runner/vmBootstrap/bootstrapKubernetes.sh
@@ -33,8 +33,8 @@ dns_clusterip='10.254.199.254'
 #dns_domainname=`hostname -d`
 dns_domainname=cluster.local
 docker_ipaddress=`ifconfig docker0  | perl -e 'while(<>) { if (/inet\s+(\S+)\s/) { print $1."\n"; } }'`
-sudo systemctl stop firewalld
-sudo systemctl disable firewalld
+sudo systemctl stop firewalld || true
+sudo systemctl disable firewalld || true
 #my_ipaddress=127.0.0.1
 upper_level_dns=`perl -e 'while (<>) { if ($_=~ /nameserver (.*)/) { print $1."\n"; exit }}' < /etc/resolv.conf`
 
