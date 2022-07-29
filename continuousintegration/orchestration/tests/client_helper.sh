7b9f17921000e3168ece87d9ed9543cc5c07f742
Joao Afonso <joao.afonso@cern.ch>
2022-07-29 18:08:49 +0200

    Added some system tests

--- continuousintegration/orchestration/tests/client_helper.sh
+++ continuousintegration/orchestration/tests/client_helper.sh
@@ -14,6 +14,10 @@
 #               submit itself to any jurisdiction.
 
 
+################################################################
+# Helper functions - KRB5
+################################################################
+
 ###
 # Helper functions for tests running on client pod.
 #
@@ -92,4 +96,150 @@ eosadmin_kinit() {
 eosadmin_kdestroy() {
   KRB5CCNAME=/tmp/${EOSADMIN_USER}/krb5cc_0 kdestroy
   eosadmin_klist
+}
+
+################################################################
+# Helper functions - Requests
+################################################################
+
+# Pass list of files waiting for archival
+
+wait_for_archive () {
+
+  EOS_INSTANCE=$1
+  SECONDS_PASSED=0
+  WAIT_FOR_ARCHIVED_FILE_TIMEOUT=90
+
+  while test $(($# - 1)) != $(echo "${@:2}" | tr " " "\n" | xargs -iFILE eos root://${EOS_INSTANCE} info FILE | awk '{print $4;}' | grep tape | wc -l); do
+    echo "Waiting for files to be archived to tape: seconds passed = ${SECONDS_PASSED}"
+    sleep 1
+    let SECONDS_PASSED=SECONDS_PASSED+1
+
+    if test ${SECONDS_PASSED} == ${WAIT_FOR_ARCHIVED_FILE_TIMEOUT}; then
+      echo "ERROR: Timed out after ${WAIT_FOR_ARCHIVED_FILE_TIMEOUT} seconds waiting for files to be archived to tape"
+      exit 1
+    fi
+  done
+
+}
+
+# Pass list of files waiting for retrieval
+
+wait_for_retrieve () {
+
+  EOS_INSTANCE=$1
+  SECONDS_PASSED=0
+  WAIT_FOR_RETRIEVED_FILE_TIMEOUT=90
+  while test $(($# - 1)) != $(echo "${@:2}" | tr " " "\n" | xargs -iFILE eos root://${EOS_INSTANCE} info FILE | awk '{print $4;}' | grep -F "default.0" | wc -l); do
+    echo "Waiting for files to be retrieved from tape: Seconds passed = ${SECONDS_PASSED}"
+    sleep 1
+    let SECONDS_PASSED=SECONDS_PASSED+1
+
+    if test ${SECONDS_PASSED} == ${WAIT_FOR_RETRIEVED_FILE_TIMEOUT}; then
+      echo "Timed out after ${WAIT_FOR_RETRIEVED_FILE_TIMEOUT} seconds waiting for files to be retrieved from tape"
+      exit 1
+    fi
+  done
+
+}
+
+# Pass list of files waiting for eviction
+
+wait_for_evict () {
+
+  EOS_INSTANCE=$1
+  SECONDS_PASSED=0
+  WAIT_FOR_EVICTED_FILE_TIMEOUT=90
+  while test 0 != $(echo "${@:2}" | tr " " "\n" | xargs -iFILE eos root://${EOS_INSTANCE} info FILE | awk '{print $4;}' | grep -F "default.0" | wc -l); do
+    echo "Waiting for files to be evicted from disk: Seconds passed = ${SECONDS_PASSED}"
+    sleep 1
+    let SECONDS_PASSED=SECONDS_PASSED+1
+
+    if test ${SECONDS_PASSED} == ${WAIT_FOR_EVICTED_FILE_TIMEOUT}; then
+      echo "Timed out after ${WAIT_FOR_EVICTED_FILE_TIMEOUT} seconds waiting for files to be evicted from disk"
+      exit 1
+    fi
+  done
+
+}
+
+# Wait for tape change
+
+wait_for_tape_state() {
+
+  SECONDS_PASSED=0
+  WAIT_FOR_EVICTED_FILE_TIMEOUT=90
+  echo "Waiting for tape $1 state to change to $2: Seconds passed = ${SECONDS_PASSED}"
+  while test $2 != $(admin_cta --json tape ls --vid $1 | jq -r '.[] | .state'); do
+    sleep 1
+    let SECONDS_PASSED=SECONDS_PASSED+1
+    echo "Waiting for tape $1 state to change to $2: Seconds passed = ${SECONDS_PASSED}"
+
+    if test ${SECONDS_PASSED} == ${WAIT_FOR_EVICTED_FILE_TIMEOUT}; then
+      echo "Timed out after ${WAIT_FOR_EVICTED_FILE_TIMEOUT} seconds waiting for tape $1 state to change to $2"
+      exit 1
+    fi
+  done
+
+}
+
+# Pass "UP" or "DOWN" as argument
+
+put_all_drives () {
+
+  NEXT_STATE=$1
+  [ "$1" = "UP" ] && PREV_STATE="DOWN" || PREV_STATE="UP"
+  next_state=$(echo $NEXT_STATE | awk '{print tolower($0)}')
+  prev_state=$(echo $PREV_STATE | awk '{print tolower($0)}')
+
+  # Put all tape drives up/down
+  INITIAL_DRIVES_STATE=`admin_cta --json dr ls`
+  echo INITIAL_DRIVES_STATE:
+  echo ${INITIAL_DRIVES_STATE} | jq -r '.[] | [ .driveName, .driveStatus] | @tsv' | column -t
+  echo -n "Will put $next_state those drives : "
+  drivesToModify=`echo ${INITIAL_DRIVES_STATE} | jq -r ".[].driveName"`
+  echo $drivesToModify
+  for d in `echo $drivesToModify`; do
+    admin_cta drive $next_state $d --reason "PUTTING DRIVE $NEXT_STATE FOR TESTS"
+  done
+
+  echo "$(date +%s): Waiting for the drives to be $next_state"
+  SECONDS_PASSED=0
+  WAIT_FOR_DRIVES_TIMEOUT=$((10))
+  while [[ $SECONDS_PASSED < $WAIT_FOR_DRIVES_TIMEOUT ]]; do
+    sleep 1
+    oneStatusRemaining=0
+    for d in `echo $drivesToModify`; do
+      status=`admin_cta --json drive ls | jq -r ". [] | select(.driveName == \"$d\") | .driveStatus"`
+      if [[ $NEXT_STATE == "DOWN" ]]; then
+        # Anything except DOWN is not acceptable
+        if [[ $status != "DOWN" ]]; then
+          oneStatusRemaining=1
+        fi;
+      else
+        # Only DOWN is not OK. Starting, Unmounting, Running == UP
+        if [[ $status == "DOWN" ]]; then
+          oneStatusRemaining=1
+        fi;
+      fi;
+    done
+    if [[ $oneStatusRemaining -eq 0 ]]; then
+      echo "Drives : $drivesToModify are $next_state"
+      break;
+    fi
+    echo -n "."
+    SECONDS_PASSED=$SECONDS_PASSED+1
+    if [[ $SECONDS_PASSED -gt $WAIT_FOR_DRIVES_TIMEOUT ]]; then
+      die "ERROR: Timeout reach for trying to put all drives $next_state"
+    fi
+  done
+
+}
+
+put_all_drives_up () {
+  put_all_drives "UP"
+}
+
+put_all_drives_down () {
+  put_all_drives "DOWN"
 }
