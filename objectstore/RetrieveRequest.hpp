624966b77c05f20ae5918fdd32eb33076d7aa8c9
Joao Afonso <joao.afonso@cern.ch>
2022-09-27 13:48:54 +0200

    Fixed protobuf errors caused by putting back 'force_disabled_tape' flag

--- objectstore/RetrieveRequest.hpp
+++ objectstore/RetrieveRequest.hpp
@@ -172,6 +172,7 @@ public:
       rrri.set_file_buffer_url(fileBufferURL);
       rrri.set_repack_request_address(repackRequestAddress);
       rrri.set_fseq(fSeq);
+      rrri.set_force_disabled_tape(false); // TODO: To remove after REPACKING state is fully deployed
       if(rrri.has_has_user_provided_file()){
 	rrri.set_has_user_provided_file(hasUserProvidedFile);
       }
