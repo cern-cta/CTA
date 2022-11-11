1da3e10b7927a078969745e8218d4745f2cf830c
lwardena <lasse.tjernaes.wardenaer@cern.ch>
2022-11-11 09:06:35 +0100

    Edit option opt_inaccessible

--- cmdline/CtaAdminCmdParse.hpp
+++ cmdline/CtaAdminCmdParse.hpp
@@ -655,10 +655,10 @@ const std::map<cmd_key_t, cmd_val_t> cmdOptions = {
         opt_full.optional(), opt_fidfile.optional(), opt_all.optional(), opt_state.optional(), opt_fromcastor.optional() }},
    /*----------------------------------------------------------------------------------------------------*/
    {{ AdminCmd::CMD_TAPEFILE,             AdminCmd::SUBCMD_LS    },
-      { opt_inaccessible, opt_vid.optional(), opt_instance.optional(), opt_fid.optional(), opt_fidfile.optional(),
+      { opt_vid.optional(), opt_instance.optional(), opt_fid.optional(), opt_fidfile.optional(),
         opt_lookupns.optional(), opt_archivefileid.optional() }},
    {{ AdminCmd::CMD_TAPEFILE,             AdminCmd::SUBCMD_CH    },
-      { opt_vid.optional(), opt_instance.optional(), opt_fid.optional(), opt_fidfile.optional(),
+      { opt_inaccessible, opt_vid.optional(), opt_instance.optional(), opt_fid.optional(), opt_fidfile.optional(),
         opt_lookupns.optional(), opt_archivefileid.optional() }},
    {{ AdminCmd::CMD_TAPEFILE,             AdminCmd::SUBCMD_RM    },
      { opt_vid, opt_instance.optional(), opt_fid.optional(), opt_archivefileid.optional(), opt_reason }},
