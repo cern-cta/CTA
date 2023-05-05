49dc98c1888edb2f3701ee5cd2dfe42269ca66c9
lwardena <lasse.tjernaes.wardenaer@cern.ch>
2023-05-05 16:19:25 +0200

    Add physical library data structure

---
+++ common/dataStructures/PhysicalLibrary.hpp
@@ -0,0 +1,55 @@
+/*
+ * @project      The CERN Tape Archive (CTA)
+ * @copyright    Copyright © 2021-2022 CERN
+ * @license      This program is free software, distributed under the terms of the GNU General Public
+ *               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
+ *               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
+ *               option) any later version.
+ *
+ *               This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *               WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *               PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *               In applying this licence, CERN does not waive the privileges and immunities
+ *               granted to it by virtue of its status as an Intergovernmental Organization or
+ *               submit itself to any jurisdiction.
+ */
+
+#pragma once
+
+#include <list>
+#include <map>
+#include <optional>
+#include <stdint.h>
+#include <string>
+
+#include "common/dataStructures/EntryLog.hpp"
+
+namespace cta {
+namespace common {
+namespace dataStructures {
+
+/**
+ * The logical library is an attribute of both drives and tapes, it declares
+ * which tapes can be mounted in which drives
+ */
+struct LogicalLibrary {
+  std::string name;
+  std::string manufacturer;
+  std::string model;
+  std::optional<std::string> type;
+  std::optional<std::string> guiUrl;
+  std::optional<std::string> webcamUrl;
+  std::optional<std::string> location;
+  uint64_t nb_physical_cartridge_slots;
+  std::optional<uint64_t> nb_available_cartridge_slots;
+  uint64_t nb_physical_drive_slots;
+  EntryLog creationLog;
+  EntryLog lastModificationLog;
+  std::string comment;
+
+}; // struct LogicalLibrary
+
+} // namespace dataStructures
+} // namespace common
+} // namespace cta
