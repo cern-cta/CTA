a9976d6fdcc710359b284f2e8c13c36f1ce0adc5
lwardena <lasse.tjernaes.wardenaer@cern.ch>
2023-05-08 09:52:22 +0200

    Add RdbmsPhysicalLibraryCatalogue.hpp

---
+++ catalogue/rdbms/RdbmsPhysicalLibraryCatalogue.hpp
@@ -0,0 +1,113 @@
+/*
+ * @project      The CERN Tape Archive (CTA)
+ * @copyright    Copyright © 2022 CERN
+ * @license      This program is free software, distributed under the terms of the GNU General Public
+ *               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
+ *               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
+ *               option) any later version.
+ *
+ *               This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ *               WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *               PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ *               In applying this licence, CERN does not waive the privileges and immunities
+ *               granted to it by virtue of its status as an Intergovernmental Organization or
+ *               submit itself to any jurisdiction.
+ */
+
+#pragma once
+
+#include <list>
+#include <memory>
+#include <optional>
+#include <string>
+
+#include "catalogue/interfaces/PhysicalLibraryCatalogue.hpp"
+
+namespace cta {
+
+namespace rdbms {
+class Conn;
+class ConnPool;
+}
+
+namespace log {
+class Logger;
+}
+
+namespace catalogue {
+
+class RdbmsCatalogue;
+
+class RdbmsPhysicalLibraryCatalogue: public PhysicalLibraryCatalogue {
+public:
+  ~RdbmsPhysicalLibraryCatalogue() override = default;
+
+  void createPhysicalLibrary(const common::dataStructures::SecurityIdentity &admin, const common::dataStructures::PhysicalLibrary pl) override;
+
+  void deletePhysicalLibrary(const std::string &name) override;
+
+  std::list<common::dataStructures::PhysicalLibrary> getLogicalLibraries() const override;
+
+  void modifyPhysicalLibraryName(const common::dataStructures::SecurityIdentity &admin,
+   const std::string &currentName, const std::string &newName) override;
+
+  void modifyPhysicalLibraryManufacturer(const common::dataStructures::SecurityIdentity &admin,
+   const std::string& name, const std::string &manufacturer) override;
+
+  void modifyPhysicalLibraryModel(const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const std::string &model) override;
+
+  void modifyPhysicalLibraryType     (const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const std::string &type) override;
+
+  void modifyPhysicalLibraryGuiUrl   (const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const std::string &guiUrl) override;
+
+  void modifyPhysicalLibraryWebcamUrl(const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const std::string &webcamUrl) override;
+
+  void modifyPhysicalLibraryLocation (const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const std::string &location) override;
+
+  void modifyPhysicalLibraryNbPhysicalCartridgeSlots  (const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const uint64_t &nbPhysicalCartridgeSlots) override;
+
+  void modifyPhysicalLibraryNbAvailableCartridgeSlots (const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const uint64_t &nbAvailableCartridgeSlots) override;
+
+  void modifyPhysicalLibraryNbPhysicalDriveSlots (const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const uint64_t &nbPhysicalDriveSlots) override;
+
+  void modifyPhysicalLibraryComment (const common::dataStructures::SecurityIdentity &admin,
+   const std::string &name, const std::string &comment) override;
+
+protected:
+  RdbmsPhysicalLibraryCatalogue(log::Logger &log, std::shared_ptr<rdbms::ConnPool> connPool,
+    RdbmsCatalogue *rdbmsCatalogue);
+
+  /**
+   * Returns a unique physical library ID that can be used by a new physical
+   * library within the catalogue.
+   *
+   * This method must be implemented by the sub-classes of RdbmsCatalogue
+   * because different database technologies propose different solution to the
+   * problem of generating ever increasing numeric identifiers.
+   *
+   * @param conn The database connection.
+   * @return a unique logical library ID that can be used by a new physical
+   * library within the catalogue.
+   */
+  virtual uint64_t getNextPhysicalLibraryId(rdbms::Conn &conn) const = 0;
+
+private:
+  log::Logger &m_log;
+  std::shared_ptr<rdbms::ConnPool> m_connPool;
+  RdbmsCatalogue *m_rdbmsCatalogue;
+
+  friend class RdbmsTapeCatalogue;
+  std::optional<uint64_t> getPhysicalLibraryId(rdbms::Conn &conn, const std::string &name) const;
+};
+
+} // namespace catalogue
+} // namespace cta
