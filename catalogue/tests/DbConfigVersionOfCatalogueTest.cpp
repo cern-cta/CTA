3837f1b4100311b1e474ab10782a9050ba25333e
lwardena <lasse.tjernaes.wardenaer@cern.ch>
2023-05-11 10:36:19 +0200

    Add tests for adding a physical library

--- catalogue/tests/DbConfigVersionOfCatalogueTest.cpp
+++ catalogue/tests/DbConfigVersionOfCatalogueTest.cpp
@@ -25,6 +25,7 @@
 #include "catalogue/tests/modules/DriveStateCatalogueTest.hpp"
 #include "catalogue/tests/modules/FileRecycleLogCatalogueTest.hpp"
 #include "catalogue/tests/modules/LogicalLibraryCatalogueTest.hpp"
+#include "catalogue/tests/modules/PhysicalLibraryCatalogueTest.hpp"
 #include "catalogue/tests/modules/MediaTypeCatalogueTest.hpp"
 #include "catalogue/tests/modules/MountPolicyCatalogueTest.hpp"
 #include "catalogue/tests/modules/RequesterActivityMountRuleTest.hpp"
@@ -56,6 +57,7 @@ INSTANTIATE_TEST_CASE_P(DbConfigFile, cta_catalogue_RequesterActivityMountRuleTe
 INSTANTIATE_TEST_CASE_P(DbConfigFile, cta_catalogue_RequesterMountRuleTest, ::testing::Values(&g_catalogueFactoryForUnitTests));
 INSTANTIATE_TEST_CASE_P(DbConfigFile, cta_catalogue_RequesterGroupMountRuleTest, ::testing::Values(&g_catalogueFactoryForUnitTests));
 INSTANTIATE_TEST_CASE_P(DbConfigFile, cta_catalogue_LogicalLibraryTest, ::testing::Values(&g_catalogueFactoryForUnitTests));
+INSTANTIATE_TEST_CASE_P(DbConfigFile, cta_catalogue_PhysicalLibraryTest, ::testing::Values(&g_catalogueFactoryForUnitTests));
 INSTANTIATE_TEST_CASE_P(DbConfigFile, cta_catalogue_TapeFileTest, ::testing::Values(&g_catalogueFactoryForUnitTests));
 INSTANTIATE_TEST_CASE_P(DbConfigFile, cta_catalogue_FileRecycleLogTest, ::testing::Values(&g_catalogueFactoryForUnitTests));
 INSTANTIATE_TEST_CASE_P(DbConfigFile, cta_catalogue_DriveConfigTest, ::testing::Values(&g_catalogueFactoryForUnitTests));
--- catalogue/tests/modules/PhysicalLibraryCatalogueTest.cpp
+++ catalogue/tests/modules/PhysicalLibraryCatalogueTest.cpp
@@ -47,4 +47,59 @@ void cta_catalogue_PhysicalLibraryTest::TearDown() {
   m_catalogue.reset();
 }
 
+TEST_P(cta_catalogue_PhysicalLibraryTest, createPhysicalLibraryMandatoryFields) {
+  ASSERT_TRUE(m_catalogue->PhysicalLibrary()->getPhysicalLibraries().empty());
+
+  m_catalogue->PhysicalLibrary()->createPhysicalLibrary(m_admin, m_physicalLibrary1);
+
+  const auto libs = m_catalogue->PhysicalLibrary()->getPhysicalLibraries();
+
+  ASSERT_EQ(1, libs.size());
+
+  const cta::common::dataStructures::PhysicalLibrary lib = libs.front();
+  ASSERT_EQ(m_physicalLibrary1.name, lib.name);
+  ASSERT_EQ(m_physicalLibrary1.manufacturer, lib.manufacturer);
+  ASSERT_EQ(m_physicalLibrary1.model, lib.model);
+  ASSERT_EQ(m_physicalLibrary1.nbPhysicalCartridgeSlots, lib.nbPhysicalCartridgeSlots);
+  ASSERT_EQ(m_physicalLibrary1.nbPhysicalDriveSlots, lib.nbPhysicalDriveSlots);
+
+  const cta::common::dataStructures::EntryLog creationLog = lib.creationLog;
+  ASSERT_EQ(m_admin.username, creationLog.username);
+  ASSERT_EQ(m_admin.host, creationLog.host);
+
+  const cta::common::dataStructures::EntryLog lastModificationLog = lib.lastModificationLog;
+  ASSERT_EQ(creationLog, lastModificationLog);
+
+  m_catalogue->PhysicalLibrary()->deletePhysicalLibrary(m_physicalLibrary1.name);
+}
+
+TEST_P(cta_catalogue_PhysicalLibraryTest, createPhysicalLibraryAllFields) {
+  m_catalogue->PhysicalLibrary()->createPhysicalLibrary(m_admin, m_physicalLibrary2);
+
+  const auto libs = m_catalogue->PhysicalLibrary()->getPhysicalLibraries();
+
+  ASSERT_EQ(1, libs.size());
+
+  const cta::common::dataStructures::PhysicalLibrary lib = libs.front();
+  ASSERT_EQ(m_physicalLibrary2.name, lib.name);
+  ASSERT_EQ(m_physicalLibrary2.manufacturer, lib.manufacturer);
+  ASSERT_EQ(m_physicalLibrary2.model, lib.model);
+  ASSERT_EQ(m_physicalLibrary2.type.value(), lib.type.value());
+  ASSERT_EQ(m_physicalLibrary2.guiUrl.value(), lib.guiUrl.value());
+  ASSERT_EQ(m_physicalLibrary2.webcamUrl.value(), lib.webcamUrl.value());
+  ASSERT_EQ(m_physicalLibrary2.location.value(), lib.location.value());
+  ASSERT_EQ(m_physicalLibrary2.nbPhysicalCartridgeSlots, lib.nbPhysicalCartridgeSlots);
+  ASSERT_EQ(m_physicalLibrary2.nbAvailableCartridgeSlots.value(), lib.nbAvailableCartridgeSlots.value());
+  ASSERT_EQ(m_physicalLibrary2.nbPhysicalDriveSlots, lib.nbPhysicalDriveSlots);
+
+  const cta::common::dataStructures::EntryLog creationLog = lib.creationLog;
+  ASSERT_EQ(m_admin.username, creationLog.username);
+  ASSERT_EQ(m_admin.host, creationLog.host);
+
+  const cta::common::dataStructures::EntryLog lastModificationLog = lib.lastModificationLog;
+  ASSERT_EQ(creationLog, lastModificationLog);
+
+  m_catalogue->PhysicalLibrary()->deletePhysicalLibrary(m_physicalLibrary2.name);
+}
+
 }  // namespace unitTests
