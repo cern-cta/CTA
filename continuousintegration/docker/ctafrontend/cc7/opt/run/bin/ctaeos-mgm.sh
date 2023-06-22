50737635f5b5ca0d14dfbb1c47b2a708cac00866
Jorge Camarero Vera <jorge.camarero@cern.ch>
2023-06-22 11:05:57 +0200

    Debug package in frontend

--- continuousintegration/docker/ctafrontend/cc7/opt/run/bin/ctaeos-mgm.sh
+++ continuousintegration/docker/ctafrontend/cc7/opt/run/bin/ctaeos-mgm.sh
@@ -31,6 +31,9 @@ fi
 # Install missing RPMs
 yum -y install eos-client eos-server xrootd-client xrootd-debuginfo xrootd-server cta-cli cta-debuginfo sudo logrotate cta-fst-gcd
 
+yum info xrootd* | grep Name -A 3
+yum info eos* | grep Name -A 3
+
 ## Keep this temporary fix that may be needed if going to protobuf3-3.5.1 for CTA
 # Install eos-protobuf3 separately as eos is OK with protobuf3 but cannot use it..
 # yum -y install eos-protobuf3
