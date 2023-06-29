e8b668190cc99e8a46d173a9020f35971aa8ab27
Joao Afonso <joao.afonso@cern.ch>
2023-06-29 15:59:23 +0200

    Improving coverage of eos evict system tests

--- continuousintegration/orchestration/tests/evict_tests.sh
+++ continuousintegration/orchestration/tests/evict_tests.sh
@@ -78,9 +78,9 @@ else
   echo "'eos evict' command failed as expected"
 fi
 
-# 3.2. Check that evict --fsid <fsid> fails when there is no tape replica
-echo "Testing 'eos root://${EOS_INSTANCE} evict --fsid ${DISK_FSID} ${TEMP_FILE}'..."
-KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --fsid ${DISK_FSID} ${TEMP_FILE}
+# 3.2. Check that evict --force --fsid <fsid> fails when there is no tape replica
+echo "Testing 'eos root://${EOS_INSTANCE} evict --force --fsid ${DISK_FSID} ${TEMP_FILE}'..."
+KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --force --fsid ${DISK_FSID} ${TEMP_FILE}
 if [ $? -eq 0 ]; then
   error "'eos evict' command succeeded where it should have failed"
 else
@@ -118,7 +118,46 @@ else
   echo "Tape replica created, as expected"
 fi
 
-# 7. Setting up dummy spaces and file systems for testing
+# 7. Staging file multiple times
+NB_RETRIEVES=3
+echo "Retrieving ${TEMP_FILE} to disk ${NB_RETRIEVES} times..."
+# We need the -s as we are staging the files from tape (see xrootd prepare definition)
+for ((retrieve_req=0; retrieve_req < ${NB_RETRIEVES}; retrieve_req++)); do
+  KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 xrdfs ${EOS_INSTANCE} prepare -s ${TEMP_FILE}
+done
+
+# 8. Wait for the copy to appear on disk
+SECONDS_PASSED=0
+WAIT_FOR_RETRIEVED_FILE_TIMEOUT=90
+while test 0 == "$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos --json root://${EOS_INSTANCE} info ${TEMP_FILE}  | jq -r '.locations[] | select(.schedgroup | startswith("default")) | .schedgroup' | wc -l)"; do
+  echo "Waiting for files to be retrieved from tape: seconds passed = ${SECONDS_PASSED}"
+  sleep 1
+  let SECONDS_PASSED=SECONDS_PASSED+1
+  if test ${SECONDS_PASSED} == ${WAIT_FOR_RETRIEVED_FILE_TIMEOUT}; then
+    error "Timed out after ${WAIT_FOR_RETRIEVED_FILE_TIMEOUT} seconds waiting for files to be retrieved from tape"
+  fi
+done
+echo "File retrieve successfully"
+
+# 9. Check eviction counter
+echo "Checking that evict decreases eviction counter (without --force/--fsid)..."
+for ((retrieve_req=${NB_RETRIEVES}; retrieve_req > 0; retrieve_req--)); do
+  echo "Checking evict counter, expecting ${retrieve_req}..."
+  if test ${retrieve_req} != "$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos --json root://${EOS_INSTANCE} attr get sys.retrieve.evict_counter ${TEMP_FILE} | jq -r '.attr.get[].sys.retrieve.evict_counter')"; then
+    error "Evict counter sys.retrieve.evict_counter value is not ${retrieve_req}"
+  fi
+  KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict ${TEMP_FILE}
+done
+
+# 10. Check that file was evicted after clearing the eviction counter
+echo "Checking that ${TEMP_FILE} was evicted after clearing the eviction counter..."
+if test 0 != "$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos --json root://${EOS_INSTANCE} info ${TEMP_FILE} | jq -r '.locations[] | select(.schedgroup | startswith("default")) | .schedgroup' | wc -l)"; then
+  error "Disk replica not evicted, when it should have been done after the eviction counter reached zero"
+else
+  echo "Disk replica was evicted, as expected"
+fi
+
+# 11. Setting up dummy spaces and file systems for testing
 echo "Setting up dummy spaces and file systems (${FSID_DUMMY_1_NAME}=${FSID_DUMMY_1_VALUE}, ${FSID_DUMMY_2_NAME} =${FSID_DUMMY_2_VALUE}, ${FSID_DUMMY_3_NAME}=${FSID_DUMMY_3_VALUE}) for testing..."
 XrdSecPROTOCOL=sss eos -r 0 0 root://${EOS_INSTANCE} space define ${FSID_DUMMY_1_NAME}
 XrdSecPROTOCOL=sss eos -r 0 0 root://${EOS_INSTANCE} space define ${FSID_DUMMY_2_NAME}
@@ -127,13 +166,13 @@ XrdSecPROTOCOL=sss eos -r 0 0 root://${EOS_INSTANCE} fs add -m ${FSID_DUMMY_1_VA
 XrdSecPROTOCOL=sss eos -r 0 0 root://${EOS_INSTANCE} fs add -m ${FSID_DUMMY_2_VALUE} ${FSID_DUMMY_2_NAME} localhost:1234 /does_not_exist_2 ${FSID_DUMMY_2_NAME}
 XrdSecPROTOCOL=sss eos -r 0 0 root://${EOS_INSTANCE} fs add -m ${FSID_DUMMY_3_VALUE} ${FSID_DUMMY_3_NAME} localhost:1234 /does_not_exist_3 ${FSID_DUMMY_3_NAME}
 
-# 7. Adding dummy file systems to namespace
+# 12. Adding dummy file systems to namespace
 echo "Adding dummy filesystems ${FSID_DUMMY_1_NAME}, ${FSID_DUMMY_2_NAME} and ${FSID_DUMMY_3_NAME} to ${TEMP_FILE} ..."
 XrdSecPROTOCOL=sss eos -r 0 0 root://${EOS_INSTANCE} file tag "${TEMP_FILE}" +${FSID_DUMMY_1_VALUE}
 XrdSecPROTOCOL=sss eos -r 0 0 root://${EOS_INSTANCE} file tag "${TEMP_FILE}" +${FSID_DUMMY_2_VALUE}
 XrdSecPROTOCOL=sss eos -r 0 0 root://${EOS_INSTANCE} file tag "${TEMP_FILE}" +${FSID_DUMMY_3_VALUE}
 
-# 8. Check that there are 4 replicas advertised on the namespace now
+# 13. Check that there are 4 replicas advertised on the namespace now
 echo "Checking that ${TEMP_FILE} has 4 replicas advertised on the namespace..."
 if test 4 != "$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos --json root://${EOS_INSTANCE} info ${TEMP_FILE} | jq -r '.locations[] | .schedgroup' | wc -l)"; then
   error "The number of replicas is not the expected one"
@@ -141,13 +180,13 @@ else
   echo "Disk replica count is as expected"
 fi
 
-# 9. Test removing tape replica, should fail
+# 14. Test removing tape replica, should fail
 echo "Trying to remove tape replica with fsid ${FSID_TAPE}..."
-KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --fsid ${FSID_TAPE} ${TEMP_FILE}
+KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --force --fsid ${FSID_TAPE} ${TEMP_FILE}
 if [ $? -eq 0 ]; then
-  error "'eos evict --fsid ${FSID_TAPE}' command succeeded where it should have failed"
+  error "'eos evict --force --fsid ${FSID_TAPE}' command succeeded where it should have failed"
 else
-  echo "'eos evict --fsid ${FSID_TAPE}' command failed as expected"
+  echo "'eos evict --force --fsid ${FSID_TAPE}' command failed as expected"
 fi
 if test 4 != "$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos --json root://${EOS_INSTANCE} info ${TEMP_FILE} | jq -r '.locations[] | .schedgroup' | wc -l)"; then
   error "The number of replicas is not the expected one"
@@ -155,13 +194,13 @@ else
   echo "Disk replica count is as expected"
 fi
 
-# 10. Test removing non-existing replica, should fail
+# 15. Test removing non-existing replica, should fail
 echo "Trying to remove tape replica with non existing fsid ${FSID_NOT_SET_VALUE}..."
-KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --fsid ${FSID_NOT_SET_VALUE} ${TEMP_FILE}
+KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --force --fsid ${FSID_NOT_SET_VALUE} ${TEMP_FILE}
 if [ $? -eq 0 ]; then
-  error "'eos evict --fsid ${FSID_NOT_SET_VALUE}' command succeeded where it should have failed"
+  error "'eos evict --force --fsid ${FSID_NOT_SET_VALUE}' command succeeded where it should have failed"
 else
-  echo "'eos evict --fsid ${FSID_NOT_SET_VALUE}' command failed as expected"
+  echo "'eos evict --force --fsid ${FSID_NOT_SET_VALUE}' command failed as expected"
 fi
 if test 4 != "$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos --json root://${EOS_INSTANCE} info ${TEMP_FILE} | jq -r '.locations[] | .schedgroup' | wc -l)"; then
   error "The number of replicas is not the expected one"
@@ -169,13 +208,22 @@ else
   echo "Disk replica count is as expected"
 fi
 
-# 11. Test removing one existing replica, should succeed and keep remaining replicas intact
-echo "Trying to remove a replica with existing fsid ${FSID_DUMMY_1_VALUE}..."
+# 16. Test removing existing replica, should fail when --force is not used
+echo "Testing 'eos root://${EOS_INSTANCE} evict --fsid ${FSID_DUMMY_1_VALUE} ${TEMP_FILE}'... without '--force'"
 KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --fsid ${FSID_DUMMY_1_VALUE} ${TEMP_FILE}
-if [ $? -ne 0 ]; then
-  error "'eos evict --fsid ${FSID_DUMMY_1_VALUE}' command failed where it should have succeeded"
+if [ $? -eq 0 ]; then
+  error "'eos evict' command succeeded where it should have failed"
 else
-  echo "'eos evict --fsid ${FSID_DUMMY_1_VALUE}' command succeeded as expected"
+  echo "'eos evict' command failed as expected"
+fi
+
+# 17. Test removing one existing replica, should succeed and keep remaining replicas intact
+echo "Trying to remove a replica with existing fsid ${FSID_DUMMY_1_VALUE} ${TEMP_FILE} and --force..."
+KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --force --fsid ${FSID_DUMMY_1_VALUE} ${TEMP_FILE}
+if [ $? -ne 0 ]; then
+  error "'eos evict --force --fsid ${FSID_DUMMY_1_VALUE}' command failed where it should have succeeded"
+else
+  echo "'eos evict --force --fsid ${FSID_DUMMY_1_VALUE}' command succeeded as expected"
 fi
 if test 3 != "$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos --json root://${EOS_INSTANCE} info ${TEMP_FILE} | jq -r '.locations[] | .schedgroup' | wc -l)"; then
   error "The number of replicas is not the expected one"
@@ -183,9 +231,23 @@ else
   echo "Disk replica count is as expected"
 fi
 
-# 12. Test removing all remaining replicas
+# 18. Test removing one existing replica, should succeed and keep remaining replicas intact
+echo "Trying to remove a replica with existing fsid ${FSID_DUMMY_2_VALUE} ${TEMP_FILE} and -f..."
+KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict -f --fsid ${FSID_DUMMY_2_VALUE} ${TEMP_FILE}
+if [ $? -ne 0 ]; then
+  error "'eos evict -f --fsid ${FSID_DUMMY_2_VALUE}' command failed where it should have succeeded"
+else
+  echo "'eos evict -f --fsid ${FSID_DUMMY_2_VALUE}' command succeeded as expected"
+fi
+if test 2 != "$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos --json root://${EOS_INSTANCE} info ${TEMP_FILE} | jq -r '.locations[] | .schedgroup' | wc -l)"; then
+  error "The number of replicas is not the expected one"
+else
+  echo "Disk replica count is as expected"
+fi
+
+# 19. Test removing all remaining replicas
 echo "Trying to remove all disk replicas..."
-KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict ${TEMP_FILE}
+KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOS_INSTANCE} evict --force ${TEMP_FILE}
 if [ $? -ne 0 ]; then
   error "'eos evict' command failed where it should have succeeded"
 else
