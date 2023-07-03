49d313a1b781112444dff247c4002a6ba280a8df
Joao Afonso <joao.afonso@cern.ch>
2023-07-03 17:21:48 +0200

    Fix REPACKING_DISABLED queues not always selected for queueing

--- scheduler/PostgresSchedDB/Helpers.cpp
+++ scheduler/PostgresSchedDB/Helpers.cpp
@@ -188,7 +188,8 @@ std::string Helpers::selectBestVid4Retrieve
       if ((g_retrieveQueueStatistics.at(v).tapeStatus.state == common::dataStructures::Tape::ACTIVE && !isRepack) ||
           (g_retrieveQueueStatistics.at(v).tapeStatus.state == common::dataStructures::Tape::REPACKING && isRepack)) {
         candidateVidsStats.emplace_back(g_retrieveQueueStatistics.at(v).stats);
-      } else if ((g_retrieveQueueStatistics.at(v).tapeStatus.state == common::dataStructures::Tape::DISABLED && !isRepack)) {
+      } else if ((g_retrieveQueueStatistics.at(v).tapeStatus.state == common::dataStructures::Tape::DISABLED && !isRepack) ||
+                 (g_retrieveQueueStatistics.at(v).tapeStatus.state == common::dataStructures::Tape::REPACKING_DISABLED && isRepack)) {
         candidateVidsStatsFallback.emplace_back(g_retrieveQueueStatistics.at(v).stats);
       }
     }
