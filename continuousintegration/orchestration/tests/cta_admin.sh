df34b6c133d7bbd19be47ddd45e11a7615cd7031
Lasse Wardenaer <lasse.tjernaes.wardenaer@cern.ch>
2023-07-10 07:35:38 +0200

    Edit test for cta-admin logical library

--- continuousintegration/orchestration/tests/cta_admin.sh
+++ continuousintegration/orchestration/tests/cta_admin.sh
@@ -200,21 +200,6 @@ test_and_check_cmd "Modifying admin 'test_user'" "${command}" "ch" "-u test_user
 test_command "Removing admin 'test_user'" "${command}" "rm" "-u test_user"
 test_assert || exit 1
 
-# Logical Library (ll)
-test_start "logical library" "ll"
-admin_cta pl add --pl phys1 --ma man --mo mod --npcs 3 --npds 4
-admin_cta pl add --pl phys2 --ma man --mo mod --npcs 3 --npds 4
-test_and_check_cmd "Adding logical library 'cta_adm_systest'" "${command}" "add" "-n 'cta_adm_systest' -d false --pl phys1 -m 'cta-admin systest add'"\
-  'select(.name=="cta_adm_systest" and .isDisabled==false and .physicalLibrary=="phys1" and .comment=="cta-admin systest add") | .name'\
-  "1" "adding logical library 'cta_adm_systest'"|| exit 1
-test_and_check_cmd "Changing logical library 'cta_adm_systest' to disabled" "${command}" "ch" "-n 'cta_adm_systest' -d true --pl phys2 --dr 'cta-admin systest ch'"\
-  'select(.name=="cta_adm_systest" and .isDisabled==true and .physicalLibrary=="phys2" and .disabledReason=="cta-admin systest ch") | .name'\
-  "1" "changing logical library 'cta_adm_systest'"|| exit 1
-test_command "Removing logical library 'cta_adm_systest'" "${command}" "rm" "-n cta_adm_systest" || exit 1
-admin_cta pl rm --pl phys1
-admin_cta pl rm --pl phys2
-test_assert || exit 1
-
 
 # Virtual Organizations (vo)
 test_start "virtual organization" "vo"
