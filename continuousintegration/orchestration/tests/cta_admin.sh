3982faf5782c738d5e81aa96716f612c8a3b7b26
Lasse Wardenaer <lasse.tjernaes.wardenaer@cern.ch>
2023-07-17 01:40:45 +0200

    Add test for having multiple logical  libraries related to a single physical library

--- continuousintegration/orchestration/tests/cta_admin.sh
+++ continuousintegration/orchestration/tests/cta_admin.sh
@@ -375,10 +375,14 @@ admin_cta pl add --name phys2 --ma man --mo mod --npcs 3 --npds 4
 test_and_check_cmd "Adding logical library 'cta_adm_systest'" "${command}" "add" "-n 'cta_adm_systest' -d false --pl phys1 -m 'cta-admin systest add'"\
   'select(.name=="cta_adm_systest" and .isDisabled==false and .physicalLibrary=="phys1" and .comment=="cta-admin systest add") | .name'\
   "1" "adding logical library 'cta_adm_systest'"|| exit 1
+test_and_check_cmd "Adding logical library 'cta_adm_systest'" "${command}" "add" "-n 'cta_adm_systest2' -d false --pl phys1 -m 'cta-admin systest add'"\
+  'select(.name=="cta_adm_systest" and .isDisabled==false and .physicalLibrary=="phys1" and .comment=="cta-admin systest add") | .name'\
+  "1" "adding logical library 'cta_adm_systest'"|| exit 1
 test_and_check_cmd "Changing logical library 'cta_adm_systest' to disabled" "${command}" "ch" "-n 'cta_adm_systest' -d true --pl phys2 --dr 'cta-admin systest ch'"\
   'select(.name=="cta_adm_systest" and .isDisabled==true and .physicalLibrary=="phys2" and .disabledReason=="cta-admin systest ch") | .name'\
   "1" "changing logical library 'cta_adm_systest'"|| exit 1
 test_command "Removing logical library 'cta_adm_systest'" "${command}" "rm" "-n cta_adm_systest" || exit 1
+test_command "Removing logical library 'cta_adm_systest'" "${command}" "rm" "-n cta_adm_systest2" || exit 1
 admin_cta pl rm --name phys1
 admin_cta pl rm --name phys2
 test_assert || exit 1
