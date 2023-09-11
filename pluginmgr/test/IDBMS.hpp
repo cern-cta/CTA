5dce9878c8501662c37feca485a04af39712920f
Fons Rademakers <Fons.Rademakers@cern.ch>
2023-09-11 15:49:42 +0200

    Implement two example DBMS plugins.
    Test with:
    main [PluginOracle | PluginPostgreSQL]

---
+++ pluginmgr/test/IDBMS.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+class IDBMS
+{
+public:
+	virtual const char* Name() const = 0;
+	virtual int  Query(std::string str) const = 0;	
+	virtual ~IDBMS() = default;
+};
+
