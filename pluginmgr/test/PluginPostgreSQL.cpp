5f279dcb5a01787b23a498347f9194e2cd3978f1
Fons Rademakers <Fons.Rademakers@cern.ch>
2023-09-11 15:49:42 +0200

    Implement two example DBMS plugins.
    Test with:
    main [PluginOracle | PluginPostgreSQL]

---
+++ pluginmgr/test/PluginPostgreSQL.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+#include "PluginFactory.hpp"
+
+#include "IDBMS.hpp"
+
+class PostgreSQL: public IDBMS
+{
+public:
+	const char* Name() const {
+		return "PostgreSQL";
+	}
+	int Query(std::string str) const {
+		std::cout << "PostgreSQL Query: " << str << std::endl;
+    return 0;
+	}
+};
+
+
+// ===== Factory Function - Plugin EntryPoint ==== //
+
+PLUGIN_EXPORT_C
+auto GetPluginFactory() -> IPluginFactory*
+{
+	static PluginFactory pinfo = []{
+		auto p = PluginFactory("PluginPostgreSQL", "0.2");
+		p.registerClass<PostgreSQL>("PostgreSQL");
+		return p;
+	}();
+	return &pinfo;
+}
+
+
+struct _DLLInit{
+	_DLLInit(){
+		std::cerr << " [TRACE] Shared library PluginPostgreSQL loaded OK." << std::endl;
+	}
+	~_DLLInit(){
+		std::cerr << " [TRACE] Shared library PluginPostgreSQL unloaded OK." << std::endl;
+	}
+} dll_init;
+
