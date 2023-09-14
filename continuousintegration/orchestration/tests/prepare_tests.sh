c01fbf5a9a78bd33b2a27850153c8f16ad4501b2
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2023-09-14 16:37:40 +0200

    Missing for loop close

--- continuousintegration/orchestration/tests/prepare_tests.sh
+++ continuousintegration/orchestration/tests/prepare_tests.sh
@@ -78,8 +78,10 @@ ctacliIP=`kubectl --namespace ${NAMESPACE} describe pod ctacli | grep IP | sed -
 # Get list of tape drives that have a tape server
 TAPEDRIVES_IN_USE=()
 for tapeserver in $(kubectl --namespace ${NAMESPACE} get pods | grep tpsrv | awk '{print $1}'); do
-  for drive in $(kubectl -n ${NAMESPACE} exec ${tapeserver} -c taped -- cat /etc/cta/TPCONFIG | awk '{print $1}')
+  for drive in $(kubectl -n ${NAMESPACE} exec ${tapeserver} -c taped -- cat /etc/cta/TPCONFIG | awk '{print $1}'); do
     TAPEDRIVES_IN_USE+=(${drive})
+
+  done
 done
 NB_TAPEDRIVES_IN_USE=${#TAPEDRIVES_IN_USE[@]}
 
