2ccf5896b5d7bc90a8140b92e7546bb0a0bdb6f5
Fons Rademakers <Fons.Rademakers@cern.ch>
2023-09-15 17:06:52 +0200

    Formatting and comments.

--- pluginmgr/test/main.cpp
+++ pluginmgr/test/main.cpp
@@ -4,36 +4,37 @@
 
 #include "IDBMS.hpp"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
+
+  // By default use the Oracle plugin, alternatively give "PluginPostgreSQL" as argument
   std::string pluginLib = "PluginOracle";
-  if (argc == 2)
+  if (argc == 2) {
     pluginLib = argv[1];
-  
-	PluginManager ma;
-	IPluginFactory* infoA = ma.addPlugin(pluginLib);
-	assert(infoA != nullptr);
-	
-	std::cout << " ---- Plugin Information --------- " << "\n"
-			  << "  =>              Name = " << infoA->Name() << "\n"
-			  << "  =>           Version = " << infoA->Version() << "\n"
-			  << "  => Number of classes = " << infoA->NumberOfClasses()
-			  << "\n\n";
+  }
 
-	std::cout << "Classes exported by the Plugin: (" << pluginLib << ") " << "\n";
+  PluginManager ma;
+  IPluginFactory* infoA = ma.addPlugin(pluginLib);
+  assert(infoA != nullptr);
 
-	for(size_t n = 0; n < infoA->NumberOfClasses(); n++)
-	{
-		std::cout << " -> Exported Class: " << infoA->GetClassName(n) << "\n";
-	}
-	
-	
-	// Use Oracle DB
-	auto pDB = ma.CreateInstanceAs<IDBMS>(pluginLib, infoA->GetClassName(0));
-	assert(pDB != nullptr);
-	
-	std::cout << "pDB->Name()    = " << pDB->Name() << "\n";
-	std::cout << "pDB->Query(\"SELECT (*) AGE\") = " << pDB->Query("SELECT (*) AGE") << "\n";
-		
-	return 0;
+  std::cout << " ---- Plugin Information --------- "
+            << "\n"
+            << "  =>              Name = " << infoA->Name() << "\n"
+            << "  =>           Version = " << infoA->Version() << "\n"
+            << "  => Number of classes = " << infoA->NumberOfClasses() << "\n\n";
+
+  std::cout << "Classes exported by the Plugin: (" << pluginLib << ") "
+            << "\n";
+
+  for (size_t n = 0; n < infoA->NumberOfClasses(); n++) {
+    std::cout << " -> Exported Class: " << infoA->GetClassName(n) << "\n";
+  }
+
+  // Use DB via plugin interface IDBMS
+  auto pDB = ma.CreateInstanceAs<IDBMS>(pluginLib, infoA->GetClassName(0));
+  assert(pDB != nullptr);
+
+  std::cout << "pDB->Name()    = " << pDB->Name() << "\n";
+  std::cout << "pDB->Query(\"SELECT (*) AGE\") = " << pDB->Query("SELECT (*) AGE") << "\n";
+
+  return 0;
 }
