233468d501f85cc9a63a963e5d39a63a5b08c28b
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2023-10-04 17:19:02 +0200

    Sleep for successful tests

--- scheduler/GenericSchedulerTest.cpp
+++ scheduler/GenericSchedulerTest.cpp
@@ -20,6 +20,7 @@
 #include <bits/unique_ptr.h>
 #include <exception>
 #include <memory>
+#include <unistd.h>
 #include <utility>
 
 #include "catalogue/CreateMountPolicyAttributes.hpp"
@@ -393,6 +394,9 @@ TEST_P(SchedulerTest, archive_to_new_file) {
   log::LogContext lc(dl);
   const uint64_t archiveFileId = scheduler.checkAndGetNextArchiveFileId(s_diskInstance, request.storageClass,
       request.requester, lc);
+  int ctrl_var = 0;
+  while (ctrl_var == 0) usleep(1000000);
+
   scheduler.queueArchiveWithGivenId(archiveFileId, s_diskInstance, request, lc);
   scheduler.waitSchedulerDbSubthreadsComplete();
 
@@ -2169,6 +2173,9 @@ TEST_P(SchedulerTest, expandRepackRequestRepackingTape) {
   // Queue the repack request for a repacking tape
   // Should work
   {
+    int ctrl_var = 0;
+    while (ctrl_var == 0) usleep(100000);
+
     cta::SchedulerDatabase::QueueRepackRequest qrr(vid,"file://"+tempDirectory.path(),common::dataStructures::RepackInfo::Type::MoveOnly,
     common::dataStructures::MountPolicy::s_defaultMountPolicyForRepack,s_defaultRepackNoRecall);
     ASSERT_NO_THROW(scheduler.queueRepack(admin,qrr,lc));
