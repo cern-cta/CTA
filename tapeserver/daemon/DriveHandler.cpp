9a58cf916fc32f0aa58da35e42bc3132c99f68a9
Jorge Camarero Vera <jorge.camarero@cern.ch>
2023-10-25 17:20:57 +0200

    Reduce complexity of runChild

--- tapeserver/daemon/DriveHandler.cpp
+++ tapeserver/daemon/DriveHandler.cpp
@@ -638,14 +638,6 @@ int DriveHandler::runChild() {
             .add("PreviousType", session::toString(m_sessionType));
       m_lc->log(log::INFO, "In DriveHandler::runChild(): starting cleaner after crash with tape potentially loaded.");
     }
-    // TODO: the cleaner session does not yet report to the scheduler
-//    // Before launching the transfer session, we validate that the scheduler is reachable.
-//    if (!scheduler->ping()) {
-//      m_lc->log(log::CRIT, "In DriveHandler::runChild(): failed to ping central storage before cleaner. Reporting fatal error.");
-//      driveHandlerProxy->reportState(tape::session::SessionState::Fatal, tape::session::SessionType::Undetermined, "");
-//      sleep(1);
-//      return castor::tape::tapeserver::daemon::Session::MARK_DRIVE_AS_DOWN;
-//    }
     if (!schedulerPing(scheduler.get(), driveHandlerProxy.get())) {
       // failed to do ping
       return castor::tape::tapeserver::daemon::Session::MARK_DRIVE_AS_DOWN;
@@ -654,78 +646,77 @@ int DriveHandler::runChild() {
     m_lc->log(log::DEBUG, "In DriveHandler::runChild(): will create cleaner session.");
     return executeCleanerSession(scheduler.get());
   }
-  else {
-    // The next session will be a normal session (no crash with a mounted tape before).
-    m_stateChangeTimeouts[session::SessionState::Checking] = std::chrono::duration_cast<Timeout>(
-      std::chrono::minutes(m_tapedConfig.wdCheckMaxSecs.value()));
-    m_stateChangeTimeouts[session::SessionState::Scheduling] = std::chrono::duration_cast<Timeout>(
-      std::chrono::minutes(m_tapedConfig.wdScheduleMaxSecs.value()));
-    m_stateChangeTimeouts[session::SessionState::Mounting] = std::chrono::duration_cast<Timeout>(
-      std::chrono::minutes(m_tapedConfig.wdMountMaxSecs.value()));
-    m_stateChangeTimeouts[session::SessionState::Unmounting] = std::chrono::duration_cast<Timeout>(
-      std::chrono::minutes(m_tapedConfig.wdUnmountMaxSecs.value()));
-    m_stateChangeTimeouts[session::SessionState::DrainingToDisk] = std::chrono::duration_cast<Timeout>(
-      std::chrono::minutes(m_tapedConfig.wdDrainMaxSecs.value()));
-    m_stateChangeTimeouts[session::SessionState::ShuttingDown] = std::chrono::duration_cast<Timeout>(
-      std::chrono::minutes(m_tapedConfig.wdShutdownMaxSecs.value()));
+  
+  // The next session will be a normal session (no crash with a mounted tape before).
+  m_stateChangeTimeouts[session::SessionState::Checking] = std::chrono::duration_cast<Timeout>(
+    std::chrono::minutes(m_tapedConfig.wdCheckMaxSecs.value()));
+  m_stateChangeTimeouts[session::SessionState::Scheduling] = std::chrono::duration_cast<Timeout>(
+    std::chrono::minutes(m_tapedConfig.wdScheduleMaxSecs.value()));
+  m_stateChangeTimeouts[session::SessionState::Mounting] = std::chrono::duration_cast<Timeout>(
+    std::chrono::minutes(m_tapedConfig.wdMountMaxSecs.value()));
+  m_stateChangeTimeouts[session::SessionState::Unmounting] = std::chrono::duration_cast<Timeout>(
+    std::chrono::minutes(m_tapedConfig.wdUnmountMaxSecs.value()));
+  m_stateChangeTimeouts[session::SessionState::DrainingToDisk] = std::chrono::duration_cast<Timeout>(
+    std::chrono::minutes(m_tapedConfig.wdDrainMaxSecs.value()));
+  m_stateChangeTimeouts[session::SessionState::ShuttingDown] = std::chrono::duration_cast<Timeout>(
+    std::chrono::minutes(m_tapedConfig.wdShutdownMaxSecs.value()));
 
-    // Before launching, and if this is the first session since daemon start, we will
-    // put the drive down.
-    if (m_previousSession == PreviousSession::Initiating) {
-      // Log that we put the drive's desired state to down and do it.
-      log::ScopedParamContainer params(*m_lc);
-      params.add("tapeDrive", m_driveConfig.unitName);
-      int logLevel = log::INFO;
-      std::string msg = "Startup";
-      m_lc->log(logLevel, msg);
-      try {
-        // Before setting the desired state as down, we have to make sure the drive exists in the registry.
-        // this is done by reporting the drive as down first.
-        // Checking the drive does not already exist in the database
-        if (!scheduler->checkDriveCanBeCreated(driveInfo, *m_lc)) {
-          driveHandlerProxy->reportState(tape::session::SessionState::Fatal, tape::session::SessionType::Undetermined, "");
-          return castor::tape::tapeserver::daemon::Session::MARK_DRIVE_AS_DOWN;
-        }
-
-        cta::common::dataStructures::DesiredDriveState  currentDesiredDriveState;
-        try {
-          currentDesiredDriveState = scheduler->getDesiredDriveState(m_driveConfig.unitName, *m_lc);
-        } catch (Scheduler::NoSuchDrive&) {
-          m_lc->log(log::INFO, "In DriveHandler::runChild(): the desired drive state doesn't exist in the Catalogue DB");
-        }
-
-        cta::common::dataStructures::SecurityIdentity securityIdentity;
-        cta::common::dataStructures::DesiredDriveState driveState;
-        driveState.up = false;
-        driveState.forceDown = false;
-        scheduler->createTapeDriveStatus(driveInfo, driveState, common::dataStructures::MountType::NoMount,
-                                        common::dataStructures::DriveStatus::Down, m_driveConfig, securityIdentity, *m_lc);
-
-        // Get the drive state to see if there is a reason or not, we don't want to change the reason
-        // why a drive is down at the startup of the tapeserver. If it's setted up a previous Reason From Log
-        // it will be change for this one.
-        if (!currentDesiredDriveState.reason) {
-          driveState.setReasonFromLogMsg(logLevel, msg);
-        } else if (currentDesiredDriveState.reason.value().substr(0, 11) == "[cta-taped]") {
-          driveState.setReasonFromLogMsg(logLevel, msg);
-        } else {
-          driveState.reason = currentDesiredDriveState.reason.value();
-        }
-
-        scheduler->setDesiredDriveState(securityIdentity, m_driveConfig.unitName, driveState, *m_lc);
-        scheduler->reportDriveConfig(m_driveConfig, m_tapedConfig, *m_lc);
-      } catch (cta::exception::Exception& ex) {
-        params.add("Message", ex.getMessageValue())
-              .add("Backtrace", ex.backtrace());
-        m_lc->log(log::CRIT, "In DriveHandler::runChild(): failed to set drive down");
-        // This is a fatal error (failure to access the scheduler). Shut daemon down.
+  // Before launching, and if this is the first session since daemon start, we will
+  // put the drive down.
+  if (m_previousSession == PreviousSession::Initiating) {
+    // Log that we put the drive's desired state to down and do it.
+    log::ScopedParamContainer params(*m_lc);
+    params.add("tapeDrive", m_driveConfig.unitName);
+    int logLevel = log::INFO;
+    std::string msg = "Startup";
+    m_lc->log(logLevel, msg);
+    try {
+      // Before setting the desired state as down, we have to make sure the drive exists in the registry.
+      // this is done by reporting the drive as down first.
+      // Checking the drive does not already exist in the database
+      if (!scheduler->checkDriveCanBeCreated(driveInfo, *m_lc)) {
         driveHandlerProxy->reportState(tape::session::SessionState::Fatal, tape::session::SessionType::Undetermined, "");
         return castor::tape::tapeserver::daemon::Session::MARK_DRIVE_AS_DOWN;
       }
-    }
 
-    return executeDataTransferSession(scheduler.get(), driveHandlerProxy.get());
+      cta::common::dataStructures::DesiredDriveState  currentDesiredDriveState;
+      try {
+        currentDesiredDriveState = scheduler->getDesiredDriveState(m_driveConfig.unitName, *m_lc);
+      } catch (Scheduler::NoSuchDrive&) {
+        m_lc->log(log::INFO, "In DriveHandler::runChild(): the desired drive state doesn't exist in the Catalogue DB");
+      }
+
+      cta::common::dataStructures::SecurityIdentity securityIdentity;
+      cta::common::dataStructures::DesiredDriveState driveState;
+      driveState.up = false;
+      driveState.forceDown = false;
+      scheduler->createTapeDriveStatus(driveInfo, driveState, common::dataStructures::MountType::NoMount,
+                                      common::dataStructures::DriveStatus::Down, m_driveConfig, securityIdentity, *m_lc);
+
+      // Get the drive state to see if there is a reason or not, we don't want to change the reason
+      // why a drive is down at the startup of the tapeserver. If it's setted up a previous Reason From Log
+      // it will be change for this one.
+      if (!currentDesiredDriveState.reason) {
+        driveState.setReasonFromLogMsg(logLevel, msg);
+      } else if (currentDesiredDriveState.reason.value().substr(0, 11) == "[cta-taped]") {
+        driveState.setReasonFromLogMsg(logLevel, msg);
+      } else {
+        driveState.reason = currentDesiredDriveState.reason.value();
+      }
+
+      scheduler->setDesiredDriveState(securityIdentity, m_driveConfig.unitName, driveState, *m_lc);
+      scheduler->reportDriveConfig(m_driveConfig, m_tapedConfig, *m_lc);
+    } catch (cta::exception::Exception& ex) {
+      params.add("Message", ex.getMessageValue())
+            .add("Backtrace", ex.backtrace());
+      m_lc->log(log::CRIT, "In DriveHandler::runChild(): failed to set drive down");
+      // This is a fatal error (failure to access the scheduler). Shut daemon down.
+      driveHandlerProxy->reportState(tape::session::SessionState::Fatal, tape::session::SessionType::Undetermined, "");
+      return castor::tape::tapeserver::daemon::Session::MARK_DRIVE_AS_DOWN;
+    }
   }
+
+  return executeDataTransferSession(scheduler.get(), driveHandlerProxy.get());
 }
 
 //------------------------------------------------------------------------------
