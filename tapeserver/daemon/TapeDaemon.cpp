ea4c23934d92c1d00ac7704ed8e20acc0e63f387
Joao Afonso <joao.afonso@cern.ch>
2023-10-30 15:47:36 +0100

    Catching more exceptions

--- tapeserver/daemon/TapeDaemon.cpp
+++ tapeserver/daemon/TapeDaemon.cpp
@@ -56,9 +56,16 @@ int TapeDaemon::main() {
     return EXIT_FAILURE;
   } catch (cta::exception::Exception &ex) {
     // Log the error
-    m_log(log::ERR, "Aborting cta-taped on uncaught exception. Stack trace follows.", {{"Message", ex.getMessage().str()}});
+    m_log(log::ERR, "Aborting cta-taped on uncaught exception. Stack trace follows.",
+          {{"Message", ex.getMessage().str()}});
     log::LogContext lc(m_log);
     lc.logBacktrace(log::INFO, ex.backtrace());
+    return EXIT_FAILURE;
+  } catch(const std::exception& e) {
+    m_log(log::ERR, "Aborting cta-taped on uncaught exception.", {{"Message", e.what()}});
+    return EXIT_FAILURE;
+  } catch(...) {
+    m_log(log::ERR, "Aborting cta-taped on uncaught unknown exception.");
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
