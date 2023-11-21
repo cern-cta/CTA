2bd6c8296e4c227dd4b49882412fdaacc6aa0265
Jorge Camarero Vera <jorge.camarero@cern.ch>
2023-11-21 15:23:27 +0100

    Fixes for tests in alma9

--- continuousintegration/docker/ctafrontend/alma9/cc7toalma9.sh
+++ continuousintegration/docker/ctafrontend/alma9/cc7toalma9.sh
@@ -11,14 +11,4 @@ occi_folder=$(find /usr/ | grep libocci.so.19.1 | xargs dirname)
 cp libocci_gcc53.so.19.1 $occi_folder
 ls -l $occi_folder
 
-sed -i 's/protobuf3/protobuf/g' cta.spec.in
-sed -i 's/instantclient19.3/instantclient19.19/g' cta.spec.in
-sed -i 's/radosVersion 2:15.2.15/radosVersion 2:16.2.4/g' cta.spec.in
-sed -i 's/, grpc-static//' cta.spec.in
-sed -i 's/protobuf3/protobuf/g' cmake/FindProtobuf3.cmake
-sed -i 's/lib64\/protobuf/lib64/g' cmake/FindProtobuf3.cmake
-sed -i 's/protoc3/protoc/g' cmake/FindProtobuf3.cmake
-sed -i 's/include\/protobuf/include/g' cmake/FindProtobuf3.cmake
-sed -i 's/19.3/19.19/g' cmake/Findoracle-instantclient.cmake
-sed -i 's/libocci.so/libocci_gcc53.so/g' cmake/Findoracle-instantclient.cmake
-sed -i 's/EosCtaGrpc gpr/EosCtaGrpc gpr absl_synchronization/g' eos_cta/CMakeLists.txt
+git apply continuousintegration/docker/ctafrontend/alma9/code.patch
