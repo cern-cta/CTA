fef8dd33fada97ffa1f0af4b4ae5f6f96ff63a9b
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2023-11-29 10:22:21 +0100

    Fix gfal2 test behaviour

--- continuousintegration/orchestration/tests/client-gfal2_retrieve.sh
+++ continuousintegration/orchestration/tests/client-gfal2_retrieve.sh
@@ -18,45 +18,27 @@
 
 echo "$(date +%s): Trigerring EOS retrieve workflow as poweruser1:powerusers (12001:1200)"
 
-
-#SURLs=$(mktemp)
-TMP_FILE=$(mktemp)
-db_get_files > ${TMP_FILE}
-
-# Split the input file into one file per directory to avoid cat and grep
-# the entire file for each subdirectory.
-seq 0 $(( ${NB_DIRS} - 1 )) | xargs -iSUBDIR bash -c "touch ${TMP_FILE}SUBDIR"
-cat ${TMP_FILE} | xargs -iFILE bash -c "subdir=\$(echo FILE | cut -d/ -f1); echo FILE | cut -d/ -f2 >> ${TMP_FILE}\${subdir}"
-rm -f ${TMP_FILE}
-
-# Get initial stage value.
-#current_stage_val=$(db_info 'archived')
-current_stage_val=0
-NEW_STAGE_VAL=$((${current_stage_val} + 1 ))
-
 for ((subdir=0; subdir < ${NB_DIRS}; subdir++)); do
   echo -n "Retrieving files to ${EOS_DIR}/${subdir} using gfal-bringonline and ${NB_PROCS} processes..."
 
-  gfal_call="XRD_LOGLEVEL=Dump KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 gfal-bringonline ${GFAL2_PROTOCOL}://${EOSINSTANCE}/${EOS_DIR}/${subdir}/TEST_FILE_NAME 2>${ERROR_DIR}/RETRIEVE_TEST_FILE_NAME && rm ${ERROR_DIR}/RETRIEVE_TEST_FILE_NAME"
+  gfal_call="XRD_LOGLEVEL=Dump KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 gfal-bringonline ${GFAL2_PROTOCOL}://${EOSINSTANCE}/${EOS_DIR}/${subdir}/TEST_FILE_NAME 2>${ERROR_DIR}/${subdir}RETRIEVE_TEST_FILE_NAME && rm ${ERROR_DIR}/${subdir}RETRIEVE_TEST_FILE_NAME"
 
-  gfal_error="echo Error with gfal-bringonline prepare stage for file TEST_FILE_NAME, full logs in ${ERROR_DIR}/RETRIEVE_TEST_FILE_NAME"
+  gfal_error="echo Error with gfal-bringonline prepare stage for file ${subdir}/TEST_FILE_NAME, full logs in ${ERROR_DIR}/${subdir}RETRIEVE_TEST_FILE_NAME"
 
   command_str="${gfal_call} || ${gfal_error}"
 
-  cat "${TMP_FILE}${subdir}" | xargs --max-procs=${NB_PROCS} -iTEST_FILE_NAME bash -c "${command_str}" | tee ${LOGDIR}/prepare_${subdir}.log | grep ^ERROR
+  seq -w 0 $((${NB_FILES} - 1)) | xargs --max-procs=${NB_PROCS} -iTEST_FILE_NAME bash -c "${command_str}" | tee ${LOGDIR}/prepare_${subdir}.log | grep ^ERROR
 
   echo Done.
 
   # Get extended attributes to get sys.retrieve.req_id
-  xrdfs_call="XRD_LOGLEVEL=Dump KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 xrdfs ${EOSINSTANCE} query opaquefile ${EOS_DIR}/${subdir}/TEST_FILE_NAME?mgm.pcmd=xattr\&mgm.subcmd=get\&mgm.xattrname=sys.retrieve.req_id 2>${ERROR_DIR}/XATTRGET_TEST_FILE_NAME && rm ${ERROR_DIR}/XATTRGET_TEST_FILE_NAME"
+  xrdfs_call="XRD_LOGLEVEL=Dump KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 xrdfs ${EOSINSTANCE} query opaquefile ${EOS_DIR}/${subdir}/TEST_FILE_NAME?mgm.pcmd=xattr\&mgm.subcmd=get\&mgm.xattrname=sys.retrieve.req_id 2>${ERROR_DIR}/${subdir}XATTRGET_TEST_FILE_NAME && rm ${ERROR_DIR}/${subdir}XATTRGET_TEST_FILE_NAME"
 
-  xrdfs_error=" echo ERROR with xrootd xattr get for file TEST_FILE_NAME, full logs in ${ERROR_DIR}/XATTRGET_TEST_FILE_NAME"
+  xrdfs_error=" echo ERROR with xrootd xattr get for file TEST_FILE_NAME, full logs in ${ERROR_DIR}/${subdir}XATTRGET_TEST_FILE_NAME"
 
   command_str="${xrdfs_call} || ${xrdfs_error}"
 
-  cat "${TMP_FILE}${subdir}" | xargs --max-procs=${NB_PROCS} -iTEST_FILE_NAME bash -c "$command_str" | tee ${LOGDIR}/prepare_sys.retrieve.req_id_${subdir}.log | grep ^ERROR
-
-  rm -f "${TMP_FILE}${subdir}"
+  seq -w 0 $((${NB_FILES} -1)) | xargs --max-procs=${NB_PROCS} -iTEST_FILE_NAME bash -c "$command_str" | tee ${LOGDIR}/prepare_sys.retrieve.req_id_${subdir}.log | grep ^ERROR
 done
 
 
@@ -67,7 +49,6 @@ if [ "0" != "$(ls ${ERROR_DIR} 2> /dev/null | wc -l)" ]; then
   mv ${ERROR_DIR}/* ${LOGDIR}/xrd_errors/
 fi
 
-#ARCHIVED=$(cat ${SURLs} | wc -l)
 TO_BE_RETRIEVED=$(( ${ARCHIVED} - $(ls ${ERROR_DIR}/RETRIEVE_* 2>/dev/null | wc -l) ))
 RETRIEVING=${TO_BE_RETRIEVED}
 RETRIEVED=0
@@ -78,12 +59,7 @@ WAIT_FOR_RETRIEVED_FILE_TIMEOUT=$((40+${NB_FILES}/5))
 
 status=$(mktemp)
 while test 0 -lt ${RETRIEVING}; do
-  rm -f $status
-  touch $status
   echo "$(date +%s): Waiting for files to be retrieved from tape: Seconds passed = ${SECONDS_PASSED}"
-
-  sleep 3
-
   let SECONDS_PASSED=SECONDS_PASSED+1
 
   if test ${SECONDS_PASSED} == ${WAIT_FOR_RETRIEVED_FILE_TIMEOUT}; then
@@ -93,24 +69,16 @@ while test 0 -lt ${RETRIEVING}; do
 
   RETRIEVED=0
   for ((subdir=0; subdir < ${NB_DIRS}; subdir++)); do
-    # Get retrieve status
-    eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir} | egrep '^d[1-9][0-9]*::t1' | awk -v sd="${subdir}/" '{print sd$10}' >> $status
-
-    RETRIEVED=$(( ${RETRIEVED} + $(cat $status | wc -l) ))
+    RETRIEVED=$(( ${RETRIEVED} + $( eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir} | egrep '^d[1-9][0-9]*::t1' | wc -l) ))
 
     sleep 1 # do not hammer eos too hard
   done
 
   RETRIEVING=$((${TO_BE_RETRIEVED} - ${RETRIEVED}))
 
-  echo "${RETRIEVED}/${TO_BE_RETRIEVED} retrieved"
+  echo "$(date +%s): ${RETRIEVED}/${TO_BE_RETRIEVED} retrieved"
 done
 
-db_begin_transaction
-cat $status | xargs -iFILE bash -c "db_update staged FILE 1 '='"
-db_commit_transaction
-rm -f ${status}
-
 echo "###"
-echo "${RETRIEVED}/${TO_BE_RETRIEVED} retrieved files"
+echo "$(date +%s): ${RETRIEVED}/${TO_BE_RETRIEVED} retrieved files"
 echo "###"
