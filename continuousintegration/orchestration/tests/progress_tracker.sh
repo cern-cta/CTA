d603c00475e574b12cb8e75141af2d359528acc6
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2023-12-04 16:38:37 +0100

    Tracking refactoring

---
+++ continuousintegration/orchestration/tests/progress_tracker.sh
@@ -0,0 +1,311 @@
+#!/bin/bash
+
+# @project      The CERN Tape Archive (CTA)
+# @copyright    Copyright © 2023 CERN
+# @license      This program is free software, distributed under the terms of the GNU General Public
+#               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
+#               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
+#               option) any later version.
+#
+#               This program is distributed in the hope that it will be useful, but WITHOUT ANY
+#               WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#               PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#               In applying this licence, CERN does not waive the privileges and immunities
+#               granted to it by virtue of its status as an Intergovernmental Organization or
+#               submit itself to any jurisdiction.
+
+# NOTE: Tracking should not be enabled during stress tests.
+
+trackArchive() {
+  tmpFileMap=fileMap
+  count=0
+  for s in seq 0 90; do # 90 secs timeout
+    for subdir in seq 0 $((NB_DIRS - 1)); do
+      transaction="${QUERY_PRAGMAS}; BEGIN TRANSACTION;"
+      tmp=$(eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir} |
+              grep "^d0::t1" | awk '{print $10}')
+      ts=$(date +%s)
+
+      # Update map
+      for file in tmp; do
+        if [[ -z ${tmpFileMap["${file}"]} ]]; then
+          tmpFileMap["${file}"]='1'
+          transaction+="UPDATE ${TEST_TABLE} SET archived=1, archived_t=${ts} WHERE filename=${file};"
+          count=$((count + 1))
+        fi
+      done
+
+      # Commit transaction
+      transaction+='END TRANSACTION;'
+      sqlite3 "${DB_NAME}" "${transaction}"
+
+      # Check if we are done.
+      echo "Archived ${count} out of $((NB_FILES*NB_DIRS))"
+      if [[ $count == $((NB_FILES*NB_DIRS)) ]]; then
+        return
+      fi
+
+      sleep 1
+    done
+  done
+
+  if [[ $s == 90 ]]; then echo "WARNING: timeout during archive." ; fi
+
+  unset tmpFileMap
+}
+
+trackPrepare() {
+  tmpFileMap=fileMap
+  count=0
+  evictCounter=$((base_evict + 1))
+  for s in seq 0 90; do # 90 secs timeout
+    for subdir in seq 0 $((NB_DIRS - 1)); do
+      transaction="${QUERY_PRAGMAS}; BEGIN TRANSACTION;"
+      tmp=$(eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir} |
+              grep "^d${evicCounter}::t1" | awk '{print $10}')
+      ts=$(date +%s)
+
+      # Update map
+      for file in $tmp; do
+        if [[ -z ${tmpFileMap["${file}"]} ]]; then
+          tmpFileMap["${file}"]='1'
+          transaction+="UPDATE ${TEST_TABLE} SET staged=${evictCounter}, staged_t=${ts} WHERE filename=${file};"
+          count=$((count + 1))
+        fi
+      done
+
+      # Commit transaction
+      transaction+='END TRANSACTION;'
+      sqlite3 "${DB_NAME}" "${transaction}"
+
+      # Check if we are done.
+      echo "Archived ${count} out of $((NB_FILES*NB_DIRS))"
+      if [[ $count == $((NB_FILES*NB_DIRS)) ]]; then
+        return
+      fi
+
+      sleep 1
+    done
+  done
+
+  if [[ $s == 90 ]]; then echo "WARNING: timeout during stage." ; fi
+
+  base_evict=$evictCounter
+  unset tmpFileMap
+}
+
+trackEvict() {
+  tmpFileMap=fileMap
+  count=0
+  evictCounter=$((base_evict - 1))
+  for s in seq 0 90; do # 90 secs timeout
+    for subdir in seq 0 $((NB_DIRS - 1)); do
+      transaction="${QUERY_PRAGMAS}; BEGIN TRANSACTION;"
+      tmp=$(eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir} |
+              grep "^d0::t1" | awk '{print $10}')
+      ts=$(date +%s)
+
+      # Update map
+      for file in $tmp; do
+        if [[ -z ${tmpFileMap["${file}"]} ]]; then
+          tmpFileMap["${file}"]='1'
+          transaction="UPDATE ${TEST_TABLE} SET evicted=${evictCounter}, evicted_t=${ts} WHERE filename=${file};"
+          count=$((count + 1))
+        fi
+      done
+
+      # Commit transaction
+      transaction+='END TRANSACTION;'
+      sqlite3 "${DB_NAME}" "${transaction}"
+
+      # Check if we are done.
+      echo "Archived ${count} out of $((NB_FILES*NB_DIRS))"
+      if [[ $count == $((NB_FILES*NB_DIRS)) ]]; then
+        return
+      fi
+
+      sleep 1
+    done
+  done
+
+  if [[ $s == 90 ]]; then echo "WARNING: timeout during evict." ; fi
+
+  base_evict=$evictCounter
+  unset tmpFileMap
+}
+
+trackDelete() {
+  tmpFileMap=fileMap
+  count=0
+  for s in seq 0 90; do # 90 secs timeout
+    for subdir in seq 0 $((NB_DIRS - 1)); do
+      transaction="${QUERY_PRAGMAS}; BEGIN TRANSACTION;"
+      tmp=$(eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir})
+      ts=$(date +%s)
+
+      # Update map with files not yet deleted.
+      for file in $tmp; do
+        tmpFileMap["${file}"]="-1"
+      done
+
+      # Update deleted files in the db.
+      for file in "${!tmpFileMap[@]}"; do
+        if [[ -z ${tmpFileMap["${file}"]} ]]; then
+          transaction+="UPDATE ${TEST_TABLE} SET deleted=1, deleted_t=${ts} WHERE filename=${file};"
+          count=$((count + 1 ))
+        fi
+      done
+
+      # Reset filemap
+      tmpFileMap=fileMap
+      # Commit transaction
+      transaction+='END TRANSACTION;'
+      sqlite3 "${DB_NAME}" "${transaction}"
+
+      # Check if we are done.
+      echo "Archived ${count} out of $((NB_FILES*NB_DIRS))"
+      if [[ $count == $((NB_FILES*NB_DIRS)) ]]; then
+        return
+      fi
+
+      sleep 1
+    done
+  done
+
+  if [[ $s == 90 ]]; then echo "WARNING: timeout during delete." ; fi
+
+  base_evict=0
+  unset tmpFileMap
+}
+
+
+
+# Check sequence list is valid. The responsibility for using valid sequences
+# of the commads is responsibility of the author otf the tests to track.
+SEQUENCE=$1
+for elem in $SEQUENCE; do
+    if [[ ! ${elem} =~ ^("archive|retrieve|evict|abort|delete") ]]; then
+      die "ERROR: Invalid tracking sage. ${elem} not in [abort, archive, retrieve, evict, delete]"
+    fi
+done
+
+### Setup sqlite3 DB.
+# Pragmas for performance. We don't really accre about the integrity and persistency of the DB.
+QUERY_PRAGMAS='
+PRAGMA synchronous=OFF;
+PRAGMA count_changes=OFF;
+PRAGMA journal_mode=OFF;'
+
+###
+# Table client_tests
+#   filename
+#   archived     - amount of time a file has been archived
+#   staged       - amount of times a file has been evicted
+#   evicted      - amount of times the evict call has been called for the file.
+#   deleted      - deleted files.
+
+cat <<EOF > /opt/run/bin/tracker.schema
+
+${QUERY_PRAGMAS}
+
+CREATE TABLE client_tests_${TESTID}(
+       filename INTEGER PRIMARY KEY,
+       archived INTEGER DEFAULT 0,
+       archived_t INTEGER DEFAULT 0,
+       staged   INTEGER DEFAULT 0,
+       staged_t INTEGER DEFAULT 0,
+       evicted  INTEGER DEFAULT 0,
+       evicted_t INTEGER DEFAULT 0,
+       deleted  INTEGER DEFAULT 0,
+       deleted_t INTEGER DEFAULT 0
+);
+
+EOF
+
+export DB_NAME="/root/trackerdb.db"
+export TEST_TABLE="client_tests_${TESTID}"
+declare -A fileMap
+
+### Initialize database.
+INIT_STR="${QUERY_PRAGMAS}; INSERT INTO ${TEST_TABLE} ("
+for subdir in seq 0 $((NB_DIRS - 1)); do
+  for i in seq -w 0 $(((NB_FILES*NB_DIRS) -1 )); do
+    fileMap["${subdir}${i}"]=""
+    INIT_STR+="${subdir}${i}, "
+  done
+done
+INIT_STR=${INIT_STR::-2}
+INIT_STR+=");"
+
+
+sqlite3 "${DB_NAME}" < /opt/run/bin/tracker.schema
+sqlite3 "${DB_NAME}" "${INIT_STR}"
+unset INIT_STR
+
+
+###############################################
+################### Run #######################
+###############################################
+base_evict=0
+for step in $SEQUENCE; do
+  case "${step}" in
+    abort)
+        # We can't track anything from eos for file to  be aborted. This is done in the
+    # abort test script. So we sleep for the aproximate time of the abort. TODO: change wait for abort file from script
+        sleep 15
+        ;;
+    archive)
+        trackArchive
+        ;;
+    retrieve)
+        trackPrepare
+        ;;
+    evict)
+        trackEvict
+        ;;
+    delete)
+        trackDelete
+        ;;
+  esac
+done
+
+
+###############################################
+################## Results ####################
+###############################################
+TOTAL_FILES=$((${NB_FILES} * ${NB_DIRS}))
+IFS='|' read -a results_array <<< $(db_results)
+
+ARCHIVED=${results_array[0]}
+RETRIEVED=${results_array[1]}
+EVICTED=${results_array[2]}
+ABORTED="TODO"
+DELETED=${results_array[3]}
+
+names_arr=("ARCHIVED" "RETRIEVED" "EVICTED" "ABORTED" "DELETED")
+
+echo "###"
+echo "$(date +%s): Results:"
+echo "NB FILES / ARCHIVED / RETRIEVED / EVICTED / ABORTED / DELETED"
+echo "${TOTAL_FILES} / ${ARCHIVED} / ${RETRIEVED} / ${EVICTED} / ${ABORTED} / ${DELETED}"
+echo "###"
+
+test -z ${COMMENT} || annotate "test ${TESTID} FINISHED" "Summary:</br>NB_FILES: $((${NB_FILES} * ${NB_DIRS}))</br>ARCHIVED: ${ARCHIVED}<br/>RETRIEVED: ${RETRIEVED}</br>EVICTED: ${EVICTED}<br/>DELETED: ${DELETED}" 'test,end'
+
+TOTAL_FILES=$(( ${NB_FILES} * ${NB_DIRS} ))
+for i in "${!resuls_array[@]}"; do
+  if [[ ${TOTAL_FILES} -ne ${results_array[$i]} ]]; then
+    echo "ERROR: ${names_arr[${i}]} count value ${results_arr[${i}]} does not match the expected value ${TOTAL_FILES}."
+  fi
+done
+
+
+
+
+## DB Helper Functions
+
+db_results() {
+  sqlite3 ${DB_NAME} "SELECT SUM(archived), SUM(staged), SUM(evicted), SUM(deleted) FROM ${TEST_TABLE};"
+}
+
