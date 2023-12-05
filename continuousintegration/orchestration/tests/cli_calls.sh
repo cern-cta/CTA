571cdf2b77e4fde6ca0a2c9fc6e7bf814ac493e4
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2023-12-05 22:09:39 +0100

    Fix error ouput redirection

--- continuousintegration/orchestration/tests/cli_calls.sh
+++ continuousintegration/orchestration/tests/cli_calls.sh
@@ -16,19 +16,17 @@
 #               submit itself to any jurisdiction.
 
 
+archive='XRD_LOGLEVEL=Dump xrdcp - root://${EOSINSTANCE}/${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NUM'
+
 if [[ "${CLI_TARGET}" == "xrd" ]]; then
-  archive='XRD_LOGLEVEL=Dump xrdcp - root://${EOSINSTANCE}/${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NUM 2>${ERROR_DIR}/${subdir}TEST_FILE_NUM'
+  retrieve='XRD_LOGLEVEL=Dump KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 xrdfs ${EOSINSTANCE} prepare -s ${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NAME?activity=T0Reprocess'
 
-  retrieve='XRD_LOGLEVEL=Dump KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 xrdfs ${EOSINSTANCE} prepare -s ${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NAME?activity=T0Reprocess 2>${ERROR_DIR}/${subdir}RETRIEVE_TEST_FILE_NAME'
-
-  evict='XrdSecPROTOCOL=krb5 KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0  xrdfs ${EOSINSTANCE} prepare -e ${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NAME > /dev/null'
+  evict='XrdSecPROTOCOL=krb5 KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0  xrdfs ${EOSINSTANCE} prepare -e ${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NAME'
 
   delete='KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 eos root://${EOSINSTANCE} rm -Fr ${EOS_DIR} &'
 
 elif [[ "${CLI_TARGET}" == "gfal2" ]]; then
-  archive='XRD_LOGLEVEL=Dump xrdcp - root://${EOSINSTANCE}/${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NUM 2>${ERROR_DIR}/${subdir}TEST_FILE_NUM'
-
-  retrieve='XRD_LOGLEVEL=Dump KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 gfal-bringonline ${GFAL2_PROTOCOL}://${EOSINSTANCE}/${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NAME 2>${ERROR_DIR}/${subdir}RETRIEVE_TEST_FILE_NAME'
+  retrieve='XRD_LOGLEVEL=Dump KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 gfal-bringonline ${GFAL2_PROTOCOL}://${EOSINSTANCE}/${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NAME'
 
   evict='XrdSecPROTOCOL=krb5 KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 gfal-evict ${GFAL2_PROTOCOL}://${EOSINSTANCE}/${EOS_DIR}/${subdir}/${subdir}TEST_FILE_NAME'
 
