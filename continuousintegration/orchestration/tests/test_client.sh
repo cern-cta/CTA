d698484312bc8a511f087c925627cc41ff6d3a07
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2023-12-11 11:14:06 +0100

    Improve logging

--- continuousintegration/orchestration/tests/test_client.sh
+++ continuousintegration/orchestration/tests/test_client.sh
@@ -151,7 +151,7 @@ echo " Deleting files:"
 kubectl -n ${NAMESPACE} exec client -- bash -c "${TEST_PRERUN} && /root/client_delete.sh ${TEST_POSTRUN}" || exit 1
 kubectl -n ${NAMESPACE} exec ctaeos -- bash /root/grep_xrdlog_mgm_for_error.sh || exit 1
 
-echo "$(date +%s): "
+echo "$(date +%s): Waiting for tracker process to finish. "
 wait "${TRACKER_PID}"
 if [[ $? == 1 ]]; then
   echo "Some files were lost during tape workflow."
