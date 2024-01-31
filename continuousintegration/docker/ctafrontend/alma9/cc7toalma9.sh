4c3d9b020609262d9d3d491ed52c77c462a2b0af
Jorge Camarero Vera <jorge.camarero@cern.ch>
2024-01-31 12:08:03 +0100

    Fix cta.spec.in for cc7 and remove TNS names from cc7toalma9 script

--- continuousintegration/docker/ctafrontend/alma9/cc7toalma9.sh
+++ continuousintegration/docker/ctafrontend/alma9/cc7toalma9.sh
@@ -4,6 +4,5 @@ wget https://download.oracle.com/otn_software/linux/instantclient/2112000/el9/or
 wget https://download.oracle.com/otn_software/linux/instantclient/2112000/el9/oracle-instantclient-devel-21.12.0.0.0-1.el9.x86_64.rpm
 yum install -y oracle-instantclient-basic-21.12.0.0.0-1.el9.x86_64.rpm
 yum install -y oracle-instantclient-devel-21.12.0.0.0-1.el9.x86_64.rpm
-yum install -y oracle-instantclient-tnsnames.ora
 
 git apply continuousintegration/docker/ctafrontend/alma9/code.patch
