b4f06c8d4878aadcf10fd676e49ef23ff6fc1624
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2024-04-04 13:50:02 +0200

    Resubmit failed evict requests

--- continuousintegration/orchestration/tests/client_evict.sh
+++ continuousintegration/orchestration/tests/client_evict.sh
@@ -21,23 +21,29 @@ echo "$(date +%s): Trigerring EOS evict workflow as poweruser1:powerusers (12001
 TO_EVICT=$((${NB_FILES}*${NB_DIRS}))
 echo "$(date +%s): $TO_EVICT files to be evicted from EOS using 'xrdfs prepare -e'"
 # We need the -e as we are evicting the files from disk cache (see xrootd prepare definition)
-failed_tmp=$(mktemp)
 for (( subdir=0; subdir < ${NB_DIRS}; subdir++ )); do
   command_str=$(eval echo "${evict}")
-  command_str+=" 1>/dev/null || echo ${subdir}-TEST_FILE_NAME >> ${failed_tmp} "
-
   seq -w 0 $((${NB_FILES} - 1)) | xargs --max-procs=100 -n 100 -iTEST_FILE_NAME bash -c "$command_str"
 done
 
 sleep 1
 
-# Retry once the failed requests
-while read -r line; do
-  IFS=- read -r -d subdir resubmit_file_name <<< "${line}"
-  echo "Resubmitting evict request for file: ${subdir}/${subdir}${resubmit_file_name}"
-  XrdSecPROTOCOL=krb5 KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0  xrdfs ${EOSINSTANCE} prepare -e ${EOS_DIR}/${subdir}/${subdir}${resubmit_file_name}
-done < "${failed_tmp}"
+LEFTOVER=0
+for ((subdir=0; subdir < ${NB_DIRS}; subdir++)); do
+  LEFTOVER=$(( ${LEFTOVER} + $(eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir} | egrep '^d[1-9][0-9]*::t1' | wc -l) ))
+done
 
+# Resubmit leftover requests
+if [[ ${LEFTOVER} -ne 0 ]]; then
+  echo "$LEFTOVER files not evicted, trying a second time."
+  for (( subdir=0; subdir < ${NB_DIRS}; subdir++ )); do
+    for file in $(eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir} | egrep '^d1::t1' | awk -F' ' '{print $10}'); do
+      echo "${file}" | xargs --max-procs=1 -n1 -iTEST_FILE_NAME bash -c "$command_str"
+    done
+  done
+fi
+
+# Check again for leftovers
 LEFTOVER=0
 for ((subdir=0; subdir < ${NB_DIRS}; subdir++)); do
   LEFTOVER=$(( ${LEFTOVER} + $(eos root://${EOSINSTANCE} ls -y ${EOS_DIR}/${subdir} | egrep '^d[1-9][0-9]*::t1' | wc -l) ))
