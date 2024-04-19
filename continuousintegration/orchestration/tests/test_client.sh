79e23e494128f8bdeef34f0aebccf711bddb02e1
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2024-04-19 16:55:43 +0200

    Fix tail command not being killed

--- continuousintegration/orchestration/tests/test_client.sh
+++ continuousintegration/orchestration/tests/test_client.sh
@@ -83,7 +83,7 @@ TEST_POSTRUN=""
 VERBOSE=1
 if [[ $VERBOSE == 1 ]]; then
   TEST_PRERUN="tail -v -f /mnt/logs/tpsrv0*/rmcd/cta/cta-rmcd.log & export TAILPID=\$! && ${TEST_PRERUN}"
-  TEST_POSTRUN=" && kill \${TAILPID} &> /dev/null"
+  TEST_POSTRUN="; kill \${TAILPID} &> /dev/null"
 fi
 
 echo
