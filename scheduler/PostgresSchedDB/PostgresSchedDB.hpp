bedbdb50b35d3972b9b9d3b893de49b92c6855eb
Jaroslav Guenther <JaroslavGuenther@cern.ch>
2024-04-22 10:28:27 +0200

    [PGSCHED] fix sonarcloud issues

--- scheduler/PostgresSchedDB/PostgresSchedDB.hpp
+++ scheduler/PostgresSchedDB/PostgresSchedDB.hpp
@@ -205,7 +205,7 @@ private:
     friend class PostgresSchedDB;
   public:
     PromotionToToExpandResult promotePendingRequestsForExpansion(size_t requestCount, log::LogContext& lc) override;
-    override ~RepackRequestPromotionStatistics() = default;
+    ~RepackRequestPromotionStatistics() = default override;
   private:
     RepackRequestPromotionStatistics();
   };
@@ -219,7 +219,7 @@ private:
     PromotionToToExpandResult promotePendingRequestsForExpansion(size_t requestCount, log::LogContext& lc) override {
       throw SchedulingLockNotHeld("In RepackRequestPromotionStatisticsNoLock::promotePendingRequestsForExpansion");
     }
-    override ~RepackRequestPromotionStatisticsNoLock() = default;
+    ~RepackRequestPromotionStatisticsNoLock() = default override;
   };
 
 };
