7ea14412403cc0cb313709299ffab8f53b71f45e
Niels Bugel <niels.alexander.bugel@cern.ch>
2024-06-17 14:33:36 +0200

    Added copyright notice

--- continuousintegration/ci_runner/compile_and_deploy.sh
+++ continuousintegration/ci_runner/compile_and_deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# @project      The CERN Tape Archive (CTA)
+# @copyright    Copyright © 2022 CERN
+# @license      This program is free software, distributed under the terms of the GNU General Public
+#               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
+#               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
+#               option) any later version.
+#
+#               This program is distributed in the hope that it will be useful, but WITHOUT ANY
+#               WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#               PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#               In applying this licence, CERN does not waive the privileges and immunities
+#               granted to it by virtue of its status as an Intergovernmental Organization or
+#               submit itself to any jurisdiction.
+
 # Help message
 usage() {
   echo "Performs the compilation of CTA through a dedicated Kubernetes pod."
