75b4d44c2608016f0f6809730977b5f399caabb3
Niels Bugel <niels.alexander.bugel@cern.ch>
2024-06-19 16:19:25 +0200

    Fixed some formatting and convention issues. Updated some descriptions

--- continuousintegration/ci_helpers/build_rpm.sh
+++ continuousintegration/ci_helpers/build_rpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # @project      The CERN Tape Archive (CTA)
-# @copyright    Copyright © 2022 CERN
+# @copyright    Copyright © 2024 CERN
 # @license      This program is free software, distributed under the terms of the GNU General Public
 #               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
 #               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
@@ -28,141 +28,141 @@ usage() {
   echo "      --skip-cmake          Skips the cmake step. Can be used if this script is executed multiple times in succession."
   echo "      --skip-unit-tests     Skips the unit tests. Speeds up the build time by not running the unit tests."
   echo "      --srpms-dir           The directory where the source rpms are located. Defaults to build_srpm/RPM/SRPMS (i.e. the output directory of the build_srpm.sh script). Note that this directory is relative to the root of the repository."
-  echo "      --vcs-version         Sets the VCS_VERSION variable in cmake."
-  echo "      --xrootd-ssi-version  Sets the XROOTD_SSI_PROTOBUF_INTERFACE_VERSION variable in cmake."
+  echo "      --vcs-version         Sets the VCS_VERSION variable in cmake. Defaults to \"dev\" if not provided."
+  echo "      --xrootd-ssi-version  Sets the XROOTD_SSI_PROTOBUF_INTERFACE_VERSION variable in cmake. If not provided, this will keep the existing XROOTD_SSI_PROTOBUF_INTERFACE_VERSION env variable if set, and otherwise will set it to \"dev\"."
 
   exit 1
 }
 
 build_rpm() {
 
-    # navigate to root directory
-    cd "$(dirname "$0")"
-    cd ../../
+  # navigate to root directory
+  cd "$(dirname "$0")"
+  cd ../../
 
-    # Default values
-    local JOBS=1
-    local INSTALL=false
-    local DISTRO=""
-    local SKIP_CMAKE=false
-    local SKIP_UNIT_TESTS=false
-    local SRPMS_DIR="build_srpm/RPM/SRPMS"
-    local VCS_VERSION="dev"
-    local XROOTD_SSI_VERSION="${XROOTD_SSI_PROTOBUF_INTERFACE_VERSION:-dev}"
+  # Default values
+  local num_jobs=1
+  local install=false
+  local distro=""
+  local skip_cmake=false
+  local skip_unit_tests=false
+  local srpms_dir="build_srpm/RPM/SRPMS"
+  local vcs_version="dev"
+  local xrootd_ssi_version="${XROOTD_SSI_PROTOBUF_INTERFACE_VERSION:-dev}"
 
-    # Parse command line arguments
-    while [[ "$#" -gt 0 ]]; do
-        case $1 in
-            -i|--install)
-                INSTALL=true
-                if [[ $# -gt 1 ]]; then
-                    DISTRO=$2
-                    shift
-                else
-                    echo "Error: --install requires an argument"
-                    exit 1
-                fi
-                ;;
-            -j|--jobs)
-                if [[ $# -gt 1 ]]; then
-                    JOBS=$2
-                    shift
-                else
-                    echo "Error: --jobs requires an argument"
-                    exit 1
-                fi
-                ;;
-            --skip-cmake) 
-                SKIP_CMAKE=true 
-                ;;
-            --skip-unit-tests) 
-                SKIP_UNIT_TESTS=true 
-                ;;
-            --srpms-dir)
-                if [[ $# -gt 1 ]]; then
-                    SRPMS_DIR=$2
-                    shift
-                else
-                    echo "Error: --srpms-dir requires an argument"
-                    exit 1
-                fi
-                ;;
-            --vcs-version) 
-                if [[ $# -gt 1 ]]; then
-                    VCS_VERSION=$2
-                    shift
-                else
-                    echo "Error: --vcs-version requires an argument"
-                    exit 1
-                fi
-                ;;
-            --xrootd-ssi-version) 
-                if [[ $# -gt 1 ]]; then
-                    XROOTD_SSI_VERSION=$2
-                    shift
-                else
-                    echo "Error: --vcs-version requires an argument"
-                    exit 1
-                fi
-                ;;
-            *)
-                echo "Invalid argument: $1"
-                usage 
-                ;;
-        esac
-        shift
-    done
-
-    # Setup
-    if [ "$INSTALL" = true ]; then
-        echo "Installing prerequisites..."
-        case $DISTRO in 
-            "alma9")
-                cp -f continuousintegration/docker/ctafrontend/alma9/repos/*.repo /etc/yum.repos.d/
-                cp -f continuousintegration/docker/ctafrontend/alma9/yum/pluginconf.d/versionlock.list /etc/yum/pluginconf.d/
-                yum -y install epel-release almalinux-release-devel
-                yum -y install wget gcc gcc-c++ cmake3 make rpm-build yum-utils
-                yum -y install yum-plugin-versionlock
-                ./continuousintegration/docker/ctafrontend/alma9/installOracle21.sh
-                yum-builddep --nogpgcheck -y $SRPMS_DIR/*
-                ;;
-            "cc7")
-                yum install -y devtoolset-11 cmake3 make rpm-build
-                yum -y install yum-plugin-priorities yum-plugin-versionlock
-                source /opt/rh/devtoolset-11/enable
-                yum-builddep --nogpgcheck -y $SRPMS_DIR/*
-                ;;
-            *)
-                echo "Unsupported distribution. Must be one of: [cc7, alma9]"
-                exit -1
-            ;;
-        esac
-    fi
-
-    # Cmake
-    if [ "$SKIP_CMAKE" = false ]; then
-        # Needs to be exported as cmake gets it from the environment
-        export XROOTD_SSI_PROTOBUF_INTERFACE_VERSION=$XROOTD_SSI_VERSION
-
-        CMAKE_OPTIONS=" -DVCS_VERSION=${VCS_VERSION}"
-        if [ "$SKIP_UNIT_TESTS" = true ]; then
-            CMAKE_OPTIONS+=" -DSKIP_UNIT_TESTS:STRING=1"
+  # Parse command line arguments
+  while [[ "$#" -gt 0 ]]; do
+    case $1 in
+      -i|--install)
+        install=true
+        if [[ $# -gt 1 ]]; then
+          distro="$2"
+          shift
+        else
+          echo "Error: -i|--install requires an argument"
+          exit 1
         fi
-        mkdir -p build_rpm
-        cd build_rpm
-        echo "Executing cmake..."
-        cmake3 ${CMAKE_OPTIONS} ..
-    else
-        echo "Skipping cmake..."
-        if [ ! -d build_rpm ]; then
-            echo "build_rpm/ directory does not exist. Ensure to run this script without skipping cmake first."
+        ;;
+      -j|--jobs)
+        if [[ $# -gt 1 ]]; then
+          num_jobs="$2"
+          shift
+        else
+          echo "Error: -j|--jobs requires an argument"
+          exit 1
         fi
-        cd build_rpm
-    fi
+        ;;
+      --skip-cmake) 
+        skip_cmake=true 
+        ;;
+      --skip-unit-tests) 
+        skip_unit_tests=true 
+        ;;
+      --srpms-dir)
+        if [[ $# -gt 1 ]]; then
+            srpms_dir="$2"
+            shift
+        else
+            echo "Error: --srpms-dir requires an argument"
+            exit 1
+        fi
+        ;;
+      --vcs-version) 
+        if [[ $# -gt 1 ]]; then
+          vcs_version="$2"
+          shift
+        else
+          echo "Error: --vcs-version requires an argument"
+          exit 1
+        fi
+        ;;
+      --xrootd-ssi-version) 
+        if [[ $# -gt 1 ]]; then
+          xrootd_ssi_version="$2"
+          shift
+        else
+          echo "Error: --vcs-version requires an argument"
+          exit 1
+        fi
+        ;;
+      *)
+        echo "Invalid argument: $1"
+        usage 
+        ;;
+    esac
+    shift
+  done
 
-    # Make
-    echo "Executing make..."
-    make cta_rpm -j $JOBS
+  # Setup
+  if [ "${install}" = true ]; then
+    echo "Installing prerequisites..."
+    case ${distro} in 
+      "alma9")
+        cp -f continuousintegration/docker/ctafrontend/alma9/repos/*.repo /etc/yum.repos.d/
+        cp -f continuousintegration/docker/ctafrontend/alma9/yum/pluginconf.d/versionlock.list /etc/yum/pluginconf.d/
+        yum -y install epel-release almalinux-release-devel
+        yum -y install wget gcc gcc-c++ cmake3 make rpm-build yum-utils
+        yum -y install yum-plugin-versionlock
+        ./continuousintegration/docker/ctafrontend/alma9/installOracle21.sh
+        yum-builddep --nogpgcheck -y ${srpms_dir}/*
+        ;;
+      "cc7")
+        yum install -y devtoolset-11 cmake3 make rpm-build
+        yum -y install yum-plugin-priorities yum-plugin-versionlock
+        source /opt/rh/devtoolset-11/enable
+        yum-builddep --nogpgcheck -y ${srpms_dir}/*
+        ;;
+      *)
+        echo "Unsupported distribution. Must be one of: [cc7, alma9]"
+        exit -1
+      ;;
+    esac
+  fi
+
+  # Cmake
+  if [ "$skip_cmake" = false ]; then
+    # Needs to be exported as cmake gets it from the environment
+    export XROOTD_SSI_PROTOBUF_INTERFACE_VERSION=${xrootd_ssi_version}
+
+    cmake_options=" -DVCS_VERSION=${vcs_version}"
+    if [ "${skip_unit_tests}" = true ]; then
+      cmake_options+=" -Dskip_unit_tests:STRING=1"
+    fi
+    mkdir -p build_rpm
+    cd build_rpm
+    echo "Executing cmake..."
+    cmake3 ${cmake_options} ..
+  else
+    echo "Skipping cmake..."
+    if [ ! -d build_rpm ]; then
+      echo "build_rpm/ directory does not exist. Ensure to run this script without skipping cmake first."
+    fi
+    cd build_rpm
+  fi
+
+  # Make
+  echo "Executing make..."
+  make cta_rpm -j ${num_jobs}
 }
 
 build_rpm "$@"
--- continuousintegration/ci_helpers/build_srpm.sh
+++ continuousintegration/ci_helpers/build_srpm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # @project      The CERN Tape Archive (CTA)
-# @copyright    Copyright © 2022 CERN
+# @copyright    Copyright © 2024 CERN
 # @license      This program is free software, distributed under the terms of the GNU General Public
 #               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
 #               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
@@ -18,116 +18,116 @@
 set -e
 
 usage() {
-    echo ""
-    echo "Builds the srpms."
-    echo "Usage: $0 [-i|--install <distribution>] [-j|--jobs <num-jobs>] [--skip-cmake] [--vcs-version <vcs-version>]"
-    echo ""
-    echo "Flags:"
-    echo "  -i, --install             Perform the setup and installation part of the required yum packages. Should specify which distribution to use. Should be one of [cc7, alma9]."
-    echo "  -j, --jobs                How many jobs to use for cmake/make."
-    echo "      --skip-cmake          Skips the cmake step. Can be used if this script is executed multiple times in succession."
-    echo "      --vcs-version         Sets the VCS_VERSION variable in cmake."
+  echo ""
+  echo "Builds the srpms."
+  echo "Usage: $0 [-i|--install <distribution>] [-j|--jobs <num-jobs>] [--skip-cmake] [--vcs-version <vcs-version>]"
+  echo ""
+  echo "Flags:"
+  echo "  -i, --install             Perform the setup and installation part of the required yum packages. Should specify which distribution to use. Should be one of [cc7, alma9]."
+  echo "  -j, --jobs                How many jobs to use for cmake/make."
+  echo "      --skip-cmake          Skips the cmake step. Can be used if this script is executed multiple times in succession."
+  echo "      --vcs-version         Sets the VCS_VERSION variable in cmake. Defaults to \"dev\" if not provided."
 
-    exit 1
+  exit 1
 }
 
 build_srpm() {
 
-    # navigate to root directory
-    cd "$(dirname "$0")"
-    cd ../../
+  # navigate to root directory
+  cd "$(dirname "$0")"
+  cd ../../
 
-    # Default values
-    local JOBS=1
-    local INSTALL=false
-    local DISTRO=""
-    local SKIP_CMAKE=false
-    local VCS_VERSION="dev"
+  # Default values
+  local num_jobs=1
+  local install=false
+  local distro=""
+  local skip_cmake=false
+  local vcs_version="dev"
 
-    # Parse command line arguments
-    while [[ "$#" -gt 0 ]]; do
-        case $1 in
-            -i|--install)
-                INSTALL=true
-                if [[ $# -gt 1 ]]; then
-                    DISTRO=$2
-                    shift
-                else
-                    echo "Error: --install requires an argument"
-                    exit 1
-                fi
-                ;;
-            -j|--jobs)
-                if [[ $# -gt 1 ]]; then
-                    JOBS=$2
-                    shift
-                else
-                    echo "Error: --jobs requires an argument"
-                    exit 1
-                fi
-                ;;
-            --skip-cmake) 
-                SKIP_CMAKE=true 
-                ;;
-            --vcs-version) 
-                if [[ $# -gt 1 ]]; then
-                    VCS_VERSION=$2
-                    shift
-                else
-                    echo "Error: --vcs-version requires an argument"
-                    exit 1
-                fi
-                ;;
-            *)
-                echo "Invalid argument: $1"
-                usage 
-                ;;
-        esac
-        shift
-    done
-
-    # Setup
-    if [ "$INSTALL" = true ]; then
-        echo "Installing prerequisites..."
-        case $DISTRO in 
-            "alma9")
-                cp -f continuousintegration/docker/ctafrontend/alma9/repos/*.repo /etc/yum.repos.d/
-                cp -f continuousintegration/docker/ctafrontend/alma9/yum/pluginconf.d/versionlock.list /etc/yum/pluginconf.d/
-                yum install -y epel-release almalinux-release-devel
-                yum install -y wget gcc gcc-c++ cmake3 make rpm-build yum-utils
-                ./continuousintegration/docker/ctafrontend/alma9/installOracle21.sh
-                ;;
-            "cc7")
-                yum install -y devtoolset-11 cmake3 make rpm-build
-                source /opt/rh/devtoolset-11/enable
-                ;;
-            *)
-                echo "Unsupported distribution. Must be one of: [cc7, alma9]"
-                exit -1
-            ;;
-        esac
-    fi
-
-    # Cmake
-    if [ "$SKIP_CMAKE" = false ]; then
-        CMAKE_OPTIONS=" -DVCS_VERSION=${VCS_VERSION}"
-
-        mkdir -p build_srpm
-        cd build_srpm
-        echo "Executing cmake..."
-        cmake3 -DPackageOnly:Bool=true ${CMAKE_OPTIONS} ..
-    else
-        echo "Skipping cmake..."
-        # build_srpm should exist
-        if [ ! -d build_srpm ]; then
-            echo "build_srpm/ directory does not exist. Ensure to run this script without skipping cmake first."
+  # Parse command line arguments
+  while [[ "$#" -gt 0 ]]; do
+    case $1 in
+      -i|--install)
+        install=true
+        if [[ $# -gt 1 ]]; then
+          distro="$2"
+          shift
+        else
+          echo "Error: --install requires an argument"
+          exit 1
         fi
-        cd build_srpm
-    fi
+        ;;
+      -j|--jobs)
+        if [[ $# -gt 1 ]]; then
+          num_jobs="$2"
+          shift
+        else
+          echo "Error: --jobs requires an argument"
+          exit 1
+        fi
+        ;;
+      --skip-cmake) 
+        skip_cmake=true 
+        ;;
+      --vcs-version) 
+        if [[ $# -gt 1 ]]; then
+          vcs_version="$2"
+          shift
+        else
+          echo "Error: --vcs-version requires an argument"
+          exit 1
+        fi
+        ;;
+      *)
+        echo "Invalid argument: $1"
+        usage 
+        ;;
+    esac
+    shift
+  done
 
-    # Make
-    echo "Executing make..."
-    make cta_srpm  -j $JOBS
+  # Setup
+  if [ "${install}" = true ]; then
+    echo "Installing prerequisites..."
+    case ${distro} in 
+      "alma9")
+        cp -f continuousintegration/docker/ctafrontend/alma9/repos/*.repo /etc/yum.repos.d/
+        cp -f continuousintegration/docker/ctafrontend/alma9/yum/pluginconf.d/versionlock.list /etc/yum/pluginconf.d/
+        yum install -y epel-release almalinux-release-devel
+        yum install -y wget gcc gcc-c++ cmake3 make rpm-build yum-utils
+        ./continuousintegration/docker/ctafrontend/alma9/installOracle21.sh
+        ;;
+      "cc7")
+        yum install -y devtoolset-11 cmake3 make rpm-build
+        source /opt/rh/devtoolset-11/enable
+        ;;
+      *)
+        echo "Unsupported distribution. Must be one of: [cc7, alma9]"
+        exit -1
+      ;;
+    esac
+  fi
+
+  # Cmake
+  if [ "${skip_cmake}" = false ]; then
+    cmake_options=" -DVCS_VERSION=${vcs_version}"
+
+    mkdir -p build_srpm
+    cd build_srpm
+    echo "Executing cmake..."
+    cmake3 -DPackageOnly:Bool=true ${cmake_options} ..
+  else
+    echo "Skipping cmake..."
+    # build_srpm should exist
+    if [ ! -d build_srpm ]; then
+      echo "build_srpm/ directory does not exist. Ensure to run this script without skipping cmake first."
+    fi
+    cd build_srpm
+  fi
+
+  # Make
+  echo "Executing make..."
+  make cta_srpm  -j ${num_jobs}
 }
 
 build_srpm "$@"
