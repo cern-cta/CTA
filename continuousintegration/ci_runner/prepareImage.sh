a9aab3e9522e70d3b0923c0b9b283519c619be8a
Niels Bugel <niels.alexander.bugel@cern.ch>
2024-06-19 16:02:52 +0200

    Cleaned up scripts and paved the way for inclusion of build scripts

--- continuousintegration/ci_runner/prepareImage.sh
+++ continuousintegration/ci_runner/prepareImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # @project      The CERN Tape Archive (CTA)
-# @copyright    Copyright © 2022 CERN
+# @copyright    Copyright © 2024 CERN
 # @license      This program is free software, distributed under the terms of the GNU General Public
 #               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
 #               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
@@ -15,33 +15,85 @@
 #               granted to it by virtue of its status as an Intergovernmental Organization or
 #               submit itself to any jurisdiction.
 
-cd ~/CTA
+usage() {
+  echo "Usage: $0 [-s|--rpm-src <rpm source>] -t|--tag <image_tag>"
+  echo "  -s, --rpm-src     Path to the RPMs to be installed. For example \"-s ~/CTA-build/RPM/RPMS/x86_64\""
+  echo "  -t, --tag         Docker image tag. For example \"-t dev\""
+  exit 1
+}
 
-rpm_source=$1
-image_tag=$2
-rpm_dir="build_rpm/RPM/RPMS/x86_64"
+arePathsEqual() {
+  local path1 = $(readlink -f "$1")
+  local path2 = $(readlink -f "$2")
+  return ${path1} = ${path2}
+}
 
-if [ -z "${rpm_source}" ]; then
-  echo "You should specify the path to the RPMs to be installed. Ex: ~/CTA-build/RPM/RPMS/x86_64";
-  exit 1;
-fi
+prepareImage() {
 
-if [ -z "${image_tag}" ]; then
-  echo "You should specify the docker image tag. Ex: dev";
-  exit 1;
-fi
+  # navigate to root directory
+  cd "$(dirname "$0")"
+  cd ../../
 
-trap "rm -rf $rpm_dir" EXIT
 
-mkdir -p $rpm_dir
-cp -r $rpm_source $rpm_dir
+  # Default values
+  local rpm_src=""
+  local image_tag=""
+  local rpm_default_src="build_rpm/RPM/RPMS/x86_64"
 
-if [ "$(cat /etc/redhat-release | grep -c 'AlmaLinux release 9')" -eq 0 ]; then
-  echo "Not running on AlmaLinux 9"
-  echo "sudo docker build . -f continuousintegration/docker/ctafrontend/cc7/Dockerfile -t ctageneric:${image_tag}"
-  sudo docker build . -f continuousintegration/docker/ctafrontend/cc7/Dockerfile -t ctageneric:${image_tag}
-else
-  echo "Running on AlmaLinux 9"
-  echo "podman build . -f continuousintegration/docker/ctafrontend/alma9/Dockerfile -t ctageneric:${image_tag} --network host"
-  podman build . -f continuousintegration/docker/ctafrontend/alma9/Dockerfile -t ctageneric:${image_tag} --network host
-fi
+  while [[ "$#" -gt 0 ]]; do
+    case $1 in
+      -s | --rpm-src)
+        if [[ $# -gt 1 ]]; then
+          rpm_src="$2"
+          shift
+        else
+          echo "Error: -s|--rpm-src requires an argument"
+          exit 1
+        fi
+        ;;
+      -t | --tag)
+        if [[ $# -gt 1 ]]; then
+          image_tag="$2"
+          shift
+        else
+          echo "Error: -t|--tag requires an argument"
+          exit 1
+        fi
+        ;;
+      *) 
+        usage
+        ;;
+    esac
+    shift
+  done
+
+  if [ -z "${image_tag}" ]; then
+    echo "Please specify the docker image tag";
+    usage
+    exit 1;
+  fi
+
+  if [ -z "${rpm_src}" ]; then
+    echo "No explicit rpm source specified, using default: CTA/${rpm_default_src}"
+    if [ ! -d "${rpm_default_src}" ]; then
+      echo "Default rpm source not found. Please build the rpms or provide an alternative valid path."
+      exit 1;
+    fi
+  elif ! arePathsEqual ${rpm_src} ${rpm_default_src}; then
+    trap "rm -rf ${rpm_default_src}" EXIT
+    mkdir -p ${rpm_default_src}
+    cp -r ${rpm_src} ${rpm_default_src}
+  fi
+
+  if [ "$(cat /etc/redhat-release | grep -c 'AlmaLinux release 9')" -eq 0 ]; then
+    echo "Not running on AlmaLinux 9"
+    echo "sudo docker build . -f continuousintegration/docker/ctafrontend/cc7/Dockerfile -t ctageneric:${image_tag}"
+    sudo docker build . -f continuousintegration/docker/ctafrontend/cc7/Dockerfile -t ctageneric:${image_tag}
+  else
+    echo "Running on AlmaLinux 9"
+    echo "podman build . -f continuousintegration/docker/ctafrontend/alma9/Dockerfile -t ctageneric:${image_tag} --network host"
+    podman build . -f continuousintegration/docker/ctafrontend/alma9/Dockerfile -t ctageneric:${image_tag} --network host
+  fi
+}
+
+prepareImage "$@"
--- continuousintegration/ci_runner/redeploy.sh
+++ continuousintegration/ci_runner/redeploy.sh
@@ -22,81 +22,98 @@ usage() {
   echo "Will (re)deploy the local minikube instance with the latest rpms."
   echo "These rpms are assumed to be located in CTA/build_rpm, as done by the build_rpm.sh script in ci_helpers."
   echo ""
-  echo "Usage: $0 [-n <namespace>] [-t <tag>] [-d <root directory>]"
+  echo "Usage: $0 [-n|--namespace <namespace>] [-t|--tag <tag>] [-s|--rpm-src <rpm source>]"
   echo ""
   echo "Flags:"
-  echo "  -n, --namespace <namespace>: Specify the Kubernetes namespace (optional)"
-  echo "  -t, --tag <tag>: Specify a tag (optional)"
-  echo "  -d, --root-dir <root directory>: Specify the root directory where the CTA directory is located (optional)"
+  echo "  -n, --namespace <namespace>:  Specify the Kubernetes namespace. Defaults to \"dev\" if not provided."
+  echo "  -t, --tag <tag>:              Image tag to use. Defaults to \"dev\" if not provided."
+  echo "  -s, --rpm-src <rpm source>:   Path to the RPMs that should be deployed. Defaults to CTA/build_rpm/RPM/RPMS/x86_64 if not provided."
   exit 1
 }
 
-# Default values
-KUBE_NAMESPACE="dev"
-IMAGE_TAG="dev"
-ROOT_DIR="~"
+redeploy() {
+  # Default values
+  local kube_namespace="dev"
+  local image_tag="dev"
+  local rpm_src="build_rpm/RPM/RPMS/x86_64"
 
-# Parse command line arguments
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-    -n | --namespace)
-      KUBE_NAMESPACE="$2"
-      shift
-      ;;
-    -t | --tag)
-      IMAGE_TAG="$2"
-      shift
-      ;;
-    -d | --root-dir)
-      ROOT_DIR="$2"
-      shift
-      ;;
-    *) 
-      usage
-      ;;
-  esac
-  shift
-done
+  # Parse command line arguments
+  while [[ "$#" -gt 0 ]]; do
+    case $1 in
+      -n | --namespace)
+        if [[ $# -gt 1 ]]; then
+          kube_namespace="$2"
+          shift
+        else
+          echo "Error: -n|--namespace requires an argument"
+          exit 1
+        fi
+        ;;
+      -s | --rpm-src)
+        if [[ $# -gt 1 ]]; then
+          rpm_src="$2"
+          shift
+        else
+          echo "Error: -s|--rpm-src requires an argument"
+          exit 1
+        fi
+        ;;
+      -t | --tag)
+        if [[ $# -gt 1 ]]; then
+          image_tag="$2"
+          shift
+        else
+          echo "Error: -t|--tag requires an argument"
+          exit 1
+        fi
+        ;;
+      *) 
+        usage
+        ;;
+    esac
+    shift
+  done
 
-if [ "$#" -ge 3 ]; then
+  if [ "$#" -ge 3 ]; then
     usage
     exit 1
-fi
+  fi
 
-# Script should be run as cirunner
-if [[ $(whoami) != 'cirunner' ]]; then
+  # Script should be run as cirunner
+  if [[ $(whoami) != 'cirunner' ]]; then
     echo "Current user is $(whoami), aborting. Script must run as cirunner"
     exit 1
-fi
+  fi
 
-# Delete previous instance, if it exists
-if ! kubectl get namespace "$NAMESPACE" &> /dev/null; then
-  echo "Deleting the old namespace"
-  cd $ROOT_DIR/CTA/continuousintegration/orchestration/
-  ./delete_instance.sh -n $KUBE_NAMESPACE
-fi
+  ###################################################################################################
 
-# Clear the old image and namespace
-if podman inspect ctageneric:$IMAGE_TAG &> /dev/null; then
-  echo "Deleting old image and removing it from minikube"
-  podman rmi ctageneric:$IMAGE_TAG
-  minikube image rm localhost/ctageneric:$IMAGE_TAG
-fi
+  # navigate to root directory
+  cd "$(dirname "$0")"
+  cd ../../
 
-###################################################################################################
+  # Delete previous instance, if it exists
+  if ! kubectl get namespace "$NAMESPACE" &> /dev/null; then
+    echo "Deleting the old namespace"
+    ./continuousintegration/orchestration/delete_instance.sh -n ${kube_namespace}
+  fi
 
+  # Clear the old image and namespace
+  if podman inspect ctageneric:${image_tag} &> /dev/null; then
+    echo "Deleting old image and removing it from minikube"
+    podman rmi ctageneric:${image_tag}
+    minikube image rm localhost/ctageneric:${image_tag}
+  fi
 
-## Create and load the new images
-# Prepare new image
-echo "Preparing new image"
-cd $ROOT_DIR/CTA
-# specific to alma9
-echo "Building image based on $ROOT_DIR/CTA/build_rpm"
-podman build . -f continuousintegration/docker/ctafrontend/alma9/Dockerfile -t ctageneric:${IMAGE_TAG} --network host
-echo "Loading new image into minikube"
-minikube image load  localhost/ctageneric:$IMAGE_TAG
+  ## Create and load the new image
+  echo "Building image based on ${rpm_src}"
+  ./continuousintegration/ci_runner/prepareImage.sh --tag ${image_tag} --rpm-src ${rpm_src}
+  echo "Loading new image into minikube"
+  minikube image load  localhost/ctageneric:${image_tag}
 
-# Redeploy containers
-echo "Redeploying container"
-cd $ROOT_DIR/CTA/continuousintegration/orchestration
-./create_instance.sh -n $KUBE_NAMESPACE -i $IMAGE_TAG -D -O -d internal_postgres.yaml
+  # Redeploy containers
+  echo "Redeploying container"
+  cd continuousintegration/orchestration
+  ./create_instance.sh -n $kube_namespace -i ${image_tag} -D -O -d internal_postgres.yaml
+}
+
+redeploy "$@"
