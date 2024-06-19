17c2153a312ca26b2701ceaea2e2aa01fcff4185
Jaroslav Guenther <JaroslavGuenther@cern.ch>
2024-06-19 16:23:10 +0200

    debugging toy example of getNextArchiveJobsToReportBatch()

--- scheduler/rdbms/postgres/ArchiveJobQueue.hpp
+++ scheduler/rdbms/postgres/ArchiveJobQueue.hpp
@@ -335,7 +335,7 @@ struct ArchiveJobQueueRow {
     "FROM ARCHIVE_JOB_QUEUE "
     "WHERE "
       "TAPE_POOL = :TAPE_POOL "
-      "AND STATUS = :STATUS "
+      "AND STATUS IN (:STATUS) "
       "AND MOUNT_ID = :MOUNT_ID "
     "ORDER BY PRIORITY DESC, JOB_ID "
       "LIMIT :LIMIT";
@@ -394,7 +394,7 @@ struct ArchiveJobQueueRow {
             "LAST_MOUNT_WITH_FAILURE AS LAST_MOUNT_WITH_FAILURE,"
             "MAX_TOTAL_RETRIES AS MAX_TOTAL_RETRIES "
             "FROM ARCHIVE_JOB_QUEUE "
-            "WHERE STATUS = :STATUS "
+            "WHERE STATUS IN (:STATUS) "
             "ORDER BY PRIORITY DESC, TAPE_POOL "
             "LIMIT :LIMIT";
     std::string sqlstatuspart;
