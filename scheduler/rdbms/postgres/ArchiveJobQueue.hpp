2c369b3c678b901da0ed7cdf870bdaa08c1d3c8f
Jaroslav Guenther <JaroslavGuenther@cern.ch>
2024-06-24 16:13:58 +0200

    debugging toy example of getNextArchiveJobsToReportBatch()

--- scheduler/rdbms/postgres/ArchiveJobQueue.hpp
+++ scheduler/rdbms/postgres/ArchiveJobQueue.hpp
@@ -335,20 +335,18 @@ struct ArchiveJobQueueRow {
     "FROM ARCHIVE_JOB_QUEUE "
     "WHERE "
       "TAPE_POOL = :TAPE_POOL "
-      "AND STATUS = ANY(:STATUS) "
+      "AND STATUS = ANY(ARRAY[:STATUS]) "
       "AND MOUNT_ID = :MOUNT_ID "
     "ORDER BY PRIORITY DESC, JOB_ID "
       "LIMIT :LIMIT";
 
     std::string ss;
-    ss = "'{";
     for (const auto &jstatus : statusList) {
-      ss += to_string(jstatus);
+      ss += std::string("'") + to_string(jstatus) + std::string("'");
       if (&jstatus != &statusList.back()) {
         ss += ",";
       }
     }
-    ss += "}'::archive_job_status[]";
     auto stmt = conn.createStmt(sql);
     stmt.bindString(":TAPE_POOL", tapepool);
     stmt.bindString(":STATUS", ss);
@@ -400,19 +398,17 @@ struct ArchiveJobQueueRow {
             "LAST_MOUNT_WITH_FAILURE AS LAST_MOUNT_WITH_FAILURE,"
             "MAX_TOTAL_RETRIES AS MAX_TOTAL_RETRIES "
             "FROM ARCHIVE_JOB_QUEUE "
-            "WHERE STATUS = ANY(:STATUS) "
+            "WHERE STATUS = ANY(ARRAY[:STATUS]) "
             "ORDER BY PRIORITY DESC, TAPE_POOL "
             "LIMIT :LIMIT";
 
     std::string ss;
-    ss = "'{";
     for (const auto &jstatus : statusList) {
-      ss += to_string(jstatus);
+      ss += std::string("'") + to_string(jstatus) + std::string("'");
       if (&jstatus != &statusList.back()) {
         ss += ",";
       }
     }
-    ss += "}'::archive_job_status[]";
     auto stmt = conn.createStmt(sql);
     stmt.bindString(":STATUS", ss);
     stmt.bindUint32(":LIMIT", limit);
