600f06deb50fd03a5e332e65a767cfbcb543e448
Konstantina Skovola <konstantina.skovola@cern.ch>
2024-07-03 13:23:41 +0200

    Add some debug stuff

--- cta-get-free-disk-space.sh
+++ cta-get-free-disk-space.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-diskInstance=${1:-localhost}
+# for debugging
+# echo "arguments passed: $@"
+
+# diskInstance=${1:-localhost}
+diskInstance=localhost
 output=$(/usr/bin/eos root://$diskInstance space ls -m)
 # we're interested in the result for the default namespace
 freespace=$(echo "$output" | awk -F' ' '/name=default/ { for (i=1; i<=NF; i++) if ($i ~ /sum.stat.statfs.freebytes\?configstatus@rw=/) print $i }' | awk -F'=' '{print $2}')
