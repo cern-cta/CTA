2b3b8629be2a4007afab3c2ded142e05f598c572
guenther <jaroslav.guenther@cern.ch>
2024-08-21 01:09:04 +0200

    fix how ArchiveRdbJobs are handled in ArchiveMount

--- scheduler/rdbms/RelationalDB.cpp
+++ scheduler/rdbms/RelationalDB.cpp
@@ -176,16 +176,18 @@ std::list<std::unique_ptr<SchedulerDatabase::ArchiveJob> > RelationalDB::getNext
   auto resultSet = schedulerdb::postgres::ArchiveJobQueueRow::selectJobsByJobID(sqlconn, jobIDsList);
   logContext.log(log::DEBUG, "In RelationalDB::getNextArchiveJobsToReportBatch(): After getting archive row AJS_ToReportToDiskRunnerForTransfer.");
   //std::list<cta::schedulerdb::postgres::ArchiveJobQueueRow> jobs;
-  std::list<schedulerdb::ArchiveRdbJob> dbjobs;
+  //std::list<schedulerdb::ArchiveRdbJob> dbjobs;
   logContext.log(log::DEBUG, "In RelationalDB::getNextArchiveJobsToReportBatch(): Before Next Result is fetched.");
   try {
     while(resultSet.next()) {
       logContext.log(log::DEBUG,
                      "In RelationalDB::getNextArchiveJobsToReportBatch(): After Next resultSet_ForTransfer is fetched.");
       schedulerdb::postgres::ArchiveJobQueueRow jobRow(resultSet);
+      ret.emplace_back(std::make_unique<schedulerdb::ArchiveRdbJob>(m_connPool, jobRow));
+      //schedulerdb::postgres::ArchiveJobQueueRow jobRow(resultSet);
       // Create an ArchiveRdbJob object using the jobRow data
-      schedulerdb::ArchiveRdbJob archiveJob(m_connPool, jobRow);
-      dbjobs.emplace_back(std::move(archiveJob));
+      //schedulerdb::ArchiveRdbJob archiveJob(m_connPool, jobRow);
+      //dbjobs.emplace_back(std::move(archiveJob));
       //jobs.emplace_back(resultSet);
     }
     // this is not query commit, but conn commit returning
@@ -197,9 +199,9 @@ std::list<std::unique_ptr<SchedulerDatabase::ArchiveJob> > RelationalDB::getNext
   }
   logContext.log(log::DEBUG, "In RelationalDB::getNextArchiveJobsToReportBatch(): After emplace_back resultSet_ForTransfer.");
   // Construct the return value
-  for (const auto &j : dbjobs) {
-    ret.emplace_back(std::move(j));
-  }
+  //for (const auto &j : dbjobs) {
+  //  ret.emplace_back(std::move(j));
+  //}
   logContext.log(log::DEBUG, "In RelationalDB::getNextArchiveJobsToReportBatch(): After Archive Jobs filled, before return.");
 
   return ret;
