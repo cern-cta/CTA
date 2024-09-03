d86626e2de69aa978db00a67b33dceef8f8fd41a
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2024-09-03 17:11:19 +0200

    Added fallback for registry credentials

--- continuousintegration/ci_helpers/check_registry_credentials.sh
+++ continuousintegration/ci_helpers/check_registry_credentials.sh
@@ -15,6 +15,10 @@
 #               granted to it by virtue of its status as an Intergovernmental Organization or
 #               submit itself to any jurisdiction.
 
+secret_is_dockerconfigjson() {
+  test $(kubectl get secret $1 -o jsonpath='{.type}') == "kubernetes.io/dockerconfigjson" 
+}
+
 check_credentials() {
   # The Kubernetes secret stores a base64 encoded .dockerconfigjson. This json has the following format:
   # {
@@ -29,33 +33,41 @@ check_credentials() {
   local registry_name="cta/ctageneric"
   local gitlab_server="gitlab.cern.ch"
 
-  local auth_json=$(kubectl get secret $secret_name -o jsonpath='{.data.\.dockerconfigjson}' | base64 --decode | jq -r '.auths')
+  # These variable are capatalised to match the variable in gitlabregistry.txt
+  local DOCKER_REGISTRY=$(echo $auth_json | jq -r 'keys[0]')
+  local DOCKER_LOGIN_USERNAME=$(echo $auth_json | jq -r '.[].auth' | base64 --decode | cut -d: -f1)
+  local DOCKER_LOGIN_PASSWORD=$(echo $auth_json | jq -r '.[].auth' | base64 --decode | cut -d: -f2)
+  if secret_is_dockerconfigjson $secret_name ; then
+    local auth_json=$(kubectl get secret $secret_name -o jsonpath='{.data.\.dockerconfigjson}' | base64 --decode | jq -r '.auths')
 
-  local docker_registry=$(echo $auth_json | jq -r 'keys[0]')
-  local docker_login_username=$(echo $auth_json | jq -r '.[].auth' | base64 --decode | cut -d: -f1)
-  local docker_login_password=$(echo $auth_json | jq -r '.[].auth' | base64 --decode | cut -d: -f2)
+    DOCKER_REGISTRY=$(echo $auth_json | jq -r 'keys[0]')
+    DOCKER_LOGIN_USERNAME=$(echo $auth_json | jq -r '.[].auth' | base64 --decode | cut -d: -f1)
+    DOCKER_LOGIN_PASSWORD=$(echo $auth_json | jq -r '.[].auth' | base64 --decode | cut -d: -f2)
+  else
+    source /etc/gitlab/gitlabregistry.txt
+  fi
 
-  if [[ -z "$docker_registry" ]]; then
-    echo "Error: Missing required variable: docker_registry"
+  if [[ -z "$DOCKER_REGISTRY" ]]; then
+    echo "Error: Missing required variable: DOCKER_REGISTRY"
     return 1
   fi
-  if [[ -z "$docker_login_username" ]]; then
-    echo "Error: Missing required variable: docker_login_username"
+  if [[ -z "$DOCKER_LOGIN_USERNAME" ]]; then
+    echo "Error: Missing required variable: DOCKER_LOGIN_USERNAME"
     return 1
   fi
-  if [[ -z "$docker_login_password" ]]; then
-    echo "Error: Missing required variable: docker_login_password"
+  if [[ -z "$DOCKER_LOGIN_PASSWORD" ]]; then
+    echo "Error: Missing required variable: DOCKER_LOGIN_PASSWORD"
     return 1
   fi
 
   # Retrieve JWT pull token from GitLab
-  local jwt_pull_token=$(curl -s -u "${docker_login_username}:${docker_login_password}" \
+  local jwt_pull_token=$(curl -s -u "${DOCKER_LOGIN_USERNAME}:${DOCKER_LOGIN_PASSWORD}" \
     "https://${gitlab_server}/jwt/auth?service=container_registry&scope=repository:${registry_name}:pull,push" | jq -r '.token')
 
   if [[ -z "$jwt_pull_token" ]]; then
     echo "Error: Failed to retrieve JWT pull token."
-    echo "\tRegistry: $docker_registry"
-    echo "\tUsername: $docker_login_username"
+    echo "\tRegistry: $DOCKER_REGISTRY"
+    echo "\tUsername: $DOCKER_LOGIN_USERNAME"
     return 1
   fi
 
