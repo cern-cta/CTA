ba36f835d31f1171f96d06ff445784a3cdfb3243
guenther <jaroslav.guenther@cern.ch>
2024-09-10 23:57:21 +0200

    avoiding requesting new conn for every insert, few typo fixes

--- scheduler/rdbms/RelationalDBInit.hpp
+++ scheduler/rdbms/RelationalDBInit.hpp
@@ -61,7 +61,7 @@ public:
   }
 
   std::unique_ptr<RelationalDB> getSchedDB(catalogue::Catalogue& catalogue, log::Logger& log) {
-    const uint64_t nbConns = 2;
+    const uint64_t nbConns = 3;
     return std::make_unique<RelationalDB>( clientProc, log, catalogue, login, nbConns);
   }
 
