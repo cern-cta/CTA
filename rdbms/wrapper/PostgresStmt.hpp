f9a0b7987fdea2dd0264e208beba13e99f967f90
guenther <jaroslav.guenther@cern.ch>
2024-09-11 15:43:18 +0200

    reverting back to conn pool design, introducing different lock granularity for scheduling new mount

--- rdbms/wrapper/PostgresStmt.hpp
+++ rdbms/wrapper/PostgresStmt.hpp
@@ -242,11 +242,6 @@ private:
   void setAffectedRows(const uint64_t val) { m_nbAffectedRows = val; }
 
   /**
-   * Get m_nbAffectedRows. Processed in last stmt.
-   */
-  uint64_t getAffectedRows() { return m_nbAffectedRows; }
-
-  /**
    * Called on DB error to generate error message and exception
    *
    * @param res The PGresult
