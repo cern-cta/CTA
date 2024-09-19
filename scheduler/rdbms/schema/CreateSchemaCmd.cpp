5b2fccf5930dbf8a078cf3e79662b4ef3bb388b3
guenther <jaroslav.guenther@cern.ch>
2024-09-19 16:00:03 +0200

    adding error reporting for row counts

--- scheduler/rdbms/schema/CreateSchemaCmd.cpp
+++ scheduler/rdbms/schema/CreateSchemaCmd.cpp
@@ -88,7 +88,7 @@ int CreateSchemaCmd::exceptionThrowingMain(const int argc, char *const *const ar
 //------------------------------------------------------------------------------
 // tableExists
 //------------------------------------------------------------------------------
-bool CreateSchemaCmd::tableExists(const std::string &tableName, const rdbms::Conn &conn) const {
+bool CreateSchemaCmd::tableExists(const std::string &tableName, rdbms::Conn &conn) const {
   const auto names = conn.getTableNames();
   conn.commit();
   return std::any_of(std::begin(names), std::end(names), [&tableName](const std::string& str) {
