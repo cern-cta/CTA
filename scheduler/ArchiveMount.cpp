64c8b905e0e328ce8e781974c80c68b6ccd37486
guenther <jaroslav.guenther@cern.ch>
2024-10-13 05:01:29 +0200

    fixing minor ommission

--- scheduler/ArchiveMount.cpp
+++ scheduler/ArchiveMount.cpp
@@ -189,18 +189,18 @@ std::list<std::unique_ptr<cta::ArchiveJob> > cta::ArchiveMount::getNextJobBatch(
 //------------------------------------------------------------------------------
 uint64_t cta::ArchiveMount::requeueJobBatch(const std::list<std::string>& jobIDsList, cta::log::LogContext& logContext) {
   if (jobIDsList.empty()) {
-    logContext.log(cta::log::INFO, "In cta::ArchiveMount::failJobBatch(): no job IDs provided to fail.");
+    logContext.log(cta::log::INFO, "In cta::ArchiveMount::requeueJobBatch(): no job IDs provided to fail.");
     return 0;
   }
-  // Forward the job IDs to the database handler's failJobBatch method.
+  // Forward the job IDs to the database handler's requeueJobBatch method.
   try {
-    uint64_t njobs = m_dbMount->failJobBatch(jobIDsList);
+    uint64_t njobs = m_dbMount->requeueJobBatch(jobIDsList);
     return njobs;
   }
   catch (const cta::exception::Exception& e) {
     cta::log::ScopedParamContainer params(logContext);
     params.add("exceptionMessageValue", e.getMessageValue());
-    logContext.log(cta::log::ERROR, "In cta::ArchiveMount::failJobBatch(): exception thrown.");
+    logContext.log(cta::log::ERR, "In cta::ArchiveMount::requeueJobBatch(): exception thrown.");
     return 0;
   }
 }
