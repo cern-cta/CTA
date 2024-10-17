7ca900186ba222184e3255c66a5949c22eaefcbe
guenther <jaroslav.guenther@cern.ch>
2024-10-17 18:43:16 +0200

    commenting out type checks and exceptions from pg rset

--- rdbms/wrapper/PostgresRset.cpp
+++ rdbms/wrapper/PostgresRset.cpp
@@ -126,14 +126,17 @@ std::optional<uint8_t> PostgresRset::columnOptionalUint8(const std::string &colN
     return std::nullopt;
   }
 
+  /* commenting out the string valur conversion since
+   * this might be a bottleneck if we need to get faster on thousands of rows
   const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
 
   if(!utils::isValidUInt(stringValue)) {
     throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
                                " which is not a valid unsigned integer");
   }
-
   return utils::toUint8(stringValue);
+  */
+  return utils:toUint8(PQgetvalue(m_resItr->get(), 0, ifield));
 }
 
 //------------------------------------------------------------------------------
@@ -148,139 +151,155 @@ std::optional<uint16_t> PostgresRset::columnOptionalUint16(const std::string &co
     return std::nullopt;
   }
 
-  const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
+    /* commenting out the string valur conversion since
+     * this might be a bottleneck if we need to get faster on thousands of rows
+    const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
 
-  if(!utils::isValidUInt(stringValue)) {
-    throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
-                               " which is not a valid unsigned integer");
+    if(!utils::isValidUInt(stringValue)) {
+      throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
+                                 " which is not a valid unsigned integer");
+    }
+
+    return utils::toUint16(stringValue);
+     */
+    return utils::toUint16(PQgetvalue(m_resItr->get(), 0, ifield));
   }
 
-  return utils::toUint16(stringValue);
-}
+  //------------------------------------------------------------------------------
+  // columnOptionalUint32
+  //------------------------------------------------------------------------------
+  std::optional<uint32_t> PostgresRset::columnOptionalUint32(const std::string &colName) const {
 
-//------------------------------------------------------------------------------
-// columnOptionalUint32
-//------------------------------------------------------------------------------
-std::optional<uint32_t> PostgresRset::columnOptionalUint32(const std::string &colName) const {
-
-  const int ifield = getColumnIndex(colName);
-
-  // the value can be null
-  if (PQgetisnull(m_resItr->get(), 0, ifield)) {
-    return std::nullopt;
-  }
-
-  const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
-
-  if(!utils::isValidUInt(stringValue)) {
-    throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
-                               " which is not a valid unsigned integer");
-  }
-
-  return utils::toUint32(stringValue);
-}
-
-//------------------------------------------------------------------------------
-// columnOptionalUint64
-//------------------------------------------------------------------------------
-std::optional<uint64_t> PostgresRset::columnOptionalUint64(const std::string &colName) const {
-
-  const int ifield = getColumnIndex(colName);
-
-  // the value can be null
-  if (PQgetisnull(m_resItr->get(), 0, ifield)) {
-    return std::nullopt;
-  }
-
-  const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
-
-  if(!utils::isValidUInt(stringValue)) {
-    throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
-      " which is not a valid unsigned integer");
-  }
-
-  return utils::toUint64(stringValue);
-}
-
-//------------------------------------------------------------------------------
-// columnOptionalDouble
-//------------------------------------------------------------------------------
-std::optional<double> PostgresRset::columnOptionalDouble(const std::string &colName) const {
-
-  const int ifield = getColumnIndex(colName);
+    const int ifield = getColumnIndex(colName);
 
     // the value can be null
-  if (PQgetisnull(m_resItr->get(), 0, ifield)) {
-    return std::nullopt;
+    if (PQgetisnull(m_resItr->get(), 0, ifield)) {
+      return std::nullopt;
+    }
+
+    /* commenting out the string valur conversion since
+     * this might be a bottleneck if we need to get faster on thousands of rows
+    const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
+
+    if(!utils::isValidUInt(stringValue)) {
+      throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
+                                 " which is not a valid unsigned integer");
+    }
+
+    return utils::toUint32(stringValue);*/
+    return utils::toUint32(PQgetvalue(m_resItr->get(), 0, ifield));
   }
 
-  const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
+  //------------------------------------------------------------------------------
+  // columnOptionalUint64
+  //------------------------------------------------------------------------------
+  std::optional<uint64_t> PostgresRset::columnOptionalUint64(const std::string &colName) const {
 
-  if(!utils::isValidDecimal(stringValue)) {
-    throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
-      " which is not a valid decimal");
+    const int ifield = getColumnIndex(colName);
+
+    // the value can be null
+    if (PQgetisnull(m_resItr->get(), 0, ifield)) {
+      return std::nullopt;
+    }
+
+    /* commenting out the string valur conversion since
+     * this might be a bottleneck if we need to get faster on thousands of rows
+    const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
+
+    if(!utils::isValidUInt(stringValue)) {
+      throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
+        " which is not a valid unsigned integer");
+    }
+
+    return utils::toUint64(stringValue);
+    */
+    return utils::toUint32(PQgetvalue(m_resItr->get(), 0, ifield));
   }
 
-  return utils::toDouble(stringValue);
-}
+  //------------------------------------------------------------------------------
+  // columnOptionalDouble
+  //------------------------------------------------------------------------------
+  std::optional<double> PostgresRset::columnOptionalDouble(const std::string &colName) const {
 
-//------------------------------------------------------------------------------
-// getSql
-//------------------------------------------------------------------------------
-const std::string &PostgresRset::getSql() const {
-  return m_stmt.getSql();
-}
+    const int ifield = getColumnIndex(colName);
 
-//------------------------------------------------------------------------------
-// next
-//------------------------------------------------------------------------------
-bool PostgresRset::next() {
+      // the value can be null
+    if (PQgetisnull(m_resItr->get(), 0, ifield)) {
+      return std::nullopt;
+    }
 
-  // always locks in order statement and then connection
-  threading::RWLockWrLocker locker2(m_stmt.m_lock);
-  threading::RWLockWrLocker locker(m_conn.m_lock);
-  m_columnPQindexCache.clear();
+    /* commenting out the string valur conversion since
+     * this might be a bottleneck if we need to get faster on thousands of rows
+    const std::string stringValue(PQgetvalue(m_resItr->get(), 0, ifield));
 
-  if (m_resItr->next()) {
+    if(!utils::isValidDecimal(stringValue)) {
+      throw exception::Exception(std::string("Column ") + colName + " contains the value " + stringValue +
+        " which is not a valid decimal");
+    }
 
-    // For queries expect rcode PGRES_SINGLE_TUPLE with ntuples=1 for each row,
-    // followed by PGRES_TUPLES_OK and ntuples=0 at the end.
-    // A non query would give PGRES_COMMAND_OK but we don't accept this here
-    // as a Rset is intended for an executeQuery only.
+    return utils::toDouble(stringValue);
+     */
+    return utils::toDouble(PQgetvalue(m_resItr->get(), 0, ifield));
+  }
 
-    if (PGRES_TUPLES_OK == m_resItr->rcode() && 0 == PQntuples(m_resItr->get())) {
-      const std::string stringValue = PQcmdTuples(m_resItr->get());
-      if (!stringValue.empty()) {
-        m_stmt.setAffectedRows(utils::toUint64(stringValue));
+  //------------------------------------------------------------------------------
+  // getSql
+  //------------------------------------------------------------------------------
+  const std::string &PostgresRset::getSql() const {
+    return m_stmt.getSql();
+  }
+
+  //------------------------------------------------------------------------------
+  // next
+  //------------------------------------------------------------------------------
+  bool PostgresRset::next() {
+
+    // always locks in order statement and then connection
+    threading::RWLockWrLocker locker2(m_stmt.m_lock);
+    threading::RWLockWrLocker locker(m_conn.m_lock);
+    // trying to reuse the indices per the full result set
+    // m_columnPQindexCache.clear();
+
+    if (m_resItr->next()) {
+
+      // For queries expect rcode PGRES_SINGLE_TUPLE with ntuples=1 for each row,
+      // followed by PGRES_TUPLES_OK and ntuples=0 at the end.
+      // A non query would give PGRES_COMMAND_OK but we don't accept this here
+      // as a Rset is intended for an executeQuery only.
+
+      if (PGRES_TUPLES_OK == m_resItr->rcode() && 0 == PQntuples(m_resItr->get())) {
+        const std::string stringValue = PQcmdTuples(m_resItr->get());
+        if (!stringValue.empty()) {
+          m_stmt.setAffectedRows(utils::toUint64(stringValue));
+        }
+        m_resItr->clear();
+        doClearAsync();
+        return false;
+      }
+      if (PGRES_SINGLE_TUPLE == m_resItr->rcode() && 1 == PQntuples(m_resItr->get())) {
+        ++m_nfetched;
+        m_stmt.setAffectedRows(m_nfetched);
+        return true;
       }
       m_resItr->clear();
       doClearAsync();
-      return false;
+      m_stmt.throwDB(m_resItr->get(), std::string(__FUNCTION__) + " failed while fetching results");
     }
-    if (PGRES_SINGLE_TUPLE == m_resItr->rcode() && 1 == PQntuples(m_resItr->get())) {
-      ++m_nfetched;
-      m_stmt.setAffectedRows(m_nfetched);
-      return true;
-    }
-    m_resItr->clear();
     doClearAsync();
-    m_stmt.throwDB(m_resItr->get(), std::string(__FUNCTION__) + " failed while fetching results");
+    m_stmt.doConnectionCheck();
+    return false;
   }
-  doClearAsync();
-  m_stmt.doConnectionCheck();
-  return false;
-}
 
-//------------------------------------------------------------------------------
-// doClearAsync
-//------------------------------------------------------------------------------
-void PostgresRset::doClearAsync() {
-  // assumes we have lock on connection
-  if (!m_asyncCleared) {
-    m_conn.setAsyncInProgress(false);
-    m_asyncCleared = true;
+  //------------------------------------------------------------------------------
+  // doClearAsync
+  //------------------------------------------------------------------------------
+  void PostgresRset::doClearAsync() {
+    // assumes we have lock on connection
+    if (!m_asyncCleared) {
+      m_conn.setAsyncInProgress(false);
+      m_asyncCleared = true;
+    }
   }
-}
 
 
-} // namespace cta::rdbms::wrapper
+  } // namespace cta::rdbms::wrapper
