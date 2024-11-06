f4135cccb37b510fbbcaa5d3bad953ebb3fe04a9
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2024-11-06 14:45:30 +0100

    ensured wget is installed

--- continuousintegration/docker/ctafrontend/opt/run/bin/reset_catalogue.sh
+++ continuousintegration/docker/ctafrontend/opt/run/bin/reset_catalogue.sh
@@ -55,7 +55,7 @@ if [ "$CATALOGUE_BACKEND" == "sqlite" ]; then
   cta-catalogue-schema-create -v $SCHEMA_VERSION /etc/cta/cta-catalogue.conf || die "ERROR: Could not create database schema. cta-catalogue-schema-create /etc/cta/cta-catalogue.conf FAILED"
   chmod -R 777 $(dirname $(echo ${CATALOGUE_URL} | cut -d: -f2)) # needed?
 elif [ "$CATALOGUE_BACKEND" == "oracle" ]; then
-
+  yum install -y wget
   wget https://download.oracle.com/otn_software/linux/instantclient/2112000/el9/oracle-instantclient-sqlplus-21.12.0.0.0-1.el9.x86_64.rpm
   yum install -y oracle-instantclient-sqlplus-21.12.0.0.0-1.el9.x86_64.rpm
   echo "Purging Oracle recycle bin"
