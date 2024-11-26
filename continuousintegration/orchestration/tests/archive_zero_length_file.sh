11d821f35f551ee8a2ed0ce2ba0c3d33e7484f9e
Joao Afonso <joao.afonso@cern.ch>
2024-11-26 16:31:34 +0100

    Adding system test for 0-lenght archive error

---
+++ continuousintegration/orchestration/tests/archive_zero_length_file.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# @project      The CERN Tape Archive (CTA)
+# @copyright    Copyright © 2022 CERN
+# @license      This program is free software, distributed under the terms of the GNU General Public
+#               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
+#               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
+#               option) any later version.
+#
+#               This program is distributed in the hope that it will be useful, but WITHOUT ANY
+#               WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#               PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#               In applying this licence, CERN does not waive the privileges and immunities
+#               granted to it by virtue of its status as an Intergovernmental Organization or
+#               submit itself to any jurisdiction.
+
+
+################################################################################
+# DESCRIPTION
+#
+#   - This script checks that the archival of a 0-length file fails.
+#   - For it to work, the configuration 'cta.archivefile.disallow_zero_length'
+#   must be set to 'on'.
+#
+################################################################################
+
+EOSINSTANCE=ctaeos
+CTA_TEST_DIR=/eos/ctaeos/cta
+TEST_FILE_NAME="empty_file"
+
+eosadmin_kdestroy
+eosadmin_kinit
+
+echo "Trying to archive a 0-length"
+
+touch /etc/${TEST_FILE_NAME}
+echo "xrdcp /etc/${TEST_FILE_NAME} root://${EOSINSTANCE}/${TEST_DIR}/${TEST_FILE_NAME}"
+xrdcp /etc/${TEST_FILE_NAME} root://${EOSINSTANCE}/${TEST_DIR}$/{TEST_FILE_NAME}
+if [ $? -eq 0 ]
+then
+  echo "xrdcp command succeeded where it should have failed"
+  exit 1
+else
+  echo "xrdcp command failed as expected"
+fi
+
+echo
+echo "OK: all tests passed"
+exit 0
