4f9156790a629646690a0fca7ab2a6d5859e5441
Konstantina Skovola <konstantina.skovola@cern.ch>
2024-11-28 17:51:59 +0100

    Rename grpc service class

--- frontend/grpc/Main.cpp
+++ frontend/grpc/Main.cpp
@@ -15,7 +15,7 @@
  *                 You should have received a copy of the GNU General Public License
  *                 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "FrontendGRpcSvc.h"
+#include "FrontendGrpcService.h"
 #include "version.h"
 
 #include "catalogue/Catalogue.hpp"
