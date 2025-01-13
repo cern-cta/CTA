ba68fb73630eb4be921b2eed3c3a1d853e7e5292
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-01-13 15:33:18 +0100

    Niels's feedback - remove EOSINSTANCE

--- continuousintegration/orchestration/tests/eosdf_systemtest.sh
+++ continuousintegration/orchestration/tests/eosdf_systemtest.sh
@@ -14,7 +14,8 @@
 #               submit itself to any jurisdiction.
 
 #default CI EOS instance
-EOSINSTANCE=ctaeos
+EOS_MGM_HOST="ctaeos"
+EOS_INSTANCE_NAME="ctaeos"
 
 REPORT_DIRECTORY=/var/log
 
@@ -22,7 +23,7 @@ REPORT_DIRECTORY=/var/log
 EOSDF_BUFFER_BASEDIR=/eos/ctaeos/cta
 EOSDF_BUFFER_URL=${EOSDF_BUFFER_BASEDIR}
 
-FULL_EOSDF_BUFFER_URL=root://${EOSINSTANCE}/${EOSDF_BUFFER_BASEDIR}
+FULL_EOSDF_BUFFER_URL=root://${EOS_INSTANCE_NAME}/${EOSDF_BUFFER_BASEDIR}
 
 # get some common useful helpers for krb5
 . /root/client_helper.sh ## wait_for_archive is defined in this file
@@ -38,9 +39,9 @@ admin_klist > /dev/null 2>&1 || die "Cannot get kerberos credentials for user ${
 ## All this is executed from within the client pod
 # This should be idempotent as we will be called several times
 if [[ $( admin_cta --json ds ls | jq '.[] | select(.name=="eosdfBuffer") | .name') != '"eosdfBuffer"' ]]; then
-    admin_cta di add -n ${EOSINSTANCE} -m toto
-    admin_cta dis add -n eosdfDiskInstanceSpace --di ${EOSINSTANCE} -u "eosSpace:default" -i 1 -m toto
-    admin_cta ds add -n eosdfBuffer --di ${EOSINSTANCE} --dis eosdfDiskInstanceSpace -r ".*/eos/ctaeos/.*" -f 1 -s 20 -m toto # "root://${EOSINSTANCE}/${EOSDF_BUFFER_BASEDIR}"
+    admin_cta di add -n ${EOS_INSTANCE_NAME} -m toto
+    admin_cta dis add -n eosdfDiskInstanceSpace --di ${EOS_INSTANCE_NAME} -u "eosSpace:default" -i 1 -m toto
+    admin_cta ds add -n eosdfBuffer --di ${EOS_INSTANCE_NAME} --dis eosdfDiskInstanceSpace -r ".*/eos/.*" -f 1 -s 20 -m toto # "root://${EOS_INSTANCE_NAME}/${EOSDF_BUFFER_BASEDIR}"
 else
     echo "Disk system eosdfBuffer alread defined. Ensuring very low free space requirements."
     admin_cta ds ch -n eosdfBuffer -f 1
@@ -53,25 +54,25 @@ echo
 TEST_FILE_NAME=testfile1_eosdf
 echo "foo" > /root/${TEST_FILE_NAME}
 echo
-echo "Doing xrdcp of ${TEST_FILE_NAME} in the path root://${EOSINSTANCE}/${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}"
-xrdcp /root/${TEST_FILE_NAME} root://${EOSINSTANCE}/${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}
-wait_for_archive ${EOSINSTANCE} "${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}"
+echo "Doing xrdcp of ${TEST_FILE_NAME} in the path root://${EOS_INSTANCE_NAME}/${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}"
+xrdcp /root/${TEST_FILE_NAME} root://${EOS_INSTANCE_NAME}/${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}
+wait_for_archive ${EOS_INSTANCE_NAME} "${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}"
 
 echo "File ${TEST_FILE_NAME} archived to tape"
 
 ## Retrieve the file, this should have returned a script error, but still it will work because we don't want to prevent staging due to script missing or having insufficient permissions
 REQUEST_ID=$(KRB5CCNAME=/tmp/${EOSPOWER_USER}/krb5cc_0 XrdSecPROTOCOL=krb5 xrdfs ${EOS_MGM_HOST} prepare -s ${EOSDF_BUFFER_URL}/testfile1_eosdf)
 # Wait for the copy to appear on disk
-wait_for_retrieve ${EOSINSTANCE} "${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}"
+wait_for_retrieve ${EOS_INSTANCE_NAME} "${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}"
 
 echo
 echo "File ${TEST_FILE_NAME} retrieved from disk"
 echo
 
 # remove the file so that it can be recreated in the next test run
-echo "eos root://${EOSINSTANCE} rm ${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}"
-eos root://${EOSINSTANCE} rm ${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}
+echo "eos root://${EOS_INSTANCE_NAME} rm ${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}"
+eos root://${EOS_INSTANCE_NAME} rm ${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}
 
 # Remove the disk system so it doesn't interfere with other tests
 admin_cta ds rm -n eosdfBuffer
-admin_cta dis rm -n eosdfDiskInstanceSpace --di ${EOSINSTANCE}
+admin_cta dis rm -n eosdfDiskInstanceSpace --di ${EOS_INSTANCE_NAME}
