669b52fbf4a67a56cc336ca7e93c53b18d93f15c
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-01-16 10:42:14 +0100

    Update script to use eos -j and jq

--- disk/scripts/eosdf.sh
+++ disk/scripts/eosdf.sh
@@ -5,17 +5,15 @@ source /etc/sysconfig/cta-taped
 # echo "arguments passed: $@"
 # eos:instance_name:eos_space
 diskInstance=${1:-ctaeos}
-spaceName=${2:-default}
-# diskInstance=ctaeos
-output=$(XrdSecSSSKT=$XrdSecSSSKT XrdSecPROTOCOL=$XrdSecPROTOCOL eos root://$diskInstance space ls -m)
+spaceName=${2:-retrieve}
+
+freespace=$(
+  XrdSecSSSKT=$XrdSecSSSKT XrdSecPROTOCOL=$XrdSecPROTOCOL \
+  eos -j root://$diskInstance space ls \
+  | jq  -c ".result[] | select (.name==\"$spaceName\") | .sum.stat.statfs.freebytes | tonumber"
+)
 if [[ $? -ne 0 ]]; then
   exit 1
 fi
-# we're interested in the result for the default namespace
-freespace=$(echo "$output" | awk -v spaceName="$spaceName" -F ' ' '$0 ~ "name="spaceName {
-  for (i=1; i<=NF; i++) if ($i ~ /sum.stat.statfs.freebytes\?configstatus@rw=/) {
-      print substr($i, index($i, "=")+1)
-    }
-}')
 
 echo "{\"freeSpace\":$freespace}"
