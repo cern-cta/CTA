ba3dca8ecdfee5a67eeb504305ef4f0f228c0905
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2025-01-28 11:38:18 +0100

    Clean MaintenanceHandler code

--- maintenance/MaintenanceHandler.hpp
+++ maintenance/MaintenanceHandler.hpp
@@ -1,6 +1,6 @@
 /*
  * @project      The CERN Tape Archive (CTA)
- * @copyright    Copyright © 2021-2022 CERN
+ * @copyright    Copyright © 2021-2025 CERN
  * @license      This program is free software, distributed under the terms of the GNU General Public
  *               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
  *               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
@@ -17,57 +17,25 @@
 
 #pragma once
 
-#include "SubprocessHandler.hpp"
-#include "ProcessManager.hpp"
-#include "tapeserver/daemon/common/TapedConfiguration.hpp"
-#include "common/threading/SocketPair.hpp"
+#include "configuration/MaintenanceConfiguration.hpp"
 
-namespace cta::tape::daemon {
+namespace cta::maintenance {
 
 /**
  * Handler for garbage collector subprocesses. This long lived process should live
  * as long as the main process, but will be respawned in case of crash.
  */
-class MaintenanceHandler: public SubprocessHandler {
+class MaintenanceHandler {
 public:
-  MaintenanceHandler(const common::TapedConfiguration & tapedConfig, ProcessManager & pm);
-  virtual ~MaintenanceHandler();
-  ProcessingStatus getInitialStatus() override;
-  ProcessingStatus fork() override;
-  void postForkCleanup() override;
-  int runChild() noexcept override;
-  ProcessingStatus shutdown() override;
-  void kill() override;
-  ProcessingStatus processEvent() override;
-  ProcessingStatus processSigChild() override;
-  ProcessingStatus processRefreshLoggerRequest() override;
-  ProcessingStatus refreshLogger() override;
-  ProcessingStatus processTimeout() override;
+  MaintenanceHandler(const common::MaintenanceConfiguration & maintenanceConfig);
+  ~MaintenanceHandler();
 private:
-  void exceptionThrowingRunChild();
-  
-  /**
-   * Returns true if the RepackRequestManager will be ran on this tapeserver,
-   * false otherwise
-   */
-  bool runRepackRequestManager() const;
-  /** Reference to the process manager*/
-  cta::tape::daemon::ProcessManager & m_processManager;
+  void exceptionThrowingMain();
+
   /** The parameters */
-  const common::TapedConfiguration & m_tapedConfig;
-  /** The current state we report to process manager */
-  ProcessingStatus m_processingStatus;
-  /** PID for the subprocess */
-  pid_t m_pid=-1;
-  /** Keep track of the shutdown state */
-  bool m_shutdownInProgress=false;
-  /** A socketpair to ask the child process to gracefully shut down */
-  std::unique_ptr<cta::server::SocketPair> m_socketPair;
+  const common::TapedConfiguration & m_maintenanceConfig;
   /** The poll period for the garbage collector */
   static const time_t s_pollInterval = 10;
-
-  static constexpr const char* const SHUTDOWN_MSG = "SHUTDOWN";
-  static constexpr const char* const REFRESH_LOGGER_MSG = "REFRESH_LOGGER";
 };
 
-} // namespace cta::tape::daemon
+} // namespace cta::maintenance
