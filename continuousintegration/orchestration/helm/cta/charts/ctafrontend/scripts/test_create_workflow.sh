05fd8403f1610348d92d8680271711fa81484d81
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-02-14 14:03:21 +0100

    Add some stuff to my test_workflows script

--- continuousintegration/orchestration/helm/cta/charts/ctafrontend/scripts/test_create_workflow.sh
+++ continuousintegration/orchestration/helm/cta/charts/ctafrontend/scripts/test_create_workflow.sh
@@ -12,3 +12,14 @@ TEST_FILE_NAME=testfile1_eosdf
 echo "foo" > /root/${TEST_FILE_NAME}
 
 xrdcp /root/${TEST_FILE_NAME} root://${EOS_INSTANCE_NAME}/${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}
+
+## maybe these in order to get the test to work:
+EOSDF_BUFFER_BASEDIR=/eos/ctaeos/eosdf
+EOSDF_BUFFER_URL=${EOSDF_BUFFER_BASEDIR}
+
+## in the ctaeos pod run the two following lines before starting the test
+eos mkdir ${EOSDF_BUFFER_URL}
+eos chmod 1777 ${EOSDF_BUFFER_URL}
+
+# add test for the PREPARE workflow
+xrdfs ${EOS_MGM_HOST} prepare -s ${EOSDF_BUFFER_URL}/${TEST_FILE_NAME}
