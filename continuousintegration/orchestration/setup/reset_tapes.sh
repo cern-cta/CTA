911dd8f65c25af5be11adcdde912f5efc03d4e9c
Joao Afonso <joao.afonso@cern.ch>
2025-02-25 10:49:48 +0000

    Fix broken development setup due to "Unit attention" error on sg_modes command
    Closes #1096
    
    See merge request cta/CTA!821
    
    Changelog: bug
    Issue: #1096

--- continuousintegration/orchestration/setup/reset_tapes.sh
+++ continuousintegration/orchestration/setup/reset_tapes.sh
@@ -62,5 +62,5 @@ done
 # Clear power on sense generate during boot.
 echo "Running SCSI MODE SENSE"
 for SG_DEVICE in $(lsscsi -g | grep tape | awk '{print $7}'); do
-  sg_modes $SG_DEVICE > /dev/null
+  sg_turs $SG_DEVICE 2>&1 > /dev/null || true
 done
