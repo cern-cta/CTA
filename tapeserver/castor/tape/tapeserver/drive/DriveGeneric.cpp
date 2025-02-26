850ce9d98229c78f740c58358c2763f844bee1b0
Pablo Oliver Cortés <pablo.oliver.cortes@cern.ch>
2025-02-26 11:02:50 +0100

    Remove stub CRC32c code for MHVTLDrive

--- tapeserver/castor/tape/tapeserver/drive/DriveGeneric.cpp
+++ tapeserver/castor/tape/tapeserver/drive/DriveGeneric.cpp
@@ -1500,38 +1500,6 @@ drive::compressionStats drive::DriveT10000::getCompressionStats() {
   return driveCompressionStats;
 }
 
-void drive::DriveMHVTL::disableLogicalBlockProtection() { }
-
-void drive::DriveMHVTL::enableCRC32CLogicalBlockProtectionReadOnly() {
-  throw cta::exception::Exception(
-    "In DriveMHVTL::enableCRC32CLogicalBlockProtectionReadOnly(): not supported");
-}
-
-void drive::DriveMHVTL::enableCRC32CLogicalBlockProtectionReadWrite() {
-  throw cta::exception::Exception(
-    "In DriveMHVTL::enableCRC32CLogicalBlockProtectionReadWrite(): not supported");
-}
-
-drive::LBPInfo drive::DriveMHVTL::getLBPInfo() {
-  drive::LBPInfo ret;
-  ret.enableLBPforRead = false;
-  ret.enableLBPforWrite = false;
-  ret.method = 0;
-  ret.methodLength = 0;
-  return ret;
-}
-
-drive::lbpToUse drive::DriveMHVTL::getLbpToUse() {
-  return lbpToUse::disabled;
-}
-
-void drive::DriveMHVTL::setLogicalBlockProtection(const unsigned char method,
-  unsigned char methodLength, const bool enableLPBforRead,
-  const bool enableLBBforWrite) {
-  if (method != 0 || methodLength != 0 || enableLBBforWrite || enableLPBforRead)
-    throw cta::exception::Exception("In DriveMHVTL::setLogicalBlockProtection:: LBP cannot be enabled");
-}
-
 drive::compressionStats drive::DriveLTO::getCompression()  {
   SCSI::Structures::LinuxSGIO_t sgh;
   SCSI::Structures::logSenseCDB_t cdb;
