88c3f8fa3d31758b37ec73f478d22cffe8a39e8f
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-02-28 10:20:20 +0100

    Disable repack test for now and fixes to admin test

--- continuousintegration/tests/system_tests/test_cta_admin.py
+++ continuousintegration/tests/system_tests/test_cta_admin.py
@@ -1,3 +1,7 @@
+def test_set_one_drive_down(env):
+    # The cta-admin tests rely on the second drive being down (blegh; something to change in the future)
+    env.ctacli[0].set_drive_down(env.ctataped[1].drive_name())
+
 # For now we don't refactor/split the test_cta_admin.sh script yet
 # That will come in stage 2 of the system test refactor
 def test_cta_admin(env):
