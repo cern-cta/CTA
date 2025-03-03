2f924ce2c9fb901763f7b522900564aaa9a3d10a
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-03-03 17:41:54 +0100

    Remove example scripts

--- continuousintegration/orchestration/tests/http/stage_deletion_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Usage: ./stage_deletion.sh stage_request_id
-EOS_MGM_HOST="ctaeos"
-PORT=9000
-
-sudo curl -L -v --capath /etc/grid-security/certificates --cert ~/.globus/usercert.pem --cacert ~/.globus/usercert.pem --key ~/.globus/userkey.pem -X DELETE https://$EOS_MGM_HOST:$PORT/api/v0/stage/$1 2> /dev/null
