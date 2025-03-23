70af23a9f507eaa73684be250fdfa18f133e6a5a
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-03-23 16:02:54 +0100

    Some comments to my test

--- frontend/grpc/callback_api/test.sh
+++ frontend/grpc/callback_api/test.sh
@@ -5,8 +5,10 @@ NAMESPACE=dev
 CTA_CLI_POD=cta-cli-0
 DB_POD=cta-catalogue-postgres
 kubectl exec -n ${NAMESPACE} ${CTA_CLI_POD} -c cta-cli -- yum install -y cta-admin-grpc-stream
-kubectl exec -n ${NAMESPACE} ${CTA_CLI_POD} -c cta-cli -- cta-admin-grpc-stream tape ls --all
 
 ## populate the DB
+cd /home/cirunner
 kubectl cp -n ${NAMESPACE} dump_after_ar_test ${DB_POD}:dump_after_prepare_test
-kubectl exec -n ${NAMESPACE} ${DB_POD} -- psql -U cta -d cta < dump_after_prepare_test
+# kubectl exec -n ${NAMESPACE} ${DB_POD} -- psql -U cta -d cta < dump_after_prepare_test
+
+# kubectl exec -n ${NAMESPACE} ${CTA_CLI_POD} -c cta-cli -- cta-admin-grpc-stream tape ls --all
