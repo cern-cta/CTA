65ee87647b93504793cf3f34b6a12470c0fd2585
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-03-31 17:00:09 +0200

    sqlAlchemy fix

--- api/src/dependencies.py
+++ api/src/dependencies.py
@@ -1,8 +1,11 @@
 
 from .internal.catalogue.catalogue import Catalogue
 
-with open("/etc/cta/catalogue.conf") as f:
+with open("/etc/cta/cta-catalogue.conf") as f:
     connection_string = f.read().strip()
+    # Fix for sql alchemy
+    if connection_string.startswith("postgresql:postgresql://"):
+        connection_string = connection_string.replace("postgresql:", "", 1)
 _catalogue = Catalogue(connection_string)
 
 def get_catalogue() -> Catalogue:
