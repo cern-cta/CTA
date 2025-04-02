412f20877ddd76c38aaa2459cad972e401b82ca0
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-04-02 12:26:02 +0200

    Update auth chart to serve jwks

--- api/test/conftest.py
+++ api/test/conftest.py
@@ -11,7 +11,8 @@ def client(monkeypatch):
     monkeypatch.setenv("JWKS_ENDPOINT", "https://fake-jwks.local/.well-known/jwks.json")
     mock_catalogue = Mock()
 
-
+    # We ignore all middleware to ensure we can test the endpoints directly
+    # There will be separate tests specifically for the middleware
     with patch.object(FastAPI, "add_middleware", lambda *args, **kwargs: None):
         app = create_app()
         app.dependency_overrides[get_catalogue] = lambda: mock_catalogue
