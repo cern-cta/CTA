56fe105af1a6b2520711e073c9720d60b9c1a4a2
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-04-04 16:13:25 +0200

    Allow in ci for specifying extra values file

--- continuousintegration/orchestration/helm/frontend/scripts/ctafrontend-grpc.sh
+++ continuousintegration/orchestration/helm/frontend/scripts/ctafrontend-grpc.sh
@@ -19,13 +19,15 @@
 echo "$(date '+%Y-%m-%d %H:%M:%S') [$(basename "${BASH_SOURCE[0]}")] Started"
 
 # Install missing RPMs
-dnf install -y cta-frontend
+dnf install -y cta-frontend cta-catalogueutils cta-debuginfo cta-debugsource
 if [ "$SCHEDULER_BACKEND" == "ceph" ]; then
   dnf config-manager --enable ceph
   dnf install -y ceph-common
 fi
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') [$(basename "${BASH_SOURCE[0]}")] Ready"
+
+tail -F /var/log/cta/cta-frontend-grpc.log &
 runuser --shell='/bin/bash' --session-command='/usr/bin/cta-frontend-grpc >> /var/log/cta/cta-frontend-grpc.log' cta
 
 echo "ctafrontend died"
