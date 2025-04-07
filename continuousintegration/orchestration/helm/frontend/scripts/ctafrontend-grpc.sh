5daedfd26dc30b114f6565ea8c5aa0b443166f8d
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-04-07 15:46:00 +0200

    Fix ctafrontend-grpc.sh script to install correct rpm

--- continuousintegration/orchestration/helm/frontend/scripts/ctafrontend-grpc.sh
+++ continuousintegration/orchestration/helm/frontend/scripts/ctafrontend-grpc.sh
@@ -19,7 +19,7 @@
 echo "$(date '+%Y-%m-%d %H:%M:%S') [$(basename "${BASH_SOURCE[0]}")] Started"
 
 # Install missing RPMs
-dnf install -y cta-frontend cta-catalogueutils cta-debuginfo cta-debugsource
+dnf install -y cta-frontend-grpc cta-catalogueutils cta-debuginfo cta-debugsource
 if [ "$SCHEDULER_BACKEND" == "ceph" ]; then
   dnf config-manager --enable ceph
   dnf install -y ceph-common
