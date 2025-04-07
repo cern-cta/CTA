004e31a2ad8d8028b0339c8e2ca4162557f920e8
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-04-07 14:19:37 +0200

    Fixed rmcd tail

--- continuousintegration/orchestration/tests/test_client.sh
+++ continuousintegration/orchestration/tests/test_client.sh
@@ -89,7 +89,7 @@ TEST_POSTRUN=""
 
 VERBOSE=0
 if [[ $VERBOSE == 1 ]]; then
-  TEST_PRERUN="tail -v -f /mnt/logs/cta-tpsrv*/rmcd/cta/cta-rmcd.log & export TAILPID=\$! && ${TEST_PRERUN}"
+  TEST_PRERUN="tail -v -f /mnt/logs/cta-tpsrv*/cta-rmcd/cta/cta-rmcd.log & export TAILPID=\$! && ${TEST_PRERUN}"
   TEST_POSTRUN=" && kill \${TAILPID} &> /dev/null"
 fi
 
