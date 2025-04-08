e76898db19d850e28619a9484e85966a9d0ae597
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-04-08 17:25:18 +0000

    CI support for gRPC CTA Frontend
    Closes #1015
    
    See merge request cta/CTA!857
    
    Changelog: feature/bug/maintenance
    Issue: <id>

--- frontend/grpc/FrontendGrpcService.h
+++ frontend/grpc/FrontendGrpcService.h
@@ -6,6 +6,9 @@
 #include <grpcpp/resource_quota.h>
 #include <grpcpp/security/server_credentials.h>
 
+// need to implement health check for our CI
+#include <grpcpp/health_check_service_interface.h>
+
 #include <scheduler/Scheduler.hpp>
 #include "common/log/Logger.hpp"
 #include "cta_frontend.pb.h"
@@ -28,11 +31,17 @@ class CtaRpcImpl : public CtaRpc::Service {
 
 private:
   std::unique_ptr<cta::frontend::FrontendService> m_frontendService;
+  ::grpc::HealthCheckServiceInterface* m_healthCheckService = nullptr;
+  bool m_isServing = true;
 
 public:
   CtaRpcImpl(const std::string& config);
 
   FrontendService& getFrontendService() const { return *m_frontendService; }
+  void setHealthCheckService(
+    ::grpc::HealthCheckServiceInterface* healthCheckService) {
+      m_healthCheckService = healthCheckService;
+  }
 
   // Archive/Retrieve interface
   Status Create(::grpc::ServerContext* context, const cta::xrd::Request* request, cta::xrd::Response* response);
