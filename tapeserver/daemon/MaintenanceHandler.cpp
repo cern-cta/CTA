bfe1121158f79a2fe05cc88a9eaa3e1f4902c585
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-04-25 12:00:10 +0200

    Addition of telemetry to frontend; segfault fix

--- tapeserver/daemon/MaintenanceHandler.cpp
+++ tapeserver/daemon/MaintenanceHandler.cpp
@@ -80,11 +80,14 @@ SubprocessHandler::ProcessingStatus MaintenanceHandler::fork() {
   try {
     // First prepare a socket pair for this new subprocess
     m_socketPair.reset(new cta::server::SocketPair());
+    // We don't want to fork telemetry state
+    // cta::telemetry::resetTelemetry();
     // and fork
     m_pid=::fork();
     exception::Errnum::throwOnMinusOne(m_pid, "In MaintenanceHandler::fork(): failed to fork()");
+
     if (!m_pid) {
-      cta::telemetry::reinitTelemetry();
+      // cta::telemetry::reinitTelemetry();
       // We are in the child process
       SubprocessHandler::ProcessingStatus ret;
       ret.forkState = SubprocessHandler::ForkState::child;
