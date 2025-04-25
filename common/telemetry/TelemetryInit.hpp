fc6185e8eefffba581dddb94a93ed93b7b8509c9
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-04-25 12:00:10 +0200

    Addition of telemetry to frontend; segfault fix

--- common/telemetry/TelemetryInit.hpp
+++ common/telemetry/TelemetryInit.hpp
@@ -5,7 +5,20 @@
 
 namespace cta::telemetry {
 
+/**
+ * Initialises telemetry according to the provided config
+ */
 void initTelemetry(const TelemetryConfig& config);
+
+/**
+ * Provided that initTelemetry has been called at some point in the past,
+ * this method will reuse the original telemetry config to reinitialise telemetry
+ */
 void reinitTelemetry();
+
+/**
+ * Sets telemetry to NOOP
+ */
+void resetTelemetry();
 
 }  // namespace cta::telemetry
--- continuousintegration/orchestration/helm/frontend/scripts/ctafrontend-xrd.sh
+++ continuousintegration/orchestration/helm/frontend/scripts/ctafrontend-xrd.sh
@@ -25,6 +25,6 @@ if [ "$SCHEDULER_BACKEND" == "ceph" ]; then
 fi
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') [$(basename "${BASH_SOURCE[0]}")] Ready"
-
+export OPENTELEMETRY_LOG_LEVEL=debug
 tail -F /var/log/cta/cta-frontend.log &
 runuser --shell='/bin/bash' --session-command='cd ~cta; xrootd -l /var/log/cta-frontend-xrootd.log -k fifo -n cta -c /etc/cta/cta-frontend-xrootd.conf -I v4' cta
