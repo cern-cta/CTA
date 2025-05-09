c6de4608c33b642e4b3ccdcff071edb128e11807
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-05-09 16:52:45 +0200

    Add a test with a template class

--- frontend/grpc/callback_api/ServerLogicalLibraryLs.hpp
+++ frontend/grpc/callback_api/ServerLogicalLibraryLs.hpp
@@ -6,15 +6,16 @@
 #include <grpcpp/grpcpp.h>
 #include "../RequestMessage.hpp"
 #include "common/dataStructures/LogicalLibrary.hpp"
-#include "CtaAdminServerWriteReactor.hpp"
+#include "TemplateAdminCmdStream.hpp"
 #include "cmdline/admin_common/DataItemMessageFill.hpp"
 
 namespace cta::frontend::grpc {
 
-class LogicalLibraryLsWriteReactor : public CtaAdminServerWriteReactor {
+class LogicalLibraryLsWriteReactor : public TemplateAdminCmdStream<cta::common::dataStructures::LogicalLibrary, cta::admin::LogicalLibraryLsItem, decltype(&fillLogicalLibraryItem)> {
     public:
         LogicalLibraryLsWriteReactor(cta::catalogue::Catalogue &catalogue, cta::Scheduler &scheduler, const std::string& instanceName, const cta::xrd::Request* request);
-        void NextWrite() override;
+        // void NextWrite() override;
+        cta::admin::LogicalLibraryLsItem* getMessageField(cta::xrd::Data* data) override;
     private:
         std::list<cta::common::dataStructures::LogicalLibrary> m_logicalLibraryList;
         std::optional<bool> m_disabled;
@@ -22,50 +23,56 @@ class LogicalLibraryLsWriteReactor : public CtaAdminServerWriteReactor {
 };
 
 LogicalLibraryLsWriteReactor::LogicalLibraryLsWriteReactor(cta::catalogue::Catalogue &catalogue, cta::Scheduler &scheduler, const std::string& instanceName, const cta::xrd::Request* request)
-    : CtaAdminServerWriteReactor(catalogue, scheduler, instanceName),
-      m_logicalLibraryList(catalogue.LogicalLibrary()->getLogicalLibraries()) {
-    using namespace cta::admin;
+    : TemplateAdminCmdStream<cta::common::dataStructures::LogicalLibrary,
+        cta::admin::LogicalLibraryLsItem,
+        decltype(&fillLogicalLibraryItem)>
+        (catalogue, scheduler, instanceName, catalogue.LogicalLibrary()->getLogicalLibraries(), cta::admin::HeaderType::LOGICALLIBRARY_LS, &fillLogicalLibraryItem) {
+    // using namespace cta::admin;
 
-    request::RequestMessage requestMsg(*request);
+    // request::RequestMessage requestMsg(*request);
     
-    m_disabled = requestMsg.getOptional(admin::OptionBoolean::DISABLED);
+    // m_disabled = requestMsg.getOptional(admin::OptionBoolean::DISABLED);
 
-    next_ll = m_logicalLibraryList.cbegin();
-    NextWrite();
+    // next_ll = m_logicalLibraryList.cbegin();
+    // NextWrite();
 }
 
-void LogicalLibraryLsWriteReactor::NextWrite() {
-    m_response.Clear();
-    // is this the first item? Then write the header
-    if (!m_isHeaderSent) {
-        cta::xrd::Response *header = new cta::xrd::Response();
-        header->set_type(cta::xrd::Response::RSP_SUCCESS);
-        header->set_show_header(cta::admin::HeaderType::LOGICALLIBRARY_LS);
-        m_response.set_allocated_header(header); // now the message takes ownership of the allocated object, we don't need to free header
+cta::admin::LogicalLibraryLsItem* LogicalLibraryLsWriteReactor::getMessageField(cta::xrd::Data* data) {
+    return data->mutable_llls_item();
+}
 
-        m_isHeaderSent = true;
-        StartWrite(&m_response); // this will trigger the OnWriteDone method
-        return; // because we'll be called in a loop by OnWriteDone
-    } else {
-        while(next_ll != m_logicalLibraryList.cend()) {
-            const auto& ll = *next_ll;
-            ++next_ll;
-            cta::xrd::Data* data = new cta::xrd::Data();
-            cta::admin::LogicalLibraryLsItem *ll_item = data->mutable_llls_item();
+// void LogicalLibraryLsWriteReactor::NextWrite() {
+//     m_response.Clear();
+//     // is this the first item? Then write the header
+//     if (!m_isHeaderSent) {
+//         cta::xrd::Response *header = new cta::xrd::Response();
+//         header->set_type(cta::xrd::Response::RSP_SUCCESS);
+//         header->set_show_header(cta::admin::HeaderType::LOGICALLIBRARY_LS);
+//         m_response.set_allocated_header(header); // now the message takes ownership of the allocated object, we don't need to free header
+
+//         m_isHeaderSent = true;
+//         StartWrite(&m_response); // this will trigger the OnWriteDone method
+//         return; // because we'll be called in a loop by OnWriteDone
+//     } else {
+//         while(next_ll != m_logicalLibraryList.cend()) {
+//             const auto& ll = *next_ll;
+//             ++next_ll;
+//             cta::xrd::Data* data = new cta::xrd::Data();
+//             cta::admin::LogicalLibraryLsItem *ll_item = data->mutable_llls_item();
             
-            if (m_disabled && m_disabled.value() != ll.isDisabled) {
-                continue;
-            }
+//             if (m_disabled && m_disabled.value() != ll.isDisabled) {
+//                 continue;
+//             }
 
-            fillLogicalLibraryItem(ll, ll_item, m_instanceName);
+//             fillLogicalLibraryItem(ll, ll_item, m_instanceName);
 
-            m_response.set_allocated_data(data);
-            StartWrite(&m_response);
-            return; // because we will be called in a loop by OnWriteDone()
-        } // end while
-        std::cout << "Finishing the call on the server side" << std::endl;
-        // Finish the call
-        Finish(::grpc::Status::OK);
-    }
-}
+//             m_response.set_allocated_data(data);
+//             StartWrite(&m_response);
+//             return; // because we will be called in a loop by OnWriteDone()
+//         } // end while
+//         std::cout << "Finishing the call on the server side" << std::endl;
+//         // Finish the call
+//         Finish(::grpc::Status::OK);
+//     }
+// }
 } // namespace cta::frontend::grpc
