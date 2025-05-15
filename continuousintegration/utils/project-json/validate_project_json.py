573129da2fdefab533185dbb983f4ed5a68cd076
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-05-15 17:10:09 +0200

    Renamed package groups

--- continuousintegration/utils/project-json/validate_project_json.py
+++ continuousintegration/utils/project-json/validate_project_json.py
@@ -20,15 +20,7 @@ import json
 import sys
 import jsonschema
 
-def main():
-    schema_json_path = Path(__file__).resolve().parent / "schema.json"
-    with open(schema_json_path, "r") as f:
-        schema_json = json.load(f)
-
-    project_json_path = Path(__file__).resolve().parents[3] / "project.json"
-    with open(project_json_path, "r") as f:
-        project_json = json.load(f)
-
+def validate_schema(project_json, schema_json):
     print(f"Validating project.json using schema.json schema...")
 
     try:
@@ -38,11 +30,19 @@ def main():
     except jsonschema.ValidationError as e:
         path = ".".join([str(p) for p in e.path]) or "<root>"
         print(f"Schema validation of project.json failed:")
-        print(f"  ✱ Path: {path}")
-        print(f"  ✱ Error: {e.message}")
-        print(f"  ✱ Expected: {e.schema.get('type')}")
-        print(f"  ✱ Schema rule: {e.schema}")
+        print(f"  * Path: {path}")
+        print(f"  * Error: {e.message}")
+        print(f"  * Expected: {e.schema.get('type')}")
+        print(f"  * Schema rule: {e.schema}")
         sys.exit(1)
 
 if __name__ == '__main__':
-    main()
+    project_json_path = Path(__file__).resolve().parents[3] / "project.json"
+    with open(project_json_path, "r") as f:
+        project_json = json.load(f)
+
+    schema_json_path = Path(__file__).resolve().parent / "schema.json"
+    with open(schema_json_path, "r") as f:
+        schema_json = json.load(f)
+
+    validate_schema(project_json, schema_json)
