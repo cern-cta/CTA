e8aef081aa8131e6a4809b7303cafbf2bafc57b6
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-05-15 12:25:41 +0200

    Ensure availability of repos in pipeline variable check

--- continuousintegration/utils/project-json/validate_project_json.py
+++ continuousintegration/utils/project-json/validate_project_json.py
@@ -20,26 +20,29 @@ import json
 import sys
 import jsonschema
 
+def main():
+    schema_json_path = Path(__file__).resolve().parent / "schema.json"
+    with open(schema_json_path, "r") as f:
+        schema_json = json.load(f)
 
-schema_json_path = Path(__file__).resolve().parent / "schema.json"
-with open(schema_json_path, "r") as f:
-    schema_json = json.load(f)
+    project_json_path = Path(__file__).resolve().parents[3] / "project.json"
+    with open(project_json_path, "r") as f:
+        project_json = json.load(f)
 
-project_json_path = Path(__file__).resolve().parents[3] / "project.json"
-with open(project_json_path, "r") as f:
-    project_json = json.load(f)
+    print(f"Validating project.json using schema.json schema...")
 
-print(f"Validating project.json using schema.json schema...")
+    try:
+        jsonschema.validate(instance=project_json, schema=schema_json)
+        print("Schema validation of project.json completed successfully.")
+        sys.exit(0)
+    except jsonschema.ValidationError as e:
+        path = ".".join([str(p) for p in e.path]) or "<root>"
+        print(f"Schema validation of project.json failed:")
+        print(f"  ✱ Path: {path}")
+        print(f"  ✱ Error: {e.message}")
+        print(f"  ✱ Expected: {e.schema.get('type')}")
+        print(f"  ✱ Schema rule: {e.schema}")
+        sys.exit(1)
 
-try:
-    jsonschema.validate(instance=project_json, schema=schema_json)
-    print("Schema validation of project.json completed successfully.")
-    sys.exit(0)
-except jsonschema.ValidationError as e:
-    path = ".".join([str(p) for p in e.path]) or "<root>"
-    print(f"Schema validation of project.json failed:")
-    print(f"  ✱ Path: {path}")
-    print(f"  ✱ Error: {e.message}")
-    print(f"  ✱ Expected: {e.schema.get('type')}")
-    print(f"  ✱ Schema rule: {e.schema}")
-    sys.exit(1)
+if __name__ == '__main__':
+    main()
