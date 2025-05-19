5f43149f42b00ab98163cc7c21ddbf7a9861c17a
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-05-19 11:47:27 +0200

    Add additional checks to the availability of RPMs and consistency between project.json. Removed 'default' prefix for some variables in project.json

--- continuousintegration/checks/validate_ci_input_variables.py
+++ continuousintegration/checks/validate_ci_input_variables.py
@@ -37,7 +37,8 @@ SUPPORTED = {
 
 SUPPORTED_ENV_VARS = [
     "PIPELINE_TYPE", "CUSTOM_XROOTD_VERSION",
-    "CUSTOM_EOS_VERSION", "CUSTOM_CTA_VERSION", "PLATFORM"
+    "CUSTOM_EOS_VERSION", "CUSTOM_CTA_VERSION", "PLATFORM",
+    "CUSTOM_EOS_IMAGE_TAG"
 ]
 
 def env_var_defined(name: str, ci_input_vars):
@@ -176,14 +177,44 @@ def main():
     # Ensure availability of any custom provided versions
     if env_var_defined("CUSTOM_CTA_VERSION", ci_input_vars):
         check_rpm_available("cta-frontend", ci_input_vars["CUSTOM_CTA_VERSION"])
+
     if env_var_defined("CUSTOM_XROOTD_VERSION", ci_input_vars):
-        check_rpm_available("xrootd-server", ci_input_vars["CUSTOM_XROOTD_VERSION"])
+        xrootd_version = ci_input_vars["CUSTOM_XROOTD_VERSION"]
+        project_xrootd_version = project_json["platforms"][ci_input_vars['PLATFORM']]["versionlock"]["group-xrootd"]
+        check_rpm_available("xrootd-server", xrootd_version)
+        # Check that at this point the project.json contains the same version
+        if xrootd_version != project_xrootd_version:
+            sys.exit(f"ERROR: CUSTOM_XROOTD_VERSION must be equal to value in project.json ({xrootd_version} != {project_xrootd_version}). Please verify the logic in the modify-project-json job.")
+
+
     if env_var_defined("CUSTOM_EOS_VERSION", ci_input_vars):
-        check_rpm_available("eos-server", ci_input_vars["CUSTOM_EOS_VERSION"])
-        eos_image_tag=f"{ci_input_vars['CUSTOM_EOS_VERSION']}.{ci_input_vars['PLATFORM']}"
-        check_image_tag_available(eos_image_tag, project_json["dev"]["defaultEosImageRepository"])
+        eos_version = ci_input_vars["CUSTOM_EOS_VERSION"]
+        project_eos_version = project_json["platforms"][ci_input_vars['PLATFORM']]["versionlock"]["group-eos"]
+        check_rpm_available("eos-server", eos_version)
+        # Check that at this point the project.json contains the same version
+        if eos_version != project_eos_version:
+            sys.exit(f"ERROR: CUSTOM_EOS_VERSION must be equal to value in project.json ({eos_version} != {project_eos_version}). Please verify the logic in the modify-project-json job.")
+
     if env_var_defined("CUSTOM_SYSTEM_TEST_IMAGE_TAG", ci_input_vars):
-        check_image_tag_available(eos_image_tag, project_json["dev"]["defaultEosImageRepository"])
+        check_image_tag_available(ci_input_vars['CUSTOM_SYSTEM_TEST_IMAGE_TAG'], project_json["dev"]["ctaImageRepository"])
+
+
+    project_eos_image_tag = project_json["dev"]["eosImageTag"]
+    if env_var_defined("CUSTOM_EOS_IMAGE_TAG", ci_input_vars):
+        eos_image_tag = ci_input_vars['CUSTOM_EOS_IMAGE_TAG']
+        check_image_tag_available(ci_input_vars['CUSTOM_EOS_IMAGE_TAG'], project_json["dev"]["eosImageRepository"])
+        # Check that at this point the project.json contains the same version
+        if eos_image_tag != project_eos_image_tag:
+            sys.exit(f"ERROR: CUSTOM_EOS_IMAGE_TAG must be equal to value in project.json ({eos_image_tag} != {project_eos_image_tag}). Please verify the logic in the modify-project-json job.")
+    elif env_var_defined("CUSTOM_EOS_VERSION", ci_input_vars):
+        # Deduce from CUSTOM_EOS_VERSION if provided. Must match the logic in the setup CI stage
+        custom_eos_version = ci_input_vars["CUSTOM_EOS_VERSION"]
+        eos_base_version = custom_eos_version.rsplit("-", 1)[0]  # Strip the release number
+        eos_image_tag = f"{eos_base_version}.{ci_input_vars['PLATFORM']}"
+        check_image_tag_available(eos_image_tag, project_json["dev"]["eosImageRepository"])
+        # Check that at this point the project.json contains the same version
+        if eos_image_tag != project_eos_image_tag:
+            sys.exit(f"ERROR: EOS image tag deduced from CUSTOM_EOS_VERSION must be equal to value in project.json ({eos_image_tag} != {project_eos_image_tag}). Please verify the logic in the modify-project-json job.")
 
     print("Validation was successful")
 
