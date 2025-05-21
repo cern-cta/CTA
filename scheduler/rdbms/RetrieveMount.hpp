80f0cb7ef34106f73fefefbad87b9f47d3f9a5df
guenther <jaroslav.guenther@cern.ch>
2025-05-21 19:30:10 +0200

    fixing JobPool shared_ptr initialisation in mounts

--- scheduler/rdbms/RetrieveMount.hpp
+++ scheduler/rdbms/RetrieveMount.hpp
@@ -42,7 +42,9 @@ class RetrieveMount : public SchedulerDatabase::RetrieveMount {
 public:
   //RetrieveMount(const std::string& ownerId, Transaction& txn, const std::string &vid) :
   //   , m_txn(txn), m_vid(vid) { }
-  RetrieveMount(RelationalDB& reldb) : m_RelationalDB(reldb), m_connPool(reldb.m_connPool), m_jobPool(reldb.m_connPool) {}
+  RetrieveMount(RelationalDB& pdb) : m_RelationalDB(pdb), m_connPool(pdb.m_connPool) {
+    m_jobPool = std::make_shared<schedulerdb::JobPool<schedulerdb::RetrieveRdbJob>>(m_connPool);
+  }
 
   const MountInfo& getMountInfo() override;
 
@@ -87,7 +89,7 @@ public:
 private:
   cta::RelationalDB& m_RelationalDB;
   cta::rdbms::ConnPool& m_connPool;
-  schedulerdb::JobPool<schedulerdb::RetrieveRdbJob> m_jobPool;
+  std::shared_ptr<schedulerdb::JobPool<schedulerdb::RetrieveRdbJob>> m_jobPool;
   void recycleTransferredJobs(std::list<std::unique_ptr<SchedulerDatabase::RetrieveJob>>& jobsBatch,
                               log::LogContext& lc);
 };
