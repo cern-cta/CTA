6fbaa858b2dd133677d17c70b8740a0d1dff2d2e
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-06-02 14:24:54 +0200

    Add additional debug info to repack test

--- continuousintegration/orchestration/tests/test_repack.sh
+++ continuousintegration/orchestration/tests/test_repack.sh
@@ -486,7 +486,7 @@ repackMoveAndAddCopies() {
   echo "OK"
 
   VID_TO_REPACK=$(getFirstVidContainingFiles)
-  kubectl -n ${NAMESPACE} exec ${EOS_MGM_POD} -c eos-mgm -- eos ls -l /eos/ctaeos/repack/${VID_TO_REPACK}
+  kubectl -n ${NAMESPACE} exec ${EOS_MGM_POD} -c eos-mgm -- eos ls -la /eos/ctaeos/repack/${VID_TO_REPACK}
 
   echo "Marking the tape ${VID_TO_REPACK} as REPACKING"
   modifyTapeStateAndWait ${VID_TO_REPACK} REPACKING
@@ -537,14 +537,14 @@ repackMoveAndAddCopies() {
     echo "Found $tapepoolDestination2_repack in repack archive destination pools."
   fi
 
-  kubectl -n ${NAMESPACE} exec ${EOS_MGM_POD} -c eos-mgm -- eos ls -l /eos/ctaeos/repack/${VID_TO_REPACK}
+  kubectl -n ${NAMESPACE} exec ${EOS_MGM_POD} -c eos-mgm -- eos ls -la /eos/ctaeos/repack/${VID_TO_REPACK}
   echo "----"
   removeRepackRequest ${VID_TO_REPACK}
   echo "Setting the tape ${VID_TO_REPACK} back to ACTIVE"
   modifyTapeState ${VID_TO_REPACK} ACTIVE
   echo "Reclaimimg tape ${VID_TO_REPACK}"
   kubectl -n ${NAMESPACE} exec ${CTA_CLI_POD} -c cta-cli -- cta-admin tape reclaim --vid ${VID_TO_REPACK}
-  kubectl -n ${NAMESPACE} exec ${EOS_MGM_POD} -c eos-mgm -- eos ls -l /eos/ctaeos/repack/${VID_TO_REPACK}
+  kubectl -n ${NAMESPACE} exec ${EOS_MGM_POD} -c eos-mgm -- eos ls -la /eos/ctaeos/repack/${VID_TO_REPACK}
 
   echo
   echo "***************************************************************"
