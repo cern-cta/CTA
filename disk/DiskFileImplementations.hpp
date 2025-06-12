b23f181adef6407ab1893610f39936bbeec4406b
Michael Davis <michael.davis@cern.ch>
2025-06-12 17:38:05 +0200

    Moves XrdCl out of scope of ctacommon

--- disk/DiskFileImplementations.hpp
+++ disk/DiskFileImplementations.hpp
@@ -20,8 +20,8 @@
 #include "disk/DiskFile.hpp"
 #include "tapeserver/castor/tape/tapeserver/file/Structures.hpp"
 #include "tapeserver/castor/tape/tapeserver/daemon/VolumeInfo.hpp"
-#include "common/exception/XrootCl.hpp"
 #include "common/exception/Exception.hpp"
+#include "XrdClException.hpp"
 #include <xrootd/XrdCl/XrdClFile.hh>
 #include <radosstriper/libradosstriper.hpp>
 
@@ -74,7 +74,6 @@ namespace cta::disk {
         mutable XrdCl::File m_xrootFile;
         mutable uint64_t m_readPosition;
         const uint16_t m_timeout;
-        typedef cta::exception::XrootCl XrootClEx;
       };
       
       class XrootReadFile: public XrootBaseReadFile {
@@ -95,7 +94,6 @@ namespace cta::disk {
         XrdCl::File m_xrootFile;
         uint64_t m_writePosition;
         const uint16_t m_timeout;
-        typedef cta::exception::XrootCl XrootClEx;
         bool m_closeTried;      
       };
       
