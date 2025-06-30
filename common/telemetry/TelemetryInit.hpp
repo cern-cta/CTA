3f59196ae5ac2860a671efbe8389a5911d43d736
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-06-30 14:59:20 +0200

    Split intrumentprovider, add instancehint

--- common/telemetry/TelemetryInit.hpp
+++ common/telemetry/TelemetryInit.hpp
@@ -10,18 +10,21 @@ namespace cta::telemetry {
  * Note that this method is not "fork-safe". That is, before doing a fork, reset the MeterProvider by calling resetTelemetry
  * Immediately after the fork, call reinitTelemetry. Provided that there are no meters actively being instantiated at the time of forking,
  * this will ensure both parent and child process end up with correct state.
+ *
+ * If this function is not called, any telemetry-related functionality will be a NOOP
  */
 void initTelemetry(const TelemetryConfig& config);
 
 /**
  * Initialises the telemetry config, but not telemetry itself
- * After this has been used, reinitTelemetry() can be used
+ * After this has been used, reinitTelemetry() can be used.
  */
 void initTelemetryConfig(const TelemetryConfig& config);
 
 /**
- * Provided that initTelemetry has been called at some point in the past,
- * this method will reuse the original telemetry config to reinitialise telemetry
+ * This method will reuse the existing telemetry config to reinitialise telemetry.
+ * Requires initTelemetry or initTelemetryConfig to have been called prior to this.
+ *
  */
 void reinitTelemetry();
 
