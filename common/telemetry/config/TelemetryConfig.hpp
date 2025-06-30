ce2f6e7d04b1ec51807029430388e1898bb7ed03
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-06-30 14:59:20 +0200

    Split intrumentprovider, add instancehint

--- common/telemetry/config/TelemetryConfig.hpp
+++ common/telemetry/config/TelemetryConfig.hpp
@@ -12,7 +12,7 @@ enum class MetricsBackend { NOOP, STDOUT, OTLP };
 typedef struct TelemetryConfig {
   std::string serviceName;
   std::string serviceNamespace;
-  std::string instanceHint;
+  std::string serviceInstanceHint;
   std::map<std::string, std::string> resourceAttributes;
 
   struct Metrics {
@@ -26,11 +26,33 @@ typedef struct TelemetryConfig {
 
 class TelemetryConfigBuilder {
 public:
+  /**
+   * Name of the service. Used to construct service.name
+   *
+   * The triplet (service.namespace, service.name, service.instance.id) must be globally unique.
+   *
+   * See https://opentelemetry.io/docs/specs/semconv/registry/attributes/service/
+   */
   TelemetryConfigBuilder& serviceName(std::string serviceName);
+
+  /**
+   * Namespace the service is running in. Used to construct service.namespace
+   *
+   * The triplet (service.namespace, service.name, service.instance.id) must be globally unique.
+   *
+   * See https://opentelemetry.io/docs/specs/semconv/registry/attributes/service/
+   */
   TelemetryConfigBuilder& serviceNamespace(std::string serviceNamespace);
-  // The instance hint is an optional field that can be used to generate a deterministic service.instance.id that persists across restarts
-  // For this to work, the instanceHint must allow the triplet (hostname, processName, instanceHint) to be globally unique
-  TelemetryConfigBuilder& instanceHint(std::string instanceHint);
+  /**
+   * Used to generate service.instance.id as follows: <hostname.processname.serviceInstanceHint>.
+   * When providing this, take care that the serviceInstanceHint is sufficient to distinguish between
+   * replicaes of a service running on the same host.
+   *
+   * The triplet (service.namespace, service.name, service.instance.id) must be globally unique.
+   *
+   * See https://opentelemetry.io/docs/specs/semconv/registry/attributes/service/
+   */
+  TelemetryConfigBuilder& serviceInstanceHint(std::string serviceInstanceHint);
   TelemetryConfigBuilder& resourceAttribute(std::string key, std::string value);
   TelemetryConfigBuilder& metricsBackend(const std::string& backendType);
   TelemetryConfigBuilder& metricsExportInterval(std::chrono::milliseconds interval);
