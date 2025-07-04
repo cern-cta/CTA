96f547bf901f27189dc5f6096d3d461fd038973f
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-07-04 15:42:44 +0200

    Fix cluster level resource creation in k8s

--- continuousintegration/orchestration/create_instance.sh
+++ continuousintegration/orchestration/create_instance.sh
@@ -307,7 +307,7 @@ create_instance() {
     echo "Installing Telemetry and Prometheus charts..."
     helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
     helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-    helm install opentelemetry open-telemetry/opentelemetry-collector \
+    helm install otel open-telemetry/opentelemetry-collector \
           --namespace "${namespace}" \
           --values presets/dev-opentelemetry-values.yaml \
           --wait --timeout 2m
