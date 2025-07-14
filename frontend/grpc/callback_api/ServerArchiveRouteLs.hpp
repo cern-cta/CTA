06cb5ea86057703df7716bd80661b6f47783a817
Konstantina Skovola <konstantina.skovola@cern.ch>
2025-07-14 18:01:23 +0200

    Update ArchiveRouteLs to use template class

--- frontend/grpc/callback_api/ServerArchiveRouteLs.hpp
+++ frontend/grpc/callback_api/ServerArchiveRouteLs.hpp
@@ -8,64 +8,32 @@
 #include <grpcpp/grpcpp.h>
 #include "common/dataStructures/LabelFormatSerDeser.hpp"
 #include <common/dataStructures/ArchiveRouteTypeSerDeser.hpp>
-#include "CtaAdminServerWriteReactor.hpp"
+#include "TemplateAdminCmdStream.hpp"
 #include "cmdline/admin_common/DataItemMessageFill.hpp"
 
 namespace cta::frontend::grpc {
 
-class ArchiveRouteLsWriteReactor : public CtaAdminServerWriteReactor {
+class ArchiveRouteLsWriteReactor : public TemplateAdminCmdStream<cta::common::dataStructures::ArchiveRoute, cta::admin::ArchiveRouteLsItem, decltype(&fillArchiveRouteItem)> {
     public:
         ArchiveRouteLsWriteReactor(cta::catalogue::Catalogue &catalogue, cta::Scheduler &scheduler, const std::string& instanceName, const cta::xrd::Request* request);
-        void NextWrite() override;
-    private:
-        std::list<cta::common::dataStructures::ArchiveRoute> m_archiveRouteList;
-        std::list<cta::common::dataStructures::ArchiveRoute>::const_iterator next_ar;
+        cta::admin::ArchiveRouteLsItem* getMessageField(cta::xrd::Data* data) override;
 };
 
-ArchiveRouteLsWriteReactor::ArchiveRouteLsWriteReactor(cta::catalogue::Catalogue &catalogue, cta::Scheduler &scheduler, const std::string& instanceName, const cta::xrd::Request* request)
-    : CtaAdminServerWriteReactor(catalogue, scheduler, instanceName),
-      m_archiveRouteList(catalogue.ArchiveRoute()->getArchiveRoutes()) {
-    using namespace cta::admin;
+ArchiveRouteLsWriteReactor::ArchiveRouteLsWriteReactor(cta::catalogue::Catalogue& catalogue,
+                                                       cta::Scheduler& scheduler,
+                                                       const std::string& instanceName,
+                                                       const cta::xrd::Request* request)
+    : TemplateAdminCmdStream<cta::common::dataStructures::ArchiveRoute,
+                             cta::admin::ArchiveRouteLsItem,
+                             decltype(&fillArchiveRouteItem)>(catalogue,
+                                                              scheduler,
+                                                              instanceName,
+                                                              catalogue.ArchiveRoute()->getArchiveRoutes(),
+                                                              cta::admin::HeaderType::ARCHIVEROUTE_LS,
+                                                              &fillArchiveRouteItem) {}
 
-    std::cout << "In ArchiveRouteLsWriteReactor constructor, just entered!" << std::endl;
-
-    next_ar = m_archiveRouteList.cbegin();
-    NextWrite();
+cta::admin::ArchiveRouteLsItem* ArchiveRouteLsWriteReactor::getMessageField(cta::xrd::Data* data) {
+    return data->mutable_arls_item();
 }
 
-void ArchiveRouteLsWriteReactor::NextWrite() {
-    std::cout << "In ArchiveRouteLsWriteReactor::NextWrite(), just entered!" << std::endl;
-    m_response.Clear();
-    // is this the first item? Then write the header
-    if (!m_isHeaderSent) {
-        cta::xrd::Response *header = new cta::xrd::Response();
-        std::cout << "header is not sent, sending the header" << std::endl;
-        header->set_type(cta::xrd::Response::RSP_SUCCESS);
-        header->set_show_header(cta::admin::HeaderType::ARCHIVEROUTE_LS);
-        m_response.set_allocated_header(header); // now the message takes ownership of the allocated object, we don't need to free header
-
-        m_isHeaderSent = true;
-        std::cout << "about to call StartWrite on the server side" << std::endl;
-        StartWrite(&m_response); // this will trigger the OnWriteDone method
-        std::cout << "called StartWrite on the server" << std::endl;
-        return; // because we'll be called in a loop by OnWriteDone
-    } else {
-        while(next_ar != m_archiveRouteList.cend()) {
-            const auto& ar = *next_ar;
-            ++next_ar;
-            cta::xrd::Data* data = new cta::xrd::Data();
-            cta::admin::ArchiveRouteLsItem *ar_item = data->mutable_arls_item();
-            
-            fillArchiveRouteItem(ar, ar_item, m_instanceName);
-
-            std::cout << "Calling StartWrite on the server, with some data this time" << std::endl;
-            m_response.set_allocated_data(data);
-            StartWrite(&m_response);
-            return; // because we will be called in a loop by OnWriteDone()
-        } // end while
-        std::cout << "Finishing the call on the server side" << std::endl;
-        // Finish the call
-        Finish(::grpc::Status::OK);
-    }
-}
 } // namespace cta::frontend::grpc
