9002dd1d33f2f18967461b7a963d0f7fb6e6ae3e
nbugel <niels.alexander.bugel@cern.ch>
2025-08-07 17:30:44 +0200

    Applied clang format to changed lines

--- tapeserver/daemon/TapeDaemon.cpp
+++ tapeserver/daemon/TapeDaemon.cpp
@@ -105,9 +105,9 @@ void cta::tape::daemon::TapeDaemon::mainEventLoop() {
   // Used in the process name, which can be a max of 16 bytes
   // We need at least 2 characters for the suffix and 1 for the null character
   if (shortName.size() > 13) {
-    lc.log(log::WARN, "DriveConfigEntry::DriveConfigEntry - short unitName '" + shortName +
-                                    "' exceeds max length of " + std::to_string(maxShortDriveNameLen) + " (got " +
-                                    std::to_string(shortName.size()) + ")");
+    lc.log(log::WARN,
+           "DriveConfigEntry::DriveConfigEntry - short unitName '" + shortName + "' exceeds max length of " +
+             std::to_string(maxShortDriveNameLen) + " (got " + std::to_string(shortName.size()) + ")");
   }
   std::string processName = dce.getShortUnitName() + "-parent";
   prctl(PR_SET_NAME, processName.c_str());
