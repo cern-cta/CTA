3887af014692c94e65c7b73376c4be9e5f8f48f6
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-08-12 14:59:45 +0200

    Improved telemetry values

--- continuousintegration/orchestration/tests/test_client_gfal2.sh
+++ continuousintegration/orchestration/tests/test_client_gfal2.sh
@@ -180,13 +180,6 @@ if [[ $? == 1 ]]; then
     exit 1
 fi
 
-echo "Sleeping 10 seconds to give Prometheus time to scrape the metrics"
-sleep 10
-
-# Test that telemetry agrees with the amount of files archived and retrieved
-kubectl -n ${NAMESPACE} exec ${CLIENT_POD} -c client -- bash -c "/root/client_telemetry_summary.sh"  || exit 1
-kubectl -n ${NAMESPACE} cp ${CLIENT_POD}:/metrics.txt ../../../metrics.txt
-
 # Test activity
 TEST_PRERUN=". /root/client_env "
 
@@ -202,4 +195,10 @@ echo "Checking activity was set..."
 kubectl -n ${NAMESPACE} cp grep_eosreport_for_activity.sh ${EOS_MGM_POD}:/root/ -c eos-mgm
 kubectl -n ${NAMESPACE} exec ${EOS_MGM_POD} -c eos-mgm -- bash /root/grep_eosreport_for_activity.sh || exit 1
 
-exit 0
+echo "Sleeping 15 seconds to give Prometheus time to scrape the metrics"
+sleep 15
+
+# Test that telemetry agrees with the amount of files archived and retrieved
+kubectl -n ${NAMESPACE} exec ${CLIENT_POD} -c client -- bash -c "/root/client_telemetry_summary.sh"  || exit 1
+kubectl -n ${NAMESPACE} cp ${CLIENT_POD}:/metrics.txt ../../../metrics.txt
+
