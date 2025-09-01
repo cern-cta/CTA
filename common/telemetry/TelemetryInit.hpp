99086c4c97028cbc6fe727350070bfd06cc1aa44
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-09-01 10:45:47 +0200

    Add and update documentation

--- common/telemetry/TelemetryInit.hpp
+++ common/telemetry/TelemetryInit.hpp
@@ -25,7 +25,7 @@ namespace cta::telemetry {
 
 /**
  * Initialises telemetry according to the provided config.
- * Note that this method is not "fork-safe". That is, before doing a fork, reset the MeterProvider by calling shutdownTelemetry
+ * Note that this method is not "fork-safe". That is, before doing a fork, reset the MeterProvider by calling shutdownTelemetry.
  * Immediately after the fork, call reinitTelemetry. Provided that there are no meters actively being instantiated at the time of forking,
  * this will ensure both parent and child process end up with correct state.
  *
@@ -34,7 +34,7 @@ namespace cta::telemetry {
 void initTelemetry(const TelemetryConfig& config, cta::log::LogContext& lc);
 
 /**
- * Initialises the telemetry config, but not telemetry itself
+ * Initialises the telemetry config, but not telemetry itself. Useful if the locations where the config is initialised and telemetry itself is initialised are different.
  * After this has been used, reinitTelemetry() can be used.
  */
 void initTelemetryConfig(const TelemetryConfig& config);
