e20cb923f2aaae138237cd96e99b214dc7c13363
Joao Afonso <joao.afonso@cern.ch>
2025-10-06 20:20:39 +0200

    Creating new test for tape rest api

---
+++ continuousintegration/orchestration/tests/test_client_token.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# @project      The CERN Tape Archive (CTA)
+# @copyright    Copyright © 2024 CERN
+# @license      This program is free software, distributed under the terms of the GNU General Public
+#               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
+#               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
+#               option) any later version.
+#
+#               This program is distributed in the hope that it will be useful, but WITHOUT ANY
+#               WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#               PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+#               In applying this licence, CERN does not waive the privileges and immunities
+#               granted to it by virtue of its status as an Intergovernmental Organization or
+#               submit itself to any jurisdiction.
+PREPARE=1 # run prepare by default
+
+usage() { cat <<EOF 1>&2
+Usage: $0 -n <namespace> -q
+   -q: do not run prepare
+EOF
+exit 1
+}
+
+while getopts "n:q" o; do
+    case "${o}" in
+        n)
+            NAMESPACE=${OPTARG}
+            ;;
+        q)
+            PREPARE=0
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${NAMESPACE}" ]; then
+    usage
+fi
+
+if [ ! -z "${error}" ]; then
+    echo -e "ERROR:\n${error}"
+    exit 1
+fi
+
+CLIENT_POD="cta-client-0"
+EOS_MGM_POD="eos-mgm-0"
+
+if [[ ${PREPARE} -eq 1 ]]; then
+  echo "Preparing namespace for the tests"
+    . prepare_tests.sh -n ${NAMESPACE}
+  if [ $? -ne 0 ]; then
+    echo "ERROR: failed to prepare namespace for the tests"
+    exit 1
+  fi
+fi
+
+echo
+echo "Copying test scripts to ${CLIENT_POD} pod."
+kubectl -n ${NAMESPACE} cp . ${CLIENT_POD}:/root/ -c client
+
+NB_FILES=10000
+FILE_SIZE_KB=15
+NB_PROCS=20
+
+echo
+echo "Setting up environment for tests."
+kubectl -n ${NAMESPACE} exec ${CLIENT_POD} -c client -- bash -c "/root/client_setup.sh -n ${NB_FILES} -s ${FILE_SIZE_KB} -p ${NB_PROCS} -d /eos/ctaeos/preprod -r -c xrd" || exit 1
+
+# Test are run under the cta user account which doesn't have a login
+# option so to be able to export the test setup we need to source the file
+# client_env (file generated in client_setup with all env varss and fucntions)
+#
+# Also, to show the output of tpsr-X rmcd to the logs we need to tail the files
+# before every related script and kill it a the end. Another way to do this would
+# require to change the stdin/out/err of the tail process and set//reset it
+# at the beginning and end of each kubectl exec command.
+TEST_PRERUN=". /root/client_env "
+TEST_POSTRUN=""
+
+echo "Setting up client pod for HTTPs REST API test"
+echo " Copying CA certificates to client pod from ${EOS_MGM_POD} pod."
+kubectl -n ${NAMESPACE} cp "${EOS_MGM_POD}:etc/grid-security/certificates/" /tmp/certificates/ -c eos-mgm
+kubectl -n ${NAMESPACE} cp /tmp/certificates ${CLIENT_POD}:/etc/grid-security/ -c client
+rm -rf /tmp/certificates
+
+echo " Launching client_rest_api_token.sh on client pod"
+kubectl -n ${NAMESPACE} exec ${CLIENT_POD} -c client -- bash /root/client_rest_api_token.sh || exit 1
+
+exit 0
