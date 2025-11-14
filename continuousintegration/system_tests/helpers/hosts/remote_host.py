e98f1b7648828fb1ab238cff105f10a92b204a7f
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-11-15 00:32:47 +0100

    Formatting improvements

--- continuousintegration/system_tests/helpers/hosts/remote_host.py
+++ continuousintegration/system_tests/helpers/hosts/remote_host.py
@@ -4,7 +4,8 @@ from functools import cached_property
 from typing import Optional
 import time
 
-class RemoteHost():
+
+class RemoteHost:
     def __init__(self, conn: RemoteConnection):
         self.conn = conn
         # print("Initialising remote host: " + self.description)
@@ -17,19 +18,19 @@ class RemoteHost():
     def description(self) -> str:
         return self.conn.description
 
-    def exec(self, command: str, capture_output = False, throw_on_failure = True) -> CompletedProcess[bytes]:
+    def exec(self, command: str, capture_output=False, throw_on_failure=True) -> CompletedProcess[bytes]:
         return self.conn.exec(command, capture_output=capture_output, throw_on_failure=throw_on_failure)
 
-    def execWithOutput(self, command: str, throw_on_failure = True) -> str:
+    def execWithOutput(self, command: str, throw_on_failure=True) -> str:
         return self.conn.exec(command, capture_output=True, throw_on_failure=throw_on_failure).stdout.decode().strip()
 
-    def copyTo(self, src_path: str, dst_path: str, throw_on_failure = True, permissions: Optional[str] = None) -> None:
+    def copyTo(self, src_path: str, dst_path: str, throw_on_failure=True, permissions: Optional[str] = None) -> None:
         return self.conn.copyTo(src_path, dst_path, throw_on_failure=throw_on_failure, permissions=permissions)
 
-    def copyFrom(self, src_path: str, dst_path: str, throw_on_failure = True) -> None:
+    def copyFrom(self, src_path: str, dst_path: str, throw_on_failure=True) -> None:
         return self.conn.copyFrom(src_path, dst_path, throw_on_failure=throw_on_failure)
 
-    def restart(self, throw_on_failure = True) -> None:
+    def restart(self, throw_on_failure=True) -> None:
         return self.conn.restart(throw_on_failure)
 
     def is_host_up(self) -> None:
