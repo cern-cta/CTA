4d535133f3e6662e216f76f36ac3d467c69e62ae
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-11-15 00:32:47 +0100

    Formatting improvements

--- continuousintegration/system_tests/conftest.py
+++ continuousintegration/system_tests/conftest.py
@@ -7,6 +7,7 @@ import shutil
 # General/common fixtures
 #####################################################################################################################
 
+
 # This is how all the tests get access to the different hosts (cli, frontend, taped, etc)
 @pytest.fixture(scope="session", autouse=True)
 def env(request):
@@ -17,13 +18,17 @@ def env(request):
         pytest.exit("ERROR: Only one of --namespace or --connection-config can be provided, not both.", returncode=1)
 
     if namespace is None and connection_config is None:
-        pytest.exit("ERROR: Missing mandatory argument: one of --namespace or --connection-config must be provided", returncode=1)
+        pytest.exit(
+            "ERROR: Missing mandatory argument: one of --namespace or --connection-config must be provided",
+            returncode=1,
+        )
 
     if connection_config is None:
         # No connection configuration provided, so assume everything is running in a cluster
         return TestEnv.fromNamespace(namespace)
     else:
         return TestEnv.fromConfig(connection_config)
+
 
 # The only purpose of this fixture is to make the test output easier to read
 # in particular by more clearly visually separating different test cases
@@ -34,7 +39,7 @@ def make_tests_look_pretty(request):
 
     # construct the magic separators
     test_name = request.node.name
-    test_title = f" {test_name} " # leave 2 spaces around the name
+    test_title = f" {test_name} "  # leave 2 spaces around the name
     side = (terminal_width - len(test_title)) // 2
     line = "=" * side + test_title + "=" * (terminal_width - side - len(test_title))
     separator: str = "—" * terminal_width
@@ -43,35 +48,48 @@ def make_tests_look_pretty(request):
     yield
     terminal_writer.write(f"\n\n{separator}", cyan=True)
 
+
 @pytest.fixture()
 def krb5_realm(request):
     return request.config.getoption("--krb5-realm")
+
 
 @pytest.fixture()
 def disk_instance(request):
     return request.config.getoption("--disk-instance")
 
+
 #####################################################################################################################
 # Commandline options
 #####################################################################################################################
 
+
 def pytest_addoption(parser):
     parser.addoption("--namespace", action="store", help="Namespace for tests")
-    parser.addoption("--connection-config", action="store", help="A yaml connection file specifying how to connect to each host")
+    parser.addoption(
+        "--connection-config", action="store", help="A yaml connection file specifying how to connect to each host"
+    )
     parser.addoption("--no-setup", action="store_true", help="Skip the execution of test_setup")
     parser.addoption("--no-teardown", action="store_true", help="Skip the execution of test_teardown")
-    parser.addoption("--clean-start", action="store_true", help="Run the teardown before starting the tests to ensure a clean start")
+    parser.addoption(
+        "--clean-start", action="store_true", help="Run the teardown before starting the tests to ensure a clean start"
+    )
 
     # Test specific options
-    parser.addoption("--krb5-realm", type=str, default="TEST.CTA", help="Kerberos realm to use for cta-admin/eos commands")
+    parser.addoption(
+        "--krb5-realm", type=str, default="TEST.CTA", help="Kerberos realm to use for cta-admin/eos commands"
+    )
     parser.addoption("--disk-instance", type=str, default="ctaeos", help="Name of the disk instance")
     parser.addoption("--stress-num-files", type=int, default=1000, help="Number of files to use for the stress test")
-    parser.addoption("--stress-file-size", type=int, default=100, help="Size of the files in bytes to use for the stress test")
+    parser.addoption(
+        "--stress-file-size", type=int, default=100, help="Size of the files in bytes to use for the stress test"
+    )
 
 
 #####################################################################################################################
 # Do some magic to automatically add setup and teardown to the test suite
 #####################################################################################################################
+
 
 def is_test_in_items(test_path: str, items):
     resolved_test_path = Path(test_path).resolve()
@@ -92,6 +110,7 @@ def add_test_into_existing_collection(test_path: str, items, prepend: bool = Fal
         index = 0 if prepend else len(items)
         items[index:index] = tests
 
+
 def pytest_collection_modifyitems(config, items):
     """Automatically include some tests every time pytest runs. These tests can be skipped"""
     setup_script: str = "tests/test_setup.py"
@@ -100,14 +119,16 @@ def pytest_collection_modifyitems(config, items):
 
     # Always check for errors after the run
     add_test_into_existing_collection(error_script, items, prepend=False)
+
     # Always do a setup of CTA before the tests start (unless skipped)
     if not is_test_in_items(setup_script, items) and not config.getoption("--no-setup"):
-        # Only add it if it wasn't manually added by the user
         add_test_into_existing_collection(setup_script, items, prepend=True)
+
     # Always do a teardown of CTA after the tests finnish (unless skipped)
-    if not is_test_in_items(setup_script, items) and not config.getoption("--no-teardown"):
-        # Only add it if it wasn't manually added by the user
+    if not is_test_in_items(teardown_script, items) and not config.getoption("--no-teardown"):
         add_test_into_existing_collection(teardown_script, items, prepend=False)
-    # Do the reset before the tests start. Useful when rerunning the tests multiple times on the same instance
+
+    # Do the reset before the tests start.
+    # Useful when rerunning the tests multiple times on the same instance and it wasn't properly cleaned up
     if config.getoption("--clean-start"):
         add_test_into_existing_collection(teardown_script, items, prepend=True, allow_duplicate=True)
