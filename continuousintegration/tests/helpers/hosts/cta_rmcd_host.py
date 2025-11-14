545afdee1375cba21979902daa99a3bff2fbd6f1
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-11-14 19:22:10 +0100

    Initial working system test framework

--- continuousintegration/tests/helpers/hosts/cta_rmcd_host.py
+++
@@ -1,8 +0,0 @@
-from .remote_host import RemoteHost
-
-class CtaRmcdHost(RemoteHost):
-    def __init__(self, conn):
-        super().__init__(conn)
-
-    def library_device(self) -> str:
-        return self.execWithOutput("printenv LIBRARY_DEVICE")
