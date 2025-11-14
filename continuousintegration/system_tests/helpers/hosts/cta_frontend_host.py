e81a5e9acfa9c6595df7262d35aa0cd120272210
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-11-14 19:22:10 +0100

    Initial working system test framework

--- continuousintegration/system_tests/helpers/hosts/cta_frontend_host.py
+++ continuousintegration/system_tests/helpers/hosts/cta_frontend_host.py
@@ -1,8 +1,13 @@
 from .remote_host import RemoteHost
+from functools import cached_property
 
 class CtaFrontendHost(RemoteHost):
     def __init__(self, conn):
         super().__init__(conn)
+
+    @cached_property
+    def log_file_location(self) -> str:
+        return f"/var/log/cta/cta-frontend.log"
 
     def get_schema_version(self) -> str:
         return self.execWithOutput(r"cta-catalogue-schema-verify /etc/cta/cta-catalogue.conf | grep -o -E '[0-9]+\.[0-9]")
--- continuousintegration/system_tests/tests/test_setup.py
+++ continuousintegration/system_tests/tests/test_setup.py
@@ -1,4 +1,27 @@
-from ...helpers.tape import list_all_tapes_in_libraries
+from ..helpers.hosts.cta_rmcd_host import CtaRmcdHost
+from concurrent.futures import ThreadPoolExecutor
+from itertools import cycle
+
+# Script copying
+
+def test_copy_scripts_to_client(env):
+    for client in env.client:
+        client.copyTo("tests/remote_scripts/client/", "/test/", permissions="+x")
+
+def test_copy_scripts_to_ctacli(env):
+    for ctacli in env.ctacli:
+        ctacli.copyTo("tests/remote_scripts/ctacli/", "/test/", permissions="+x")
+
+# Kerberos
+
+def test_kinit_clients(env):
+    # TODO: this together with other options should go into a config file somewhere
+    krb5_realm="TEST.CTA"
+    env.ctacli[0].exec(f"kinit -kt /root/ctaadmin1.keytab ctaadmin1@{krb5_realm}")
+    env.client[0].exec(f"kinit -kt /root/user1.keytab user1@{krb5_realm}")
+
+
+# Catalogue initialization
 
 def test_check_catalogue(env):
     env.ctafrontend[0].exec("cta-catalogue-schema-verify /etc/cta/cta-catalogue.conf")
@@ -16,17 +39,15 @@ def test_version_info(env):
 
 def test_populate_catalogue(env):
     print("Populating catalogue")
-    # TODO: figure out a nice way to do these paths
-    env.ctacli[0].copyTo("system_tests/scripts/populate_catalogue.sh", "/root/populate_catalogue.sh", permissions="+x")
-    env.ctacli[0].exec(f"./root/populate_catalogue.sh {env.disk_instance_name}")
+    env.ctacli[0].exec(f"./test/populate_catalogue.sh ctaeos")
 
 def test_populate_catalogue_tapes(env):
-    tape_drives_in_use: list[str] = [taped.drive_name() for taped in env.ctataped]
+    tape_drives_in_use: list[str] = [taped.drive_name for taped in env.ctataped]
     print("Using drives:")
     for drive in tape_drives_in_use:
         print(f"  - {drive}")
 
-    libraries_in_use: list[str] = [rmcd.library_device() for rmcd in env.ctarmcd]
+    libraries_in_use: list[str] = [rmcd.library_device for rmcd in env.ctarmcd]
     print("Using libraries:")
     for lib in libraries_in_use:
         print(f"  - {lib}")
@@ -38,12 +59,10 @@ def test_populate_catalogue_tapes(env):
                                 --comment \"ctasystest library mapped to drive {drive}\""
         env.ctacli[0].exec(add_ll_cmd)
 
-    tapes: list[str] = list_all_tapes_in_libraries(env.ctarmcd)
+    tapes: list[str] = CtaRmcdHost.list_all_tapes_in_libraries(env.ctarmcd)
     print("Using tapes:")
     for tape in tapes:
         print(f"  - {tape}")
-
-    # TODO: collect and execute at once?
 
     for idx, tape in enumerate(tapes):
         # The logical libraries correspond to the tape drive names by design.
@@ -60,3 +79,30 @@ def test_populate_catalogue_tapes(env):
                                 --full false \
                                 --comment ctasystest"
         env.ctacli[0].exec(add_tape_cmd)
+
+# Tape infrastructure initialisation
+
+def test_reset_tapes(env):
+    for ctarmcd in env.ctarmcd:
+        ctarmcd.unload_tapes()
+
+def test_reset_drive_devices(env):
+    for ctataped in env.ctataped:
+        ctataped.exec(f"sg_turs {ctataped.drive_device} 2>&1 > /dev/null || true")
+
+def test_label_tapes(env):
+    tapes: list[str] = CtaRmcdHost.list_all_tapes_in_libraries(env.ctarmcd)
+    max_workers = len(env.ctataped)
+
+    # Run labeling on every drive
+    with ThreadPoolExecutor(max_workers=max_workers) as pool:
+        futures = []
+        for tape, taped in zip(tapes, cycle(env.ctataped)):
+            futures.append(pool.submit(taped.label_tape, tape))
+
+        # force errors to surface
+        for f in futures:
+            f.result()
+
+def test_set_all_drives_up(env):
+    env.ctacli[0].set_all_drives_up()
