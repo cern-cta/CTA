310724c8eddefed5702c857d612fdb64f8b2c3cd
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2025-12-10 16:45:27 +0100

    Add maintd process

---
+++ continuousintegration/system_tests/helpers/hosts/cta/cta_maintd_host.py
@@ -0,0 +1,11 @@
+from ..remote_host import RemoteHost
+from functools import cached_property
+
+
+class CtaMaintdHost(RemoteHost):
+    def __init__(self, conn):
+        super().__init__(conn)
+
+    @cached_property
+    def log_file_location(self) -> str:
+        return "/var/log/cta/cta-maintd.log"
