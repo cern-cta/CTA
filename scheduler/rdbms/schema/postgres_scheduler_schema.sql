f46812c93542d7393bc00dc3d2523888e46dea97
guenther <jaroslav.guenther@cern.ch>
2025-12-12 17:16:53 +0100

    rename multi-copy wait status to be more explicit

--- scheduler/rdbms/schema/postgres_scheduler_schema.sql
+++ scheduler/rdbms/schema/postgres_scheduler_schema.sql
@@ -1,6 +1,6 @@
 CREATE TYPE ARCHIVE_JOB_STATUS AS ENUM (
   'AJS_ToTransferForUser',
-  'AJS_WaitSiblingsBeforeSuccessReport',
+  'AJS_WaitSiblingsBeforeReportingSuccessToDisk',
   'AJS_ToReportToUserForSuccess',
   'AJS_Complete',
   'AJS_ToReportToUserForFailure',
