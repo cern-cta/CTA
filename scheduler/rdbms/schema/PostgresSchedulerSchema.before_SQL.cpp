8be104da65560d0fa4b9bc7cf1ea51b76270930d
guenther <jaroslav.guenther@cern.ch>
2026-01-19 00:36:31 +0100

    changing schema name from public to scheduler for scheduler db backend

--- scheduler/rdbms/schema/PostgresSchedulerSchema.before_SQL.cpp
+++ scheduler/rdbms/schema/PostgresSchedulerSchema.before_SQL.cpp
@@ -14,5 +14,4 @@ PostgresSchedulerSchema::PostgresSchedulerSchema()
     : SchedulerSchema(
         // CTA_SQL_SCHEMA - The contents of postgres_scheduler_schema.cpp go here
       ) {}
-
 }  // namespace cta::schedulerdb
