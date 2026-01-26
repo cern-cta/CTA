f6feb5cde1458514dce37a774641d300ca7ee78d
guenther <jaroslav.guenther@cern.ch>
2026-01-26 22:58:25 +0100

    instrumenting telemetry with scheduler workflow stage measurements

--- scheduler/Scheduler.cpp
+++ scheduler/Scheduler.cpp
@@ -2308,9 +2308,9 @@ std::unique_ptr<TapeMount> Scheduler::getNextMount(const std::string& logicalLib
               ttel.msecs(),
               {
                 {cta::semconv::attr::kSchedulerOperationName,
-                 cta::semconv::attr::SchedulerOperationNameValues::kCheckWorkForPotentialMount},
+                 cta::semconv::attr::SchedulerOperationNameValues::kGetNextPotentialMount},
                 {cta::semconv::attr::kSchedulerOperationWorkflow,
-                 cta::semconv::attr::SchedulerOperationWorkflowValues::kArchive               }
+                 cta::semconv::attr::SchedulerOperationWorkflowValues::kArchive          }
             },
               opentelemetry::context::RuntimeContext::GetCurrent());
             return std::unique_ptr<TapeMount>(internalRet.release());
@@ -3145,7 +3145,6 @@ void Scheduler::reportArchiveJobsBatch(std::list<std::unique_ptr<ArchiveJob>>& a
     {
       {cta::semconv::attr::kSchedulerOperationName,
        cta::semconv::attr::SchedulerOperationNameValues::kReportToUserAndDeleteSchedulerDB},
-      ,
       {cta::semconv::attr::kSchedulerOperationWorkflow,
        cta::semconv::attr::SchedulerOperationWorkflowValues::kArchive                     }
   },
@@ -3155,7 +3154,6 @@ void Scheduler::reportArchiveJobsBatch(std::list<std::unique_ptr<ArchiveJob>>& a
     {
       {cta::semconv::attr::kSchedulerOperationName,
        cta::semconv::attr::SchedulerOperationNameValues::kReportToUserAndDeleteSchedulerDB},
-      ,
       {cta::semconv::attr::kSchedulerOperationWorkflow,
        cta::semconv::attr::SchedulerOperationWorkflowValues::kArchive                     }
   },
