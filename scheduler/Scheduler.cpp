660da01396061d962c6acff5e1cb8413a6c211e4
guenther <jaroslav.guenther@cern.ch>
2026-01-29 00:20:23 +0100

    attempt to fix not appearing measurements in telemetry

--- scheduler/Scheduler.cpp
+++ scheduler/Scheduler.cpp
@@ -3257,7 +3257,6 @@ void Scheduler::reportRetrieveJobsBatch(std::list<std::unique_ptr<RetrieveJob>>&
        cta::semconv::attr::SchedulerOperationWorkflowValues::kRetrieve                    }
   },
     opentelemetry::context::RuntimeContext::GetCurrent());
-  repsize =
   cta::telemetry::metrics::ctaSchedulerOperationDuration->Record(
     ttel_scheddb,
     {
