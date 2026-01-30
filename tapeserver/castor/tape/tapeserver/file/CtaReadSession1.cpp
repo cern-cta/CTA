fd303c53d82245675271a23b1dc09c0a17c7fa9d
Joao Afonso <joao.afonso@cern.ch>
2026-01-30 09:49:45 +0100

    Added file to development setup list of repos

--- tapeserver/castor/tape/tapeserver/file/CtaReadSession1.cpp
+++ tapeserver/castor/tape/tapeserver/file/CtaReadSession1.cpp
@@ -1,39 +1,28 @@
 /*
- * @project      The CERN Tape Archive (CTA)
- * @copyright    Copyright © 2022 CERN
- * @license      This program is free software, distributed under the terms of the GNU General Public
- *               Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING". You can
- *               redistribute it and/or modify it under the terms of the GPL Version 3, or (at your
- *               option) any later version.
- *
- *               This program is distributed in the hope that it will be useful, but WITHOUT ANY
- *               WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- *               PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- *               In applying this licence, CERN does not waive the privileges and immunities
- *               granted to it by virtue of its status as an Intergovernmental Organization or
- *               submit itself to any jurisdiction.
+ * SPDX-FileCopyrightText: 2022 CERN
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
+
+#include "castor/tape/tapeserver/file/CtaReadSession1.hpp"
+
+#include "castor/tape/tapeserver/file/Exceptions.hpp"
+#include "castor/tape/tapeserver/file/HeaderChecker.hpp"
+#include "castor/tape/tapeserver/file/Structures.hpp"
 
 #include <memory>
 #include <string>
 
-#include "castor/tape/tapeserver/file/Exceptions.hpp"
-#include "castor/tape/tapeserver/file/HeaderChecker.hpp"
-#include "castor/tape/tapeserver/file/CtaReadSession1.hpp"
-#include "castor/tape/tapeserver/file/Structures.hpp"
-
 namespace castor::tape::tapeFile {
 
-CtaReadSession1::CtaReadSession1(tapeserver::drive::DriveInterface &drive,
-  const tapeserver::daemon::VolumeInfo &volInfo, const bool useLbp)
-  : ReadSession(drive, volInfo, useLbp) {
+CtaReadSession1::CtaReadSession1(tapeserver::drive::DriveInterface& drive,
+                                 const tapeserver::daemon::VolumeInfo& volInfo,
+                                 const bool useLbp)
+    : ReadSession(drive, volInfo, useLbp) {
   m_drive.rewind();
   m_drive.disableLogicalBlockProtection();
   {
     VOL1 vol1;
-    m_drive.readExactBlock(reinterpret_cast<void *>(&vol1), sizeof(vol1),
-      "[ReadSession::ReadSession()] - Reading VOL1");
+    m_drive.readExactBlock(reinterpret_cast<void*>(&vol1), sizeof(vol1), "[ReadSession::ReadSession()] - Reading VOL1");
     switch (vol1.getLBPMethod()) {
       case SCSI::logicBlockProtectionMethod::CRC32C:
         m_detectedLbp = true;
@@ -45,7 +34,7 @@ CtaReadSession1::CtaReadSession1(tapeserver::drive::DriveInterface &drive,
         break;
       case SCSI::logicBlockProtectionMethod::ReedSolomon:
         throw cta::exception::Exception("In ReadSession::ReadSession(): "
-            "ReedSolomon LBP method not supported");
+                                        "ReedSolomon LBP method not supported");
       case SCSI::logicBlockProtectionMethod::DoNotUse:
         m_drive.disableLogicalBlockProtection();
         m_detectedLbp = false;
@@ -59,11 +48,10 @@ CtaReadSession1::CtaReadSession1(tapeserver::drive::DriveInterface &drive,
   m_drive.rewind();
   {
     VOL1 vol1;
-    m_drive.readExactBlock(reinterpret_cast<void *>(&vol1), sizeof(vol1),
-      "[ReadSession::ReadSession()] - Reading VOL1");
+    m_drive.readExactBlock(reinterpret_cast<void*>(&vol1), sizeof(vol1), "[ReadSession::ReadSession()] - Reading VOL1");
     try {
       vol1.verify();
-    } catch (std::exception &e) {
+    } catch (std::exception& e) {
       throw TapeFormatError(e.what());
     }
     HeaderChecker::checkVOL1(vol1, volInfo.vid);
@@ -72,4 +60,4 @@ CtaReadSession1::CtaReadSession1(tapeserver::drive::DriveInterface &drive,
   }
 }
 
-} // namespace castor::tape::tapeFile
+}  // namespace castor::tape::tapeFile
