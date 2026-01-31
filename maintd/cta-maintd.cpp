302773d5dc94e2c8635e3bb92212c28d66e15421
nbugel <niels.alexander.bugel@cern.ch>
2026-01-31 10:39:35 +0100

    Progress with the argument parser

--- maintd/cta-maintd.cpp
+++ maintd/cta-maintd.cpp
@@ -1,24 +1,26 @@
 /*
- * SPDX-FileCopyrightText: 2021 CERN
+ * SPDX-FileCopyrightText: 2026 CERN
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-#include "MaintdCliOptions.hpp"
 #include "MaintdConfig.hpp"
 #include "RoutineRunner.hpp"
-#include "RoutineRunnerFactory.hpp"
 #include "common/runtime/Application.hpp"
-
-#include <getopt.h>
-#include <iostream>
-#include <memory>
-#include <signal.h>
-#include <string>
-#include <thread>
+#include "common/runtime/ArgParser.hpp"
+#include "common/runtime/CommonCliOptions.hpp"
 
 int main(const int argc, char** const argv) {
+  // TODO: sadly this will have to be wrapped in a try catch
   using namespace cta;
-  maintd::MaintdCliOptions opts;
-  runtime::Application<maintd::RoutineRunner, maintd::MaintdConfig, maintd::MaintdCliOptions> app("cta-maintd", opts);
+  const std::string appName = "cta-maind";
+
+  runtime::CommonCliOptions opts;
+  runtime::ArgParser argParser(appName, opts);
+  argParser.parser(argc, argv);
+  // At this point, opts is populated
+
+  // TODO: can opts be inferred?
+  runtime::Application<maintd::RoutineRunner, maintd::MaintdConfig, runtime::CommonCliOptions> app(appName, opts);
+  // Starts the RoutineRunner
   return app.run();
 }
