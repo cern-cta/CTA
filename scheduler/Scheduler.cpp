94e5b99d53325c104bab6b627c1a8c29f807a55b
guenther <jaroslav.guenther@cern.ch>
2026-02-01 04:53:13 +0100

    testing 200us time window

--- scheduler/Scheduler.cpp
+++ scheduler/Scheduler.cpp
@@ -266,7 +266,7 @@ std::string Scheduler::queueArchiveWithGivenId(const uint64_t archiveFileId,
     lc.log(log::DEBUG, "In Scheduler::queueArchiveWithGivenId() 6 : " + std::to_string(archiveFileId));
 
     // Opportunistic batching window
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    //std::this_thread::sleep_for(std::chrono::microseconds(100));
     lc.log(log::DEBUG, "In Scheduler::queueArchiveWithGivenId() 7 : " + std::to_string(archiveFileId));
 
     std::vector<cta::common::dataStructures::ArchiveInsertQueueItem> batch;
