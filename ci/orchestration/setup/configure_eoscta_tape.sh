ac316c8318426f1bd5c2dc1bc1848564ab8cd3dd
guenther <jaroslav.guenther@cern.ch>
2026-02-01 01:43:00 +0100

    5M, eos no log

--- ci/orchestration/setup/configure_eoscta_tape.sh
+++ ci/orchestration/setup/configure_eoscta_tape.sh
@@ -22,6 +22,7 @@ eos space config default space.scanrate=0
 eos space config default space.scaninterval=0
 eos attr -r set default=replica /eos
 eos attr -r set sys.forced.nstripes=1 /eos
+eos debug err "*"
 
 # Users
 groupadd --gid 1100 eosusers
