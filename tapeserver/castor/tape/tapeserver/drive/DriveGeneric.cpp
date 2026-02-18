f74e984416c1c1fd66bc5e925a0a825801c6aca9
okilicki <ozlem.kilickiran@cern.ch>
2026-02-18 17:43:05 +0100

    Format-cpp

--- tapeserver/castor/tape/tapeserver/drive/DriveGeneric.cpp
+++ tapeserver/castor/tape/tapeserver/drive/DriveGeneric.cpp
@@ -86,7 +86,7 @@ std::string drive::getSerialNumber(const int& fd, System::virtualWrapper& sw) {
   return serialNumber;
 }
 
-drive::DriveGeneric::DriveGeneric(SCSI::DeviceInfo di, System::virtualWrapper& sw): m_SCSIInfo(di), m_sysWrapper(sw) {
+drive::DriveGeneric::DriveGeneric(SCSI::DeviceInfo di, System::virtualWrapper& sw) : m_SCSIInfo(di), m_sysWrapper(sw) {
   /* Open the device files */
   /* We open the tape device file non-blocking as blocking open on rewind tapes (at least)
    * will fail after a long timeout when no tape is present (at least with mhvtl)
