6b318019bf0d58f6d7d9b9fa693d70ea714e323c
Niels Alexander Buegel <niels.alexander.bugel@cern.ch>
2026-04-16 07:14:51 +0000

    [CI] Add Metrics Reports and Test Summary to merge requests (cta/CTA!1358)
    Closes #1604

--- ci/build/build_rpm.sh
+++ ci/build/build_rpm.sh
@@ -250,6 +250,7 @@ build_rpm() {
 
   # Cmake
   export CTA_VERSION=${cta_version}
+  export GTEST_COLOR="yes"
 
   if [[ "${skip_cmake}" = false ]]; then
     # Needs to be exported as cmake gets it from the environment
