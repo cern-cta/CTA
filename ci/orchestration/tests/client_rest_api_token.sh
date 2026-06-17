8e010275ec39bcae1333a429dc35ebf47dded700
Joao Afonso <joao.afonso@cern.ch>
2026-06-17 11:52:10 +0200

    Reducing tests to t2o files

--- ci/orchestration/tests/client_rest_api_token.sh
+++ ci/orchestration/tests/client_rest_api_token.sh
@@ -3,37 +3,20 @@
 # SPDX-FileCopyrightText: 2022 CERN
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-# Test CTA Tape Rest API compliance.
+# Test CTA Tape Rest API compliance using WLCG tokens
 
-#. /root/client_helper.sh
-
-#admin_kdestroy &>/dev/null
-#admin_kinit &>/dev/null
-
-#eosadmin_kdestroy &>/dev/null
-#eosadmin_kinit &>/dev/null
-
-#NOTE: In this context it should be eos service names.
 EOS_MGM_HOST="ctaeos"
 EOS_MGM_PORT="8443"
-NOW=$(date +%s)
-LATER=$(echo "${NOW}+86400" | bc)
 
-# Generate Tokens
-#TOKEN_EOSUSER1=$(eosadmin_eos root://"${EOS_MGM_HOST}" token --tree --path '/eos/ctaeos/://:/api/' --expires "${LATER}" --owner user1 --group eosusers --permission rwx)
-#TOKEN_EOSPOWER1=$(eosadmin_eos root://"${EOS_MGM_HOST}" token --tree --path '/eos/ctaeos/://:/api/' --expires "${LATER}" --owner poweruser1 --group powerusers --permission prwx)
+# Get WLCG tokens
 
-# Get locally generated SciToken
 WLCG_TOKEN_OTHER=$(cat /etc/cta/wlcg-token-other.jwt)
 WLCG_TOKEN_STAGE_ALL=$(cat /etc/cta/wlcg-token-stage-all.jwt)
-#SCI_TOKEN_EXP=$(echo $SCI_TOKEN | cut -d. -f2 | base64 --decode | jq '.exp')
-#if [ -z "$SCI_TOKEN_EXP" ] || [ "$SCI_TOKEN_EXP" -lt "$(date +%s)" ]; then
-#  echo "$(date +%s): SCI_TOKEN expired on $SCI_TOKEN_EXP."
-#  exit 1
-#fi
+WLCG_TOKEN_POLL_ALL=$(cat /etc/cta/wlcg-token-poll-all.jwt)
+WLCG_TOKEN_STAGE_TEST1=$(cat /etc/cta/wlcg-token-stage-test1.jwt)
 
 # By default check https connections certificates
-# disable for now on Alma9 so that this is not on the critical path
+
 CHECK_CERTIFICATES=1
 grep -qi almalinux /etc/redhat-release 2>/dev/null && CHECK_CERTIFICATES=0 # Disable for now in Alma9
 test ${CHECK_CERTIFICATES} -eq 0 && echo -e "\n\nWARNING: Certificate checks are disabled in this test.\n\n"
@@ -41,52 +24,35 @@ test ${CHECK_CERTIFICATES} -eq 0 && echo -e "\n\nWARNING: Certificate checks are
 CURL_OPTS=""
 test 0 -eq ${CHECK_CERTIFICATES} && CURL_OPTS+="--insecure"
 
-#echo "Printing eosuser token dump"
-#eos root://"${EOS_MGM_HOST}" token --token "${TOKEN_EOSUSER1}" | jq .
-#echo
-#echo "Printing poweruser token dump"
-#eos root://"${EOS_MGM_HOST}" token --token "${TOKEN_EOSPOWER1}" | jq .
-#echo
-
-# Discover endpoint
+# Discover Tape REST API endpoint on EOS
 
 REST_API_URI=$(curl ${CURL_OPTS} "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/.well-known/wlcg-tape-rest-api" | jq -r '.endpoints[] | select(.version == "v1") | .uri')
 
 ########################################################################################################################
-# Archive files with the sci_token
+# Archive files with the WLCG token
 ########################################################################################################################
 
 FILE1="/eos/ctaeos/preprod/test1/file"
 FILE2="/eos/ctaeos/preprod/test2/file"
-FILE3="/eos/ctaeos/preprod/test3/file"
-FILE4="/eos/ctaeos/preprod/test4/file"
 
 # Start by cleaning up any leftovers from previous tests
 
 curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X DELETE "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/${FILE1}"
 curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X DELETE "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/${FILE2}"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X DELETE "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/${FILE3}"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X DELETE "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/${FILE4}"
 
 curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X DELETE "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE1}")"
 curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X DELETE "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE2}")"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X DELETE "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE3}")"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X DELETE "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE4}")"
 
 # Now add the new files
-
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X MKCOL "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE1}")"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X MKCOL "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE2}")"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X MKCOL "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE3}")"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X MKCOL "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE4}")"
 
 tmp_file=$(mktemp)
 echo "Dummy" > "${tmp_file}"
 
+curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X MKCOL "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE1}")"
+curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" -X MKCOL "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/$(dirname "${FILE2}")"
+
 curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/${FILE1}" --upload-file "${tmp_file}"
 curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/${FILE2}" --upload-file "${tmp_file}"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/${FILE3}" --upload-file "${tmp_file}"
-curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_OTHER}" "https://${EOS_MGM_HOST}:${EOS_MGM_PORT}/${FILE4}" --upload-file "${tmp_file}"
 
 rm -f "${tmp_file}"
 
@@ -94,7 +60,7 @@ rm -f "${tmp_file}"
 # Check that files have been archived with the sci_token and Tape REST API (ARCHIVEINFO)
 ########################################################################################################################
 
-ARCHIVEINFO_REQ_BODY="{\"paths\":[\"${FILE1}\", \"${FILE2}\", \"${FILE3}\", \"${FILE4}\"]}"
+ARCHIVEINFO_REQ_BODY="{\"paths\":[\"${FILE1}\", \"${FILE2}\"]}"
 
 echo "Archiving files..."
 
@@ -132,7 +98,7 @@ FINAL_COUNT=0
 TIMEOUT=90
 SECONDS_PASSED=0
 
-STAGE_REQ_BODY="{\"files\":[{\"path\":\"${FILE1}\"}, {\"path\":\"${FILE2}\"}, {\"path\":\"${FILE3}\"}, {\"path\":\"${FILE4}\"}]}"
+STAGE_REQ_BODY="{\"files\":[{\"path\":\"${FILE1}\"}, {\"path\":\"${FILE2}\"}]}"
 REQ_ID=$(curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_STAGE_ALL}" "${REST_API_URI}/stage/" -d "$STAGE_REQ_BODY" | jq -r .requestId)
 
 # Wait for files to be staged
@@ -175,7 +141,7 @@ echo "Releasing files..."
 FINAL_COUNT=0
 TIMEOUT=90
 SECONDS_PASSED=0
-RELEASE_REQ_BODY="{\"paths\":[\"${FILE1}\", \"${FILE2}\", \"${FILE3}\", \"${FILE4}\"]}"
+RELEASE_REQ_BODY="{\"paths\":[\"${FILE1}\", \"${FILE2}\"]}"
 curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_STAGE_ALL}" "${REST_API_URI}/release/${REQ_ID}" -d "${RELEASE_REQ_BODY}"
 
 while test "${FINAL_COUNT}" -ne 4; do
@@ -205,7 +171,7 @@ echo "Cancelling files..."
 FINAL_COUNT=0
 TIMEOUT=90
 SECONDS_PASSED=0
-CANCEL_REQ_BODY="{\"paths\":[\"${FILE1}\", \"${FILE2}\", \"${FILE3}\", \"${FILE4}\"]}"
+CANCEL_REQ_BODY="{\"paths\":[\"${FILE1}\", \"${FILE2}\"]}"
 curl ${CURL_OPTS} -L -s -H "Accept: application/json" -H "Authorization: Bearer ${WLCG_TOKEN_STAGE_ALL}" "${REST_API_URI}/stage/${REQ_ID}/cancel" -d "${CANCEL_REQ_BODY}"
 
 while test "${FINAL_COUNT}" -ne 4; do
