c387b2f518401e4a379af17756933dc04193a6d4
nbugel <niels.alexander.bugel@cern.ch>
2026-08-14 15:37:15 +0200

    Make cta-rmcd spawn correctly

--- ci/orchestration/helm/rmcd/scripts/rmcd.sh
+++ ci/orchestration/helm/rmcd/scripts/rmcd.sh
@@ -5,4 +5,4 @@
 
 # to get rmcd logs to stdout
 tail -F /var/log/cta/cta-rmcd.log 2>/dev/null &
-/usr/bin/cta-rmcd -f /dev/smc
+/usr/bin/cta-rmcd --log-file=/var/log/cta/cta-rmcd.log --config-strict --config /etc/cta/cta-rmcd.toml --runtime-dir /run/cta
