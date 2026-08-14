d65f4e90b1aca1162a589e62c90f31608d3e7164
nbugel <niels.alexander.bugel@cern.ch>
2026-08-14 15:37:15 +0200

    Make cta-rmcd spawn correctly

---
+++ mediachanger/rmcd/rmc_serv.hpp
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2026 CERN
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <string>
+
+int rmc_main(std::string robot, int port, std::string listen_scope);
