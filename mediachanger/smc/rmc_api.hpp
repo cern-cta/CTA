1b96f6931014341450b62bae532f1e5ae1f6df57
nbugel <niels.alexander.bugel@cern.ch>
2026-08-21 15:38:13 +0200

    Pass port explicitly

--- mediachanger/smc/rmc_api.hpp
+++ mediachanger/smc/rmc_api.hpp
@@ -8,24 +8,34 @@
 #include "mediachanger/librmc/smc_struct.hpp"
 #include "mediachanger/rmcd/rmc_constants.hpp"
 
-int rmc_dismount(const char* const server, const char* const vid, const int drvord, const int force);
+int rmc_dismount(const char* const server,
+                 const unsigned short port,
+                 const char* const vid,
+                 const int drvord,
+                 const int force);
 
 /**
  * Send error message to user-defined client buffer or to stderr
  */
 int rmc_errmsg(const char* const func, const char* const msg, ...);
 
-int rmc_export(const char* const server, const char* const vid);
+int rmc_export(const char* const server, const unsigned short port, const char* const vid);
 int rmc_find_cartridge(const char* const server,
+                       const unsigned short port,
                        const char* const pattern,
                        const int type,
                        const int startaddr,
                        const int nbelem,
                        struct smc_element_info* const element_info);
-int rmc_get_geometry(const char* const server, struct robot_info* const robot_info);
-int rmc_import(const char* const server, const char* const vid);
-int rmc_mount(const char* const server, const char* const vid, const int side, const int drvord);
+int rmc_get_geometry(const char* const server, const unsigned short port, struct robot_info* const robot_info);
+int rmc_import(const char* const server, const unsigned short port, const char* const vid);
+int rmc_mount(const char* const server,
+              const unsigned short port,
+              const char* const vid,
+              const int side,
+              const int drvord);
 int rmc_read_elem_status(const char* const server,
+                         const unsigned short port,
                          const int type,
                          const int startaddr,
                          const int nbelem,
@@ -36,9 +46,8 @@ int rmc_read_elem_status(const char* const server,
  */
 void rmc_seterrbuf(const char* const buffer, const int buflen);
 
-void rmc_set_port(const unsigned short port);
-
 int send2rmc(const char* const host,
+             const unsigned short port,
              const char* const reqp,
              const int reql,
              char* const user_repbuf,
