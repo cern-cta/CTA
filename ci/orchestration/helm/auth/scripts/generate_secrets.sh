ddd20b17b228c0e24e4f3e0c2e9c6785e95a74ad
Idriss Larbi <idriss.larbi@cern.ch>
2026-09-15 10:41:53 +0200

    fix generate_secrets.sh

--- ci/orchestration/helm/auth/scripts/generate_secrets.sh
+++ ci/orchestration/helm/auth/scripts/generate_secrets.sh
@@ -50,7 +50,7 @@ generate_signed_cert() {
   echo "Generating cert/key pair '$file_prefix' for '$host'"
 
   openssl genrsa -passout pass:1234 -des3 -out $key 4096
-  openssl rsa -passin pass:1234 -in jwt-private.pem -pubout -out $pub
+  openssl rsa -passin pass:1234 -in $key -pubout -out $pub
   openssl req -passin pass:1234 -new -key $key -out $csr \
     -subj "/C=CH/ST=Geneva/L=Geneva/O=Test/OU=Server/CN=$host" \
     -addext "subjectAltName=$subject_alt_names"
@@ -75,7 +75,6 @@ python3 /scripts/generate_jwt.py \
   --output-dir "$SECRETS_DIR" \
   --public-key "$SECRETS_DIR/server-admin.pub.pem" \
   --private-key "$SECRETS_DIR/server-admin.key.pem" \
-  --password pass:1234 \
   --jwks jwks.json \
   --issuer cta \
   --audience cta-admin \
@@ -86,7 +85,6 @@ python3 /scripts/generate_jwt.py \
   --output-dir "$SECRETS_DIR" \
   --public-key "$SECRETS_DIR/server-admin.pub.pem" \
   --private-key "$SECRETS_DIR/server-admin.key.pem" \
-  --password pass:1234 \
   --issuer cta \
   --audience cta-wfe \
   --sub ctaeos \
