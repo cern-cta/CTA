05b6d061c4d49451f589e0842baa6c793e4ea1a2
Pedro Ferreira <pedro.ferreira@cern.ch>
2026-09-17 17:25:14 +0200

    Fix unit tests

--- common/auth/jwt/JwkCacheTest.cpp
+++ common/auth/jwt/JwkCacheTest.cpp
@@ -29,33 +29,13 @@ public:
 
 private:
   std::string generateTestJWKS() {
-    std::string sample_cert_base64_der = "MIIDSTCCAjGgAwIBAgIUQQp5TK9J3SemQXrCF+ffmED4qy4wDQYJKoZIhvcNAQELBQAwTTELMAkG"
-                                         "A1UEBhMCQ0gxDzANBgNVBAgMBkdlbmV2YTEPMA0GA1UEBwwGR2VuZXZhMRwwGgYDVQQKDBNEZWZh"
-                                         "dWx0IENvbXBhbnkgTHRkMB4XDTI1MDcwOTA4NDYyN1oXDTM1MDcwNzA4NDYyN1owTTELMAkGA1UE"
-                                         "BhMCQ0gxDzANBgNVBAgMBkdlbmV2YTEPMA0GA1UEBwwGR2VuZXZhMRwwGgYDVQQKDBNEZWZhdWx0"
-                                         "IENvbXBhbnkgTHRkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh0gY8QsodbL06ls2"
-                                         "vRuY9ETBefO2llFkpfYpExdT0WVeNq7iV5AXD+pyI1rHt6ua59gnvhSFwpIqcMF1DXW4LuQIFy0h"
-                                         "TOYPDBpbt6YBDb5imHHosE+pWUu6fU/dBy0m0cp84z0/UHDuHQSYYsDMDLnSTIk/F8k4idPkZfoY"
-                                         "N2a7gNTiMfxM7MvoJkZ43FSU/LVnm2dymn+5LZJHT5+oZLx70tjNBqCSiroYTmHWnu79agWr0Yiv"
-                                         "3U9UKCkjmz3hHemcz5mJdoHtaVHe2+FoprnT0pY/nyLFcmlsTsIDYHTZRi9sfE/RnC2ANaWV4T3L"
-                                         "/DLPOghy56gGICRAXudUqwIDAQABoyEwHzAdBgNVHQ4EFgQUZxkzqXZASKTRanmOKg6r52Wcj1Mw"
-                                         "DQYJKoZIhvcNAQELBQADggEBACVb/KiCg1PD+DYSHet5eZ0sskx6AtB4CwCsErTzy4z6Noy3zSuH"
-                                         "3RjYFR/1nsG2M8ZMn6LrB3T6VCnGdZAc6DLHaDZWzt+8g1yNP/9+0p3H9FcemIOVEwdvE/ExwFu9"
-                                         "W0AKcHVrhUK7OT7RemSfEodzUU+e6Ze/2Joq1vDNW7/ui/pC8XDljqSkwJqPCJeU4KGlTtloXWPw"
-                                         "GREcpm5DVoJKJ9li9xIj2VHxmXPcdsmeiBL/5BB/1ldcOueirUPTyGiXxR2R1paHrjHZNBXKZ5Du"
-                                         "2N4HyvOmkj/xht5wkZU3OqA31aScrWF5MjMIu4FBVO3fY7El5s0rCp/cJivDq0Y=";
-
     std::string raw_jwks = R"({
     "keys": [{
         "kid": "test-kid",
         "alg": "RS256",
         "kty": "RSA",
         "use": "sig",
-        "x5c": [
-        ")" + sample_cert_base64_der
-                           +
-                           R"("
-        ],
+        "n": "h0gY8QsodbL06ls2vRuY9ETBefO2llFkpfYpExdT0WVeNq7iV5AXD-pyI1rHt6ua59gnvhSFwpIqcMF1DXW4LuQIFy0hTOYPDBpbt6YBDb5imHHosE-pWUu6fU_dBy0m0cp84z0_UHDuHQSYYsDMDLnSTIk_F8k4idPkZfoYN2a7gNTiMfxM7MvoJkZ43FSU_LVnm2dymn-5LZJHT5-oZLx70tjNBqCSiroYTmHWnu79agWr0Yiv3U9UKCkjmz3hHemcz5mJdoHtaVHe2-FoprnT0pY_nyLFcmlsTsIDYHTZRi9sfE_RnC2ANaWV4T3L_DLPOghy56gGICRAXudUqw",
         "e": "AQAB"
     }]
     })";
@@ -92,9 +72,7 @@ TEST(JwkCacheTest, UpdateCacheRemovesExpiredKeys) {
         "alg": "RS256",
         "kty": "RSA",
         "use": "sig",
-        "x5c": [
-        "MIIDSTCCAjGgAwIBAgIUQQp5TK9J3SemQXrCF+ffmED4qy4wDQYJKoZIhvcNAQELBQAwTTELMAkGA1UEBhMCQ0gxDzANBgNVBAgMBkdlbmV2YTEPMA0GA1UEBwwGR2VuZXZhMRwwGgYDVQQKDBNEZWZhdWx0IENvbXBhbnkgTHRkMB4XDTI1MDcwOTA4NDYyN1oXDTM1MDcwNzA4NDYyN1owTTELMAkGA1UEBhMCQ0gxDzANBgNVBAgMBkdlbmV2QTEPMA0GA1UEBwwGR2VuZXZhMRwwGgYDVQQKDBNEZWZhdWx0IENvbXBhbnkgTHRkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh0gY8QsodbL06ls2vRuY9ETBefO2llFkpfYpExdT0WVeNq7iV5AXD+pyI1rHt6ua59gnvhSFwpIqcMF1DXW4LuQIFy0hTOYPDBpbt6YBDb5imHHosE+pWUu6fU/dBy0m0cp84z0/UHDuHQSYYsDMDLnSTIk/F8k4idPkZfoYN2a7gNTiMfxM7MvoJkZ43FSU/LVnm2dymn+5LZJHT5+oZLx70tjNBqCSiroYTmHWnu79agWr0Yiv3U9UKCkjmz3hHemcz5mJdoHtaVHe2+FoprnT0pY/nyLFcmlsTsIDYHTZRi9sfE/RnC2ANaWV4T3L/DLPOghy56gGICRAXudUqwIDAQABoyEwHzAdBgNVHQ4EFgQUZxkzqXZASKTRanmOKg6r52Wcj1MwDQYJKoZIhvcNAQELBQADggEBACVb/KiCg1PD+DYSHet5eZ0sskx6AtB4CwCsErTzy4z6Noy3zSuH3RjYFR/1nsG2M8ZMn6LrB3T6VCnGdZAc6DLHaDZWzt+8g1yNP/9+0p3H9FcemIOVEwdvE/ExwFu9W0AKcHVrhUK7OT7RemSfEodzUU+e6Ze/2Joq1vDNW7/ui/pC8XDljqSkwJqPCJeU4KGlTtloXWPwGREcpm5DVoJKJ9li9xIj2VHxmXPcdsmeiBL/5BB/1ldcOueirUPTyGiXxR2R1paHrjHZNBXKZ5Du2N4HyvOmkj/xht5wkZU3OqA31aScrWF5MjMIu4FBVO3fY7El5s0rCp/cJivDq0Y="
-        ],
+        "n": "h0gY8QsodbL06ls2vRuY9ETBefO2llFkpfYpExdT0WVeNq7iV5AXD-pyI1rHt6ua59gnvhSFwpIqcMF1DXW4LuQIFy0hTOYPDBpbt6YBDb5imHHosE-pWUu6fU_dBy0m0cp84z0_UHDuHQSYYsDMDLnSTIk_F8k4idPkZfoYN2a7gNTiMfxM7MvoJkZ43FSU_LVnm2dymn-5LZJHT5-oZLx70tjNBqCSiroYTmHWnu79agWr0Yiv3U9UKCkjmz3hHemcz5mJdoHtaVHe2-FoprnT0pY_nyLFcmlsTsIDYHTZRi9sfE_RnC2ANaWV4T3L_DLPOghy56gGICRAXudUqw",
         "e": "AQAB"
     }]
     })";
